ac0190ce9e2967abdc30fcf3dee70b0a40c619fe
sgbridges <sean.bridges@gmail.com>
2002-01-23 20:02:25 +0000

    make m_id private
    git-svn-id: https://svn.code.sf.net/p/triplea/code/trunk/triplea@12 a4c711aa-641f-0410-879d-9b464e52b682

--- src/games/strategy/net/GUID.java
+++ src/games/strategy/net/GUID.java
@@ -1,4 +1,18 @@
 /*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+/*
  * GUID.java
  *
  * Created on January 4, 2002, 2:33 PM
@@ -18,7 +32,7 @@ import java.io.Serializable;
  */
 public class GUID implements Serializable
 {
-	String m_id;
+	private String m_id;
 	
 	/**
 	 * Creates an id based on the given int. <br>
@@ -27,10 +41,10 @@ public class GUID implements Serializable
 	 * GUID's created in this way will be equivalent across
 	 * vm's.
 	 */
-    public GUID(int id) 
+	public GUID(int id) 
 	{
 		m_id = String.valueOf(id);
-    }
+	}
 
 	public GUID() 
 	{
