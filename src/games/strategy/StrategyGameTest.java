a68f38411459e9185137af223532d7cef535940e
sgbridges <sean.bridges@gmail.com>
2002-01-27 23:43:23 +0000

    adding thread pool
    git-svn-id: https://svn.code.sf.net/p/triplea/code/trunk/triplea@20 a4c711aa-641f-0410-879d-9b464e52b682

--- src/games/strategy/StrategyGameTest.java
+++ src/games/strategy/StrategyGameTest.java
@@ -1,4 +1,18 @@
 /*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+/*
  * StrategyGameTest.java
  *
  * Created on October 12, 2001, 2:10 PM
@@ -27,6 +41,7 @@ public class StrategyGameTest extends TestCase
 		suite.addTestSuite(games.strategy.engine.data.SerializationTest.class);
 		suite.addTestSuite(games.strategy.engine.message.ManagerTest.class);
 		suite.addTestSuite(games.strategy.net.MessengerTest.class);
+		suite.addTestSuite(games.strategy.thread.ThreadPoolTest.class);
 		
 		return suite;
 	}
