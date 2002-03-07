7263773352403bf1914a5cf0387026e1eac572ac
bcarothers <bcarothers@mycompany.com>
2002-03-07 22:58:27 +0000

    Added file
    git-svn-id: https://svn.code.sf.net/p/triplea/code/trunk/triplea@41 a4c711aa-641f-0410-879d-9b464e52b682

---
+++ src/games/strategy/engine/framework/IGameLoader.java
@@ -0,0 +1,43 @@
+/*
+ * IGameLoader.java
+ *
+ * Created on January 29, 2002, 12:25 PM
+ */
+
+package games.strategy.engine.framework;
+
+import java.util.*;
+
+/**
+ * 
+ * A game loader is responsible for telling the framework 
+ * what types of players are available, for creating players, and
+ * starting the game.
+ *
+ * @author  Sean Bridges
+ */
+public interface IGameLoader 
+{
+	/**
+	 * Return an array of player types that can play on the server.
+	 * This array must not contain any entries that could play on the client.
+	 */
+	String[] getServerPlayerTypes();
+	
+	/**
+	 * Return an array of player types that can play on the client.
+	 * This array must not contain any entries that could play on the server.
+	 */
+	String[] getClientPlayerTypes();
+	
+	/**
+	 * Create the players.  Given a map of playerName -> type, 
+	 * where type is one of the Strings returned by a get*PlayerType() method.
+	 */
+	Set createPlayers(Map players);
+	
+	/**
+	 * The game is about to start.
+	 */
+	void startGame(IGame game, Set players);
+}
