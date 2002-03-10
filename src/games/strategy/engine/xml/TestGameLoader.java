3e33ed765fc62167c91094f2b2eab39e9ab6ef68
sgbridges <sean.bridges@gmail.com>
2002-03-10 22:16:18 +0000

    added file
    git-svn-id: https://svn.code.sf.net/p/triplea/code/trunk/triplea@44 a4c711aa-641f-0410-879d-9b464e52b682

---
+++ src/games/strategy/engine/xml/TestGameLoader.java
@@ -0,0 +1,52 @@
+/*
+ * TestGameLoader.java
+ *
+ * Created on January 29, 2002, 12:38 PM
+ */
+
+package games.strategy.engine.xml;
+
+import java.util.*;
+
+import games.strategy.engine.framework.IGameLoader;
+import games.strategy.engine.framework.IGame;
+
+/**
+ *
+ * @author  Sean Bridges
+ */
+public class TestGameLoader implements IGameLoader
+{
+	/**
+	 * Return an array of player types that can play on the server.
+	 * This array must not contain any entries that could play on the client.
+	 */
+	public String[] getServerPlayerTypes()
+	{
+		return null;
+	}
+	
+	/**
+	 * Return an array of player types that can play on the client.
+	 * This array must not contain any entries that could play on the server.
+	 */
+	public String[] getClientPlayerTypes()
+	{
+		return null;
+	}
+	
+	/**
+	 * The game is about to start.
+	 */
+	public void startGame(IGame game, Set players)
+	{}
+	
+	/**
+	 * Create the players.  Given a map of playerName -> type,
+	 * where type is one of the Strings returned by a get*PlayerType() method.
+	 */
+	public Set createPlayers(Map players)
+	{
+		return null;
+	}	
+}
