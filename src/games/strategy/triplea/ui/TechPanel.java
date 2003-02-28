ca3d29d32cd758ab808638de6680bfefe87a52fd
sgbridges <sean.bridges@gmail.com>
2003-02-28 07:50:50 +0000

    improve look of tech roll pop up
    git-svn-id: https://svn.code.sf.net/p/triplea/code/trunk/triplea@108 a4c711aa-641f-0410-879d-9b464e52b682

--- src/games/strategy/triplea/ui/TechPanel.java
+++ src/games/strategy/triplea/ui/TechPanel.java
@@ -40,121 +40,124 @@ import games.strategy.triplea.delegate.message.*;
  */
 public class TechPanel extends ActionPanel
 {
-	private JLabel m_actionLabel = new JLabel();
-	private IntegerMessage m_intMessage;
-	
-	/** Creates new BattlePanel */
-    public TechPanel(GameData data, MapPanel map) 
-	{
-		super(data, map);
+  private JLabel m_actionLabel = new JLabel();
+  private IntegerMessage m_intMessage;
+
+  /** Creates new BattlePanel */
+    public TechPanel(GameData data, MapPanel map)
+  {
+    super(data, map);
     }
-		
-	public void display(PlayerID id)
-	{
-		super.display(id);
-		removeAll();
-		m_actionLabel.setText(id.getName() + " Tech Roll");
-		add(m_actionLabel);	
-		add(new JButton(GetTechRollsAction));
-		add(new JButton(DontBother));
-		
-	}
-	
-	public String toString()
-	{
-		return "TechPanel";
-	}
-	
-	public IntegerMessage waitForTech()
-	{
-		try
-		{
-			synchronized(getLock())
-			{
-				getLock().wait();
-			}
-		} catch(InterruptedException ie)
-		{
-			waitForTech();
-		}
-		
-		if(m_intMessage == null)
-			return null;
-		
-		if(m_intMessage.getMessage() == 0)
-			return null;
 
-		return m_intMessage;
-	}
-	
-	private Action GetTechRollsAction = new AbstractAction("Roll Tech")
-	{
-		public void actionPerformed(ActionEvent event)
-		{
-			int ipcs = getCurrentPlayer().getResources().getQuantity(Constants.IPCS);
-			String message = "Select number of tech rolls";
-			TechRollPanel techRollPanel = new TechRollPanel(ipcs);
-			int choice = JOptionPane.showConfirmDialog(getTopLevelAncestor(), techRollPanel, message, JOptionPane.OK_CANCEL_OPTION);
-			if(choice != JOptionPane.OK_OPTION)
-				return;
-			
-			int quantity = techRollPanel.getValue();
-			m_intMessage =  new IntegerMessage(quantity);
-			synchronized(getLock())
-			{
-				getLock().notifyAll();
-			}
-			
-		}
-	};
-	
-	private Action DontBother = new AbstractAction("Done")
-	{
-		public void actionPerformed(ActionEvent event)
-		{
-			synchronized(getLock())
-			{
-				m_intMessage = null;
-				getLock().notifyAll();
-			}
-		}
-	};
+  public void display(PlayerID id)
+  {
+    super.display(id);
+    removeAll();
+    m_actionLabel.setText(id.getName() + " Tech Roll");
+    add(m_actionLabel);
+    add(new JButton(GetTechRollsAction));
+    add(new JButton(DontBother));
 
-	
+  }
+
+  public String toString()
+  {
+    return "TechPanel";
+  }
+
+  public IntegerMessage waitForTech()
+  {
+    try
+    {
+      synchronized(getLock())
+      {
+        getLock().wait();
+      }
+    } catch(InterruptedException ie)
+    {
+      waitForTech();
+    }
+
+    if(m_intMessage == null)
+      return null;
+
+    if(m_intMessage.getMessage() == 0)
+      return null;
+
+    return m_intMessage;
+  }
+
+  private Action GetTechRollsAction = new AbstractAction("Roll Tech")
+  {
+    public void actionPerformed(ActionEvent event)
+    {
+      int ipcs = getCurrentPlayer().getResources().getQuantity(Constants.IPCS);
+      String message = "Roll Tech";
+      TechRollPanel techRollPanel = new TechRollPanel(ipcs);
+      int choice = JOptionPane.showConfirmDialog(getTopLevelAncestor(), techRollPanel, message, JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE, null);
+      if(choice != JOptionPane.OK_OPTION)
+        return;
+
+      int quantity = techRollPanel.getValue();
+      m_intMessage =  new IntegerMessage(quantity);
+      synchronized(getLock())
+      {
+        getLock().notifyAll();
+      }
+
+    }
+  };
+
+  private Action DontBother = new AbstractAction("Done")
+  {
+    public void actionPerformed(ActionEvent event)
+    {
+      synchronized(getLock())
+      {
+        m_intMessage = null;
+        getLock().notifyAll();
+      }
+    }
+  };
+
+
 }
 
 class TechRollPanel extends JPanel
 {
-	int m_ipcs;
-	JLabel m_left = new JLabel();
-	ScrollableTextField m_text;
-	
-	TechRollPanel(int ipcs)
-	{
-		setLayout(new BorderLayout());
-		m_ipcs = ipcs;
-		m_text = new ScrollableTextField(0, ipcs / Constants.TECH_ROLL_COST);
-		m_text.addChangeListener(m_listener);
-		setLabel(ipcs);
-		add(m_text, BorderLayout.CENTER);
-		add(m_left, BorderLayout.SOUTH);
-	}
-	
-	private void setLabel(int ipcs)
-	{
-		m_left.setText("Left to spend:" + ipcs);
-	}
-	
-	private ScrollableTextFieldListener m_listener = new ScrollableTextFieldListener()
-	{
-		public void changedValue(ScrollableTextField stf)
-		{
-			setLabel(m_ipcs - (Constants.TECH_ROLL_COST * m_text.getValue()));
-		}
-	};
-	
-	public int getValue()
-	{
-		return m_text.getValue();
-	}
+  int m_ipcs;
+  JLabel m_left = new JLabel();
+  ScrollableTextField m_text;
+
+  TechRollPanel(int ipcs)
+  {
+    setLayout(new BorderLayout());
+    m_ipcs = ipcs;
+    JLabel title = new JLabel("Select the number of tech rolls:");
+    title.setBorder(new javax.swing.border.EmptyBorder(5,5,5,5));
+    m_text = new ScrollableTextField(0, ipcs / Constants.TECH_ROLL_COST);
+    m_text.addChangeListener(m_listener);
+    setLabel(ipcs);
+    add(title, BorderLayout.NORTH);
+    add(m_text, BorderLayout.CENTER);
+    add(m_left, BorderLayout.SOUTH);
+  }
+
+  private void setLabel(int ipcs)
+  {
+    m_left.setText("Left to spend:" + ipcs);
+  }
+
+  private ScrollableTextFieldListener m_listener = new ScrollableTextFieldListener()
+  {
+    public void changedValue(ScrollableTextField stf)
+    {
+      setLabel(m_ipcs - (Constants.TECH_ROLL_COST * m_text.getValue()));
+    }
+  };
+
+  public int getValue()
+  {
+    return m_text.getValue();
+  }
 }
