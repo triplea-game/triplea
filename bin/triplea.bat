9498bd31f159c34a05ed7f869c57c428013ab215
sgbridges <sean.bridges@gmail.com>
2003-03-08 19:06:33 +0000

    add 2dnodraw for windows, makes some machines display better add kenstuff.jar to classpath
    git-svn-id: https://svn.code.sf.net/p/triplea/code/trunk/triplea@120 a4c711aa-641f-0410-879d-9b464e52b682

--- bin/triplea.bat
+++ bin/triplea.bat
@@ -1,1 +1,1 @@
-java -Xincgc -classpath ../classes;../lib/crimson.jar;../lib/jaxp.jar  games.strategy.engine.framework.GameRunner 
+java -Dsun.java2d.nodraw=true  -Xincgc -classpath ../classes;../lib/crimson.jar;../lib/jaxp.jar;../lib/kunststoff.jar  games.strategy.engine.framework.GameRunner 
