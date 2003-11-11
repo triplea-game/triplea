7d6ca3da9994cc1c7076b1a624a1ed7da902e33d
sgbridges <sean.bridges@gmail.com>
2003-11-11 22:26:23 +0000

    add undo move feature
    git-svn-id: https://svn.code.sf.net/p/triplea/code/trunk/triplea@174 a4c711aa-641f-0410-879d-9b464e52b682

---
+++ src/games/strategy/triplea/delegate/message/UndoMoveMessage.java
@@ -0,0 +1,18 @@
+package games.strategy.triplea.delegate.message;
+
+import games.strategy.engine.message.*;
+
+/**
+ * <p>Title: </p>
+ * <p>Description: </p>
+ * <p>Copyright: </p>
+ * <p>Company: </p>
+ * @author not attributable
+ * @version 1.0
+ */
+
+public class UndoMoveMessage implements Message
+{
+
+
+}
