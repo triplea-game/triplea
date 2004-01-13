f3e0570ae5cb5203819032d3078556d8ac023a87
sgbridges <sean.bridges@gmail.com>
2004-01-13 02:12:23 +0000

    adding image utility files
    git-svn-id: https://svn.code.sf.net/p/triplea/code/trunk/triplea@258 a4c711aa-641f-0410-879d-9b464e52b682

---
+++ src/util/image/PlacementPicker.java
@@ -0,0 +1,300 @@
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+
+package util.image;
+
+
+
+import java.util.*;
+import java.util.List;
+
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
+import java.awt.image.BufferedImage;
+import javax.swing.*;
+
+import games.strategy.ui.Util;
+import games.strategy.util.*;
+import java.io.*;
+import java.awt.event.*;
+
+
+public class PlacementPicker extends JFrame
+{
+    private Point m_currentSquare;
+    private Image m_image;
+    private JLabel m_location = new JLabel();
+    private Map m_polygons = new HashMap();
+
+    private Map m_placements;
+    private List m_currentPlacements;
+    private String m_currentCountry;
+
+    private static final int PLACE_SIZE = 48;
+
+    public PlacementPicker(String fileName)
+    {
+        super("Center Picker");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        try
+        {
+            m_polygons = new PointFileReaderWriter().readOneToManyPolygons(new FileInputStream("polygons.txt"));
+        }
+        catch (IOException ex1)
+        {
+            ex1.printStackTrace();
+        }
+
+
+        m_image = Toolkit.getDefaultToolkit().createImage(fileName);
+
+        try
+        {
+            Util.ensureImageLoaded(m_image, this);
+        }
+        catch (InterruptedException ex)
+        {
+            ex.printStackTrace();
+        }
+
+
+        JPanel imagePanel = createMainPanel();
+
+        imagePanel.addMouseMotionListener(
+        new MouseMotionAdapter()
+        {
+
+            public void mouseMoved(MouseEvent e)
+             {
+                 m_location.setText("x:"+ e.getX() + " y:" + e.getY());
+                 m_currentSquare = new Point(e.getPoint());
+                 repaint();
+
+             }
+        }
+        );
+
+        imagePanel.addMouseListener(
+        new MouseAdapter()
+        {
+            public void mouseClicked(MouseEvent e)
+            {
+                mouseEvent(e.getPoint(), e.isControlDown(), SwingUtilities.isRightMouseButton(e));
+            }
+        }
+
+        );
+
+        imagePanel.setMinimumSize(new Dimension( m_image.getWidth(this), m_image.getHeight(this)));
+        imagePanel.setPreferredSize(new Dimension( m_image.getWidth(this), m_image.getHeight(this)));
+        imagePanel.setMaximumSize(new Dimension( m_image.getWidth(this), m_image.getHeight(this)));
+
+        this.getContentPane().setLayout(new BorderLayout());
+        this.getContentPane().add(new JScrollPane(imagePanel),  BorderLayout.CENTER);
+        this.getContentPane().add(m_location, BorderLayout.SOUTH);
+
+        JToolBar toolBar = new JToolBar();
+        toolBar.add(new AbstractAction("Save")
+        {
+            public void actionPerformed(ActionEvent e)
+            {
+                save();
+            }
+        }
+        );
+
+        toolBar.add(new AbstractAction("Load")
+       {
+           public void actionPerformed(ActionEvent e)
+           {
+               load();
+           }
+       }
+       );
+
+
+        this.getContentPane().add(toolBar, BorderLayout.NORTH);
+    }
+
+    private JPanel createMainPanel()
+    {
+        JPanel imagePanel = new JPanel()
+        {
+            public void paint(Graphics g)
+            {
+                //super.paint(g);
+                g.drawImage(m_image, 0, 0, this);
+                g.setColor(Color.red);
+
+                if(m_currentSquare != null)
+                {
+                    g.drawRect(m_currentSquare.x, m_currentSquare.y, PLACE_SIZE, PLACE_SIZE);
+                }
+
+                if(m_currentPlacements == null)
+                    return;
+
+                 Iterator pointIter = m_currentPlacements.iterator();
+                 while (pointIter.hasNext())
+                 {
+                     Point item = (Point)pointIter.next();
+                     g.fillRect(item.x, item.y, PLACE_SIZE ,PLACE_SIZE);
+                 }
+            }
+        };
+        return imagePanel;
+    }
+
+
+
+    private void save()
+    {
+
+        try
+        {
+            String fileName = JOptionPane.showInputDialog(this, "enter name to save");
+            if (fileName.trim().length() == 0)
+                return;
+            FileOutputStream out = new FileOutputStream(fileName);
+            new PointFileReaderWriter().writeOneToMany(out, m_placements);
+            out.flush();
+            out.close();
+            System.out.println("Data written to :" + new File(fileName).getCanonicalPath());
+        }
+
+        catch (FileNotFoundException ex)
+        {
+            ex.printStackTrace();
+        }
+        catch (HeadlessException ex)
+        {
+            ex.printStackTrace();
+        }
+        catch (Exception ex)
+        {
+            ex.printStackTrace();
+        }
+
+    }
+
+    private void load()
+    {
+        try
+         {
+             String fileName = JOptionPane.showInputDialog(this, "enter name to load");
+             if(fileName.trim().length() == 0)
+                 return;
+             FileInputStream in = new FileInputStream(fileName);
+             m_placements = new PointFileReaderWriter().readOneToMany(in);
+             repaint();
+         }
+         catch (FileNotFoundException ex)
+         {
+             ex.printStackTrace();
+         }
+         catch (IOException ex)
+         {
+             ex.printStackTrace();
+         }
+         catch (HeadlessException ex)
+         {
+             ex.printStackTrace();
+         }
+
+
+
+    }
+
+
+    private String findTerritoryName(Point p)
+    {
+        String seaName = "there be dragons";
+
+        //try to find a land territory.
+        //sea zones often surround a land territory
+        Iterator keyIter = m_polygons.keySet().iterator();
+        while (keyIter.hasNext())
+        {
+            String name = (String)keyIter.next();
+            Collection polygons = (Collection) m_polygons.get(name);
+            Iterator polyIter = polygons.iterator();
+            while (polyIter.hasNext())
+            {
+                Polygon poly = (Polygon)polyIter.next();
+                if(poly.contains(p))
+                {
+                    if(name.endsWith("Sea Zone"))
+                    {
+                        seaName = name;
+                    }
+                    else
+                        return name;
+                }
+
+            }
+        }
+        return seaName;
+    }
+
+    private void mouseEvent(Point point, boolean ctrlDown, boolean rightMouse)
+    {
+        //left button start in territory
+        //left button + control, add point
+        //right button and cntrl write
+        //right button remove last
+        if(!rightMouse && !ctrlDown)
+        {
+            m_currentCountry = findTerritoryName(point);
+            m_currentPlacements =new ArrayList( (List) m_placements.get(m_currentCountry));
+
+            JOptionPane.showMessageDialog(this, m_currentCountry);
+        }
+
+        else if(!rightMouse && ctrlDown)
+        {
+            m_currentPlacements.add(point);
+        }
+
+        else if (rightMouse && ctrlDown)
+        {
+            m_placements.put(m_currentCountry, m_currentPlacements);
+            m_currentPlacements = new ArrayList();
+            System.out.println("done:" + m_currentCountry);
+        }
+        else if (rightMouse)
+        {
+            if(!m_currentPlacements.isEmpty())
+                m_currentPlacements.remove(m_currentPlacements.size() -1);
+        }
+
+
+        repaint();
+
+
+    }
+
+    public static void main(String[] args)
+     {
+         String fileName = "/home/sgb/dev/triplea/additonalImageData/baseMap.gif";
+         PlacementPicker picker = new PlacementPicker(fileName);
+         picker.setSize(600,550);
+         picker.show();
+     }
+
+
+}
---
+++ src/util/image/ReliefImageBreaker.java
@@ -0,0 +1,221 @@
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+package util.image;
+
+import games.strategy.util.PointFileReaderWriter;
+import java.io.*;
+import java.util.Map;
+import games.strategy.ui.Util;
+import java.awt.*;
+import java.awt.image.*;
+import java.util.*;
+import javax.swing.*;
+import javax.imageio.*;
+import java.awt.Graphics2D;
+import games.strategy.ui.*;
+import games.strategy.triplea.ui.*;
+
+/**
+ * Utility for breaking an image into seperate smaller images.
+ */
+
+public class ReliefImageBreaker
+{
+    private static JFrame observer = new JFrame();
+
+    private static Image loadImage(String name)
+    {
+        File f = new File(name);
+        if(!f.exists())
+            throw new IllegalArgumentException("file not found");
+        Image img = Toolkit.getDefaultToolkit().createImage(name);
+        MediaTracker tracker = new MediaTracker(new Panel());
+        tracker.addImage(img, 1);
+        try
+        {
+            tracker.waitForAll();
+            return img;
+        }
+        catch (InterruptedException ie)
+        {
+            ie.printStackTrace();
+            return loadImage(name);
+        }
+    }
+
+    //for the relief map
+//    public static final String LARGE_MAP_FILENAME = "../additonalImageData/relief.png";
+//    public static final String SMALL_MAPS_LOCATION = "newImages";
+//    public static final boolean ONLY_DO_SEA_ZONES= false;
+
+    //for the sea map
+    public static final String LARGE_MAP_FILENAME = "/home/sgb/dev/triplea/data/games/strategy/triplea/image/images/maps/largeMap.gif";
+    public static final String SMALL_MAPS_LOCATION = "newImages";
+    public static final boolean ONLY_DO_SEA_ZONES= true;
+
+
+
+    public static void main(String[] args) throws Exception
+    {
+        new ReliefImageBreaker().createMaps();
+    }
+
+    public void createMaps() throws IOException
+    {
+
+        Image map = loadImage(LARGE_MAP_FILENAME);
+
+        Iterator unitIter = TerritoryData.getInstance().getTerritories().iterator();
+
+        while (unitIter.hasNext())
+        {
+
+            String territoryName = (String) unitIter.next();
+            boolean seaZone = territoryName.endsWith("Sea Zone");
+            if(!seaZone && ONLY_DO_SEA_ZONES)
+                continue;
+            if(seaZone && !ONLY_DO_SEA_ZONES)
+                continue;
+
+
+            processImage(territoryName, map);
+        }
+    }
+
+
+    private void processImage(String territory, Image map) throws IOException
+    {
+        Rectangle bounds = TerritoryData.getInstance().getBoundingRect(territory);
+        int width = bounds.width;
+        int height = bounds.height;
+
+        BufferedImage alphaChannelImage = Util.createImage(bounds.width, bounds.height, true);
+        Iterator iter = TerritoryData.getInstance().getPolygons(territory).iterator();
+        while (iter.hasNext())
+        {
+            Polygon item = (Polygon)iter.next();
+            item = new Polygon(item.xpoints, item.ypoints, item.npoints);
+            item.translate(-bounds.x, -bounds.y);
+            alphaChannelImage.getGraphics().fillPolygon(item);
+        }
+
+        GraphicsConfiguration   m_localGraphicSystem = GraphicsEnvironment.getLocalGraphicsEnvironment()
+       .getDefaultScreenDevice()
+       .getDefaultConfiguration();
+
+        BufferedImage relief = m_localGraphicSystem.createCompatibleImage(width,
+            height,
+            ONLY_DO_SEA_ZONES ? Transparency.BITMASK :  Transparency.TRANSLUCENT);
+        relief.getGraphics().drawImage(map, 0,0, width, height, bounds.x, bounds.y, bounds.x + width, bounds.y + height, observer);
+
+        blankOutline(alphaChannelImage, relief);
+
+        String outFileName = SMALL_MAPS_LOCATION + "/" + territory;
+        if (!ONLY_DO_SEA_ZONES)
+            outFileName += "_relief.png";
+        else
+            outFileName += ".png";
+
+          outFileName = outFileName.replace(' ', '_');
+
+          ImageIO.write(relief, "png" , new File(outFileName));
+    }
+
+    private final int ALPHA_MASK = 0xFF000000;
+    private final int NONALPHA_MASK = 0x00FFFFFF;
+    private final int R_MASK = 0x00FF0000;
+    private final int G_MASK = 0x0000FF00;
+    private final int B_MASK = 0x000000FF;
+
+    private int minComponent(int pixel)
+    {
+        int  red =  (R_MASK & pixel) >> 16 ;
+        int  green =  (G_MASK & pixel) >> 8 ;
+        int  blue =  (B_MASK & pixel) ;
+
+        int min = Math.min(red, green);
+        min = Math.min(min, blue);
+        return min;
+
+
+    }
+
+
+    private int maxComponent(int pixel)
+    {
+        int  red =  (R_MASK & pixel) >> 16 ;
+        int  green =  (G_MASK & pixel) >> 8 ;
+        int  blue =  (B_MASK & pixel) ;
+
+        int max = Math.max(red, green);
+        max = Math.max(max, blue);
+        return max;
+
+
+    }
+
+    private void correctAlpha(BufferedImage relief)
+    {
+
+
+        for(int x = 0; x < relief.getWidth(); x++)
+        {
+            for(int y = 0; y < relief.getHeight(); y++)
+            {
+                int pixel = relief.getRGB(x,y);
+                //if the pixel isnt already transparent
+                if((pixel & ALPHA_MASK) != 0)
+                {
+
+
+                    //if its nearly all white, make it transparent
+                    int min = minComponent(pixel);
+
+                    int alpha = 255 - min;
+                    pixel = (alpha << 24) | (pixel & NONALPHA_MASK);
+
+                    relief.setRGB(x,y, pixel);
+
+
+                }
+            }
+        }
+    }
+
+
+    //set the alpha channel to the same as that of the base image
+    private void blankOutline(Image alphaChannelImage, BufferedImage relief)
+    {
+        Graphics2D gc = (Graphics2D) relief.getGraphics();
+        // setup our composite
+        Composite prevComposite = gc.getComposite();
+        gc.setComposite(AlphaComposite.getInstance(AlphaComposite.DST_IN));
+
+        // draw the image, and check for the possibility it doesn't complete now
+        ImageIoCompletionWatcher watcher = new ImageIoCompletionWatcher();
+        boolean drawComplete = gc.drawImage(alphaChannelImage, 0, 0, watcher);
+
+        // use the watcher to for the draw to finish
+        if (!drawComplete)
+        {
+          watcher.waitForCompletion();
+        }
+
+        // cleanup
+        gc.setComposite(prevComposite);
+    }
+
+
+}
