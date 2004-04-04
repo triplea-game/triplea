849b2b33754d241e42d0daec1cdc1243dfc3b88e
sgbridges <sean.bridges@gmail.com>
2004-04-04 18:44:01 +0000

    fix unit tests
    git-svn-id: https://svn.code.sf.net/p/triplea/code/trunk/triplea@383 a4c711aa-641f-0410-879d-9b464e52b682

--- src/games/strategy/triplea/delegate/DelegateTest.java
+++ src/games/strategy/triplea/delegate/DelegateTest.java
@@ -1,4 +1,18 @@
 /*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+/*
  * DelegateTest.java
  *
  * Created on November 9, 2001, 3:29 PM
@@ -104,7 +118,9 @@ public class DelegateTest extends TestCase
 		m_data = (new GameParser()).parse(input);
 		
 		british = m_data.getPlayerList().getPlayerID("British");
+		british.addAttatchment(Constants.TECH_ATTATCHMENT_NAME, new TechAttatchment());
 		japanese = m_data.getPlayerList().getPlayerID("Japanese");
+		japanese.addAttatchment(Constants.TECH_ATTATCHMENT_NAME, new TechAttatchment());		
 		
 		northSea = m_data.getMap().getTerritory("North Sea Zone");
 		blackSea = m_data.getMap().getTerritory("Black Sea Zone");
