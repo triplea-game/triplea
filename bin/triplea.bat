ebd54c93f48e771447a2f98df6321d49fb0db42c
sgbridges <sean.bridges@gmail.com>
2004-04-12 22:02:46 +0000

    remove nodraw switch
    git-svn-id: https://svn.code.sf.net/p/triplea/code/trunk/triplea@415 a4c711aa-641f-0410-879d-9b464e52b682

--- bin/triplea.bat
+++ bin/triplea.bat
@@ -1,1 +1,1 @@
-java -mx96M -Dsun.java2d.nodraw=true  -classpath ../lib/patch.jar;../classes;../lib/plastic-1.2.0.jar  games.strategy.engine.framework.GameRunner 
+java -mx96M -classpath ../lib/patch.jar;../classes;../lib/plastic-1.2.0.jar  games.strategy.engine.framework.GameRunner 
