e5395681c1c63ccebe4059b64f4742547b127571
sgbridges <sean.bridges@gmail.com>
2004-04-13 04:14:16 +0000

    update version
    git-svn-id: https://svn.code.sf.net/p/triplea/code/trunk/triplea@421 a4c711aa-641f-0410-879d-9b464e52b682

--- src/games/strategy/engine/EngineVersion.java
+++ src/games/strategy/engine/EngineVersion.java
@@ -28,5 +28,5 @@ import games.strategy.util.Version;
  */
 public class EngineVersion
 {
-  public static final Version VERSION = new Version(0,3,4);
+  public static final Version VERSION = new Version(0,4,0);
 }
