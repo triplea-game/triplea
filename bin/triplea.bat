934b07ecd55509e4cfb38d5738c69b0296625057
nekromancer <nekromancer@mycompany.com>
2004-11-13 13:12:31 +0000

    Added "pause" so DOS prompt wont vanish on error, and saved them in DOS format with nedit to eliminate UNIX EOL chars.
    git-svn-id: https://svn.code.sf.net/p/triplea/code/trunk/triplea@651 a4c711aa-641f-0410-879d-9b464e52b682

--- bin/triplea.bat
+++ bin/triplea.bat
@@ -1,1 +1,2 @@
 java -classpath ../lib/patch.jar;../classes;../lib/plastic-1.2.0.jar  games.strategy.engine.framework.GameRunner 
+pause
--- bin/triplea_ai.bat
+++ bin/triplea_ai.bat
@@ -1,1 +1,2 @@
 java -Dtriplea.ai=true -classpath ../lib/patch.jar;../classes;../lib/plastic-1.2.0.jar  games.strategy.engine.framework.GameRunner 
+pause
