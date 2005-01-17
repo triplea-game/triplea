855c5451eb0a5341c3d9791e8f45fcd80a8f23e7
sgbridges <sean.bridges@gmail.com>
2005-01-17 16:00:52 +0000

    use escape key to close window
    git-svn-id: https://svn.code.sf.net/p/triplea/code/trunk/triplea@749 a4c711aa-641f-0410-879d-9b464e52b682

--- src/games/strategy/triplea/ui/ProductionPanel.java
+++ src/games/strategy/triplea/ui/ProductionPanel.java
@@ -35,239 +35,250 @@ import games.strategy.triplea.Constants;
 import java.util.List;
 import javax.swing.border.*;
 
-
 /**
- *
- * @author  Sean Bridges
+ * 
+ * @author Sean Bridges
  * @version 1.0
- *
- *
+ * 
+ *  
  */
 public class ProductionPanel extends JPanel
 {
-  private static JFrame s_owner;
-  private static JDialog s_dialog;
-  private static ProductionPanel s_panel;
+    private static JFrame s_owner;
+    private static JDialog s_dialog;
+    private static ProductionPanel s_panel;
 
-  private List m_rules = new ArrayList();
-  private JLabel m_left = new JLabel();
-  private PlayerID m_id;
-  private boolean m_bid;
-  private GameData m_data;
-
-  /**
-   * Shows the production panel, and returns a map of
-   * selected rules.
-   */
-  public static IntegerMap show(PlayerID id, JFrame parent, GameData data, boolean bid, IntegerMap initialPurchase)
-  {
-
-    if(!(parent == s_owner))
-      s_dialog = null;
-
-    if(s_dialog == null)
-      initDialog(parent);
-
-    s_panel.m_bid = bid;
-    s_panel.m_data = data;
-    s_panel.initRules(id, data, initialPurchase);
-    s_panel.initLayout(id);
-    s_panel.calculateLimits();
-
-    s_dialog.pack();
-    s_dialog.setLocationRelativeTo(parent);
-    s_dialog.show();
-
-    return s_panel.getProduction();
-
-  }
-
-
-
-  private static void initDialog(JFrame root)
-  {
-    s_panel = new ProductionPanel();
-    s_dialog = new JDialog(root, "Produce", true);
-    s_dialog.getContentPane().add(s_panel);
-  }
-
-  /** Creates new ProductionPanel */
-    private ProductionPanel()
-  {
-
-    }
-
-  private void initRules(PlayerID player, GameData data, IntegerMap initialPurchase)
-  {
-    m_id= player;
-    Iterator iter = player.getProductionFrontier().getRules().iterator();
-    while(iter.hasNext())
-    {
-      ProductionRule productionRule = (ProductionRule) iter.next();
-      Rule rule = new Rule(productionRule , data, player);
-      int initialQuantity = initialPurchase.getInt(productionRule);
-      rule.setQuantity(initialQuantity);
-      m_rules.add(rule);
-    }
-
-  }
-
-  private void initLayout(PlayerID id)
-  {
-    Insets nullInsets = new Insets(0,0,0,0);
-    this.removeAll();
-    this.setLayout(new GridBagLayout());
-    int ipcs = getIPCs();
-    JLabel totalIPCs = new JLabel("You have " + ipcs + " " + StringUtil.plural("IPC", ipcs) + "  to spend");
-    add(totalIPCs, new GridBagConstraints(0,0,30,1,1,1,GridBagConstraints.EAST,GridBagConstraints.HORIZONTAL,  new Insets(8,8,8,0),0,0));
-
-    for(int x = 0;x < m_rules.size() / 2; x++)
-    {
-      add( (Rule) m_rules.get(2*x), new GridBagConstraints(x,1,1,1,1,1,GridBagConstraints.EAST,GridBagConstraints.HORIZONTAL, nullInsets,0,0));
-      if((2*x) + 1 > m_rules.size());
-        add( (Rule) m_rules.get((2* x) + 1), new GridBagConstraints(x,2,1,1,1,1,GridBagConstraints.EAST,GridBagConstraints.HORIZONTAL, nullInsets,0,0));
-    }
-
-
-    add(m_left, new GridBagConstraints(0,3,30,1,1,1,GridBagConstraints.WEST,GridBagConstraints.NONE, new Insets(8,8,0,12),0,0));
-    setLeft(ipcs);
-
-    add(new JButton(m_done_action), new GridBagConstraints(0,4,30,1,1,1,GridBagConstraints.CENTER,GridBagConstraints.NONE, new Insets(0,0,8,0),0,0));
-
-
-
-  }
-
-  private void setLeft(int left)
-  {
-    m_left.setText("Left to spend:" + left);
-  }
-
-  Action m_done_action = new AbstractAction("Done")
-  {
-    public void actionPerformed(ActionEvent e)
-    {
-      s_dialog.setVisible(false);
-    }
-  };
-
-  public IntegerMap getProduction()
-  {
-    IntegerMap prod = new IntegerMap();
-    Iterator iter = m_rules.iterator();
-    while(iter.hasNext())
-    {
-      Rule rule = (Rule) iter.next();
-      int quantity = rule.getQuantity();
-      if(quantity != 0)
-      {
-        prod.put(rule.getProductionRule(), quantity);
-      }
-    }
-    return prod;
-  }
-
-  private void calculateLimits()
-  {
-    int ipcs = getIPCs();
-    int spent = 0;
-    Iterator iter = m_rules.iterator();
-    while(iter.hasNext())
-    {
-      Rule current = (Rule) iter.next();
-      spent += current.getQuantity() * current.getCost();
-    }
-    int leftToSpend = ipcs - spent;
-    setLeft(leftToSpend);
-
-    iter = m_rules.iterator();
-    while(iter.hasNext())
-    {
-      Rule current = (Rule) iter.next();
-      int max = leftToSpend /  current.getCost();
-      max += current.getQuantity();
-      current.setMax(max);
-    }
-
-  }
-
-  private int getIPCs()
-  {
-    if(m_bid)
-    {
-      String propertyName = m_id.getName() + " bid";
-      return Integer.parseInt(m_data.getProperties().get(propertyName).toString());
-    }
-    else
-     return m_id.getResources().getQuantity(Constants.IPCS);
-  }
-
-  class Rule extends JPanel
-  {
-    private ScrollableTextField m_text = new ScrollableTextField(0, Integer.MAX_VALUE);
-    private int m_cost;
+    private List m_rules = new ArrayList();
+    private JLabel m_left = new JLabel();
+    private PlayerID m_id;
+    private boolean m_bid;
     private GameData m_data;
-    private UnitType m_type;
-    private ProductionRule m_rule;
 
-    Rule(ProductionRule rule, GameData data, PlayerID id)
+    /**
+     * Shows the production panel, and returns a map of selected rules.
+     */
+    public static IntegerMap show(PlayerID id, JFrame parent, GameData data, boolean bid, IntegerMap initialPurchase)
     {
-      setLayout(new GridBagLayout());
-      m_data = data;
-      m_rule = rule;
-      m_cost = rule.getCosts().getInt( m_data.getResourceList().getResource(Constants.IPCS));
-      m_type = (UnitType) rule.getResults().keySet().iterator().next();
-      Icon icon = UnitIconImageFactory.instance().getIcon(m_type, id, m_data, false);
-      String text =  " x " + (m_cost < 10 ? " " : "") + m_cost;
-      JLabel label = new JLabel(text, icon, SwingConstants.LEFT);
 
-      int space = 8;
-      this.add(new JLabel( m_type.getName()), new GridBagConstraints(0,0,1,1,1,1,GridBagConstraints.CENTER,GridBagConstraints.NONE, new Insets(2,0,0,0),0,0));
-      this.add(label,  new GridBagConstraints(0,1,1,1,1,1,GridBagConstraints.CENTER,GridBagConstraints.NONE, new Insets(5,space,space,space),0,0));
+        if (!(parent == s_owner))
+            s_dialog = null;
+
+        if (s_dialog == null)
+            initDialog(parent);
+
+        s_panel.m_bid = bid;
+        s_panel.m_data = data;
+        s_panel.initRules(id, data, initialPurchase);
+        s_panel.initLayout(id);
+        s_panel.calculateLimits();
+
+        s_dialog.pack();
+        s_dialog.setLocationRelativeTo(parent);
+        s_dialog.show();
+
+        return s_panel.getProduction();
+
+    }
+
+    private static void initDialog(JFrame root)
+    {
+        s_panel = new ProductionPanel();
+        s_dialog = new JDialog(root, "Produce", true);
+        s_dialog.getContentPane().add(s_panel);
+
+        Action closeAction = new AbstractAction("")
+        {
+            public void actionPerformed(ActionEvent e)
+            {
+                s_dialog.setVisible(false);
+            }
+        };
+        
+        //close the window on escape
+        //this is mostly for developers, makes it much easier to quickly cycle through steps
+        KeyStroke stroke = KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0);
+        s_dialog.getRootPane().registerKeyboardAction(closeAction, stroke, JComponent.WHEN_IN_FOCUSED_WINDOW);
+    
+    }
+
+    /** Creates new ProductionPanel */
+    private ProductionPanel()
+    {
+        
       
-     
-      this.add(m_text, new GridBagConstraints(0,2,1,1,1,1,GridBagConstraints.CENTER,GridBagConstraints.NONE, new Insets(10,space,space,space),0,0));
-      
-      
-      
-      m_text.addChangeListener(m_listener);
-      setBorder(new EtchedBorder());
     }
 
-    int getCost()
+    private void initRules(PlayerID player, GameData data, IntegerMap initialPurchase)
     {
-      return m_cost;
+        m_id = player;
+        Iterator iter = player.getProductionFrontier().getRules().iterator();
+        while (iter.hasNext())
+        {
+            ProductionRule productionRule = (ProductionRule) iter.next();
+            Rule rule = new Rule(productionRule, data, player);
+            int initialQuantity = initialPurchase.getInt(productionRule);
+            rule.setQuantity(initialQuantity);
+            m_rules.add(rule);
+        }
+
     }
 
-    int getQuantity()
+    private void initLayout(PlayerID id)
     {
-      return m_text.getValue();
+        Insets nullInsets = new Insets(0, 0, 0, 0);
+        this.removeAll();
+        this.setLayout(new GridBagLayout());
+        int ipcs = getIPCs();
+        JLabel totalIPCs = new JLabel("You have " + ipcs + " " + StringUtil.plural("IPC", ipcs) + "  to spend");
+        add(totalIPCs,
+                new GridBagConstraints(0, 0, 30, 1, 1, 1, GridBagConstraints.EAST, GridBagConstraints.HORIZONTAL, new Insets(8, 8, 8, 0), 0, 0));
+
+        for (int x = 0; x < m_rules.size() / 2; x++)
+        {
+            add((Rule) m_rules.get(2 * x), new GridBagConstraints(x, 1, 1, 1, 1, 1, GridBagConstraints.EAST, GridBagConstraints.HORIZONTAL,
+                    nullInsets, 0, 0));
+            if ((2 * x) + 1 > m_rules.size())
+                ;
+            add((Rule) m_rules.get((2 * x) + 1), new GridBagConstraints(x, 2, 1, 1, 1, 1, GridBagConstraints.EAST, GridBagConstraints.HORIZONTAL,
+                    nullInsets, 0, 0));
+        }
+
+        add(m_left, new GridBagConstraints(0, 3, 30, 1, 1, 1, GridBagConstraints.WEST, GridBagConstraints.NONE, new Insets(8, 8, 0, 12), 0, 0));
+        setLeft(ipcs);
+
+        add(new JButton(m_done_action), new GridBagConstraints(0, 4, 30, 1, 1, 1, GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(0,
+                0, 8, 0), 0, 0));
+
     }
 
-    void setQuantity(int quantity)
+    private void setLeft(int left)
     {
-      m_text.setValue(quantity);
+        m_left.setText("Left to spend:" + left);
     }
 
-    ProductionRule getProductionRule()
+    Action m_done_action = new AbstractAction("Done")
     {
-      return m_rule;
+        public void actionPerformed(ActionEvent e)
+        {
+            s_dialog.setVisible(false);
+        }
+    };
+
+    public IntegerMap getProduction()
+    {
+        IntegerMap prod = new IntegerMap();
+        Iterator iter = m_rules.iterator();
+        while (iter.hasNext())
+        {
+            Rule rule = (Rule) iter.next();
+            int quantity = rule.getQuantity();
+            if (quantity != 0)
+            {
+                prod.put(rule.getProductionRule(), quantity);
+            }
+        }
+        return prod;
     }
 
-    void setMax(int max)
+    private void calculateLimits()
     {
-      m_text.setMax(max);
-    }
-  }
+        int ipcs = getIPCs();
+        int spent = 0;
+        Iterator iter = m_rules.iterator();
+        while (iter.hasNext())
+        {
+            Rule current = (Rule) iter.next();
+            spent += current.getQuantity() * current.getCost();
+        }
+        int leftToSpend = ipcs - spent;
+        setLeft(leftToSpend);
 
-  private ScrollableTextFieldListener m_listener = new ScrollableTextFieldListener()
-  {
-    public void changedValue(ScrollableTextField stf)
-    {
-      calculateLimits();
+        iter = m_rules.iterator();
+        while (iter.hasNext())
+        {
+            Rule current = (Rule) iter.next();
+            int max = leftToSpend / current.getCost();
+            max += current.getQuantity();
+            current.setMax(max);
+        }
+
     }
-  };
+
+    private int getIPCs()
+    {
+        if (m_bid)
+        {
+            String propertyName = m_id.getName() + " bid";
+            return Integer.parseInt(m_data.getProperties().get(propertyName).toString());
+        } else
+            return m_id.getResources().getQuantity(Constants.IPCS);
+    }
+
+    class Rule extends JPanel
+    {
+        private ScrollableTextField m_text = new ScrollableTextField(0, Integer.MAX_VALUE);
+        private int m_cost;
+        private GameData m_data;
+        private UnitType m_type;
+        private ProductionRule m_rule;
+
+        Rule(ProductionRule rule, GameData data, PlayerID id)
+        {
+            setLayout(new GridBagLayout());
+            m_data = data;
+            m_rule = rule;
+            m_cost = rule.getCosts().getInt(m_data.getResourceList().getResource(Constants.IPCS));
+            m_type = (UnitType) rule.getResults().keySet().iterator().next();
+            Icon icon = UnitIconImageFactory.instance().getIcon(m_type, id, m_data, false);
+            String text = " x " + (m_cost < 10 ? " " : "") + m_cost;
+            JLabel label = new JLabel(text, icon, SwingConstants.LEFT);
+
+            int space = 8;
+            this.add(new JLabel(m_type.getName()), new GridBagConstraints(0, 0, 1, 1, 1, 1, GridBagConstraints.CENTER, GridBagConstraints.NONE,
+                    new Insets(2, 0, 0, 0), 0, 0));
+            this.add(label, new GridBagConstraints(0, 1, 1, 1, 1, 1, GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(5, space, space,
+                    space), 0, 0));
+
+            this.add(m_text, new GridBagConstraints(0, 2, 1, 1, 1, 1, GridBagConstraints.CENTER, GridBagConstraints.NONE, new Insets(10, space,
+                    space, space), 0, 0));
+
+            m_text.addChangeListener(m_listener);
+            setBorder(new EtchedBorder());
+        }
+
+        int getCost()
+        {
+            return m_cost;
+        }
+
+        int getQuantity()
+        {
+            return m_text.getValue();
+        }
+
+        void setQuantity(int quantity)
+        {
+            m_text.setValue(quantity);
+        }
+
+        ProductionRule getProductionRule()
+        {
+            return m_rule;
+        }
+
+        void setMax(int max)
+        {
+            m_text.setMax(max);
+        }
+    }
+
+    private ScrollableTextFieldListener m_listener = new ScrollableTextFieldListener()
+    {
+        public void changedValue(ScrollableTextField stf)
+        {
+            calculateLimits();
+        }
+    };
 
 }
-
 
