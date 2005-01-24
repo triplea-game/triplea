65e17a6e1781413c26235fe81c90511e59612974
nekromancer <nekromancer@mycompany.com>
2005-01-24 12:18:38 +0000

    added the  sh signature.. should work when double clicked.
    git-svn-id: https://svn.code.sf.net/p/triplea/code/trunk/triplea@773 a4c711aa-641f-0410-879d-9b464e52b682

--- bin/triplea.sh
+++ bin/triplea.sh
@@ -1,1 +1,2 @@
+#!/bin/sh
 java -cp  ../lib/patch.jar:../classes:../lib/plastic-1.2.0.jar games.strategy.engine.framework.GameRunner 
--- bin/triplea_ai.sh
+++ bin/triplea_ai.sh
@@ -1,1 +1,2 @@
+#!/bin/sh
 java -Dtriplea.ai=true -cp  ../lib/patch.jar:../classes:../lib/plastic-1.2.0.jar games.strategy.engine.framework.GameRunner 
