81b9385d474c8c5b9e7f8ca56e938c363198045d
sgbridges <sean.bridges@gmail.com>
2005-04-15 04:10:42 +0000

    use png
    git-svn-id: https://svn.code.sf.net/p/triplea/code/trunk/triplea@963 a4c711aa-641f-0410-879d-9b464e52b682

--- src/util/image/TileImageBreaker.java
+++ src/util/image/TileImageBreaker.java
@@ -148,7 +148,7 @@ public class TileImageBreaker
 	                
                 
                 
-                ImageIO.write(relief, "jpg", new File(outFileName));
+                ImageIO.write(relief, "png", new File(outFileName));
                 System.out.println("wrote " + outFileName);
             }
         }
