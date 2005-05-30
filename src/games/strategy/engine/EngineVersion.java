98775b9aec8c51dabfd857834885e9e47cfb150e
george_h <george_h@mycompany.com>
2005-05-30 02:01:16 +0000

    bump version to 0.5.4
    git-svn-id: https://svn.code.sf.net/p/triplea/code/trunk/triplea@1019 a4c711aa-641f-0410-879d-9b464e52b682

--- src/games/strategy/engine/EngineVersion.java
+++ src/games/strategy/engine/EngineVersion.java
@@ -28,5 +28,5 @@ import games.strategy.util.Version;
  */
 public class EngineVersion
 {
-  public static final Version VERSION = new Version(0,5,3,0);
+  public static final Version VERSION = new Version(0,5,4,0);
 }
