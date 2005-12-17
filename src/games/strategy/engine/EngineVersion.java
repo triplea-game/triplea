fbf6de557ad5bfc2e4c994a5145edaa5c6de7bd8
sgbridges <sean.bridges@gmail.com>
2005-12-17 16:42:55 +0000

    version bump
    git-svn-id: https://svn.code.sf.net/p/triplea/code/trunk/triplea@1175 a4c711aa-641f-0410-879d-9b464e52b682

--- src/games/strategy/engine/EngineVersion.java
+++ src/games/strategy/engine/EngineVersion.java
@@ -28,5 +28,5 @@ import games.strategy.util.Version;
  */
 public class EngineVersion
 {
-  public static final Version VERSION = new Version(0,7,2,2);
+  public static final Version VERSION = new Version(0,7,2,3);
 }
