ae136361a656a2bb34409cb11461e466b7b7bc65
sgbridges <sean.bridges@gmail.com>
2006-02-19 18:56:10 +0000

    remove backport argument
    git-svn-id: https://svn.code.sf.net/p/triplea/code/trunk/triplea@1247 a4c711aa-641f-0410-879d-9b464e52b682

--- triplea_windows.bat
+++ triplea_windows.bat
@@ -1,3 +1,3 @@
 @echo off
-java -classpath lib/patch.jar;classes;lib/looks-1.3.1.jar;lib/backport-util-concurrent.jar  games.strategy.engine.framework.GameRunner 
+java -classpath lib/patch.jar;classes;lib/looks-1.3.1.jar;  games.strategy.engine.framework.GameRunner
 pause
