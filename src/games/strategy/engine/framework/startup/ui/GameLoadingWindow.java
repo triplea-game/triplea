5f56679e3fa32218f23190f2f7bdb4c54b1f2659
sgbridges <sean.bridges@gmail.com>
2006-03-19 18:39:21 +0000

    fix race condition
    git-svn-id: https://svn.code.sf.net/p/triplea/code/trunk/triplea@1319 a4c711aa-641f-0410-879d-9b464e52b682

--- src/games/strategy/engine/framework/startup/ui/GameLoadingWindow.java
+++ src/games/strategy/engine/framework/startup/ui/GameLoadingWindow.java
@@ -7,117 +7,116 @@ import java.util.Timer;
 import javax.swing.*;
 import javax.swing.border.*;
 
-public class GameLoadingWindow extends JWindow 
+public class GameLoadingWindow extends JWindow
 {
-    
-    
-    Timer t = new Timer();
-    
-    
+
+    private final Object m_mutex = new Object();
+    private Timer m_timer = new Timer();
+
     public GameLoadingWindow()
     {
-//        super("Game Loading, Please wait");
-//        setIconImage(GameRunner.getGameIcon(this));
+        // super("Game Loading, Please wait");
+        // setIconImage(GameRunner.getGameIcon(this));
         setSize(200, 80);
-        
-        
+
         JPanel mainPanel = new JPanel();
-        
-        
+
         mainPanel.setLayout(new BorderLayout());
-        
+
         JLabel label = new JLabel("Loading game, please wait.");
-        label.setBorder(new EmptyBorder(10,10,10,10 ));
-        
+        label.setBorder(new EmptyBorder(10, 10, 10, 10));
+
         mainPanel.add(BorderLayout.NORTH, label);
-        
-            
-          int min = 0;
-          int max = 100;
-          JProgressBar progress = new JProgressBar(min, max);
-          progress.setBorder(new EmptyBorder(10,10,10,10));
-          mainPanel.add(progress, BorderLayout.CENTER);
-          progress.setIndeterminate(true);
-        
-          setLocationRelativeTo(null);
-         // setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-          
-          
-          mainPanel.setBorder(new LineBorder(Color.BLACK));
-          
-          
-          setLayout(new BorderLayout());
-          add(mainPanel, BorderLayout.CENTER);
+
+        int min = 0;
+        int max = 100;
+        JProgressBar progress = new JProgressBar(min, max);
+        progress.setBorder(new EmptyBorder(10, 10, 10, 10));
+        mainPanel.add(progress, BorderLayout.CENTER);
+        progress.setIndeterminate(true);
+
+        setLocationRelativeTo(null);
+        // setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+
+        mainPanel.setBorder(new LineBorder(Color.BLACK));
+
+        setLayout(new BorderLayout());
+        add(mainPanel, BorderLayout.CENTER);
     }
 
-    
     public void showWait()
     {
-       TimerTask task = new TimerTask()
+        TimerTask task = new TimerTask()
         {
-        
+
             @Override
             public void run()
             {
-		SwingUtilities.invokeLater(new Runnable() 
-				{
-					public void run()
-					{
-				                toFront();
-					}
-				}
-
-				
-				);
-
-        
-            }
-        
-        };  
-        
-       t.schedule(task, 15, 15);
-       
-    }
-
-    
-    public void doneWait()
-    {
-        t.cancel();
-        
-        SwingUtilities.invokeLater(new Runnable()
+                SwingUtilities.invokeLater(new Runnable()
                 {
-                
                     public void run()
                     {
-                        
-                        setVisible(false);
-                        removeAll();
-                        dispose();
-
+                        toFront();
                     }
                 }
-        );
-        
+
+                );
+
+            }
+
+        };
+
+        synchronized(m_mutex)
+        {
+            if(m_timer != null)
+                m_timer.schedule(task, 15, 15);
+        }
+
     }
-    
-    public static void main(String[] args)
+
+    public void doneWait()
     {
-        
-        
+        synchronized(m_mutex)
+        {
+            if(m_timer != null)
+            {
+                m_timer.cancel();
+                m_timer = null;
+            }
+        }
+
         SwingUtilities.invokeLater(new Runnable()
         {
-        
+
+            public void run()
+            {
+
+                setVisible(false);
+                removeAll();
+                dispose();
+
+            }
+        });
+
+    }
+
+    public static void main(String[] args)
+    {
+
+        SwingUtilities.invokeLater(new Runnable()
+        {
+
             public void run()
             {
                 final GameLoadingWindow window = new GameLoadingWindow();
                 window.setVisible(true);
-                
+
                 window.showWait();
-        
+
             }
-        
+
         });
-        
+
     }
-    
+
 }
