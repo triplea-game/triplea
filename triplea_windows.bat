2b82ea45f67bd3b06ebe8bc0ec987ab2e086fdc8
sgbridges <sean.bridges@gmail.com>
2006-04-08 15:46:34 +0000

    increase memory
    git-svn-id: https://svn.code.sf.net/p/triplea/code/trunk/triplea@1349 a4c711aa-641f-0410-879d-9b464e52b682

--- triplea_windows.bat
+++ triplea_windows.bat
@@ -1,3 +1,3 @@
 @echo off
-java -classpath lib/patch.jar;classes;bin/triplea.jar;lib/looks-1.3.1.jar;  games.strategy.engine.framework.GameRunner
+java -Xmx128m -classpath lib/patch.jar;classes;bin/triplea.jar;lib/looks-1.3.1.jar;  games.strategy.engine.framework.GameRunner
 pause
