b55660dfe301c53faaa2bf7bfbb55434fded2f50
sgbridges <sean.bridges@gmail.com>
2006-07-07 17:42:01 +0000

    dont let user close wait dialog
    git-svn-id: https://svn.code.sf.net/p/triplea/code/trunk/triplea@1481 a4c711aa-641f-0410-879d-9b464e52b682

--- src/games/strategy/engine/framework/ui/background/WaitDialog.java
+++ src/games/strategy/engine/framework/ui/background/WaitDialog.java
@@ -10,6 +10,7 @@ public class WaitDialog extends JDialog
     public WaitDialog(Component parent, String waitMessage)
     {
         super(JOptionPane.getFrameForComponent(parent), "Please Wait" , true);
+        setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
         WaitPanel panel = new WaitPanel(waitMessage);
         setLayout(new BorderLayout());
         add(panel, BorderLayout.CENTER);
