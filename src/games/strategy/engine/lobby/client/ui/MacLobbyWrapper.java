37b8e7d4eafba15e29f1aad1a12a3dd160c2d258
sgbridges <sean.bridges@gmail.com>
2006-08-23 04:43:28 +0000

    suppress deprecate warnings
    git-svn-id: https://svn.code.sf.net/p/triplea/code/trunk/triplea@1504 a4c711aa-641f-0410-879d-9b464e52b682

--- src/games/strategy/engine/lobby/client/ui/MacLobbyWrapper.java
+++ src/games/strategy/engine/lobby/client/ui/MacLobbyWrapper.java
@@ -1,12 +1,13 @@
 package games.strategy.engine.lobby.client.ui;
 
-
+@SuppressWarnings("deprecation")
 public class MacLobbyWrapper
 {
     
     //keep this in its own class, otherwise we get a no class def error when 
     //we try to load the game and the stubs arent in the classpath
     //i think the java validator triggers this
+    
     public static void registerMacShutdownHandler(final LobbyFrame frame)
     {
         com.apple.mrj.MRJApplicationUtils.registerQuitHandler(new com.apple.mrj.MRJQuitHandler()
--- src/games/strategy/triplea/ui/MacWrapper.java
+++ src/games/strategy/triplea/ui/MacWrapper.java
@@ -5,6 +5,7 @@ public class MacWrapper
     //keep this in its own class, otherwise we get a no class def error when 
     //we try to load the game and the stubs arent in the classpath
     //i think the java validator triggers this
+    @SuppressWarnings("deprecation")
     public static void registerMacShutdownHandler(final TripleAFrame frame)
     {
         com.apple.mrj.MRJApplicationUtils.registerQuitHandler(new com.apple.mrj.MRJQuitHandler()
