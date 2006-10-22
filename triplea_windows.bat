4f6db3f5ff218fe55a5b22672f87a625be876dc5
sgbridges <sean.bridges@gmail.com>
2006-10-22 23:58:32 +0000

    add back patch.jar to shell scripts
    git-svn-id: https://svn.code.sf.net/p/triplea/code/trunk/triplea@1574 a4c711aa-641f-0410-879d-9b464e52b682

--- triplea_windows.bat
+++ triplea_windows.bat
@@ -1,3 +1,3 @@
 @echo off
-java -Xmx128m -classpath bin/triplea.jar  games.strategy.engine.framework.GameRunner
+java -Xmx128m -classpath bin/patch.jar;bin/triplea.jar  games.strategy.engine.framework.GameRunner
 pause
