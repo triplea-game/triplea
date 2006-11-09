c883914301c4ba42ad753373c22bf27dcc8517ad
sgbridges <sean.bridges@gmail.com>
2006-11-09 04:46:51 +0000

    allow looking up server lookup url in a properties file
    git-svn-id: https://svn.code.sf.net/p/triplea/code/trunk/triplea@1611 a4c711aa-641f-0410-879d-9b464e52b682

--- src/games/strategy/engine/framework/startup/ui/MetaSetupPanel.java
+++ src/games/strategy/engine/framework/startup/ui/MetaSetupPanel.java
@@ -1,6 +1,7 @@
 package games.strategy.engine.framework.startup.ui;
 
 import games.strategy.engine.EngineVersion;
+import games.strategy.engine.framework.GameRunner;
 import games.strategy.engine.framework.startup.mc.SetupPanelModel;
 import games.strategy.engine.framework.ui.background.BackgroundTaskRunner;
 import games.strategy.engine.lobby.client.LobbyClient;
@@ -13,11 +14,16 @@ import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicReference;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.swing.JButton;
 import javax.swing.JOptionPane;
@@ -25,6 +31,8 @@ import javax.swing.JPanel;
 
 public class MetaSetupPanel extends SetupPanel
 {
+    private static final Logger s_logger = Logger.getLogger(MetaSetupPanel.class.getName());
+    
     private JButton m_startLocal;
     private JButton m_startPBEM;
     private JButton  m_hostGame;
@@ -131,8 +139,12 @@ public class MetaSetupPanel extends SetupPanel
         final URL serverPropsURL;
         try
         {
-            String defaultURL = "http://triplea.sourceforge.net/lobby/server_" + EngineVersion.VERSION.toString() +  ".properties" ;
-            serverPropsURL =new URL(System.getProperties().getProperty("triplea.lobby.serverURL", defaultURL));
+            serverPropsURL = getServerLookupURL();
+            if (s_logger.isLoggable(Level.FINE))
+            {
+                s_logger.log(Level.FINE, "lobby url:" + serverPropsURL);
+            }
+            
         } catch (MalformedURLException e)
         {
             e.printStackTrace();
@@ -226,6 +238,55 @@ public class MetaSetupPanel extends SetupPanel
         lobbyFrame.setVisible(true);
         
         
+    }
+
+    /**
+     * Get the url which we use to lookup the lobby server.
+     * 
+     * we look first for a file called lobby.properties, with a key server
+     * then we look for a system property triplea.lobby.server.lookup.url
+     * finally we default to looking on sourceforge, with a version dependent url
+     */
+    private URL getServerLookupURL() throws MalformedURLException
+    {
+        //step 1, check for a file
+        File f = new File(GameRunner.getRootFolder(), "lobby.properties");
+        if(f.exists() && f.canRead()) {
+            java.util.Properties props = new java.util.Properties();
+            FileInputStream in = null;
+            try
+            {
+                in = new FileInputStream(f);
+                props.load(in);
+                if(props.containsKey("server"))
+                    return new URL(props.getProperty("server"));
+                
+            } catch(IOException ioe) 
+            {
+                ioe.printStackTrace();
+            }
+            finally
+            {
+              try
+              {
+                if(in != null)
+                    in.close();
+              } catch (IOException e)
+              {
+                  //ignore, we just want to close
+              }  
+            }
+            
+            
+        }
+        
+        
+        //step 2 check for a system property
+        //step 3 default
+        final URL serverPropsURL;
+        String defaultURL = "http://triplea.sourceforge.net/lobby/server_" + EngineVersion.VERSION.toString() +  ".properties" ;
+        serverPropsURL =new URL(System.getProperties().getProperty("triplea.lobby.server.lookup.url", defaultURL));
+        return serverPropsURL;
     }
 
     private void setWidgetActivation()
