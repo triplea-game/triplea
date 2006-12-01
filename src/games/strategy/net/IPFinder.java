1b6bbb2992de65ec8daaa4dbc69cc42881e99f72
sgbridges <sean.bridges@gmail.com>
2006-12-01 04:33:43 +0000

    better logic for finding local host
    git-svn-id: https://svn.code.sf.net/p/triplea/code/trunk/triplea@1633 a4c711aa-641f-0410-879d-9b464e52b682

--- src/games/strategy/net/IPFinder.java
+++ src/games/strategy/net/IPFinder.java
@@ -39,14 +39,19 @@ package games.strategy.net;
 
  */
 
-import java.util.*;
-import java.net.*;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
 
 public class IPFinder {
 
 	/**
 	   We iterate through an enumeration of network interfaces on the machine
-	   and picks the first IP that is not a loopback and not a link local.
+	   and picks the first IP that is not a loopback and not a link local and not private.
 	   In the case of IRIX computers connected on a LAN through a central
 	   gateway running java off a telnet session will result in a null
 	   network interface (patched below).
@@ -58,33 +63,99 @@ public class IPFinder {
 	*/
 	public static InetAddress findInetAddress() throws SocketException, UnknownHostException
 	{
+       
 		Enumeration enum1 = NetworkInterface.getNetworkInterfaces();
 
 		// Test if null, no point taking a performance hit by
 		// letting the JVM check for a NullPointerException.
-		if(enum1 == null) {
+		if(enum1 == null) 
+        {
 			InetAddress ip1 = InetAddress.getLocalHost();
 			return ip1;
 		}
-		else {
-			while (enum1.hasMoreElements()) {
-				NetworkInterface netface = (NetworkInterface)enum1.nextElement();
-				Enumeration enum2 = netface.getInetAddresses();
-				while (enum2.hasMoreElements()) {
-					InetAddress ip2 = (InetAddress) enum2.nextElement();
-					if(! ip2.isLoopbackAddress()) {
-					if(! ip2.isLinkLocalAddress()) {
-						return ip2;
-					}
-					}
-				}
-			}
-			//return null  <-- old code never throw null
-			//If all else fails we return the localhost
-			InetAddress ip3 = InetAddress.getLocalHost();
-			return ip3;
-		}//else
+        
+        List<InetAddress> allButLoopback = new ArrayList<InetAddress>();
+		
+		while (enum1.hasMoreElements()) 
+        {
+            NetworkInterface netface = (NetworkInterface)enum1.nextElement();
+            Enumeration enum2 = netface.getInetAddresses();
+            while (enum2.hasMoreElements()) 
+            {
+                InetAddress ip2 = (InetAddress) enum2.nextElement();
+                if(!ip2.isLoopbackAddress())
+                {
+                    allButLoopback.add(ip2);
+                }
+            }
+        }
+		
+        //try to find one that is not private and ip4
+        for(InetAddress address : allButLoopback) 
+        {
+            if(address.getAddress().length == 4 && !isPrivateNetworkAddress(address)) 
+            {
+                return address;
+            }
+        }
+        
+        //try to find one that is not private
+        for(InetAddress address : allButLoopback) 
+        {
+            if(!isPrivateNetworkAddress(address)) 
+            {
+                return address;
+            }
+        }
+
+        //try to find one that is not link local
+        for(InetAddress address : allButLoopback) 
+        {
+            if(!address.isLinkLocalAddress())
+            {
+                return address;
+            }
+        }
+
+        
+        //all else fails, return localhost
+        return InetAddress.getLocalHost();
 
 	}//end static findInetAddress()
+    
+    
+    private static boolean isPrivateNetworkAddress(InetAddress address) {
+        
+        byte[] bytes = address.getAddress();
+        //ip 4
+        if(bytes.length == 4)
+        {
+            //http://en.wikipedia.org/wiki/Private_network
+            if(
+              (bytes[0] == 10) ||
+              (bytes[0] == 172 && bytes[1] >= 16 && bytes[1] <= 31) ||
+              (bytes[0] == 192 &&  bytes[1] == 168 ) ||
+              (bytes[0] == 169 &&  bytes[1] == 254 )
+              )
+                return true;
+        }
+        //ip 6
+        else {
+            //http://en.wikipedia.org/wiki/IPv6#Addressing
+            if(bytes[0] ==  252 && bytes[1] == 0) {
+                return true;
+            }
+        }
+        
+        return false;
+        
+        
+    }
 
+    
+    public static void main(String[] args) throws SocketException, UnknownHostException {
+       System.out.println(findInetAddress());
+    }
 }//end class IPFinder
+
+
