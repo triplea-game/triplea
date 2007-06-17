8e0d2015c79f9c3c293d476b917772cd6e1d443d
sgbridges <sean.bridges@gmail.com>
2007-06-17 17:52:20 +0000

    add missing space
    git-svn-id: https://svn.code.sf.net/p/triplea/code/trunk/triplea@1776 a4c711aa-641f-0410-879d-9b464e52b682

--- src/games/strategy/triplea/ui/ProductionPanel.java
+++ src/games/strategy/triplea/ui/ProductionPanel.java
@@ -176,7 +176,7 @@ public class ProductionPanel extends JPanel
         int total = getIPCs();
         int spent = total - left;
         
-        m_left.setText("You have spent " + spent + " out of" + total +  " "  + StringUtil.plural("IPC", total)) ;
+        m_left.setText("You have spent " + spent + " out of " + total +  " "  + StringUtil.plural("IPC", total)) ;
     }
 
     Action m_done_action = new AbstractAction("Done")
