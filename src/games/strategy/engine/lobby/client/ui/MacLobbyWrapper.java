c8f14d04b262cca49d12e3117be467d98440e6ec
dowobeha <dowobeha@mycompany.com>
2007-07-24 15:33:14 +0000

    Updated MacLobbyWrapper to use non-deprecated Mac api instead of MRJ.
    git-svn-id: https://svn.code.sf.net/p/triplea/code/trunk/triplea@1864 a4c711aa-641f-0410-879d-9b464e52b682

--- src/games/strategy/engine/lobby/client/ui/MacLobbyWrapper.java
+++ src/games/strategy/engine/lobby/client/ui/MacLobbyWrapper.java
@@ -1,6 +1,27 @@
+/*
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
 package games.strategy.engine.lobby.client.ui;
 
-@SuppressWarnings("deprecation")
+import com.apple.eawt.Application;
+import com.apple.eawt.ApplicationAdapter;
+import com.apple.eawt.ApplicationEvent;
+
+/**
+ * TODO This class should be merged with games.strategy.common.ui.MacWrapper.
+ * 
+ */
 public class MacLobbyWrapper
 {
     
@@ -10,13 +31,18 @@ public class MacLobbyWrapper
     
     public static void registerMacShutdownHandler(final LobbyFrame frame)
     {
-        com.apple.mrj.MRJApplicationUtils.registerQuitHandler(new com.apple.mrj.MRJQuitHandler()
-            {
-               public void handleQuit()
-               {
-                   frame.shutdown();
-               }
-            });
+        Application application = new Application();
+        
+        application.addApplicationListener(
+                new ApplicationAdapter() {
+                    public void handleQuit(ApplicationEvent event) 
+                    {   
+                        if(frame != null)
+                            frame.shutdown();
+                        else 
+                            System.exit(0);   
+                    }});
+        
     }
 
 
