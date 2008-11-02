246a46bebf2887038481e12f7b4f7b475700bd3e
sgbridges <sean.bridges@gmail.com>
2008-11-02 16:23:59 +0000

    When executing sql in headless lobby, show error messages on the console (sgb)
    git-svn-id: https://svn.code.sf.net/p/triplea/code/trunk/triplea@2156 a4c711aa-641f-0410-879d-9b464e52b682

--- src/games/strategy/engine/lobby/server/headless/HeadlessLobbyConsole.java
+++ src/games/strategy/engine/lobby/server/headless/HeadlessLobbyConsole.java
@@ -202,6 +202,7 @@ public class HeadlessLobbyConsole
         catch(SQLException sqle)
         {
             sqle.printStackTrace();
+            out.println(sqle.getMessage());
         }
         finally
         {
