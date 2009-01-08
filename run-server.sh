b2c686c3860983c68ff120ce811d7b4f39e00857
sgbridges <sean.bridges@gmail.com>
2009-01-08 04:41:37 +0000

    command can be run from any directory now
    git-svn-id: https://svn.code.sf.net/p/triplea/code/trunk/triplea@2234 a4c711aa-641f-0410-879d-9b464e52b682

--- run-server.sh
+++ run-server.sh
@@ -1,1 +1,2 @@
+cd $(dirname $0)
 java -server  -Xmx128m -classpath bin/triplea.jar:lib/derby_10_1_2.jar -Dtriplea.lobby.port=3303 -Dtriplea.lobby.console=true  games.strategy.engine.lobby.server.LobbyServer 
