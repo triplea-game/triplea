ea427cf495f8a9b3a69444f91831d86d2d93116a
comradekev <comradekev@yahoo.com>
2009-01-30 14:49:41 +0000

    Update Changelog & version to 1.0.2.0
    git-svn-id: https://svn.code.sf.net/p/triplea/code/trunk/triplea@2278 a4c711aa-641f-0410-879d-9b464e52b682

--- src/games/strategy/engine/EngineVersion.java
+++ src/games/strategy/engine/EngineVersion.java
@@ -28,5 +28,5 @@ import games.strategy.util.Version;
  */
 public class EngineVersion
 {
-  public static final Version VERSION = new Version(1,0,1,5);
+  public static final Version VERSION = new Version(1,0,2,0);
 }
