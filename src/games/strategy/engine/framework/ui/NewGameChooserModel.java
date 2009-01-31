81d34efc8b81077ac1c2a537e8a678317d5e944f
sgbridges <sean.bridges@gmail.com>
2009-01-31 17:30:22 +0000

    better fix for spaces in zip paths
    git-svn-id: https://svn.code.sf.net/p/triplea/code/trunk/triplea@2292 a4c711aa-641f-0410-879d-9b464e52b682

--- src/games/strategy/engine/framework/ui/NewGameChooserModel.java
+++ src/games/strategy/engine/framework/ui/NewGameChooserModel.java
@@ -10,7 +10,6 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.net.URLClassLoader;
-import java.net.URLDecoder;
 import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -87,7 +86,7 @@ public class NewGameChooserModel extends DefaultListModel
                         URL url = loader.getResource(entry.getName());
                         try
                         {
-                            entries.add(createEntry(new URI(URLEncoder.encode(url.toString(), "utf-8"))));
+                            entries.add(createEntry(new URI(url.toString().replace(" ", "%20"))));
                         } catch (URISyntaxException e)
                         {
                             e.printStackTrace();
