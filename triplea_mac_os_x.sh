970561c262895b93c2845fc1549724d48e1a70c2
sgbridges <sean.bridges@gmail.com>
2009-07-05 15:33:12 +0000

    use java 1.6 if available
    git-svn-id: https://svn.code.sf.net/p/triplea/code/trunk/triplea@2490 a4c711aa-641f-0410-879d-9b464e52b682

--- triplea_mac_os_x.sh
+++ triplea_mac_os_x.sh
@@ -11,4 +11,5 @@ relativePathToGame=`dirname $0`
 cd $relativePathToGame
 
 export PATH=/System/Library/Frameworks/JavaVM.framework/Versions/1.5/Home/bin/:$PATH
+export PATH=/System/Library/Frameworks/JavaVM.framework/Versions/1.6/Home/bin/:$PATH
 java -Xmx196m -cp bin/patch.jar:bin/triplea.jar -Dapple.laf.useScreenMenuBar=true -Xdock:name="TripleA" -Xdock:icon="./icons/triplea_icon.png" games.strategy.engine.framework.GameRunner
