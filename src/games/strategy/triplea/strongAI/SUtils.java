d068d9bbd002547458c3656b962ce04ee3393d9e
comradekev <comradekev@yahoo.com>
2009-11-25 16:11:40 +0000

    Add Rocket to NWO units (ComradeKev)
    git-svn-id: https://svn.code.sf.net/p/triplea/code/trunk/triplea@2580 a4c711aa-641f-0410-879d-9b464e52b682

--- src/games/strategy/triplea/strongAI/SUtils.java
+++ src/games/strategy/triplea/strongAI/SUtils.java
@@ -4138,8 +4138,7 @@ public class SUtils
 			}
 			else if (Matches.isTerritoryAllied(player, data).match(eTerr) && Matches.TerritoryIsNotNeutral.match(eTerr))
 			{
-				boolean hasENeighbors = Matches.territoryHasEnemyLandNeighbor(data, player).match(eTerr);
-				//BUG- the following route will always be null for island nations with no land route to enemies (Japan/US)
+				boolean hasENeighbors = Matches.territoryHasEnemyLandNeighbor(data, player).match(eTerr);				
 				Route testERoute = findNearest(eTerr, enemyAndNoWater, noEnemyOrWater, data);
 
 				eTerrValue += (hasENeighbors ? 1.0F : -1.0F);
