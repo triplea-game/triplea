fbb216a1c5b90261769408d6662d8811d78ec706
Chris Duncan <veqryn@hotmail.com>
2010-12-15 06:55:35 +0000

    MoveValidator always returns true for if a route contains only subs and/or transports, if that route has a single territory that contains only subs and/or transports.  It should check all territories on route for subs and/or transports and return a false if even one sea-territory contains some other ship type. (squidbait)
    git-svn-id: https://svn.code.sf.net/p/triplea/code/trunk/triplea@2826 a4c711aa-641f-0410-879d-9b464e52b682

--- src/games/strategy/triplea/delegate/MoveValidator.java
+++ src/games/strategy/triplea/delegate/MoveValidator.java
@@ -293,7 +293,7 @@ public class MoveValidator
                         continue;
                     }
                     
-                    validMove = false;
+                    return false;
                 }
             }
     	return validMove;
