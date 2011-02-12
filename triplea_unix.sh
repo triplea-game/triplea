ed7efb9133db0af24f87aa2c28ff351b26d37845
Chris Duncan <veqryn@hotmail.com>
2011-02-12 04:57:46 +0000

    Increasing Max Memory to 512mb for linux/mac/windows, and to 192mb for the server. (veqryn)
    git-svn-id: https://svn.code.sf.net/p/triplea/code/trunk/triplea@2927 a4c711aa-641f-0410-879d-9b464e52b682

--- triplea_unix.sh
+++ triplea_unix.sh
@@ -9,4 +9,4 @@ relativePathToGame=`dirname $0`
 cd $relativePathToGame
 
 
-java -Xmx256m -cp bin/patch.jar:bin/triplea.jar games.strategy.engine.framework.GameRunner
+java -Xmx512m -cp bin/patch.jar:bin/triplea.jar games.strategy.engine.framework.GameRunner
--- triplea_windows.bat
+++ triplea_windows.bat
@@ -1,3 +1,3 @@
 @echo off
-java -Xmx196m -classpath bin/patch.jar;bin/triplea.jar  games.strategy.engine.framework.GameRunner
+java -Xmx512m -classpath bin/patch.jar;bin/triplea.jar  games.strategy.engine.framework.GameRunner
 pause
