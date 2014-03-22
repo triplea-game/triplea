d25c92fdc04459a20a0ed04883523cf9d5f1d010
Chris Duncan <veqryn@hotmail.com>
2014-03-22 03:58:14 +0000

    bumping version number for stable (veqryn)
    git-svn-id: https://svn.code.sf.net/p/triplea/code/trunk/triplea@4182 a4c711aa-641f-0410-879d-9b464e52b682

--- src/games/strategy/engine/EngineVersion.java
+++ src/games/strategy/engine/EngineVersion.java
@@ -26,5 +26,5 @@ import games.strategy.util.Version;
  */
 public class EngineVersion
 {
-	public static final Version VERSION = new Version(1, 8, 0, 1);
+	public static final Version VERSION = new Version(1, 8, 0, 3);
 }
