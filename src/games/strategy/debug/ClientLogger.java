de71b7e37d00b6d95249bd944c16ed3d730d8aeb
Dan Van Atta <dhvatta@gmail.com>
2015-07-29 03:38:49 -0700

    Add an error reporting method to ClientLogger: logError(Exception e)

--- src/games/strategy/debug/ClientLogger.java
+++ src/games/strategy/debug/ClientLogger.java
@@ -5,13 +5,19 @@ import java.io.PrintStream;
 public class ClientLogger
 {
 	private static final PrintStream developerOutputStream = System.out;
+	private static final PrintStream userOutputStream = System.err;
 	
 	public static void logQuietly(final Exception e)
 	{
-		logQuietly("Exception: " + e.getMessage());
+		log(developerOutputStream, e);
+	}
+
+	private static void log(PrintStream stream, Exception e)
+	{
+		stream.println("Exception: " + e.getMessage());
 		for (final StackTraceElement stackTraceElement : e.getStackTrace())
 		{
-			developerOutputStream.println(stackTraceElement.toString());
+			stream.println(stackTraceElement.toString());
 		}
 	}
 	
@@ -19,5 +25,9 @@ public class ClientLogger
 	{
 		developerOutputStream.println(msg);
 	}
-	
+
+	public static void logError(Exception e)
+	{
+		log(userOutputStream, e);
+	}	
 }
