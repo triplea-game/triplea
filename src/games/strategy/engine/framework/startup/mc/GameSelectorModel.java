acb847c403a20607ed5998c0dfe13c263646ac5c
Dan Van Atta <dhvatta@gmail.com>
2015-07-31 17:02:29 -0700

    GameSelectorModel.java - Call gameDataManager.loadGame(input) instead of loadGame(input,null)

--- src/games/strategy/engine/framework/startup/mc/GameSelectorModel.java
+++ src/games/strategy/engine/framework/startup/mc/GameSelectorModel.java
@@ -113,7 +113,7 @@ public class GameSelectorModel extends Observable {
     final GameDataManager manager = new GameDataManager();
     GameData newData;
     try {
-      newData = manager.loadGame(input, null);
+      newData = manager.loadGame(input);
       if (newData != null) {
         m_fileName = fileName;
         setGameData(newData);
@@ -139,7 +139,7 @@ public class GameSelectorModel extends Observable {
     final GameDataManager manager = new GameDataManager();
     GameData newData;
     try {
-      newData = manager.loadGame(input, null);
+      newData = manager.loadGame(input);
       if (newData != null) {
         return newData;
       }
