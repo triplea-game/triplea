de847c6bbfb6e92f7b38880f7c07eafae094360b
Dan Van Atta <dhvatta@gmail.com>
2015-08-26 12:58:27 -0700

    ClientLogger - add utility method and make existing a bit more generic - accept Throwable instead of Exception as parameter in utility methods - add logError(String,Throwable) method

--- src/games/strategy/debug/ClientLogger.java
+++ src/games/strategy/debug/ClientLogger.java
@@ -6,11 +6,11 @@ public class ClientLogger {
   private static final PrintStream developerOutputStream = System.out;
   private static final PrintStream userOutputStream = System.err;
 
-  public static void logQuietly(final Exception e) {
+  public static void logQuietly(final Throwable e) {
     log(developerOutputStream, e);
   }
 
-  private static void log(final PrintStream stream, final Exception e) {
+  private static void log(final PrintStream stream, final Throwable e) {
     stream.println("Exception: " + e.getMessage());
     for (final StackTraceElement stackTraceElement : e.getStackTrace()) {
       stream.println(stackTraceElement.toString());
@@ -21,7 +21,12 @@ public class ClientLogger {
     developerOutputStream.println(msg);
   }
 
-  public static void logError(final Exception e) {
+  public static void logQuietly(final String msg, final Throwable e) {
+    logQuietly(msg);
+    logQuietly(e);
+  }
+
+  public static void logError(final Throwable e) {
     log(userOutputStream, e);
   }
 
@@ -29,9 +34,8 @@ public class ClientLogger {
     userOutputStream.println(msg);
   }
 
-  public static void logQuietly(final String msg, final Exception e) {
-    logQuietly(msg);
-    logQuietly(e);
+  public static void logError(final String msg, final Throwable e) {
+    logError(msg);
+    logError(e);
   }
-
 }
