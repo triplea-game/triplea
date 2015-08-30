bf227a75d94bd0258e97558b76196581c8c1d644
Ron <ron.murhammer@gmail.com>
2015-08-30 13:45:25 -0500

    Updating version to 1.8.0.9

--- src/games/strategy/engine/EngineVersion.java
+++ src/games/strategy/engine/EngineVersion.java
@@ -3,5 +3,5 @@ package games.strategy.engine;
 import games.strategy.util.Version;
 
 public class EngineVersion {
-  public static final Version VERSION = new Version(1, 8, 0, 7);
+  public static final Version VERSION = new Version(1, 8, 0, 9);
 }
