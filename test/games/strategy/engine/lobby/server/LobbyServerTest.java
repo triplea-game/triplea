b736c69048489628884935fafb322bd3c45e3b0d
Dan Van Atta <dhvatta@gmail.com>
2015-09-09 13:23:40 -0700

    LobbyServerTest.java - New test that for now just starts up the lobby.

---
+++ test/games/strategy/engine/lobby/server/LobbyServerTest.java
@@ -0,0 +1,19 @@
+package games.strategy.engine.lobby.server;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class LobbyServerTest {
+
+  @Before
+  public void setUp() throws Exception {}
+
+  @Test
+  public void test() {
+    LobbyServer.main(new String[0]);
+
+  }
+
+}
