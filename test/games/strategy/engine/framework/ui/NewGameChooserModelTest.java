32e6ba3d859e2607e5d52f336001116e27d036bb
DanVanAtta <dhvatta@gmail.com>
2015-10-15 22:59:10 -0700

    Fix for NewGameChooserModelTest, avoid NPE in test setup scenario.

--- test/games/strategy/engine/framework/ui/NewGameChooserModelTest.java
+++ test/games/strategy/engine/framework/ui/NewGameChooserModelTest.java
@@ -1,17 +1,14 @@
 package games.strategy.engine.framework.ui;
 
 import org.junit.Test;
-import org.mockito.Mock;
 
 public class NewGameChooserModelTest  {
 
-  @Mock
-  private ClearGameChooserCacheMessenger mockClearGameChooserCacheMessenger;
 
   /** Simply create the object to see that we can do that without exception */
   @SuppressWarnings("unused")
   @Test
   public void testCreate() {
-    new NewGameChooserModel(mockClearGameChooserCacheMessenger);
+    new NewGameChooserModel(new ClearGameChooserCacheMessenger());
   }
 }
