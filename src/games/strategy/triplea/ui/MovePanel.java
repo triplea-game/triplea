375d2ccc4f495ddbb5f0a70ef223c59a7b5b64ea
DanVanAtta <dhvatta@gmail.com>
2015-10-30 12:49:20 -0700

    MovePanel.java - Format

--- src/games/strategy/triplea/ui/MovePanel.java
+++ src/games/strategy/triplea/ui/MovePanel.java
@@ -1432,7 +1432,8 @@ public class MovePanel extends AbstractMovePanel {
   private final KeyListener undoUnitMoveKeyListener = new KeyListener() {
     @Override
     public void keyTyped(KeyEvent e) {
-      if (e.getKeyCode() == KeyEvent.VK_U && getMap().getHighlightUnits() != null&& !getMap().getHighlightUnits().isEmpty()) {
+      if (e.getKeyCode() == KeyEvent.VK_U && getMap().getHighlightUnits() != null
+          && !getMap().getHighlightUnits().isEmpty()) {
         m_undoableMovesPanel.undoMoves(getMap().getHighlightUnits());
       }
     }
