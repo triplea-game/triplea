4e03490438406d570533ba357993bbd7bf6aba6a
DanVanAtta <dhvatta@gmail.com>
2015-11-23 18:52:56 -0800

    [GITHUB MAPS] update check_maps.sh script to print the total number of maps checked and the total number that failed.

--- scripts/check_maps.sh
+++ scripts/check_maps.sh
@@ -10,6 +10,12 @@ if [ ! -f "$MAP_FILE" ]; then
   exit -1
 fi
 
-for i in $(grep "<url>" "$MAP_FILE"  | grep -v "<\!--" | sed 's| *</*url>||g' | grep -v "^\!" | sort | uniq); 
-   do curl -sI "$i" > /dev/null 2>&1 || echo "$i" is dead; 
+TOTAL=0
+FAIL_COUNT=0
+
+for i in $(grep "<url>" "$MAP_FILE"  | grep -v "<\!--" | sed 's| *</*url>||g' | grep -v "^\!" | sort | uniq);  do
+  curl -sI "$i" > /dev/null 2>&1 || { echo "$i" is dead; FAIL_COUNT=$((FAIL_COUNT+1)); }
+  TOTAL=$((TOTAL+1))
 done;
+
+echo "$FAIL_COUNT failed out of $TOTAL"
