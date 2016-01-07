d3b130433edb39c4acbc5c8ba454a931c0df852d
DanVanAtta <dhvatta@gmail.com>
2016-01-07 00:32:54 -0800

    Renamed:    src/games/strategy/debug/PerformanceConsole.java -> src/games/performance/PerformanceConsole.java

--- src/games/performance/PerformanceConsole.java
+++ src/games/performance/PerformanceConsole.java
@@ -1,4 +1,6 @@
-package games.strategy.debug;
+package games.performance;
+
+import games.strategy.debug.GenericConsole;
 
 public class PerformanceConsole extends GenericConsole {
   private static final long serialVersionUID = -1249524819991242464L;
