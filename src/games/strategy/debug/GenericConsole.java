a1d0265d77a99229352bacf0ff117cebe078dd3f
DanVanAtta <dhvatta@gmail.com>
2016-01-07 00:09:17 -0800

    Add a Performance Console - add basic 'GenericConsole' and 'PerformanceConsole' classes.

---
+++ src/games/strategy/debug/GenericConsole.java
@@ -0,0 +1,11 @@
+package games.strategy.debug;
+
+import javax.swing.JFrame;
+
+public class GenericConsole extends JFrame {
+  private static final long serialVersionUID = 5754914217052820386L;
+
+  public GenericConsole(String title) {
+    super(title);
+  }
+}
