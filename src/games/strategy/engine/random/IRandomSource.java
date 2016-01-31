1bf60fc80a5b097f07d478c6d23dd2ff8c5db99b
DanVanAtta <dhvatta@gmail.com>
2016-01-30 20:24:59 -0800

    [Cleanup for IRandomSource API] Remove unnecessary throws clause. Add a javadoc to getRandom() method, explaining the 'annotation' param.

--- src/games/strategy/engine/random/IRandomSource.java
+++ src/games/strategy/engine/random/IRandomSource.java
@@ -4,7 +4,12 @@ package games.strategy.engine.random;
  * A source for random numbers.
  */
 public interface IRandomSource {
-  public int getRandom(int max, String annotation) throws IllegalArgumentException, IllegalStateException;
 
-  public int[] getRandom(int max, int count, String annotation) throws IllegalArgumentException, IllegalStateException;
+  /**
+   * @param annotation Used by dice servers as the email subject when dice roll results are emailed. Active for
+   *        PBEM games. @see HttpDiceRollerDialog
+   */
+  public int getRandom(int max, String annotation);
+
+  public int[] getRandom(int max, int count, String annotation);
 }
