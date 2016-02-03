fc184664977002270f96899201f4cb6aabdb39f5
DanVanAtta <dhvatta@gmail.com>
2016-02-02 19:18:49 -0800

    [Engine Version on UI] Update EngineVersion to get the engine version value from configuration file, via a PropertyReader object

--- src/games/strategy/engine/EngineVersion.java
+++ src/games/strategy/engine/EngineVersion.java
@@ -1,7 +1,33 @@
 package games.strategy.engine;
 
+import games.strategy.engine.config.GameEngineProperty;
+import games.strategy.engine.config.PropertyReader;
 import games.strategy.util.Version;
 
 public class EngineVersion {
-  public static final Version VERSION = new Version(1, 8, 0, 9);
+  private final Version version;
+  private final String exactVersion;
+
+  public EngineVersion(PropertyReader propertyReader) {
+    exactVersion = propertyReader.readProperty(GameEngineProperty.ENGINE_VERSION);
+    version = new Version(exactVersion);
+  }
+
+
+  /**
+   * @return the game engine 'Version' used for in-game compatibility checks.
+   */
+  public Version getVersion() {
+    return version;
+  }
+
+
+  /**
+   * Intended for use when displaying the game engine version to users.
+   *
+   * @return the full non-truncated game engine version String, as found in the game engine configuration
+   */
+  public String getExactVersion() {
+    return exactVersion;
+  }
 }
