47d3f067830916658a04c96ff6d57e153f32cab2
DanVanAtta <dhvatta@gmail.com>
2016-02-02 18:57:48 -0800

    [ClientContext IOC Container] ClientContext.java - Add to class javadoc

--- src/games/strategy/engine/ClientContext.java
+++ src/games/strategy/engine/ClientContext.java
@@ -13,6 +13,17 @@ import games.strategy.engine.framework.mapDownload.MapListingSource;
  * This class roughly follows the singleton pattern. The singleton instance
  * can be updated, this is allowed to enable a mock instance of this class to
  * be used.
+ *
+ * Caution: the public API of this class will grow to be fairly large. For every object we wish to return, we'll have an
+ * "object()" method that will returns that same object. When things become hard to manage it'll be a good time
+ * to move to an annotation or configuration based IOC framework.
+ *
+ * Second note, try to put as much class specific construction logic into the constructor of each class managed by this
+ * container. This class should focus on just creating and wiring classes together. Contrast that with generating the data
+ * needed to create classes. For example, instead of parsing a file and passing that value to the constructor of another class,
+ * we would instead create an intermediary class that knows everything about which file to parse and how to parse it, and we would
+ * pass that intermediary class to the new class we wish to create. Said in another way, this class should not contain any 'business'
+ * logic.
  */
 public final class ClientContext {
 
@@ -25,7 +36,7 @@ public final class ClientContext {
   }
 
   /** Useful for testing, not meant for normal code paths */
-  public static void setMockHandler( ClientContext mockHandler) {
+  public static void setMockHandler(ClientContext mockHandler) {
     instance = mockHandler;
   }
 
