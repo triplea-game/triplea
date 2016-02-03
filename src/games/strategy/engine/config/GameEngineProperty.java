6bcce2a8f7fa7a0e72b5e4bc201ca7fa7f8cc16c
DanVanAtta <dhvatta@gmail.com>
2016-02-02 19:18:49 -0800

    [Engine Version on UI] Update EngineVersion to get the engine version value from configuration file, via a PropertyReader object

--- src/games/strategy/engine/config/GameEngineProperty.java
+++ src/games/strategy/engine/config/GameEngineProperty.java
@@ -1,8 +1,8 @@
 package games.strategy.engine.config;
 
 public final class GameEngineProperty {
-
   public static final GameEngineProperty MAP_LISTING_SOURCE_FILE = new GameEngineProperty("Map_List_File");
+  public static final GameEngineProperty ENGINE_VERSION = new GameEngineProperty("engine_version");
 
 
   private final String value;
