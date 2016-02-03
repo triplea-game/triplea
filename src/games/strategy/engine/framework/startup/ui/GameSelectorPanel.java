d76bedb91e015d88530c66cfdb9d9fc50b106d92
DanVanAtta <dhvatta@gmail.com>
2016-02-02 22:31:35 -0800

    [Engine Version on UI] GameSelectorPanel.java - Minor, fix warnings, mark a method as static and remove an unused import.

--- src/games/strategy/engine/framework/startup/ui/GameSelectorPanel.java
+++ src/games/strategy/engine/framework/startup/ui/GameSelectorPanel.java
@@ -36,7 +36,6 @@ import games.strategy.engine.data.GameParseException;
 import games.strategy.engine.data.properties.IEditableProperty;
 import games.strategy.engine.data.properties.PropertiesUI;
 import games.strategy.engine.framework.GameRunner;
-import games.strategy.engine.framework.GameRunner2;
 import games.strategy.engine.framework.startup.mc.ClientModel;
 import games.strategy.engine.framework.startup.mc.GameSelectorModel;
 import games.strategy.engine.framework.ui.NewGameChooser;
@@ -115,7 +114,7 @@ public class GameSelectorPanel extends JPanel implements Observer {
    *        maximum number of characters per line
    * @return filename formatted file name - in case it is too long (> maxLength) to two lines
    */
-  private String getFormattedFileNameText(final String fileName, final int maxLength) {
+  private static String getFormattedFileNameText(final String fileName, final int maxLength) {
     if (fileName.length() <= maxLength) {
       return fileName;
     }
@@ -165,9 +164,6 @@ public class GameSelectorPanel extends JPanel implements Observer {
   private void layoutComponents() {
     setLayout(new GridBagLayout());
 
-    add(m_engineVersionLabel, buildGridCell(0, 0, new Insets(10, 10, 3, 5)));
-    add(m_engineVersionText, buildGridCell(1, 0, new Insets(10, 0, 3, 0)));
-
     add(m_nameLabel, buildGridCell(0, 0, new Insets(10, 10, 3, 5)));
     add(m_nameText, buildGridCell(1, 0, new Insets(10, 0, 3, 0)));
 
@@ -185,7 +181,7 @@ public class GameSelectorPanel extends JPanel implements Observer {
 
     add(m_loadSavedGame, buildGridRow(0, 6, new Insets(0, 10, 10, 10)));
 
-    add(m_gameOptions, buildGridRow(0, 7,new Insets(25, 10, 10, 10)));
+    add(m_gameOptions, buildGridRow(0, 7, new Insets(25, 10, 10, 10)));
 
     // spacer
     add(new JPanel(), new GridBagConstraints(0, 8, 2, 1, 1, 1, GridBagConstraints.CENTER, GridBagConstraints.BOTH,
