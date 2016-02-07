a91e91df0fed9a8deb7739387dcc6644496d8f2a
Dan Van Atta <dhvatta@gmail.com>
2016-02-07 11:26:36 -0800

    [Add a SystemChecker] Add classes that support running local system checks at game launch. So far we check that we are able to create temporary files and able to connect to the internet. The goal behind this is to let users know earlier rather than later that some features may not work. This then allows us to focus on slightly more detailed error messages up front, and slightly less detailed messages later as we potentially encounter various error scenarios.

---
+++ src/games/strategy/engine/framework/systemcheck/LocalSystemChecker.java
@@ -0,0 +1,68 @@
+package games.strategy.engine.framework.systemcheck;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Set;
+
+import com.google.common.base.Throwables;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Sets;
+
+/**
+ * This class runs a set of local system checks, like access network, and create a temp file.
+ * Each check is always run, and this class records the results of those checks.
+ */
+public final class LocalSystemChecker {
+
+  private final Set<SystemCheck> systemChecks;
+
+  public LocalSystemChecker() {
+    this(ImmutableSet.of(defaultNetworkCheck(), defaultFileSystemCheck()));
+  }
+
+  private static final SystemCheck defaultNetworkCheck() {
+    return new SystemCheck("Can connect to internet", () -> {
+      try {
+        URL url = new URL("http://www.github.com");
+        url.openConnection();
+      } catch (Exception e) {
+        Throwables.propagate(e);
+      }
+    });
+  }
+
+  private static final SystemCheck defaultFileSystemCheck() {
+    return new SystemCheck("Can create temporary files", () -> {
+      try {
+        File.createTempFile("prefix", "suffix");
+      } catch (IOException e) {
+        Throwables.propagate(e);
+      }
+    });
+  }
+
+
+  protected LocalSystemChecker(Set<SystemCheck> checks) {
+    systemChecks = checks;
+  }
+
+  /** Return any exceptions encountered while running each each check */
+  public Set<Exception> getExceptions() {
+    final Set<Exception> exceptions = Sets.newHashSet();
+    for (SystemCheck systemCheck : systemChecks) {
+      if (systemCheck.getException().isPresent()) {
+        exceptions.add(systemCheck.getException().get());
+      }
+    }
+    return exceptions;
+  }
+
+  public String getStatusMessage() {
+    StringBuilder sb = new StringBuilder();
+    for (SystemCheck systemCheck : systemChecks) {
+      sb.append(systemCheck.getResultMessage()).append("\n");
+    }
+    return sb.toString();
+  }
+}
