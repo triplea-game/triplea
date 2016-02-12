e2d45fe3978b9e9003e5de6f3fdf610a02e30595
Dan Van Atta <dhvatta@gmail.com>
2016-02-11 20:21:37 -0800

    [Code Updates to Determine the List of Available/Installed/Out-of-Date Maps] - Add FileSystemStrategy.java, a class that can be used to access the local file system, will be used to determine if a map is present on the file system or not. - Add MapDownloadList, a utility class that accepts a list of download description objects and splits it out into three new lists: available, out of date, installed.

---
+++ test/games/strategy/engine/framework/mapDownload/MapDownloadListTest.java
@@ -0,0 +1,91 @@
+package games.strategy.engine.framework.mapDownload;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import com.google.common.collect.Lists;
+
+import games.strategy.util.Version;
+
+
+@RunWith(MockitoJUnitRunner.class)
+public class MapDownloadListTest {
+
+
+  private final static String MAP_NAME = "new_test_order";
+  private final static Version MAP_VERSION = new Version(10,10);
+  private final static Version lowVersion = new Version(0,0);
+
+  private final static DownloadFileDescription TEST_MAP =
+      new DownloadFileDescription("", "", MAP_NAME, MAP_VERSION, "");
+
+  @Mock
+  private FileSystemStrategy strategy;
+
+  private List<DownloadFileDescription> descriptions;
+
+  @Before
+  public void setUp() throws Exception {
+    descriptions = Lists.newArrayList();
+    descriptions.add(TEST_MAP);
+
+  }
+
+
+  @Test
+  public void testAvailable() {
+    when(strategy.getMapVersion(MAP_NAME)).thenReturn(Optional.empty());
+    MapDownloadList testObj = new MapDownloadList(descriptions, strategy);
+
+    List<DownloadFileDescription> available = testObj.getAvailable();
+    assertThat(available.size(), is(1));
+
+    List<DownloadFileDescription> installed = testObj.getInstalled();
+    assertThat(installed.size(), is(0));
+
+    List<DownloadFileDescription> outOfDate = testObj.getOutOfDate();
+    assertThat(outOfDate.size(), is(0));
+  }
+
+  @Test
+  public void testInstalled() {
+    when(strategy.getMapVersion(MAP_NAME)).thenReturn(Optional.of(MAP_VERSION));
+    MapDownloadList testObj = new MapDownloadList(descriptions, strategy);
+
+    List<DownloadFileDescription> available = testObj.getAvailable();
+    assertThat(available.size(), is(0));
+
+    List<DownloadFileDescription> installed = testObj.getInstalled();
+    assertThat(installed.size(), is(1));
+
+    List<DownloadFileDescription> outOfDate = testObj.getOutOfDate();
+    assertThat(outOfDate.size(), is(0));
+  }
+
+  @Test
+  public void testOutOfDate() {
+    when(strategy.getMapVersion(MAP_NAME)).thenReturn(Optional.of(lowVersion));
+    MapDownloadList testObj = new MapDownloadList(descriptions, strategy);
+
+    List<DownloadFileDescription> available = testObj.getAvailable();
+    assertThat(available.size(), is(0));
+
+    List<DownloadFileDescription> installed = testObj.getInstalled();
+    assertThat(installed.size(), is(1));
+
+    List<DownloadFileDescription> outOfDate = testObj.getOutOfDate();
+    assertThat(outOfDate.size(), is(1));
+  }
+
+
+}
