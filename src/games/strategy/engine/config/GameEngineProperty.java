a653f79f280352113e704f6c3042ab2a9aa26db1
Dan Van Atta <dhvatta@gmail.com>
2016-02-13 11:45:50 -0800

    [Java Style Enum to Actual Enum] Convert GameEngineProperty to an enum type rather than being a 'java style enum'.

--- src/games/strategy/engine/config/GameEngineProperty.java
+++ src/games/strategy/engine/config/GameEngineProperty.java
@@ -1,14 +1,14 @@
 package games.strategy.engine.config;
 
-public final class GameEngineProperty {
-  public static final GameEngineProperty MAP_LISTING_SOURCE_FILE = new GameEngineProperty("Map_List_File");
-  public static final GameEngineProperty ENGINE_VERSION = new GameEngineProperty("engine_version");
-
+public enum GameEngineProperty {
+  MAP_LISTING_SOURCE_FILE("Map_List_File"), ENGINE_VERSION("engine_version");
 
   private final String value;
-  private GameEngineProperty(String value) {
+
+  private GameEngineProperty(final String value) {
     this.value = value;
   }
+
   @Override
   public String toString() {
     return value;
