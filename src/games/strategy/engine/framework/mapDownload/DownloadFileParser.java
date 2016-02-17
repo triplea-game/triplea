9ab72dda6ef0176fcc13334f51024033ad43c154
Dan Van Atta <dhvatta@gmail.com>
2016-02-17 02:58:55 -0800

    Add additional utility methods to DownloadFileDescription, and update parsing to fill in an additional mapType property

--- src/games/strategy/engine/framework/mapDownload/DownloadFileParser.java
+++ src/games/strategy/engine/framework/mapDownload/DownloadFileParser.java
@@ -1,12 +1,11 @@
 package games.strategy.engine.framework.mapDownload;
 
+import static com.google.common.base.Preconditions.checkState;
+
 import java.io.InputStream;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Locale;
 import java.util.Set;
 
 import javax.xml.parsers.SAXParser;
@@ -19,8 +18,20 @@ import org.xml.sax.helpers.DefaultHandler;
 
 import games.strategy.util.Version;
 
-public class DownloadFileParser {
-  public static List<DownloadFileDescription> parse(final InputStream is, final String hostedUrl) {
+/** Utility class to parse an available map list file config file - used to determine which maps are available for download */
+public final class DownloadFileParser {
+
+  private DownloadFileParser() {}
+
+  public static enum Tags {
+    mapType, version, mapName, game, description
+  }
+
+  public static enum ValueType {
+    MAP, MAP_TOOL, MAP_SKIN, MAP_MOD
+  }
+
+  public static List<DownloadFileDescription> parse(final InputStream is) {
     final List<DownloadFileDescription> rVal = new ArrayList<DownloadFileDescription>();
     try {
       final SAXParser parser = SAXParserFactory.newInstance().newSAXParser();
@@ -30,6 +41,7 @@ public class DownloadFileParser {
         private String description;
         private String mapName;
         private Version version;
+        private DownloadFileDescription.DownloadType downloadType;
 
         @Override
         public void characters(final char[] ch, final int start, final int length) throws SAXException {
@@ -39,18 +51,28 @@ public class DownloadFileParser {
         @Override
         public void endElement(final String uri, final String localName, final String qName) throws SAXException {
           final String elementName = qName;
-          if (elementName.equals("description")) {
+
+
+          if (elementName.equals(Tags.description.toString())) {
             description = content.toString().trim();
           } else if (elementName.equals("url")) {
             url = content.toString().trim();
-          } else if (elementName.equals("mapName")) {
+          } else if (elementName.equals(Tags.mapName.toString())) {
             mapName = content.toString().trim();
-          } else if (elementName.equals("version")) {
+          } else if (elementName.equals(Tags.mapType.toString())) {
+            downloadType = DownloadFileDescription.DownloadType.valueOf(content.toString().trim());
+          } else if (elementName.equals(Tags.version.toString())) {
             this.version = new Version(content.toString().trim());
-          } else if (elementName.equals("game")) {
-            rVal.add(new DownloadFileDescription(url, description, mapName, version, hostedUrl));
+          } else if (elementName.equals(Tags.game.toString())) {
+            if (downloadType == null) {
+              downloadType = DownloadFileDescription.DownloadType.MAP;
+            }
+            DownloadFileDescription downloadFileDescription =
+                new DownloadFileDescription(url, description, mapName, version, downloadType);
+            rVal.add(downloadFileDescription);
             // clear optional properties
             version = null;
+            downloadType = null;
           } else if (!elementName.equals("games")) {
             throw new IllegalStateException("unexpected tag:" + elementName);
           }
@@ -58,9 +80,11 @@ public class DownloadFileParser {
         }
       });
     } catch (final SAXParseException e) {
+      e.printStackTrace();
       throw new IllegalStateException("Could not parse xml error at line:" + e.getLineNumber() + " column:"
           + e.getColumnNumber() + " error:" + e.getMessage());
     } catch (final Exception e) {
+      e.printStackTrace();
       throw new IllegalStateException(e);
     }
     validate(rVal);
@@ -70,37 +94,20 @@ public class DownloadFileParser {
   private static void validate(final List<DownloadFileDescription> downloads) {
     final Set<String> urls = new HashSet<String>();
     final Set<String> names = new HashSet<String>();
-    for (final DownloadFileDescription d : downloads) {
-      if (isEmpty(d.getUrl())) {
-        throw new IllegalStateException("Missing game url");
-      }
-      // ignore urls
-      if (!d.isDummyUrl()) {
-        if (isEmpty(d.getDescription())) {
-          throw new IllegalStateException("Missing game description");
-        }
-        if (isEmpty(d.getMapName())) {
-          throw new IllegalStateException("Missing map name");
-        }
-        if (!names.add(d.getMapName())) {
-          throw new IllegalStateException("duplicate mapName:" + d.getMapName());
-        }
-        if (!urls.add(d.getUrl())) {
-          throw new IllegalStateException("duplicate url:" + d.getUrl());
-        }
-        try {
-          final URL url = new URL(d.getUrl());
-          if (!url.getProtocol().toLowerCase(Locale.ENGLISH).startsWith("http")) {
-            throw new IllegalStateException("Url must start with http:" + url);
-          }
-        } catch (final MalformedURLException e) {
-          throw new IllegalStateException(e);
-        }
+
+
+    for (DownloadFileDescription download : downloads) {
+      checkState(!download.getUrl().isEmpty());
+      if (!download.isDummyUrl()) {
+        checkState(!download.getDescription().isEmpty());
+        checkState(!download.getMapName().isEmpty());
+        checkState(!download.getUrl().isEmpty());
+
+        checkState(names.add(download.getMapName()), "duplicate mapName:" + download.getMapName());
+        checkState(urls.add(download.getUrl()), "duplicate url:" + download.getUrl());
+        // verify we can parse a URL
+        download.newURL();
       }
     }
-  }
-
-  private static boolean isEmpty(final String s) {
-    return s == null || s.trim().length() == 0;
   }
 }
