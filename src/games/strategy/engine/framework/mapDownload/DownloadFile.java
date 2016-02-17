c6dd86d1ab33bee3eb29a6a50fa4f256e1f445a0
Dan Van Atta <dhvatta@gmail.com>
2016-02-17 03:16:28 -0800

    Overhaul InstallMapDialog, map download UI to sort maps in to tabs by game type, and then again by whether maps are installed or available for download. Rework the install maps UI in a number of other ways, add a feedback button to open a maps github issue queue, list the file download size on the map download UI, add the capability to remove maps that are installed, use progress bars for map downloads, and convert InstallMapDialog from a JDialog to a JFrame so it can be non-modal.

---
+++ src/games/strategy/engine/framework/mapDownload/DownloadFile.java
@@ -0,0 +1,130 @@
+package games.strategy.engine.framework.mapDownload;
+
+import java.io.File;
+import java.util.List;
+import java.util.function.Consumer;
+
+import com.google.common.collect.Lists;
+import com.google.common.io.Files;
+
+import games.strategy.debug.ClientLogger;
+import games.strategy.engine.ClientContext;
+import games.strategy.engine.ClientFileSystemHelper;
+import games.strategy.performance.Perf;
+import games.strategy.performance.PerfTimer;
+
+/**
+ * Keeps track of the state for a file download from a URL.
+ * This class notifies listeners as appropriate while download state changes.
+ * A 'DownloadStrategy' does the heavy lifting URL download work.
+ *
+ * @see MapDownloadStrategy
+ */
+public class DownloadFile {
+
+  public static enum DownloadState {
+    NOT_STARTED, DOWNLOADING, CANCELLED, DONE
+  }
+
+  private final MapDownloadStrategy strategy;
+  private final List<Runnable> downloadCompletedListeners;
+  private final Consumer<Integer> progressUpdateListener;
+  private final DownloadFileDescription downloadDescription;
+  private volatile DownloadState state = DownloadState.NOT_STARTED;
+
+  /**
+   * Creates a new FileDownload object.
+   * Does not actually start the download, call 'startAsyncDownload()' to start the donwload.
+   *
+   * @param download The details of what to download
+   * @param progressUpdateListener Called periodically while download progress is made.
+   * @param completionListener Called when the File download is complete.
+   */
+  public DownloadFile(DownloadFileDescription download, Consumer<Integer> progressUpdateListener,
+      Runnable completionListener) {
+    this(download, progressUpdateListener, ClientContext.mapDownloadStrategy());
+    this.addDownloadCompletedListener(completionListener);
+  }
+
+  protected DownloadFile(DownloadFileDescription download, Consumer<Integer> progressUpdateListener,
+      MapDownloadStrategy downloadStrategy) {
+    this.downloadDescription = download;
+    this.strategy = downloadStrategy;
+    this.progressUpdateListener = progressUpdateListener;
+    this.downloadCompletedListeners = Lists.newArrayList();
+  }
+
+  public void startAsyncDownload() {
+    final File fileToDownloadTo = ClientFileSystemHelper.createTempFile();
+    final FileSizeWatcher watcher = new FileSizeWatcher(fileToDownloadTo, progressUpdateListener);
+    addDownloadCompletedListener(() -> watcher.stop());
+    state = DownloadState.DOWNLOADING;
+    createDownloadThread(fileToDownloadTo).start();
+  }
+
+
+  /*
+   * Creates a thread that will download to a target temporary file, and once
+   * complete and if the download state is not cancelled, it will then move
+   * the completed download temp file to: 'downloadDescription.getInstallLocation()'
+   */
+  private Thread createDownloadThread(final File fileToDownloadTo) {
+    return new Thread(() -> {
+      if (state != DownloadState.CANCELLED) {
+        System.out.println("DOWNLOADING: " + downloadDescription.getMapName());
+
+        try (PerfTimer timer = Perf.startTimer("Download map: " + downloadDescription.getUrl())) {
+          strategy.download(downloadDescription.newURL(), fileToDownloadTo);
+        }
+        if (state == DownloadState.CANCELLED) {
+          return;
+        }
+        state = DownloadState.DONE;
+        try {
+          Files.move(fileToDownloadTo, downloadDescription.getInstallLocation());
+
+          final DownloadFileProperties props = new DownloadFileProperties();
+          props.setFrom(downloadDescription);
+          DownloadFileProperties.saveForZip(downloadDescription.getInstallLocation(), props);
+
+        } catch (Exception e) {
+          String msg = "Failed to move downloaded file (" + fileToDownloadTo.getAbsolutePath() + ") to: "
+              + downloadDescription.getInstallLocation().getAbsolutePath();
+          ClientLogger.logError(msg, e);
+        }
+      }
+      // notify listeners we finished the download
+      downloadCompletedListeners.forEach(e -> e.run());
+    });
+
+  }
+
+  protected DownloadState getDownloadState() {
+    return state;
+  }
+
+  public void cancelDownload() {
+    if (!isDone()) {
+      System.out.println("Cancelled: " + downloadDescription.getMapName());
+      state = DownloadState.CANCELLED;
+    } else {
+      System.out.println("Cancelled: , but was done" + downloadDescription.getMapName());
+    }
+  }
+
+  public boolean isDone() {
+    return state == DownloadState.CANCELLED || state == DownloadState.DONE;
+  }
+
+  public boolean isInProgress() {
+    return state == DownloadState.DOWNLOADING;
+  }
+
+  public boolean isWaiting() {
+    return state == DownloadState.NOT_STARTED;
+  }
+
+  public void addDownloadCompletedListener(Runnable listener) {
+    downloadCompletedListeners.add(listener);
+  }
+}
