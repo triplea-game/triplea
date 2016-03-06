df147cdd6ef2b802829660436cad5c97b8aa93f0
Dan Van Atta <dhvatta@gmail.com>
2016-03-05 20:59:25 -0800

    Fix for component state change not done on event dispatch Thread in GenericConsole.
    To address error message:
    org.pushingpixels.substance.api.UiThreadingViolationException: Component state change must be done on Event Dispatch Thread
    
    at games.strategy.debug.GenericConsole.(GenericConsole.java:42)

--- src/games/strategy/debug/GenericConsole.java
+++ src/games/strategy/debug/GenericConsole.java
@@ -14,6 +14,7 @@ import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.JToolBar;
 import javax.swing.SwingConstants;
+import javax.swing.SwingUtilities;
 import javax.swing.WindowConstants;
 
 import games.strategy.common.swing.SwingAction;
@@ -39,7 +40,7 @@ public abstract class GenericConsole extends JFrame {
     m_actions.add(m_propertiesAction);
     m_actions.add(m_copyAction);
     m_actions.add(m_clearAction);
-    pack();
+    SwingUtilities.invokeLater(() -> pack());
   }
 
   public abstract GenericConsole getConsoleInstance();
