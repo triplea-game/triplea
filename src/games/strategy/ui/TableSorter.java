56c87da01d2082fa190ac8ffbd93ce8810f04bfd
RoiEX <RoiEXLab@users.noreply.github.com>
2016-05-26 12:50:00 +0200

    Enhanced - Reformatted

--- src/games/strategy/ui/TableSorter.java
+++ src/games/strategy/ui/TableSorter.java
@@ -74,8 +74,9 @@ public class TableSorter extends AbstractTableModel {
   public static final int NOT_SORTED = 0;
   public static final int ASCENDING = 1;
   private static Directive EMPTY_DIRECTIVE = new Directive(-1, NOT_SORTED);
+  //TODO needs to be rewritten in order to remove the warning
   @SuppressWarnings("unchecked")
-  public static final Comparator<Object> COMPARABLE_COMAPRATOR = (Comparator<Object>)(o1, o2) -> ((Comparable<Object>) o1).compareTo(o2);//TODO needs to be rewritten in order to remove the warning
+  public static final Comparator<Object> COMPARABLE_COMAPRATOR = (Comparator<Object>)(o1, o2) -> ((Comparable<Object>) o1).compareTo(o2);
   public static final Comparator<Object> LEXICAL_COMPARATOR = (Comparator<Object>)(o1, o2) -> o1.toString().compareTo(o2.toString());
   private Row[] viewToModel;
   private int[] modelToView;
