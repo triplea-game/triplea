da8f08f5daa9fb01e0f6cf0c3fd3af00d9331f98
Ron <ron.murhammer@gmail.com>
2016-05-26 22:33:00 -0500

    Add new unit method to compare whether 2 units have the same properties and state

--- src/games/strategy/engine/data/Unit.java
+++ src/games/strategy/engine/data/Unit.java
@@ -95,6 +95,13 @@ public class Unit extends GameDataComponent implements Serializable {
     return this.m_uid.equals(other.m_uid);
   }
 
+  public boolean hasSamePropertiesAndState(final Unit unit) {
+    if (m_type == null || m_owner == null) {
+      return false;
+    }
+    return m_type.equals(unit.getType()) && m_owner.equals(unit.getOwner()) && m_hits == unit.getHits();
+  }
+
   @Override
   public int hashCode() {
     if (m_type == null || m_owner == null || m_uid == null || this.getData() == null) {
