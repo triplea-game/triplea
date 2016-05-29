dbe497b53263b02840d44b394e8cbfd72e131ff0
RoiEX <RoiEXLab@users.noreply.github.com>
2016-05-29 22:53:41 +0200

    Added missing tests

--- test/games/strategy/triplea/ui/TestRoute.java
+++ test/games/strategy/triplea/ui/TestRoute.java
@@ -2,6 +2,9 @@ package games.strategy.triplea.ui;
 
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 import java.awt.Point;
 
@@ -9,6 +12,10 @@ import org.apache.commons.math3.analysis.interpolation.SplineInterpolator;
 import org.apache.commons.math3.analysis.polynomials.PolynomialSplineFunction;
 import org.junit.Ignore;
 import org.junit.Test;
+
+import games.strategy.engine.data.Route;
+import games.strategy.engine.data.Territory;
+import games.strategy.util.Tuple;
 
 public class TestRoute {
   private Point[] dummyPoints = new Point[]{new Point(0,0), new Point(100,0), new Point(0,100)};
@@ -25,25 +32,34 @@ public class TestRoute {
   }
   
   @Test
-  @Ignore
+  @Ignore//Not working...
   public void testCurve(){//TODO multiply the value from coords array with a unknown factor in order to get the original value
     final double[] testYValues = new double[]{20, 40, 90};
     final PolynomialSplineFunction testFunction = new SplineInterpolator().interpolate(dummyIndex, testYValues);
     final double[] coords = MapRouteDrawer.getCoords(testFunction, dummyIndex);
-    assertEquals(testYValues[0], coords[(int)Math.round(dummyIndex[0])], 0);
-    assertEquals(testYValues[1], coords[(int)Math.round(dummyIndex[1])], 0);
-    assertEquals(testYValues[2], coords[(int)Math.round(dummyIndex[2])], 0);
+    final double stepSize = testFunction.getKnots()[testFunction.getKnots().length - 1] / coords.length;
+    assertEquals(testYValues[0], coords[(int)Math.round(dummyIndex[0]) + 1] * stepSize, 1);
+    assertEquals(testYValues[1], coords[(int)Math.round(dummyIndex[1]) + 1] * stepSize, 1);
+    assertEquals(testYValues[2], coords[(int)Math.round(dummyIndex[2]) + 1] * stepSize, 1);
   }
   
   @Test
-  @Ignore
   public void testPointSplitting(){
-//    MapRouteDrawer.pointsToDoubleArrays(points);
+    double[] xCoords = new double[]{0, 100, 0};
+    double[] yCoords = new double[]{0, 0, 100};
+    Tuple<double[], double[]> dummyCoords = MapRouteDrawer.pointsToDoubleArrays(dummyPoints);
+    assertArrayEquals(xCoords, dummyCoords.getFirst(), 0);
+    assertArrayEquals(yCoords, dummyCoords.getSecond(), 0);
   }
   
   @Test
-  @Ignore
   public void testPointAcquisition(){
-//    MapRouteDrawer.getRoutePoints(routeDescription, mapData);
+    Route dummyRoute = new Route();
+    MapData dummyMapData = mock(MapData.class);
+    when(dummyMapData.getCenter(any(Territory.class))).thenReturn(dummyPoints[1]);
+    dummyRoute.add(mock(Territory.class));//This will e overridden with the startPoint, since it's the origin territory
+    dummyRoute.add(mock(Territory.class));
+    RouteDescription dummyRouteDescription = new RouteDescription(dummyRoute, dummyPoints[0], dummyPoints[2], null);
+    assertArrayEquals(dummyPoints, MapRouteDrawer.getRoutePoints(dummyRouteDescription, dummyMapData));
   }
 }
