7c3113da1a6d617871f97a498e7e96c9a8ed2321
Dan Van Atta <dhvatta@gmail.com>
2016-06-11 12:36:52 -0700

    PurchasePanel - use lambda instead of runnable

--- src/games/strategy/triplea/ui/PurchasePanel.java
+++ src/games/strategy/triplea/ui/PurchasePanel.java
@@ -56,46 +56,39 @@ public class PurchasePanel extends ActionPanel {
   public void display(final PlayerID id) {
     super.display(id);
     m_purchase = new IntegerMap<>();
-    SwingUtilities.invokeLater(new Runnable() {
-      @Override
-      public void run() {
-        removeAll();
-        actionLabel.setText(id.getName() + " production");
-        m_buyButton.setText(BUY);
-        add(actionLabel);
-        add(m_buyButton);
-        add(new JButton(DoneAction));
-        m_purchasedLabel.setText("");
-        add(Box.createVerticalStrut(9));
-        add(m_purchasedLabel);
+    SwingUtilities.invokeLater(() -> {
+      removeAll();
+      actionLabel.setText(id.getName() + " production");
+      m_buyButton.setText(BUY);
+      add(actionLabel);
+      add(m_buyButton);
+      add(new JButton(DoneAction));
+      m_purchasedLabel.setText("");
+      add(Box.createVerticalStrut(9));
+      add(m_purchasedLabel);
+      add(Box.createVerticalStrut(4));
+      m_purhcasedUnits.setUnitsFromProductionRuleMap(new IntegerMap<>(), id, getData());
+      add(m_purhcasedUnits);
+      getData().acquireReadLock();
+      try {
+        m_purchasedPreviousRoundsUnits.setUnitsFromCategories(UnitSeperator.categorize(id.getUnits().getUnits()),
+            getData());
         add(Box.createVerticalStrut(4));
-        m_purhcasedUnits.setUnitsFromProductionRuleMap(new IntegerMap<>(), id, getData());
-        add(m_purhcasedUnits);
-        getData().acquireReadLock();
-        try {
-          m_purchasedPreviousRoundsUnits.setUnitsFromCategories(UnitSeperator.categorize(id.getUnits().getUnits()),
-              getData());
-          add(Box.createVerticalStrut(4));
-          if (!id.getUnits().isEmpty()) {
-            add(m_purchasedPreviousRoundsLabel);
-          }
-          add(m_purchasedPreviousRoundsUnits);
-        } finally {
-          getData().releaseReadLock();
+        if (!id.getUnits().isEmpty()) {
+          add(m_purchasedPreviousRoundsLabel);
         }
-        add(Box.createVerticalGlue());
-        SwingUtilities.invokeLater(REFRESH);
+        add(m_purchasedPreviousRoundsUnits);
+      } finally {
+        getData().releaseReadLock();
       }
+      add(Box.createVerticalGlue());
+      SwingUtilities.invokeLater(REFRESH);
     });
   }
 
   private void refreshActionLabelText() {
-    SwingUtilities.invokeLater(new Runnable() {
-      @Override
-      public void run() {
-        actionLabel.setText(getCurrentPlayer().getName() + " production " + (m_bid ? " for bid" : ""));
-      }
-    });
+    SwingUtilities.invokeLater(
+        () -> actionLabel.setText(getCurrentPlayer().getName() + " production " + (m_bid ? " for bid" : "")));
   }
 
   public IntegerMap<ProductionRule> waitForPurchase(final boolean bid) {
