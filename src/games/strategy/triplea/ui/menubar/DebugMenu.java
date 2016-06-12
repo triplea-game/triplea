e7e48d11fa35478b26824854beb31de3c0b5ad29
Dan Van Atta <dhvatta@gmail.com>
2016-06-12 13:15:58 -0700

    DebugMenu cleanup

--- src/games/strategy/triplea/ui/menubar/DebugMenu.java
+++ src/games/strategy/triplea/ui/menubar/DebugMenu.java
@@ -13,37 +13,25 @@ import games.strategy.performance.EnablePerformanceLoggingCheckBox;
 import games.strategy.triplea.ai.proAI.ProAI;
 import games.strategy.triplea.ui.TripleAFrame;
 import games.strategy.ui.SwingAction;
+import games.strategy.ui.SwingComponents;
 
 public class DebugMenu {
 
   public DebugMenu(final JMenuBar menuBar, final TripleAFrame frame) {
-    final JMenu debugMenu = new JMenu("Debug");
+    final JMenu debugMenu = SwingComponents.newJMenu("Debug", SwingComponents.KeyboardCode.D);
     menuBar.add(debugMenu);
-    addChangeProAISettings(debugMenu, frame);
-    debugMenu.add(new EnablePerformanceLoggingCheckBox());
-    debugMenu.setMnemonic(KeyEvent.VK_D);
-    addConsoleMenu(debugMenu);
-  }
 
-  private void addChangeProAISettings(final JMenu parentMenu, final TripleAFrame frame) {
-    boolean areThereProAIs = false;
     final Set<IGamePlayer> players = frame.getLocalPlayers().getLocalPlayers();
-    for (final IGamePlayer player : players) {
-      if (player instanceof ProAI) {
-        areThereProAIs = true;
-      }
-    }
+    boolean areThereProAIs = players.stream().filter(player -> player instanceof  ProAI ).findFirst().isPresent();
     if (areThereProAIs) {
       ProAI.initialize(frame);
-      parentMenu.add(SwingAction.of("Show Hard AI Logs", e -> ProAI.showSettingsWindow())).setMnemonic(KeyEvent.VK_X);
+      debugMenu.add(SwingAction.of("Show Hard AI Logs", e -> ProAI.showSettingsWindow())).setMnemonic(KeyEvent.VK_X);
     }
-  }
 
-  private void addConsoleMenu(final JMenu parentMenu) {
-    parentMenu.add(SwingAction.of("Show Console...", e -> {
+    debugMenu.add(new EnablePerformanceLoggingCheckBox());
+    debugMenu.add(SwingAction.of("Show Console...", e -> {
       ErrorConsole.getConsole().setVisible(true);
       ErrorConsole.getConsole().append(DebugUtils.getMemory());
     })).setMnemonic(KeyEvent.VK_C);
   }
-
 }
