41722c9956ca925b9761d42716de0b054913ea54
Dan Van Atta <dhvatta@gmail.com>
2016-06-13 01:10:32 -0700

    Simplify SystemPreferences API, minor update to revert confirmation dialog

--- src/games/strategy/triplea/settings/SystemPreferences.java
+++ src/games/strategy/triplea/settings/SystemPreferences.java
@@ -9,32 +9,32 @@ public class SystemPreferences {
    * Note, this method does a flush after being called, which is slow.
    * If there is a need to do many of these one after another, then call 'putNoFlush'
    */
-  public static void put(Class className, PreferenceKey key, String value) {
-    putNoFlush(className, key, value);
-    flush(className);
+  public static void put(PreferenceKey key, String value) {
+    putNoFlush(key, value);
+    flush();
   }
 
-  public static void putNoFlush(Class className, PreferenceKey key, String value) {
-    Preferences prefs = Preferences.userNodeForPackage(className);
+  public static void putNoFlush(PreferenceKey key, String value) {
+    Preferences prefs = Preferences.userNodeForPackage(SystemPreferences.class);
     prefs.put(key.name(), value);
   }
 
-  public static void flush(Class className) {
-    Preferences prefs = Preferences.userNodeForPackage(className);
+  public static void flush() {
+    Preferences prefs = Preferences.userNodeForPackage(SystemPreferences.class);
     try {
       prefs.flush();
     } catch (BackingStoreException e) {
-      throw new IllegalStateException("Failed to persist: " + className.getName());
+      throw new IllegalStateException("Failed to persist", e);
     }
   }
 
-  public static int get(Class className, PreferenceKey key, int defaultValue) {
-    Preferences prefs = Preferences.userNodeForPackage(className);
+  public static int get(PreferenceKey key, int defaultValue) {
+    Preferences prefs = Preferences.userNodeForPackage(SystemPreferences.class);
     return prefs.getInt(key.name(), defaultValue);
   }
 
-  public static String get(Class className, PreferenceKey key, String defaultValue) {
-    Preferences prefs = Preferences.userNodeForPackage(className);
+  public static String get(PreferenceKey key, String defaultValue) {
+    Preferences prefs = Preferences.userNodeForPackage(SystemPreferences.class);
     return prefs.get(key.name(), defaultValue);
   }
 
