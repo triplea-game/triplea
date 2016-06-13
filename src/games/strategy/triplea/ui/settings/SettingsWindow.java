9ce59d488f16bade4e825d4e96c418371363b10d
Dan Van Atta <dhvatta@gmail.com>
2016-06-12 20:35:08 -0700

    Wire in scroll settings, allow setting values to take effect after 'save', but are not yet persisted between restarts.

--- src/games/strategy/triplea/ui/settings/SettingsWindow.java
+++ src/games/strategy/triplea/ui/settings/SettingsWindow.java
@@ -7,6 +7,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
@@ -47,16 +48,16 @@ public class SettingsWindow extends JFrame {
 
 
     final JPanel windowContents = new JPanel();
-    windowContents.setLayout(new GridLayout(inputs.size(),1));
+    windowContents.setLayout(new GridLayout(inputs.size(), 1));
     windowContents.setAlignmentX(JComponent.LEFT_ALIGNMENT);
 
 
     inputs.forEach(input -> {
       final JPanel rowContents = new JPanel();
-      rowContents.setLayout(new GridLayout(1,2));
+      rowContents.setLayout(new GridLayout(1, 2));
 
       final JPanel labelInputPanel = new JPanel();
-      labelInputPanel.setLayout(new GridLayout(1,2));
+      labelInputPanel.setLayout(new GridLayout(1, 2));
       rowContents.add(labelInputPanel);
 
       JLabel label = new JLabel(input.getLabel());
@@ -76,12 +77,27 @@ public class SettingsWindow extends JFrame {
     });
 
     JPanel panel = new JPanel();
-    panel.add(windowContents,BorderLayout.CENTER);
+    panel.add(windowContents, BorderLayout.CENTER);
+
+
+
+    JPanel buttonsPanel = new JPanel();
+    buttonsPanel.setLayout(new BoxLayout(buttonsPanel, BoxLayout.Y_AXIS));
+
+    JButton useDefaults = SwingComponents.newJButton("To Default",
+        e -> SwingComponents.promptUser("Revert to default?",
+            "Are you sure you would like to go back to default settings?", () -> settingTab.setToDefault()));
+    buttonsPanel.add(useDefaults);
+
+    buttonsPanel.add(Box.createVerticalStrut(100));
 
     JButton saveButton = SwingComponents.newJButton("Save", e -> {
-       settingTab.updateSettings(inputs);
+      settingTab.updateSettings(inputs);
     });
-    panel.add(saveButton, BorderLayout.SOUTH);
+    buttonsPanel.add(saveButton);
+
+
+    panel.add(buttonsPanel);
 
     return new JScrollPane(panel);
   }
