8bd45c97a7a44e5d24cb41068f1c1c96300a5237
Dan Van Atta <dhvatta@gmail.com>
2016-06-14 00:45:54 -0700

    Cleanup

--- src/games/strategy/triplea/settings/InputValidator.java
+++ src/games/strategy/triplea/settings/InputValidator.java
@@ -6,6 +6,7 @@ import java.util.function.Function;
 public interface InputValidator extends Function<String, Boolean> {
 
   InputValidator IS_DIRECTORY = ((string) -> (new File(string)).isDirectory());
+
   InputValidator IS_INTEGER = ((string) -> {
     try {
       Integer.parseInt(string);
@@ -15,6 +16,9 @@ public interface InputValidator extends Function<String, Boolean> {
     }
   });
 
+  /**
+   * Verifies a value is an integer and falls inside of a given range (inclusive)
+   */
   static InputValidator inRange(int min, int max) {
     return (value) -> {
       if(!IS_INTEGER.apply(value)) {
