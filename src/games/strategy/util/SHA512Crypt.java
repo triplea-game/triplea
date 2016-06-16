1db56feac779d4f8ead2e36b7851a0466ebcb83c
RoiEX <RoiEXLab@users.noreply.github.com>
2016-06-16 22:46:43 +0200

    Added library dependency set MD5 Deprecated

--- src/games/strategy/util/SHA512Crypt.java
+++ src/games/strategy/util/SHA512Crypt.java
@@ -8,6 +8,9 @@ import java.security.NoSuchAlgorithmException;
 /**
  * Replacement for MD5Crypt
  * using SHA512 instead
+ * 
+ * Do not use for Passwords!
+ * Use the BCrypt library instead!
  */
 public class SHA512Crypt {
 
@@ -15,19 +18,33 @@ public class SHA512Crypt {
 
   /**
    * Returns the SHA256-Hash of the given String
-   * is used non-static for testing purposes
    */
   public static String crypt(String text) {
+    return crypt(text, "");
+  }
+  
+  /**
+   * Returns the SHA256-Hash of the given String using the specified Salt
+   */
+  public static String crypt(String text, String salt){
     try {
       MessageDigest md = MessageDigest.getInstance(SHA_512);
 
-      md.update(text.getBytes(StandardCharsets.UTF_8));
-      byte[] digest = md.digest();
+      md.update(salt.getBytes(StandardCharsets.UTF_8));
+      byte[] digest = md.digest(text.getBytes(StandardCharsets.UTF_8));
 
       return String.format("%064x", new BigInteger(1, digest));
     } catch (NoSuchAlgorithmException e) {
       // This Code shouldn't be executed under any circumstances
       throw new IllegalStateException("The SHA512Crypt class uses an invalid algorithm", e);
     }
+  }
+  
+  /**
+   * 
+   * Same as {@link SHA512Crypt.crypt(text, salt)}, but passing the salt as well
+   */
+  public static String cryptPassSalt(String text, String salt){
+    return "$" + salt + crypt(text, salt);
   }
 }
