a7fa1224cf85d40a7ad7be22d6fff11bf72d5f44
Dan Van Atta <dhvatta@gmail.com>
2016-06-25 13:53:50 -0700

    Add some basic messaging for invalid values

--- src/games/strategy/triplea/settings/SettingsTab.java
+++ src/games/strategy/triplea/settings/SettingsTab.java
@@ -1,5 +1,6 @@
 package games.strategy.triplea.settings;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import games.strategy.ui.SwingComponents;
@@ -13,6 +14,7 @@ public interface SettingsTab<T extends HasDefaults> {
 
   default void updateSettings(List<SettingInputComponent<T>> inputs) {
     final StringBuilder msg = new StringBuilder();
+    final List<String> invalidValues = new ArrayList<>();
     inputs.forEach(input -> {
       T settingsObject = getSettingsObject();
 
@@ -22,6 +24,8 @@ public interface SettingsTab<T extends HasDefaults> {
         if(!newValue.equals(oldValue)) {
           msg.append("\n").append(input.getLabel()).append(": ").append(oldValue).append(" -> ").append(newValue);
         }
+      } else {
+        invalidValues.add(input.getInputElement().getText());
       }
     });
 
@@ -30,6 +34,13 @@ public interface SettingsTab<T extends HasDefaults> {
       message = "No values updated";
     } else {
       message = "Updated values:" + msg.toString();
+    }
+
+    if( !invalidValues.isEmpty() ) {
+      message += "\nValues out of range, not updated: ";
+      for (String invalidValue : invalidValues) {
+        message += "\n" + invalidValue;
+      }
     }
     SwingComponents.showDialog(message);
   }
