6e9fccb23d26ed8e242904f0991e1be9c4f7b1ae
Dan Van Atta <dhvatta@gmail.com>
2016-06-26 21:49:51 -0700

    TechTracker.java - Simplify if statements

--- src/games/strategy/triplea/delegate/TechTracker.java
+++ src/games/strategy/triplea/delegate/TechTracker.java
@@ -69,13 +69,8 @@ public class TechTracker implements java.io.Serializable {
   public static synchronized void addAdvance(final PlayerID player, final IDelegateBridge bridge,
       final TechAdvance advance) {
     Change attachmentChange;
-    if (advance instanceof GenericTechAdvance) {
-      if (((GenericTechAdvance) advance).getAdvance() == null) {
+    if (advance instanceof GenericTechAdvance && ((GenericTechAdvance) advance).getAdvance() == null) {
         attachmentChange = ChangeFactory.genericTechChange(TechAttachment.get(player), true, advance.getProperty());
-      } else {
-        attachmentChange =
-            ChangeFactory.attachmentPropertyChange(TechAttachment.get(player), "true", advance.getProperty());
-      }
     } else {
       attachmentChange =
           ChangeFactory.attachmentPropertyChange(TechAttachment.get(player), "true", advance.getProperty());
