e81024b348104827cdaae67cd8ea8b07b97c371c
Dan Van Atta <dhvatta@gmail.com>
2016-07-09 00:44:06 -0700

    Cleanup unused+unnecessary in IDrawable.java

--- src/games/strategy/triplea/ui/screen/drawable/IDrawable.java
+++ src/games/strategy/triplea/ui/screen/drawable/IDrawable.java
@@ -1,47 +1,19 @@
 package games.strategy.triplea.ui.screen.drawable;
 
-import games.strategy.engine.data.GameData;
-import games.strategy.engine.data.PlayerID;
-import games.strategy.engine.data.Territory;
-import games.strategy.engine.data.TerritoryEffect;
-import games.strategy.engine.data.Unit;
-import games.strategy.triplea.TripleAUnit;
-import games.strategy.triplea.attachments.TerritoryAttachment;
-import games.strategy.triplea.formatter.MyFormatter;
-import games.strategy.triplea.image.MapImage;
-import games.strategy.triplea.image.TileImageFactory;
-import games.strategy.triplea.ui.IUIContext;
-import games.strategy.triplea.ui.mapdata.MapData;
-import games.strategy.triplea.util.Stopwatch;
-
-import java.awt.Color;
-import java.awt.FontMetrics;
-import java.awt.GradientPaint;
 import java.awt.Graphics2D;
-import java.awt.Image;
-import java.awt.Paint;
-import java.awt.Point;
-import java.awt.Polygon;
 import java.awt.Rectangle;
-import java.awt.RenderingHints;
 import java.awt.geom.AffineTransform;
-import java.util.Comparator;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Optional;
-import java.util.Set;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import games.strategy.engine.data.GameData;
+import games.strategy.triplea.ui.mapdata.MapData;
+
 public interface IDrawable {
-  public Logger s_logger = Logger.getLogger(IDrawable.class.getName());
   int BASE_MAP_LEVEL = 1;
   int POLYGONS_LEVEL = 2;
   int RELIEF_LEVEL = 3;
   int OPTIONAL_EXTRA_TERRITORY_BORDERS_MEDIUM_LEVEL = 4;
   int OPTIONAL_EXTRA_TERRITORY_BORDERS_HIGH_LEVEL = 18;
-  int CONVOY_LEVEL = 5;
   int TERRITORY_EFFECT_LEVEL = 6;
   int CAPITOL_MARKER_LEVEL = 8;
   int VC_MARKER_LEVEL = 9;
@@ -54,7 +26,7 @@ public interface IDrawable {
   /**
    * This is for the optional extra territory borders. LOW means off
    */
-  public static enum OptionalExtraBorderLevel {
+  enum OptionalExtraBorderLevel {
     LOW, MEDIUM, HIGH
   }
 
@@ -64,10 +36,10 @@ public interface IDrawable {
    * <p>
    * The affine transform will be set to the scaled version.
    */
-  public void draw(Rectangle bounds, GameData data, Graphics2D graphics, MapData mapData, AffineTransform unscaled,
+  void draw(Rectangle bounds, GameData data, Graphics2D graphics, MapData mapData, AffineTransform unscaled,
       AffineTransform scaled);
 
-  public int getLevel();
+  int getLevel();
 }
 
 
