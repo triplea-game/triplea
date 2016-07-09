c127b703f5954b46f43610e3fb30fa6f5001bd0c
Dan Van Atta <dhvatta@gmail.com>
2016-07-09 00:41:36 -0700

    Suppress illegal state when loading an image that does not exist, return an optional.empty instead. TODO: update IDrawable.java to fix breaking changes

--- src/games/strategy/triplea/ui/MapPanel.java
+++ src/games/strategy/triplea/ui/MapPanel.java
@@ -791,19 +791,19 @@ public class MapPanel extends ImageScrollerLargeView {
     setCursor(uiContext.getCursor());
   }
 
-  public Image getErrorImage() {
+  public Optional<Image> getErrorImage() {
     return uiContext.getMapData().getErrorImage();
   }
 
-  public Image getWarningImage() {
+  public Optional<Image> getWarningImage() {
     return uiContext.getMapData().getWarningImage();
   }
 
-  public Image getInfoImage() {
+  public Optional<Image> getInfoImage() {
     return uiContext.getMapData().getInfoImage();
   }
 
-  public Image getHelpImage() {
+  public Optional<Image> getHelpImage() {
     return uiContext.getMapData().getHelpImage();
   }
 }
