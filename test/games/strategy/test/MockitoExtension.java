df2ea6d4a8cc71a68788273ad9b15b8a88d8c3b7
RoiEX <RoiEXLab@users.noreply.github.com>
2016-07-12 17:28:00 +0200

    Fixed formatting

--- test/games/strategy/test/MockitoExtension.java
+++ test/games/strategy/test/MockitoExtension.java
@@ -28,48 +28,43 @@ import org.mockito.MockitoAnnotations;
  * and {@link ParameterResolver} extension APIs of JUnit 5 by providing
  * dependency injection support at the field level and at the method parameter
  * level via Mockito 2.x's {@link Mock @Mock} annotation.
- *
- * @since 5.0
  */
 public class MockitoExtension implements TestInstancePostProcessor, ParameterResolver {
 
-    @Override
-    public void postProcessTestInstance(Object testInstance, ExtensionContext context) {
-        MockitoAnnotations.initMocks(testInstance);
+  @Override
+  public void postProcessTestInstance(Object testInstance, ExtensionContext context) {
+    MockitoAnnotations.initMocks(testInstance);
+  }
+
+  @Override
+  public boolean supports(ParameterContext parameterContext, ExtensionContext extensionContext) {
+    return parameterContext.getParameter().isAnnotationPresent(Mock.class);
+  }
+
+  @Override
+  public Object resolve(ParameterContext parameterContext, ExtensionContext extensionContext) {
+    return getMock(parameterContext.getParameter(), extensionContext);
+  }
+
+  private Object getMock(Parameter parameter, ExtensionContext extensionContext) {
+    Class<?> mockType = parameter.getType();
+    Store mocks = extensionContext.getStore(Namespace.create(MockitoExtension.class, mockType));
+    String mockName = getMockName(parameter);
+
+    if (mockName != null) {
+      return mocks.getOrComputeIfAbsent(mockName, key -> mock(mockType, mockName));
+    } else {
+      return mocks.getOrComputeIfAbsent(mockType.getCanonicalName(), key -> mock(mockType));
     }
+  }
 
-    @Override
-    public boolean supports(ParameterContext parameterContext, ExtensionContext extensionContext) {
-        return parameterContext.getParameter().isAnnotationPresent(Mock.class);
+  private String getMockName(Parameter parameter) {
+    String explicitMockName = parameter.getAnnotation(Mock.class).name().trim();
+    if (!explicitMockName.isEmpty()) {
+      return explicitMockName;
+    } else if (parameter.isNamePresent()) {
+      return parameter.getName();
     }
-
-    @Override
-    public Object resolve(ParameterContext parameterContext, ExtensionContext extensionContext) {
-        return getMock(parameterContext.getParameter(), extensionContext);
-    }
-
-    private Object getMock(Parameter parameter, ExtensionContext extensionContext) {
-        Class<?> mockType = parameter.getType();
-        Store mocks = extensionContext.getStore(Namespace.create(MockitoExtension.class, mockType));
-        String mockName = getMockName(parameter);
-
-        if (mockName != null) {
-            return mocks.getOrComputeIfAbsent(mockName, key -> mock(mockType, mockName));
-        }
-        else {
-            return mocks.getOrComputeIfAbsent(mockType.getCanonicalName(), key -> mock(mockType));
-        }
-    }
-
-    private String getMockName(Parameter parameter) {
-        String explicitMockName = parameter.getAnnotation(Mock.class).name().trim();
-        if (!explicitMockName.isEmpty()) {
-            return explicitMockName;
-        }
-        else if (parameter.isNamePresent()) {
-            return parameter.getName();
-        }
-        return null;
-    }
-
+    return null;
+  }
 }
