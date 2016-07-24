c5dbca3f7119fd9d68ca19e9bcdd27ae0f6dea82
Dan Van Atta <dhvatta@gmail.com>
2016-07-24 14:44:45 -0700

    Update testObj name

--- test/games/strategy/engine/chat/ChatFloodControlTest.java
+++ test/games/strategy/engine/chat/ChatFloodControlTest.java
@@ -6,26 +6,26 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 public class ChatFloodControlTest {
-  private final ChatFloodControl fc = new ChatFloodControl();
+  private final ChatFloodControl testObj = new ChatFloodControl();
 
   @Test
   public void testSimple() {
-    assertTrue(fc.allow("", System.currentTimeMillis()));
+    assertTrue(testObj.allow("", System.currentTimeMillis()));
   }
 
   @Test
   public void testDeny() {
     for (int i = 0; i < ChatFloodControl.EVENTS_PER_WINDOW; i++) {
-      fc.allow("", System.currentTimeMillis());
+      testObj.allow("", System.currentTimeMillis());
     }
-    assertFalse(fc.allow("", System.currentTimeMillis()));
+    assertFalse(testObj.allow("", System.currentTimeMillis()));
   }
 
   @Test
   public void testReney() {
     for (int i = 0; i < 100; i++) {
-      fc.allow("", System.currentTimeMillis());
+      testObj.allow("", System.currentTimeMillis());
     }
-    assertTrue(fc.allow("", System.currentTimeMillis() + 1000 * 60 * 60));
+    assertTrue(testObj.allow("", System.currentTimeMillis() + 1000 * 60 * 60));
   }
 }
