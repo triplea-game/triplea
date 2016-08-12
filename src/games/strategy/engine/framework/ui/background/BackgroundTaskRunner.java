59476fcc4f211938f89284d457a61f015385116b
Dan Van Atta <dhvatta@gmail.com>
2016-08-11 22:31:10 -0700

    BackgroundTaskRunner - Actually fix swing threading

--- src/games/strategy/engine/framework/ui/background/BackgroundTaskRunner.java
+++ src/games/strategy/engine/framework/ui/background/BackgroundTaskRunner.java
@@ -6,27 +6,27 @@ import javax.swing.SwingUtilities;
 
 public class BackgroundTaskRunner {
   public static void runInBackground(final String waitMessage, final Runnable r) {
-    if (!SwingUtilities.isEventDispatchThread()) {
-      SwingUtilities.invokeLater(() -> runInBackground(waitMessage, r));
-    }
-    final WaitDialog window = new WaitDialog(null, waitMessage);
-    // this will center the window
-    window.setLocationRelativeTo(null);
-    final AtomicBoolean doneWait = new AtomicBoolean(false);
-    new Thread(() -> {
-      try {
-        r.run();
-      } finally {
-        SwingUtilities.invokeLater(() -> {
-          doneWait.set(true);
-          window.setVisible(false);
-          window.dispose();
-        });
+    SwingUtilities.invokeLater(() -> {
+      final WaitDialog window = new WaitDialog(null, waitMessage);
+      // this will center the window
+      window.setLocationRelativeTo(null);
+      final AtomicBoolean doneWait = new AtomicBoolean(false);
+      new Thread(() -> {
+        try {
+          r.run();
+        } finally {
+          // clean up the window
+          SwingUtilities.invokeLater(() -> {
+            doneWait.set(true);
+            window.setVisible(false);
+            window.dispose();
+          });
+        }
+      }).start();
+      if (!doneWait.get()) {
+        window.pack();
+        window.setVisible(true);
       }
-    }).start();
-    if (!doneWait.get()) {
-      window.pack();
-      window.setVisible(true);
-    }
+    });
   }
 }
