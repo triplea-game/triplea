998f676ed9fccc002ef0bbf6675cf01ccd6aac5c
Dan Van Atta <dhvatta@gmail.com>
2016-08-11 22:23:56 -0700

    BackgroundTaskRunner - Collapse similar 'runInBackground' methods, and simplify parent component parameter since it is always null.

--- src/games/strategy/engine/framework/ui/background/BackgroundTaskRunner.java
+++ src/games/strategy/engine/framework/ui/background/BackgroundTaskRunner.java
@@ -1,16 +1,15 @@
 package games.strategy.engine.framework.ui.background;
 
-import java.awt.Component;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import javax.swing.SwingUtilities;
 
 public class BackgroundTaskRunner {
-  public static void runInBackground(final Component parent, final String waitMessage, final Runnable r) {
+  public static void runInBackground(final String waitMessage, final Runnable r) {
     if (!SwingUtilities.isEventDispatchThread()) {
-      throw new IllegalStateException("Wrong thread");
+      SwingUtilities.invokeLater(() -> runInBackground(waitMessage, r));
     }
-    final WaitDialog window = new WaitDialog(parent, waitMessage);
+    final WaitDialog window = new WaitDialog(null, waitMessage);
     final AtomicBoolean doneWait = new AtomicBoolean(false);
     new Thread(() -> {
       try {
@@ -25,16 +24,7 @@ public class BackgroundTaskRunner {
     }).start();
     if (!doneWait.get()) {
       window.pack();
-      window.setLocationRelativeTo(parent);
       window.setVisible(true);
     }
-  }
-
-  public static void runInBackground(final String waitMessage, final Runnable r) {
-    if (SwingUtilities.isEventDispatchThread()) {
-      runInBackground(null, waitMessage, r);
-      return;
-    }
-    SwingUtilities.invokeLater(() -> runInBackground(null, waitMessage, r));
   }
 }
