1e651bab93cfee0dcdcf138fa4f518f46de84bc1
Dan Van Atta <dhvatta@gmail.com>
2016-08-24 21:33:37 -0700

    Add fix from revert that broke the build.

--- src/games/strategy/util/CountUpAndDownLatch.java
+++ src/games/strategy/util/CountUpAndDownLatch.java
@@ -14,6 +14,7 @@ import java.util.concurrent.locks.AbstractQueuedSynchronizer;
 public class CountUpAndDownLatch implements Serializable {
   private static final long serialVersionUID = -1656388212821764097L;
   private final Sync sync;
+  private int originalCount;
 
   /**
    * Constructs a {@link CountUpAndDownLatch} initialized with zero.
