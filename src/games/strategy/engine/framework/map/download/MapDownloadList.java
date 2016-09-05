d8b3d95a6fde30aca5ac06ebc2cd6be378baa829
Dan Van Atta <dhvatta@gmail.com>
2016-09-04 21:47:49 -0700

    NPE fix for when maps do not have versions attached to them

--- src/games/strategy/engine/framework/map/download/MapDownloadList.java
+++ src/games/strategy/engine/framework/map/download/MapDownloadList.java
@@ -14,11 +14,14 @@ public class MapDownloadList {
 
   public MapDownloadList(final List<DownloadFileDescription> downloads, final FileSystemAccessStrategy strategy) {
     for (final DownloadFileDescription download : downloads) {
+      if(download == null) {
+        return;
+      }
       final Optional<Version> mapVersion = strategy.getMapVersion(download.getInstallLocation().getAbsolutePath());
 
       if (mapVersion.isPresent()) {
         installed.add(download);
-        if (download.getVersion().isGreaterThan(mapVersion.get())) {
+        if (download.getVersion() != null && download.getVersion().isGreaterThan(mapVersion.get())) {
           outOfDate.add(download);
         }
       } else {
