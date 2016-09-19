748096e56c5306635b81906075e196cd7e3ee6ea
simon33-2 <simon58500@bigpond.com>
2016-09-19 22:05:25 +1000

    Additional tests added to #1140

--- src/games/strategy/triplea/delegate/BattleDelegateOrdered.java
+++ src/games/strategy/triplea/delegate/BattleDelegateOrdered.java
@@ -1,8 +1,12 @@
 package games.strategy.triplea.delegate;
 
 import java.util.Collection;
+import java.util.Collections;
+import java.util.ArrayList;
+import java.util.Map;
 
 import games.strategy.engine.data.Territory;
+import games.strategy.engine.data.Unit;
 import games.strategy.triplea.delegate.BattleDelegate;
 import games.strategy.triplea.delegate.IBattle;
 import games.strategy.triplea.delegate.IBattle.BattleType;
@@ -10,71 +14,62 @@ import games.strategy.util.Match;
 import games.strategy.triplea.MapSupport;
 
 /**
- * This is to support the new maps added to world_war_ii_global which enforce strategic bombing first in the combat order 
- * @author simon
- *
+ * This is to support the new maps added to world_war_ii_global which enforce strategic bombing first in the combat
+ * order
+ * 
  */
 @MapSupport
 public class BattleDelegateOrdered extends BattleDelegate {
   /**
-   * After the super class .start() method runs, this is called to fight all battles not requiring significant attacker input,
+   * After the super class .start() method runs, this is called to fight all battles not requiring significant attacker
+   * input,
    * in the order required for Global 1940. Other maps with the same required order can also use it or add it anyway
    */
+  private Collection<IBattle> battleList = new ArrayList<>();
+
   @Override
   public void start() {
     super.start();
-
-    stratBombing();
-    killUndefendedTransports();
-    final int battleCount = processAmphib();
-
-    if( battleCount == 1 ) {  // If there is only one remaining normal combat, fight it here rather than requiring the user to click it.
-      fightRemainingBattle();
-    }
-  }
-
-  // Fight all air and bombing raids which are supposed to be fought first. Order shouldn't matter because there won't normally be an attacker decision
-  private void stratBombing() {
-    for( final Territory t : m_battleTracker.getPendingBattleSites(true) ) {            // Returns all territories with air/bombing raids
+    
+    // Remove air raids - this adds all the bombing raids to the battle tracker
+    for( final Territory t : m_battleTracker.getPendingSBRSites() ) {
       final IBattle airRaid = m_battleTracker.getPendingBattle(t, true, BattleType.AIR_RAID);   // Get air raid for current territory (if any)
       if( airRaid != null ) {
-        airRaid.fight(m_bridge);
+        airRaid.fight(m_bridge);        // Can't add it to the list of battles because the bombing raid wouldn't then be able to be added
       }
-      final IBattle bombingRaid = m_battleTracker.getPendingBattle(t, true, BattleType.BOMBING_RAID );
-      bombingRaid.fight(m_bridge);
+      battleList.add( m_battleTracker.getPendingBattle( t, true, BattleType.BOMBING_RAID ) );
     }
-  }
-  
-  private void killUndefendedTransports() {
-    // Remove all undefended transport battles. Doing so here means that such a fight will not prevent an amphibious assault from being auto-resolved
-    for( final Territory t : m_battleTracker.getPendingBattleSites(false) ) {  // Loop through normal combats i.e. not bombing or air raid
+
+    // Kill undefended transports. Done here to remove potentially dependent sea battles below
+    for( final Territory t : m_battleTracker.getPendingNonSBRSites() ) {  // Loop through normal combats i.e. not bombing or air raid
       final IBattle battle = m_battleTracker.getPendingBattle(t, false, BattleType.NORMAL);
       if( m_battleTracker.getDependentOn(battle).isEmpty() 
           && Match.allMatch( battle.getDefendingUnits(), Matches.UnitIsTransportButNotCombatTransport) ) {
-        battle.fight( m_bridge );
+        battle.fight( m_bridge );           // Must be fought here to remove dependencies
       }
     }
-  }
 
-  private int processAmphib() {
     // Fight all amphibious assaults with no retreat option for the attacker and no sea combat - these have no real attacker decisions
     // Also remove all remaining defenseless fights by fighting them
     int battleCount = 0;
     int amphibCount = 0;
     IBattle lastAmphib = null;
-    for( final Territory t : m_battleTracker.getPendingBattleSites(false) ) {  // Loop through normal combats i.e. not bombing or air raid
+    for( final Territory t : m_battleTracker.getPendingNonSBRSites() ) {  // Loop through normal combats i.e. not bombing or air raid
       final IBattle battle = m_battleTracker.getPendingBattle(t, false, BattleType.NORMAL);
       if( battle instanceof NonFightingBattle && m_battleTracker.getDependentOn(battle).isEmpty() ) {
-        battle.fight( m_bridge );         // Remove non fighting battles by fighting them automatically. Conveniently done here.
+        battleList.add( battle );         // Remove non fighting battles by fighting them automatically. Conveniently done here.
+        System.out.format("Adding non fight battle in %s\n", battle.getTerritory().getName() );
         continue;
       } else if (!(battle instanceof MustFightBattle) ) {
         continue;
       }
       battleCount++;
+      
       if( battle.isAmphibious() ) {
         // If there is no dependent sea battle and no retreat, fight it now 
         if( m_battleTracker.getDependentOn(battle).isEmpty() && !( (MustFightBattle) battle).canAnyAttackersRetreat() ) {
-          battle.fight( m_bridge );
+          battleList.add( battle );
+          System.out.format("Adding non retreat amphib battle in %s\n", battle.getTerritory().getName() );
         } else {
           amphibCount++;
           lastAmphib = battle;      // Otherwise store it to see if it can be fought later i.e. if there's only one
@@ -84,34 +79,45 @@ public class BattleDelegateOrdered extends BattleDelegate {
 
     // Fight amphibious assault if there is one remaining. Fight naval battles in random order if prerequisites first.
     if( amphibCount == 1 ) {
-      battleCount -= fightAmphib( lastAmphib );   // Subtract off the number of fought battles 
-    }
-    return battleCount;
-  }
-
-  private int fightAmphib( IBattle amphib ) {
-    int foughtBattleCount = 0;
-    // are there battles that must occur first
-    for( final IBattle seaBattle : m_battleTracker.getDependentOn( amphib ) ) {
-      if( seaBattle instanceof MustFightBattle && seaBattle != null ) {
-        seaBattle.fight( m_bridge );
-        foughtBattleCount++;
+      // are there battles that must occur first
+      for( final IBattle seaBattle : m_battleTracker.getDependentOn( lastAmphib ) ) {
+        System.out.format("Sea Battle in %s\n", seaBattle.getTerritory().getName() );
+        if( seaBattle instanceof MustFightBattle && seaBattle != null ) {
+          battleList.add( seaBattle );
+          System.out.format("Adding sea battle in %s\n", seaBattle.getTerritory().getName() );
+          battleCount--;
+        }
       }
+      battleList.add( lastAmphib );
+      battleCount--;
+      
+      //battleCount -= fightAmphib( lastAmphib );   // Subtract off the number of fought battles 
     }
-    amphib.fight( m_bridge );
-    foughtBattleCount++;
+
+    for( IBattle x : battleList) {
+      System.out.format("Battle in %s\n", x.getTerritory().getName() );
+
+    }
     
-    return foughtBattleCount;
-  }
+    battleList.forEach( battle -> battle.fight( m_bridge ) );
 
-  private void fightRemainingBattle() {
-    // This needs another loop just in case the last otherBattleCount was triggered by a sea combat already fought
-    for( final Territory t : m_battleTracker.getPendingBattleSites(false) ) {  // Will only find one
-      final IBattle lastBattle = m_battleTracker.getPendingBattle( t, false, BattleType.NORMAL );
-      if( lastBattle instanceof MustFightBattle ) {
-        lastBattle.fight( m_bridge );
+    if( battleCount == 1 ) {  // If there is only one remaining normal combat, fight it here rather than requiring the user to click it.
+      // This needs another loop just in case the last otherBattleCount was triggered by a sea combat already fought
+      for( final Territory t : m_battleTracker.getPendingNonSBRSites() ) {  // Will only find one
+        final IBattle lastBattle = m_battleTracker.getPendingBattle( t, false, BattleType.NORMAL );
+        if( lastBattle instanceof MustFightBattle ) {
+          //battleList.add( lastBattle );
+          System.out.format("Adding last battle in %s\n", lastBattle.getTerritory().getName() );
+          lastBattle.fight( m_bridge );
+        }
       }
     }
   }
-
+  /*
+   * private int fightAmphib( IBattle amphib ) {
+   * int foughtBattleCount = 0;
+   * 
+   * return foughtBattleCount;
+   * }
+   */
 }
--- test/games/strategy/triplea/delegate/BattleOrderTest.java
+++ test/games/strategy/triplea/delegate/BattleOrderTest.java
@@ -4,22 +4,30 @@ import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.File;
 
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertEquals;
 
+import games.strategy.engine.data.export.GameDataExporter;
+import games.strategy.debug.ClientLogger;
 import games.strategy.engine.data.GameData;
 import games.strategy.engine.data.ITestDelegateBridge;
 import games.strategy.engine.data.PlayerID;
 import games.strategy.engine.data.RouteScripted;
 import games.strategy.engine.data.Territory;
 import games.strategy.engine.data.Unit;
+import games.strategy.engine.data.Route;
 import games.strategy.engine.data.changefactory.ChangeFactory;
 import games.strategy.engine.random.ScriptedRandomSource;
 import games.strategy.test.TestUtil;
@@ -27,7 +35,8 @@ import games.strategy.triplea.TripleAUnit;
 import games.strategy.triplea.player.ITripleAPlayer;
 import games.strategy.triplea.xml.LoadGameUtil;
 
-// Test Global 1940 rule of having strat bombing first, then amphibious assaults, then others. Also removes some auto resolved combats.
+// Test Global 1940 rule of having strat bombing first, then amphibious assaults, then others. Also removes some auto
+// resolved combats.
 
 public class BattleOrderTest {
   private GameData m_data;
@@ -44,18 +53,117 @@ public class BattleOrderTest {
 
   @Test
   public void BattleTest() {
-	final BattleDelegate delegate = (BattleDelegate) m_data.getDelegateList().getDelegate("battle");
-	delegate.initialize("BattleDelegateOrdered", "BattleDelegateOrdered");
-	final Territory wgermany = m_data.getMap().getTerritory("Western Germany");
-    final Territory uk = m_data.getMap().getTerritory("United Kingdom");
+    // final InitializationDelegate initDel =
+    // (InitializationDelegate) m_data.getDelegateList().getDelegate("initDelegate");
+    final BattleDelegate battleDelegate = (BattleDelegate) m_data.getDelegateList().getDelegate("battle");
+    final MoveDelegate moveDelegate = (MoveDelegate) m_data.getDelegateList().getDelegate("move");
+    final Territory wGermany = m_data.getMap().getTerritory("Western Germany");
+    final Territory scotland = m_data.getMap().getTerritory("Scotland");
     final Territory iceland = m_data.getMap().getTerritory("Iceland");
-    final Territory sz122 = m_data.getMap().getTerritory("122 Sea Zone");
-    final PlayerID british = GameDataTestUtil.british(m_data);
+    final Territory sz123 = m_data.getMap().getTerritory("123 Sea Zone");
+    final Territory sz114 = m_data.getMap().getTerritory("114 Sea Zone");
+    final Territory sz115 = m_data.getMap().getTerritory("115 Sea Zone");
+    final Territory balticStates = m_data.getMap().getTerritory("Baltic States");
+    final Territory easternPoland = m_data.getMap().getTerritory("Eastern Poland");
     final PlayerID germans = GameDataTestUtil.germans(m_data);
     final ITestDelegateBridge bridge = getDelegateBridge(germans);
+    Collection<Unit> balticFleet = new ArrayList<>();
+    Collection<Unit> icelandicAssault = new ArrayList<>();
+    Collection<Unit> bsAssault = new ArrayList<>();
+    Collection<Unit> scotRaid = new ArrayList<>();
+    bridge.setStepName("germansCombatMove");
+    moveDelegate.initialize("MoveDelegate", "MoveDelegate");
+    moveDelegate.setDelegateBridgeAndPlayer(bridge);
+    moveDelegate.start();
+
+    // Find inf in SZ123
+    for (final Unit aUnit : sz123.getUnits().getUnits()) {
+      if (aUnit.getType().getName().equals("infantry")) {
+        icelandicAssault.add(aUnit);
+      }
+    }
+    moveDelegate.move(icelandicAssault, new Route(sz123, iceland));
+
+    // There is doubtless a better way of doing this, but this way works
+    for (final Unit aUnit : sz114.getUnits().getUnits()) {
+      balticFleet.add(aUnit);
+    }
+    moveDelegate.move(balticFleet, new Route(sz114, sz115));
+
+    // Find inf & art in SZ115
+    for (final Unit aUnit : sz115.getUnits().getUnits()) {
+      if (aUnit.getType().getName().equals("infantry") || aUnit.getType().getName().equals("artillery")) {
+        bsAssault.add(aUnit);
+      }
+    }
+    moveDelegate.move(bsAssault, new Route(sz115, balticStates));
+
+    /*
+     * Find Strat Bombers in W Germany
+     * for( final Unit aUnit : wGermany.getUnits().getUnits() ) {
+     * if( aUnit.getType().getName().equals( "bomber" ) ) {
+     * scotRaid.add( aUnit );
+     * }
+     * }
+     * moveDelegate.move( scotRaid, new Route( wGermany, scotland ) );
+     */
+
+    moveDelegate.end();
+
+    final GameDataExporter exporter1 = new games.strategy.engine.data.export.GameDataExporter(m_data, false);
+    try {
+      final FileWriter writer = new FileWriter("xml_b4.xml");
+      writer.write(exporter1.getXML());
+      writer.close();
+    } catch (final IOException e1) {
+      ClientLogger.logQuietly(e1);
+    }
+
     bridge.setStepName("germansBattle");
-    delegate.setDelegateBridgeAndPlayer(bridge);
-    delegate.start();
-    assertEquals( iceland.getOwner() , germans );
+    battleDelegate.initialize("BattleDelegateOrdered", "BattleDelegateOrdered");
+    // Germans hit, Soviets miss. Prevents casualty selection popup box
+    bridge.setRandomSource(new ScriptedRandomSource(new int[] {1, 3, 6, 1, 1, 1, 1, 1, 6, 6, 6, 1, 1, 1, 6, 6, 6}));
+    battleDelegate.setDelegateBridgeAndPlayer(bridge);
+
+    battleDelegate.start();
+/*    } catch (Exception e) {
+      final GameDataExporter exporter = new games.strategy.engine.data.export.GameDataExporter(m_data, false);
+      try {
+        final FileWriter writer = new FileWriter("xml1.xml");
+        writer.write(exporter.getXML());
+        writer.close();
+      } catch (final IOException e2) {
+        ClientLogger.logQuietly(e2);
+      }
+    }*/
+
+    assertEquals(germans, iceland.getOwner()); // Ensure that the amphibious assault on Iceland actually happened
+
+    /*
+     * Count bombers left in Scotland
+     * int bombers = 0;
+     * for( final Unit aUnit : scotland.getUnits().getUnits() ) {
+     * if( aUnit.getType().getName().equals( "bomber" ) ) {
+     * bombers++;
+     * }
+     * }
+     * 
+     * assertEquals( bombers, 1 ); // One shot by AAA, one bombs
+     */
+    assertEquals(germans, balticStates.getOwner()); // Ensure assault succeeded and occurred.
+    // Ownership won't change without amphibious units
+
+    int transports = 0;
+    for (final Unit aUnit : sz123.getUnits().getUnits()) {
+      if (aUnit.getType().getName().equals("transport")) {
+        transports++;
+      }
+    }
+
+    assertEquals(1, transports); // Only german TT should be alive
+    assertEquals(2, sz123.getUnits().getUnits().size()); // Even better, only two units should be present
+
+    // assertEquals( germans, easternPoland.getOwner() ); // Ensure last battle occurred.
+
   }
 }
