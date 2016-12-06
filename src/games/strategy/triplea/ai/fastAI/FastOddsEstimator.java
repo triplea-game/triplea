7b07a074e408177db2f209b596f0476671861958
Ron <ron.murhammer@gmail.com>
2016-12-05 20:51:24 -0600

    Update Fast AI remaining units calculation

--- src/games/strategy/triplea/ai/fastAI/FastOddsEstimator.java
+++ src/games/strategy/triplea/ai/fastAI/FastOddsEstimator.java
@@ -2,6 +2,7 @@ package games.strategy.triplea.ai.fastAI;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 import games.strategy.engine.data.GameData;
@@ -10,6 +11,7 @@ import games.strategy.engine.data.Territory;
 import games.strategy.engine.data.TerritoryEffect;
 import games.strategy.engine.data.Unit;
 import games.strategy.triplea.ai.proAI.util.ProBattleUtils;
+import games.strategy.triplea.ai.proAI.util.ProPurchaseUtils;
 import games.strategy.triplea.oddsCalculator.ta.AggregateResults;
 import games.strategy.triplea.oddsCalculator.ta.IOddsCalculator;
 import games.strategy.triplea.oddsCalculator.ta.OddsCalculatorListener;
@@ -37,12 +39,18 @@ public class FastOddsEstimator implements IOddsCalculator {
     final double winPercentage = ProBattleUtils.estimateStrengthDifference(location, new ArrayList<>(attackingUnits),
         new ArrayList<>(defendingUnits));
     final int battleRoundsFought = 3;
-    final List<Unit> remainingAttackingUnits = new ArrayList<>();
-    final List<Unit> remainingDefendingUnits = new ArrayList<>();
+    List<Unit> remainingAttackingUnits = new ArrayList<>();
+    List<Unit> remainingDefendingUnits = new ArrayList<>();
     if (winPercentage > 50) {
       remainingAttackingUnits.addAll(attackingUnits);
+      Collections.sort(remainingAttackingUnits, ProPurchaseUtils.getCostComparator().reversed());
+      final int numRemainingUnits = (int) Math.ceil(attackingUnits.size() * (Math.min(100, winPercentage) - 50) / 50);
+      remainingAttackingUnits = remainingAttackingUnits.subList(0, numRemainingUnits);
     } else {
       remainingDefendingUnits.addAll(defendingUnits);
+      Collections.sort(remainingDefendingUnits, ProPurchaseUtils.getCostComparator().reversed());
+      final int numRemainingUnits = (int) Math.ceil(defendingUnits.size() * (50 - Math.max(0, winPercentage)) / 50);
+      remainingDefendingUnits = remainingDefendingUnits.subList(0, numRemainingUnits);
     }
     return new AggregateEstimate(battleRoundsFought, winPercentage / 100, remainingAttackingUnits,
         remainingDefendingUnits);
