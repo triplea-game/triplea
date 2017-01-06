95ba3594ebf63a0f50af1121711e10f178b2be3f
Simon <simon58500@bigpond>
2017-01-06 19:19:30 +0800

    More feedback incorporated

--- src/games/strategy/triplea/delegate/BattleTracker.java
+++ src/games/strategy/triplea/delegate/BattleTracker.java
@@ -1085,8 +1085,8 @@ public class BattleTracker implements java.io.Serializable {
   }
 
   public void raids(final IDelegateBridge m_bridge) {
-    // Remove air raids - this adds all the bombing raids to the battle tracker
-    for( final Territory t : getPendingSBRSites() ) {
+    // Remove air raids - this finds  all the raids in the battle tracker
+    for( final Territory t : getPendingBattleSites(true) ) {
       final IBattle airRaid = getPendingBattle(t, true, BattleType.AIR_RAID);   // Get air raid for current territory (if any)
       if( airRaid != null ) {
         airRaid.fight(m_bridge);
@@ -1098,19 +1098,5 @@ public class BattleTracker implements java.io.Serializable {
   public String toString() {
     return "BattleTracker:" + "\n" + "Conquered:" + m_conquered + "\n" + "Blitzed:" + m_blitzed + "\n" + "Fought:"
         + m_foughBattles + "\n" + "Pending:" + m_pendingBattles;
-  }
-
-  /**
-   * @return a collection of territories where strategic bombing raid battles are pending
-   */
-  public Collection<Territory> getPendingSBRSites() {
-    return getPendingBattleSites(true);
-  }
-
-  /**
-   * @return a collection of territories where non strategic bombing raid battles are pending
-   */
-  public Collection<Territory> getPendingNonSBRSites() {
-    return getPendingBattleSites(false);
   }
 }
