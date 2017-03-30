1441bc55b5210d90c5e0ab23a72c399cde4414dc
RoiEX <RoiEXLab@users.noreply.github.com>
2017-03-31 00:36:22 +0200

    Refactor Route Drawer

---
+++ src/main/java/games/strategy/triplea/ui/logic/RouteCalculator.java
@@ -0,0 +1,175 @@
+package games.strategy.triplea.ui.logic;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class RouteCalculator {
+
+  public final boolean isInfiniteY;
+  public final boolean isInfiniteX;
+
+  private Point endPoint;
+  private final int mapWidth;
+  private final int mapHeight;
+
+  public RouteCalculator(boolean isInfiniteX, boolean isInfiniteY, int mapWidth, int mapHeight) {
+    this.isInfiniteX = isInfiniteX;
+    this.isInfiniteY = isInfiniteY;
+    this.mapWidth = mapWidth;
+    this.mapHeight = mapHeight;
+  }
+
+  /**
+   * Algorithm for finding the shortest path for the given Route
+   * 
+   * @param route The joints on the Map
+   * @return A Point array which goes through Map Borders if necessary
+   */
+  public Point[] getTranslatedRoute(Point... route) {
+    if (route == null || route.length == 0) {
+      // Or the array is too small
+      return route;
+    }
+    if (!isInfiniteX && !isInfiniteY) {
+      // If the Map is not infinite scrolling, we can safely return the given Points
+      endPoint = route[route.length - 1];
+      return route;
+    }
+    List<Point> result = new ArrayList<>();
+    Point previousPoint = null;
+    for (Point point : route) {
+      if (previousPoint == null) {
+        previousPoint = point;
+        result.add(point);
+        continue;
+      }
+      Point closestPoint = getClosestPoint(previousPoint, getPossiblePoints(point));
+      result.add(closestPoint);
+      previousPoint = closestPoint;
+    }
+    endPoint = result.get(result.size() - 1);
+    return result.toArray(new Point[result.size()]);
+  }
+
+  /**
+   * Returns the Closest Point out of the given Pool
+   * 
+   * @param source the reference Point
+   * @param pool Point List with all possible options
+   * @return the closest point in the Pool to the source
+   */
+  public static Point getClosestPoint(Point source, List<Point> pool) {
+    double closestDistance = Double.MAX_VALUE;
+    Point closestPoint = null;
+    for (Point possibleClosestPoint : pool) {
+      if (closestPoint == null) {
+        closestDistance = source.distance(possibleClosestPoint);
+        closestPoint = possibleClosestPoint;
+      } else {
+        double distance = source.distance(possibleClosestPoint);
+        if (closestDistance > distance) {
+          closestPoint = possibleClosestPoint;
+          closestDistance = distance;
+        }
+      }
+    }
+    return closestPoint;
+  }
+
+  /**
+   * Method for getting Points, which are a mapHeight/Width away from the actual Point
+   * Used to display routes with higher offsets than the map width/height
+   * 
+   * @param point The Point to "clone"
+   * @return A List of all possible Points depending in map Properties
+   *         size may vary
+   */
+  public List<Point> getPossiblePoints(Point point) {
+    List<Point> result = new ArrayList<>();
+    result.add(point);
+    if (isInfiniteX && isInfiniteY) {
+      result.addAll(Arrays.asList(
+          new Point(point.getX() - mapWidth, point.getY() - mapHeight),
+          new Point(point.getX() - mapWidth, point.getY() + mapHeight),
+          new Point(point.getX() + mapWidth, point.getY() - mapHeight),
+          new Point(point.getX() + mapWidth, point.getY() + mapHeight)));
+    }
+    if (isInfiniteX) {
+      result.addAll(Arrays.asList(
+          new Point(point.getX() - mapWidth, point.getY()),
+          new Point(point.getX() + mapWidth, point.getY())));
+
+    }
+    if (isInfiniteY) {
+      result.addAll(Arrays.asList(
+          new Point(point.getX(), point.getY() - mapHeight),
+          new Point(point.getX(), point.getY() + mapHeight)));
+
+    }
+    return result;
+  }
+
+  public Point getLastEndPoint() {
+    return endPoint;
+  }
+
+  /**
+   * Matrix Transpose method to transpose the 2dimensional point list
+   * 
+   * @param points A Point array
+   * @return Offset Point Arrays including points
+   */
+  public List<Point[]> getAllPoints(Point... points) {
+    List<Point[]> allPoints = new ArrayList<>();
+    for (int i = 0; i < points.length; i++) {
+      List<Point> subPoints = getPossiblePoints(points[i]);
+      for (int y = 0; y < subPoints.size(); y++) {
+        if (i == 0) {
+          allPoints.add(new Point[points.length]);
+        }
+        allPoints.get(y)[i] = (subPoints.get(y));
+      }
+    }
+    return allPoints;
+  }
+
+  /**
+   * Generates a List of Lines which represent "normalized forms" of the given arrays
+   * 
+   * @param xcoords an array of xCoordinates
+   * @param ycoords an array of yCoordinates
+   * @return a List of corresponding Lines
+   */
+  private List<Line> getNormalizedLines(double[] xcoords, double[] ycoords) {
+    List<Line> lines = new ArrayList<>();
+    Point previousPoint = null;
+    for (int i = 0; i < xcoords.length; i++) {
+      Point trimmedPoint = new Point(xcoords[i], ycoords[i]);
+      if (previousPoint != null) {
+        lines.add(new Line(previousPoint, trimmedPoint));
+      }
+      previousPoint = trimmedPoint;
+    }
+    return lines;
+  }
+
+  /**
+   * A List of Lines which represent all possible lines on multiple screens size may vary
+   * 
+   * @param xcoords an array of xCoordinates
+   * @param ycoords an array of yCoordinates
+   * @return a List of corresponding Lines on every possible screen
+   */
+  public List<Line> getAllNormalizedLines(double[] xcoords, double[] ycoords) {
+    List<Line> centerLines = getNormalizedLines(xcoords, ycoords);
+    List<Line> result = new ArrayList<>();
+    for (Line line : centerLines) {
+      List<Point[]> allPoints = getAllPoints(line.getP1(), line.getP2());
+      for (Point[] points : allPoints) {
+        result.add(new Line(points[0], points[1]));
+      }
+    }
+    return result;
+  }
+}
---
+++ src/test/java/games/strategy/triplea/ui/logic/RouteCalculatorTest.java
@@ -0,0 +1,117 @@
+package games.strategy.triplea.ui.logic;
+
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import games.strategy.triplea.ui.MapPanel;
+import games.strategy.triplea.ui.mapdata.MapData;
+
+@RunWith(MockitoJUnitRunner.class)
+public class RouteCalculatorTest {
+
+  @Mock
+  private MapPanel mapPanel;
+  @Mock
+  private MapData mapData;
+
+  @Before
+  public void setUp() {
+    when(mapPanel.getImageWidth()).thenReturn(1000);
+    when(mapPanel.getImageHeight()).thenReturn(1000);
+    when(mapData.scrollWrapX()).thenReturn(true, true, false, false);
+    when(mapData.scrollWrapY()).thenReturn(true, false, true, false);
+  }
+
+  @Test
+  public void testRouteTranslation() {
+    Point[] inputArray = new Point[] {p(1, 4), p(1001, 1001), p(600, 600)};
+    assertArrayEquals(new Point[] {p(1, 4), p(1, 1), p(-400, -400)},
+        new RouteCalculator(true, true, 1000, 1000).getTranslatedRoute(inputArray));
+    assertArrayEquals(new Point[] {p(1, 4), p(1, 1001), p(-400, 600)},
+        new RouteCalculator(true, false, 1000, 1000).getTranslatedRoute(inputArray));
+    assertArrayEquals(new Point[] {p(1, 4), p(1001, 1), p(600, -400)},
+        new RouteCalculator(false, true, 1000, 1000).getTranslatedRoute(inputArray));
+    assertArrayEquals(inputArray, new RouteCalculator(false, false, 1000, 1000).getTranslatedRoute(inputArray));
+  }
+
+  private static Point p(double x, double y) {
+    return new Point(x, y);
+  }
+
+  @Test
+  public void testClosestPoint() {
+    Point origin = new Point(0, 0);
+    Point closestPoint = new Point(1, 1);
+    List<Point> pool = new ArrayList<>();
+    for (int i = 0; i < 9; i++) {
+      pool.add(p((int) (Math.random() * 1000 + 1), (int) (Math.random() * 1000 + 1)));
+    }
+    pool.add(closestPoint);
+    assertEquals(closestPoint, RouteCalculator.getClosestPoint(origin, pool));
+  }
+
+  @Test
+  public void testPossiblePoints() {
+    List<Point> possiblePoints = new ArrayList<>();
+    // The values below must be all combinations of
+    // x and y values 0, -mapWidth/height, +mapWidth/Height
+    possiblePoints.add(p(-1000, -1000));
+    possiblePoints.add(p(-1000, 0));
+    possiblePoints.add(p(-1000, 1000));
+    possiblePoints.add(p(0, -1000));
+    possiblePoints.add(p(0, 0));
+    possiblePoints.add(p(0, 1000));
+    possiblePoints.add(p(1000, -1000));
+    possiblePoints.add(p(1000, 0));
+    possiblePoints.add(p(1000, 1000));
+    checkPoints(0, possiblePoints, true, true);
+    checkPoints(6, possiblePoints, true, false);
+    checkPoints(6, possiblePoints, false, true);
+    checkPoints(8, possiblePoints, false, false);
+  }
+
+  private void checkPoints(int offset, List<Point> expected, boolean isInfiniteX, boolean isInfiniteY) {
+    List<Point> calculatedPoints =
+        new RouteCalculator(isInfiniteX, isInfiniteY, 1000, 1000).getPossiblePoints(new Point());
+    assertEquals(expected.size(), calculatedPoints.size() + offset);
+    for (Point point : calculatedPoints) {
+      assertTrue(expected.contains(point));
+    }
+  }
+
+  @Test
+  public void testMatrixTransposal() {
+    Point[] input = new Point[] {p(0, 0), p(1, 1)};
+    Point[] nw = new Point[] {p(-1000, -1000), p(-999, -999)};
+    Point[] n = new Point[] {p(0, -1000), p(1, -999)};
+    Point[] ne = new Point[] {p(1000, -1000), p(1001, -999)};
+    Point[] w = new Point[] {p(-1000, 0), p(-999, 1)};
+    Point[] e = new Point[] {p(1000, 0), p(1001, 1)};
+    Point[] sw = new Point[] {p(-1000, 1000), p(-999, 1001)};
+    Point[] s = new Point[] {p(0, 1000), p(1, 1001)};
+    Point[] se = new Point[] {p(1000, 1000), p(1001, 1001)};
+
+    List<Point[]> points = new RouteCalculator(true, true, 1000, 1000).getAllPoints(input);
+    // This may be changed along with the RouteOptimizer#getPossiblePoints method
+    assertArrayEquals(input, points.get(0));
+    assertArrayEquals(nw, points.get(1));
+    assertArrayEquals(sw, points.get(2));
+    assertArrayEquals(ne, points.get(3));
+    assertArrayEquals(se, points.get(4));
+    assertArrayEquals(w, points.get(5));
+    assertArrayEquals(e, points.get(6));
+    assertArrayEquals(n, points.get(7));
+    assertArrayEquals(s, points.get(8));
+  }
+}
