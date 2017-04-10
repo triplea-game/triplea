8da098a75f6bd8fef52c36cb721407feae0a2c79
Simon <simon@bigpond.com>
2017-04-10 12:59:16 +1000

    Remove latest_version_new file dependency

--- src/main/java/games/strategy/engine/framework/EngineVersionProperties.java
+++ src/main/java/games/strategy/engine/framework/EngineVersionProperties.java
@@ -28,7 +28,7 @@ public class EngineVersionProperties {
   private final String linkAlt;
   private final String changelogLink;
   private static final String TRIPLEA_VERSION_LINK =
-      "https://raw.githubusercontent.com/triplea-game/triplea/master/latest_version_new.properties";
+      "https://raw.githubusercontent.com/triplea-game/triplea/master/latest_version.properties";
 
   private EngineVersionProperties() {
     this(getProperties());
