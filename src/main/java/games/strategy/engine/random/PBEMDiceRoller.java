20665f78e96093954936f798a4cae32a69ec1ceb
RoiEX <RoiEXLab@users.noreply.github.com>
2017-04-23 00:33:49 +0200

    Update links

--- src/main/java/games/strategy/engine/random/PBEMDiceRoller.java
+++ src/main/java/games/strategy/engine/random/PBEMDiceRoller.java
@@ -285,7 +285,7 @@ class HttpDiceRollerDialog extends JDialog {
         appendText("  2: Firewall could be blocking TripleA from connecting to the Dice Server\n");
         appendText("  3: The e-mail address does not exist\n");
         appendText("  4: An unknown error, please see the error console and consult the forums for help\n");
-        appendText("     Visit http://tripleadev.org  for extra help\n");
+        appendText("     Visit https://forums.triplea-game.org  for extra help\n");
         if (text != null) {
           appendText("Text from dice server:\n" + text + "\n");
         }
