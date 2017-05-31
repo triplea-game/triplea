890bd6a02c96cfbb0a9a191277ee06fa7aaedd5d
Steven Soloff <steven.soloff@gmail.com>
2017-05-30 21:48:54 -0400

    Nest all related test fixtures under a single outer fixture
    Using the Enclosed test runner, we can keep all independent, but
    related, test fixtures under a single fixture that provides a namespace
    for the tests.  This allows us to achieve the goal of having one test
    file per Java class.
    
    Unfortunately, using the Enclosed test runner causes each test to be
    executed twice under certain conditions.  This is due to the Enclosed
    test runner being essentially a JUnit suite.  The test discovery
    protocol of certain tools will identify the suite and the test classes
    that make up the suite independently.  Thus, those tests nested within
    an Enclosed test runner will be executed twice if not compensated for.
    
    The following sections describe the conditions under which Enclosed
    tests may be run twice:
    
    1. Eclipse IDE
    
    Running an Enclosed test directly via Run As > JUnit Test will cause
    Eclipse to prompt the user if they want to run the outer (Enclosed)
    fixture or one of the nested fixtures.  Choosing to run the outer
    fixture will run all nested tests as expected: that is, once.
    
    However, choosing to run JUnit tests for any workspace container that is
    an ancestor of the Enclosed test will cause the nested tests to be run
    twice.  For example, selecting the package containing an Enclosed test
    (or any parent package) and using Run As > JUnit Test will results in
    the nested tests being run twice.  It is not known if these duplicate
    test runs can be prevented in this scenario.
    
    2. Gradle
    
    Running the test task will result in all nested tests being run twice
    because the test discovery protocol picks up both the Enclosed suite as
    well as the individual tests.  This is more serious as the test counts
    in the CI environment can effectively be double of what they actually
    are leading to longer build times and false test metrics.
    
    The solution is to specifically exclude the test discovery protocol from
    running the Enclosed suites.  To do this, a unique method for
    identifying such tests had to be established.  The convention chosen in
    this change is to use the suffix "Tests" instead of the more traditional
    "Test" for Enclosed suites.  Gradle is then directed to exclude tests
    whose class names end in "Tests".  Thus, as lone as new Enclosed suites
    follow this naming convention, no future changes need be made to the
    Gradle configuration to prevent duplicate test runs.

---
+++ src/test/java/games/strategy/engine/framework/map/download/MapDownloadControllerTests.java
@@ -0,0 +1,184 @@
+package games.strategy.engine.framework.map.download;
+
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
+import static org.junit.Assert.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Optional;
+
+import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import games.strategy.engine.framework.map.download.MapDownloadController.DownloadedMaps;
+import games.strategy.engine.framework.map.download.MapDownloadController.TutorialMapPreferences;
+import games.strategy.engine.framework.map.download.MapDownloadController.UserMaps;
+import games.strategy.util.Version;
+
+@RunWith(Enclosed.class)
+public final class MapDownloadControllerTests {
+  @RunWith(MockitoJUnitRunner.class)
+  public static final class GetOutOfDateMapNames {
+    private static final String MAP_NAME = "myMap";
+
+    private static final File MAP_ZIP_FILE_1 = new File("file1.zip");
+
+    private static final File MAP_ZIP_FILE_2 = new File("file2.zip");
+
+    private static final Version VERSION_1 = new Version(1, 0);
+
+    private static final Version VERSION_2 = new Version(2, 0);
+
+    private static final Version VERSION_UNKNOWN = null;
+
+    @Mock
+    private DownloadedMaps downloadedMaps;
+
+    @Test
+    public void shouldIncludeMapWhenMapIsOutOfDate() {
+      final Collection<DownloadFileDescription> downloads = givenLatestMapVersionIs(VERSION_2);
+      givenDownloadedMapVersionIs(VERSION_1);
+
+      final Collection<String> outOfDateMapNames = getOutOfDateMapNames(downloads);
+
+      assertThat(outOfDateMapNames, contains(MAP_NAME));
+    }
+
+    private static Collection<DownloadFileDescription> givenLatestMapVersionIs(final Version version) {
+      return givenDownload(newDownloadWithVersion(version));
+    }
+
+    private static Collection<DownloadFileDescription> givenDownload(final DownloadFileDescription download) {
+      return Collections.singletonList(download);
+    }
+
+    private static DownloadFileDescription newDownloadWithVersion(final Version version) {
+      return new DownloadFileDescription(
+          "url",
+          "description",
+          MAP_NAME,
+          version,
+          DownloadFileDescription.DownloadType.MAP,
+          DownloadFileDescription.MapCategory.BEST);
+    }
+
+    private void givenDownloadedMapVersionIs(final Version version) {
+      when(downloadedMaps.getZipFileCandidates(MAP_NAME)).thenReturn(Arrays.asList(MAP_ZIP_FILE_1, MAP_ZIP_FILE_2));
+      when(downloadedMaps.getVersionForZipFile(MAP_ZIP_FILE_1)).thenReturn(Optional.empty());
+      when(downloadedMaps.getVersionForZipFile(MAP_ZIP_FILE_2)).thenReturn(Optional.of(version));
+    }
+
+    private Collection<String> getOutOfDateMapNames(final Collection<DownloadFileDescription> downloads) {
+      return MapDownloadController.getOutOfDateMapNames(downloads, downloadedMaps);
+    }
+
+    @Test
+    public void shouldExcludeMapWhenMapIsUpToDate() {
+      final Collection<DownloadFileDescription> downloads = givenLatestMapVersionIs(VERSION_1);
+      givenDownloadedMapVersionIs(VERSION_1);
+
+      final Collection<String> outOfDateMapNames = getOutOfDateMapNames(downloads);
+
+      assertThat(outOfDateMapNames, not(contains(MAP_NAME)));
+    }
+
+    @Test
+    public void shouldExcludeMapWhenDownloadedVersionIsUnknown() {
+      final Collection<DownloadFileDescription> downloads = givenLatestMapVersionIs(VERSION_1);
+      givenDownloadedMapVersionIsUnknown();
+
+      final Collection<String> outOfDateMapNames = getOutOfDateMapNames(downloads);
+
+      assertThat(outOfDateMapNames, not(contains(MAP_NAME)));
+    }
+
+    private void givenDownloadedMapVersionIsUnknown() {
+      when(downloadedMaps.getZipFileCandidates(MAP_NAME)).thenReturn(Arrays.asList(MAP_ZIP_FILE_1, MAP_ZIP_FILE_2));
+      when(downloadedMaps.getVersionForZipFile(any())).thenReturn(Optional.empty());
+    }
+
+    @Test
+    public void shouldExcludeMapWhenLatestVersionIsUnknown() {
+      final Collection<DownloadFileDescription> downloads = givenLatestMapVersionIs(VERSION_UNKNOWN);
+      givenDownloadedMapVersionIs(VERSION_1);
+
+      final Collection<String> outOfDateMapNames = getOutOfDateMapNames(downloads);
+
+      assertThat(outOfDateMapNames, not(contains(MAP_NAME)));
+    }
+
+    @Test
+    public void shouldExcludeMapWhenDownloadIsNull() {
+      final Collection<DownloadFileDescription> downloads = givenDownload(null);
+      givenDownloadedMapVersionIs(VERSION_1);
+
+      final Collection<String> outOfDateMapNames = getOutOfDateMapNames(downloads);
+
+      assertThat(outOfDateMapNames, empty());
+    }
+  }
+
+  @RunWith(MockitoJUnitRunner.class)
+  public static final class PreventPromptToDownloadTutorialMap {
+    @Mock
+    private TutorialMapPreferences tutorialMapPreferences;
+
+    @Test
+    public void shouldChangePreventPromptToDownloadPreference() {
+      preventPromptToDownloadTutorialMap();
+
+      verify(tutorialMapPreferences).preventPromptToDownload();
+    }
+
+    private void preventPromptToDownloadTutorialMap() {
+      MapDownloadController.preventPromptToDownloadTutorialMap(tutorialMapPreferences);
+    }
+  }
+
+  @RunWith(MockitoJUnitRunner.class)
+  public static final class ShouldPromptToDownloadTutorialMap {
+    @Mock
+    private TutorialMapPreferences tutorialMapPreferences;
+
+    @Mock
+    private UserMaps userMaps;
+
+    @Test
+    public void shouldReturnTrueWhenCanPromptToDownloadAndUserMapsIsEmpty() {
+      when(tutorialMapPreferences.canPromptToDownload()).thenReturn(true);
+      when(userMaps.isEmpty()).thenReturn(true);
+
+      assertThat(shouldPromptToDownloadTutorialMap(), is(true));
+    }
+
+    private boolean shouldPromptToDownloadTutorialMap() {
+      return MapDownloadController.shouldPromptToDownloadTutorialMap(tutorialMapPreferences, userMaps);
+    }
+
+    @Test
+    public void shouldReturnFalseWhenCanPromptToDownloadAndUserMapsIsNotEmpty() {
+      when(tutorialMapPreferences.canPromptToDownload()).thenReturn(true);
+      when(userMaps.isEmpty()).thenReturn(false);
+
+      assertThat(shouldPromptToDownloadTutorialMap(), is(false));
+    }
+
+    @Test
+    public void shouldReturnFalseWhenCannotPromptToDownload() {
+      when(tutorialMapPreferences.canPromptToDownload()).thenReturn(false);
+
+      assertThat(shouldPromptToDownloadTutorialMap(), is(false));
+    }
+  }
+}
