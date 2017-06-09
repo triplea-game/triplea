8ae62071cfd01415ace79187544e07b3b01699e7
Ron <ron.murhammer@gmail.com>
2017-06-09 17:11:28 -0500

    Add button to show/hide resource modifiers and add PU income bonus

--- src/main/java/games/strategy/engine/framework/startup/ui/LocalSetupPanel.java
+++ src/main/java/games/strategy/engine/framework/startup/ui/LocalSetupPanel.java
@@ -1,23 +1,15 @@
 package games.strategy.engine.framework.startup.ui;
 
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.Insets;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Observable;
 import java.util.Observer;
 
-import javax.swing.Action;
-import javax.swing.JButton;
-import javax.swing.JLabel;
 import javax.swing.SwingUtilities;
 
 import games.strategy.engine.data.GameData;
-import games.strategy.engine.data.PlayerID;
 import games.strategy.engine.framework.message.PlayerListing;
 import games.strategy.engine.framework.startup.launcher.ILauncher;
 import games.strategy.engine.framework.startup.launcher.LocalLauncher;
@@ -25,7 +17,6 @@ import games.strategy.engine.framework.startup.mc.GameSelectorModel;
 import games.strategy.engine.pbem.PBEMMessagePoster;
 import games.strategy.engine.random.IRandomSource;
 import games.strategy.engine.random.PlainRandomSource;
-import games.strategy.ui.SwingAction;
 
 /** Setup panel when hosting a local game. */
 public class LocalSetupPanel extends SetupPanel implements Observer {
@@ -36,85 +27,24 @@ public class LocalSetupPanel extends SetupPanel implements Observer {
   public LocalSetupPanel(final GameSelectorModel model) {
     m_gameSelectorModel = model;
     createComponents();
-    layoutComponents();
+    layoutPlayerComponents(this, m_playerTypes, m_gameSelectorModel.getGameData());
     setupListeners();
     setWidgetActivation();
   }
 
   private void createComponents() {}
 
-  private void layoutComponents() {
-    final GameData data = m_gameSelectorModel.getGameData();
-    removeAll();
-    m_playerTypes.clear();
-    setLayout(new GridBagLayout());
-    if (data == null) {
-      add(new JLabel("No game selected!"));
-      return;
-    }
-    final Collection<String> disableable = data.getPlayerList().getPlayersThatMayBeDisabled();
-    final HashMap<String, Boolean> playersEnablementListing = data.getPlayerList().getPlayersEnabledListing();
-    final Map<String, String> reloadSelections = PlayerID.currentPlayers(data);
-    final String[] playerTypes = data.getGameLoader().getServerPlayerTypes();
-    final List<PlayerID> players = data.getPlayerList().getPlayers();
-    // if the xml was created correctly, this list will be in turn order. we want to keep it that way.
-    int gridx = 0;
-    int gridy = 0;       
-    if (!disableable.isEmpty() || playersEnablementListing.containsValue(Boolean.FALSE)) {
-      final JLabel enableLabel = new JLabel("Use");
-      this.add(enableLabel, new GridBagConstraints(gridx++, gridy, 1, 1, 0, 0, GridBagConstraints.WEST,
-          GridBagConstraints.NONE, new Insets(0, 5, 5, 0), 0, 0));
-    }
-    final JLabel nameLabel = new JLabel("Name");
-    this.add(nameLabel, new GridBagConstraints(gridx++, gridy, 1, 1, 0, 0, GridBagConstraints.WEST,
-        GridBagConstraints.NONE, new Insets(0, 5, 5, 0), 0, 0));
-    final JLabel typeLabel = new JLabel("Type");
-    this.add(typeLabel, new GridBagConstraints(gridx++, gridy, 1, 1, 0, 0, GridBagConstraints.WEST,
-        GridBagConstraints.NONE, new Insets(0, 5, 5, 0), 0, 0));
-    final JLabel allianceLabel = new JLabel("Alliance");
-    this.add(allianceLabel, new GridBagConstraints(gridx++, gridy, 1, 1, 0, 0, GridBagConstraints.WEST,
-        GridBagConstraints.NONE, new Insets(0, 7, 5, 5), 0, 0));
-    final JLabel bonusLabel = new JLabel("Income");
-    this.add(bonusLabel, new GridBagConstraints(gridx++, gridy, 1, 1, 0, 0, GridBagConstraints.WEST,
-        GridBagConstraints.NONE, new Insets(0, 20, 5, 0), 0, 0));
-    bonusLabel.setVisible(false);
-    JButton resourceModifiers = new JButton();   
-    this.add(resourceModifiers, new GridBagConstraints(gridx++, gridy, 1, 1, 0, 0, GridBagConstraints.EAST,
-            GridBagConstraints.NONE, new Insets(5, 5, 5, 0), 0, 0));
-    for (final PlayerID player : players) {
-      final PlayerSelectorRow selector =
-          new PlayerSelectorRow(player, reloadSelections, disableable, playersEnablementListing,
-              data.getAllianceTracker().getAlliancesPlayerIsIn(player), playerTypes, this, data.getProperties());
-      m_playerTypes.add(selector);
-      if (!player.isHidden()) {
-        selector.layout(++gridy, this);
-        selector.setResourceModifiersVisble(false);
-      }      
-    }
-    
-    Action resourceModifiersAction = SwingAction.of("Resource Modifiers", e -> {
-    	boolean isVisible = bonusLabel.isVisible();
-    	bonusLabel.setVisible(!isVisible);
-        m_playerTypes.forEach(row -> row.setResourceModifiersVisble(!isVisible));
-    });
-    resourceModifiers.setAction(resourceModifiersAction);
-    
-    validate();
-    invalidate();
-    setWidgetActivation();
-  }
-
   private void setupListeners() {
     m_gameSelectorModel.addObserver(this);
   }
 
   @Override
+  public void setWidgetActivation() {}
+
+  @Override
   public boolean isMetaSetupPanelInstance() {
     return false;
   }
-
-  @Override
-  public void setWidgetActivation() {}
 
   @Override
   public boolean canGameStart() {
@@ -149,10 +79,10 @@ public class LocalSetupPanel extends SetupPanel implements Observer {
   @Override
   public void update(final Observable o, final Object arg) {
     if (!SwingUtilities.isEventDispatchThread()) {
-      SwingUtilities.invokeLater(() -> layoutComponents());
+      SwingUtilities.invokeLater(() -> layoutPlayerComponents(this, m_playerTypes, m_gameSelectorModel.getGameData()));
       return;
     }
-    layoutComponents();
+    layoutPlayerComponents(this, m_playerTypes, m_gameSelectorModel.getGameData());
   }
 
   @Override
@@ -172,4 +102,5 @@ public class LocalSetupPanel extends SetupPanel implements Observer {
     final LocalLauncher launcher = new LocalLauncher(m_gameSelectorModel, randomSource, pl);
     return launcher;
   }
+
 }
