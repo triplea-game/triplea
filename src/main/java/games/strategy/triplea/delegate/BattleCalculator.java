6c0dbccc0014685d7a7a40f5c3fee56750f926cc
Dan Van Atta <dhvatta@gmail.com>
2017-06-15 11:38:06 -0700

    Use ternary operator in a series of if statements to simplify

--- src/main/java/games/strategy/triplea/delegate/BattleCalculator.java
+++ src/main/java/games/strategy/triplea/delegate/BattleCalculator.java
@@ -1339,38 +1339,27 @@ public class BattleCalculator {
       final Collection<TerritoryEffect> territoryEffects) {
     final boolean lhtrBombers = games.strategy.triplea.Properties.getLHTR_Heavy_Bombers(data);
     final UnitAttachment ua = UnitAttachment.get(current.getType());
-    final int rolls;
-    if (defending) {
-      rolls = ua.getDefenseRolls(current.getOwner());
-    } else {
-      rolls = ua.getAttackRolls(current.getOwner());
-    }
+    final int rolls = (defending) ?  ua.getDefenseRolls(current.getOwner()) :
+        ua.getAttackRolls(current.getOwner());
     int strengthWithoutSupport = 0;
     // Find the strength the unit has without support
     // lhtr heavy bombers take best of n dice for both attack and defense
     if (rolls > 1 && (lhtrBombers || ua.getChooseBestRoll())) {
-      if (defending) {
-        strengthWithoutSupport = ua.getDefense(current.getOwner());
-      } else {
-        strengthWithoutSupport = ua.getAttack(current.getOwner());
-      }
+      strengthWithoutSupport = (defending) ? ua.getDefense(current.getOwner()) :
+        ua.getAttack(current.getOwner());
       strengthWithoutSupport +=
           TerritoryEffectHelper.getTerritoryCombatBonus(current.getType(), territoryEffects, defending);
       // just add one like LL if we are LHTR bombers
       strengthWithoutSupport = Math.min(Math.max(strengthWithoutSupport + 1, 0), data.getDiceSides());
     } else {
       for (int i = 0; i < rolls; i++) {
-        final int tempStrength;
-        if (defending) {
-          tempStrength = ua.getDefense(current.getOwner());
-        } else {
-          tempStrength = ua.getAttack(current.getOwner());
-        }
+        final int tempStrength = (defending) ? ua.getDefense(current.getOwner()) :
+            ua.getAttack(current.getOwner());
         strengthWithoutSupport +=
             TerritoryEffectHelper.getTerritoryCombatBonus(current.getType(), territoryEffects, defending);
         strengthWithoutSupport += Math.min(Math.max(tempStrength, 0), data.getDiceSides());
       }
     }
     return strengthWithoutSupport;
-  }
+}
 }
