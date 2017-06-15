704a0c6374cc9305c182c3d2edd4b0863efce8e9
Dan Van Atta <dhvatta@gmail.com>
2017-06-15 01:50:26 -0700

    Add postgres user and password properties to config

---
+++ src/test/java/games/strategy/engine/config/LobbyPropertyReaderTest.java
@@ -0,0 +1,59 @@
+package games.strategy.engine.config;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class LobbyPropertyReaderTest {
+  private LobbyPropertyReader testObj;
+
+  /**
+   * Sets up an example lobby property file with some fake values, then sets up a property reader test object
+   * pointed at this file.
+   */
+  @Before
+  public void setup() throws IOException {
+    final File testFile = File.createTempFile("testing", "tmp");
+    testFile.deleteOnExit();
+    try (FileWriter writer = new FileWriter(testFile)) {
+      writer.write(keyValuePair(LobbyPropertyReader.PropertyKeys.port, String.valueOf(TestData.fakePort)));
+      writer.write(keyValuePair(LobbyPropertyReader.PropertyKeys.postgresUser, TestData.fakeUser));
+      writer.write(keyValuePair(LobbyPropertyReader.PropertyKeys.postgresPassword, TestData.fakePassword));
+      writer.close();
+    }
+
+    testObj = new LobbyPropertyReader(testFile);
+  }
+
+  private static String keyValuePair(final String key, final String value) {
+    return key + "=" + value + "\n";
+  }
+
+  @Test
+  public void getPort() throws Exception {
+    assertThat(testObj.getPort(), is(TestData.fakePort));
+
+  }
+
+  @Test
+  public void postgresUser() throws Exception {
+    assertThat(testObj.getPostgresUser(), is(TestData.fakeUser));
+  }
+
+  @Test
+  public void postgresPassword() throws Exception {
+    assertThat(testObj.getPostgresPassword(), is(TestData.fakePassword));
+  }
+
+  private interface TestData {
+    int fakePort = 100;
+    String fakeUser = "funnyName";
+    String fakePassword = "funnyPasssword";
+  }
+}
