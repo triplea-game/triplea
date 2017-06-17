369024436a38888a3bd7ff0db628c89398313611
RoiEX <RoiEXLab@users.noreply.github.com>
2017-06-17 21:49:00 +0200

    Move resources

--- src/main/java/org/triplea/client/ui/javafx/FxmlManager.java
+++ src/main/java/org/triplea/client/ui/javafx/FxmlManager.java
@@ -11,8 +11,8 @@ public enum FxmlManager {
   DOWNLOAD_PANE("./fxml/TripleADownload.fxml"),
   MAIN_MANU_PANE("./fxml/TripleAMainMenu.fxml"),
   SETTINGS_PANE("./fxml/TripleASettings.fxml"),
-  LANG_CLASS_BASENAME("org.triplea.ui.lang.TripleA"),
-  STYLESHEET_MAIN("/org/triplea/ui/css/main.css"),
+  LANG_CLASS_BASENAME("org.triplea.client.ui.javafx.lang.TripleA"),
+  STYLESHEET_MAIN("/org/triplea/client/ui/javafx/css/main.css"),
   ICON_LOCATION("/games/strategy/engine/framework/ta_icon.png");
 
   private String value;
