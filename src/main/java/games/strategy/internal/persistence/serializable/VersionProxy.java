49098b2774ef303d58a59d4bd6e925602bc713c2
Steven Soloff <steven.soloff@gmail.com>
2017-06-27 23:33:20 -0400

    Remove support for serialization proxy versioning

--- src/main/java/games/strategy/internal/persistence/serializable/VersionProxy.java
+++ src/main/java/games/strategy/internal/persistence/serializable/VersionProxy.java
@@ -2,12 +2,8 @@ package games.strategy.internal.persistence.serializable;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
-import java.io.Externalizable;
-import java.io.IOException;
-import java.io.ObjectInput;
-import java.io.ObjectOutput;
+import java.io.Serializable;
 
-import games.strategy.persistence.serializable.VersionedProxySupport;
 import games.strategy.util.Version;
 
 /**
@@ -17,37 +13,28 @@ import games.strategy.util.Version;
  * Instances of this class are not thread safe.
  * </p>
  */
-public final class VersionProxy implements Externalizable {
+public final class VersionProxy implements Serializable {
   private static final long serialVersionUID = 6092507250760560736L;
-
-  private static final long CURRENT_VERSION = 1L;
-
-  private final VersionedProxySupport versionedProxySupport = new VersionedProxySupport(this);
 
   /**
    * @serial The major (first) component of the version.
    */
-  private int major;
+  private final int major;
 
   /**
    * @serial The minor (second) component of the version.
    */
-  private int minor;
+  private final int minor;
 
   /**
    * @serial The point (third) component of the version.
    */
-  private int point;
+  private final int point;
 
   /**
    * @serial The micro (fourth) component of the version.
    */
-  private int micro;
-
-  /**
-   * Initializes a new instance of the {@code VersionProxy} class during deserialization.
-   */
-  public VersionProxy() {}
+  private final int micro;
 
   /**
    * Initializes a new instance of the {@code VersionProxy} class from the specified {@code Version} instance.
@@ -65,31 +52,5 @@ public final class VersionProxy implements Externalizable {
 
   private Object readResolve() {
     return new Version(major, minor, point, micro);
-  }
-
-  @Override
-  public void readExternal(final ObjectInput in) throws IOException, ClassNotFoundException {
-    versionedProxySupport.read(in);
-  }
-
-  @SuppressWarnings("unused")
-  private void readExternalV1(final ObjectInput in) throws IOException {
-    major = in.readInt();
-    minor = in.readInt();
-    point = in.readInt();
-    micro = in.readInt();
-  }
-
-  @Override
-  public void writeExternal(final ObjectOutput out) throws IOException {
-    versionedProxySupport.write(out, CURRENT_VERSION);
-  }
-
-  @SuppressWarnings("unused")
-  private void writeExternalV1(final ObjectOutput out) throws IOException {
-    out.writeInt(major);
-    out.writeInt(minor);
-    out.writeInt(point);
-    out.writeInt(micro);
   }
 }
