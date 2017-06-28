aea7dd256e259145ff3d751705cba884e45f44ab
Steven Soloff <steven.soloff@gmail.com>
2017-06-27 23:33:20 -0400

    Remove support for serialization proxy versioning

--- src/main/java/games/strategy/internal/persistence/serializable/PropertyBagMementoProxy.java
+++ src/main/java/games/strategy/internal/persistence/serializable/PropertyBagMementoProxy.java
@@ -2,42 +2,29 @@ package games.strategy.internal.persistence.serializable;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
-import java.io.Externalizable;
-import java.io.IOException;
-import java.io.ObjectInput;
-import java.io.ObjectOutput;
+import java.io.Serializable;
 import java.util.Map;
 
-import games.strategy.persistence.serializable.VersionedProxySupport;
 import games.strategy.util.memento.PropertyBagMemento;
 
-public final class PropertyBagMementoProxy implements Externalizable {
+public final class PropertyBagMementoProxy implements Serializable {
   private static final long serialVersionUID = 7813364982800353383L;
-
-  private static final long CURRENT_VERSION = 1L;
-
-  private final VersionedProxySupport versionedProxySupport = new VersionedProxySupport(this);
 
   /**
    * @serial The collection of originator properties; never {@code null}. The key is the property name. The value is the
    *         property value.
    */
-  private Map<String, Object> propertiesByName;
+  private final Map<String, Object> propertiesByName;
 
   /**
    * @serial The memento schema identifier; never {@code null}.
    */
-  private String schemaId;
+  private final String schemaId;
 
   /**
    * @serial The memento schema version.
    */
-  private long schemaVersion;
-
-  /**
-   * Initializes a new instance of the {@code PropertyBagMementoProxy} class during deserialization.
-   */
-  public PropertyBagMementoProxy() {}
+  private final long schemaVersion;
 
   /**
    * Initializes a new instance of the {@code PropertyBagMementoProxy} class from the specified
@@ -55,31 +42,5 @@ public final class PropertyBagMementoProxy implements Externalizable {
 
   private Object readResolve() {
     return new PropertyBagMemento(schemaId, schemaVersion, propertiesByName);
-  }
-
-  @SuppressWarnings("unused")
-  private void readExternalV1(final ObjectInput in) throws IOException, ClassNotFoundException {
-    schemaId = in.readUTF();
-    schemaVersion = in.readLong();
-    @SuppressWarnings("unchecked")
-    final Map<String, Object> propertiesByName = (Map<String, Object>) in.readObject();
-    this.propertiesByName = propertiesByName;
-  }
-
-  @Override
-  public void readExternal(final ObjectInput in) throws IOException, ClassNotFoundException {
-    versionedProxySupport.read(in);
-  }
-
-  @Override
-  public void writeExternal(final ObjectOutput out) throws IOException {
-    versionedProxySupport.write(out, CURRENT_VERSION);
-  }
-
-  @SuppressWarnings("unused")
-  private void writeExternalV1(final ObjectOutput out) throws IOException {
-    out.writeUTF(schemaId);
-    out.writeLong(schemaVersion);
-    out.writeObject(propertiesByName);
   }
 }
