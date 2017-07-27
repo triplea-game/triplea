849028e76ace4c9bf65db4b9b3a9ab4754d8355b
Dan Van Atta <dhvatta@gmail.com>
2017-07-26 17:31:25 -0700

    Add some GridBagHelper javadocs

--- src/main/java/games/strategy/ui/SwingComponents.java
+++ src/main/java/games/strategy/ui/SwingComponents.java
@@ -7,6 +7,7 @@ import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Frame;
 import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.Window;
 import java.awt.event.ActionListener;
@@ -454,6 +455,19 @@ public class SwingComponents {
     return promise;
   }
 
+  /**
+   * Helper class for gridbag layouts with a fixed number of columns.
+   * Example usage:
+   * <code><pre>
+   * JPanel panelToHaveGridBag = new JPanel();
+   * int columnCount = 2;
+   * GridBagHelper helper = new GridBagHelper(panelToHaveGridBag, 2);
+   * // adding 10 elements would create a 2x5 grid
+   * for(int i = 0; i < 10; i ++ ) {
+   *   helper.addComponents(childComponent);
+   * }
+   * </pre></code>
+   */
   public static class GridBagHelper {
     private final JComponent parent;
     private final int columns;
@@ -463,13 +477,19 @@ public class SwingComponents {
 
     public GridBagHelper(final JComponent parent, final int columns) {
       this.parent = parent;
+      this.parent.setLayout(new GridBagLayout());
       this.columns = columns;
       constraints = new GridBagConstraints();
     }
 
+    /**
+     * Adds components to the parent component used when constructing the {@code GridBagHelper}.
+     * Components are added as rows in a grid bag layout and wrap to the next column when appropriate.
+     * You can call this method multiple times and keep appending components.
+     */
     public void addComponents(final JComponent ... children) {
       Preconditions.checkArgument(children.length > 0);
-      for(final JComponent child : children) {
+      for (final JComponent child : children) {
 
         final int x = elementCount % columns;
         final int y = elementCount / columns;
