2e7d7e2e6882d046bad92b86f9d20c51123b7ca7
Dan Van Atta <dhvatta@gmail.com>
2017-07-28 17:21:16 -0700

    Add missing test for JTextAreaBuilder and fill in missing implementation for JTextAreaBuilder

--- src/main/java/swinglib/JTextAreaBuilder.java
+++ src/main/java/swinglib/JTextAreaBuilder.java
@@ -1,8 +1,12 @@
 package swinglib;
 
+import java.awt.Color;
+import java.awt.Dimension;
+
+import javax.swing.BorderFactory;
 import javax.swing.JTextArea;
 
-import com.google.common.base.Preconditions;
+import com.google.common.base.Strings;
 
 /**
  * Example usage:
@@ -23,6 +27,9 @@ public final class JTextAreaBuilder {
   private String text;
   private int rows;
   private int columns;
+  private int borderWidth;
+  private boolean readOnly = false;
+  private Dimension maxSize;
 
   private JTextAreaBuilder() {
 
@@ -38,38 +45,47 @@ public final class JTextAreaBuilder {
    * By default the JTextArea will have line wrapping turned on.
    */
   public JTextArea build() {
-    Preconditions.checkNotNull(text);
-    Preconditions.checkState(rows > 0);
-    Preconditions.checkState(columns > 0);
-
-    final JTextArea textArea = new JTextArea();
+    final JTextArea textArea = new JTextArea(Strings.nullToEmpty(text), rows, columns);
     textArea.setWrapStyleWord(true);
     textArea.setLineWrap(true);
+
+    if (borderWidth > 0) {
+      textArea.setBorder(BorderFactory.createLineBorder(Color.black, borderWidth));
+    }
+
+    if (readOnly) {
+      textArea.setEditable(false);
+    }
+
+    if (maxSize != null) {
+      textArea.setMaximumSize(maxSize);
+    }
     return textArea;
   }
 
-
-
   public JTextAreaBuilder readOnly() {
+    readOnly = true;
     return this;
   }
 
+  /* TODO: test me */
   public JTextAreaBuilder borderWidth(final int width) {
-
+    this.borderWidth = width;
     return this;
   }
 
   public JTextAreaBuilder maximumSize(final int width, final int height) {
-
+    maxSize = new Dimension(width, height);
     return this;
   }
 
   public JTextAreaBuilder rows(final int value) {
-
+    this.rows = value;
     return this;
   }
 
   public JTextAreaBuilder columns(final int value) {
+    this.columns = value;
     return this;
   }
 
---
+++ src/test/java/swinglib/JTextAreaBuilderTest.java
@@ -0,0 +1,87 @@
+package swinglib;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import javax.swing.JTextArea;
+
+import org.hamcrest.core.Is;
+
+import org.junit.Test;
+
+public class JTextAreaBuilderTest {
+
+  @Test
+  public void defaultValues() {
+    final JTextArea area = JTextAreaBuilder.builder()
+        .build();
+
+    assertThat(area.getLineWrap(), Is.is(true));
+    assertThat(area.getWrapStyleWord(), Is.is(true));
+    assertThat(area.isEditable(), Is.is(true));
+  }
+
+
+  @Test
+  public void text() {
+    final JTextArea area = JTextAreaBuilder.builder()
+        .text("value")
+        .build();
+
+    assertThat(area.getText(), Is.is("value"));
+  }
+
+  @Test
+  public void rows() {
+    final JTextArea area = JTextAreaBuilder.builder()
+        .rows(5)
+        .build();
+
+    assertThat(area.getRows(), Is.is(5));
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void rowsNonZero() {
+    JTextAreaBuilder.builder()
+        .rows(0)
+        .build();
+  }
+
+
+  @Test
+  public void columns() {
+    final JTextArea area = JTextAreaBuilder.builder()
+        .columns(20)
+        .build();
+
+    assertThat(area.getColumns(), Is.is(20));
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void columnsNonZero() {
+    JTextAreaBuilder.builder()
+        .columns(0)
+        .build();
+  }
+
+  @Test
+  public void maximumSize() {
+    final int maxWidth = 1000;
+    final int maxHeight = 33;
+
+    final JTextArea area = JTextAreaBuilder.builder()
+        .maximumSize(maxWidth, maxHeight)
+        .build();
+    assertThat(area.getMaximumSize().width, Is.is(maxWidth));
+    assertThat(area.getMaximumSize().height, Is.is(maxHeight));
+  }
+
+  @Test
+  public void readOnly() {
+    final JTextArea area = JTextAreaBuilder.builder()
+        .readOnly()
+        .build();
+    assertThat(area.isEditable(), Is.is(false));
+  }
+
+
+}
