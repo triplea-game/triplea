9842e2dfd707bb5ccb09966390b361db2c355ec9
Steven Soloff <steven.soloff@gmail.com>
2017-08-08 20:59:01 -0400

    Add serializable proxy for GameData

---
+++ src/test/java/games/strategy/internal/persistence/serializable/GameDataProxyAsProxyTest.java
@@ -0,0 +1,42 @@
+package games.strategy.internal.persistence.serializable;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+import static games.strategy.engine.data.Matchers.equalToGameData;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import games.strategy.engine.data.GameData;
+import games.strategy.engine.data.TestGameDataFactory;
+import games.strategy.persistence.serializable.AbstractProxyTestCase;
+import games.strategy.persistence.serializable.ProxyFactory;
+
+public final class GameDataProxyAsProxyTest extends AbstractProxyTestCase<GameData> {
+  public GameDataProxyAsProxyTest() {
+    super(GameData.class);
+  }
+
+  @Override
+  protected void assertPrincipalEquals(final GameData expected, final GameData actual) {
+    checkNotNull(expected);
+    checkNotNull(actual);
+
+    assertThat(actual, is(equalToGameData(expected)));
+  }
+
+  @Override
+  protected Collection<GameData> createPrincipals() {
+    return Arrays.asList(TestGameDataFactory.newValidGameData());
+  }
+
+  @Override
+  protected Collection<ProxyFactory> getProxyFactories() {
+    return Arrays.asList(
+        GameDataProxy.FACTORY,
+        PropertyBagMementoProxy.FACTORY,
+        TripleAProxy.FACTORY,
+        VersionProxy.FACTORY);
+  }
+}
--- src/test/java/games/strategy/persistence/serializable/ProxyableObjectInputOutputStreamIntegrationTest.java
+++ src/test/java/games/strategy/persistence/serializable/ProxyableObjectInputOutputStreamIntegrationTest.java
@@ -5,6 +5,7 @@ import static com.googlecode.catchexception.CatchException.caughtException;
 import static org.hamcrest.Matchers.instanceOf;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.nullValue;
+import static org.hamcrest.Matchers.sameInstance;
 import static org.junit.Assert.assertThat;
 
 import java.io.ByteArrayInputStream;
@@ -25,15 +26,29 @@ public final class ProxyableObjectInputOutputStreamIntegrationTest {
   private final ByteArrayOutputStream baos = new ByteArrayOutputStream();
 
   private Object readObject() throws Exception {
-    try (final InputStream is = new ByteArrayInputStream(baos.toByteArray());
-        final ObjectInputStream ois = new ObjectInputStream(is)) {
-      return ois.readObject();
-    }
+    return readObjects(1)[0];
   }
 
-  private void writeObject(final Object obj, final ProxyRegistry proxyRegistry) throws Exception {
+  private Object[] readObjects(final int count) throws Exception {
+    final Object[] objs = new Object[count];
+    try (final InputStream is = new ByteArrayInputStream(baos.toByteArray());
+        final ObjectInputStream ois = new ObjectInputStream(is)) {
+      for (int i = 0; i < count; ++i) {
+        objs[i] = ois.readObject();
+      }
+    }
+    return objs;
+  }
+
+  private void writeObject(final ProxyRegistry proxyRegistry, final Object obj) throws Exception {
+    writeObjects(proxyRegistry, obj);
+  }
+
+  private void writeObjects(final ProxyRegistry proxyRegistry, final Object... objs) throws Exception {
     try (final ObjectOutputStream oos = new ProxyableObjectOutputStream(baos, proxyRegistry)) {
-      oos.writeObject(obj);
+      for (final Object obj : objs) {
+        oos.writeObject(obj);
+      }
     }
   }
 
@@ -41,7 +56,7 @@ public final class ProxyableObjectInputOutputStreamIntegrationTest {
   public void shouldBeAbleToRoundTripNull() throws Exception {
     final ProxyRegistry proxyRegistry = ProxyRegistry.newInstance();
 
-    writeObject(null, proxyRegistry);
+    writeObject(proxyRegistry, null);
     final Object deserializedObj = readObject();
 
     assertThat(deserializedObj, is(nullValue()));
@@ -52,10 +67,24 @@ public final class ProxyableObjectInputOutputStreamIntegrationTest {
     final ProxyRegistry proxyRegistry = ProxyRegistry.newInstance();
     final Date obj = new Date();
 
-    writeObject(obj, proxyRegistry);
+    writeObject(proxyRegistry, obj);
     final Date deserializedObj = (Date) readObject();
 
     assertThat(deserializedObj, is(obj));
+  }
+
+  @Test
+  public void shouldPreserveReferencesToPreviouslySerializedSerializableObject() throws Exception {
+    final ProxyRegistry proxyRegistry = ProxyRegistry.newInstance();
+    final Date obj = new Date();
+
+    writeObjects(proxyRegistry, obj, obj);
+    final Object[] deserializedObjs = readObjects(2);
+    final Date deserializedObj1 = (Date) deserializedObjs[0];
+    final Date deserializedObj2 = (Date) deserializedObjs[1];
+
+    assertThat(deserializedObj1, is(obj));
+    assertThat(deserializedObj2, is(sameInstance(deserializedObj1)));
   }
 
   @Test
@@ -63,17 +92,31 @@ public final class ProxyableObjectInputOutputStreamIntegrationTest {
     final ProxyRegistry proxyRegistry = ProxyRegistry.newInstance(FakeNonSerializableClassProxy.FACTORY);
     final FakeNonSerializableClass obj = new FakeNonSerializableClass(2112, "42");
 
-    writeObject(obj, proxyRegistry);
+    writeObject(proxyRegistry, obj);
     final FakeNonSerializableClass deserializedObj = (FakeNonSerializableClass) readObject();
 
     assertThat(deserializedObj, is(obj));
   }
 
   @Test
+  public void shouldPreserveReferencesToPreviouslySerializedNonSerializableObject() throws Exception {
+    final ProxyRegistry proxyRegistry = ProxyRegistry.newInstance(FakeNonSerializableClassProxy.FACTORY);
+    final FakeNonSerializableClass obj = new FakeNonSerializableClass(2112, "42");
+
+    writeObjects(proxyRegistry, obj, obj);
+    final Object[] deserializedObjs = readObjects(2);
+    final FakeNonSerializableClass deserializedObj1 = (FakeNonSerializableClass) deserializedObjs[0];
+    final FakeNonSerializableClass deserializedObj2 = (FakeNonSerializableClass) deserializedObjs[1];
+
+    assertThat(deserializedObj1, is(obj));
+    assertThat(deserializedObj2, is(sameInstance(deserializedObj1)));
+  }
+
+  @Test
   public void shouldThrowExceptionWhenWritingNonSerializableObjectWithNoRegisteredProxyFactory() throws Exception {
     final ProxyRegistry proxyRegistry = ProxyRegistry.newInstance();
 
-    catchException(() -> writeObject(new FakeNonSerializableClass(2112, "42"), proxyRegistry));
+    catchException(() -> writeObject(proxyRegistry, new FakeNonSerializableClass(2112, "42")));
 
     assertThat(caughtException(), is(instanceOf(NotSerializableException.class)));
   }
