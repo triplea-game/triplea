d3fb80f0914347907dd29a250d25e2ae92a18440
RoiEX <RoiEXLab@users.noreply.github.com>
2017-08-24 21:21:43 +0200

    Migrate LobbyPropertyFileParser as well

--- src/main/java/games/strategy/engine/config/client/LobbyPropertyFileParser.java
+++ src/main/java/games/strategy/engine/config/client/LobbyPropertyFileParser.java
@@ -6,9 +6,9 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.List;
-import java.util.Map;
-import java.util.Optional;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.yaml.snakeyaml.Yaml;
 
 import games.strategy.engine.lobby.client.login.LobbyServerProperties;
@@ -23,33 +23,33 @@ class LobbyPropertyFileParser {
 
 
   public static LobbyServerProperties parse(final File file, final Version currentVersion) {
-    final List<Map<String, Object>> lobbyProperties;
+    final JSONArray lobbyProperties;
     try {
       lobbyProperties = loadYaml(file);
     } catch (final IOException e) {
       throw new RuntimeException("Failed loading file: " + file.getAbsolutePath() + ", please try again, if the "
           + "problem does not go away please report a bug: " + UrlConstants.GITHUB_ISSUES);
     }
-    final Map<String, Object> configForThisVersion = matchCurrentVersion(lobbyProperties, currentVersion);
-    return new LobbyServerProperties(configForThisVersion);
+    return new LobbyServerProperties(matchCurrentVersion(lobbyProperties, currentVersion).toMap());
   }
 
-  private static Map<String, Object> matchCurrentVersion(
-      final List<Map<String, Object>> lobbyProps,
+  private static JSONObject matchCurrentVersion(
+      final JSONArray lobbyProps,
       final Version currentVersion) {
     checkNotNull(lobbyProps);
 
-    final Optional<Map<String, Object>> matchingVersionProps = lobbyProps.stream()
-        .filter(props -> currentVersion.equals(props.get("version")))
-        .findFirst();
-    return matchingVersionProps.orElse(lobbyProps.get(0));
+    for (int i = 0; i < lobbyProps.length(); i++) {
+      final JSONObject currentObject = lobbyProps.getJSONObject(i);
+      if (currentVersion.equals(currentObject.opt("version"))) {
+        return currentObject;
+      }
+    }
+    return lobbyProps.getJSONObject(0);
   }
 
-  private static List<Map<String, Object>> loadYaml(final File yamlFile) throws IOException {
+  private static JSONArray loadYaml(final File yamlFile) throws IOException {
     final String yamlContent = new String(Files.readAllBytes(yamlFile.toPath()));
     final Yaml yaml = new Yaml();
-    @SuppressWarnings("unchecked")
-    final List<Map<String, Object>> yamlData = (List<Map<String, Object>>) yaml.load(yamlContent);
-    return yamlData;
+    return new JSONArray(yaml.loadAs(yamlContent, List.class));
   }
 }
