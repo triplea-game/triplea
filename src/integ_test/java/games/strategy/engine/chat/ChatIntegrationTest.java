989e47194cb68da749adcabc26780bd35aa00edb
Steven Soloff <steven.soloff@gmail.com>
2017-08-31 23:38:23 -0400

    Improve chat integration test
    This test intermittently fails.  It appears the root cause is due to the
    limited amount of time the test allows for messages to be delivered.  It
    sets that limit to 250 ms, which should be more than sufficient.
    However, when Travis gets heavily loaded, the time it takes to spin up
    other threads to send the messages and deliver them through the network
    stack can exceed this duration.  This change bumps the maximum time for
    which the test may run by an order of magnitude just to be safe.  This
    does not adversely affect the actual time it may take to run this test;
    it simply allows more time for the expected condition to be satisfied.
    
    Several refactorings were also applied to improve the test overall.

---
+++ src/integ_test/java/games/strategy/engine/chat/ChatIntegrationTest.java
@@ -0,0 +1,186 @@
+package games.strategy.engine.chat;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.util.Collection;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.IntStream;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import games.strategy.engine.lobby.server.NullModeratorController;
+import games.strategy.engine.message.ChannelMessenger;
+import games.strategy.engine.message.IChannelMessenger;
+import games.strategy.engine.message.IRemoteMessenger;
+import games.strategy.engine.message.RemoteMessenger;
+import games.strategy.engine.message.unifiedmessenger.UnifiedMessenger;
+import games.strategy.net.ClientMessenger;
+import games.strategy.net.IMessenger;
+import games.strategy.net.INode;
+import games.strategy.net.IServerMessenger;
+import games.strategy.net.MacFinder;
+import games.strategy.net.ServerMessenger;
+import games.strategy.sound.SoundPath;
+import games.strategy.test.TestUtil;
+
+public final class ChatIntegrationTest {
+  private static final String CHAT_NAME = "c";
+  private static final int MESSAGE_COUNT = 50;
+  private static final int NODE_COUNT = 3;
+
+  private final int serverPort = TestUtil.getUniquePort();
+  private IServerMessenger serverMessenger;
+  private IMessenger client1Messenger;
+  private IMessenger client2Messenger;
+  private RemoteMessenger serverRemoteMessenger;
+  private ChannelMessenger serverChannelMessenger;
+  private RemoteMessenger client1RemoteMessenger;
+  private ChannelMessenger client1ChannelMessenger;
+  private RemoteMessenger client2RemoteMessenger;
+  private ChannelMessenger client2ChannelMessenger;
+  private final TestChatListener serverChatListener = new TestChatListener();
+  private final TestChatListener client1ChatListener = new TestChatListener();
+  private final TestChatListener client2ChatListener = new TestChatListener();
+  private NullModeratorController serverModeratorController;
+
+  @Before
+  public void setUp() throws Exception {
+    serverMessenger = new ServerMessenger("Server", serverPort);
+    serverMessenger.setAcceptNewConnections(true);
+    final String mac = MacFinder.getHashedMacAddress();
+    client1Messenger = new ClientMessenger("localhost", serverPort, "client1", mac);
+    client2Messenger = new ClientMessenger("localhost", serverPort, "client2", mac);
+    final UnifiedMessenger serverUnifiedMessenger = new UnifiedMessenger(serverMessenger);
+    serverRemoteMessenger = new RemoteMessenger(serverUnifiedMessenger);
+    serverChannelMessenger = new ChannelMessenger(serverUnifiedMessenger);
+    final UnifiedMessenger client1UnifiedMessenger = new UnifiedMessenger(client1Messenger);
+    client1RemoteMessenger = new RemoteMessenger(client1UnifiedMessenger);
+    client1ChannelMessenger = new ChannelMessenger(client1UnifiedMessenger);
+    final UnifiedMessenger client2UnifiedMessenger = new UnifiedMessenger(client2Messenger);
+    client2RemoteMessenger = new RemoteMessenger(client2UnifiedMessenger);
+    client2ChannelMessenger = new ChannelMessenger(client2UnifiedMessenger);
+    serverModeratorController = new NullModeratorController(serverMessenger, null);
+    serverModeratorController.register(serverRemoteMessenger);
+  }
+
+  @After
+  public void tearDown() {
+    if (serverMessenger != null) {
+      serverMessenger.shutDown();
+    }
+    if (client1Messenger != null) {
+      client1Messenger.shutDown();
+    }
+    if (client2Messenger != null) {
+      client2Messenger.shutDown();
+    }
+  }
+
+  @Test(timeout = 15_000)
+  public void shouldBeAbleToChatAcrossMultipleNodes() throws Exception {
+    final ChatController controller = newChatController();
+    final Chat server = newChat(serverMessenger, serverChannelMessenger, serverRemoteMessenger);
+    server.addChatListener(serverChatListener);
+    final Chat client1 = newChat(client1Messenger, client1ChannelMessenger, client1RemoteMessenger);
+    client1.addChatListener(client1ChatListener);
+    final Chat client2 = newChat(client2Messenger, client2ChannelMessenger, client2RemoteMessenger);
+    client2.addChatListener(client2ChatListener);
+    waitFor(this::allNodesToConnect);
+
+    sendMessagesFrom(client2);
+    sendMessagesFrom(server);
+    sendMessagesFrom(client1);
+    waitFor(this::allMessagesToArrive);
+
+    client1.shutdown();
+    client2.shutdown();
+    waitFor(this::clientNodesToDisconnect);
+
+    controller.deactivate();
+    waitFor(this::serverNodeToDisconnect);
+  }
+
+  private ChatController newChatController() {
+    return new ChatController(
+        CHAT_NAME,
+        serverMessenger,
+        serverRemoteMessenger,
+        serverChannelMessenger,
+        serverModeratorController);
+  }
+
+  private static Chat newChat(
+      final IMessenger messenger,
+      final IChannelMessenger channelMessenger,
+      final IRemoteMessenger remoteMessenger) {
+    return new Chat(messenger, CHAT_NAME, channelMessenger, remoteMessenger, Chat.CHAT_SOUND_PROFILE.NO_SOUND);
+  }
+
+  private static void waitFor(final Runnable assertion) throws InterruptedException {
+    final long timeoutInMilliseconds = 10_000L;
+    final long startTimeInMilliseconds = System.currentTimeMillis();
+    while ((System.currentTimeMillis() - startTimeInMilliseconds) < timeoutInMilliseconds) {
+      try {
+        assertion.run();
+        return;
+      } catch (final AssertionError e) {
+        Thread.sleep(25L);
+      }
+    }
+
+    assertion.run();
+  }
+
+  private void allNodesToConnect() {
+    assertThat(serverChatListener.playerCount.get(), is(NODE_COUNT));
+    assertThat(client1ChatListener.playerCount.get(), is(NODE_COUNT));
+    assertThat(client2ChatListener.playerCount.get(), is(NODE_COUNT));
+  }
+
+  private void allMessagesToArrive() {
+    assertThat(serverChatListener.messageCount.get(), is(NODE_COUNT * MESSAGE_COUNT));
+    assertThat(client1ChatListener.messageCount.get(), is(NODE_COUNT * MESSAGE_COUNT));
+    assertThat(client2ChatListener.messageCount.get(), is(NODE_COUNT * MESSAGE_COUNT));
+  }
+
+  private void clientNodesToDisconnect() {
+    assertThat(serverChatListener.playerCount.get(), is(1));
+  }
+
+  private void serverNodeToDisconnect() {
+    assertThat(serverChatListener.playerCount.get(), is(0));
+  }
+
+  private static void sendMessagesFrom(final Chat node) {
+    new Thread(() -> {
+      IntStream.range(0, MESSAGE_COUNT).forEach(i -> node.sendMessage("Test", false));
+    }).start();
+  }
+
+  private static final class TestChatListener implements IChatListener {
+    final AtomicInteger playerCount = new AtomicInteger();
+    final AtomicInteger messageCount = new AtomicInteger();
+
+    @Override
+    public void updatePlayerList(final Collection<INode> players) {
+      playerCount.set(players.size());
+    }
+
+    @Override
+    public void addMessageWithSound(final String message, final String from, final boolean thirdperson,
+        final String sound) {
+      messageCount.incrementAndGet();
+    }
+
+    @Override
+    public void addMessage(final String message, final String from, final boolean thirdperson) {
+      addMessageWithSound(message, from, thirdperson, SoundPath.CLIP_CHAT_MESSAGE);
+    }
+
+    @Override
+    public void addStatusMessage(final String message) {}
+  }
+}
