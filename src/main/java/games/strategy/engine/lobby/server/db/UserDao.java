2277313b536a201c1efef361a654651d5873abec
RoiEX <RoiEXLab@users.noreply.github.com>
2017-09-12 12:06:15 +0200

    Use 2 rows for password storage
    Improve SQL timestamps
    
    
    Formatting
    
    
    Adjust Storage sizes
    
    
    Create References | Add default values
    
    
    Remove redundant arguments
    
    
    Formatting
    
    
    Add backwards password compatibility
    
    
    Avoid relying on error codes
    
    
    Fix reference
    
    
    Fix tested code
    
    
    Reduce duplicate code
    
    
    Change admin to boolean type
    
    
    Add stricter checks
    
    
    Use Md5Password method
    
    
    Fix test
    
    
    Revert Non-password related code
    
    This reverts commit 9eb568c527afde7242b27bd547e8e9a9b8664724.
    
    Revert "Fix reference"
    
    This reverts commit 57f10168f92d31fff226f99fdac29a17be925d36.
    
    Revert "Avoid relying on error codes"
    
    This reverts commit f9e0caa8d935e1368e3182c2dbd8200f1907b325.
    
    Revert "Formatting"
    
    This reverts commit 099487a96e46f8a3071158f3d157a39491c424e4.
    Revert "Remove redundant arguments"
    
    This reverts commit 7955fb3e2b0638ebc22727e2c8c71ec84dde9a30.
    Revert "Create References | Add default values"
    
    This reverts commit cc616a9471f5e1fe17ef4d80a461d53c0b223661.
    Revert "Formatting"
    
    This reverts commit 8ca8d24d73de3e2416b5a03d7b9d8210956326b2.
    
    Revert "Improve SQL Timestamps"
    
    
    Revert "Clean up schema"
    
    This reverts commit fcedeca6f4f20dd1282cf1f39491a9d22161aff1.

--- src/main/java/games/strategy/engine/lobby/server/db/UserDao.java
+++ src/main/java/games/strategy/engine/lobby/server/db/UserDao.java
@@ -4,15 +4,17 @@ import games.strategy.engine.lobby.server.userDB.DBUser;
 
 public interface UserDao {
 
-  HashedPassword getPassword(String userName);
+  HashedPassword getPassword(String username);
 
-  boolean doesUserExist(String userName);
+  HashedPassword getMd5Password(String username);
+
+  boolean doesUserExist(String username);
 
   void updateUser(DBUser user, HashedPassword password);
 
   void createUser(DBUser user, HashedPassword password);
 
-  boolean login(String userName, HashedPassword password);
+  boolean login(String username, HashedPassword password);
 
-  DBUser getUserByName(String userName);
+  DBUser getUserByName(String username);
 }
