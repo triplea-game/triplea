5ab0f1ae9a8f943581f43964051a5f905aa55894
Steven Soloff <steven.soloff@gmail.com>
2017-09-14 01:07:37 -0400

    Replace IllegalCharacterRemover implementation with Guava CharMatcher

--- src/main/java/games/strategy/util/IllegalCharacterRemover.java
+++ src/main/java/games/strategy/util/IllegalCharacterRemover.java
@@ -1,49 +1,34 @@
 package games.strategy.util;
 
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.base.CharMatcher;
+
 /**
- * Designed to remove/replace<br>
+ * Designed to remove/replace the following illegal file name characters:
+ *
+ * <pre>
  * / \b \n \r \t \0 \f ` ? * \ < > | " ' : . , ^ [ ] = + ;
+ * </pre>
  */
-public class IllegalCharacterRemover {
-  private static final char[] ILLEGAL_CHARACTERS = {'/', '\b', '\n', '\r', '\t', '\0', '\f', '`', '?', '*', '\\', '<',
-      '>', '|', '\"', '\'', ':', '.', ',', '^', '[', ']', '=', '+', ';'};
+public final class IllegalCharacterRemover {
+  @VisibleForTesting
+  static final String ILLEGAL_CHARACTERS = "/\b\n\r\t\0\f`?*\\<>|\"\':.,^[]=+;";
+
+  private static final CharMatcher ILLEGAL_CHAR_MATCHER = CharMatcher.anyOf(ILLEGAL_CHARACTERS);
+
+  private IllegalCharacterRemover() {}
 
   /**
    * Designed to remove / \b \n \r \t \0 \f ` ? * \ < > | " ' : . , ^ [ ] = + ;
    */
   public static String removeIllegalCharacter(final String text) {
-    final StringBuilder sb = new StringBuilder();
-    for (int i = 0; i < text.length(); ++i) {
-      if (!isIllegalFileNameChar(text.charAt(i))) {
-        sb.append(text.charAt(i));
-      }
-    }
-    return sb.toString();
+    return ILLEGAL_CHAR_MATCHER.removeFrom(text);
   }
 
   /**
    * Designed to replace / \b \n \r \t \0 \f ` ? * \ < > | " ' : . , ^ [ ] = + ;
    */
   public static String replaceIllegalCharacter(final String text, final char replacement) {
-    final StringBuilder sb = new StringBuilder();
-    for (int i = 0; i < text.length(); ++i) {
-      if (!isIllegalFileNameChar(text.charAt(i))) {
-        sb.append(text.charAt(i));
-      } else {
-        sb.append(replacement);
-      }
-    }
-    return sb.toString();
-  }
-
-  private static boolean isIllegalFileNameChar(final char c) {
-    boolean isIllegal = false;
-    for (final char element : ILLEGAL_CHARACTERS) {
-      if (c == element) {
-        isIllegal = true;
-        break;
-      }
-    }
-    return isIllegal;
+    return ILLEGAL_CHAR_MATCHER.replaceFrom(text, replacement);
   }
 }
