a92756009600a1eff0501780d80971b44df16172
RoiEX <8350879+RoiEXLab@users.noreply.github.com>
2017-09-16 12:13:03 +0200

    Fix variable naming
    Copy pasting *DOES* introduce issues regularily

--- src/integ_test/java/games/strategy/engine/lobby/server/db/BannedUsernameControllerTest.java
+++ src/integ_test/java/games/strategy/engine/lobby/server/db/BannedUsernameControllerTest.java
@@ -21,46 +21,46 @@ public class BannedUsernameControllerTest {
   @Test
   public void testBanUsernameForever() {
     final String hashedUsername = banUsername(null);
-    final Tuple<Boolean, Timestamp> UsernameDetails = controller.isUsernameBanned(hashedUsername);
-    assertTrue(UsernameDetails.getFirst());
-    assertNull(UsernameDetails.getSecond());
+    final Tuple<Boolean, Timestamp> usernameDetails = controller.isUsernameBanned(hashedUsername);
+    assertTrue(usernameDetails.getFirst());
+    assertNull(usernameDetails.getSecond());
   }
 
   @Test
   public void testBanUsername() {
     final Instant banUntil = Instant.now().plusSeconds(2L);
     final String hashedUsername = banUsername(banUntil);
-    final Tuple<Boolean, Timestamp> UsernameDetails = controller.isUsernameBanned(hashedUsername);
-    assertTrue(UsernameDetails.getFirst());
-    assertEquals(banUntil, UsernameDetails.getSecond().toInstant());
+    final Tuple<Boolean, Timestamp> usernameDetails = controller.isUsernameBanned(hashedUsername);
+    assertTrue(usernameDetails.getFirst());
+    assertEquals(banUntil, usernameDetails.getSecond().toInstant());
     while (banUntil.isAfter(Instant.now())) {
       ThreadUtil.sleep(100);
     }
-    final Tuple<Boolean, Timestamp> UsernameDetails2 = controller.isUsernameBanned(hashedUsername);
-    assertFalse(UsernameDetails2.getFirst());
-    assertEquals(banUntil, UsernameDetails2.getSecond().toInstant());
+    final Tuple<Boolean, Timestamp> usernameDetails2 = controller.isUsernameBanned(hashedUsername);
+    assertFalse(usernameDetails2.getFirst());
+    assertEquals(banUntil, usernameDetails2.getSecond().toInstant());
   }
 
   @Test
   public void testBanUsernameInThePast() {
     final Instant banUntil = Instant.now().minusSeconds(10L);
     final String hashedUsername = banUsername(banUntil);
-    final Tuple<Boolean, Timestamp> UsernameDetails = controller.isUsernameBanned(hashedUsername);
-    assertFalse(UsernameDetails.getFirst());
-    assertEquals(banUntil, UsernameDetails.getSecond().toInstant());
+    final Tuple<Boolean, Timestamp> usernameDetails = controller.isUsernameBanned(hashedUsername);
+    assertFalse(usernameDetails.getFirst());
+    assertEquals(banUntil, usernameDetails.getSecond().toInstant());
   }
 
   @Test
   public void testBanUsernameUpdate() {
     final String hashedUsername = banUsername(null);
-    final Tuple<Boolean, Timestamp> UsernameDetails = controller.isUsernameBanned(hashedUsername);
-    assertTrue(UsernameDetails.getFirst());
-    assertNull(UsernameDetails.getSecond());
+    final Tuple<Boolean, Timestamp> usernameDetails = controller.isUsernameBanned(hashedUsername);
+    assertTrue(usernameDetails.getFirst());
+    assertNull(usernameDetails.getSecond());
     final Instant banTill = Instant.now().plusSeconds(100L);
     controller.addBannedUsername(hashedUsername, banTill);
-    final Tuple<Boolean, Timestamp> UsernameDetails2 = controller.isUsernameBanned(hashedUsername);
-    assertTrue(UsernameDetails2.getFirst());
-    assertEquals(banTill, UsernameDetails2.getSecond().toInstant());
+    final Tuple<Boolean, Timestamp> usernameDetails2 = controller.isUsernameBanned(hashedUsername);
+    assertTrue(usernameDetails2.getFirst());
+    assertEquals(banTill, usernameDetails2.getSecond().toInstant());
   }
 
   private String banUsername(final Instant length) {
