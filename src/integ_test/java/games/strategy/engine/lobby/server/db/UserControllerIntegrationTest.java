b3761717bfebee17f606d9e6a700ca90eab6b5d6
RoiEX <8350879+RoiEXLab@users.noreply.github.com>
2017-09-26 21:16:21 +0200

    Refactor UserControllerIntegrationTest

--- src/integ_test/java/games/strategy/engine/lobby/server/db/UserControllerIntegrationTest.java
+++ src/integ_test/java/games/strategy/engine/lobby/server/db/UserControllerIntegrationTest.java
@@ -1,8 +1,13 @@
 package games.strategy.engine.lobby.server.db;
 
+import static com.googlecode.catchexception.CatchException.catchException;
+import static com.googlecode.catchexception.CatchException.caughtException;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -16,54 +21,26 @@ import games.strategy.util.MD5Crypt;
 
 public class UserControllerIntegrationTest {
 
-  private static final DbTestConnection POSTGRES =
-      new DbTestConnection(
-          Database::getPostgresConnection,
-          new UserController(Database::getPostgresConnection));
+  private static final Supplier<Connection> connectionSupplier = Database::getPostgresConnection;
+  private final UserController controller = new UserController(connectionSupplier);
 
   @Test
   public void testCreate() throws Exception {
-    testCreate(POSTGRES);
-  }
+    final DBUser user = newUser();
+    final int startCount = getUserCount();
 
-  private static void testCreate(final DbTestConnection dbTestConnection) throws Exception {
-    final DBUser user = givenUser();
-    final int startCount = getUserCount(dbTestConnection.connectionSupplier);
+    controller.createUser(user, new HashedPassword(MD5Crypt.crypt(user.getName())));
 
-    dbTestConnection.controller.createUser(user, new HashedPassword(MD5Crypt.crypt(user.getName())));
-
-    final int endCount = getUserCount(dbTestConnection.connectionSupplier);
+    final int endCount = getUserCount();
     assertEquals(endCount, startCount + 1);
   }
 
-  private static DBUser givenUser() {
-    final String name = UUID.randomUUID().toString().substring(0, 20);
-    final String email = name + "@none.none";
-    return new DBUser(
-        new DBUser.UserName(name),
-        new DBUser.UserEmail(email));
-  }
-
-  private static int getUserCount(final Supplier<Connection> connection) throws Exception {
-    try (final Connection dbConnection = connection.get()) {
-      final String sql = "select count(*) from TA_USERS";
-      final ResultSet rs = dbConnection.createStatement().executeQuery(sql);
-      assertTrue(rs.next());
-      return rs.getInt(1);
-    }
-  }
-
-
   @Test
   public void testGet() throws Exception {
-    testGet(POSTGRES);
-  }
+    final DBUser user = newUser();
+    controller.createUser(user, new HashedPassword(MD5Crypt.crypt(user.getName())));
 
-  private static void testGet(final DbTestConnection dbTestConnection) throws Exception {
-    final DBUser user = givenUser();
-    dbTestConnection.controller.createUser(user, new HashedPassword(MD5Crypt.crypt(user.getName())));
-
-    final DBUser loadedUser = dbTestConnection.controller.getUserByName(user.getName());
+    final DBUser loadedUser = controller.getUserByName(user.getName());
 
     assertEquals(loadedUser.getName(), user.getName());
     assertEquals(loadedUser.getEmail(), user.getEmail());
@@ -72,69 +49,43 @@ public class UserControllerIntegrationTest {
 
   @Test
   public void testDoesUserExist() {
-    testDoesUserExist(POSTGRES);
-  }
+    final DBUser user = newUser();
 
-  private static void testDoesUserExist(final DbTestConnection dbTestConnection) {
-    final DBUser user = givenUser();
+    controller.createUser(user, new HashedPassword(MD5Crypt.crypt(user.getName())));
 
-    dbTestConnection.controller.createUser(user, new HashedPassword(MD5Crypt.crypt(user.getName())));
-
-    assertTrue(dbTestConnection.controller.doesUserExist(user.getName()));
-  }
-
-  @Test
-  public void testDoesUserExistPostgres() {
-    testDoesUserExist(POSTGRES);
+    assertTrue(controller.doesUserExist(user.getName()));
   }
 
   @Test
   public void testCreateDupe() throws Exception {
-    testCreateDupe(POSTGRES);
-  }
+    final DBUser user = newUser();
 
-  private static void testCreateDupe(final DbTestConnection dbTestConnection) throws Exception {
-    final DBUser user = givenUser();
-
-    dbTestConnection.controller.createUser(user, new HashedPassword(MD5Crypt.crypt(user.getName())));
-
-    try {
-      dbTestConnection.controller.createUser(user, new HashedPassword(MD5Crypt.crypt(user.getName())));
-      fail("Should not be allowed to create a dupe user");
-    } catch (final Exception expected) {
-      // expected
-    }
+    controller.createUser(user, new HashedPassword(MD5Crypt.crypt(user.getName())));
+    catchException(() -> controller.createUser(user, new HashedPassword(MD5Crypt.crypt(user.getName()))));
+    assertThat("Should not be allowed to create a dupe user", caughtException(), is(not(equalTo(null))));
   }
 
   @Test
   public void testLogin() throws Exception {
-    testLogin(POSTGRES);
-  }
-
-  private static void testLogin(final DbTestConnection dbTestConnection) throws Exception {
-    final DBUser user = givenUser();
+    final DBUser user = newUser();
     final HashedPassword password = new HashedPassword(MD5Crypt.crypt(user.getName()));
-    dbTestConnection.controller.createUser(user, password);
+    controller.createUser(user, password);
 
-    assertTrue(dbTestConnection.controller.login(user.getName(), password));
+    assertTrue(controller.login(user.getName(), password));
   }
 
   @Test
   public void testUpdate() throws Exception {
-    testUpdate(POSTGRES);
-  }
-
-  private static void testUpdate(final DbTestConnection dbTestConnection) throws Exception {
-    final DBUser user = givenUser();
+    final DBUser user = newUser();
     final HashedPassword password = new HashedPassword(MD5Crypt.crypt(user.getName()));
-    dbTestConnection.controller.createUser(user, password);
-    assertTrue(dbTestConnection.controller.doesUserExist(user.getName()));
+    controller.createUser(user, password);
+    assertTrue(controller.doesUserExist(user.getName()));
     final String password2 = MD5Crypt.crypt("foo");
     final String email2 = "foo@foo.foo";
-    dbTestConnection.controller.updateUser(
+    controller.updateUser(
         new DBUser(new DBUser.UserName(user.getName()), new DBUser.UserEmail(email2)),
         new HashedPassword(password2));
-    try (final Connection con = dbTestConnection.connectionSupplier.get()) {
+    try (final Connection con = connectionSupplier.get()) {
       final String sql = " select * from ta_users where username = '" + user.getName() + "'";
       final ResultSet rs = con.createStatement().executeQuery(sql);
       assertTrue(rs.next());
@@ -143,15 +94,20 @@ public class UserControllerIntegrationTest {
     }
   }
 
+  private static DBUser newUser() {
+    final String name = UUID.randomUUID().toString().substring(0, 20);
+    final String email = name + "@none.none";
+    return new DBUser(
+        new DBUser.UserName(name),
+        new DBUser.UserEmail(email));
+  }
 
-  private static class DbTestConnection {
-    final Supplier<Connection> connectionSupplier;
-    final UserDao controller;
-
-    DbTestConnection(final Supplier<Connection> connectionSupplier,
-        final UserDao controller) {
-      this.connectionSupplier = connectionSupplier;
-      this.controller = controller;
+  private static int getUserCount() throws Exception {
+    try (final Connection dbConnection = connectionSupplier.get()) {
+      final String sql = "select count(*) from TA_USERS";
+      final ResultSet rs = dbConnection.createStatement().executeQuery(sql);
+      assertTrue(rs.next());
+      return rs.getInt(1);
     }
   }
 }
