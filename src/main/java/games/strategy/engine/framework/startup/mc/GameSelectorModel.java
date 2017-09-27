51f1451194cf84b27ad7273a7b3c3ec411a1429b
Dan Van Atta <dhvatta@gmail.com>
2017-09-27 15:40:48 -0700

    GameSelectorModel.java - Couple cleanups - fix variable names, remove 'm_' prefix - reduce method scope where possible - remove always true boolean condition - convert always false variable parameter to a 'hardcoded' false so it's more clear the value is always false at that point of execution - add a class javadoc

--- src/main/java/games/strategy/engine/framework/startup/mc/GameSelectorModel.java
+++ src/main/java/games/strategy/engine/framework/startup/mc/GameSelectorModel.java
@@ -25,40 +25,38 @@ import games.strategy.engine.framework.ui.NewGameChooserModel;
 import games.strategy.triplea.ai.proAI.ProAI;
 import games.strategy.triplea.settings.ClientSetting;
 
+/**
+ * Data model that represents the users current 'game' selection.
+ * This includes for example which file was used to load the game,
+ * a partially parsed game data object.
+ */
 public class GameSelectorModel extends Observable {
-  /**
-   * Returns the name of the directory within a map's directory where the game xml is held.
-   * Example: returns "games" which would be the games folder of "triplea/maps/someMapFooBar/games"
-   */
-  public static final String DEFAULT_GAME_XML_DIRECTORY_NAME = "games";
-  private GameData m_data = null;
-  private String m_gameName = "";
-  private String m_gameVersion = "";
-  private String m_gameRound = "";
-  private String m_fileName = "";
-  private boolean m_canSelect = true;
-  private boolean m_isHostHeadlessBot = false;
+  private GameData data;
+  private String gameName = "";
+  private String gameVersion = "";
+  private String gameRound = "";
+  private String fileName = "";
+  private boolean canSelect = true;
+  private boolean isHostHeadlessBot = false;
   // just for host bots, so we can get the actions for loading/saving games on the bots
   // from this model
-  private ClientModel m_clientModelForHostBots = null;
+  private ClientModel clientModelForHostBots;
 
   public GameSelectorModel() {
     setGameData(null);
-    m_fileName = "";
   }
 
   public void resetGameDataToNull() {
     setGameData(null);
-    m_fileName = "";
   }
 
   public void load(final GameData data, final String fileName) {
     setGameData(data);
-    m_fileName = fileName;
+    this.fileName = fileName;
   }
 
   public void load(final NewGameChooserEntry entry) {
-    m_fileName = entry.getLocation();
+    fileName = entry.getLocation();
     setGameData(entry.getGameData());
     if (entry.getGameData() != null) {
       ClientSetting.DEFAULT_GAME_NAME_PREF.save(entry.getGameData().getGameName());
@@ -97,7 +95,7 @@ public class GameSelectorModel extends Observable {
         newData = GameDataManager.loadGame(file);
       }
       if (newData != null) {
-        m_fileName = file.getName();
+        fileName = file.getName();
         setGameData(newData);
       }
     } catch (final EngineVersionException e) {
@@ -129,11 +127,11 @@ public class GameSelectorModel extends Observable {
   }
 
   public synchronized GameData getGameData() {
-    return m_data;
+    return data;
   }
 
   public boolean isSavedGame() {
-    return !m_fileName.endsWith(".xml");
+    return !fileName.endsWith(".xml");
   }
 
   private static void error(final String message, final Component ui) {
@@ -144,87 +142,85 @@ public class GameSelectorModel extends Observable {
 
   void setCanSelect(final boolean canSelect) {
     synchronized (this) {
-      m_canSelect = canSelect;
+      this.canSelect = canSelect;
     }
     notifyObs();
   }
 
   public synchronized boolean canSelect() {
-    return m_canSelect;
+    return canSelect;
   }
 
   void setIsHostHeadlessBot(final boolean isHostHeadlessBot) {
     synchronized (this) {
-      m_isHostHeadlessBot = isHostHeadlessBot;
+      this.isHostHeadlessBot = isHostHeadlessBot;
     }
     notifyObs();
   }
 
   public synchronized boolean isHostHeadlessBot() {
-    return m_isHostHeadlessBot;
+    return isHostHeadlessBot;
   }
 
   void setClientModelForHostBots(final ClientModel clientModel) {
     synchronized (this) {
-      m_clientModelForHostBots = clientModel;
+      clientModelForHostBots = clientModel;
     }
   }
 
   public synchronized ClientModel getClientModelForHostBots() {
-    return m_clientModelForHostBots;
+    return clientModelForHostBots;
   }
 
   /**
    * We dont have a gane data (ie we are a remote player and the data has not been sent yet), but
    * we still want to display game info.
    */
-  public void clearDataButKeepGameInfo(final String gameName, final String gameRound, final String gameVersion) {
+  void clearDataButKeepGameInfo(final String gameName, final String gameRound, final String gameVersion) {
     synchronized (this) {
-      m_data = null;
-      m_gameName = gameName;
-      m_gameRound = gameRound;
-      m_gameVersion = gameVersion;
+      data = null;
+      this.gameName = gameName;
+      this.gameRound = gameRound;
+      this.gameVersion = gameVersion;
     }
     notifyObs();
   }
 
   public synchronized String getFileName() {
-    if (m_data == null) {
-      return "-";
-    } else {
-      return m_fileName;
-    }
+    return data == null ? "-" : fileName;
   }
 
   public synchronized String getGameName() {
-    return m_gameName;
+    return gameName;
   }
 
   public synchronized String getGameRound() {
-    return m_gameRound;
+    return gameRound;
   }
 
   public synchronized String getGameVersion() {
-    return m_gameVersion;
+    return gameVersion;
   }
 
   void setGameData(final GameData data) {
     synchronized (this) {
       if (data == null) {
-        m_gameName = m_gameRound = m_gameVersion = "-";
+        gameName = "-";
+        gameRound = "-";
+        gameVersion = "-";
       } else {
-        m_gameName = data.getGameName();
-        m_gameRound = "" + data.getSequence().getRound();
-        m_gameVersion = data.getGameVersion().toString();
+        gameName = data.getGameName();
+        gameRound = "" + data.getSequence().getRound();
+        gameVersion = data.getGameVersion().toString();
       }
-      m_data = data;
+      this.data = data;
     }
     notifyObs();
   }
 
   private void notifyObs() {
     super.setChanged();
-    super.notifyObservers(m_data);
+    super.notifyObservers(data);
     super.clearChanged();
   }
 
@@ -254,9 +250,9 @@ public class GameSelectorModel extends Observable {
     // we don't want to load a game file by default that is not within the map folders we can load. (ie: if a previous
     // version of triplea
     // was using running a game within its root folder, we shouldn't open it)
-    NewGameChooserEntry selectedGame = null;
+    NewGameChooserEntry selectedGame;;
     final String user = ClientFileSystemHelper.getUserRootFolder().toURI().toString();
-    if (!forceFactoryDefault && userPreferredDefaultGameUri != null && userPreferredDefaultGameUri.length() > 0
+    if (!forceFactoryDefault && userPreferredDefaultGameUri.length() > 0
         && userPreferredDefaultGameUri.contains(user)) {
       // if the user has a preferred URI, then we load it, and don't bother parsing or doing anything with the whole
       // game model list
@@ -264,7 +260,7 @@ public class GameSelectorModel extends Observable {
         final URI defaultUri = new URI(userPreferredDefaultGameUri);
         selectedGame = new NewGameChooserEntry(defaultUri);
       } catch (final Exception e) {
-        selectedGame = selectByName(forceFactoryDefault);
+        selectedGame = selectByName(false);
         if (selectedGame == null) {
           return;
         }
