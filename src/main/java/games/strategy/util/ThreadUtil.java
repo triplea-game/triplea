1588b516b3f07034e72836644c71fb59419825e9
Steven Soloff <steven.soloff@gmail.com>
2017-09-27 00:47:03 -0400

    Change parameter type for parity with Thread#sleep()

--- src/main/java/games/strategy/util/ThreadUtil.java
+++ src/main/java/games/strategy/util/ThreadUtil.java
@@ -1,18 +1,19 @@
 package games.strategy.util;
 
 /** Utility class for java Thread related operations. */
-public class ThreadUtil {
+public final class ThreadUtil {
+  private ThreadUtil() {}
 
   /**
    * Sleeps the current thread, useful to handle interrupted exceptions.
    * This method sets the interrupted flag on the current thread, per best practice:
-   *  - http://www.yegor256.com/2015/10/20/interrupted-exception.html
-   *  - http://stackoverflow.com/questions/3976344/handling-interruptedexception-in-java
+   * - http://www.yegor256.com/2015/10/20/interrupted-exception.html
+   * - http://stackoverflow.com/questions/3976344/handling-interruptedexception-in-java
    *
    * @param millis Number of milliseconds to sleep
    * @return False on InterruptedException, true otherwise (implying that we have slept for the desired time duration)
    */
-  public static boolean sleep(final int millis) {
+  public static boolean sleep(final long millis) {
     try {
       Thread.sleep(millis);
       return true;
