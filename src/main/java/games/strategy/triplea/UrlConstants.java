73985db80e6de64d85f3098c123eb4e643f0aa81
Steven Soloff <steven.soloff@gmail.com>
2017-09-27 01:23:46 -0400

    Convert http URLs to https where applicable

--- src/main/java/games/strategy/triplea/UrlConstants.java
+++ src/main/java/games/strategy/triplea/UrlConstants.java
@@ -11,13 +11,13 @@ package games.strategy.triplea;
  * </pre></code>
  */
 public enum UrlConstants {
-  GITHUB_ISSUES("http://github.com/triplea-game/triplea/issues/new"),
-  RULE_BOOK("http://triplea-game.github.io/files/TripleA_RuleBook.pdf"),
+  GITHUB_ISSUES("https://github.com/triplea-game/triplea/issues/new"),
+  RULE_BOOK("https://triplea-game.github.io/files/TripleA_RuleBook.pdf"),
   LOBBY_PROPS("https://raw.githubusercontent.com/triplea-game/triplea/master/lobby_server.yaml"),
   PAYPAL_DONATE("https://sourceforge.net/donate/index.php?group_id=44492"),
   GITHUB_HELP("http://www.triplea-game.org/help/"),
   HOSTING_GUIDE("https://forums.triplea-game.org/topic/100"),
-  TRIPLEA_FORUM("http://forums.triplea-game.org/"),
+  TRIPLEA_FORUM("https://forums.triplea-game.org/"),
   TRIPLEA_LOBBY_RULES("https://forums.triplea-game.org/topic/4"),
   LATEST_GAME_DOWNLOAD_WEBSITE("http://www.triplea-game.org/download/"),
   TRIPLEA_WEBSITE("http://www.triplea-game.org/"),
