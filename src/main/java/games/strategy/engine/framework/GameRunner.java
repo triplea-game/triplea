018d77ee7666be2d9a0e99eaee94c3c420ccfe1c
Dan Van Atta <dhvatta@gmail.com>
2017-09-29 08:27:23 -0700

    Make variable final and move string concatenation symbol to beginning of line instead of end

--- src/main/java/games/strategy/engine/framework/GameRunner.java
+++ src/main/java/games/strategy/engine/framework/GameRunner.java
@@ -412,12 +412,9 @@ public class GameRunner {
       return false;
     }
 
-    String message = "<html>" +
-            "Would you like to download the tutorial map?<br>" +
-            "<br>" +
-            "(You can always download it later using the Download Maps<br>" +
-            "command if you don't want to do it now.)" +
-            "</html>";
+    final String message = "<html>Would you like to download the tutorial map?<br><br>"
+            + "(You can always download it later using the Download Maps<br>"
+            + "command if you don't want to do it now.)</html>";
     SwingComponents.promptUser("Welcome to TripleA", message, () -> {
       DownloadMapsWindow.showDownloadMapsWindowAndDownload("Tutorial");
     });
