24a302aed27afb9e0f37e5d0107c4b0b49f8247f
Steven Soloff <steven.soloff@gmail.com>
2017-10-02 23:26:47 -0400

    Remove redundant null check

--- src/main/java/tools/image/ReliefImageBreaker.java
+++ src/main/java/tools/image/ReliefImageBreaker.java
@@ -118,13 +118,11 @@ public class ReliefImageBreaker {
   private static boolean doSeaZone() {
     while (true) {
       final String answer = JOptionPane.showInputDialog(null, "Only Do Sea Zones? Enter [Y/N]");
-
       if (answer == null) {
         continue;
-      }
-      if (answer.equalsIgnoreCase("Y")) {
+      } else if (answer.equalsIgnoreCase("Y")) {
         return true;
-      } else if (answer == null || answer.equalsIgnoreCase("N")) {
+      } else if (answer.equalsIgnoreCase("N")) {
         return false;
       }
     }
