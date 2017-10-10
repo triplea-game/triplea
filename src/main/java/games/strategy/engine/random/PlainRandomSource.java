53da6df7dbbc571e9555824fa641bbde15003d0a
Steven Soloff <steven.soloff@gmail.com>
2017-10-10 12:49:56 -0400

    Fix thread safety bug in PlainRandomSource
    PlainRandomSource uses a single MersenneTwister instance to service all
    PlainRandomSource instances.  It attempts to serialize access to the
    MersenneTwister, which is not thread safe, by synchronizing on the
    PlainRandomSource instance lock.  If there are multiple instances of
    PlainRandomSource active at once, this synchronization will not protect
    against concurrent access by each of those instances to the single
    MersenneTwister.
    
    A class-level lock was introduced to protect access to the class-level
    MersenneTwister instance.  Additionally, lock scope was reduced to the
    minimum required.

--- src/main/java/games/strategy/engine/random/PlainRandomSource.java
+++ src/main/java/games/strategy/engine/random/PlainRandomSource.java
@@ -1,15 +1,22 @@
 package games.strategy.engine.random;
 
+import javax.annotation.concurrent.GuardedBy;
+import javax.annotation.concurrent.ThreadSafe;
+
 import org.apache.commons.math3.random.MersenneTwister;
 
 /**
  * A source of random numbers that uses a pseudorandom number generator.
  */
-public class PlainRandomSource implements IRandomSource {
+@ThreadSafe
+public final class PlainRandomSource implements IRandomSource {
+  private static final Object classLock = new Object();
+
+  @GuardedBy("classLock")
   private static MersenneTwister random;
 
   @Override
-  public synchronized int[] getRandom(final int max, final int count, final String annotation)
+  public int[] getRandom(final int max, final int count, final String annotation)
       throws IllegalArgumentException {
     if (count <= 0) {
       throw new IllegalArgumentException("count must be > 0, annotation:" + annotation);
@@ -22,10 +29,12 @@ public class PlainRandomSource implements IRandomSource {
   }
 
   @Override
-  public synchronized int getRandom(final int max, final String annotation) throws IllegalArgumentException {
-    if (random == null) {
-      random = new MersenneTwister();
+  public int getRandom(final int max, final String annotation) throws IllegalArgumentException {
+    synchronized (classLock) {
+      if (random == null) {
+        random = new MersenneTwister();
+      }
+      return random.nextInt(max);
     }
-    return random.nextInt(max);
   }
 }
