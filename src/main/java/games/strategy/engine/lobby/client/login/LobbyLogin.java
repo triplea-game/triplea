2fe809173444d989fcff8a4f45d800fd156c9383
Steven Soloff <steven.soloff@gmail.com>
2017-10-27 14:10:49 -0400

    Display wait indicator during lobby account creation

--- src/main/java/games/strategy/engine/lobby/client/login/LobbyLogin.java
+++ src/main/java/games/strategy/engine/lobby/client/login/LobbyLogin.java
@@ -37,7 +37,7 @@ public class LobbyLogin {
    * If we could not login, return null.
    * </p>
    */
-  public LobbyClient login() {
+  public @Nullable LobbyClient login() {
     if (!lobbyServerProperties.isServerAvailable()) {
       showError("Could not connect to server", lobbyServerProperties.serverErrorMessage);
       return null;
@@ -91,7 +91,6 @@ public class LobbyLogin {
             } else {
               final String salt = challenge.getOrDefault(LobbyLoginValidator.SALT_KEY, MD5Crypt.newSalt());
               response.put(LobbyLoginValidator.HASHED_PASSWORD_KEY, MD5Crypt.crypt(password, salt));
-
               if (RsaAuthenticator.canProcessChallenge(challenge)) {
                 response.putAll(RsaAuthenticator.newResponse(challenge, password));
               }
@@ -106,7 +105,7 @@ public class LobbyLogin {
     JOptionPane.showMessageDialog(parentWindow, message, title, JOptionPane.ERROR_MESSAGE);
   }
 
-  private LobbyClient loginToServer() {
+  private @Nullable LobbyClient loginToServer() {
     final LoginPanel loginPanel = new LoginPanel(LobbyLoginPreferences.load());
     final LoginPanel.ReturnValue returnValue = loginPanel.show(parentWindow);
     switch (returnValue) {
@@ -121,7 +120,7 @@ public class LobbyLogin {
     }
   }
 
-  private LobbyClient createAccount() {
+  private @Nullable LobbyClient createAccount() {
     final CreateUpdateAccountPanel createAccountPanel = CreateUpdateAccountPanel.newCreatePanel();
     final CreateUpdateAccountPanel.ReturnValue returnValue = createAccountPanel.show(parentWindow);
     switch (returnValue) {
@@ -134,41 +133,51 @@ public class LobbyLogin {
     }
   }
 
-  private LobbyClient createAccount(final CreateUpdateAccountPanel createAccount) {
+  private @Nullable LobbyClient createAccount(final CreateUpdateAccountPanel panel) {
     try {
-      final String mac = MacFinder.getHashedMacAddress();
-      final ClientMessenger messenger = new ClientMessenger(lobbyServerProperties.host, lobbyServerProperties.port,
-          createAccount.getUserName(), mac, new IConnectionLogin() {
-            @Override
-            public void notifyFailedLogin(final String message) {
-              showError("Login Failed", message);
-            }
-
-            @Override
-            public Map<String, String> getProperties(final Map<String, String> challengeProperties) {
-              final Map<String, String> props = new HashMap<>();
-              props.put(LobbyLoginValidator.REGISTER_NEW_USER_KEY, Boolean.TRUE.toString());
-              props.put(LobbyLoginValidator.EMAIL_KEY, createAccount.getEmail());
-              // TODO: Don't send the md5-hashed password once the lobby removes the support, kept for
-              // backwards-compatibility
-              props.put(LobbyLoginValidator.HASHED_PASSWORD_KEY, MD5Crypt.crypt(createAccount.getPassword()));
-              if (RsaAuthenticator.canProcessChallenge(challengeProperties)) {
-                props.putAll(RsaAuthenticator.newResponse(challengeProperties, createAccount.getPassword()));
-              }
-              props.put(LobbyLoginValidator.LOBBY_VERSION, LobbyServer.LOBBY_VERSION.toString());
-              return props;
-            }
-          });
-
-      // lobby login was successful if we reach this point
-      createAccount.getLobbyLoginPreferences().save();
+      final IMessenger messenger = BackgroundTaskRunner.runInBackgroundAndReturnOrThrow(
+          "Connecting to lobby...",
+          () -> createAccount(panel.getUserName(), panel.getPassword(), panel.getEmail()),
+          IOException.class);
+      panel.getLobbyLoginPreferences().save();
       return new LobbyClient(messenger, false);
-    } catch (final CouldNotLogInException clne) {
-      // this has already been dealt with
-      return createAccount();
+    } catch (final CouldNotLogInException e) {
+      showError("Account Creation Failed", e.getMessage());
+      return createAccount(); // NB: potential stack overflow due to recursive call
     } catch (final IOException e) {
-      showError("Account creation failed", e.getMessage());
+      showError("Could Not Connect", "Could not connect to lobby: " + e.getMessage());
+      return null;
+    } catch (final InterruptedException e) {
+      Thread.currentThread().interrupt();
       return null;
     }
+  }
+
+  private IMessenger createAccount(final String userName, final String password, final String email)
+      throws IOException {
+    return new ClientMessenger(
+        lobbyServerProperties.host,
+        lobbyServerProperties.port,
+        userName,
+        MacFinder.getHashedMacAddress(),
+        new IConnectionLogin() {
+          @Override
+          public void notifyFailedLogin(final String message) {}
+
+          @Override
+          public Map<String, String> getProperties(final Map<String, String> challenge) {
+            final Map<String, String> response = new HashMap<>();
+            response.put(LobbyLoginValidator.REGISTER_NEW_USER_KEY, Boolean.TRUE.toString());
+            response.put(LobbyLoginValidator.EMAIL_KEY, email);
+            // TODO: Don't send the md5-hashed password once the lobby removes the support, kept for
+            // backwards-compatibility
+            response.put(LobbyLoginValidator.HASHED_PASSWORD_KEY, MD5Crypt.crypt(password));
+            if (RsaAuthenticator.canProcessChallenge(challenge)) {
+              response.putAll(RsaAuthenticator.newResponse(challenge, password));
+            }
+            response.put(LobbyLoginValidator.LOBBY_VERSION, LobbyServer.LOBBY_VERSION.toString());
+            return response;
+          }
+        });
   }
 }
