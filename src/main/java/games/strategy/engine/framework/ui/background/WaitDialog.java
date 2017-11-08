ac4b492af9506ec92828011a63a5533e337445fb
Steven Soloff <steven.soloff@gmail.com>
2017-11-08 00:32:39 -0500

    Map WaitDialog system close button to cancel action

--- src/main/java/games/strategy/engine/framework/ui/background/WaitDialog.java
+++ src/main/java/games/strategy/engine/framework/ui/background/WaitDialog.java
@@ -2,11 +2,13 @@ package games.strategy.engine.framework.ui.background;
 
 import java.awt.BorderLayout;
 import java.awt.Component;
-import java.awt.event.ActionListener;
 
+import javax.annotation.Nullable;
 import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JOptionPane;
+
+import games.strategy.ui.SwingComponents;
 
 /**
  * A dialog that can be displayed during a long-running operation that optionally provides the user with the ability to
@@ -15,19 +17,24 @@ import javax.swing.JOptionPane;
 public final class WaitDialog extends JDialog {
   private static final long serialVersionUID = 7433959812027467868L;
 
-  public WaitDialog(final Component parent, final String waitMessage) {
-    this(parent, waitMessage, null);
+  public WaitDialog(final Component parent, final String message) {
+    this(parent, message, null);
   }
 
-  public WaitDialog(final Component parent, final String waitMessage, final ActionListener cancelAction) {
+  public WaitDialog(final Component parent, final String message, final @Nullable Runnable cancelAction) {
     super(JOptionPane.getFrameForComponent(parent), "Please Wait", true);
-    final WaitPanel panel = new WaitPanel(waitMessage);
-    getContentPane().setLayout(new BorderLayout());
-    getContentPane().add(panel, BorderLayout.CENTER);
+
+    setLayout(new BorderLayout());
+    add(new WaitPanel(message), BorderLayout.CENTER);
+
+    setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
     if (cancelAction != null) {
       final JButton cancelButton = new JButton("Cancel");
-      cancelButton.addActionListener(cancelAction);
-      getContentPane().add(cancelButton, BorderLayout.SOUTH);
+      cancelButton.addActionListener(e -> cancelAction.run());
+      add(cancelButton, BorderLayout.SOUTH);
+
+      SwingComponents.addEscapeKeyListener(this, cancelAction);
+      SwingComponents.addWindowClosingListener(this, cancelAction);
     }
 
     pack();
