a61f02576f55272ca93bbc0e1a03573b958cc2bb
RoiEX <8350879+RoiEXLab@users.noreply.github.com>
2017-11-09 21:15:32 +0100

    Make ArgParser non-static

--- src/main/java/games/strategy/engine/framework/ArgParser.java
+++ src/main/java/games/strategy/engine/framework/ArgParser.java
@@ -4,6 +4,7 @@ import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
+import java.util.Set;
 
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
@@ -12,7 +13,8 @@ import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 
-import games.strategy.debug.ClientLogger;
+import com.google.common.base.Joiner;
+
 import games.strategy.triplea.settings.ClientSetting;
 
 /**
@@ -21,59 +23,65 @@ import games.strategy.triplea.settings.ClientSetting;
 public final class ArgParser {
   private static final String TRIPLEA_PROTOCOL = "triplea:";
   private static final String TRIPLEA_PROPERTY_PREFIX = "P";
+  private final Set<String> availableProperties;
 
-  private ArgParser() {}
+  public ArgParser(final Set<String> availableProperties) {
+    this.availableProperties = availableProperties;
+  }
 
   /**
    * Move command line arguments to system properties or client settings.
    *
    * @return Return true if all args were valid and accepted, false otherwise.
    */
-  public static boolean handleCommandLineArgs(final String[] args, final String[] availableProperties) {
+  public boolean handleCommandLineArgs(final String[] args) {
     resetTransientClientSettings();
     final Options options = getOptions();
     final CommandLineParser parser = new DefaultParser();
     try {
       final CommandLine cli = parser.parse(options, args);
       if (!cli.getOptionProperties(TRIPLEA_PROPERTY_PREFIX).entrySet().stream().allMatch(
-          entry -> setSystemPropertyOrClientSetting((String) entry.getKey(), (String) entry.getValue(),
-              availableProperties))) {
+          entry -> setSystemPropertyOrClientSetting((String) entry.getKey(), (String) entry.getValue()))) {
+        return false;
       }
 
       // Parse remaining options
-      parseRemaining(cli.getArgs(), availableProperties);
+      parseRemaining(cli.getArgs());
 
     } catch (final ParseException e) {
-      ClientLogger.logError(e);
+      throw new IllegalArgumentException(e);
     }
     ClientSetting.flush();
     return true;
   }
 
-  private static void parseRemaining(final String[] remainingArgs, final String[] availableProperties) {
-    if (remainingArgs.length >= 1) {
+  private void parseRemaining(final String[] remainingArgs) {
+    if (remainingArgs.length == 1) {
       if (remainingArgs[0].startsWith(TRIPLEA_PROTOCOL)) {
         final String encoding = StandardCharsets.UTF_8.displayName();
         try {
           setSystemPropertyOrClientSetting(GameRunner.TRIPLEA_MAP_DOWNLOAD_PROPERTY,
-              URLDecoder.decode(remainingArgs[0].substring(TRIPLEA_PROTOCOL.length()), encoding), availableProperties);
+              URLDecoder.decode(remainingArgs[0].substring(TRIPLEA_PROTOCOL.length()), encoding));
         } catch (final UnsupportedEncodingException e) {
           throw new AssertionError(encoding + " is not a supported encoding!", e);
         }
       } else {
-        setSystemPropertyOrClientSetting(GameRunner.TRIPLEA_GAME_PROPERTY, remainingArgs[0], availableProperties);
+        setSystemPropertyOrClientSetting(GameRunner.TRIPLEA_GAME_PROPERTY, remainingArgs[0]);
       }
+    } else if (remainingArgs.length > 1) {
+      throw new IllegalArgumentException("Too much arguments: " + Arrays.toString(remainingArgs));
     }
   }
 
-  private static Options getOptions() {
+  private Options getOptions() {
     final Options options = new Options();
     options.addOption(Option.builder(TRIPLEA_PROPERTY_PREFIX)
-        .argName("property=value")
+        .argName("key=value")
         .hasArgs()
         .numberOfArgs(2)
         .valueSeparator()
-        .desc("Assign the given value to the given property key.")
+        .desc("Assign the given value to the given property key.\nValid keys are : "
+            + Joiner.on(",\n").join(availableProperties))
         .build());
     // See https://github.com/triplea-game/triplea/pull/2574 for more information
     options.addOption(Option.builder("console").build());
@@ -95,11 +103,10 @@ public final class ArgParser {
         .forEach(setting -> setting.save(setting.defaultValue));
   }
 
-  private static boolean setSystemPropertyOrClientSetting(
+  private boolean setSystemPropertyOrClientSetting(
       final String key,
-      final String value,
-      final String[] availableProperties) {
-    if (!Arrays.stream(availableProperties).anyMatch(key::equals)) {
+      final String value) {
+    if (!availableProperties.contains(key)) {
       System.out.println(String.format("Key %s is unknown", key));
       return false;
     }
@@ -117,7 +124,6 @@ public final class ArgParser {
       ClientSetting.MAP_FOLDER_OVERRIDE.save(value);
       return true;
     }
-
     return false;
   }
 }
--- src/test/java/games/strategy/engine/framework/ArgParserTest.java
+++ src/test/java/games/strategy/engine/framework/ArgParserTest.java
@@ -3,9 +3,11 @@ package games.strategy.engine.framework;
 import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
@@ -25,8 +27,7 @@ public class ArgParserTest {
     assertThat("check precondition, system property for our test key should not be set yet.",
         System.getProperty(TestData.propKey), nullValue());
 
-    final boolean result = ArgParser.handleCommandLineArgs(
-        TestData.sampleArgInput, TestData.samplePropertyNameSet);
+    final boolean result = new ArgParser(TestData.samplePropertyNameSet).handleCommandLineArgs(TestData.sampleArgInput);
 
     assertThat("prop key was supplied as an available value, "
         + " which was passed as a test value - everything should "
@@ -38,29 +39,15 @@ public class ArgParserTest {
 
   @Test
   public void emptySystemPropertiesCanBeSet() {
-    ArgParser.handleCommandLineArgs(new String[] {"a="}, new String[] {"a"});
+    new ArgParser(Collections.singleton("a")).handleCommandLineArgs(new String[] {"-Pa="});
     assertThat("expecting the system property to be empty string instead of null",
         System.getProperty("a"), is(""));
   }
 
   @Test
-  public void malformedInputThrowsException() {
-    Arrays.asList(
-        new String[] {"=a"}, // no key
-        new String[] {"="},
-        new String[] {"a=b", "a"},
-        new String[] {"a=b", " "})
-        .forEach(invalidInput -> {
-          assertThrows(
-              IllegalArgumentException.class,
-              () -> ArgParser.handleCommandLineArgs(invalidInput, new String[] {"a"}),
-              Arrays.toString(invalidInput));
-        });
-  }
-
-  @Test
   public void singleFileArgIsAssumedToBeGameProperty() {
-    ArgParser.handleCommandLineArgs(new String[] {TestData.propValue}, new String[] {GameRunner.TRIPLEA_GAME_PROPERTY});
+    new ArgParser(Collections.singleton(GameRunner.TRIPLEA_GAME_PROPERTY))
+        .handleCommandLineArgs(new String[] {TestData.propValue});
     assertThat("if we pass only one arg, it is assumed to mean we are specifying the 'game property'",
         System.getProperty(GameRunner.TRIPLEA_GAME_PROPERTY), is(TestData.propValue));
   }
@@ -68,7 +55,8 @@ public class ArgParserTest {
   @Test
   public void singleUrlArgIsAssumedToBeMapDownloadProperty() {
     final String testUrl = "triplea:" + TestData.propValue;
-    ArgParser.handleCommandLineArgs(new String[] {testUrl}, new String[] {GameRunner.TRIPLEA_MAP_DOWNLOAD_PROPERTY});
+    new ArgParser(Collections.singleton(GameRunner.TRIPLEA_MAP_DOWNLOAD_PROPERTY))
+        .handleCommandLineArgs(new String[] {testUrl});
     assertThat("if we pass only one arg prefixed with 'triplea:',"
         + " it's assumed to mean we are specifying the 'map download property'",
         System.getProperty(GameRunner.TRIPLEA_MAP_DOWNLOAD_PROPERTY), is(TestData.propValue));
@@ -77,7 +65,8 @@ public class ArgParserTest {
   @Test
   public void singleUrlArgIsUrlDecoded() {
     final String testUrl = "triplea:Something%20with+spaces%20and%20Special%20chars%20%F0%9F%A4%94";
-    ArgParser.handleCommandLineArgs(new String[] {testUrl}, new String[] {GameRunner.TRIPLEA_MAP_DOWNLOAD_PROPERTY});
+    new ArgParser(Collections.singleton(GameRunner.TRIPLEA_MAP_DOWNLOAD_PROPERTY))
+        .handleCommandLineArgs(new String[] {testUrl});
     assertThat("if we pass only one arg prefixed with 'triplea:',"
         + " it should be properly URL-decoded as it's probably coming from a browser",
         System.getProperty(GameRunner.TRIPLEA_MAP_DOWNLOAD_PROPERTY), is("Something with spaces and Special chars 🤔"));
@@ -85,22 +74,22 @@ public class ArgParserTest {
 
   @Test
   public void commandLineSwitchesAreIgnored() {
-    assertThat(ArgParser.handleCommandLineArgs(new String[] {"-console"}, new String[] {}), is(true));
+    assertThat(new ArgParser(Collections.emptySet()).handleCommandLineArgs(new String[] {"-console"}), is(true));
   }
 
   @Test
   public void returnFalseIfWeCannotMapKeysToAvailableSet() {
-    final String[] validKeys = {"a", "b"};
+    final Set<String> validKeys = new HashSet<>(Arrays.asList("a", "b"));
     Arrays.asList(
-        new String[] {"notMapped="},
-        new String[] {"notMapped=test"},
-        new String[] {"notMapped=test", "a=valid"},
-        new String[] {"a=valid", "notMapped=test"},
-        new String[] {"a=valid", "notMapped=test", "b=valid"},
-        new String[] {"a=valid", "b=valid", "notMapped=test"})
+        new String[] {"-PnotMapped="},
+        new String[] {"-PnotMapped=test"},
+        new String[] {"-PnotMapped=test", "-Pa=valid"},
+        new String[] {"-Pa=valid", "-PnotMapped=test"},
+        new String[] {"-Pa=valid", "-PnotMapped=test", "-Pb=valid"},
+        new String[] {"-Pa=valid", "-Pb=valid", "-PnotMapped=test"})
         .forEach(invalidInput -> assertThat("A key in the input is not in the valid key set, expecting this to be seen"
             + "as invalid: " + Arrays.asList(invalidInput),
-            ArgParser.handleCommandLineArgs(invalidInput, validKeys), is(false)));
+            new ArgParser(validKeys).handleCommandLineArgs(invalidInput), is(false)));
   }
 
   @Test
@@ -108,7 +97,8 @@ public class ArgParserTest {
     ClientSetting.MAP_FOLDER_OVERRIDE.save("some value");
     final String mapFolderPath = "/path/to/maps";
 
-    ArgParser.handleCommandLineArgs(new String[] {"mapFolder=" + mapFolderPath}, new String[] {GameRunner.MAP_FOLDER});
+    new ArgParser(Collections.singleton(GameRunner.MAP_FOLDER))
+        .handleCommandLineArgs(new String[] {"-PmapFolder=" + mapFolderPath});
 
     assertThat(ClientSetting.MAP_FOLDER_OVERRIDE.value(), is(mapFolderPath));
   }
@@ -117,7 +107,7 @@ public class ArgParserTest {
   public void mapFolderOverrideClientSettingIsResetWhenNotSpecified() {
     ClientSetting.MAP_FOLDER_OVERRIDE.save("some value");
 
-    ArgParser.handleCommandLineArgs(new String[0], new String[0]);
+    new ArgParser(Collections.emptySet()).handleCommandLineArgs(new String[0]);
 
     assertThat(ClientSetting.MAP_FOLDER_OVERRIDE.value(), is(ClientSetting.MAP_FOLDER_OVERRIDE.defaultValue));
   }
@@ -125,7 +115,7 @@ public class ArgParserTest {
   private interface TestData {
     String propKey = "key";
     String propValue = "value";
-    String[] sampleArgInput = new String[] {propKey + "=" + propValue};
-    String[] samplePropertyNameSet = new String[] {propKey};
+    String[] sampleArgInput = new String[] {"-P" + propKey + "=" + propValue};
+    Set<String> samplePropertyNameSet = Collections.singleton(propKey);
   }
 }
