048fe104510647bbf7c7ad2504887eb474eb2557
Dan Van Atta <dhvatta@gmail.com>
2017-12-08 20:18:47 -0800

    Mark interface as functional

--- src/main/java/org/triplea/client/launch/screens/staging/GameStartedCallback.java
+++ src/main/java/org/triplea/client/launch/screens/staging/GameStartedCallback.java
@@ -4,6 +4,7 @@ package org.triplea.client.launch.screens.staging;
  * Marker interface for listeners of the 'game started' event. Notably used by network hosts to send the
  * 'game start' event to network clients.
  */
+@FunctionalInterface
 public interface GameStartedCallback {
   void gameIsStarted();
 }
