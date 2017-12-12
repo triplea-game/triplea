bcd5a0c639ec1554428fee712059c29813332230
RoiEX <8350879+RoiEXLab@users.noreply.github.com>
2017-12-12 20:24:47 +0100

    Remove imports | simplify append statement

--- src/main/java/games/strategy/triplea/ui/StatPanel.java
+++ src/main/java/games/strategy/triplea/ui/StatPanel.java
@@ -8,7 +8,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Predicate;
