8c7de6c492ead1f2543d06bc885bf589a2276d20
RoiEX <8350879+RoiEXLab@users.noreply.github.com>
2017-12-14 22:46:47 +0100

    Remove unused imports

--- src/main/java/games/strategy/triplea/attachments/UnitSupportAttachment.java
+++ src/main/java/games/strategy/triplea/attachments/UnitSupportAttachment.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
