1572f661797056cfeba3e2f86a5a04271b26730f
Steven Soloff <steven.soloff@gmail.com>
2017-12-30 15:21:00 -0500

    Prevent exception when test lobby host set but port not set
    The fix now requires both the host and port to be set simultaneously in
    order for the override to be used.  This prevents the empty port from
    being parsed as an integer when it is not set.  If either setting is not
    set, the override will be ignored and a helpful message will be quietly
    written to the console.

--- src/test/java/games/strategy/engine/config/client/LobbyServerPropertiesFetcherTest.java
+++ src/test/java/games/strategy/engine/config/client/LobbyServerPropertiesFetcherTest.java
@@ -1,73 +1,171 @@
 package games.strategy.engine.config.client;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.sameInstance;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.fail;
 import static org.mockito.Mockito.when;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Optional;
 
 import org.junit.experimental.extensions.MockitoExtension;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 
 import games.strategy.engine.framework.map.download.DownloadUtils;
 import games.strategy.engine.lobby.client.login.LobbyServerProperties;
+import games.strategy.triplea.settings.GameSetting;
+import games.strategy.util.OptionalUtils;
 import games.strategy.util.Version;
 
 @ExtendWith(MockitoExtension.class)
 public class LobbyServerPropertiesFetcherTest {
-
-  private static final Version fakeVersion = new Version("0.0.0.0");
-
   @Mock
   private LobbyLocationFileDownloader mockFileDownloader;
 
   private LobbyServerPropertiesFetcher testObj;
 
-  /**
-   * Sets up a test object with mocked dependencies. We will primarily verify control flow.
-   */
   @BeforeEach
   public void setup() {
     testObj = new LobbyServerPropertiesFetcher(mockFileDownloader);
   }
 
-  /**
-   * Happy case test path, download file, parse it, return values parsed.
-   */
-  @Test
-  public void downloadAndParseRemoteFile() throws Exception {
-    givenHappyCase();
+  @ExtendWith(MockitoExtension.class)
+  @Nested
+  public final class DownloadAndParseRemoteFileTest {
+    /**
+     * Happy case test path, download file, parse it, return values parsed.
+     */
+    @Test
+    public void happyCase() throws Exception {
+      givenHappyCase();
 
-    final LobbyServerProperties result = testObj.downloadAndParseRemoteFile(TestData.url,
-        fakeVersion, (a, b) -> TestData.lobbyServerProperties);
+      final LobbyServerProperties result = testObj.downloadAndParseRemoteFile(TestData.url,
+          TestData.version, (a, b) -> TestData.lobbyServerProperties);
 
-    assertThat(result, sameInstance(TestData.lobbyServerProperties));
-  }
+      assertThat(result, sameInstance(TestData.lobbyServerProperties));
+    }
 
-  private void givenHappyCase() throws Exception {
-    final File temp = File.createTempFile("temp", "tmp");
-    temp.deleteOnExit();
-    when(mockFileDownloader.download(TestData.url)).thenReturn(DownloadUtils.FileDownloadResult.success(temp));
-  }
-
-  @Test
-  public void throwsOnDownloadFailure() {
-    assertThrows(IOException.class, () -> {
+    private void givenHappyCase() throws Exception {
       final File temp = File.createTempFile("temp", "tmp");
       temp.deleteOnExit();
-      when(mockFileDownloader.download(TestData.url)).thenReturn(DownloadUtils.FileDownloadResult.FAILURE);
+      when(mockFileDownloader.download(TestData.url)).thenReturn(DownloadUtils.FileDownloadResult.success(temp));
+    }
 
-      testObj.downloadAndParseRemoteFile(TestData.url, fakeVersion, (a, b) -> TestData.lobbyServerProperties);
-    });
+    @Test
+    public void throwsOnDownloadFailure() {
+      assertThrows(IOException.class, () -> {
+        when(mockFileDownloader.download(TestData.url)).thenReturn(DownloadUtils.FileDownloadResult.FAILURE);
+
+        testObj.downloadAndParseRemoteFile(TestData.url, TestData.version, (a, b) -> TestData.lobbyServerProperties);
+      });
+    }
   }
 
+  @ExtendWith(MockitoExtension.class)
+  @Nested
+  public final class GetTestOverridePropertiesTest {
+    @Mock
+    private GameSetting testLobbyHostSetting;
+
+    @Mock
+    private GameSetting testLobbyPortSetting;
+
+    private Optional<LobbyServerProperties> result;
+
+    private void givenTestLobbyHostIsNotSet() {
+      when(testLobbyHostSetting.isSet()).thenReturn(false);
+    }
+
+    private void givenTestLobbyHostIsSet() {
+      givenTestLobbyHostIsSetTo("host");
+    }
+
+    private void givenTestLobbyHostIsSetTo(final String host) {
+      when(testLobbyHostSetting.isSet()).thenReturn(true);
+      when(testLobbyHostSetting.value()).thenReturn(host);
+    }
+
+    private void givenTestLobbyPortIsNotSet() {
+      when(testLobbyPortSetting.isSet()).thenReturn(false);
+    }
+
+    private void givenTestLobbyPortIsSet() {
+      givenTestLobbyPortIsSetTo(0);
+    }
+
+    private void givenTestLobbyPortIsSetTo(final int port) {
+      when(testLobbyPortSetting.isSet()).thenReturn(true);
+      when(testLobbyPortSetting.intValue()).thenReturn(port);
+    }
+
+    private void whenGetTestOverrideProperties() {
+      result = LobbyServerPropertiesFetcher.getTestOverrideProperties(testLobbyHostSetting, testLobbyPortSetting);
+    }
+
+    private void thenResultIsEmpty() {
+      assertThat(result, is(Optional.empty()));
+    }
+
+    private void thenResultHasHostAndPort(final String host, final int port) {
+      OptionalUtils.ifPresentOrElse(
+          result,
+          it -> {
+            assertThat(it.host, is(host));
+            assertThat(it.port, is(port));
+          },
+          () -> fail("expected non-empty properties, but was empty"));
+    }
+
+    @Test
+    public void shouldReturnEmptyWhenHostNotSetAndPortNotSet() {
+      givenTestLobbyHostIsNotSet();
+      givenTestLobbyPortIsNotSet();
+
+      whenGetTestOverrideProperties();
+
+      thenResultIsEmpty();
+    }
+
+    @Test
+    public void shouldReturnEmptyWhenHostSetAndPortNotSet() {
+      givenTestLobbyHostIsSet();
+      givenTestLobbyPortIsNotSet();
+
+      whenGetTestOverrideProperties();
+
+      thenResultIsEmpty();
+    }
+
+    @Test
+    public void shouldReturnEmptyWhenHostNotSetAndPortSet() {
+      givenTestLobbyHostIsNotSet();
+      givenTestLobbyPortIsSet();
+
+      whenGetTestOverrideProperties();
+
+      thenResultIsEmpty();
+    }
+
+    @Test
+    public void shouldReturnPropertiesWhenHostSetAndPortSet() {
+      givenTestLobbyHostIsSetTo("foo");
+      givenTestLobbyPortIsSetTo(4242);
+
+      whenGetTestOverrideProperties();
+
+      thenResultHasHostAndPort("foo", 4242);
+    }
+  }
 
   private interface TestData {
+    Version version = new Version("0.0.0.0");
     String url = "someUrl";
     LobbyServerProperties lobbyServerProperties = new LobbyServerProperties("host", 123);
   }
