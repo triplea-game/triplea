223c32ed06e7d4f8e2e5d444985d13a07e5a033c
RoiEX <8350879+RoiEXLab@users.noreply.github.com>
2018-01-23 09:25:02 +0100

    Simplify ComboProperty class (#2886)
    * Simplify class
    
    * Remove impossible case
    
    * simplify validate method

--- src/main/java/games/strategy/engine/data/properties/ComboProperty.java
+++ src/main/java/games/strategy/engine/data/properties/ComboProperty.java
@@ -20,36 +20,20 @@ public class ComboProperty<T> extends AEditableProperty {
   private T value;
 
   /**
-   * @param name
-   *        name of the property.
-   * @param defaultValue
-   *        default string value
-   * @param possibleValues
-   *        collection of values
+   * @param name name of the property.
+   * @param defaultValue default string value
+   * @param possibleValues collection of values
    */
-  public ComboProperty(final String name, final String description, final T defaultValue,
+  public ComboProperty(final String name,
+      final String description,
+      final T defaultValue,
       final Collection<T> possibleValues) {
-    this(name, description, defaultValue, possibleValues, false);
-  }
-
-  @SuppressWarnings("unchecked")
-  private ComboProperty(final String name, final String description, final T defaultValue,
-      final Collection<T> possibleValues, final boolean allowNone) {
     super(name, description);
-    if (!allowNone && !possibleValues.contains(defaultValue) && defaultValue == null) {
-      throw new IllegalStateException("possible values does not contain default");
-    } else if (allowNone && !possibleValues.contains(defaultValue) && !possibleValues.isEmpty()) {
-      value = possibleValues.iterator().next();
-    } else if (allowNone && !possibleValues.contains(defaultValue)) {
-      try {
-        value = (T) "";
-      } catch (final Exception e) {
-        value = null;
-      }
-    } else {
-      value = defaultValue;
-    }
     this.possibleValues = new ArrayList<>(possibleValues);
+    if (defaultValue == null && !possibleValues.contains(defaultValue)) {
+      throw new IllegalStateException("possible values does not contain default");
+    }
+    value = defaultValue;
   }
 
   @Override
@@ -77,18 +61,6 @@ public class ComboProperty<T> extends AEditableProperty {
 
   @Override
   public boolean validate(final Object value) {
-    if (possibleValues == null || possibleValues.isEmpty()) {
-      return false;
-    }
-    try {
-      if (possibleValues.contains(value)) {
-        return true;
-      }
-    } catch (final ClassCastException e) {
-      return false;
-    } catch (final NullPointerException e) {
-      return false;
-    }
-    return false;
+    return possibleValues != null && possibleValues.contains(value);
   }
 }
