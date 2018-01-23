fc3795ca443991acbb2f5adc1168d8e14071d59f
RoiEX <8350879+RoiEXLab@users.noreply.github.com>
2018-01-23 08:38:13 +0100

    Remove impossible case

--- src/main/java/games/strategy/engine/data/properties/ComboProperty.java
+++ src/main/java/games/strategy/engine/data/properties/ComboProperty.java
@@ -24,33 +24,16 @@ public class ComboProperty<T> extends AEditableProperty {
    * @param defaultValue default string value
    * @param possibleValues collection of values
    */
-  public ComboProperty(final String name, final String description, final T defaultValue,
+  public ComboProperty(final String name,
+      final String description,
+      final T defaultValue,
       final Collection<T> possibleValues) {
-    this(name, description, defaultValue, possibleValues, false);
-  }
-
-  @SuppressWarnings("unchecked")
-  private ComboProperty(final String name, final String description, final T defaultValue,
-      final Collection<T> possibleValues, final boolean allowNone) {
     super(name, description);
-    if (!allowNone) {
-      if (defaultValue == null && !possibleValues.contains(defaultValue)) {
-        throw new IllegalStateException("possible values does not contain default");
-      }
-    } else if (!possibleValues.contains(defaultValue)) {
-      if (!possibleValues.isEmpty()) {
-        value = possibleValues.iterator().next();
-      } else {
-        try {
-          value = (T) "";
-        } catch (final ClassCastException e) {
-          value = null;
-        }
-      }
-    } else {
-      value = defaultValue;
-    }
     this.possibleValues = new ArrayList<>(possibleValues);
+    if (defaultValue == null && !possibleValues.contains(defaultValue)) {
+      throw new IllegalStateException("possible values does not contain default");
+    }
+    value = defaultValue;
   }
 
   @Override
