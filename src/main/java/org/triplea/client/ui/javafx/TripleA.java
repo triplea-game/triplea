8a3ac8ae65ca76d2167ca231c04ab4064751a9de
RoiEX <8350879+RoiEXLab@users.noreply.github.com>
2018-01-25 23:42:48 +0100

    Use Borderless Fullscreen as only and default option

--- src/main/java/org/triplea/client/ui/javafx/TripleA.java
+++ src/main/java/org/triplea/client/ui/javafx/TripleA.java
@@ -12,6 +12,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.Tooltip;
 import javafx.scene.image.Image;
+import javafx.scene.input.KeyCombination;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
@@ -44,15 +45,17 @@ public class TripleA extends Application {
   public void start(final Stage stage) throws Exception {
     final FXMLLoader loader = FxmlManager.getLoader(getClass().getResource(FxmlManager.ROOT_CONTAINER.toString()));
     loader.setController(this);
-    final double widthOffsetCompensateValue = 20;
-    final double heightOffsetCompensateValue = 30;
-    // TODO make those values configurable
-    final Scene scene = new Scene(loader.load(), 960 + widthOffsetCompensateValue, 540 + heightOffsetCompensateValue);
-    Font.loadFont(TripleA.class.getResourceAsStream(FxmlManager.RELATIVE_FONT_PATH.toString()), 14);
+    final Scene scene = new Scene(loader.load());
     scene.getStylesheets().add(FxmlManager.STYLESHEET_MAIN.toString());
     mainMenu = addRootContent(new MainMenuPane(this));
-    stage.setMinHeight(scene.getHeight());
-    stage.setMinWidth(scene.getWidth());
+    setupStage(stage, scene);
+  }
+
+  private void setupStage(final Stage stage, final Scene scene) {
+    stage.setFullScreenExitKeyCombination(KeyCombination.NO_MATCH);
+    stage.setFullScreen(true);
+
+    Font.loadFont(TripleA.class.getResourceAsStream(FxmlManager.RELATIVE_FONT_PATH.toString()), 14);
     stage.setScene(scene);
     stage.getIcons().add(new Image(getClass().getResourceAsStream(FxmlManager.ICON_LOCATION.toString())));
     stage.setTitle("TripleA");
