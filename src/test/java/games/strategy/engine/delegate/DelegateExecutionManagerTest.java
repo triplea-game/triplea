bd069b9165c505b829d14bba6f89e380e758f33d
Steven Soloff <steven.soloff@gmail.com>
2018-02-06 12:13:33 -0500

    Add test to demonstrate concurrent delegate execution on different threads is permitted

--- src/test/java/games/strategy/engine/delegate/DelegateExecutionManagerTest.java
+++ src/test/java/games/strategy/engine/delegate/DelegateExecutionManagerTest.java
@@ -3,6 +3,8 @@ package games.strategy.engine.delegate;
 import static games.strategy.test.Assertions.assertNotThrows;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.util.concurrent.CountDownLatch;
+
 import org.junit.jupiter.api.Test;
 
 public final class DelegateExecutionManagerTest {
@@ -27,5 +29,30 @@ public final class DelegateExecutionManagerTest {
     // when: a second delegate is executed on the current thread while the first delegate is still running
     // then: an exception should be thrown to prevent the second delegate's execution
     assertThrows(IllegalStateException.class, delegateExecutionManager::enterDelegateExecution);
+  }
+
+  @Test
+  public void shouldAllowConcurrentExecutionOfDelegatesOnDifferentThreads() throws Exception {
+    // given: a delegate is executed on some thread
+    final CountDownLatch testCompleteLatch = new CountDownLatch(1);
+    final CountDownLatch delegate1RunningLatch = new CountDownLatch(1);
+    final Thread delegate1Thread = new Thread(() -> {
+      delegateExecutionManager.enterDelegateExecution();
+      delegate1RunningLatch.countDown();
+      try {
+        testCompleteLatch.await();
+      } catch (final InterruptedException e) {
+        Thread.currentThread().interrupt();
+      }
+    });
+    delegate1Thread.start();
+    delegate1RunningLatch.await();
+
+    // when: a second delegate is executed on a different thread while the first delegate is still running
+    // then: no exception should be thrown to prevent the second delegate's execution
+    assertNotThrows(delegateExecutionManager::enterDelegateExecution);
+
+    testCompleteLatch.countDown();
+    delegate1Thread.join();
   }
 }
