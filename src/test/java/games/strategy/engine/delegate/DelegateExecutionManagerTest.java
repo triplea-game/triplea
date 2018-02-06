9e1383032effe4eb4af7bb8bf4ceac2e5a557a87
Steven Soloff <steven.soloff@gmail.com>
2018-02-05 23:12:04 -0500

    Add test for regression introduced in #2944

---
+++ src/test/java/games/strategy/engine/delegate/DelegateExecutionManagerTest.java
@@ -0,0 +1,19 @@
+package games.strategy.engine.delegate;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+
+public final class DelegateExecutionManagerTest {
+  private final DelegateExecutionManager delegateExecutionManager = new DelegateExecutionManager();
+
+  @Test
+  public void enterDelegateExecution_ShouldThrowExceptionWhenThreadTriesToReacquireLock() {
+    delegateExecutionManager.enterDelegateExecution();
+    try {
+      assertThrows(IllegalStateException.class, delegateExecutionManager::enterDelegateExecution);
+    } finally {
+      delegateExecutionManager.leaveDelegateExecution();
+    }
+  }
+}
