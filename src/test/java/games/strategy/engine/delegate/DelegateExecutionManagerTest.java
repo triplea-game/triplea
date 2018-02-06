1e8acee752cb3c9d677779de08d3c352fbb3df51
Steven Soloff <steven.soloff@gmail.com>
2018-02-06 11:44:17 -0500

    Add test to demonstrate serial delegate execution is permitted

--- src/test/java/games/strategy/engine/delegate/DelegateExecutionManagerTest.java
+++ src/test/java/games/strategy/engine/delegate/DelegateExecutionManagerTest.java
@@ -1,5 +1,6 @@
 package games.strategy.engine.delegate;
 
+import static games.strategy.test.Assertions.assertNotThrows;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
@@ -8,12 +9,23 @@ public final class DelegateExecutionManagerTest {
   private final DelegateExecutionManager delegateExecutionManager = new DelegateExecutionManager();
 
   @Test
-  public void enterDelegateExecution_ShouldThrowExceptionWhenDelegateExecutingOnCurrentThread() {
+  public void shouldAllowSerialExecutionOfDelegatesOnSameThread() {
+    // given: a delegate is executed on the current thread and runs to completion
     delegateExecutionManager.enterDelegateExecution();
-    try {
-      assertThrows(IllegalStateException.class, delegateExecutionManager::enterDelegateExecution);
-    } finally {
-      delegateExecutionManager.leaveDelegateExecution();
-    }
+    delegateExecutionManager.leaveDelegateExecution();
+
+    // when: a second delegate is executed on the current thread after the first delegate's execution ends
+    // then: no exception should be thrown to prevent the second delegate's execution
+    assertNotThrows(delegateExecutionManager::enterDelegateExecution);
+  }
+
+  @Test
+  public void shouldNotAllowNestedExecutionOfDelegatesOnSameThread() {
+    // given: a delegate is executed on the current thread
+    delegateExecutionManager.enterDelegateExecution();
+
+    // when: a second delegate is executed on the current thread while the first delegate is still running
+    // then: an exception should be thrown to prevent the second delegate's execution
+    assertThrows(IllegalStateException.class, delegateExecutionManager::enterDelegateExecution);
   }
 }
