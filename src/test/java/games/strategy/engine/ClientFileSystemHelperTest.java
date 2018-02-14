278c6a0acfe73be8ee4183af7cecb3eba12e1436
Steven Soloff <steven.soloff@gmail.com>
2018-02-14 00:24:33 -0500

    Simplify ClientFileSystemHelper#getRootFolder()

--- src/test/java/games/strategy/engine/ClientFileSystemHelperTest.java
+++ src/test/java/games/strategy/engine/ClientFileSystemHelperTest.java
@@ -2,18 +2,70 @@ package games.strategy.engine;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 
+import java.io.File;
+import java.io.IOException;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.Spy;
 
 import com.example.mockito.MockitoExtension;
 
 import games.strategy.triplea.settings.GameSetting;
 
 public final class ClientFileSystemHelperTest {
+  @ExtendWith(MockitoExtension.class)
+  @Nested
+  public final class GetFolderContainingFileWithNameTest {
+    @Spy
+    private final File file = new File("filename.ext");
+
+    @Spy
+    private final File parentFolder = new File("parent");
+
+    @Spy
+    private final File startFolder = new File("start");
+
+    private File getFolderContainingFileWithName() throws Exception {
+      return ClientFileSystemHelper.getFolderContainingFileWithName(startFolder, file.getName());
+    }
+
+    @BeforeEach
+    public void setUp() {
+      when(file.isFile()).thenReturn(true);
+      when(startFolder.getParentFile()).thenReturn(parentFolder);
+    }
+
+    @Test
+    public void shouldReturnStartFolderWhenStartFolderContainsFile() throws Exception {
+      when(startFolder.listFiles()).thenReturn(new File[] {file});
+
+      assertThat(getFolderContainingFileWithName(), is(startFolder));
+    }
+
+    @Test
+    public void shouldReturnAncestorFolderWhenAncestorFolderContainsFile() throws Exception {
+      when(startFolder.listFiles()).thenReturn(new File[0]);
+      when(parentFolder.listFiles()).thenReturn(new File[] {file});
+
+      assertThat(getFolderContainingFileWithName(), is(parentFolder));
+    }
+
+    @Test
+    public void shouldThrowExceptionWhenNoFolderContainsFile() {
+      when(startFolder.listFiles()).thenReturn(new File[0]);
+      when(parentFolder.listFiles()).thenReturn(new File[0]);
+
+      assertThrows(IOException.class, () -> getFolderContainingFileWithName());
+    }
+  }
+
   @ExtendWith(MockitoExtension.class)
   @Nested
   public final class GetUserMapsFolderPathTest {
