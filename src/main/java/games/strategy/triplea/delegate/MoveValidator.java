ea331b03ee885be46cb0584d0cfa9fca9576d59a
Ron <ron.murhammer@gmail.com>
2018-02-13 21:27:21 -0600

    Add check so disabled units can't move

--- src/main/java/games/strategy/triplea/delegate/MoveValidator.java
+++ src/main/java/games/strategy/triplea/delegate/MoveValidator.java
@@ -179,6 +179,10 @@ public class MoveValidator {
     for (final Unit unit : units) {
       if (TripleAUnit.get(unit).getSubmerged()) {
         result.addDisallowedUnit("Cannot move submerged units", unit);
+        continue;
+      }
+      if (Matches.unitIsDisabled().test(unit)) {
+        result.addDisallowedUnit("Cannot move disabled units", unit);
       }
     }
     // make sure all units are actually in the start territory
