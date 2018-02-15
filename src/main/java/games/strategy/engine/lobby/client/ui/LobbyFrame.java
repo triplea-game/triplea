b532f58ba3f91ab242abbb8cd3b2f676c8bfe76b
RoiEX <8350879+RoiEXLab@users.noreply.github.com>
2018-02-15 18:03:11 +0100

    Fix Lobby divider location on resize (#2998)
    * Fix Lobby divider location on resize
    
    * Divide chat panel from game table more fairly on resize

--- src/main/java/games/strategy/engine/lobby/client/ui/LobbyFrame.java
+++ src/main/java/games/strategy/engine/lobby/client/ui/LobbyFrame.java
@@ -71,13 +71,14 @@ public class LobbyFrame extends JFrame {
     leftSplit.setOrientation(JSplitPane.VERTICAL_SPLIT);
     leftSplit.setTopComponent(gamePanel);
     leftSplit.setBottomComponent(chatMessagePanel);
-    leftSplit.setResizeWeight(0.8);
+    leftSplit.setResizeWeight(0.5);
     gamePanel.setPreferredSize(new Dimension(700, 200));
     chatMessagePanel.setPreferredSize(new Dimension(700, 400));
     final JSplitPane mainSplit = new JSplitPane();
     mainSplit.setOrientation(JSplitPane.HORIZONTAL_SPLIT);
     mainSplit.setLeftComponent(leftSplit);
     mainSplit.setRightComponent(chatPlayers);
+    mainSplit.setResizeWeight(1);
     add(mainSplit, BorderLayout.CENTER);
     pack();
     chatMessagePanel.requestFocusInWindow();
