a3e30423ab089aeef5e2f0a2e39150ccb19cd1ec
Steven Soloff <steven.soloff@gmail.com>
2018-02-15 21:57:52 -0500

    Consolidate MacFinder tests

--- src/test/java/games/strategy/net/MacFinderTest.java
+++ src/test/java/games/strategy/net/MacFinderTest.java
@@ -5,6 +5,8 @@ import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.startsWith;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.util.Arrays;
+
 import org.junit.jupiter.api.Test;
 
 public final class MacFinderTest {
@@ -28,19 +30,18 @@ public final class MacFinderTest {
   }
 
   @Test
-  public void isValidHashedMacAddress_ShouldReturnFalseWhenLengthIsInvalid() {
-    assertThat(MacFinder.isValidHashedMacAddress("$1$MH$ABCDWXYZabcdwxyz0189."), is(false));
-    assertThat(MacFinder.isValidHashedMacAddress("$1$MH$ABCDWXYZabcdwxyz0189./1"), is(false));
+  public void isValidHashedMacAddress_ShouldReturnFalseWhenNotValidMd5CryptedValue() {
+    Arrays.asList(
+        "$1$MH$ABCDWXYZabcdwxyz0189.",
+        "$1$MH$ABCDWXYZabcdwxyz0189./1",
+        "1$MH$ABCDWXYZabcdwxyz0189./1",
+        "$1$MH$ABCDWXYZabcdwxyz0189._")
+        .forEach(hashedMacAddress -> assertThat(MacFinder.isValidHashedMacAddress(hashedMacAddress), is(false)));
   }
 
   @Test
-  public void isValidHashedMacAddress_ShouldReturnFalseWhenDoesNotHaveExpectedPrefix() {
-    assertThat(MacFinder.isValidHashedMacAddress("1$MH$ABCDWXYZabcdwxyz0189./1"), is(false));
-  }
-
-  @Test
-  public void isValidHashedMacAddress_ShouldReturnFalseWhenContainsInvalidCharacters() {
-    assertThat(MacFinder.isValidHashedMacAddress("$1$MH$ABCDWXYZabcdwxyz0189._"), is(false));
+  public void isValidHashedMacAddress_ShouldReturnFalseWhenDoesNotHaveExpectedSalt() {
+    assertThat(MacFinder.isValidHashedMacAddress("$1$SALT$ABCDWXYZabcdwxyz0189./"), is(false));
   }
 
   @Test
