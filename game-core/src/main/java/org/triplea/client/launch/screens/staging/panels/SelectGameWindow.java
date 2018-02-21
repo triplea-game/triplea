da0a683cf1fca7db0593c3ad62abe1eb7e58c0a4
Steven Soloff <steven.soloff@gmail.com>
2018-02-20 23:45:42 -0500

    Use Interruptibles wrappers where possible

--- game-core/src/main/java/org/triplea/client/launch/screens/staging/panels/SelectGameWindow.java
+++ game-core/src/main/java/org/triplea/client/launch/screens/staging/panels/SelectGameWindow.java
@@ -10,6 +10,7 @@ import games.strategy.engine.data.GameParseException;
 import games.strategy.engine.framework.ui.GameChooser;
 import games.strategy.engine.framework.ui.GameChooserEntry;
 import games.strategy.triplea.settings.ClientSetting;
+import games.strategy.util.Interruptibles;
 
 /**
  * Opens a dialog window where users can select from the available games they have installed.
@@ -22,23 +23,18 @@ public class SelectGameWindow {
    * the Runnable will open the select map window.
    */
   public static Runnable openSelectGameWindow(final LaunchScreen previousScreen, final StagingScreen stagingScreen) {
-    return () -> {
-      try {
-        final GameChooserEntry entry;
-        entry = GameChooser.chooseGame(
-            JOptionPane.getFrameForComponent(null),
-            ClientSetting.SELECTED_GAME_LOCATION.value());
-        if (entry != null) {
-          ClientSetting.SELECTED_GAME_LOCATION.save(entry.getLocation());
-          try {
-            LaunchScreenWindow.draw(previousScreen, stagingScreen, entry.fullyParseGameData());
-          } catch (final GameParseException e) {
-            throw new IllegalStateException(String.format("Could not parse: %s", entry.toString()), e);
-          }
+    return () -> Interruptibles.await(() -> {
+      final GameChooserEntry entry = GameChooser.chooseGame(
+          JOptionPane.getFrameForComponent(null),
+          ClientSetting.SELECTED_GAME_LOCATION.value());
+      if (entry != null) {
+        ClientSetting.SELECTED_GAME_LOCATION.save(entry.getLocation());
+        try {
+          LaunchScreenWindow.draw(previousScreen, stagingScreen, entry.fullyParseGameData());
+        } catch (final GameParseException e) {
+          throw new IllegalStateException(String.format("Could not parse: %s", entry.toString()), e);
         }
-      } catch (final InterruptedException e) {
-        Thread.currentThread().interrupt();
       }
-    };
+    });
   }
 }
