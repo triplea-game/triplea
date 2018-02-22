944bbd64a80e99c2dcf7b741271573bce3cc1b98
Dan Van Atta <dhvatta@gmail.com>
2018-02-21 22:14:52 -0800

    Use builder() for FightBattleDetails value object

--- game-core/src/main/java/games/strategy/triplea/delegate/dataObjects/FightBattleDetails.java
+++ game-core/src/main/java/games/strategy/triplea/delegate/dataObjects/FightBattleDetails.java
@@ -2,27 +2,16 @@ package games.strategy.triplea.delegate.dataObjects;
 
 import games.strategy.engine.data.Territory;
 import games.strategy.triplea.delegate.IBattle.BattleType;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
 
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
 public class FightBattleDetails {
-  private final boolean bombingRaid;
-  private final BattleType type;
   private final Territory where;
-
-  public FightBattleDetails(final Territory where, final boolean bombingRaid, final BattleType type) {
-    this.bombingRaid = bombingRaid;
-    this.where = where;
-    this.type = type;
-  }
-
-  public boolean isBombingRaid() {
-    return bombingRaid;
-  }
-
-  public Territory getWhere() {
-    return where;
-  }
-
-  public BattleType getBattleType() {
-    return type;
-  }
+  private final boolean bombingRaid;
+  private final BattleType battleType;
 }
