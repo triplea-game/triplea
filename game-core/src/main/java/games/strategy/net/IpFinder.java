29404bda7029aa716c5a758523751cb4da58c96e
RoiEX <8350879+RoiEXLab@users.noreply.github.com>
2018-02-24 20:55:07 +0100

    Remove unused import

--- game-core/src/main/java/games/strategy/net/IpFinder.java
+++ game-core/src/main/java/games/strategy/net/IpFinder.java
@@ -9,7 +9,6 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.Enumeration;
-import java.util.stream.Collectors;
 
 import games.strategy.util.Util;
 
