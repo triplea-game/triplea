843f271974de957c49a7e889313cb976b79433bb
Ron <ron.murhammer@gmail.com>
2018-02-24 00:43:56 -0600

    Create AbstractImageFactory to hold common methods

--- game-core/src/main/java/games/strategy/triplea/image/ResourceImageFactory.java
+++ game-core/src/main/java/games/strategy/triplea/image/ResourceImageFactory.java
@@ -1,62 +1,21 @@
 package games.strategy.triplea.image;
 
-import java.awt.Image;
-import java.awt.Toolkit;
-import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
-
-import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 
 import games.strategy.engine.data.Resource;
-import games.strategy.triplea.ResourceLoader;
-import games.strategy.ui.Util;
 
-public class ResourceImageFactory {
+/**
+ * Used to manage resource images.
+ */
+public class ResourceImageFactory extends AbstractImageFactory {
 
   private static final String FILE_NAME_BASE = "resources/";
-  private final Map<String, ImageIcon> icons = new HashMap<>();
-  private ResourceLoader resourceLoader;
 
   public ResourceImageFactory() {}
 
-  public void setResourceLoader(final ResourceLoader loader) {
-    resourceLoader = loader;
-    clearImageCache();
-  }
-
-  // Clear the image and icon cache
-  private void clearImageCache() {
-    icons.clear();
-  }
-
-  private Image getBaseImage(final String baseImageName) {
-    // URL uses '/' not '\'
-    final String fileName = FILE_NAME_BASE + baseImageName + ".png";
-    final URL url = resourceLoader.getResource(fileName);
-    if (url == null) {
-      throw new IllegalStateException("Cant load: " + baseImageName + "  looking in: " + fileName);
-    }
-    final Image image = Toolkit.getDefaultToolkit().getImage(url);
-    Util.ensureImageLoaded(image);
-    return image;
-  }
-
-  /**
-   * Return a icon image.
-   *
-   * @throws IllegalStateException if image can't be found
-   */
-  public ImageIcon getIcon(final Resource type, final boolean large) {
-    final String fullName = type.getName() + (large ? "_large" : "");
-    if (icons.containsKey(fullName)) {
-      return icons.get(fullName);
-    }
-    final Image img = getBaseImage(fullName);
-    final ImageIcon icon = new ImageIcon(img);
-    icons.put(fullName, icon);
-    return icon;
+  @Override
+  protected String getFileNameBase() {
+    return FILE_NAME_BASE;
   }
 
   /**
--- game-core/src/main/java/games/strategy/triplea/image/TerritoryEffectImageFactory.java
+++ game-core/src/main/java/games/strategy/triplea/image/TerritoryEffectImageFactory.java
@@ -1,63 +1,17 @@
 package games.strategy.triplea.image;
 
-import java.awt.Image;
-import java.awt.Toolkit;
-import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
-
-import javax.swing.ImageIcon;
-
-import games.strategy.engine.data.TerritoryEffect;
-import games.strategy.triplea.ResourceLoader;
-import games.strategy.ui.Util;
-
 /**
  * Used to manage territory effect images.
  */
-public class TerritoryEffectImageFactory {
+public class TerritoryEffectImageFactory extends AbstractImageFactory {
 
-  private static final String FILE_NAME_BASE = "territoryEffects/";
-  private final Map<String, ImageIcon> icons = new HashMap<>();
-  private ResourceLoader resourceLoader;
+  static final String FILE_NAME_BASE = "territoryEffects/";
 
   public TerritoryEffectImageFactory() {}
 
-  public void setResourceLoader(final ResourceLoader loader) {
-    resourceLoader = loader;
-    clearImageCache();
-  }
-
-  private void clearImageCache() {
-    icons.clear();
-  }
-
-  private Image getBaseImage(final String baseImageName) {
-    // URL uses '/' not '\'
-    final String fileName = FILE_NAME_BASE + baseImageName + ".png";
-    final URL url = resourceLoader.getResource(fileName);
-    if (url == null) {
-      throw new IllegalStateException("Cant load: " + baseImageName + "  looking in: " + fileName);
-    }
-    final Image image = Toolkit.getDefaultToolkit().getImage(url);
-    Util.ensureImageLoaded(image);
-    return image;
-  }
-
-  /**
-   * Return a icon image.
-   *
-   * @throws IllegalStateException if image can't be found
-   */
-  public ImageIcon getIcon(final TerritoryEffect type, final boolean large) {
-    final String fullName = type.getName() + (large ? "_large" : "");
-    if (icons.containsKey(fullName)) {
-      return icons.get(fullName);
-    }
-    final Image img = getBaseImage(fullName);
-    final ImageIcon icon = new ImageIcon(img);
-    icons.put(fullName, icon);
-    return icon;
+  @Override
+  protected String getFileNameBase() {
+    return FILE_NAME_BASE;
   }
 
 }
