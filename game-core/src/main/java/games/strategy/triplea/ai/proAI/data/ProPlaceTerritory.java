77730789657f84b51cdc28a073ba32b918d6b04f
Dan Van Atta <dhvatta@gmail.com>
2018-02-26 22:37:48 -0800

    Simplify ProPlaceTerritory

--- game-core/src/main/java/games/strategy/triplea/ai/proAI/data/ProPlaceTerritory.java
+++ game-core/src/main/java/games/strategy/triplea/ai/proAI/data/ProPlaceTerritory.java
@@ -2,49 +2,31 @@ package games.strategy.triplea.ai.proAI.data;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 import games.strategy.engine.data.Territory;
 import games.strategy.engine.data.Unit;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
+@EqualsAndHashCode(of = "territory")
 @Getter
 @Setter
 public class ProPlaceTerritory {
   private final Territory territory;
-  private List<Unit> defendingUnits;
-  private ProBattleResult minBattleResult;
-  private double defenseValue;
-  private double strategicValue;
-  private final List<Unit> placeUnits;
-  private boolean canHold;
+  private final List<Unit> placeUnits = new ArrayList<>();
+  private List<Unit> defendingUnits = new ArrayList<>();
+  private ProBattleResult minBattleResult = new ProBattleResult();
+  private double defenseValue = 0;
+  private double strategicValue = 0;
+  private boolean canHold = true;
 
   ProPlaceTerritory(final Territory territory) {
     this.territory = territory;
-    defendingUnits = new ArrayList<>();
-    minBattleResult = new ProBattleResult();
-    defenseValue = 0;
-    strategicValue = 0;
-    placeUnits = new ArrayList<>();
-    canHold = true;
   }
 
   @Override
   public String toString() {
     return territory.toString();
-  }
-
-  @Override
-  public boolean equals(final Object o) {
-    if (o instanceof ProPlaceTerritory) {
-      return ((ProPlaceTerritory) o).getTerritory().equals(territory);
-    }
-    return false;
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hashCode(territory);
   }
 }
