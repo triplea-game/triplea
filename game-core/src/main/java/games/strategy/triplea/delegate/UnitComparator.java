41d252b2f1b997cba11abaaf4f306488e032740b
RoiEX <8350879+RoiEXLab@users.noreply.github.com>
2018-03-06 19:14:46 +0100

    Simplify comparator (#3238)

--- game-core/src/main/java/games/strategy/triplea/delegate/UnitComparator.java
+++ game-core/src/main/java/games/strategy/triplea/delegate/UnitComparator.java
@@ -109,10 +109,9 @@ public class UnitComparator {
    */
   public static Comparator<Unit> getUnloadableUnitsComparator(final List<Unit> units, final Route route,
       final PlayerID player) {
-    return Comparator
-        .<Unit, TripleAUnit>comparing(
-            unit -> TripleAUnit.get(unit).getTransportedBy(),
-            Comparator.nullsLast(getUnloadableTransportsComparator(units, route, player, false)))
+    return Comparator.comparing(TripleAUnit::get,
+        Comparator.comparing(TripleAUnit::getTransportedBy,
+            Comparator.nullsLast(getUnloadableTransportsComparator(units, route, player, false))))
         .thenComparing(getMovableUnitsComparator(units, route));
   }
 
