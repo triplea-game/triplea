3994052702ae1ac101cc5d6076e7a24febea305d
Steven Soloff <steven.soloff@gmail.com>
2018-03-06 12:26:36 -0500

    Make MemoryPropertyReader mutable
    Some tests may require the ability to change the property source
    out-of-band while the test is running.

--- game-core/src/test/java/games/strategy/engine/config/MemoryPropertyReader.java
+++ game-core/src/test/java/games/strategy/engine/config/MemoryPropertyReader.java
@@ -1,28 +1,52 @@
 package games.strategy.engine.config;
 
+import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkNotNull;
 
-import java.util.HashMap;
+import java.util.Collections;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 import javax.annotation.Nullable;
-import javax.annotation.concurrent.Immutable;
+import javax.annotation.concurrent.ThreadSafe;
 
 /**
  * Implementation of {@link PropertyReader} that uses a memory store as the property source.
  */
-@Immutable
+@ThreadSafe
 public final class MemoryPropertyReader extends AbstractPropertyReader {
   private final Map<String, String> properties;
 
+  public MemoryPropertyReader() {
+    this(Collections.emptyMap());
+  }
+
+  /**
+   * @throws IllegalArgumentException If {@code properties} contains a {@code null} key or value.
+   */
   public MemoryPropertyReader(final Map<String, String> properties) {
     checkNotNull(properties);
+    checkArgument(!properties.containsKey(null), "properties must not contain a null key");
+    checkArgument(!properties.containsValue(null), "properties must not contain a null value");
 
-    this.properties = new HashMap<>(properties);
+    this.properties = new ConcurrentHashMap<>(properties);
   }
 
   @Override
   protected @Nullable String readPropertyInternal(final String key) {
     return properties.get(key);
+  }
+
+  /**
+   * Sets the value of the property with the specified key.
+   *
+   * @param key The property key.
+   * @param value The property value.
+   */
+  public void setProperty(final String key, final String value) {
+    checkNotNull(key);
+    checkNotNull(value);
+
+    properties.put(key, value);
   }
 }
