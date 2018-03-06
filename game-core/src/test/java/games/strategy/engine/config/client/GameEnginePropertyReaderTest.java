33788ec23ec0792cb2b7ff096aaee726f0317d4f
Steven Soloff <steven.soloff@gmail.com>
2018-03-06 13:16:12 -0500

    Make MemoryPropertyReader mutable (#3241)
    Some tests may require the ability to change the property source
    out-of-band while the test is running.

--- game-core/src/test/java/games/strategy/engine/config/client/GameEnginePropertyReaderTest.java
+++ game-core/src/test/java/games/strategy/engine/config/client/GameEnginePropertyReaderTest.java
@@ -3,23 +3,19 @@ package games.strategy.engine.config.client;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
-import java.util.Collections;
-
 import org.junit.jupiter.api.Test;
 
 import games.strategy.engine.config.MemoryPropertyReader;
 import games.strategy.engine.config.client.GameEnginePropertyReader.PropertyKeys;
 import games.strategy.util.Version;
 
-public class GameEnginePropertyReaderTest {
-  private static GameEnginePropertyReader newGameEnginePropertyReader(final String key, final String value) {
-    return new GameEnginePropertyReader(new MemoryPropertyReader(Collections.singletonMap(key, value)));
-  }
+public final class GameEnginePropertyReaderTest {
+  private final MemoryPropertyReader memoryPropertyReader = new MemoryPropertyReader();
+  private final GameEnginePropertyReader gameEnginePropertyReader = new GameEnginePropertyReader(memoryPropertyReader);
 
   @Test
   public void engineVersion() {
-    final GameEnginePropertyReader gameEnginePropertyReader =
-        newGameEnginePropertyReader(PropertyKeys.ENGINE_VERSION, "1.0.1.3");
+    memoryPropertyReader.setProperty(PropertyKeys.ENGINE_VERSION, "1.0.1.3");
 
     assertThat(gameEnginePropertyReader.getEngineVersion(), is(new Version(1, 0, 1, 3)));
   }
--- game-core/src/test/java/games/strategy/engine/config/lobby/LobbyPropertyReaderTest.java
+++ game-core/src/test/java/games/strategy/engine/config/lobby/LobbyPropertyReaderTest.java
@@ -4,8 +4,6 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.emptyString;
 import static org.hamcrest.Matchers.is;
 
-import java.util.Collections;
-
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
@@ -14,23 +12,22 @@ import games.strategy.engine.config.lobby.LobbyPropertyReader.DefaultValues;
 import games.strategy.engine.config.lobby.LobbyPropertyReader.PropertyKeys;
 
 public final class LobbyPropertyReaderTest {
-  private static LobbyPropertyReader newLobbyPropertyReader(final String key, final String value) {
-    return new LobbyPropertyReader(new MemoryPropertyReader(Collections.singletonMap(key, value)));
-  }
+  private final MemoryPropertyReader memoryPropertyReader = new MemoryPropertyReader();
+  private final LobbyPropertyReader lobbyPropertyReader = new LobbyPropertyReader(memoryPropertyReader);
 
   @Nested
   public final class GetPortTest {
     @Test
     public void shouldReturnValueWhenPresent() {
       final int value = 100;
-      final LobbyPropertyReader lobbyPropertyReader = newLobbyPropertyReader(PropertyKeys.PORT, String.valueOf(value));
+      memoryPropertyReader.setProperty(PropertyKeys.PORT, String.valueOf(value));
 
       assertThat(lobbyPropertyReader.getPort(), is(value));
     }
 
     @Test
     public void shouldReturnDefaultValueWhenAbsent() {
-      final LobbyPropertyReader lobbyPropertyReader = newLobbyPropertyReader(PropertyKeys.PORT, "");
+      memoryPropertyReader.setProperty(PropertyKeys.PORT, "");
 
       assertThat(lobbyPropertyReader.getPort(), is(DefaultValues.PORT));
     }
@@ -41,14 +38,14 @@ public final class LobbyPropertyReaderTest {
     @Test
     public void shouldReturnValueWhenPresent() {
       final String value = "database";
-      final LobbyPropertyReader lobbyPropertyReader = newLobbyPropertyReader(PropertyKeys.POSTGRES_DATABASE, value);
+      memoryPropertyReader.setProperty(PropertyKeys.POSTGRES_DATABASE, value);
 
       assertThat(lobbyPropertyReader.getPostgresDatabase(), is(value));
     }
 
     @Test
     public void shouldReturnDefaultValueWhenAbsent() {
-      final LobbyPropertyReader lobbyPropertyReader = newLobbyPropertyReader(PropertyKeys.POSTGRES_DATABASE, "");
+      memoryPropertyReader.setProperty(PropertyKeys.POSTGRES_DATABASE, "");
 
       assertThat(lobbyPropertyReader.getPostgresDatabase(), is(DefaultValues.POSTGRES_DATABASE));
     }
@@ -59,14 +56,14 @@ public final class LobbyPropertyReaderTest {
     @Test
     public void shouldReturnValueWhenPresent() {
       final String value = "myhost.mydomain";
-      final LobbyPropertyReader lobbyPropertyReader = newLobbyPropertyReader(PropertyKeys.POSTGRES_HOST, value);
+      memoryPropertyReader.setProperty(PropertyKeys.POSTGRES_HOST, value);
 
       assertThat(lobbyPropertyReader.getPostgresHost(), is(value));
     }
 
     @Test
     public void shouldReturnDefaultValueWhenAbsent() {
-      final LobbyPropertyReader lobbyPropertyReader = newLobbyPropertyReader(PropertyKeys.POSTGRES_HOST, "");
+      memoryPropertyReader.setProperty(PropertyKeys.POSTGRES_HOST, "");
 
       assertThat(lobbyPropertyReader.getPostgresHost(), is(DefaultValues.POSTGRES_HOST));
     }
@@ -77,14 +74,14 @@ public final class LobbyPropertyReaderTest {
     @Test
     public void shouldReturnValueWhenPresent() {
       final String value = "funnyPasssword";
-      final LobbyPropertyReader lobbyPropertyReader = newLobbyPropertyReader(PropertyKeys.POSTGRES_PASSWORD, value);
+      memoryPropertyReader.setProperty(PropertyKeys.POSTGRES_PASSWORD, value);
 
       assertThat(lobbyPropertyReader.getPostgresPassword(), is(value));
     }
 
     @Test
     public void shouldReturnEmptyStringWhenAbsent() {
-      final LobbyPropertyReader lobbyPropertyReader = newLobbyPropertyReader(PropertyKeys.POSTGRES_PASSWORD, "");
+      memoryPropertyReader.setProperty(PropertyKeys.POSTGRES_PASSWORD, "");
 
       assertThat(lobbyPropertyReader.getPostgresPassword(), is(emptyString()));
     }
@@ -95,15 +92,14 @@ public final class LobbyPropertyReaderTest {
     @Test
     public void shouldReturnValueWhenPresent() {
       final int value = 1234;
-      final LobbyPropertyReader lobbyPropertyReader =
-          newLobbyPropertyReader(PropertyKeys.POSTGRES_PORT, String.valueOf(value));
+      memoryPropertyReader.setProperty(PropertyKeys.POSTGRES_PORT, String.valueOf(value));
 
       assertThat(lobbyPropertyReader.getPostgresPort(), is(value));
     }
 
     @Test
     public void shouldReturnDefaultValueWhenAbsent() {
-      final LobbyPropertyReader lobbyPropertyReader = newLobbyPropertyReader(PropertyKeys.POSTGRES_PORT, "");
+      memoryPropertyReader.setProperty(PropertyKeys.POSTGRES_PORT, "");
 
       assertThat(lobbyPropertyReader.getPostgresPort(), is(DefaultValues.POSTGRES_PORT));
     }
@@ -114,14 +110,14 @@ public final class LobbyPropertyReaderTest {
     @Test
     public void shouldReturnValueWhenPresent() {
       final String value = "funnyName";
-      final LobbyPropertyReader lobbyPropertyReader = newLobbyPropertyReader(PropertyKeys.POSTGRES_USER, value);
+      memoryPropertyReader.setProperty(PropertyKeys.POSTGRES_USER, value);
 
       assertThat(lobbyPropertyReader.getPostgresUser(), is(value));
     }
 
     @Test
     public void shouldReturnEmptyStringWhenAbsent() {
-      final LobbyPropertyReader lobbyPropertyReader = newLobbyPropertyReader(PropertyKeys.POSTGRES_USER, "");
+      memoryPropertyReader.setProperty(PropertyKeys.POSTGRES_USER, "");
 
       assertThat(lobbyPropertyReader.getPostgresUser(), is(emptyString()));
     }
@@ -132,15 +128,14 @@ public final class LobbyPropertyReaderTest {
     @Test
     public void shouldReturnValueWhenPresent() {
       final boolean value = !DefaultValues.MAINTENANCE_MODE;
-      final LobbyPropertyReader lobbyPropertyReader =
-          newLobbyPropertyReader(PropertyKeys.MAINTENANCE_MODE, String.valueOf(value));
+      memoryPropertyReader.setProperty(PropertyKeys.MAINTENANCE_MODE, String.valueOf(value));
 
       assertThat(lobbyPropertyReader.isMaintenanceMode(), is(value));
     }
 
     @Test
     public void shouldReturnDefaultValueWhenAbsent() {
-      final LobbyPropertyReader lobbyPropertyReader = newLobbyPropertyReader(PropertyKeys.MAINTENANCE_MODE, "");
+      memoryPropertyReader.setProperty(PropertyKeys.MAINTENANCE_MODE, "");
 
       assertThat(lobbyPropertyReader.isMaintenanceMode(), is(DefaultValues.MAINTENANCE_MODE));
     }
