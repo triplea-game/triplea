f87f7523e336fb2d642ff9d33e8fa972b90d518d
Dan Van Atta <dhvatta@gmail.com>
2018-03-14 18:59:00 -0700

    Create lobby sub-project (#3232)
    * Fork a very early 'lobby' sub-project with build file and properties file
    
    * Move db migrations to lobby config and organize lobby config folder
    
    * Depend on game-core project from lobby properly
    
    * Move Lobby main method to lobby sub-project
    
    * Fix lobby release task
    
    * Get lobby version from env variable, update travis artifact path
    
    * Fix lobby checkstyle, add a javadoc
    
    * Remove move 'lobby/config' folder, move contents to 'lobby/'
    
    * Add a readme for lobby sub-project
    
    * Remove lobby sections from game-core build file, cleanup
    
    * Move a copy of the lobby.properties file back to game-core
    
    * Update path to DB migration files for travis
    
    * Fix out of date game-core build reference to lobby server zip, that was moved to lobby sub-project

---
+++ lobby/src/main/java/games/strategy/engine/lobby/server/LobbyRunner.java
@@ -0,0 +1,29 @@
+package games.strategy.engine.lobby.server;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import games.strategy.sound.ClipPlayer;
+
+/**
+ * A 'main' class to launch the lobby server.
+ */
+public class LobbyRunner {
+  private static final Logger logger = Logger.getLogger(LobbyServer.class.getName());
+
+  /**
+   * Launches a lobby instance.
+   * Lobby stays running until the process is killed or the lobby is shutdown.
+   */
+  public static void main(final String[] args) {
+    try {
+      ClipPlayer.setBeSilentInPreferencesWithoutAffectingCurrent(true);
+      final int port = LobbyContext.lobbyPropertyReader().getPort();
+      logger.info("Trying to listen on port:" + port);
+      new LobbyServer(port);
+      logger.info("Lobby started");
+    } catch (final Exception ex) {
+      logger.log(Level.SEVERE, ex.toString(), ex);
+    }
+  }
+}
