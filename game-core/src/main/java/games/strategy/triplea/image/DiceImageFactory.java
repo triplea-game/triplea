bb9872e2103879f4df72b75dda5a66ec670f8770
RoiEX <8350879+RoiEXLab@users.noreply.github.com>
2018-04-20 17:50:21 +0200

    Refactor class

--- game-core/src/main/java/games/strategy/triplea/image/DiceImageFactory.java
+++ game-core/src/main/java/games/strategy/triplea/image/DiceImageFactory.java
@@ -8,9 +8,12 @@ import java.awt.Image;
 import java.awt.RenderingHints;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
+
+import com.google.common.collect.ImmutableMap;
 
 import games.strategy.triplea.ResourceLoader;
 import games.strategy.triplea.delegate.Die;
@@ -25,7 +28,7 @@ public class DiceImageFactory {
   private final int diceSides;
   private final ResourceLoader resourceLoader;
   private static final Color IGNORED = new Color(100, 100, 100, 200);
-  // maps Integer -> Image
+
   private final Map<Integer, Image> images = new HashMap<>();
   private final Map<Integer, Image> imagesHit = new HashMap<>();
   private final Map<Integer, Image> imagesIgnored = new HashMap<>();
@@ -42,64 +45,67 @@ public class DiceImageFactory {
   private void generateDice(final int pipSize, final Color color, final Map<Integer, Image> images) {
     final ImageFactory imageFactory = new ImageFactory();
     imageFactory.setResourceLoader(resourceLoader);
-    for (int i = 0; i <= diceSides; i++) {
-      Image img = null;
-      if (resourceLoader != null) {
-        if (Color.BLACK.equals(color)) {
-          img = imageFactory.getImage("dice/" + i + ".png", false);
-        } else if (Color.RED.equals(color)) {
-          img = imageFactory.getImage("dice/" + i + "_hit.png", false);
-        } else if (IGNORED.equals(color)) {
-          img = imageFactory.getImage("dice/" + i + "_ignored.png", false);
-        }
-      }
-      if (img != null) {
-        images.put(i, img);
-      } else {
-        final Image canvas = Util.createImage(DIE_WIDTH, DIE_HEIGHT, true);
-        final Graphics graphics = canvas.getGraphics();
-        graphics.setColor(color);
-        graphics.drawRoundRect(1, 1, DIE_WIDTH - 3, DIE_HEIGHT - 3, 5, 5);
-        ((Graphics2D) graphics).setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        // center dot
-        if (i == 1 || i == 3 || i == 5) {
-          graphics.fillOval(DIE_WIDTH / 2 - (pipSize / 2), DIE_HEIGHT / 2 - (pipSize / 2), pipSize, pipSize);
-        }
-        // dots in top left and bottom right
-        if (i == 3 || i == 5 || i == 4) {
-          graphics.fillOval(DIE_WIDTH / 4 - (pipSize / 2), DIE_HEIGHT / 4 - (pipSize / 2), pipSize, pipSize);
-          graphics.fillOval(3 * DIE_WIDTH / 4 - (pipSize / 2), 3 * DIE_HEIGHT / 4 - (pipSize / 2), pipSize,
-              pipSize);
-        }
-        // dots in bottom left and top right
-        if (i == 5 || i == 4) {
-          graphics.fillOval(3 * DIE_WIDTH / 4 - (pipSize / 2), DIE_HEIGHT / 4 - (pipSize / 2), pipSize, pipSize);
-          graphics.fillOval(DIE_WIDTH / 4 - (pipSize / 2), 3 * DIE_HEIGHT / 4 - (pipSize / 2), pipSize, pipSize);
-        }
-        // center two for 2
-        if (i == 2 || i == 6) {
-          graphics.fillOval(DIE_WIDTH / 3 - (pipSize / 2), DIE_HEIGHT / 2 - (pipSize / 2), pipSize, pipSize);
-          graphics.fillOval(2 * DIE_WIDTH / 3 - (pipSize / 2), DIE_HEIGHT / 2 - (pipSize / 2), pipSize, pipSize);
-        }
-        if (i == 6) {
-          graphics.fillOval(DIE_WIDTH / 3 - (pipSize / 2), DIE_HEIGHT / 4 - (pipSize / 2), pipSize, pipSize);
-          graphics.fillOval(2 * DIE_WIDTH / 3 - (pipSize / 2), DIE_HEIGHT / 4 - (pipSize / 2), pipSize, pipSize);
-          graphics.fillOval(DIE_WIDTH / 3 - (pipSize / 2), 3 * DIE_HEIGHT / 4 - (pipSize / 2), pipSize, pipSize);
-          graphics.fillOval(2 * DIE_WIDTH / 3 - (pipSize / 2), 3 * DIE_HEIGHT / 4 - (pipSize / 2), pipSize,
-              pipSize);
-        }
-        if (i > 6) {
-          graphics.setFont(new Font("Arial", Font.BOLD, 16));
-          ((Graphics2D) graphics).setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-          final String number = Integer.toString(i);
-          final int widthOffset = graphics.getFontMetrics().charsWidth(number.toCharArray(), 0, number.length());
-          final int heightOffset = graphics.getFontMetrics().getHeight();
-          graphics.drawString(number, (DIE_WIDTH - widthOffset) / 2, (DIE_HEIGHT + heightOffset) / 2 - 2);
-        }
-        images.put(i, canvas);
-        graphics.dispose();
-      }
+    for (int currentSide = 1; currentSide <= diceSides; currentSide++) {
+      final Image img = resourceLoader != null
+          ? imageFactory.getImage(getDiceResourceName(color, currentSide), false)
+          : null;
+      images.put(currentSide, img != null ? img : drawFallbackDie(pipSize, currentSide, color));
     }
+  }
+
+
+  private String getDiceResourceName(final Color color, final int dieSide) {
+    final String suffix = ImmutableMap
+        .<Color, String>builder()
+        .put(Color.BLACK, "")
+        .put(Color.RED, "_hit")
+        .put(IGNORED, "_ignored")
+        .build()
+        .get(color);
+    return "dice/" + dieSide + Objects.requireNonNull(suffix) + ".png";
+  }
+
+  private Image drawFallbackDie(final int pipSize, final int dieSide, final Color color) {
+    final Image canvas = Util.createImage(DIE_WIDTH, DIE_HEIGHT, true);
+    final Graphics graphics = canvas.getGraphics();
+    graphics.setColor(color);
+    graphics.drawRoundRect(1, 1, DIE_WIDTH - 3, DIE_HEIGHT - 3, 5, 5);
+    ((Graphics2D) graphics).setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+    // center dot
+    if (dieSide == 1 || dieSide == 3 || dieSide == 5) {
+      graphics.fillOval(DIE_WIDTH / 2 - (pipSize / 2), DIE_HEIGHT / 2 - (pipSize / 2), pipSize, pipSize);
+    }
+    // dots in top left and bottom right
+    if (dieSide == 3 || dieSide == 5 || dieSide == 4) {
+      graphics.fillOval(DIE_WIDTH / 4 - (pipSize / 2), DIE_HEIGHT / 4 - (pipSize / 2), pipSize, pipSize);
+      graphics.fillOval(3 * DIE_WIDTH / 4 - (pipSize / 2), 3 * DIE_HEIGHT / 4 - (pipSize / 2), pipSize, pipSize);
+    }
+    // dots in bottom left and top right
+    if (dieSide == 5 || dieSide == 4) {
+      graphics.fillOval(3 * DIE_WIDTH / 4 - (pipSize / 2), DIE_HEIGHT / 4 - (pipSize / 2), pipSize, pipSize);
+      graphics.fillOval(DIE_WIDTH / 4 - (pipSize / 2), 3 * DIE_HEIGHT / 4 - (pipSize / 2), pipSize, pipSize);
+    }
+    // center two for 2
+    if (dieSide == 2 || dieSide == 6) {
+      graphics.fillOval(DIE_WIDTH / 3 - (pipSize / 2), DIE_HEIGHT / 2 - (pipSize / 2), pipSize, pipSize);
+      graphics.fillOval(2 * DIE_WIDTH / 3 - (pipSize / 2), DIE_HEIGHT / 2 - (pipSize / 2), pipSize, pipSize);
+    }
+    if (dieSide == 6) {
+      graphics.fillOval(DIE_WIDTH / 3 - (pipSize / 2), DIE_HEIGHT / 4 - (pipSize / 2), pipSize, pipSize);
+      graphics.fillOval(2 * DIE_WIDTH / 3 - (pipSize / 2), DIE_HEIGHT / 4 - (pipSize / 2), pipSize, pipSize);
+      graphics.fillOval(DIE_WIDTH / 3 - (pipSize / 2), 3 * DIE_HEIGHT / 4 - (pipSize / 2), pipSize, pipSize);
+      graphics.fillOval(2 * DIE_WIDTH / 3 - (pipSize / 2), 3 * DIE_HEIGHT / 4 - (pipSize / 2), pipSize, pipSize);
+    }
+    if (dieSide > 6) {
+      graphics.setFont(new Font("Arial", Font.BOLD, 16));
+      ((Graphics2D) graphics).setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+      final String number = Integer.toString(dieSide);
+      final int widthOffset = graphics.getFontMetrics().charsWidth(number.toCharArray(), 0, number.length());
+      final int heightOffset = graphics.getFontMetrics().getHeight();
+      graphics.drawString(number, (DIE_WIDTH - widthOffset) / 2, (DIE_HEIGHT + heightOffset) / 2 - 2);
+    }
+    graphics.dispose();
+    return canvas;
   }
 
   private Image getDieImage(final int i, final Die.DieType type) {
@@ -121,7 +127,7 @@ public class DiceImageFactory {
           graphics.setColor(IGNORED);
           break;
         default:
-          throw new IllegalStateException("??");
+          throw new AssertionError("There's no die type '" + type + "'");
       }
       graphics.drawRoundRect(1, 1, DIE_WIDTH - 3, DIE_HEIGHT - 3, 5, 5);
       ((Graphics2D) graphics).setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
@@ -139,7 +145,7 @@ public class DiceImageFactory {
       case IGNORED:
         return imagesIgnored.get(i);
       default:
-        throw new IllegalStateException("??");
+        throw new AssertionError("There's no die type '" + type + "'");
     }
   }
 
