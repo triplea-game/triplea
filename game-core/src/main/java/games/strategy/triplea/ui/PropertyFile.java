90ccaf650b50860c2704a0543cced284a90ec749
RoiEX <8350879+RoiEXLab@users.noreply.github.com>
2018-06-10 17:28:18 +0200

    Unify Caching code (#3456)

--- game-core/src/main/java/games/strategy/triplea/ui/PropertyFile.java
+++ game-core/src/main/java/games/strategy/triplea/ui/PropertyFile.java
@@ -5,6 +5,12 @@ import java.io.InputStream;
 import java.net.URL;
 import java.util.Optional;
 import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Supplier;
+
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
 
 import games.strategy.debug.ClientLogger;
 import games.strategy.triplea.ResourceLoader;
@@ -16,10 +22,13 @@ import games.strategy.util.UrlStreams;
  */
 public abstract class PropertyFile {
 
+  protected static final Cache<Class<? extends PropertyFile>, PropertyFile> cache = CacheBuilder.newBuilder()
+      .expireAfterWrite(10, TimeUnit.SECONDS)
+      .build();
+
   protected final Properties properties = new Properties();
 
-  protected PropertyFile(final String fileName) {
-    final ResourceLoader loader = AbstractUiContext.getResourceLoader();
+  PropertyFile(final String fileName, final ResourceLoader loader) {
     final URL url = loader.getResource(fileName);
     if (url != null) {
       final Optional<InputStream> inputStream = UrlStreams.openStream(url);
@@ -30,6 +39,19 @@ public abstract class PropertyFile {
           ClientLogger.logError("Error reading " + fileName, e);
         }
       }
+    }
+  }
+
+  protected PropertyFile(final String fileName) {
+    this(fileName, AbstractUiContext.getResourceLoader());
+  }
+
+  @SuppressWarnings("unchecked")
+  protected static <T extends PropertyFile> T getInstance(final Class<T> clazz, final Supplier<T> constructor) {
+    try {
+      return (T) cache.get(clazz, constructor::get);
+    } catch (final ExecutionException e) {
+      throw new IllegalStateException("Error in constructor", e);
     }
   }
 
---
+++ game-core/src/test/java/games/strategy/triplea/ui/PropertyFileTest.java
@@ -0,0 +1,59 @@
+package games.strategy.triplea.ui;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.util.Arrays;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import games.strategy.test.extensions.TemporaryFolder;
+import games.strategy.test.extensions.TemporaryFolderExtension;
+import games.strategy.triplea.ResourceLoader;
+
+@ExtendWith(TemporaryFolderExtension.class)
+public class PropertyFileTest {
+  private TemporaryFolder temporaryFolder;
+  private final ResourceLoader mock = mock(ResourceLoader.class);
+  private File file;
+
+  @BeforeEach
+  public void setup() throws Exception {
+    file = temporaryFolder.newFile(getClass().getName());
+    when(mock.getResource(file.getAbsolutePath())).thenReturn(file.toURI().toURL());
+    PropertyFile.cache.invalidateAll();
+  }
+
+  @Test
+  public void testConstructor() throws Exception {
+    Files.write(file.toPath(), Arrays.asList("abc=def", "123: 456"));
+    final PropertyFile instance = new PropertyFile(file.getAbsolutePath(), mock) {
+    };
+    assertEquals("def", instance.properties.getProperty("abc"));
+    assertEquals("456", instance.properties.getProperty("123"));
+    assertEquals(2, instance.properties.size());
+  }
+
+  @Test
+  public void testCaching() {
+    final DummyPropertyFile dummy = new DummyPropertyFile(file.getAbsolutePath(), mock);
+    assertSame(dummy, PropertyFile.getInstance(DummyPropertyFile.class, () -> dummy));
+    assertSame(dummy, PropertyFile.getInstance(DummyPropertyFile.class,
+        () -> new DummyPropertyFile(file.getAbsolutePath(), mock)));
+    DummyPropertyFile.cache.invalidateAll();
+    final DummyPropertyFile dummy2 = new DummyPropertyFile(file.getAbsolutePath(), mock);
+    assertSame(dummy2, PropertyFile.getInstance(DummyPropertyFile.class, () -> dummy2));
+  }
+
+  private static class DummyPropertyFile extends PropertyFile {
+    DummyPropertyFile(final String path, final ResourceLoader loader) {
+      super(path, loader);
+    }
+  }
+}
