998106860edbc9ad281964e35889299c7fbeb328
RoiEX <8350879+RoiEXLab@users.noreply.github.com>
2018-06-30 11:26:43 +0200

    Speed up ImageIO.read slightly

--- game-core/src/main/java/games/strategy/triplea/image/ImageFactory.java
+++ game-core/src/main/java/games/strategy/triplea/image/ImageFactory.java
@@ -38,6 +38,8 @@ public class ImageFactory {
         return null;
       }
       try {
+        // We're implementing our own caching system
+        ImageIO.setUseCache(false);
         images.put(key, ImageIO.read(url));
       } catch (final IOException e) {
         throw new IllegalStateException(e);
