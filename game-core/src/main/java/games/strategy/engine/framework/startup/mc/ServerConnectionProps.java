3abff5fc4e82adc761090a1c29fffb9ba62765bb
Dan Van Atta <dhvatta@gmail.com>
2018-08-01 12:56:08 -0700

    ServerConnectionProps  simplify (#3671)
    * Use @Builder+@Getter with ServerConnectionProps to simplify
    
    * Reduce scope of class ServerConnectionProps

--- game-core/src/main/java/games/strategy/engine/framework/startup/mc/ServerConnectionProps.java
+++ game-core/src/main/java/games/strategy/engine/framework/startup/mc/ServerConnectionProps.java
@@ -1,46 +1,25 @@
 package games.strategy.engine.framework.startup.mc;
 
+import javax.annotation.Nonnull;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
 /**
  * Simple data class for parameter values to connect to a remote host server (EG: to connect to a hosted game).
  */
-public class ServerConnectionProps {
+@Builder
+@AllArgsConstructor(access = AccessLevel.PACKAGE)
+@Getter
+class ServerConnectionProps {
   /** Player name, the desired name for the current player connecting to remote host. */
-  private String name;
+  @Nonnull
+  private final String name;
   /** Remote host address. */
-  private String host;
-  private int port;
+  @Nonnull
+  private final int port;
   /** Password to use to connect to the remotely hosted game, this is set by the host of the game. */
-  private String password;
-
-  public String getHost() {
-    return host;
-  }
-
-  public void setHost(final String host) {
-    this.host = host;
-  }
-
-  public String getPassword() {
-    return password;
-  }
-
-  public void setPassword(final String password) {
-    this.password = password;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public void setName(final String name) {
-    this.name = name;
-  }
-
-  public int getPort() {
-    return port;
-  }
-
-  public void setPort(final int port) {
-    this.port = port;
-  }
+  private final String password;
 }
