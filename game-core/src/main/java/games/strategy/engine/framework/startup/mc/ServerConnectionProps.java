67967d12ee0f93a25909c446c246d760932fb15b
Dan Van Atta <dhvatta@gmail.com>
2018-08-01 10:47:44 -0700

    Reduce scope of class ServerConnectionProps

--- game-core/src/main/java/games/strategy/engine/framework/startup/mc/ServerConnectionProps.java
+++ game-core/src/main/java/games/strategy/engine/framework/startup/mc/ServerConnectionProps.java
@@ -2,6 +2,8 @@ package games.strategy.engine.framework.startup.mc;
 
 import javax.annotation.Nonnull;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -9,8 +11,9 @@ import lombok.Getter;
  * Simple data class for parameter values to connect to a remote host server (EG: to connect to a hosted game).
  */
 @Builder
+@AllArgsConstructor(access = AccessLevel.PACKAGE)
 @Getter
-public class ServerConnectionProps {
+class ServerConnectionProps {
   /** Player name, the desired name for the current player connecting to remote host. */
   @Nonnull
   private final String name;
