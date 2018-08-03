c9d67c38dfbf9863fdd643008411f0c37a334703
Dan Van Atta <dhvatta@gmail.com>
2018-08-02 17:58:09 -0700

    Custom Matcher Util and introduction of 'test-util' project (#3675)
    * Custom Hamcrest Matcher Util and introduction of 'test-util' project
    
    Changes:
    - Introduce a test-util project that can be shared between multiple projects. This includes to start a Hamcrest customer matcher builder that gives us some nice defaulting and syntactic sugar when building custom hamcrest matchers.
    
    - We retrofit the existing location where we created a custom hamcrest matcher to use the new syntactic sugar.
    
    * rename test-util -> test-common
    
    * Rename MatchBuilder -> CustomMatcher
    
    * Rename Test class too"
    
    * Fix typo
    
    * Fix typos
    
    * Rename package from lib->common
    
    * Convert Function<t,Boolean> to Predicate<T>

---
+++ test-common/src/test/java/org/triplea/test/common/CustomMatcherTest.java
@@ -0,0 +1,82 @@
+package org.triplea.test.common;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.hamcrest.Matcher;
+import org.hamcrest.MatcherAssert;
+
+import org.junit.jupiter.api.Test;
+
+class CustomMatcherTest {
+
+
+  /**
+   * Example output using a matcher with a failure injected.:
+   * 
+   * <pre>
+   *       MatcherAssert.assertThat(500, numberIsEqualToExampleMatcher(3));
+   java.lang.AssertionError:
+   Expected: Test value should have been equal to: 3
+   but: 500
+   * </pre>
+   */
+  @Test
+  void exampleWithDefaultBehavior() {
+    MatcherAssert.assertThat(0, numberIsEqualToExampleMatcher(0));
+    MatcherAssert.assertThat(5, numberIsEqualToExampleMatcher(5));
+  }
+
+
+  private Matcher<Integer> numberIsEqualToExampleMatcher(final int expected) {
+    return CustomMatcher.<Integer>builder()
+        .description("Test value should have been equal to: " + expected)
+        .checkCondition(testValue -> testValue == expected)
+        .build();
+  }
+
+  @Test
+  void basicMismatchCase() {
+    assertThrows(AssertionError.class, () -> MatcherAssert.assertThat(0, numberIsEqualToExampleMatcher(100)));
+  }
+
+
+
+  /**
+   * Example output using a matcher with a failure injected.:
+   * 
+   * <pre>
+   *       MatcherAssert.assertThat(
+   *         "Hashcode values are expected to match between 'abc' and 'abc'",
+   *         "abc",
+   *         hashCodesMatch("abc"));
+   java.lang.AssertionError: In this example, if there is a failure, the ..[shortened]... get the #toString of 'abc'
+   Expected: Expected hashcode: 2987023 (hashed from: abc1)
+   but: Hashcode value is: 96354
+   * </pre>
+   */
+  @Test
+  public void exampleWithTestValueDebug() {
+    MatcherAssert.assertThat(
+        "Hashcode values are expected to match between 'abc' and 'abc'",
+        "abc",
+        hashCodesMatch("abc"));
+
+    assertThrows(
+        AssertionError.class,
+        () -> MatcherAssert.assertThat(
+            "The hashcodes of different strings do not match, so we'll expect an exception here."
+                + "This test can be used as an example to demo the debug messaging we get on a a failure by "
+                + "commenting out the assertThrows",
+            "123",
+            hashCodesMatch("abc")));
+  }
+
+  private Matcher<String> hashCodesMatch(final String stringWithHashToMatch) {
+    return CustomMatcher.<String>builder()
+        .description(
+            "Expected hashcode: " + stringWithHashToMatch.hashCode() + " ( hashed from: " + stringWithHashToMatch + ")")
+        .checkCondition(testValue -> testValue.hashCode() == stringWithHashToMatch.hashCode())
+        .debug(testValue -> "Hashcode value is: " + testValue.hashCode())
+        .build();
+  }
+}
