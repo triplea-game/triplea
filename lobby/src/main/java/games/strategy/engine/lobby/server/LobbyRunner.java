ea49002e0b95f3f2de13f38a6977887ea859bb66
Dan Van Atta <dhvatta@gmail.com>
2018-08-10 01:00:05 -0700

    Explicitly terminate if we cannot find property file instead of relying on exception handling to terminate

--- lobby/src/main/java/games/strategy/engine/lobby/server/LobbyRunner.java
+++ lobby/src/main/java/games/strategy/engine/lobby/server/LobbyRunner.java
@@ -1,6 +1,8 @@
 package games.strategy.engine.lobby.server;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.logging.Level;
 
 import games.strategy.engine.config.FilePropertyReader;
 import games.strategy.engine.config.lobby.LobbyPropertyReader;
@@ -18,8 +20,14 @@ public final class LobbyRunner {
    * is shut down via administrative command.
    */
   public static void main(final String[] args) throws IOException {
+    File propertyFile = new File("config/lobby/lobby.properties");
+    if (!propertyFile.exists()) {
+      log.log(Level.SEVERE, "Could not find property file: " + propertyFile.getAbsolutePath());
+      return;
+    }
+
     final LobbyPropertyReader lobbyPropertyReader =
-        new LobbyPropertyReader(new FilePropertyReader("config/lobby/lobby.properties"));
+        new LobbyPropertyReader(new FilePropertyReader(propertyFile));
     log.info("Starting lobby on port " + lobbyPropertyReader.getPort());
     LobbyServer.start(lobbyPropertyReader);
     log.info("Lobby started");
