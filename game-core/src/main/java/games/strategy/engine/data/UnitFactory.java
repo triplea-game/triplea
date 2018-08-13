38af157a3ee3eaef3b71c1c3552d4b3339ac13f2
Steven Soloff <steven.soloff@gmail.com>
2018-08-13 15:45:55 -0400

    Annotate UnitFactory with @FunctionalInterface

--- game-core/src/main/java/games/strategy/engine/data/UnitFactory.java
+++ game-core/src/main/java/games/strategy/engine/data/UnitFactory.java
@@ -3,6 +3,7 @@ package games.strategy.engine.data;
 /**
  * Factory for creating instances of {@link Unit}.
  */
+@FunctionalInterface
 public interface UnitFactory {
   Unit createUnit(UnitType type, PlayerID owner, GameData data);
 }
