42f6dd30981557f73e5d2e2b32d50da47bf5928f
Steven Soloff <steven.soloff@gmail.com>
2018-08-14 22:38:21 -0400

    Move ILobbyGameBroadcaster to common package

--- lobby/src/main/java/games/strategy/engine/lobby/server/LobbyGameController.java
+++ lobby/src/main/java/games/strategy/engine/lobby/server/LobbyGameController.java
@@ -9,6 +9,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
+import games.strategy.engine.lobby.common.ILobbyGameBroadcaster;
 import games.strategy.engine.message.IRemoteMessenger;
 import games.strategy.engine.message.MessageContext;
 import games.strategy.net.GUID;
