b7c17338c8fa12b87278633c860456ab5a6f1866
Steven Soloff <steven.soloff@gmail.com>
2018-08-15 22:43:05 -0400

    Inline DBUser forbidden name list into test
    This collection is only used by test code.
    
    Also replace hard-coded "admin" string with appropriate constant.

--- game-core/src/main/java/games/strategy/engine/lobby/server/userDB/DBUser.java
+++ game-core/src/main/java/games/strategy/engine/lobby/server/userDB/DBUser.java
@@ -1,11 +1,7 @@
 package games.strategy.engine.lobby.server.userDB;
 
 import java.io.Serializable;
-import java.util.Arrays;
-import java.util.Collection;
 import java.util.Objects;
-
-import com.google.common.annotations.VisibleForTesting;
 
 import games.strategy.engine.lobby.common.LobbyConstants;
 import games.strategy.util.Util;
@@ -20,10 +16,6 @@ public final class DBUser implements Serializable {
   private final String m_name;
   private final String m_email;
   private final Role userRole;
-
-
-  @VisibleForTesting
-  static final Collection<String> forbiddenNameParts = Arrays.asList(LobbyConstants.LOBBY_WATCHER_NAME, "admin");
 
   public enum Role {
     NOT_ADMIN, ADMIN
@@ -44,8 +36,8 @@ public final class DBUser implements Serializable {
       if (userName.toLowerCase().contains(LobbyConstants.LOBBY_WATCHER_NAME.toLowerCase())) {
         return LobbyConstants.LOBBY_WATCHER_NAME + " cannot be part of a name";
       }
-      if (userName.toLowerCase().contains("admin")) {
-        return "Name can't contain the word admin";
+      if (userName.toLowerCase().contains(LobbyConstants.ADMIN_USERNAME.toLowerCase())) {
+        return "Name can't contain the word " + LobbyConstants.ADMIN_USERNAME;
       }
       return null;
     }
