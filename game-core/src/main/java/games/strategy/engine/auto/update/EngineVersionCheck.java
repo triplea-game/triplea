6bcd8461ea38c4efd979dba44ffbd7ca9ae553ea
Steven Soloff <steven.soloff@gmail.com>
2018-08-28 23:54:33 -0400

    Error Prone: Fix string splitter violations

--- game-core/src/main/java/games/strategy/engine/auto/update/EngineVersionCheck.java
+++ game-core/src/main/java/games/strategy/engine/auto/update/EngineVersionCheck.java
@@ -1,37 +1,31 @@
 package games.strategy.engine.auto.update;
 
-import java.time.LocalDateTime;
-import java.time.temporal.ChronoField;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
 import java.util.logging.Level;
 
 import javax.swing.JOptionPane;
 import javax.swing.SwingUtilities;
 
+import com.google.common.annotations.VisibleForTesting;
+import com.google.common.base.Splitter;
+
 import games.strategy.engine.ClientContext;
 import games.strategy.triplea.settings.ClientSetting;
+import games.strategy.triplea.settings.GameSetting;
 import games.strategy.util.EventThreadJOptionPane;
 import lombok.extern.java.Log;
 
 @Log
-class EngineVersionCheck {
+final class EngineVersionCheck {
+  private EngineVersionCheck() {}
+
   static void checkForLatestEngineVersionOut() {
     try {
-      final boolean firstTimeThisVersion = ClientSetting.TRIPLEA_FIRST_TIME_THIS_VERSION_PROPERTY.booleanValue();
-      // check at most once per 2 days (but still allow a 'first run message' for a new version of triplea)
-      final LocalDateTime localDateTime = LocalDateTime.now();
-      final int year = localDateTime.get(ChronoField.YEAR);
-      final int day = localDateTime.get(ChronoField.DAY_OF_YEAR);
-      // format year:day
-      final String lastCheckTime = ClientSetting.TRIPLEA_LAST_CHECK_FOR_ENGINE_UPDATE.value();
-      if (!firstTimeThisVersion && lastCheckTime.trim().length() > 0) {
-        final String[] yearDay = lastCheckTime.split(":");
-        if (Integer.parseInt(yearDay[0]) >= year && Integer.parseInt(yearDay[1]) + 1 >= day) {
-          return;
-        }
+      if (!isEngineUpdateCheckRequired()) {
+        return;
       }
-
-      ClientSetting.TRIPLEA_LAST_CHECK_FOR_ENGINE_UPDATE.save(year + ":" + day);
-      ClientSetting.flush();
 
       final EngineVersionProperties latestEngineOut = EngineVersionProperties.contactServerForEngineVersionProperties();
       if (latestEngineOut == null) {
@@ -45,5 +39,46 @@ class EngineVersionCheck {
     } catch (final Exception e) {
       log.log(Level.SEVERE, "Error while checking for engine updates", e);
     }
+  }
+
+  private static boolean isEngineUpdateCheckRequired() {
+    return isEngineUpdateCheckRequired(
+        LocalDate.now(),
+        ClientSetting.TRIPLEA_FIRST_TIME_THIS_VERSION_PROPERTY,
+        ClientSetting.TRIPLEA_LAST_CHECK_FOR_ENGINE_UPDATE,
+        ClientSetting::flush);
+  }
+
+  @VisibleForTesting
+  static boolean isEngineUpdateCheckRequired(
+      final LocalDate now,
+      final GameSetting firstRunSetting,
+      final GameSetting updateCheckDateSetting,
+      final Runnable flushSetting) {
+    // check at most once per 2 days (but still allow a 'first run message' for a new version of TripleA)
+    final boolean firstRun = firstRunSetting.booleanValue();
+    final String encodedUpdateCheckDate = updateCheckDateSetting.value();
+    if (!firstRun && !encodedUpdateCheckDate.trim().isEmpty()) {
+      final LocalDate updateCheckDate = parseUpdateCheckDate(encodedUpdateCheckDate);
+      if (updateCheckDate.until(now, ChronoUnit.DAYS) < 2) {
+        return false;
+      }
+    }
+
+    updateCheckDateSetting.save(formatUpdateCheckDate(now));
+    flushSetting.run();
+
+    return true;
+  }
+
+  @VisibleForTesting
+  static LocalDate parseUpdateCheckDate(final String encodedUpdateCheckDate) {
+    final List<String> tokens = Splitter.on(':').splitToList(encodedUpdateCheckDate);
+    return LocalDate.ofYearDay(Integer.parseInt(tokens.get(0)), Integer.parseInt(tokens.get(1)));
+  }
+
+  @VisibleForTesting
+  static String formatUpdateCheckDate(final LocalDate updateCheckDate) {
+    return updateCheckDate.getYear() + ":" + updateCheckDate.getDayOfYear();
   }
 }
