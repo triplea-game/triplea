ff0ea3476659e329c19ea12ef88b2f703302dc9f
Dan Van Atta <dhvatta@gmail.com>
2018-09-03 11:42:42 -0700

    Error reporting - first http server and server component stub (#3956)
    * Add lobby-client with minimal implementation, a first http server and system test
    
    Implementation for right now is super basic, we log a message we get and we log if we throttle. There is quite a bit to continue doing in these areas, notably to actually persist the message, and when we throttle we should only emit a counter metric. We will also need to implement some throttling rules.
    
    The first http server is spark, it was pretty fast to get up and running. A next server will be dropwizard landing in parallel to the http package.
    
    The integration test runs in under 500ms, fires up the server by invoking main, waits and then calls the server and verifies we get a response. The intent of this test is not to verify correctness (that is already verified by the unit tests), but instead to verify that the server works can serve requests and gives reasonable signs of success and correctly shaped output.
    
    * Rename 'http-lobby' -> 'http-server'
    Also rename package 'error.report' -> 'reporting.error'

---
+++ http-server/src/main/java/org/triplea/server/reporting/error/upload/ErrorReport.java
@@ -0,0 +1,31 @@
+package org.triplea.server.reporting.error.upload;
+
+import java.time.Instant;
+
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+
+/**
+ * A data object with the full details of an error report that we can record on server side for
+ * future reference and investigation.
+ */
+@Builder
+@ToString
+@EqualsAndHashCode
+public class ErrorReport {
+  /**
+   * An identifier for the reporting machine.
+   */
+  private final String reportingHostId;
+  /**
+   * The time when the report was uploaded to our backend server.
+   */
+  private final Instant reportedOn;
+  /**
+   * An unstructured payload of the report contents.
+   */
+  private final String reportContents;
+
+}
