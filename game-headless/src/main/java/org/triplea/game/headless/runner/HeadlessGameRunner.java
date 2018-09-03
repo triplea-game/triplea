7326356f5a8449ac057cba7687fc7883580892eb
Steven Soloff <steven.soloff@gmail.com>
2018-09-03 11:06:44 -0400

    Add subproject for headless game server (#3951)

---
+++ game-headless/src/main/java/org/triplea/game/headless/runner/HeadlessGameRunner.java
@@ -0,0 +1,18 @@
+package org.triplea.game.headless.runner;
+
+import games.strategy.engine.framework.headlessGameServer.HeadlessGameServer;
+
+/**
+ * Runs a headless game server.
+ */
+public final class HeadlessGameRunner {
+  private HeadlessGameRunner() {}
+
+  /**
+   * Entry point for running a new headless game server. The headless game server runs until the process is killed or
+   * the headless game server is shut down via administrative command.
+   */
+  public static void main(final String[] args) {
+    HeadlessGameServer.main(args);
+  }
+}
