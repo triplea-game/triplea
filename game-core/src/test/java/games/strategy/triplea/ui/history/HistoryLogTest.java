0bf97fcc891d116ccf885e7caae498cc048a79f7
Steven Soloff <steven.soloff@gmail.com>
2018-09-04 15:59:47 -0400

    Error Prone: Fix string splitter violations in HistoryLog (#3975)
    * Add characterization tests
    
    * Error Prone: Fix string splitter violations

---
+++ game-core/src/test/java/games/strategy/triplea/ui/history/HistoryLogTest.java
@@ -0,0 +1,20 @@
+package games.strategy.triplea.ui.history;
+
+import static games.strategy.triplea.ui.history.HistoryLog.parsePlayerNameFromDiceRollMessage;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+final class HistoryLogTest {
+  @Nested
+  final class ParsePlayerNameFromDiceRollMessageTest {
+    @Test
+    void shouldParsePlayerName() {
+      assertThat(
+          parsePlayerNameFromDiceRollMessage("Japanese roll dice for 1 battleship in Australia, round 2 :"),
+          is("Japanese"));
+    }
+  }
+}
