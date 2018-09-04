fd41c43bcb278c05fd19c5bee8a94c8131efccfa
Dan Van_Atta <dhvatta@gmail.com>
2018-09-03 17:05:34 -0700

    Update rate test to use mocks and not rely on wall clock timing

--- http-client/src/main/java/org/triplea/http/client/throttle/rate/RateLimitingThrottle.java
+++ http-client/src/main/java/org/triplea/http/client/throttle/rate/RateLimitingThrottle.java
@@ -3,10 +3,12 @@ package org.triplea.http.client.throttle.rate;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.function.Consumer;
+import java.util.function.Supplier;
 
 import org.triplea.http.data.error.report.ErrorReport;
 
 import com.google.common.annotations.VisibleForTesting;
+
 
 /**
  * A simple rate limit throttle that remembers the time of last request
@@ -18,19 +20,21 @@ public class RateLimitingThrottle implements Consumer<ErrorReport> {
   private static final int MIN_MILLIS_BETWEEN_REQUESTS = 15_000;
   private final int minMillisBetweenRequests;
   private volatile Instant lastInstant = Instant.ofEpochMilli(0);
+  private final Supplier<Instant> instantSupplier;
 
   public RateLimitingThrottle() {
-    this(MIN_MILLIS_BETWEEN_REQUESTS);
+    this(MIN_MILLIS_BETWEEN_REQUESTS, Instant::now);
   }
 
   @VisibleForTesting
-  RateLimitingThrottle(final int minMillisBetweenRequests) {
+  RateLimitingThrottle(final int minMillisBetweenRequests, final Supplier<Instant> instantSupplier) {
     this.minMillisBetweenRequests = minMillisBetweenRequests;
+    this.instantSupplier = instantSupplier;
   }
 
   @Override
   public void accept(final ErrorReport errorReport) {
-    final Instant nextInstant = Instant.now();
+    final Instant nextInstant = instantSupplier.get();
 
     final long millisSinceLast;
     millisSinceLast = lastInstant.until(nextInstant, ChronoUnit.MILLIS);
--- http-client/src/test/java/org/triplea/http/client/throttle/rate/RateLimitingThrottleTest.java
+++ http-client/src/test/java/org/triplea/http/client/throttle/rate/RateLimitingThrottleTest.java
@@ -1,15 +1,22 @@
 package org.triplea.http.client.throttle.rate;
 
+import java.time.Instant;
 import java.util.function.Consumer;
+import java.util.function.Supplier;
 import java.util.logging.Level;
 import java.util.logging.LogRecord;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.triplea.http.data.error.report.ErrorReport;
 import org.triplea.http.data.error.report.ErrorReportDetails;
 
+@ExtendWith(MockitoExtension.class)
 class RateLimitingThrottleTest {
 
   private static final ErrorReport ERROR_REPORT = new ErrorReport(ErrorReportDetails.builder()
@@ -19,26 +26,30 @@ class RateLimitingThrottleTest {
 
   private static final int MIN_MILLIS_BETWEEN_REQUSETS = 5;
 
+  private static final Instant NOW = Instant.now();
+
+  @Mock
+  private Supplier<Instant> instantSupplier;
+
 
   @Test
-  void throttleNumberOfRequestPer() throws Exception {
+  void throttleNumberOfRequestPer() {
     final Consumer<ErrorReport> throttle =
-        new RateLimitingThrottle(MIN_MILLIS_BETWEEN_REQUSETS);
+        new RateLimitingThrottle(MIN_MILLIS_BETWEEN_REQUSETS, instantSupplier);
 
-    // no throttle expected on the first call
+    Mockito.when(instantSupplier.get())
+        .thenReturn(
+            NOW,
+            NOW.plusMillis(MIN_MILLIS_BETWEEN_REQUSETS + 1),
+            NOW.plusMillis(MIN_MILLIS_BETWEEN_REQUSETS - 1));
+
+    // no throttle expected on the first call, that should always pass
     throttle.accept(ERROR_REPORT);
 
-    Thread.sleep(MIN_MILLIS_BETWEEN_REQUSETS + 1);
-
-    // no thottle expected given we had the wait period
+    // second call is after the min time, so it should be okay
     throttle.accept(ERROR_REPORT);
 
-    Thread.sleep(MIN_MILLIS_BETWEEN_REQUSETS + 1);
-
-    // ensure the n+1 works if we wait longer than the min time
-    throttle.accept(ERROR_REPORT);
-
-    // now trigger the throttle by sending another request without any delay
+    // the third call did not wait long enough, we expect an exception
     Assertions.assertThrows(RateLimitException.class,
         () -> throttle.accept(ERROR_REPORT));
   }
