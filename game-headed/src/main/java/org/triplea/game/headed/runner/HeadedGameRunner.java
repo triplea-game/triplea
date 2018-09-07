54170f4b3160dc7fdac648791f4504508ba89ef6
Steven Soloff <steven.soloff@gmail.com>
2018-09-06 23:51:38 -0400

    Add subproject for headed game client

---
+++ game-headed/src/main/java/org/triplea/game/headed/runner/HeadedGameRunner.java
@@ -0,0 +1,17 @@
+package org.triplea.game.headed.runner;
+
+import games.strategy.engine.framework.GameRunner;
+
+/**
+ * Runs a headed game client.
+ */
+public final class HeadedGameRunner {
+  private HeadedGameRunner() {}
+
+  /**
+   * Entry point for running a new headed game client.
+   */
+  public static void main(final String[] args) {
+    GameRunner.main(args);
+  }
+}
