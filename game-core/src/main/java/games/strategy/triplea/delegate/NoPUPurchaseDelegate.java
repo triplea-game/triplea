1b757d9a875414fe8b17c30e263bd92bab8ca25f
RoiEX <8350879+RoiEXLab@users.noreply.github.com>
2018-09-21 17:55:29 +0200

    Restructure if statement to guarantee null-safety

--- game-core/src/main/java/games/strategy/triplea/delegate/NoPUPurchaseDelegate.java
+++ game-core/src/main/java/games/strategy/triplea/delegate/NoPUPurchaseDelegate.java
@@ -50,11 +50,12 @@ public class NoPUPurchaseDelegate extends PurchaseDelegate {
     final RulesAttachment ra = (RulesAttachment) player.getAttachment(Constants.RULES_ATTACHMENT_NAME);
     // if they have no rules attachments, but are calling NoPU purchase, and have the game property
     // isProductionPerValuedTerritoryRestricted, then they want 1 infantry for each territory with PU value > 0
-    if (isProductionPerValuedTerritoryRestricted()
-        && (ra == null || ra.getProductionPerXTerritories() == null || ra.getProductionPerXTerritories().size() == 0)) {
-      productionPerXTerritories.put(getData().getUnitTypeList().getUnitType(Constants.UNIT_TYPE_INFANTRY), 1);
-    } else if (isProductionPerXTerritoriesRestricted()) {
-      productionPerXTerritories = ra.getProductionPerXTerritories();
+    if(isProductionPerValuedTerritoryRestricted()) {
+      if (ra == null || ra.getProductionPerXTerritories() == null || ra.getProductionPerXTerritories().size() == 0) {
+        productionPerXTerritories.put(getData().getUnitTypeList().getUnitType(Constants.UNIT_TYPE_INFANTRY), 1);
+      } else {
+        productionPerXTerritories = ra.getProductionPerXTerritories();
+      }
     } else {
       return productionUnits;
     }
