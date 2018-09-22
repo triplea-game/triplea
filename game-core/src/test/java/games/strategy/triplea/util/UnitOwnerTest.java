ef3c021643a4553ce1c0d7f4efa31b8026185c8d
Steven Soloff <steven.soloff@gmail.com>
2018-09-22 17:06:23 -0400

    Nest EqualsVerifier tests to avoid static method warnings (#4089)

--- game-core/src/test/java/games/strategy/triplea/util/UnitOwnerTest.java
+++ game-core/src/test/java/games/strategy/triplea/util/UnitOwnerTest.java
@@ -1,5 +1,6 @@
 package games.strategy.triplea.util;
 
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import games.strategy.engine.data.GameData;
@@ -7,19 +8,22 @@ import games.strategy.engine.data.PlayerID;
 import games.strategy.engine.data.UnitType;
 import nl.jqno.equalsverifier.EqualsVerifier;
 
-public final class UnitOwnerTest {
-  @Test
-  public void shouldBeEquatableAndHashable() {
-    final GameData gameData = new GameData();
-    EqualsVerifier.forClass(UnitOwner.class)
-        .withPrefabValues(
-            PlayerID.class,
-            new PlayerID("player1Name", gameData),
-            new PlayerID("player2Name", gameData))
-        .withPrefabValues(
-            UnitType.class,
-            new UnitType("unitType1Name", gameData),
-            new UnitType("unitType2Name", gameData))
-        .verify();
+final class UnitOwnerTest {
+  @Nested
+  final class EqualsAndHashCodeTest {
+    @Test
+    void shouldBeEquatableAndHashable() {
+      final GameData gameData = new GameData();
+      EqualsVerifier.forClass(UnitOwner.class)
+          .withPrefabValues(
+              PlayerID.class,
+              new PlayerID("player1Name", gameData),
+              new PlayerID("player2Name", gameData))
+          .withPrefabValues(
+              UnitType.class,
+              new UnitType("unitType1Name", gameData),
+              new UnitType("unitType2Name", gameData))
+          .verify();
+    }
   }
 }
--- lobby/src/test/java/org/triplea/lobby/server/UserTest.java
+++ lobby/src/test/java/org/triplea/lobby/server/UserTest.java
@@ -1,12 +1,16 @@
 package org.triplea.lobby.server;
 
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import nl.jqno.equalsverifier.EqualsVerifier;
 
-public final class UserTest {
-  @Test
-  public void shouldBeEquatableAndHashable() {
-    EqualsVerifier.forClass(User.class).verify();
+final class UserTest {
+  @Nested
+  final class EqualsAndHashCodeTest {
+    @Test
+    void shouldBeEquatableAndHashable() {
+      EqualsVerifier.forClass(User.class).verify();
+    }
   }
 }
