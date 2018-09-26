f002cf2ab840fc7ca7c8ea588203d206a86e99e9
Steven Soloff <steven.soloff@gmail.com>
2018-09-26 01:20:30 -0400

    Error Prone: Fix immutable enum checker violations in ClientSettingJavaFxUiBinding (#4106)
    * Error Prone: Fix immutable enum checker violations
    
    * Remove unnecessary Supplier indirection

--- game-headed/src/main/java/org/triplea/game/client/ui/javafx/util/ClientSettingJavaFxUiBinding.java
+++ game-headed/src/main/java/org/triplea/game/client/ui/javafx/util/ClientSettingJavaFxUiBinding.java
@@ -1,12 +1,18 @@
 package org.triplea.game.client.ui.javafx.util;
 
-import java.util.function.Supplier;
+import static org.triplea.game.client.ui.javafx.util.JavaFxSelectionComponentFactory.filePath;
+import static org.triplea.game.client.ui.javafx.util.JavaFxSelectionComponentFactory.folderPath;
+import static org.triplea.game.client.ui.javafx.util.JavaFxSelectionComponentFactory.intValueRange;
+import static org.triplea.game.client.ui.javafx.util.JavaFxSelectionComponentFactory.textField;
+import static org.triplea.game.client.ui.javafx.util.JavaFxSelectionComponentFactory.toggleButton;
 
 import games.strategy.triplea.settings.ClientSetting;
 import games.strategy.triplea.settings.GameSettingUiBinding;
 import games.strategy.triplea.settings.SelectionComponent;
 import games.strategy.triplea.settings.SettingType;
 import javafx.scene.layout.Region;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
 /**
  * Binds a {@link ClientSetting} to a JavaFX UI component. This is done by adding an enum element. As part of that the
@@ -21,121 +27,167 @@ import javafx.scene.layout.Region;
  * 1:1, and not all {@link ClientSetting}s will be available in the UI.
  * </p>
  */
+@AllArgsConstructor
 public enum ClientSettingJavaFxUiBinding implements GameSettingUiBinding<Region> {
-  AI_PAUSE_DURATION_BINDING(
-      SettingType.AI,
-      JavaFxSelectionComponentFactory.intValueRange(ClientSetting.AI_PAUSE_DURATION, 0, 3000)),
+  AI_PAUSE_DURATION_BINDING(SettingType.AI) {
+    @Override
+    public SelectionComponent<Region> newSelectionComponent() {
+      return intValueRange(ClientSetting.AI_PAUSE_DURATION, 0, 3000);
+    }
+  },
 
-  ARROW_KEY_SCROLL_SPEED_BINDING(
-      SettingType.MAP_SCROLLING,
-      JavaFxSelectionComponentFactory.intValueRange(ClientSetting.ARROW_KEY_SCROLL_SPEED, 0, 500)),
+  ARROW_KEY_SCROLL_SPEED_BINDING(SettingType.MAP_SCROLLING) {
+    @Override
+    public SelectionComponent<Region> newSelectionComponent() {
+      return intValueRange(ClientSetting.ARROW_KEY_SCROLL_SPEED, 0, 500);
+    }
+  },
 
-  BATTLE_CALC_SIMULATION_COUNT_DICE_BINDING(
-      SettingType.BATTLE_SIMULATOR,
-      JavaFxSelectionComponentFactory.intValueRange(ClientSetting.BATTLE_CALC_SIMULATION_COUNT_DICE, 10, 100000)),
+  BATTLE_CALC_SIMULATION_COUNT_DICE_BINDING(SettingType.BATTLE_SIMULATOR) {
+    @Override
+    public SelectionComponent<Region> newSelectionComponent() {
+      return intValueRange(ClientSetting.BATTLE_CALC_SIMULATION_COUNT_DICE, 10, 100000);
+    }
+  },
 
-  BATTLE_CALC_SIMULATION_COUNT_LOW_LUCK_BINDING(
-      SettingType.BATTLE_SIMULATOR,
-      JavaFxSelectionComponentFactory.intValueRange(ClientSetting.BATTLE_CALC_SIMULATION_COUNT_LOW_LUCK, 10, 100000)),
+  BATTLE_CALC_SIMULATION_COUNT_LOW_LUCK_BINDING(SettingType.BATTLE_SIMULATOR) {
+    @Override
+    public SelectionComponent<Region> newSelectionComponent() {
+      return intValueRange(ClientSetting.BATTLE_CALC_SIMULATION_COUNT_LOW_LUCK, 10, 100000);
+    }
+  },
 
-  CONFIRM_DEFENSIVE_ROLLS_BINDING(
-      SettingType.COMBAT,
-      ClientSetting.CONFIRM_DEFENSIVE_ROLLS),
+  CONFIRM_DEFENSIVE_ROLLS_BINDING(SettingType.COMBAT) {
+    @Override
+    public SelectionComponent<Region> newSelectionComponent() {
+      return toggleButton(ClientSetting.CONFIRM_DEFENSIVE_ROLLS);
+    }
+  },
 
-  CONFIRM_ENEMY_CASUALTIES_BINDING(
-      SettingType.COMBAT,
-      ClientSetting.CONFIRM_ENEMY_CASUALTIES),
+  CONFIRM_ENEMY_CASUALTIES_BINDING(SettingType.COMBAT) {
+    @Override
+    public SelectionComponent<Region> newSelectionComponent() {
+      return toggleButton(ClientSetting.CONFIRM_ENEMY_CASUALTIES);
+    }
+  },
 
-  SPACE_BAR_CONFIRMS_CASUALTIES_BINDING(
-      SettingType.COMBAT,
-      ClientSetting.SPACE_BAR_CONFIRMS_CASUALTIES),
+  SPACE_BAR_CONFIRMS_CASUALTIES_BINDING(SettingType.COMBAT) {
+    @Override
+    public SelectionComponent<Region> newSelectionComponent() {
+      return toggleButton(ClientSetting.SPACE_BAR_CONFIRMS_CASUALTIES);
+    }
+  },
 
-  MAP_EDGE_SCROLL_SPEED_BINDING(
-      SettingType.MAP_SCROLLING,
-      JavaFxSelectionComponentFactory.intValueRange(ClientSetting.MAP_EDGE_SCROLL_SPEED, 0, 300)),
+  MAP_EDGE_SCROLL_SPEED_BINDING(SettingType.MAP_SCROLLING) {
+    @Override
+    public SelectionComponent<Region> newSelectionComponent() {
+      return intValueRange(ClientSetting.MAP_EDGE_SCROLL_SPEED, 0, 300);
+    }
+  },
 
-  MAP_EDGE_SCROLL_ZONE_SIZE_BINDING(
-      SettingType.MAP_SCROLLING,
-      JavaFxSelectionComponentFactory.intValueRange(ClientSetting.MAP_EDGE_SCROLL_ZONE_SIZE, 0, 300)),
+  MAP_EDGE_SCROLL_ZONE_SIZE_BINDING(SettingType.MAP_SCROLLING) {
+    @Override
+    public SelectionComponent<Region> newSelectionComponent() {
+      return intValueRange(ClientSetting.MAP_EDGE_SCROLL_ZONE_SIZE, 0, 300);
+    }
+  },
 
-  SERVER_START_GAME_SYNC_WAIT_TIME_BINDING(
-      SettingType.NETWORK_TIMEOUTS,
-      JavaFxSelectionComponentFactory.intValueRange(ClientSetting.SERVER_START_GAME_SYNC_WAIT_TIME, 120, 1500)),
+  SERVER_START_GAME_SYNC_WAIT_TIME_BINDING(SettingType.NETWORK_TIMEOUTS) {
+    @Override
+    public SelectionComponent<Region> newSelectionComponent() {
+      return intValueRange(ClientSetting.SERVER_START_GAME_SYNC_WAIT_TIME, 120, 1500);
+    }
+  },
 
-  SERVER_OBSERVER_JOIN_WAIT_TIME_BINDING(
-      SettingType.NETWORK_TIMEOUTS,
-      JavaFxSelectionComponentFactory.intValueRange(ClientSetting.SERVER_OBSERVER_JOIN_WAIT_TIME, 60, 1500)),
+  SERVER_OBSERVER_JOIN_WAIT_TIME_BINDING(SettingType.NETWORK_TIMEOUTS) {
+    @Override
+    public SelectionComponent<Region> newSelectionComponent() {
+      return intValueRange(ClientSetting.SERVER_OBSERVER_JOIN_WAIT_TIME, 60, 1500);
+    }
+  },
 
-  SHOW_BATTLES_WHEN_OBSERVING_BINDING(
-      SettingType.GAME,
-      ClientSetting.SHOW_BATTLES_WHEN_OBSERVING),
+  SHOW_BATTLES_WHEN_OBSERVING_BINDING(SettingType.GAME) {
+    @Override
+    public SelectionComponent<Region> newSelectionComponent() {
+      return toggleButton(ClientSetting.SHOW_BATTLES_WHEN_OBSERVING);
+    }
+  },
 
-  SHOW_BETA_FEATURES_BINDING(
-      SettingType.TESTING,
-      ClientSetting.SHOW_BETA_FEATURES),
+  SHOW_BETA_FEATURES_BINDING(SettingType.TESTING) {
+    @Override
+    public SelectionComponent<Region> newSelectionComponent() {
+      return toggleButton(ClientSetting.SHOW_BETA_FEATURES);
+    }
+  },
 
-  MAP_LIST_OVERRIDE_BINDING(
-      SettingType.TESTING,
-      JavaFxSelectionComponentFactory.filePath(ClientSetting.MAP_LIST_OVERRIDE)),
+  MAP_LIST_OVERRIDE_BINDING(SettingType.TESTING) {
+    @Override
+    public SelectionComponent<Region> newSelectionComponent() {
+      return filePath(ClientSetting.MAP_LIST_OVERRIDE);
+    }
+  },
 
-  TEST_LOBBY_HOST_BINDING(
-      SettingType.TESTING,
-      JavaFxSelectionComponentFactory.textField(ClientSetting.TEST_LOBBY_HOST)),
+  TEST_LOBBY_HOST_BINDING(SettingType.TESTING) {
+    @Override
+    public SelectionComponent<Region> newSelectionComponent() {
+      return textField(ClientSetting.TEST_LOBBY_HOST);
+    }
+  },
 
-  TEST_LOBBY_PORT_BINDING(
-      SettingType.TESTING,
-      JavaFxSelectionComponentFactory.intValueRange(ClientSetting.TEST_LOBBY_PORT, 1, 65535, true)),
+  TEST_LOBBY_PORT_BINDING(SettingType.TESTING) {
+    @Override
+    public SelectionComponent<Region> newSelectionComponent() {
+      return intValueRange(ClientSetting.TEST_LOBBY_PORT, 1, 65535, true);
+    }
+  },
 
-  TRIPLEA_FIRST_TIME_THIS_VERSION_PROPERTY_BINDING(
-      SettingType.GAME,
-      ClientSetting.TRIPLEA_FIRST_TIME_THIS_VERSION_PROPERTY),
+  TRIPLEA_FIRST_TIME_THIS_VERSION_PROPERTY_BINDING(SettingType.GAME) {
+    @Override
+    public SelectionComponent<Region> newSelectionComponent() {
+      return toggleButton(ClientSetting.TRIPLEA_FIRST_TIME_THIS_VERSION_PROPERTY);
+    }
+  },
 
-  SAVE_GAMES_FOLDER_PATH_BINDING(
-      SettingType.FOLDER_LOCATIONS,
-      JavaFxSelectionComponentFactory.folderPath(ClientSetting.SAVE_GAMES_FOLDER_PATH)),
+  SAVE_GAMES_FOLDER_PATH_BINDING(SettingType.FOLDER_LOCATIONS) {
+    @Override
+    public SelectionComponent<Region> newSelectionComponent() {
+      return folderPath(ClientSetting.SAVE_GAMES_FOLDER_PATH);
+    }
+  },
 
-  USER_MAPS_FOLDER_PATH_BINDING(
-      SettingType.FOLDER_LOCATIONS,
-      JavaFxSelectionComponentFactory.folderPath(ClientSetting.USER_MAPS_FOLDER_PATH)),
+  USER_MAPS_FOLDER_PATH_BINDING(SettingType.FOLDER_LOCATIONS) {
+    @Override
+    public SelectionComponent<Region> newSelectionComponent() {
+      return folderPath(ClientSetting.USER_MAPS_FOLDER_PATH);
+    }
+  },
 
-  WHEEL_SCROLL_AMOUNT_BINDING(
-      SettingType.MAP_SCROLLING,
-      JavaFxSelectionComponentFactory.intValueRange(ClientSetting.WHEEL_SCROLL_AMOUNT, 10, 300)),
+  WHEEL_SCROLL_AMOUNT_BINDING(SettingType.MAP_SCROLLING) {
+    @Override
+    public SelectionComponent<Region> newSelectionComponent() {
+      return intValueRange(ClientSetting.WHEEL_SCROLL_AMOUNT, 10, 300);
+    }
+  },
 
-  PROXY_CHOICE(
-      SettingType.NETWORK_PROXY,
-      JavaFxSelectionComponentFactory.proxySettings()),
+  PROXY_CHOICE(SettingType.NETWORK_PROXY) {
+    @Override
+    public SelectionComponent<Region> newSelectionComponent() {
+      return JavaFxSelectionComponentFactory.proxySettings();
+    }
+  },
 
-  USE_EXPERIMENTAL_JAVAFX_UI(
-      SettingType.TESTING,
-      ClientSetting.USE_EXPERIMENTAL_JAVAFX_UI);
+  USE_EXPERIMENTAL_JAVAFX_UI(SettingType.TESTING) {
+    @Override
+    public SelectionComponent<Region> newSelectionComponent() {
+      return toggleButton(ClientSetting.USE_EXPERIMENTAL_JAVAFX_UI);
+    }
+  };
 
+  @Getter(onMethod_ = {@Override})
   private final SettingType type;
-  private final Supplier<SelectionComponent<Region>> selectionComponentFactory;
-
-  ClientSettingJavaFxUiBinding(
-      final SettingType type,
-      final Supplier<SelectionComponent<Region>> selectionComponentFactory) {
-    this.type = type;
-    this.selectionComponentFactory = selectionComponentFactory;
-  }
-
-  ClientSettingJavaFxUiBinding(final SettingType type, final ClientSetting setting) {
-    this(type, JavaFxSelectionComponentFactory.toggleButton(setting));
-  }
-
-  @Override
-  public SelectionComponent<Region> newSelectionComponent() {
-    return selectionComponentFactory.get();
-  }
 
   @Override
   public String getTitle() {
     return "";
-  }
-
-  @Override
-  public SettingType getType() {
-    return type;
   }
 }
