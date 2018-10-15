c1bc2bdd5963f471bf6b8e9d766f3e968abe8cf9
Steven Soloff <steven.soloff@gmail.com>
2018-10-15 00:07:04 -0400

    Improve hit differential grouping for AA dice

--- game-core/src/test/java/games/strategy/triplea/ui/history/HistoryLogTest.java
+++ game-core/src/test/java/games/strategy/triplea/ui/history/HistoryLogTest.java
@@ -1,6 +1,6 @@
 package games.strategy.triplea.ui.history;
 
-import static games.strategy.triplea.ui.history.HistoryLog.parsePlayerNameFromDiceRollMessage;
+import static games.strategy.triplea.ui.history.HistoryLog.parseHitDifferentialKeyFromDiceRollMessage;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
@@ -9,20 +9,29 @@ import org.junit.jupiter.api.Test;
 
 final class HistoryLogTest {
   @Nested
-  final class ParsePlayerNameFromDiceRollMessageTest {
-
+  final class ParseHitDifferentialKeyFromDiceRollMessageTest {
     @Test
-    void shouldParsePlayerName() {
+    void shouldReturnPlayerNameAndRegularDiceTypeWhenMessageContainsRegularDiceRoll() {
       assertThat(
-          parsePlayerNameFromDiceRollMessage("Japanese roll dice for 1 battleship in Australia, round 2 :"),
-          is("Japanese"));
+          parseHitDifferentialKeyFromDiceRollMessage("Russians roll dice for 1 fighter in Karelia S.S.R., round 3 :"),
+          is("Russians regular"));
     }
 
     @Test
-    void shouldParsePlayerNameWithoutRoll() {
+    void shouldReturnPlayerNameAndAaDiceTypeWhenMessageContainsAaDiceRoll() {
       assertThat(
-          parsePlayerNameFromDiceRollMessage("Japanese"),
-          is("Japanese"));
+          parseHitDifferentialKeyFromDiceRollMessage("Russians roll AA dice in Karelia S.S.R. :"),
+          is("Russians AA"));
+    }
+
+    @Test
+    void shouldReturnMessageWithoutTrailingColonWhenMessageDoesNotContainDiceRoll() {
+      assertThat(
+          parseHitDifferentialKeyFromDiceRollMessage("AA fire in Karelia S.S.R."),
+          is("AA fire in Karelia S.S.R."));
+      assertThat(
+          parseHitDifferentialKeyFromDiceRollMessage("AA fire in Karelia S.S.R. :"),
+          is("AA fire in Karelia S.S.R."));
     }
   }
 }
