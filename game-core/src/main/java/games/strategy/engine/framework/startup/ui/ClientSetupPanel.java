a24270f3e32cdced831d30bd4579ef312a6e1518
simon@33-2 <simon@58500bigpond.com>
2018-10-23 20:06:04 +1100

    Allow user to load autosaves on bots at End Turn, End move phases and before and after battles

--- game-core/src/main/java/games/strategy/engine/framework/startup/ui/ClientSetupPanel.java
+++ game-core/src/main/java/games/strategy/engine/framework/startup/ui/ClientSetupPanel.java
@@ -254,6 +254,10 @@ public class ClientSetupPanel extends SetupPanel {
     actions.add(clientModel.getHostBotChangeToAutosaveClientAction(this, AUTOSAVE_TYPE.AUTOSAVE));
     actions.add(clientModel.getHostBotChangeToAutosaveClientAction(this, AUTOSAVE_TYPE.AUTOSAVE_ODD));
     actions.add(clientModel.getHostBotChangeToAutosaveClientAction(this, AUTOSAVE_TYPE.AUTOSAVE_EVEN));
+    actions.add(clientModel.getHostBotChangeToAutosaveClientAction(this, AUTOSAVE_TYPE.AUTOSAVE_END_TURN));
+    actions.add(clientModel.getHostBotChangeToAutosaveClientAction(this, AUTOSAVE_TYPE.AUTOSAVE_BEFORE_BATTLE));
+    actions.add(clientModel.getHostBotChangeToAutosaveClientAction(this, AUTOSAVE_TYPE.AUTOSAVE_AFTER_COMBAT_MOVE));
+    actions.add(clientModel.getHostBotChangeToAutosaveClientAction(this, AUTOSAVE_TYPE.AUTOSAVE_AFTER_NON_COMBAT_MOVE));
     actions.add(clientModel.getHostBotGetGameSaveClientAction(this));
     return actions;
   }
