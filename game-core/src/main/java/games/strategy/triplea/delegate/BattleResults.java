0d1e8f5bdf730f085353876414ad1e4fc8e52e8e
RoiEX <8350879+RoiEXLab@users.noreply.github.com>
2018-10-27 00:26:29 +0200

    Rename fields

--- game-core/src/main/java/games/strategy/triplea/delegate/BattleResults.java
+++ game-core/src/main/java/games/strategy/triplea/delegate/BattleResults.java
@@ -5,18 +5,16 @@ import java.util.List;
 import games.strategy.engine.data.GameData;
 import games.strategy.engine.data.GameDataComponent;
 import games.strategy.engine.data.Unit;
-import games.strategy.triplea.delegate.IBattle;
 import games.strategy.triplea.delegate.IBattle.WhoWon;
-import games.strategy.triplea.delegate.Matches;
 import games.strategy.util.CollectionUtils;
 
 public class BattleResults extends GameDataComponent {
   private static final long serialVersionUID = 1381361441940258702L;
 
-  private final int m_battleRoundsFought;
-  final List<Unit> m_remainingAttackingUnits;
-  final List<Unit> m_remainingDefendingUnits;
-  private final WhoWon m_whoWon;
+  private final int battleRoundsFought;
+  private final List<Unit> remainingAttackingUnits;
+  private final List<Unit> remainingDefendingUnits;
+  private final WhoWon whoWon;
 
   // FYI: do not save the battle in BattleResults. It is both too much memory overhead, and also causes problems with
   // BattleResults being
@@ -28,11 +26,11 @@ public class BattleResults extends GameDataComponent {
    */
   public BattleResults(final IBattle battle, final GameData data) {
     super(data);
-    m_battleRoundsFought = battle.getBattleRound();
-    m_remainingAttackingUnits = battle.getRemainingAttackingUnits();
-    m_remainingDefendingUnits = battle.getRemainingDefendingUnits();
-    m_whoWon = battle.getWhoWon();
-    if (m_whoWon == WhoWon.NOTFINISHED) {
+    battleRoundsFought = battle.getBattleRound();
+    remainingAttackingUnits = battle.getRemainingAttackingUnits();
+    remainingDefendingUnits = battle.getRemainingDefendingUnits();
+    whoWon = battle.getWhoWon();
+    if (whoWon == WhoWon.NOTFINISHED) {
       throw new IllegalStateException("Battle not finished yet: " + battle);
     }
   }
@@ -42,47 +40,47 @@ public class BattleResults extends GameDataComponent {
    */
   public BattleResults(final IBattle battle, final WhoWon scriptedWhoWon, final GameData data) {
     super(data);
-    m_battleRoundsFought = battle.getBattleRound();
-    m_remainingAttackingUnits = battle.getRemainingAttackingUnits();
-    m_remainingDefendingUnits = battle.getRemainingDefendingUnits();
-    m_whoWon = scriptedWhoWon;
+    battleRoundsFought = battle.getBattleRound();
+    remainingAttackingUnits = battle.getRemainingAttackingUnits();
+    remainingDefendingUnits = battle.getRemainingDefendingUnits();
+    whoWon = scriptedWhoWon;
   }
 
 
   public List<Unit> getRemainingAttackingUnits() {
-    return m_remainingAttackingUnits;
+    return remainingAttackingUnits;
   }
 
   public List<Unit> getRemainingDefendingUnits() {
-    return m_remainingDefendingUnits;
+    return remainingDefendingUnits;
   }
 
   public int getAttackingCombatUnitsLeft() {
-    return CollectionUtils.countMatches(m_remainingAttackingUnits, Matches.unitIsNotInfrastructure());
+    return CollectionUtils.countMatches(remainingAttackingUnits, Matches.unitIsNotInfrastructure());
   }
 
   public int getDefendingCombatUnitsLeft() {
-    return CollectionUtils.countMatches(m_remainingDefendingUnits, Matches.unitIsNotInfrastructure());
+    return CollectionUtils.countMatches(remainingDefendingUnits, Matches.unitIsNotInfrastructure());
   }
 
   public int getBattleRoundsFought() {
-    return m_battleRoundsFought;
+    return battleRoundsFought;
   }
 
   // These could easily screw up an AI into thinking it has won when it really hasn't. Must make sure we only count
   // combat units that can
   // die.
   public boolean attackerWon() {
-    return !draw() && m_whoWon == WhoWon.ATTACKER;
+    return !draw() && whoWon == WhoWon.ATTACKER;
   }
 
   public boolean defenderWon() {
-    // if noone is left, it is considered a draw, even if m_whoWon says defender.
-    return !draw() && m_whoWon == WhoWon.DEFENDER;
+    // if noone is left, it is considered a draw, even if whoWon says defender.
+    return !draw() && whoWon == WhoWon.DEFENDER;
   }
 
   public boolean draw() {
-    return (m_whoWon != WhoWon.ATTACKER && m_whoWon != WhoWon.DEFENDER)
+    return (whoWon != WhoWon.ATTACKER && whoWon != WhoWon.DEFENDER)
         || (getAttackingCombatUnitsLeft() == 0 && getDefendingCombatUnitsLeft() == 0);
   }
 }
