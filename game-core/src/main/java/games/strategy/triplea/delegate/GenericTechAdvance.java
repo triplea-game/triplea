119a552666139e8f455f70851531b1ae0c73ba7b
Steven Soloff <steven.soloff@gmail.com>
2018-10-29 11:39:47 -0400

    Fix naming violations in GenericTechAdvance

--- game-core/src/main/java/games/strategy/triplea/delegate/GenericTechAdvance.java
+++ game-core/src/main/java/games/strategy/triplea/delegate/GenericTechAdvance.java
@@ -7,33 +7,34 @@ import games.strategy.triplea.attachments.TechAttachment;
 
 public class GenericTechAdvance extends TechAdvance {
   private static final long serialVersionUID = -5985281030083508185L;
-  private final TechAdvance m_advance;
+
+  private final TechAdvance advance;
 
   public GenericTechAdvance(final String name, final TechAdvance techAdvance, final GameData data) {
     super(name, data);
-    m_advance = techAdvance;
+    advance = techAdvance;
   }
 
   @Override
   public String getProperty() {
-    return (m_advance != null) ? m_advance.getProperty() : getName();
+    return (advance != null) ? advance.getProperty() : getName();
   }
 
   @Override
   public void perform(final PlayerID id, final IDelegateBridge bridge) {
-    if (m_advance != null) {
-      m_advance.perform(id, bridge);
+    if (advance != null) {
+      advance.perform(id, bridge);
     }
   }
 
   public TechAdvance getAdvance() {
-    return m_advance;
+    return advance;
   }
 
   @Override
   public boolean hasTech(final TechAttachment ta) {
-    if (m_advance != null) {
-      return m_advance.hasTech(ta);
+    if (advance != null) {
+      return advance.hasTech(ta);
     }
     // this can be null!!!
     final Boolean has = ta.hasGenericTech(getName());
