e35b7882c95fd75067a4287cc035e571a6a43df8
Steven Soloff <steven.soloff@gmail.com>
2018-10-29 20:32:24 -0400

    Fix naming violations in MoveExtendedDelegateState

--- game-core/src/main/java/games/strategy/triplea/delegate/MoveExtendedDelegateState.java
+++ game-core/src/main/java/games/strategy/triplea/delegate/MoveExtendedDelegateState.java
@@ -7,9 +7,10 @@ import games.strategy.util.IntegerMap;
 
 class MoveExtendedDelegateState implements Serializable {
   private static final long serialVersionUID = 5352248885420819215L;
+
   Serializable superState;
   // add other variables here:
-  public boolean m_needToInitialize;
-  public boolean m_needToDoRockets;
-  public IntegerMap<Territory> m_PUsLost;
+  public boolean needToInitialize;
+  public boolean needToDoRockets;
+  public IntegerMap<Territory> pusLost;
 }
