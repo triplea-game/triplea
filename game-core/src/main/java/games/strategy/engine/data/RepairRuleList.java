df27f38082980146dae4d0794a8f8e9901ebf92b
Steven Soloff <steven.soloff@gmail.com>
2018-11-02 12:11:27 -0400

    Fix naming violations in RepairRuleList

--- game-core/src/main/java/games/strategy/engine/data/RepairRuleList.java
+++ game-core/src/main/java/games/strategy/engine/data/RepairRuleList.java
@@ -10,21 +10,22 @@ import java.util.Map;
 // TODO: rename this class upon next incompatible release to replace "List" suffix since this collection is not ordered
 public class RepairRuleList extends GameDataComponent {
   private static final long serialVersionUID = 8153102637443800391L;
-  private final Map<String, RepairRule> m_repairRules = new HashMap<>();
+
+  private final Map<String, RepairRule> repairRules = new HashMap<>();
 
   public RepairRuleList(final GameData data) {
     super(data);
   }
 
   protected void addRepairRule(final RepairRule pf) {
-    m_repairRules.put(pf.getName(), pf);
+    repairRules.put(pf.getName(), pf);
   }
 
   public RepairRule getRepairRule(final String name) {
-    return m_repairRules.get(name);
+    return repairRules.get(name);
   }
 
   public Collection<RepairRule> getRepairRules() {
-    return m_repairRules.values();
+    return repairRules.values();
   }
 }
