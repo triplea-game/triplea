1952471c12a938377b31d08095d6c75296e79f59
Steven Soloff <steven.soloff@gmail.com>
2018-11-02 11:46:53 -0400

    Fix naming violations in DefaultNamed

--- game-core/src/main/java/games/strategy/engine/data/DefaultNamed.java
+++ game-core/src/main/java/games/strategy/engine/data/DefaultNamed.java
@@ -11,37 +11,38 @@ import com.google.common.base.MoreObjects;
  */
 public class DefaultNamed extends GameDataComponent implements Named {
   private static final long serialVersionUID = -5737716450699952621L;
-  private final String m_name;
+
+  private final String name;
 
   public DefaultNamed(final String name, final GameData data) {
     super(data);
     if (name == null || name.length() == 0) {
       throw new IllegalArgumentException("Name must not be null");
     }
-    m_name = name;
+    this.name = name;
   }
 
   @Override
   public String getName() {
-    return m_name;
+    return name;
   }
 
   @Override
   public boolean equals(final Object other) {
     if (other instanceof Named) {
-      return Objects.equals(m_name, ((Named) other).getName());
+      return Objects.equals(name, ((Named) other).getName());
     }
     return false;
   }
 
   @Override
   public int hashCode() {
-    return Objects.hashCode(m_name);
+    return Objects.hashCode(name);
   }
 
   @Override
   public String toString() {
-    return MoreObjects.toStringHelper(getClass()).add("name", m_name).toString();
+    return MoreObjects.toStringHelper(getClass()).add("name", name).toString();
   }
 
   // Workaround for JDK-8199664
