3afdb863c0d5a3be809f6ce275460e8a9fbc9383
Steven Soloff <steven.soloff@gmail.com>
2018-11-02 12:21:12 -0400

    Fix naming violations in Route

--- game-core/src/main/java/games/strategy/engine/data/Route.java
+++ game-core/src/main/java/games/strategy/engine/data/Route.java
@@ -39,8 +39,9 @@ import games.strategy.util.Tuple;
 public class Route implements Serializable, Iterable<Territory> {
   private static final long serialVersionUID = 8743882455488948557L;
   private static final List<Territory> EMPTY_TERRITORY_LIST = Collections.emptyList();
-  private final List<Territory> m_steps = new ArrayList<>();
-  private Territory m_start;
+
+  private final List<Territory> steps = new ArrayList<>();
+  private Territory start;
 
   public Route() {}
 
@@ -85,9 +86,9 @@ public class Route implements Serializable, Iterable<Territory> {
         throw new IllegalArgumentException("Cannot join, r1 doesnt end where r2 starts. r1:" + r1 + " r2:" + r2);
       }
     }
-    final Collection<Territory> c1 = new ArrayList<>(r1.m_steps);
+    final Collection<Territory> c1 = new ArrayList<>(r1.steps);
     c1.add(r1.getStart());
-    final Collection<Territory> c2 = new ArrayList<>(r2.m_steps);
+    final Collection<Territory> c2 = new ArrayList<>(r2.steps);
     if (!CollectionUtils.intersection(c1, c2).isEmpty()) {
       return null;
     }
@@ -116,7 +117,7 @@ public class Route implements Serializable, Iterable<Territory> {
 
   @Override
   public int hashCode() {
-    return Objects.hash(m_start, getSteps());
+    return Objects.hash(start, getSteps());
   }
 
   /**
@@ -126,14 +127,14 @@ public class Route implements Serializable, Iterable<Territory> {
     if (newStartTerritory == null) {
       throw new IllegalStateException("Null territory");
     }
-    m_start = newStartTerritory;
+    start = newStartTerritory;
   }
 
   /**
    * Returns start territory for this route.
    */
   public Territory getStart() {
-    return m_start;
+    return start;
   }
 
   /**
@@ -146,7 +147,7 @@ public class Route implements Serializable, Iterable<Territory> {
     if (hasNoSteps()) {
       return false;
     }
-    final boolean startLand = !m_start.isWater();
+    final boolean startLand = !start.isWater();
     final boolean overWater = anyMatch(Territory::isWater);
 
     // If we started on land, went over water, and ended on land, we cross water.
@@ -160,18 +161,18 @@ public class Route implements Serializable, Iterable<Territory> {
     if (territory == null) {
       throw new IllegalStateException("Null territory");
     }
-    if (territory.equals(m_start) || m_steps.contains(territory)) {
+    if (territory.equals(start) || steps.contains(territory)) {
       throw new IllegalArgumentException(String.format(
-          "Loops not allowed in m_routes, route: %s, new territory: %s", this, territory));
+          "Loops not allowed in steps, route: %s, new territory: %s", this, territory));
     }
-    m_steps.add(territory);
+    steps.add(territory);
   }
 
   /**
    * Returns the number of steps in this route. Does not include start.
    */
   public int numberOfSteps() {
-    return m_steps.size();
+    return steps.size();
   }
 
   /**
@@ -187,7 +188,7 @@ public class Route implements Serializable, Iterable<Territory> {
    * @param i step number
    */
   public Territory getTerritoryAtStep(final int i) {
-    return m_steps.get(i);
+    return steps.get(i);
   }
 
   /**
@@ -197,7 +198,7 @@ public class Route implements Serializable, Iterable<Territory> {
    * @return whether any territories in this route match the given match (start territory is not tested).
    */
   public boolean anyMatch(final Predicate<Territory> match) {
-    return m_steps.stream().anyMatch(match);
+    return steps.stream().anyMatch(match);
   }
 
   /**
@@ -224,12 +225,12 @@ public class Route implements Serializable, Iterable<Territory> {
    * @param match referring match
    */
   public Collection<Territory> getMatches(final Predicate<Territory> match) {
-    return CollectionUtils.getMatches(m_steps, match);
+    return CollectionUtils.getMatches(steps, match);
   }
 
   @Override
   public String toString() {
-    final StringBuilder buf = new StringBuilder("Route:").append(m_start);
+    final StringBuilder buf = new StringBuilder("Route:").append(start);
     for (final Territory t : getSteps()) {
       buf.append(" -> ");
       buf.append(t.getName());
@@ -238,8 +239,8 @@ public class Route implements Serializable, Iterable<Territory> {
   }
 
   public List<Territory> getAllTerritories() {
-    final List<Territory> list = new ArrayList<>(m_steps);
-    list.add(0, m_start);
+    final List<Territory> list = new ArrayList<>(steps);
+    list.add(0, start);
     return list;
   }
 
@@ -248,7 +249,7 @@ public class Route implements Serializable, Iterable<Territory> {
    */
   public List<Territory> getSteps() {
     if (numberOfSteps() > 0) {
-      return new ArrayList<>(m_steps);
+      return new ArrayList<>(steps);
     }
     return EMPTY_TERRITORY_LIST;
   }
@@ -258,7 +259,7 @@ public class Route implements Serializable, Iterable<Territory> {
    */
   public List<Territory> getMiddleSteps() {
     if (numberOfSteps() > 1) {
-      return new ArrayList<>(m_steps).subList(0, numberOfSteps() - 1);
+      return new ArrayList<>(steps).subList(0, numberOfSteps() - 1);
     }
     return EMPTY_TERRITORY_LIST;
   }
@@ -267,10 +268,10 @@ public class Route implements Serializable, Iterable<Territory> {
    * Returns last territory in the route.
    */
   public Territory getEnd() {
-    if (m_steps.isEmpty()) {
-      return m_start;
+    if (steps.isEmpty()) {
+      return start;
     }
-    return m_steps.get(m_steps.size() - 1);
+    return steps.get(steps.size() - 1);
   }
 
   @Override
@@ -282,7 +283,7 @@ public class Route implements Serializable, Iterable<Territory> {
    * Indicates whether this route has any steps.
    */
   public boolean hasSteps() {
-    return !m_steps.isEmpty();
+    return !steps.isEmpty();
   }
 
   /**
@@ -298,7 +299,7 @@ public class Route implements Serializable, Iterable<Territory> {
    * @return whether the route has 1 step
    */
   public boolean hasExactlyOneStep() {
-    return this.m_steps.size() == 1;
+    return this.steps.size() == 1;
   }
 
   /**
@@ -308,7 +309,7 @@ public class Route implements Serializable, Iterable<Territory> {
    * @return the territory before the end territory
    */
   public Territory getTerritoryBeforeEnd() {
-    return (m_steps.size() <= 1) ? getStart() : getTerritoryAtStep(m_steps.size() - 2);
+    return (steps.size() <= 1) ? getStart() : getTerritoryAtStep(steps.size() - 2);
   }
 
   /**
@@ -338,7 +339,7 @@ public class Route implements Serializable, Iterable<Territory> {
    * Indicates whether this route has more then one step.
    */
   public boolean hasMoreThenOneStep() {
-    return m_steps.size() > 1;
+    return steps.size() > 1;
   }
 
   /**
