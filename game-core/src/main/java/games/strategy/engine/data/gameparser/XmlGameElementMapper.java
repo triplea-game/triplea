1dc5b1426ead044894a8e93483038f19806ae75f
Steven Soloff <steven.soloff@gmail.com>
2018-11-03 12:32:41 -0400

    Replace TwoIfBySeaInitDelegate with InitializationDelegate
    These two classes have identical implementations.

--- game-core/src/main/java/games/strategy/engine/data/gameparser/XmlGameElementMapper.java
+++ game-core/src/main/java/games/strategy/engine/data/gameparser/XmlGameElementMapper.java
@@ -157,9 +157,7 @@ public class XmlGameElementMapper {
         .put(
             "games.strategy.twoIfBySea.delegate.EndTurnDelegate",
             games.strategy.triplea.delegate.TwoIfBySeaEndTurnDelegate::new)
-        .put(
-            "games.strategy.twoIfBySea.delegate.InitDelegate",
-            games.strategy.triplea.delegate.TwoIfBySeaInitDelegate::new)
+        .put("games.strategy.twoIfBySea.delegate.InitDelegate", InitializationDelegate::new)
         .put(
             "games.strategy.twoIfBySea.delegate.PlaceDelegate",
             games.strategy.triplea.delegate.TwoIfBySeaPlaceDelegate::new)
