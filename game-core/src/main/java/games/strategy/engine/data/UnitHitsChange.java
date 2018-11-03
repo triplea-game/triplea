bb36c4233f83f5f974a33510d7520db3a36e5ed0
Steven Soloff <steven.soloff@gmail.com>
2018-11-03 01:08:24 -0400

    Fix naming violations in UnitHitsChange

--- game-core/src/main/java/games/strategy/engine/data/UnitHitsChange.java
+++ game-core/src/main/java/games/strategy/engine/data/UnitHitsChange.java
@@ -10,28 +10,29 @@ import games.strategy.util.IntegerMap;
  */
 public class UnitHitsChange extends Change {
   private static final long serialVersionUID = 2862726651812142713L;
-  private final IntegerMap<Unit> m_hits;
-  private final IntegerMap<Unit> m_undoHits;
+
+  private final IntegerMap<Unit> hits;
+  private final IntegerMap<Unit> undoHits;
 
   private UnitHitsChange(final IntegerMap<Unit> hits, final IntegerMap<Unit> undoHits) {
-    m_hits = hits;
-    m_undoHits = undoHits;
+    this.hits = hits;
+    this.undoHits = undoHits;
   }
 
   public UnitHitsChange(final IntegerMap<Unit> hits) {
-    m_hits = new IntegerMap<>(hits);
-    m_undoHits = new IntegerMap<>();
-    for (final Unit item : m_hits.keySet()) {
-      m_undoHits.put(item, item.getHits());
+    this.hits = new IntegerMap<>(hits);
+    undoHits = new IntegerMap<>();
+    for (final Unit item : this.hits.keySet()) {
+      undoHits.put(item, item.getHits());
     }
   }
 
   @Override
   protected void perform(final GameData data) {
-    for (final Unit item : m_hits.keySet()) {
-      item.setHits(m_hits.getInt(item));
+    for (final Unit item : hits.keySet()) {
+      item.setHits(hits.getInt(item));
     }
-    final Set<Unit> units = m_hits.keySet();
+    final Set<Unit> units = hits.keySet();
     for (final Territory element : data.getMap().getTerritories()) {
       if (CollectionUtils.someIntersect(element.getUnits().getUnits(), units)) {
         element.notifyChanged();
@@ -41,6 +42,6 @@ public class UnitHitsChange extends Change {
 
   @Override
   public Change invert() {
-    return new UnitHitsChange(m_undoHits, m_hits);
+    return new UnitHitsChange(undoHits, hits);
   }
 }
