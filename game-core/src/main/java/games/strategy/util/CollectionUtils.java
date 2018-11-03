df747646c1ba44757d0ff91151b77a99490ab2e6
RoiEX <8350879+RoiEXLab@users.noreply.github.com>
2018-11-03 23:44:56 +0100

    Remove unused import

--- game-core/src/main/java/games/strategy/util/CollectionUtils.java
+++ game-core/src/main/java/games/strategy/util/CollectionUtils.java
@@ -5,7 +5,6 @@ import static com.google.common.base.Preconditions.checkNotNull;
 
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 import java.util.function.Predicate;
