6f9bc937909b4e23b0d9cc8976f9c66bc79bac24
Steven Soloff <steven.soloff@gmail.com>
2018-11-10 01:48:56 -0500

    Remove deprecated HeadlessAutoSaveType enum constant (#4338)
    * Remove deprecated AUTOSAVE2 enum constant
    
    * Remove redundant AUTOSAVE prefix from enum constants

--- game-core/src/main/java/games/strategy/engine/framework/HeadlessAutoSaveType.java
+++ game-core/src/main/java/games/strategy/engine/framework/HeadlessAutoSaveType.java
@@ -7,31 +7,21 @@ import java.nio.file.Path;
  * The types of auto-saves that can be loaded by a headless game server.
  */
 public enum HeadlessAutoSaveType {
-  AUTOSAVE(AutoSaveFileUtils.getHeadlessAutoSaveFile()),
+  DEFAULT(AutoSaveFileUtils.getHeadlessAutoSaveFile()),
 
-  /**
-   * A second auto-save that a headless game server will alternate between (the other being {@link #AUTOSAVE}).
-   *
-   * @deprecated No longer supported. If an old client happens to request this auto-save, it now forwards to the
-   *             same file as {@link #AUTOSAVE} instead of simply doing nothing. Remove upon next stable release (i.e.
-   *             once no stable client will ever request this auto-save).
-   */
-  @Deprecated
-  AUTOSAVE2(AutoSaveFileUtils.getHeadlessAutoSaveFile()),
+  ODD_ROUND(AutoSaveFileUtils.getOddRoundAutoSaveFile(true)),
 
-  AUTOSAVE_ODD(AutoSaveFileUtils.getOddRoundAutoSaveFile(true)),
+  EVEN_ROUND(AutoSaveFileUtils.getEvenRoundAutoSaveFile(true)),
 
-  AUTOSAVE_EVEN(AutoSaveFileUtils.getEvenRoundAutoSaveFile(true)),
+  END_TURN(AutoSaveFileUtils.getBeforeStepAutoSaveFile("EndTurn", true)),
 
-  AUTOSAVE_END_TURN(AutoSaveFileUtils.getBeforeStepAutoSaveFile("EndTurn", true)),
+  BEFORE_BATTLE(AutoSaveFileUtils.getBeforeStepAutoSaveFile("Battle", true)),
 
-  AUTOSAVE_BEFORE_BATTLE(AutoSaveFileUtils.getBeforeStepAutoSaveFile("Battle", true)),
+  AFTER_BATTLE(AutoSaveFileUtils.getAfterStepAutoSaveFile("Battle", true)),
 
-  AUTOSAVE_AFTER_BATTLE(AutoSaveFileUtils.getAfterStepAutoSaveFile("Battle", true)),
+  AFTER_COMBAT_MOVE(AutoSaveFileUtils.getAfterStepAutoSaveFile("CombatMove", true)),
 
-  AUTOSAVE_AFTER_COMBAT_MOVE(AutoSaveFileUtils.getAfterStepAutoSaveFile("CombatMove", true)),
-
-  AUTOSAVE_AFTER_NON_COMBAT_MOVE(AutoSaveFileUtils.getAfterStepAutoSaveFile("NonCombatMove", true));
+  AFTER_NON_COMBAT_MOVE(AutoSaveFileUtils.getAfterStepAutoSaveFile("NonCombatMove", true));
 
   private final Path path;
 
