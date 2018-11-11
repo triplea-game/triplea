3fa73670ad34a9123f6a5ea598d09e34e9ba6622
RoiEX <8350879+RoiEXLab@users.noreply.github.com>
2018-11-11 19:00:56 +0100

    Add basic Test case

---
+++ game-core/src/test/java/games/strategy/triplea/settings/ProtectedStringClientSettingTest.java
@@ -0,0 +1,27 @@
+package games.strategy.triplea.settings;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+final class ProtectedStringClientSettingTest {
+  private final ProtectedStringClientSetting clientSetting = new ProtectedStringClientSetting("name", "");
+
+  @Nested
+  final class FormatValueTest {
+    @Test
+    void shouldReturnValueUnchanged() {
+      assertThat(clientSetting.formatValue("value"), is("value"));
+    }
+  }
+
+  @Nested
+  final class ParseValueTest {
+    @Test
+    void shouldReturnEncodedValueUnchanged() {
+      assertThat(clientSetting.parseValue("encodedValue"), is("encodedValue"));
+    }
+  }
+}
