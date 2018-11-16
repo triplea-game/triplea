363e26e26c5c13946b82c938280c142859027cb1
RoiEX <8350879+RoiEXLab@users.noreply.github.com>
2018-11-16 23:44:00 +0100

    Add additional test

--- game-core/src/test/java/games/strategy/triplea/settings/ProtectedStringClientSettingTest.java
+++ game-core/src/test/java/games/strategy/triplea/settings/ProtectedStringClientSettingTest.java
@@ -3,20 +3,38 @@ package games.strategy.triplea.settings;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.sonatype.goodies.prefs.memory.MemoryPreferences;
 
 import games.strategy.security.CredentialManager;
 
 final class ProtectedStringClientSettingTest {
   private final ProtectedStringClientSetting clientSetting = new ProtectedStringClientSetting("name", false);
 
+  @BeforeAll
+  static void setup() {
+    ClientSetting.setPreferences(new MemoryPreferences());
+  }
+
+  @Test
+  void testDisplayValue() {
+    clientSetting.setValue("$eCrEt".toCharArray());
+    assertThat(clientSetting.getDisplayValue(), is("$eCrEt"));
+    final ProtectedStringClientSetting sensitive = new ProtectedStringClientSetting("name", true);
+    sensitive.setValue("$eCrEt".toCharArray());
+    assertThat(sensitive.getDisplayValue(), is("******"));
+  }
+
   @Nested
   final class FormatValueTest {
     @Test
     void shouldReturnValueUnchanged() throws Exception {
       try (CredentialManager manager = CredentialManager.newInstance()) {
-        assertThat(manager.unprotectToString(clientSetting.formatValue("value".toCharArray())), is("value"));
+        final char[] sensitiveValue = "value".toCharArray();
+        assertThat(manager.unprotectToString(clientSetting.formatValue(sensitiveValue)), is("value"));
+        assertThat(sensitiveValue, is(new char[]{0, 0, 0, 0, 0}));
       }
     }
   }
