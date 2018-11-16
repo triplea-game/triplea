8e38024b1f9b1ff38f022894a52a3e33734a030f
RoiEX <8350879+RoiEXLab@users.noreply.github.com>
2018-11-16 22:47:05 +0100

    Fix Tests

--- game-core/src/test/java/games/strategy/triplea/settings/ProtectedStringClientSettingTest.java
+++ game-core/src/test/java/games/strategy/triplea/settings/ProtectedStringClientSettingTest.java
@@ -6,22 +6,28 @@ import static org.hamcrest.Matchers.is;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
+import games.strategy.security.CredentialManager;
+
 final class ProtectedStringClientSettingTest {
   private final ProtectedStringClientSetting clientSetting = new ProtectedStringClientSetting("name", "", false);
 
   @Nested
   final class FormatValueTest {
     @Test
-    void shouldReturnValueUnchanged() {
-      assertThat(clientSetting.formatValue("value"), is("value"));
+    void shouldReturnValueUnchanged() throws Exception {
+      try (CredentialManager manager = CredentialManager.newInstance()) {
+        assertThat(manager.unprotectToString(clientSetting.formatValue("value")), is("value"));
+      }
     }
   }
 
   @Nested
   final class ParseValueTest {
     @Test
-    void shouldReturnEncodedValueUnchanged() {
-      assertThat(clientSetting.parseValue("encodedValue"), is("encodedValue"));
+    void shouldReturnEncodedValueUnchanged() throws Exception {
+      try (CredentialManager manager = CredentialManager.newInstance()) {
+        assertThat(clientSetting.parseValue(manager.protect("encodedValue")), is("encodedValue"));
+      }
     }
   }
 }
