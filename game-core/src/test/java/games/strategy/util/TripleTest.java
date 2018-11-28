123cbca69ba60779f2d226eac2062713ec34f6a9
Steven Soloff <steven.soloff@gmail.com>
2018-11-28 15:26:11 -0500

    Fix EqualsGetClass violations in Triple

---
+++ game-core/src/test/java/games/strategy/util/TripleTest.java
@@ -0,0 +1,16 @@
+package games.strategy.util;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import nl.jqno.equalsverifier.EqualsVerifier;
+
+final class TripleTest {
+  @Nested
+  final class EqualsAndHashCodeTest {
+    @Test
+    void shouldBeEquatableAndHashable() {
+      EqualsVerifier.forClass(Triple.class).verify();
+    }
+  }
+}
