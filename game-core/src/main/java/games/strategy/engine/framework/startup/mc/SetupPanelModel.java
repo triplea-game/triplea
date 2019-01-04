9ea198267be012e495dc077be40279a4f1894738
Dan Van Atta <dhvatta@gmail.com>
2019-01-04 01:51:19 -0800

    Put LobbyServerProperties behind cache (#4503)
    - Here we add a transparent cache to LobbyServerPropertiesFetcher, this
    way if we invoke the fetch from multiple places we will not be doing a
    remote network call each time.
    - Introduce a configuration object to construct our
    LobbyServerPropertiesFetcher and to keep hold of a singleton instance

--- game-core/src/main/java/games/strategy/engine/framework/startup/mc/SetupPanelModel.java
+++ game-core/src/main/java/games/strategy/engine/framework/startup/mc/SetupPanelModel.java
@@ -18,7 +18,7 @@ import games.strategy.engine.framework.startup.ui.PbemSetupPanel;
 import games.strategy.engine.framework.startup.ui.ServerSetupPanel;
 import games.strategy.engine.framework.startup.ui.panels.main.ScreenChangeListener;
 import games.strategy.engine.lobby.client.login.LobbyLogin;
-import games.strategy.engine.lobby.client.login.LobbyServerPropertiesFetcher;
+import games.strategy.engine.lobby.client.login.LobbyPropertyFetcherConfiguration;
 import games.strategy.engine.lobby.client.ui.LobbyFrame;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -105,7 +105,7 @@ public class SetupPanelModel {
    * @param uiParent Used to center pop-up's prompting user for their lobby credentials.
    */
   public void login(final Component uiParent) {
-    new LobbyServerPropertiesFetcher().fetchLobbyServerProperties()
+    LobbyPropertyFetcherConfiguration.lobbyServerPropertiesFetcher().fetchLobbyServerProperties()
         .ifPresent(lobbyServerProperties -> {
           final LobbyLogin login =
               new LobbyLogin(JOptionPane.getFrameForComponent(uiParent), lobbyServerProperties);
