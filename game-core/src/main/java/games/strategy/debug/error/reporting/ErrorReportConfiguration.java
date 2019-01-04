9df2a737e1e63039400feaa803b499ebe4cfcfab
DanVanAtta <dhvatta@gmail.com>
2019-01-04 10:57:16 -0800

    Replace if statement with equivalent mapping statements.

--- game-core/src/main/java/games/strategy/debug/error/reporting/ErrorReportConfiguration.java
+++ game-core/src/main/java/games/strategy/debug/error/reporting/ErrorReportConfiguration.java
@@ -1,6 +1,7 @@
 package games.strategy.debug.error.reporting;
 
 import java.net.URI;
+import java.util.Optional;
 import java.util.function.BiConsumer;
 
 import javax.swing.JFrame;
@@ -18,19 +19,26 @@ class ErrorReportConfiguration {
    * dialog and sending error report data to the remote http server.
    */
   static BiConsumer<JFrame, UserErrorReport> newReportHandler() {
-    if (ClientSetting.httpLobbyUriOverride.isSet()) {
-      return new ErrorReportUploadAction(
-          ErrorReportClientFactory.newErrorUploader(
-              URI.create(ClientSetting.httpLobbyUriOverride.getValueOrThrow())));
-    }
+    return newFromUserOverride()
+        .orElseGet(
+            () -> newFromRemoteProperties().orElse(ErrorReportUploadAction.OFFLINE_STRATEGY));
+  }
 
+  private static Optional<BiConsumer<JFrame, UserErrorReport>> newFromUserOverride() {
+    return ClientSetting.httpLobbyUriOverride
+        .getValue()
+        .map(URI::create)
+        .map(ErrorReportClientFactory::newErrorUploader)
+        .map(ErrorReportUploadAction::new);
+  }
+
+  private static Optional<BiConsumer<JFrame, UserErrorReport>> newFromRemoteProperties() {
     return LobbyPropertyFetcherConfiguration.lobbyServerPropertiesFetcher()
         .fetchLobbyServerProperties()
         .<BiConsumer<JFrame, UserErrorReport>>map(
             lobbyServerProperties ->
                 new ErrorReportUploadAction(
                     ErrorReportClientFactory.newErrorUploader(
-                        lobbyServerProperties.getHttpServerUri())))
-        .orElse(ErrorReportUploadAction.OFFLINE_STRATEGY);
+                        lobbyServerProperties.getHttpServerUri())));
   }
 }
