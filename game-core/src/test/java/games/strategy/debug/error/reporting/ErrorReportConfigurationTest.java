4c024707c22e9b140a2f7a3b2d07130de0dfb333
DanVanAtta <dhvatta@gmail.com>
2019-01-09 10:39:19 -0800

    Extend AbstractClientsettingtestCase

--- game-core/src/test/java/games/strategy/debug/error/reporting/ErrorReportConfigurationTest.java
+++ game-core/src/test/java/games/strategy/debug/error/reporting/ErrorReportConfigurationTest.java
@@ -3,18 +3,16 @@ package games.strategy.debug.error.reporting;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
-import org.sonatype.goodies.prefs.memory.MemoryPreferences;
 import org.triplea.test.common.Integration;
 
-import games.strategy.triplea.settings.ClientSetting;
+import games.strategy.triplea.settings.AbstractClientSettingTestCase;
 
 
 @Integration
-class ErrorReportConfigurationTest {
+class ErrorReportConfigurationTest extends AbstractClientSettingTestCase {
 
   @Test
   void verifyConstructionIsWithoutError() {
-    ClientSetting.setPreferences(new MemoryPreferences());
     ErrorReportConfiguration.newReportHandler();
     ErrorReportConfiguration.newReportHandler(Optional::empty);
     ErrorReportConfiguration.newReportHandler(() -> Optional.of("http://sample.uri"));
