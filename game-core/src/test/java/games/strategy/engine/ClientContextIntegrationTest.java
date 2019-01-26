a22bcb9a21a067498e5bca1b2bdb99c6427fa67d
RoiEX <8350879+RoiEXLab@users.noreply.github.com>
2019-01-26 02:31:19 +0100

    Remove unused import

--- game-core/src/test/java/games/strategy/engine/ClientContextIntegrationTest.java
+++ game-core/src/test/java/games/strategy/engine/ClientContextIntegrationTest.java
@@ -2,7 +2,6 @@ package games.strategy.engine;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.notNullValue;
-import static org.hamcrest.core.Is.is;
 
 import org.junit.jupiter.api.Test;
 import org.triplea.test.common.Integration;
