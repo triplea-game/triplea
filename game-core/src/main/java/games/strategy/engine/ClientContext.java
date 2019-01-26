be3327fe8a3a7dcbba671b5e5137808df53973d6
RoiEX <8350879+RoiEXLab@users.noreply.github.com>
2019-01-26 01:12:09 +0100

    Use Optional

--- game-core/src/main/java/games/strategy/engine/ClientContext.java
+++ game-core/src/main/java/games/strategy/engine/ClientContext.java
@@ -60,9 +60,9 @@ public final class ClientContext {
   }
 
   public static Optional<List<DownloadFileDescription>> getMapDownloadList() {
-    final String mapDownloadListUrl = ClientSetting.mapListOverride.isSet()
-        ? ClientSetting.mapListOverride.getValueOrThrow().toString()
-        : UrlConstants.MAP_DOWNLOAD_LIST.toString();
+    final String mapDownloadListUrl = ClientSetting.mapListOverride.getValue()
+        .map(Object::toString)
+        .orElse(UrlConstants.MAP_DOWNLOAD_LIST.toString());
 
     return new DownloadRunnable(mapDownloadListUrl).getDownloads();
   }
