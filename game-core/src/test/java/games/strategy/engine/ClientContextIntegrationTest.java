4d71615244f83a00f30515064b1ee34b3da4e962
RoiEX <8350879+RoiEXLab@users.noreply.github.com>
2019-01-27 18:37:03 +0100

    Enhance tests

--- game-core/src/test/java/games/strategy/engine/ClientContextIntegrationTest.java
+++ game-core/src/test/java/games/strategy/engine/ClientContextIntegrationTest.java
@@ -1,11 +1,16 @@
 package games.strategy.engine;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.not;
 import static org.hamcrest.Matchers.notNullValue;
+
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.triplea.test.common.Integration;
 
+import games.strategy.engine.framework.map.download.DownloadFileDescription;
 import games.strategy.triplea.settings.AbstractClientSettingTestCase;
 
 @Integration
@@ -16,6 +21,9 @@ class ClientContextIntegrationTest extends AbstractClientSettingTestCase {
     assertThat(ClientContext.downloadCoordinator(), notNullValue());
     assertThat(ClientContext.engineVersion(), notNullValue());
 
-    assertThat(ClientContext.getMapDownloadList(), notNullValue());
+    final List<DownloadFileDescription> list = ClientContext.getMapDownloadList();
+
+    assertThat(list, notNullValue());
+    assertThat(list, not(empty()));
   }
 }
