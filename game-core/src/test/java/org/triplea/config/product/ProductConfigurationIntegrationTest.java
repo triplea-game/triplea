72a56b2f1336920521f181de7c8356ebb40472e9
DanVanAtta <dhvatta@gmail.com>
2019-02-17 09:59:12 -0800

    Update integration test to not depend on hardcoded version number
    It was a surprise to see a test failure after an explicit version
    update. This can be fixed by removing the hard-coded "1" and "10"
    version numbers from an integration test check and instead just look for
    numbers.

--- game-core/src/test/java/org/triplea/config/product/ProductConfigurationIntegrationTest.java
+++ game-core/src/test/java/org/triplea/config/product/ProductConfigurationIntegrationTest.java
@@ -14,6 +14,6 @@ final class ProductConfigurationIntegrationTest {
   void shouldReadPropertiesFromResource() {
     assertThat(
         productConfiguration.getVersion().getExactVersion(),
-        matchesPattern("1\\.10\\.(@buildId@|dev|\\d+)"));
+        matchesPattern("\\d+\\.\\d+\\.(@buildId@|dev|\\d+)"));
   }
 }
