97b25deb22b3fab66a0187e6f1c86d4916e0e7f6
DanVanAtta <dhvatta@gmail.com>
2019-03-05 13:31:37 -0800

    Simplify No-Op DelegateHistoryWriter
    Simplification update where we use nullable internal state for a no-op rather than injectin ga no-op strategy object.

--- game-core/src/main/java/games/strategy/engine/history/DelegateHistoryWriter.java
+++ game-core/src/main/java/games/strategy/engine/history/DelegateHistoryWriter.java
@@ -1,5 +1,9 @@
 package games.strategy.engine.history;
 
+import java.util.Optional;
+
+import javax.annotation.Nullable;
+
 import games.strategy.engine.framework.IGame;
 import games.strategy.engine.framework.IGameModifiedChannel;
 import games.strategy.engine.message.IChannelMessenger;
@@ -10,28 +14,31 @@ import games.strategy.engine.message.IChannelMessenger;
  * The rest of the history writers functions should only be used by the GameData.
  */
 public class DelegateHistoryWriter implements IDelegateHistoryWriter {
+  public static final DelegateHistoryWriter NO_OP_INSTANCE = new DelegateHistoryWriter();
+
+  @Nullable
   private final IGameModifiedChannel channel;
 
+  private DelegateHistoryWriter() {
+    this((IGameModifiedChannel) null);
+  }
+
   public DelegateHistoryWriter(final IChannelMessenger messenger) {
-    channel = (IGameModifiedChannel) messenger.getChannelBroadcaster(IGame.GAME_MODIFICATION_CHANNEL);
+    this((IGameModifiedChannel) messenger.getChannelBroadcaster(IGame.GAME_MODIFICATION_CHANNEL));
   }
 
   public DelegateHistoryWriter(final IGameModifiedChannel channel) {
     this.channel = channel;
   }
 
-  private IGameModifiedChannel getGameModifiedChannel() {
-    return channel;
-  }
-
   @Override
   public void startEvent(final String eventName, final Object renderingData) {
-    getGameModifiedChannel().startHistoryEvent(eventName, renderingData);
+    Optional.ofNullable(channel).ifPresent(channel -> channel.startHistoryEvent(eventName, renderingData));
   }
 
   @Override
   public void startEvent(final String eventName) {
-    getGameModifiedChannel().startHistoryEvent(eventName);
+    Optional.ofNullable(channel).ifPresent(channel -> channel.startHistoryEvent(eventName));
   }
 
   @Override
@@ -41,6 +48,6 @@ public class DelegateHistoryWriter implements IDelegateHistoryWriter {
 
   @Override
   public void addChildToEvent(final String child, final Object renderingData) {
-    getGameModifiedChannel().addChildToEvent(child, renderingData);
+    Optional.ofNullable(channel).ifPresent(channel -> channel.addChildToEvent(child, renderingData));
   }
 }
