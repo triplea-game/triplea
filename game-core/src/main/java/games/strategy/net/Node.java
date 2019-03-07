9a666672ea652704e9968eba46f0a5c5031fa328
Dan Van Atta <dhvatta@gmail.com>
2019-03-07 15:16:41 -0800

    Use Lombok annotations with Node.java (#4744)

--- game-core/src/main/java/games/strategy/net/Node.java
+++ game-core/src/main/java/games/strategy/net/Node.java
@@ -7,20 +7,30 @@ import java.io.ObjectOutput;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.UnknownHostException;
-import java.util.Objects;
 
 import games.strategy.engine.framework.system.SystemProperties;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 /**
  * Written very often over the network, so make externalizable to make faster and reduce traffic.
  */
+@ToString
+@Getter(onMethod_ = {@Override})
+// NoArgsConstructor needed to support Externalizable
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(exclude = "name")
 public class Node implements INode, Externalizable {
   public static final INode NULL_NODE;
   private static final long serialVersionUID = -2908980662926959943L;
 
   private String name;
-  private int port;
   private InetAddress address;
+  private int port;
 
   static {
     try {
@@ -30,71 +40,16 @@ public class Node implements INode, Externalizable {
     }
   }
 
-  // needed to support Externalizable
-  public Node() {}
-
   public Node(final String name, final InetSocketAddress address) {
-    this.name = name;
-    this.address = address.getAddress();
-    port = address.getPort();
-  }
-
-  public Node(final String name, final InetAddress address, final int port) {
-    this.name = name;
-    this.address = address;
-    this.port = port;
+    this(name, address.getAddress(), address.getPort());
   }
 
   /** Returns the localhost InetAddress. */
-  public static InetAddress getLocalHost() throws UnknownHostException {
+  static InetAddress getLocalHost() throws UnknownHostException {
     // On Mac, InetAddress.getLocalHost() can be extremely slow (30 seconds)
     // due to a bug in macOS Sierra and higher. Use a work around to avoid
     // this. See: https://thoeni.io/post/macos-sierra-java/
     return SystemProperties.isMac() ? InetAddress.getByName("localhost") : InetAddress.getLocalHost();
-  }
-
-  @Override
-  public String getName() {
-    return name;
-  }
-
-  /**
-   * Node equality is done based on network adress/port.
-   * The name is not part of the node identity.
-   */
-  @Override
-  public boolean equals(final Object obj) {
-    if (obj == this) {
-      return true;
-    }
-    if (obj == null) {
-      return false;
-    }
-    if (!(obj instanceof Node)) {
-      return false;
-    }
-    final Node other = (Node) obj;
-    return other.port == this.port && other.address.equals(this.address);
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(port, address);
-  }
-
-  @Override
-  public String toString() {
-    return name + " port:" + port + " ip:" + address.getHostAddress();
-  }
-
-  @Override
-  public int getPort() {
-    return port;
-  }
-
-  @Override
-  public InetAddress getAddress() {
-    return address;
   }
 
   @Override
