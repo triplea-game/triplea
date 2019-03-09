abd59d1f90cdf02d17a72ca8844cd582590efba1
DanVanAtta <dhvatta@gmail.com>
2019-03-09 10:18:01 -0800

    Rename migration file, format and add column comments
    - 's1.10' stands for  'schema 1.10'

--- lobby-db/src/main/resources/db/migration/V1.07__rename_bad_words_table.sql
+++
@@ -1,4 +0,0 @@
-create table bad_word as select word, now() as date_created from bad_words;
-alter table bad_word add primary key (word);
-alter table bad_word alter column date_created set not null;
-alter table bad_word alter column date_created set default now();
