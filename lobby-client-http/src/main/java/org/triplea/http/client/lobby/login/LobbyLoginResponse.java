02081a46f13b8c37675e73616c2fbe7c128bf074
DanVanAtta <dhvatta@gmail.com>
2019-03-28 00:35:43 -0700

    Add Http LobbyLogin client with stubbed server controller
    - Adds a working http client to do lobby login. Not yet integrated with the main client code and for now it is just stand-alone.
    - Adds a stubbed http lobby login controller to listen to login requests.
    
    Next steps:
    - migrate DB tables to new normalized schema
    - replace server login stub with implementation
    - migrate client code to use the new http endpoint and update socket QuarantineConversation.java to use the http endpoint single-use token.

---
+++ lobby-client-http/src/main/java/org/triplea/http/client/lobby/login/LobbyLoginResponse.java
@@ -0,0 +1,50 @@
+package org.triplea.http.client.lobby.login;
+
+import java.util.Optional;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
+/** Represents data that would be uploaded to a server. */
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@EqualsAndHashCode
+public class LobbyLoginResponse {
+  private final String loginToken;
+  private final String failReason;
+
+  /**
+   * Factory method for creating login responses where login was not successful.
+   *
+   * @param reason A reason for why login failed, to be shown to user.
+   */
+  public static LobbyLoginResponse newFailResponse(final String reason) {
+    return new LobbyLoginResponse(null, reason);
+  }
+
+  /**
+   * Factory method for creating login response where login was successful.
+   *
+   * @param token Single-use token that can be used to establish a socket connection.
+   */
+  public static LobbyLoginResponse newSuccessResponse(final String token) {
+    return new LobbyLoginResponse(token, null);
+  }
+
+  /**
+   * If present, indicates login was success.
+   */
+  public Optional<String> getLoginToken() {
+    return Optional.ofNullable(loginToken);
+  }
+
+  /**
+   * If login fails, this will return a 'reason' string that can be shown to the user. This is here
+   * so we can tell a user that they were banned or if they simply got the wrong password.
+   */
+  public Optional<String> getFailReason() {
+    return Optional.ofNullable(failReason);
+  }
+}
--- lobby/src/test/java/org/triplea/server/http/spark/SparkServerSystemTest.java
+++ lobby/src/test/java/org/triplea/server/http/spark/SparkServerSystemTest.java
@@ -3,11 +3,14 @@ package org.triplea.server.http.spark;
 import static org.mockito.Mockito.reset;
 
 import java.net.URI;
+import java.util.function.Function;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.mockito.Mockito;
+import org.triplea.http.client.lobby.login.LobbyLoginResponse;
+import org.triplea.http.client.lobby.login.RegisteredUserLoginRequest;
 import org.triplea.server.ServerConfiguration;
 import org.triplea.server.reporting.error.CreateIssueStrategy;
 import org.triplea.test.common.Integration;
@@ -15,10 +18,13 @@ import org.triplea.test.common.Integration;
 import spark.Spark;
 
 
+@SuppressWarnings("unchecked")
 @Integration
 class SparkServerSystemTest {
-
   static final CreateIssueStrategy errorUploadStrategy = Mockito.mock(CreateIssueStrategy.class);
+  static final Function<RegisteredUserLoginRequest, LobbyLoginResponse> registeredUserLogin =
+      Mockito.mock(Function.class);
+  static final Function<String, LobbyLoginResponse> anonymousUserLogin = Mockito.mock(Function.class);
 
   private static final int SPARK_PORT = 5000;
 
@@ -29,6 +35,8 @@ class SparkServerSystemTest {
     Spark.port(SPARK_PORT);
     SparkServer.start(ServerConfiguration.builder()
         .errorUploader(errorUploadStrategy)
+        .anonymousUserLogin(anonymousUserLogin)
+        .registeredUserLogin(registeredUserLogin)
         .build());
     Spark.awaitInitialization();
   }
@@ -41,5 +49,6 @@ class SparkServerSystemTest {
   @AfterAll
   static void stopServer() {
     Spark.stop();
+    Spark.awaitStop();
   }
 }
