ecce9d0e4bc23d92fc5f630e233ff0895e8649d3
RoiEX <8350879+RoiEXLab@users.noreply.github.com>
2019-04-01 18:19:59 +0200

    Play around with animations

--- game-headed/src/main/java/org/triplea/game/client/ui/javafx/screen/NavigationPane.java
+++ game-headed/src/main/java/org/triplea/game/client/ui/javafx/screen/NavigationPane.java
@@ -5,14 +5,9 @@ import java.util.Map;
 
 import com.google.common.base.Preconditions;
 
-import javafx.animation.KeyFrame;
-import javafx.animation.KeyValue;
-import javafx.animation.Timeline;
 import javafx.application.Platform;
-import javafx.beans.property.DoubleProperty;
 import javafx.scene.Node;
 import javafx.scene.layout.StackPane;
-import javafx.util.Duration;
 
 public class NavigationPane extends StackPane implements ScreenController<Class<? extends Node>> {
   private final Map<Class<? extends Node>, Node> screens = new HashMap<>();
@@ -28,35 +23,12 @@ public class NavigationPane extends StackPane implements ScreenController<Class<
     Preconditions.checkNotNull(identifier);
     Preconditions.checkArgument(screens.containsKey(identifier), "Screen of Type " + identifier + " not present");
 
-    final DoubleProperty opacity = opacityProperty();
     final Node screen = screens.get(identifier);
 
     if (getChildren().isEmpty()) {
-      fadeInScreen(screen, opacity);
+      getChildren().add(screen);
     } else {
-      fadeInReplacementScreen(screen, opacity);
+      getChildren().set(0, screen);
     }
-  }
-
-  private void fadeInScreen(final Node screen, final DoubleProperty opacity) {
-    setOpacity(0.0);
-    getChildren().add(screen);
-    final Timeline fadeIn = new Timeline(
-        new KeyFrame(Duration.ZERO, new KeyValue(opacity, 0.0)),
-        new KeyFrame(new Duration(500), new KeyValue(opacity, 1.0)));
-    fadeIn.play();
-  }
-
-  private void fadeInReplacementScreen(final Node screen, final DoubleProperty opacity) {
-    final Timeline fade = new Timeline(
-        new KeyFrame(Duration.ZERO, new KeyValue(opacity, 1.0)),
-        new KeyFrame(new Duration(400), t -> {
-          getChildren().set(0, screen);
-          final Timeline fadeIn = new Timeline(
-              new KeyFrame(Duration.ZERO, new KeyValue(opacity, 0.0)),
-              new KeyFrame(new Duration(400), new KeyValue(opacity, 1.0)));
-          fadeIn.play();
-        }, new KeyValue(opacity, 0.0)));
-    fade.play();
   }
 }
--- game-headed/src/main/java/org/triplea/game/client/ui/javafx/screen/RootActionPane.java
+++ game-headed/src/main/java/org/triplea/game/client/ui/javafx/screen/RootActionPane.java
@@ -10,12 +10,16 @@ import org.triplea.game.client.ui.javafx.util.FxmlManager;
 import com.google.common.base.Preconditions;
 
 import games.strategy.engine.framework.GameRunner;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.util.Duration;
 
 public class RootActionPane extends StackPane implements ScreenController<Screens> {
 
@@ -36,7 +40,8 @@ public class RootActionPane extends StackPane implements ScreenController<Screen
   @FXML
   private VBox exitOverlay;
 
-
+  @FXML
+  private VBox exitFrame;
 
   public RootActionPane() throws Exception {
     final FXMLLoader loader = FxmlManager.getLoader(getClass().getResource(FxmlManager.ROOT_CONTAINER.toString()));
@@ -79,8 +84,11 @@ public class RootActionPane extends StackPane implements ScreenController<Screen
   public void switchScreen(final Screens identifier) {
     Preconditions.checkNotNull(identifier);
 
+
     switch (identifier) {
       case EXIT:
+        final Timeline fadeIn = getAnimation(exitFrame);
+        fadeIn.play();
         exitOverlay.setVisible(true);
         break;
       case LOADING:
@@ -92,5 +100,17 @@ public class RootActionPane extends StackPane implements ScreenController<Screen
         rootPane.setVisible(true);
         break;
     }
+  }
+
+  private static Timeline getAnimation(final Node node) {
+    return new Timeline(
+        new KeyFrame(Duration.ZERO,
+            new KeyValue(node.scaleXProperty(), 0.0),
+            new KeyValue(node.scaleYProperty(), 0.0)),
+        new KeyFrame(new Duration(100), new KeyValue(node.scaleYProperty(), 1.1)),
+        new KeyFrame(new Duration(200), new KeyValue(node.scaleXProperty(), 0.4)),
+        new KeyFrame(new Duration(300),
+            new KeyValue(node.scaleXProperty(), 1.0),
+            new KeyValue(node.scaleYProperty(), 1.0)));
   }
 }
