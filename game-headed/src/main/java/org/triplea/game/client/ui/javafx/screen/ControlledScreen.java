6bd34cde4363709a6d14ad3f05578e691db0bcf6
RoiEX <8350879+RoiEXLab@users.noreply.github.com>
2019-04-10 19:26:41 +0200

    Formatting

--- game-headed/src/main/java/org/triplea/game/client/ui/javafx/screen/ControlledScreen.java
+++ game-headed/src/main/java/org/triplea/game/client/ui/javafx/screen/ControlledScreen.java
@@ -5,6 +5,7 @@ import javafx.scene.Node;
 /**
  * Interface that all Controllers should
  * implement that represent a screen that can be swapped out by another one.
+ *
  * @param <T> The Type of the {@link ScreenController}.
  */
 public interface ControlledScreen<T extends ScreenController<?>> {
@@ -12,6 +13,7 @@ public interface ControlledScreen<T extends ScreenController<?>> {
   /**
    * A method being called by the {@link ScreenController}
    * when this Screen is getting registered.
+   *
    * @param screenController The calling {@link ScreenController} instance.
    */
   void connect(T screenController);
