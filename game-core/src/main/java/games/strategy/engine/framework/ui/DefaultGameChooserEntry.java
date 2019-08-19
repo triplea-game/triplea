748afee7f95a509846fa86b4cb59209d1ed26f31
RoiEX <8350879+RoiEXLab@users.noreply.github.com>
2019-08-19 23:21:57 +0200

    Remove redundant null checks

--- game-core/src/main/java/games/strategy/engine/framework/ui/DefaultGameChooserEntry.java
+++ game-core/src/main/java/games/strategy/engine/framework/ui/DefaultGameChooserEntry.java
@@ -11,13 +11,14 @@ import java.net.URI;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.logging.Level;
+import javax.annotation.Nonnull;
 import lombok.extern.java.Log;
 import org.triplea.java.UrlStreams;
 
 @Log
 final class DefaultGameChooserEntry implements GameChooserEntry {
   private final URI url;
-  private GameData gameData;
+  @Nonnull private GameData gameData;
   private boolean gameDataFullyLoaded = false;
   private final String gameNameAndMapNameProperty;
 
@@ -89,6 +90,7 @@ final class DefaultGameChooserEntry implements GameChooserEntry {
     return getGameName();
   }
 
+  @Nonnull
   @Override
   public GameData getGameData() {
     return gameData;
@@ -118,11 +120,7 @@ final class DefaultGameChooserEntry implements GameChooserEntry {
     }
 
     final DefaultGameChooserEntry other = (DefaultGameChooserEntry) obj;
-    if (gameData == null && other.gameData != null) {
-      return false;
-    }
-    return other.gameData != null
-        && this.gameNameAndMapNameProperty.equals(other.gameNameAndMapNameProperty);
+    return this.gameNameAndMapNameProperty.equals(other.gameNameAndMapNameProperty);
   }
 
   @Override
