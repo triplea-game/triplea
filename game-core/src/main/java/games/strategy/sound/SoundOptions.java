dab23acdb75513c5bc0f7e1bd5e90294970d6893
RoiEX <8350879+RoiEXLab@users.noreply.github.com>
2019-08-29 18:03:44 +0200

    Sound options enhancements (#6)
    * Move PropertiesSelector -> SoundOptions
    
    Also make the constructor to a static method
    
    * Simplify method only called from EDT
    
    * Inline method
    
    * Simplify method
    
    * Inline method
    
    * Move code around
    
    * Reduce code
    
    * Spotless
    
    * Fix method order

--- game-core/src/main/java/games/strategy/sound/SoundOptions.java
+++ game-core/src/main/java/games/strategy/sound/SoundOptions.java
@@ -1,15 +1,27 @@
 package games.strategy.sound;
 
-import games.strategy.engine.framework.ui.PropertiesSelector;
+import games.strategy.engine.data.properties.PropertiesUi;
 import java.awt.event.KeyEvent;
 import java.util.List;
 import javax.swing.JCheckBoxMenuItem;
 import javax.swing.JComponent;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
 
 /** Sound option window framework. */
 public final class SoundOptions {
-  private SoundOptions(final JComponent parent) {
+  private SoundOptions() {}
+
+  /** Builds a "Sound Options" menu item. */
+  public static JMenuItem buildSoundOptionsMenuItem() {
+    final JMenuItem soundOptions = new JMenuItem("Sound Options");
+    soundOptions.setMnemonic(KeyEvent.VK_S);
+    soundOptions.addActionListener(e -> showSoundOptions(soundOptions));
+    return soundOptions;
+  }
+
+  private static void showSoundOptions(final JComponent parent) {
     final ClipPlayer clipPlayer = ClipPlayer.getInstance();
     final String ok = "OK";
     final String cancel = "Cancel";
@@ -17,38 +29,34 @@ public final class SoundOptions {
     final String selectNone = "None";
 
     final List<SoundOptionCheckBox> properties = SoundPath.getSoundOptions();
-    final Object pressedButton =
-        PropertiesSelector.getButton(
-            parent, "Sound Options", properties, ok, selectAll, selectNone, cancel);
+    final JScrollPane scroll = new JScrollPane(new PropertiesUi(properties, true));
+    scroll.setBorder(null);
+    scroll.getViewport().setBorder(null);
+    final JOptionPane pane =
+        new JOptionPane(
+            scroll,
+            JOptionPane.PLAIN_MESSAGE,
+            JOptionPane.DEFAULT_OPTION,
+            null,
+            new Object[] {ok, selectAll, selectNone, cancel});
+    pane.createDialog(parent, "Sound Options").setVisible(true);
+    final Object pressedButton = pane.getValue();
     if (pressedButton == null || pressedButton.equals(cancel)) {
       return;
     }
-    if (pressedButton.equals(ok)) {
-      for (final SoundOptionCheckBox property : properties) {
-        clipPlayer.setMute(property.getClipName(), !property.getValue());
-      }
-      clipPlayer.saveSoundPreferences();
-    } else if (pressedButton.equals(selectAll)) {
+    if (pressedButton.equals(selectAll)) {
       for (final SoundOptionCheckBox property : properties) {
         property.setValue(true);
-        clipPlayer.setMute(property.getClipName(), false);
       }
-      clipPlayer.saveSoundPreferences();
     } else if (pressedButton.equals(selectNone)) {
       for (final SoundOptionCheckBox property : properties) {
         property.setValue(false);
-        clipPlayer.setMute(property.getClipName(), true);
       }
-      clipPlayer.saveSoundPreferences();
     }
-  }
-
-  /** Builds a "Sound Options" menu item. */
-  public static JMenuItem buildSoundOptionsMenuItem() {
-    final JMenuItem soundOptions = new JMenuItem("Sound Options");
-    soundOptions.setMnemonic(KeyEvent.VK_S);
-    soundOptions.addActionListener(e -> new SoundOptions(soundOptions));
-    return soundOptions;
+    for (final SoundOptionCheckBox property : properties) {
+      clipPlayer.setMute(property.getClipName(), !property.getValue());
+    }
+    clipPlayer.saveSoundPreferences();
   }
 
   /** Builds a checkbox menu item to turn sounds on or off. */
