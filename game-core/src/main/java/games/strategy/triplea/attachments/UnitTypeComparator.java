c473f86843507225b692232af9301c52b91c8704
RoiEX <8350879+RoiEXLab@users.noreply.github.com>
2019-09-02 01:18:01 +0200

    Add tests for UnitTypeComparator.java (#5169)
    * Add characterization tests
    
    * Remove impossible condition
    
    * Simplify comparator
    
    * Use hamcrest matchers

--- game-core/src/main/java/games/strategy/triplea/attachments/UnitTypeComparator.java
+++ game-core/src/main/java/games/strategy/triplea/attachments/UnitTypeComparator.java
@@ -1,5 +1,6 @@
 package games.strategy.triplea.attachments;
 
+import games.strategy.engine.data.NamedAttachable;
 import games.strategy.engine.data.UnitType;
 import games.strategy.triplea.delegate.Matches;
 import java.io.Serializable;
@@ -24,41 +25,15 @@ public class UnitTypeComparator implements Comparator<UnitType>, Serializable {
 
   @Override
   public int compare(final UnitType u1, final UnitType u2) {
-    final UnitAttachment ua1 = UnitAttachment.get(u1);
-    final UnitAttachment ua2 = UnitAttachment.get(u2);
-    if (ua1 == null) {
-      throw new IllegalStateException("No unit type attachment for unit type : " + u1.getName());
-    }
-    if (ua2 == null) {
-      throw new IllegalStateException("No unit type attachment for unit type : " + u2.getName());
-    }
-    if (ua1.getIsInfrastructure() && !ua2.getIsInfrastructure()) {
-      return 1;
-    }
-    if (ua2.getIsInfrastructure() && !ua1.getIsInfrastructure()) {
-      return -1;
-    }
-    if (Matches.unitTypeIsAaForAnything().test(u1) && !Matches.unitTypeIsAaForAnything().test(u2)) {
-      return 1;
-    }
-    if (!Matches.unitTypeIsAaForAnything().test(u1) && Matches.unitTypeIsAaForAnything().test(u2)) {
-      return -1;
-    }
-    if (ua1.getIsAir() && !ua2.getIsAir()) {
-      return 1;
-    }
-    if (ua2.getIsAir() && !ua1.getIsAir()) {
-      return -1;
-    }
-    if (ua1.getIsSea() && !ua2.getIsSea()) {
-      return 1;
-    }
-    if (ua2.getIsSea() && !ua1.getIsSea()) {
-      return -1;
-    }
-    if (ua1.getAttack() != ua2.getAttack()) {
-      return ua1.getAttack() - ua2.getAttack();
-    }
-    return u1.getName().compareTo(u2.getName());
+    return Comparator.<UnitType, UnitAttachment>comparing(
+            UnitAttachment::get, Comparator.comparing(UnitAttachment::getIsInfrastructure))
+        .thenComparing(Matches.unitTypeIsAaForAnything()::test)
+        .thenComparing(
+            UnitAttachment::get,
+            Comparator.comparing(UnitAttachment::getIsAir)
+                .thenComparing(UnitAttachment::getIsSea)
+                .thenComparingInt(UnitAttachment::getAttack))
+        .thenComparing(NamedAttachable::getName)
+        .compare(u1, u2);
   }
 }
