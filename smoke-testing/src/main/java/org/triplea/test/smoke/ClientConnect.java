d0c20aa7e8d1224e3477aff877ce4b08cce19c8d
Dan Van Atta <dhvatta@gmail.com>
2019-09-03 12:48:43 -0700

    Add new test to be run by Travis: Smoke testing (#5178)
    * Smoke testing
    
    Add a new sub-project for smoke testing. This is end-to-end verification where we avoid
    validating logic and we send requests into the system and validate that we get no error
    messages.
    
    A travis job is added to run the test. The job is powered by a shell script that coordinates
    the steps for the testing:
    - start lobby
    - start bot
    - verify the two have started and are listening on respective ports
    - use smoke-test Java client to connect to lobby
    - use smoke-test Java client to connect to bot
    - verify logs of bot and lobby do not contain any SEVERE logging.
    
    If the bot fails to connect to lobby, it will continue running but will log a SEVERE error.
    Thus, we check logs to detect that condition.
    
    If either lobby or bot fail to startup completely, we'll get a fail fast error using 'nc' (netcat)
    which checks if the lobby or bot ports are open.
    
    On exit, bot and lobby are stopped and logs are printed to help with debug. We will always print logs so that
    on successful builds we have a point of comparison, particularly to help compare against failure builds.
    
    Note:
    - The smoke test is not integrated with gradle check command as it depends on the current system state
    and closes current lobby and bot. If there is any manual testing then this could quickly get in the way.
    
    * Add small delay to give lobby time to start up

---
+++ smoke-testing/src/main/java/org/triplea/test/smoke/ClientConnect.java
@@ -0,0 +1,51 @@
+package org.triplea.test.smoke;
+
+import games.strategy.engine.framework.GameObjectStreamFactory;
+import games.strategy.engine.framework.startup.login.ClientLogin;
+import games.strategy.engine.framework.startup.mc.ClientModel;
+import games.strategy.engine.lobby.client.login.LobbyServerProperties;
+import games.strategy.net.ClientMessengerFactory;
+import games.strategy.net.IClientMessenger;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import lombok.extern.java.Log;
+
+/**
+ * A simple application to test connectivity to a running bot and lobby. Failure is indicted by
+ * throwing an exception which causes a non-zero exit code.
+ */
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@Log
+public final class ClientConnect {
+  public static void main(final String[] args) throws Exception {
+    connectToLobby();
+    connectToBot();
+  }
+
+  private static void connectToLobby() throws Exception {
+    log.info("Connecting to lobby...");
+    final IClientMessenger messenger =
+        ClientMessengerFactory.newAnonymousUserMessenger(
+            LobbyServerProperties.builder().host("localhost").port(3304).httpsPort(5432).build(),
+            "test-user");
+    Thread.sleep(500L);
+    log.info("Connection to lobby SUCCESSFUL, closing connection");
+    messenger.shutDown();
+  }
+
+  private static void connectToBot() throws Exception {
+    log.info("Connecting to bot server...");
+    final IClientMessenger messenger =
+        ClientMessengerFactory.newClientMessenger(
+            ClientModel.ClientProps.builder()
+                .name("test-user")
+                .host("localhost")
+                .port(4000)
+                .build(),
+            new GameObjectStreamFactory(null),
+            new ClientLogin(null));
+    Thread.sleep(500L);
+    log.info("Connection to bot server SUCCESSFUL, closing connection");
+    messenger.shutDown();
+  }
+}
