7fbe388d3a2f6510c3755e0a3371406efda997fe
Ron <ron.murhammer@gmail.com>
2019-09-10 17:08:40 -0500

    Rename new image transformer class and add flip method

--- game-core/src/main/java/games/strategy/triplea/image/ImageTransformer.java
+++ game-core/src/main/java/games/strategy/triplea/image/ImageTransformer.java
@@ -1,29 +1,32 @@
 package games.strategy.triplea.image;
 
 import java.awt.Color;
+import java.awt.geom.AffineTransform;
+import java.awt.image.AffineTransformOp;
 import java.awt.image.BufferedImage;
 
 /**
- * Class used to colorize images. Based on finding the luminance from the original image then
- * applying hue, saturation, and brightness to colorize it.
+ * Class used to transform images including colorizing and flipping. Colorizing is based on finding
+ * the luminance from the original image then applying hue, saturation, and brightness to colorize
+ * it. More information about luminance can be found here:
+ * https://en.wikipedia.org/wiki/Relative_luminance.
  */
-public class ImageColorizer {
+public class ImageTransformer {
 
   private static final int MAX_COLOR = 256;
   private static final float LUMINANCE_RED = 0.2126f;
   private static final float LUMINANCE_GREEN = 0.7152f;
   private static final float LUMINANCE_BLUE = 0.0722f;
 
-  private ImageColorizer() {}
+  private ImageTransformer() {}
 
   /**
-   * Apply color to the given image. This takes the hue and saturation of the given color and
-   * applies it to the image. It ignores the brightness of the color which can cause images to
-   * become poor quality.
+   * Apply color and brightness to the given image. This uses the hue and saturation of the given
+   * color. It ignores the brightness of the color and uses the given brightness value.
    */
-  public static void applyColor(final Color color, final BufferedImage image) {
+  public static void colorize(final Color color, final int brightness, final BufferedImage image) {
     final float[] hsb = Color.RGBtoHSB(color.getRed(), color.getGreen(), color.getBlue(), null);
-    applyHsb((int) (hsb[0] * 360), (int) (hsb[1] * 100), 0, image);
+    colorize((int) (hsb[0] * 360), (int) (hsb[1] * 100), brightness, image);
   }
 
   /**
@@ -34,7 +37,7 @@ public class ImageColorizer {
    * @param brightness -100 to 100, which adjusts the given image's brightness
    * @param image the image to transform
    */
-  public static void applyHsb(
+  private static void colorize(
       final int hue, final int saturation, final int brightness, final BufferedImage image) {
 
     // Create color lookup table for luminance values
@@ -44,7 +47,7 @@ public class ImageColorizer {
     final float hueFloat = hue / 360f;
     final float saturationFloat = saturation / 100f;
     for (int i = 0; i < MAX_COLOR; i++) {
-      final float brightnessFloat = i / 255f;
+      final float brightnessFloat = (float) i / (MAX_COLOR - 1);
       final Color color = Color.getHSBColor(hueFloat, saturationFloat, brightnessFloat);
       redLookup[i] = color.getRed();
       greenLookup[i] = color.getGreen();
@@ -89,5 +92,12 @@ public class ImageColorizer {
       lum = (int) ((lum * (brightness + 100f)) / 100f);
     }
     return lum;
+  }
+
+  public static BufferedImage flipHorizontally(final BufferedImage image) {
+    final AffineTransform tx = AffineTransform.getScaleInstance(-1, 1);
+    tx.translate(-image.getWidth(null), 0);
+    final AffineTransformOp op = new AffineTransformOp(tx, AffineTransformOp.TYPE_NEAREST_NEIGHBOR);
+    return op.filter(image, null);
   }
 }
