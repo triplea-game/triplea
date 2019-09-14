c1f936aa4079334364d2444958c8ab1ff7402eee
DanVanAtta <dhvatta@gmail.com>
2019-09-13 19:02:54 -0700

    Remove out of date warning (fixes a deadlock)
    First, our 'out-of-date' warning code shows a dialog while we are in a background runner task. Swing components cannot be accessed while the background dialog is open. Because the background dialog runner is waiting for the dialog to be closed, we enter a deadlock situation.
    
    The removal is valid because the dialog is actually questionable. If already check if the current engine is compatible with the save, here we are checking if the save was done with a future version. It should not actually matter if the save was with a future version because that version is still compatible. If there were a problem loading the save of a future version, it would not be a compatible.

--- game-core/src/main/java/games/strategy/engine/framework/GameDataManager.java
+++ game-core/src/main/java/games/strategy/engine/framework/GameDataManager.java
@@ -64,7 +64,6 @@ public final class GameDataManager {
     final ObjectInputStream input = new ObjectInputStream(new GZIPInputStream(is));
     try {
       final Version readVersion = (Version) input.readObject();
-      final boolean headless = HeadlessGameServer.headless();
       if (!ClientContext.engineVersion().isCompatibleWithEngineVersion(readVersion)) {
         final String error =
             "Incompatible engine versions. We are: "
@@ -74,12 +73,6 @@ public final class GameDataManager {
                 + "\nTo download the latest version of TripleA, Please visit "
                 + UrlConstants.LATEST_GAME_DOWNLOAD_WEBSITE;
         throw new IOException(error);
-      } else if (!headless && readVersion.isGreaterThan(ClientContext.engineVersion())) {
-        // we can still load it because our engine is compatible, however this save was made by a
-        // newer engine, so prompt the user to upgrade
-        if (!promptToLoadNewerSaveGame(readVersion)) {
-          return null;
-        }
       }
 
       final GameData data = (GameData) input.readObject();
@@ -89,38 +82,6 @@ public final class GameDataManager {
     } catch (final ClassNotFoundException cnfe) {
       throw new IOException(cnfe.getMessage());
     }
-  }
-
-  private static boolean promptToLoadNewerSaveGame(final Version saveGameVersion) {
-    final int answer =
-        Interruptibles.awaitResult(
-                () ->
-                    SwingAction.invokeAndWaitResult(
-                        () -> {
-                          final String message =
-                              "Your TripleA engine is OUT OF DATE. "
-                                  + "This save was made by a newer version of TripleA.\n\n"
-                                  + "However, because the first 3 version numbers are the same "
-                                  + "as your current version, we can still open the save.\n\n"
-                                  + "This TripleA engine is version "
-                                  + ClientContext.engineVersion()
-                                  + " and you are trying "
-                                  + "to open a save made with version "
-                                  + saveGameVersion
-                                  + "\n\n"
-                                  + "To download the latest version of TripleA, please visit "
-                                  + UrlConstants.LATEST_GAME_DOWNLOAD_WEBSITE
-                                  + ".\n\n"
-                                  + "It is recommended that you upgrade to the latest version of "
-                                  + "TripleA before playing this save.\n\n"
-                                  + "Do you wish to continue and open this save with your "
-                                  + "current 'old' version?";
-                          return JOptionPane.showConfirmDialog(
-                              null, message, "Open Newer Save Game?", JOptionPane.YES_NO_OPTION);
-                        }))
-            .result
-            .orElse(JOptionPane.NO_OPTION);
-    return answer == JOptionPane.YES_OPTION;
   }
 
   private static void loadDelegates(final ObjectInputStream input, final GameData data)
