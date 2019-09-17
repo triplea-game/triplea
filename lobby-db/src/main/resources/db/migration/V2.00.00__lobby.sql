9d3625387388511fa85598d8601720572c7189a5
DanVanAtta <dhvatta@gmail.com>
2019-09-17 16:34:26 -0700

    Remove ban data migration code and obvious column comments for banned_user columnsj

--- lobby-db/src/main/resources/db/migration/V2.00.00__lobby.sql
+++ lobby-db/src/main/resources/db/migration/V2.00.00__lobby.sql
@@ -275,31 +275,16 @@ create table banned_user
 
 alter table banned_user owner to lobby_user;
 
--- migrate existing data to the new table name
-insert into banned_user(public_id, username, hashed_mac, ip, ban_expiry)
-select random()::varchar, username, mac, ip, coalesce(ban_till, now() + interval '30 days')
-from banned_macs
-where ban_till > now();
-
 drop table banned_macs;
 
 comment on table banned_user is
     $$Table that records player bans, when players join lobby we check their IP address and hashed mac
           against this table. If there there is an IP or mac match, then the user is not allowed to join.$$;
-comment on column banned_user.id is 'synthetic PK column';
 comment on column banned_user.public_id is
     $$A value that publicly identifiers the ban. When a player is rejected from joining lobby we can
         show them this ID value. If the player wants to dispute the ban, they can give us the public id
         and we would be able to remove the ban.$$;
-comment on column banned_user.username is
-    'The name of the player at the time of banning.';
-comment on column banned_user.hashed_mac is
-    $$Mac is hashed when sent to TripleA, this is the hashed mac value that software received.
-        One note, the mac sent by TripleA is software generated and can be altered by a custom
-        compiled version of TripleA$$;
-comment on column banned_user.ip is 'IP address of the user at the time of banning.';
-comment on column banned_user.ban_expiry is 'Timestamp of when the player ban expires';
-comment on column banned_user.date_created is 'Timestamp of when the ban was created.';
+
 create table temp_password_request
 (
     id               serial primary key,
