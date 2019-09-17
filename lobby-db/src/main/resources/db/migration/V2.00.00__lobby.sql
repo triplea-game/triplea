bb7f335a2f075a8f10dce167dd88dfd57c04c8ac
DanVanAtta <dhvatta@gmail.com>
2019-09-16 17:27:41 -0700

    Squash changesets together
    Because we are doing a clean DB install, all changesets are being squashed together. If that were not the case, then all of the V1.10 changesets would be squashed together only and leave the already applied V1.00.00__lobby.sql changeset unsquashed.
    
    There are no changes other than concatenating files together.

--- lobby-db/src/main/resources/db/migration/V2.00.00__lobby.sql
+++ lobby-db/src/main/resources/db/migration/V2.00.00__lobby.sql
@@ -1,3 +1,54 @@
+
+create table bad_words (
+    word character varying(40) not null primary key
+);
+alter table bad_words owner to lobby_user;
+
+create table banned_ips (
+    ip character varying(40) not null primary key,
+    ban_till timestamp without time zone
+);
+alter table banned_ips owner to lobby_user;
+
+create table banned_macs (
+    mac character varying(40) not null primary key,
+    ban_till timestamp without time zone
+);
+alter table banned_macs owner to lobby_user;
+
+create table banned_usernames (
+    username character varying(40) not null primary key,
+    ban_till timestamp without time zone
+);
+alter table banned_usernames owner to lobby_user;
+
+create table muted_ips (
+    ip character varying(40) not null primary key,
+    mute_till timestamp without time zone
+);
+alter table muted_ips owner to lobby_user;
+
+create table muted_macs (
+    mac character varying(40) not null primary key,
+    mute_till timestamp without time zone
+);
+alter table muted_macs owner to lobby_user;
+
+create table muted_usernames (
+    username character varying(40) not null primary key,
+    mute_till timestamp without time zone
+);
+alter table muted_usernames owner to lobby_user;
+
+create table ta_users (
+    username character varying(40) not null primary key,
+    password character varying(60) not null,
+    email character varying(40) not null,
+    joined timestamp without time zone not null,
+    lastlogin timestamp without time zone not null,
+    admin integer not null
+);
+alter table ta_users owner to lobby_user;
 -- update_tables
 alter table ta_users
     alter column email type varchar(254),
@@ -218,3 +269,211 @@ comment on column access_log.registered is 'True if the user was registered when
 
 alter table access_log owner to lobby_user;
 
+-- Recreate 'bad_words' table with new table name 'bad_word', updated comments and add 'date_created' column
+
+create table bad_word as
+select word, now() as date_created
+from bad_words;
+
+alter table bad_word
+  add primary key (word);
+
+alter table bad_word
+  alter column date_created set not null;
+
+alter table bad_word
+  alter column date_created set default now();
+
+comment on table bad_word is 'A table representing a blacklist of words';
+comment on column bad_word.word is 'Stores the banned words';
+comment on column bad_word.date_created is 'Row creation date';
+
+drop table bad_words;
+
+-- mute only by network identifiers, drop mute by username
+drop table muted_usernames;
+
+-- 'banned_usernames' will now keep track of username blacklist with no expiration
+alter table banned_usernames
+ drop column ip;
+
+alter table banned_usernames
+  drop column mac;
+
+alter table banned_usernames
+  drop column ban_till;
+
+
+-- mod name identifies a mod, drop unnecessary network identifier tracking for moderator
+alter table banned_usernames
+  drop column mod_ip;
+
+alter table banned_usernames
+  drop column mod_mac;
+drop table muted_macs;
+create table lobby_user
+(
+    id              serial primary key,
+    username        character varying(40) not null unique,
+    password        character varying(60),
+    email           character varying(40) not null,
+    check (email <> ''),
+    date_created    timestamptz           not null default current_timestamp,
+    last_login      timestamptz,
+    role            character varying(16) not null default 'PLAYER' check (role in ('PLAYER', 'MODERATOR', 'ADMIN')),
+    bcrypt_password character(60) check (char_length(bcrypt_password) = 60)
+);
+
+alter table lobby_user
+    owner to lobby_user;
+alter table lobby_user
+    add constraint lobby_user_pass_check check (password IS NOT NULL OR bcrypt_password IS NOT NULL);
+
+
+comment on table lobby_user is 'The table storing all the information about Lobby TripleA users.';
+comment on column lobby_user.id is 'Synthetic PK column';
+comment on column lobby_user.username is 'Defines the in-game username of everyone.';
+comment on column lobby_user.password is 'The legacy MD5Crypt hash of the password. The length of the hash must always be 34 chars. Either password or bcrypt_password must be not null.';
+comment on column lobby_user.email is 'Email storage of every user. Large size to match the maximum email length. More information here: https://stackoverflow.com/a/574698.';
+comment on column lobby_user.date_created is 'The timestamp of the creation of the account.';
+comment on column lobby_user.last_login is 'The timestamp of the last successful login.';
+comment on column lobby_user.role is
+    $$The role of the user, controls privileges. If moderator the user is able to ban and mute other people.
+     Admin is able to add/remove other moderators.$$;
+comment on column lobby_user.bcrypt_password is 'The BCrypt-Hashed password of the user, should be the same as the md5 password but in another form. The length of the hash must always be 60 chars. Either password or bcrypt_password must be not null.';
+
+
+create table moderator_action_history
+(
+    id            serial primary key,
+    lobby_user_id int         not null references lobby_user (id),
+    date_created  timestamptz not null default current_timestamp,
+    action_name   varchar(64) not null,
+    action_target varchar(40) not null
+);
+
+alter table moderator_action_history
+    owner to lobby_user;
+
+comment on table moderator_action_history is 'Table storing an audit history of actions taken by moderators';
+comment on column moderator_action_history.id is 'Table storing an audit history of actions taken by moderators';
+
+comment on column moderator_action_history.lobby_user_id is 'FK to lobby_user table, this is the moderator that initiated an action.';
+comment on column moderator_action_history.date_created is 'Row creation timestamp, when the action was taken.';
+comment on column moderator_action_history.action_name is 'Specifier of what action the moderator took, eg: ban|mute';
+comment on column moderator_action_history.action_target is 'The target of the action, eg: banned player name, banned mac address';
+
+
+create table error_report_history(
+  id       serial primary key,
+  user_ip  character varying(30) not null,
+  date_created timestamp not null default now()
+);
+
+alter table error_report_history owner to lobby_user;
+
+comment on table error_report_history is 'Table that stores timestamps by user IP address of when error reports were created. Used to do rate limiting.';
+comment on column error_report_history.id is 'Synthetic PK column';
+comment on column error_report_history.user_ip is 'IP address of a user that has submitted an error report';
+comment on column error_report_history.date_created is 'Timestamp when error report was created in DB';
+alter table banned_usernames
+    add column date_created timestamptz not null default now();
+comment on column banned_usernames.date_created is
+    'Timestamp of when the banned username is added';
+alter table banned_usernames
+    drop column mod_username;
+
+create table banned_user
+(
+    id           serial primary key,
+    public_id    varchar(36)   not null unique,
+    username     varchar(40)   not null,
+    hashed_mac   character(28) not null check (char_length(hashed_mac) = 28),
+    ip           inet          not null,
+    ban_expiry   timestamptz   not null check (ban_expiry > now()),
+    date_created timestamptz   not null default now()
+);
+
+alter table banned_user owner to lobby_user;
+
+-- migrate existing data to the new table name
+insert into banned_user(public_id, username, hashed_mac, ip, ban_expiry)
+select random()::varchar, username, mac, ip, coalesce(ban_till, now() + interval '30 days')
+from banned_macs
+where ban_till > now();
+
+drop table banned_macs;
+
+comment on table banned_user is
+    $$Table that records player bans, when players join lobby we check their IP address and hashed mac
+          against this table. If there there is an IP or mac match, then the user is not allowed to join.$$;
+comment on column banned_user.id is 'synthetic PK column';
+comment on column banned_user.public_id is
+    $$A value that publicly identifiers the ban. When a player is rejected from joining lobby we can
+        show them this ID value. If the player wants to dispute the ban, they can give us the public id
+        and we would be able to remove the ban.$$;
+comment on column banned_user.username is
+    'The name of the player at the time of banning.';
+comment on column banned_user.hashed_mac is
+    $$Mac is hashed when sent to TripleA, this is the hashed mac value that software received.
+        One note, the mac sent by TripleA is software generated and can be altered by a custom
+        compiled version of TripleA$$;
+comment on column banned_user.ip is 'IP address of the user at the time of banning.';
+comment on column banned_user.ban_expiry is 'Timestamp of when the player ban expires';
+comment on column banned_user.date_created is 'Timestamp of when the ban was created.';
+create table temp_password_request
+(
+    id               serial primary key,
+    lobby_user_id    int         not null references lobby_user (id),
+    temp_password    varchar(60) not null,
+    date_created     timestamptz not null default now(),
+    date_invalidated timestamptz
+);
+
+alter table temp_password_request
+    owner to lobby_user;
+
+comment on table temp_password_request is
+    $$Table that stores temporary passwords issued to players. They are intended to be single use.$$;
+comment on column temp_password_request.id is 'synthetic PK column';
+comment on column temp_password_request.lobby_user_id is 'FK to lobby_user table.';
+comment on column temp_password_request.temp_password is 'Temp password value created for user.';
+comment on column temp_password_request.date_created is 'Timestamp of when the ban temporary password was created.';
+comment on column temp_password_request.date_invalidated is
+    $$Timestamp of when the temporary password is either used or marked invalid.
+    A temp password can be marked as invalid if multiple are issued.$$;
+
+create table temp_password_request_history
+(
+    id           serial primary key,
+    inetaddress  inet        not null,
+    username     varchar(40) not null,
+    date_created timestamptz not null default now()
+);
+
+alter table temp_password_request_history
+    owner to lobby_user;
+
+comment on table temp_password_request_history is
+    $$Table that stores requests for temporary passwords for audit purposes. This will let us rate limit requests and
+    prevent a single player from spamming email to many userse.$$;
+comment on column temp_password_request_history.id is 'synthetic PK column';
+comment on column temp_password_request_history.inetaddress is 'IP of the address making the temp password request.';
+comment on column temp_password_request_history.username is 'The requested username for a temp password.';
+comment on column temp_password_request_history.date_created is 'Timestamp of when the temp password request is made';
+
+create index temp_password_request_history_inet on temp_password_request_history (inetaddress);
+create table api_key
+(
+    id            serial primary key,
+    lobby_user_id integer references lobby_user (id),
+    key           character varying(256) not null unique,
+    date_created  timestamptz            not null default now()
+);
+
+alter table api_key
+    owner to lobby_user;
+
+comment on table api_key is
+    $$Table that stores api keys of users that have logged into the lobby. Denormalized to reflect time
+        of login and to support anonymous user logins.$$;
