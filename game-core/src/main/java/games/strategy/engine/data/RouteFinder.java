dad8c293ae44609ff19be0f2235e8f840850b940
Ron <ron.murhammer@gmail.com>
2019-09-17 17:41:02 -0500

    Update route finding to use movement costs

--- game-core/src/main/java/games/strategy/engine/data/RouteFinder.java
+++ game-core/src/main/java/games/strategy/engine/data/RouteFinder.java
@@ -1,21 +1,20 @@
 package games.strategy.engine.data;
 
+import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Preconditions;
+import games.strategy.triplea.delegate.TerritoryEffectHelper;
+import java.math.BigDecimal;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.Queue;
 import java.util.function.Predicate;
 
-// TODO this class doesn't take movementcost into account... typically the shortest route is the
-// fastest route, but not
-// always...
 class RouteFinder {
 
   private final GameMap map;
@@ -24,7 +23,7 @@ class RouteFinder {
   private final PlayerId player;
 
   RouteFinder(final GameMap map, final Predicate<Territory> condition) {
-    this(map, condition, new HashSet<>(), null);
+    this(map, condition, Collections.emptySet(), null);
   }
 
   RouteFinder(
@@ -66,19 +65,65 @@ class RouteFinder {
     return Optional.empty();
   }
 
+  Optional<Route> findRouteByCost(final Territory start, final Territory end) {
+    Preconditions.checkNotNull(start);
+    Preconditions.checkNotNull(end);
+
+    if (start.equals(end)) {
+      return Optional.of(new Route(start));
+    }
+
+    final Map<Territory, Territory> previous = new HashMap<>();
+    previous.put(start, null);
+    final Queue<Territory> toVisit = new ArrayDeque<>();
+    toVisit.add(start);
+    final Map<Territory, BigDecimal> routeCosts = new HashMap<>();
+    routeCosts.put(start, BigDecimal.ZERO);
+    BigDecimal minCost = new BigDecimal(Integer.MAX_VALUE);
+
+    while (!toVisit.isEmpty()) {
+      final Territory currentTerritory = toVisit.remove();
+      if (routeCosts.get(currentTerritory).compareTo(minCost) >= 0) {
+        continue;
+      }
+      for (final Territory neighbor :
+          map.getNeighborsValidatingCanals(currentTerritory, condition, units, player)) {
+        final BigDecimal routeCost =
+            routeCosts.get(currentTerritory).add(getMaxMovementCost(neighbor));
+        if (!previous.containsKey(neighbor) || routeCost.compareTo(routeCosts.get(neighbor)) < 0) {
+          previous.put(neighbor, currentTerritory);
+          routeCosts.put(neighbor, routeCost);
+          if (neighbor.equals(end) && routeCost.compareTo(minCost) < 0) {
+            minCost = routeCost;
+            break;
+          }
+          toVisit.add(neighbor);
+        }
+      }
+    }
+    return (minCost.compareTo(new BigDecimal(Integer.MAX_VALUE)) == 0)
+        ? Optional.empty()
+        : Optional.of(getRoute(start, end, previous));
+  }
+
+  @VisibleForTesting
+  BigDecimal getMaxMovementCost(final Territory t) {
+    return TerritoryEffectHelper.getMaxMovementCost(t, units);
+  }
+
   private static Route getRoute(
       final Territory start,
       final Territory destination,
       final Map<Territory, Territory> previous) {
-    final List<Territory> route = new ArrayList<>();
+    final List<Territory> territories = new ArrayList<>();
     Territory current = destination;
     while (!start.equals(current)) {
       assert current != null : "Route was calculated but isn't connected";
-      route.add(current);
+      territories.add(current);
       current = previous.get(current);
     }
-    route.add(start);
-    Collections.reverse(route);
-    return new Route(route);
+    territories.add(start);
+    Collections.reverse(territories);
+    return new Route(territories);
   }
 }
