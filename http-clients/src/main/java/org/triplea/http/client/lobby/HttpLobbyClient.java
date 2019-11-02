44d1682eb89dde24fdf94a2a17084c39a82d5aac
DanVanAtta <dhvatta@gmail.com>
2019-11-02 13:36:03 -0700

    Wire LobbyChatTransmitter to LobbyChatClient
    Adds client side infrastructure for lobby websocket chat.
    LobbyChatTransmitter is a new class that can be swapped out for a
    'MessengersChatTransmitters' as they both implement the same chat API.
    
    The ChatTransmitter is wired to a set of listener APIs and send
    APIs that are backed by an http component that eventually sends
    and receives requests from the GenericWebsocket connection.
    
    As part of this wiring is an event handler that handles server
    messages and routes them back to the appropriate listener.

--- http-clients/src/main/java/org/triplea/http/client/lobby/HttpLobbyClient.java
+++ http-clients/src/main/java/org/triplea/http/client/lobby/HttpLobbyClient.java
@@ -1,10 +1,13 @@
 package org.triplea.http.client.lobby;
 
 import java.net.URI;
+import java.util.function.Consumer;
 import lombok.Getter;
 import org.triplea.domain.data.ApiKey;
+import org.triplea.http.client.lobby.chat.LobbyChatClient;
 import org.triplea.http.client.lobby.game.ConnectivityCheckClient;
 import org.triplea.http.client.lobby.game.listing.GameListingClient;
+import org.triplea.http.client.lobby.moderator.ModeratorLobbyClient;
 import org.triplea.http.client.lobby.moderator.toolbox.HttpModeratorToolboxClient;
 import org.triplea.http.client.lobby.user.account.UserAccountClient;
 
@@ -16,16 +19,24 @@ public class HttpLobbyClient {
   private final ConnectivityCheckClient connectivityCheckClient;
   private final GameListingClient gameListingClient;
   private final HttpModeratorToolboxClient httpModeratorToolboxClient;
+  private final LobbyChatClient lobbyChatClient;
+  private final ModeratorLobbyClient moderatorLobbyClient;
   private final UserAccountClient userAccountClient;
 
   private HttpLobbyClient(final URI lobbyUri, final ApiKey apiKey) {
     connectivityCheckClient = ConnectivityCheckClient.newClient(lobbyUri, apiKey);
     gameListingClient = GameListingClient.newClient(lobbyUri, apiKey);
     httpModeratorToolboxClient = HttpModeratorToolboxClient.newClient(lobbyUri, apiKey);
+    lobbyChatClient = LobbyChatClient.newClient(lobbyUri, apiKey);
+    moderatorLobbyClient = ModeratorLobbyClient.newClient(lobbyUri, apiKey);
     userAccountClient = UserAccountClient.newClient(lobbyUri, apiKey);
   }
 
   public static HttpLobbyClient newClient(final URI lobbyUri, final ApiKey apiKey) {
     return new HttpLobbyClient(lobbyUri, apiKey);
+  }
+
+  public void addConnectionLostListener(final Consumer<String> connectionClosedListener) {
+    lobbyChatClient.addConnectionLostListener(connectionClosedListener);
   }
 }
