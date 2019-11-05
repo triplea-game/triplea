331f16fe9e9b2032bccffbb02d7660e293cbd576
asvitkine <alexei.svitkine@gmail.com>
2019-11-05 00:02:10 -0500

    Optimize TripleAUnit.getTransporting(). (#5415)
    * Optimize TripleAUnit.getTransporting().
    
    This is called extensively by ProAI and this change improves the runtime significantly.
    
    * use functions from Matches

--- game-core/src/main/java/games/strategy/triplea/TripleAUnit.java
+++ game-core/src/main/java/games/strategy/triplea/TripleAUnit.java
@@ -110,12 +110,14 @@ public class TripleAUnit extends Unit {
    * method if possible.
    */
   public List<Unit> getTransporting() {
-    // we don't store the units we are transporting
-    // rather we look at the transported by property of units
-    for (final Territory t : getData().getMap()) {
-      // find the territory this transport is in
-      if (t.getUnitCollection().contains(this)) {
-        return getTransporting(t.getUnitCollection());
+    if (Matches.unitCanTransport().test(this) || Matches.unitIsCarrier().test(this)) {
+      // we don't store the units we are transporting
+      // rather we look at the transported by property of units
+      for (final Territory t : getData().getMap()) {
+        // find the territory this transport is in
+        if (t.getUnitCollection().contains(this)) {
+          return getTransporting(t.getUnitCollection());
+        }
       }
     }
     return Collections.emptyList();
