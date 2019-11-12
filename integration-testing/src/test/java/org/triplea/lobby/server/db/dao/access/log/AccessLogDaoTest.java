a43b0bcd22ad1dec3b23239e7f9de30f4e9ccff8
DanVanAtta <dhvatta@gmail.com>
2019-11-11 21:49:54 -0800

    AccessLogUpdater implementation
    - Add insert method to AccessLogDao.
    - Implement AccessLogUpdater, use the new DAO insert method.

--- integration-testing/src/test/java/org/triplea/lobby/server/db/dao/access/log/AccessLogDaoTest.java
+++ integration-testing/src/test/java/org/triplea/lobby/server/db/dao/access/log/AccessLogDaoTest.java
@@ -5,6 +5,7 @@ import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
 import static org.hamcrest.core.Is.is;
 
 import com.github.database.rider.core.api.dataset.DataSet;
+import com.github.database.rider.core.api.dataset.ExpectedDataSet;
 import java.time.Instant;
 import java.util.List;
 import org.junit.jupiter.api.Test;
@@ -50,5 +51,19 @@ class AccessLogDaoTest extends DaoTest {
   @DataSet(cleanBefore = true, value = "access_log/two_rows.yml")
   void requestingRowsOffDataSetReturnsNothing() {
     assertThat(accessLogDao.fetchAccessLogRows(2, 1), hasSize(0));
+  }
+
+  @Test
+  @DataSet(cleanBefore = true, value = "access_log/empty_data.yml")
+  @ExpectedDataSet("access_log/insert_registered_after.yml")
+  void insertRegisteredUserAccessLog() {
+    accessLogDao.insertRegisteredUserRecord("registered", "127.0.0.20", "registered-system-id");
+  }
+
+  @Test
+  @DataSet(cleanBefore = true, value = "access_log/empty_data.yml")
+  @ExpectedDataSet("access_log/insert_anonymous_after.yml")
+  void insertAnonymousAccessLog() {
+    accessLogDao.insertAnonymousUserRecord("anonymous", "127.0.0.50", "anonymous-system-id");
   }
 }
--- lobby-db-dao/src/main/java/org/triplea/lobby/server/db/dao/access/log/AccessLogDao.java
+++ lobby-db-dao/src/main/java/org/triplea/lobby/server/db/dao/access/log/AccessLogDao.java
@@ -9,6 +9,7 @@ import static org.triplea.lobby.server.db.dao.access.log.AccessLogRecord.USERNAM
 import java.util.List;
 import org.jdbi.v3.sqlobject.customizer.Bind;
 import org.jdbi.v3.sqlobject.statement.SqlQuery;
+import org.jdbi.v3.sqlobject.statement.SqlUpdate;
 
 /**
  * Provides access to the access log table. This is a table that records user data as they enter the
@@ -36,4 +37,16 @@ public interface AccessLogDao {
           + "fetch next :rowCount rows only")
   List<AccessLogRecord> fetchAccessLogRows(
       @Bind("rowOffset") int rowOffset, @Bind("rowCount") int rowCount);
+
+  @SqlUpdate(
+      "insert into access_log(username, ip, system_id, registered)\n"
+          + "values(:username, :ip::inet, :systemId, true)")
+  int insertRegisteredUserRecord(
+      @Bind("username") String username, @Bind("ip") String ip, @Bind("systemId") String systemId);
+
+  @SqlUpdate(
+      "insert into access_log(username, ip, system_id, registered)\n"
+          + "values(:username, :ip::inet, :systemId, false)")
+  int insertAnonymousUserRecord(
+      @Bind("username") String username, @Bind("ip") String ip, @Bind("systemId") String systemId);
 }
