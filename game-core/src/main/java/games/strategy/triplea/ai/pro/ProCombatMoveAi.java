437a152c62ed370b10df77e1482c617353dfe223
Ron <ron.murhammer@gmail.com>
2019-11-19 00:24:14 -0600

    AI fix v3 naval bombard

--- game-core/src/main/java/games/strategy/triplea/ai/pro/ProCombatMoveAi.java
+++ game-core/src/main/java/games/strategy/triplea/ai/pro/ProCombatMoveAi.java
@@ -1674,7 +1674,7 @@ public class ProCombatMoveAi {
                     final double strengthDifference =
                         ProBattleUtils.estimateStrengthDifference(
                             territoryToMoveTransport, attackers, defenders);
-                    if (strengthDifference < minStrengthDifference) {
+                    if (strengthDifference <= minStrengthDifference) {
                       minStrengthDifference = strengthDifference;
                       minUnloadFromTerritory = territoryToMoveTransport;
                     }
