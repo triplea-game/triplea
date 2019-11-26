bab45584bda0311f86089f6504f8cf04ab7c6d1a
tripleabuilderbot <tripleabuilderbot@gmail.com>
2019-11-26 14:16:06 +0000

    Auto-Formatting

--- game-core/src/main/java/games/strategy/triplea/ai/pro/simulate/ProSimulateTurnUtils.java
+++ game-core/src/main/java/games/strategy/triplea/ai/pro/simulate/ProSimulateTurnUtils.java
@@ -26,11 +26,9 @@ import games.strategy.triplea.delegate.TransportTracker;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.Set;
 import javax.annotation.Nullable;
 import org.triplea.java.collections.CollectionUtils;
 
