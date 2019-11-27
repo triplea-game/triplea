b99eacba409bcb046ccf716026a1c6766d3ca6f9
DanVanAtta <dhvatta@gmail.com>
2019-11-27 14:59:33 -0800

    Simplify DefaultNamed: remove boilerplate code
    Use utility functions and lombok to remove boilerplate code

--- game-core/src/main/java/games/strategy/engine/data/DefaultNamed.java
+++ game-core/src/main/java/games/strategy/engine/data/DefaultNamed.java
@@ -1,50 +1,25 @@
 package games.strategy.engine.data;
 
-import static com.google.common.base.Preconditions.checkNotNull;
-
-import com.google.common.base.MoreObjects;
 import java.io.IOException;
 import java.io.ObjectInputStream;
-import java.util.Objects;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+import org.triplea.java.ArgChecker;
 
 /** Default implementation of {@link Named} for game data components. */
+@EqualsAndHashCode(callSuper = false)
+@ToString
 public class DefaultNamed extends GameDataComponent implements Named {
   private static final long serialVersionUID = -5737716450699952621L;
 
+  @Getter(onMethod_ = @Override)
   private final String name;
 
   public DefaultNamed(final String name, final GameData data) {
     super(data);
-
-    checkNotNull(name);
-    if (name.length() == 0) {
-      throw new IllegalArgumentException("Name must not be empty");
-    }
-
+    ArgChecker.checkNotEmpty(name);
     this.name = name;
-  }
-
-  @Override
-  public String getName() {
-    return name;
-  }
-
-  @Override
-  public boolean equals(final Object other) {
-    if (other instanceof Named) {
-      return Objects.equals(name, ((Named) other).getName());
-    }
-    return false;
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hashCode(name);
-  }
-
-  @Override
-  public String toString() {
-    return MoreObjects.toStringHelper(getClass()).add("name", name).toString();
   }
 
   // Workaround for JDK-8199664
