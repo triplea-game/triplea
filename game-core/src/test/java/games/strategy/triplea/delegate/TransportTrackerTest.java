189437ab293fd91dc890ad02fe9ab9189c6f71d8
Alexei Svitkine <alexei.svitkine@gmail.com>
2019-12-04 00:43:53 -0500

    Fix TransportTracker.isTransporting().

---
+++ game-core/src/test/java/games/strategy/triplea/delegate/TransportTrackerTest.java
@@ -0,0 +1,39 @@
+package games.strategy.triplea.delegate;
+
+import static games.strategy.triplea.delegate.GameDataTestUtil.addTo;
+import static games.strategy.triplea.delegate.GameDataTestUtil.americans;
+import static games.strategy.triplea.delegate.GameDataTestUtil.armour;
+import static games.strategy.triplea.delegate.GameDataTestUtil.territory;
+import static games.strategy.triplea.delegate.GameDataTestUtil.transport;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import games.strategy.engine.data.Change;
+import games.strategy.engine.data.GameData;
+import games.strategy.engine.data.Territory;
+import games.strategy.engine.data.Unit;
+import games.strategy.triplea.TripleAUnit;
+import games.strategy.triplea.xml.TestMapGameData;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+class TransportTrackerTest {
+  private final GameData gameData = TestMapGameData.REVISED.getGameData();
+  private final Territory brazil = territory("Brazil", gameData);
+  private final Territory sz18 = territory("18 Sea Zone", gameData);
+  private final Unit transport = transport(gameData).create(americans(gameData));
+  private final Unit tank = armour(gameData).create(americans(gameData));
+
+  public TransportTrackerTest() throws Exception {}
+
+  @Test
+  void testIsTransporting() throws Exception {
+    addTo(sz18, List.of(transport));
+    assertThat(TransportTracker.isTransporting(transport), is(false));
+
+    addTo(sz18, List.of(tank));
+    final Change change = TransportTracker.loadTransportChange((TripleAUnit) transport, tank);
+    gameData.performChange(change);
+    assertThat(TransportTracker.isTransporting(transport), is(true));
+  }
+}
