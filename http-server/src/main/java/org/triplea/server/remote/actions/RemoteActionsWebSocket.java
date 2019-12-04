fcc5d93d79a0256c32ec038201577f1282b62921
DanVanAtta <dhvatta@gmail.com>
2019-12-03 22:59:04 -0800

    Remove testing TODO and remove unneeded websocket method

--- http-server/src/main/java/org/triplea/server/remote/actions/RemoteActionsWebSocket.java
+++ http-server/src/main/java/org/triplea/server/remote/actions/RemoteActionsWebSocket.java
@@ -4,14 +4,12 @@ import com.google.common.base.Preconditions;
 import javax.websocket.CloseReason;
 import javax.websocket.OnClose;
 import javax.websocket.OnError;
-import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.ServerEndpoint;
 import lombok.extern.slf4j.Slf4j;
 import org.triplea.http.client.remote.actions.RemoteActionsWebsocketListener;
 
-// TODO: test-me
 /**
  * This websocket is available for game hosts to 'listen' to remote action events. This might be a
  * player was banned and should be disconnected, or for moderator actions like requesting a server
@@ -32,10 +30,6 @@ public class RemoteActionsWebSocket {
     return Preconditions.checkNotNull(
         (RemoteActionsEventQueue) session.getUserProperties().get(ACTIONS_QUEUE_KEY));
   }
-
-  // TODO: do we need to define this method?
-  @OnMessage
-  public void message(final Session session, final String message) {}
 
   @OnClose
   public void close(final Session session, final CloseReason closeReason) {
