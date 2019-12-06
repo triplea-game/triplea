aea7dfb93a8255f10c7b6e1156a49f5f065b4189
asvitkine <alexei.svitkine@gmail.com>
2019-12-05 22:43:35 -0500

    Use MoveDescription in MoveBatcher class. (#5668)
    * Use MoveDescription in MoveBatcher class.
    
    * Use addAll().

--- game-core/src/main/java/games/strategy/triplea/ai/pro/util/MoveBatcher.java
+++ game-core/src/main/java/games/strategy/triplea/ai/pro/util/MoveBatcher.java
@@ -13,55 +13,7 @@ import java.util.Map;
  * distinct moves the AI does (and thus the visual delay) by combining moves together.
  */
 public class MoveBatcher {
-  // TODO: #5499 Merge/replace with MoveDescription.
-  private static class Move {
-    private final ArrayList<Unit> units;
-    private final Route route;
-    private final HashMap<Unit, Unit> unitsToTransports;
-
-    private Move(
-        final ArrayList<Unit> units,
-        final Route route,
-        final HashMap<Unit, Unit> unitsToTransports) {
-      this.units = units;
-      this.route = route;
-      this.unitsToTransports = unitsToTransports;
-    }
-
-    Move(final ArrayList<Unit> units, final Route route) {
-      this(units, route, new HashMap<>());
-    }
-
-    Move(final Unit unit, final Route route, final Unit transportToLoad) {
-      this(new ArrayList<>(List.of(unit)), route, new HashMap<>(Map.of(unit, transportToLoad)));
-    }
-
-    private boolean isTransportLoad() {
-      return this.unitsToTransports != null;
-    }
-
-    boolean canMergeWith(final Move other) {
-      return other.isTransportLoad() == isTransportLoad() && route.equals(other.route);
-    }
-
-    boolean mergeWith(final Move other) {
-      if (canMergeWith(other)) {
-        // Merge units and transports.
-        units.addAll(other.units);
-        if (isTransportLoad()) {
-          unitsToTransports.putAll(other.unitsToTransports);
-        }
-        return true;
-      }
-      return false;
-    }
-
-    MoveDescription toMoveDescription() {
-      return new MoveDescription(units, route, unitsToTransports, Map.of());
-    }
-  }
-
-  private final ArrayList<ArrayList<Move>> moveSequences = new ArrayList<>();
+  private final ArrayList<ArrayList<MoveDescription>> moveSequences = new ArrayList<>();
 
   /**
    * Starts a new sequence. This must be called before calling any add*() methods. A sequence
@@ -79,24 +31,27 @@ public class MoveBatcher {
    * @param transport The transport to load.
    */
   public void addTransportLoad(final Unit unit, final Route route, final Unit transport) {
-    addMove(new Move(unit, route, transport));
+    addMove(new MoveDescription(List.of(unit), route, Map.of(unit, transport)));
   }
 
   /**
    * Adds a move for the specified units. The units list must not be changed after adding the move.
    *
-   * @param units The units to move. Note that type is ArrayList explicitly to prevent an
-   *     unmodifiable List.of() being passed.
+   * @param units The units to move.
    * @param route The route to move on.
    */
-  public void addMove(final ArrayList<Unit> units, final Route route) {
-    addMove(new Move(units, route));
+  public void addMove(final List<Unit> units, final Route route) {
+    addMove(new MoveDescription(units, route));
   }
 
-  private void addMove(final Move newMove) {
-    final ArrayList<Move> sequence = moveSequences.get(moveSequences.size() - 1);
-    if (!sequence.isEmpty() && sequence.get(sequence.size() - 1).mergeWith(newMove)) {
-      return;
+  private void addMove(final MoveDescription newMove) {
+    final ArrayList<MoveDescription> sequence = moveSequences.get(moveSequences.size() - 1);
+    if (!sequence.isEmpty()) {
+      final MoveDescription lastMove = sequence.get(sequence.size() - 1);
+      if (canMergeMoves(lastMove, newMove)) {
+        sequence.set(sequence.size() - 1, mergeMoves(lastMove, newMove));
+        return;
+      }
     }
     sequence.add(newMove);
   }
@@ -112,9 +67,7 @@ public class MoveBatcher {
     for (final var sequence : moveSequences) {
       if (!sequence.isEmpty()) {
         mergeSequences(sequence, moveSequences.subList(i + 1, moveSequences.size()));
-        for (final Move move : sequence) {
-          moves.add(move.toMoveDescription());
-        }
+        moves.addAll(sequence);
       }
       i++;
     }
@@ -129,22 +82,41 @@ public class MoveBatcher {
    * @param sequences The sequences to try to merge into the sequence.
    */
   private static void mergeSequences(
-      final ArrayList<Move> sequence, final List<ArrayList<Move>> sequences) {
+      final ArrayList<MoveDescription> sequence, final List<ArrayList<MoveDescription>> sequences) {
     for (final var otherSequence : sequences) {
       boolean merge = (otherSequence.size() == sequence.size());
       for (int i = 0; merge && i < sequence.size(); i++) {
-        merge = sequence.get(i).canMergeWith(otherSequence.get(i));
+        merge = canMergeMoves(sequence.get(i), otherSequence.get(i));
       }
       if (!merge) {
         continue;
       }
       for (int i = 0; i < sequence.size(); i++) {
-        if (!sequence.get(i).mergeWith(otherSequence.get(i))) {
-          throw new IllegalStateException(
-              "Could not merge move despite checking canMergeWith() earlier.");
-        }
+        sequence.set(i, mergeMoves(sequence.get(i), otherSequence.get(i)));
       }
       otherSequence.clear();
     }
+  }
+
+  private static boolean canMergeMoves(final MoveDescription move1, final MoveDescription move2) {
+    return isTransportLoad(move1) == isTransportLoad(move2)
+        && move1.getRoute().equals(move2.getRoute());
+  }
+
+  private static boolean isTransportLoad(final MoveDescription move) {
+    return !move.getUnitsToTransports().isEmpty();
+  }
+
+  // Merges the two moves. Caller must ensure canMergeMoves() is true before calling.
+  private static MoveDescription mergeMoves(
+      final MoveDescription move1, final MoveDescription move2) {
+    if (!canMergeMoves(move1, move2)) {
+      throw new IllegalStateException("can't merge moves: " + move1 + " and " + move2);
+    }
+    final var units = new ArrayList<Unit>(move1.getUnits());
+    units.addAll(move2.getUnits());
+    final var unitsToTransports = new HashMap<Unit, Unit>(move1.getUnitsToTransports());
+    unitsToTransports.putAll(move2.getUnitsToTransports());
+    return new MoveDescription(units, move1.getRoute(), unitsToTransports);
   }
 }
