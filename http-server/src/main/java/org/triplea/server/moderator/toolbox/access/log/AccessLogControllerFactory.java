f0efb007338f10fdae776eabe033e5573b1883fd
DanVanAtta <dhvatta@gmail.com>
2019-12-06 15:48:53 -0800

    Remove unused parameter "AppConfig"

--- http-server/src/main/java/org/triplea/server/moderator/toolbox/access/log/AccessLogControllerFactory.java
+++ http-server/src/main/java/org/triplea/server/moderator/toolbox/access/log/AccessLogControllerFactory.java
@@ -4,13 +4,12 @@ import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import org.jdbi.v3.core.Jdbi;
 import org.triplea.lobby.server.db.dao.access.log.AccessLogDao;
-import org.triplea.server.http.AppConfig;
 
 /** Factory class, instantiates {@code AccessLogControllerFactory}. */
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class AccessLogControllerFactory {
 
-  public static AccessLogController buildController(final AppConfig appConfig, final Jdbi jdbi) {
+  public static AccessLogController buildController(final Jdbi jdbi) {
     return AccessLogController.builder()
         .accessLogService(
             AccessLogService.builder().accessLogDao(jdbi.onDemand(AccessLogDao.class)).build())
