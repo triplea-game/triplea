triplea-game/triplea

commit 1b70b83b0e59578c534a5a8ce9d5c404974dcb65

Batch AI amphibious moves. (#5416)
* Batch AI amphibious moves.

Prior to this change, the AI would move load and move transports
one unit at a time. So moving 20 units via transports would take
40 moves: 20 x (1 load, 1 load, 1 move, 1 unload). With this change,
it's just 3 moves (when they all have the same destination):
  1. Load 20 units
  2. Move 10 transports
  3. Unload 20 units.

This makes a big difference when playing with AI since this makes
the AI moves be much faster when amphibious moves are involved.

game-core/src/main/java/games/strategy/triplea/ui/MovePanel.java
@@ -299,9 +299,11 @@ public class MovePanel extends AbstractMovePanel implements KeyBindingSupplier {
                   final Collection<Unit> loadedAirTransports =
                       getLoadedAirTransports(route, unitsToLoad, airTransportsToLoad, player);
                   selectedUnits.addAll(loadedAirTransports);
+                  final Map<Unit, Unit> unitsToTransports =
+                      TransportUtils.mapTransportsToLoad(loadedAirTransports, airTransportsToLoad);
                   final MoveDescription message =
                       new MoveDescription(
-                          loadedAirTransports, route, airTransportsToLoad, dependentUnits);
+                          loadedAirTransports, route, unitsToTransports, dependentUnits);
                   setMoveMessage(message);
                 }
               }
@@ -592,8 +594,10 @@ public class MovePanel extends AbstractMovePanel implements KeyBindingSupplier {
               return;
             }
           }
+          final Map<Unit, Unit> unitsToTransports =
+              transports == null ? null : TransportUtils.mapTransports(route, units, transports);
           final MoveDescription message =
-              new MoveDescription(units, route, transports, dependentUnits);
+              new MoveDescription(units, route, unitsToTransports, dependentUnits);
           setMoveMessage(message);
           setFirstSelectedTerritory(null);
           setSelectedEndpointTerritory(null);
@@ -1061,14 +1065,15 @@ public class MovePanel extends AbstractMovePanel implements KeyBindingSupplier {
     }
     getMap().hideMouseCursor();
     // TODO kev check for already loaded airTransports
-    Collection<Unit> transportsToLoad = Collections.emptyList();
+    Map<Unit, Unit> unitsToTransports = Map.of();
     if (MoveValidator.isLoad(units, dependentUnits, route, getData(), getCurrentPlayer())) {
-      transportsToLoad =
+      final Collection<Unit> transportsToLoad =
           route
               .getEnd()
               .getUnitCollection()
               .getMatches(
                   Matches.unitIsTransport().and(Matches.alliedUnit(getCurrentPlayer(), getData())));
+      unitsToTransports = TransportUtils.mapTransports(route, units, transportsToLoad);
     }
     List<Unit> best = new ArrayList<>(units);
     // if the player selects a land unit and other units when the
@@ -1094,7 +1099,7 @@ public class MovePanel extends AbstractMovePanel implements KeyBindingSupplier {
               bestWithDependents,
               route,
               getCurrentPlayer(),
-              transportsToLoad,
+              unitsToTransports,
               dependentUnits,
               nonCombat,
               getUndoableMoves(),
@@ -1116,7 +1121,7 @@ public class MovePanel extends AbstractMovePanel implements KeyBindingSupplier {
                 bestWithDependents,
                 route,
                 getCurrentPlayer(),
-                transportsToLoad,
+                unitsToTransports,
                 dependentUnits,
                 nonCombat,
                 getUndoableMoves(),
@@ -1131,7 +1136,7 @@ public class MovePanel extends AbstractMovePanel implements KeyBindingSupplier {
                 bestWithDependents,
                 route,
                 getCurrentPlayer(),
-                transportsToLoad,
+                unitsToTransports,
                 dependentUnits,
                 nonCombat,
                 getUndoableMoves(),

game-core/src/main/java/games/strategy/triplea/TripleAPlayer.java
@@ -346,7 +346,7 @@ public abstract class TripleAPlayer extends AbstractHumanPlayer {
         moveDel.move(
             moveDescription.getUnits(),
             moveDescription.getRoute(),
-            moveDescription.getTransportsThatCanBeLoaded(),
+            moveDescription.getUnitsToTransports(),
             moveDescription.getDependentUnits());
     if (error != null) {
       ui.notifyError(error);

game-core/src/main/java/games/strategy/triplea/ai/pro/ProCombatMoveAi.java
@@ -168,7 +168,7 @@ public class ProCombatMoveAi {
 
     moveUnits.clear();
     moveRoutes.clear();
-    final List<Collection<Unit>> transportsToLoad = new ArrayList<>();
+    final var transportsToLoad = new ArrayList<Map<Unit, Unit>>();
     ProMoveUtils.calculateAmphibRoutes(
         player, moveUnits, moveRoutes, transportsToLoad, attackMap, true);
     ProMoveUtils.doMove(moveUnits, moveRoutes, transportsToLoad, moveDel);

game-core/src/main/java/games/strategy/triplea/ai/pro/ProNonCombatMoveAi.java
@@ -218,7 +218,7 @@ class ProNonCombatMoveAi {
     // Calculate amphib move routes and perform moves
     moveUnits.clear();
     moveRoutes.clear();
-    final List<Collection<Unit>> transportsToLoad = new ArrayList<>();
+    final var transportsToLoad = new ArrayList<Map<Unit, Unit>>();
     ProMoveUtils.calculateAmphibRoutes(
         player, moveUnits, moveRoutes, transportsToLoad, moveMap, false);
     ProMoveUtils.doMove(moveUnits, moveRoutes, transportsToLoad, moveDel);
@@ -2527,14 +2527,7 @@ class ProNonCombatMoveAi {
                       player);
           final MoveValidationResult mvr =
               MoveValidator.validateMove(
-                  Collections.singletonList(u),
-                  r,
-                  player,
-                  new ArrayList<>(),
-                  new HashMap<>(),
-                  true,
-                  null,
-                  data);
+                  Collections.singletonList(u), r, player, Map.of(), Map.of(), true, null, data);
           if (!mvr.isMoveValid()) {
             continue;
           }

game-core/src/main/java/games/strategy/triplea/ai/pro/util/MoveBatcher.java
@@ -1,0 +1,161 @@
+package games.strategy.triplea.ai.pro.util;
+
+import games.strategy.engine.data.Route;
+import games.strategy.engine.data.Unit;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.annotation.Nullable;
+
+/**
+ * MoveBatcher implements an algorithm that batches AI moves together, reducing the number of
+ * distinct moves the AI does (and thus the visual delay) by combining moves together.
+ */
+public class MoveBatcher {
+  private static class Move {
+    private final ArrayList<Unit> units;
+    private final Route route;
+    private final @Nullable HashMap<Unit, Unit> unitsToTransports;
+
+    private Move(
+        final ArrayList<Unit> units,
+        final Route route,
+        final @Nullable HashMap<Unit, Unit> unitsToTransports) {
+      this.units = units;
+      this.route = route;
+      this.unitsToTransports = unitsToTransports;
+    }
+
+    Move(final ArrayList<Unit> units, final Route route) {
+      this(units, route, null);
+    }
+
+    Move(final Unit unit, final Route route, final Unit transportToLoad) {
+      this(new ArrayList<>(List.of(unit)), route, new HashMap<>(Map.of(unit, transportToLoad)));
+    }
+
+    private boolean isTransportLoad() {
+      return this.unitsToTransports != null;
+    }
+
+    boolean canMergeWith(final Move other) {
+      return other.isTransportLoad() == isTransportLoad() && route.equals(other.route);
+    }
+
+    boolean mergeWith(final Move other) {
+      if (canMergeWith(other)) {
+        // Merge units and transports.
+        units.addAll(other.units);
+        if (isTransportLoad()) {
+          unitsToTransports.putAll(other.unitsToTransports);
+        }
+        return true;
+      }
+      return false;
+    }
+
+    void addTo(
+        final List<Collection<Unit>> moveUnits,
+        final List<Route> moveRoutes,
+        final List<Map<Unit, Unit>> unitsToTransports) {
+      moveUnits.add(units);
+      moveRoutes.add(route);
+      unitsToTransports.add(this.unitsToTransports);
+    }
+  }
+
+  private final ArrayList<ArrayList<Move>> moveSequences = new ArrayList<>();
+
+  /**
+   * Starts a new sequence. This must be called before calling any add*() methods. A sequence
+   * indicates a logical dependency relationship between the moves in that sequence.
+   */
+  public void newSequence() {
+    moveSequences.add(new ArrayList<>());
+  }
+
+  /**
+   * Adds a transport load move.
+   *
+   * @param unit The unit to move.
+   * @param route The route to move on.
+   * @param transport The transport to load.
+   */
+  public void addTransportLoad(final Unit unit, final Route route, final Unit transport) {
+    addMove(new Move(unit, route, transport));
+  }
+
+  /**
+   * Adds a move for the specified units. The units list must not be changed after adding the move.
+   *
+   * @param units The units to move. Note that type is ArrayList explicitly to prevent an
+   *     unmodifiable List.of() being passed.
+   * @param route The route to move on.
+   */
+  public void addMove(final ArrayList<Unit> units, final Route route) {
+    addMove(new Move(units, route));
+  }
+
+  private void addMove(final Move newMove) {
+    final ArrayList<Move> sequence = moveSequences.get(moveSequences.size() - 1);
+    if (!sequence.isEmpty() && sequence.get(sequence.size() - 1).mergeWith(newMove)) {
+      return;
+    }
+    sequence.add(newMove);
+  }
+
+  /**
+   * Emits the accumulated moves to the passed output params.
+   *
+   * <p>TODO: #5416 Refactor the IMoveDelegate and ProMoveUtils code to use a List of Move objects,
+   * instead of these parallel lists.
+   *
+   * @param moveUnits The units to move.
+   * @param moveRoutes The routes to move along
+   * @param unitsToTransports The map from units to transports to be loaded.
+   */
+  public void batchAndEmit(
+      final List<Collection<Unit>> moveUnits,
+      final List<Route> moveRoutes,
+      final List<Map<Unit, Unit>> unitsToTransports) {
+    int i = 0;
+    for (final var sequence : moveSequences) {
+      if (!sequence.isEmpty()) {
+        mergeSequences(sequence, moveSequences.subList(i + 1, moveSequences.size()));
+        for (final Move move : sequence) {
+          move.addTo(moveUnits, moveRoutes, unitsToTransports);
+        }
+      }
+      i++;
+    }
+  }
+
+  /**
+   * Attempts to merge a sequence of moves with other sequences. Any sequence merged into this
+   * sequence will have its moves cleared.
+   *
+   * @param sequence The sequence to merge other sequences into.
+   * @param sequences The sequences to try to merge into the sequence.
+   */
+  private static void mergeSequences(
+      final ArrayList<Move> sequence, final List<ArrayList<Move>> sequences) {
+    for (final var otherSequence : sequences) {
+      boolean merge = (otherSequence.size() == sequence.size());
+      for (int i = 0; merge && i < sequence.size(); i++) {
+        merge = sequence.get(i).canMergeWith(otherSequence.get(i));
+      }
+      if (!merge) {
+        continue;
+      }
+      for (int i = 0; i < sequence.size(); i++) {
+        if (!sequence.get(i).mergeWith(otherSequence.get(i))) {
+          throw new IllegalStateException(
+              "Could not merge move despite checking canMergeWith() earlier.");
+        }
+      }
+      otherSequence.clear();
+    }
+  }
+}

game-core/src/main/java/games/strategy/triplea/ai/pro/util/ProMoveUtils.java
@@ -74,7 +74,7 @@ public final class ProMoveUtils {
         }
 
         // Add unit to move list
-        final List<Unit> unitList = new ArrayList<>();
+        final var unitList = new ArrayList<Unit>();
         unitList.add(u);
         moveUnits.add(unitList);
         if (Matches.unitIsLandTransport().test(u)) {
@@ -162,7 +162,7 @@ public final class ProMoveUtils {
    *
    * @param moveUnits Receives the unit groups to move.
    * @param moveRoutes Receives the routes for each unit group in {@code moveUnits}.
-   * @param transportsToLoad Receives the transport groups for each unit group in {@code moveUnits}.
+   * @param unitsToTransports Receives the maps from units to transports to load.
    * @param attackMap Specifies the territories to be attacked. Will be updated to reflect any
    *     transports unloading in a specific territory.
    */
@@ -170,12 +170,14 @@ public final class ProMoveUtils {
       final PlayerId player,
       final List<Collection<Unit>> moveUnits,
       final List<Route> moveRoutes,
-      final List<Collection<Unit>> transportsToLoad,
+      final List<Map<Unit, Unit>> unitsToTransports,
       final Map<Territory, ProTerritory> attackMap,
       final boolean isCombatMove) {
 
     final GameData data = ProData.getData();
     final GameMap map = data.getMap();
+
+    final MoveBatcher moves = new MoveBatcher();
 
     // Loop through all territories to attack
     for (final Territory t : attackMap.keySet()) {
@@ -185,10 +187,12 @@ public final class ProMoveUtils {
       for (final Unit transport : amphibAttackMap.keySet()) {
         int movesLeft = TripleAUnit.get(transport).getMovementLeft().intValue();
         Territory transportTerritory = ProData.unitTerritoryMap.get(transport);
+        moves.newSequence();
 
         // Check if units are already loaded or not
-        final List<Unit> loadedUnits = new ArrayList<>();
-        final List<Unit> remainingUnitsToLoad = new ArrayList<>();
+        final var loadedUnits = new ArrayList<Unit>();
+        final var remainingUnitsToLoad = new ArrayList<Unit>();
+
         if (TransportTracker.isTransporting(transport)) {
           loadedUnits.addAll(amphibAttackMap.get(transport));
         } else {
@@ -200,15 +204,12 @@ public final class ProMoveUtils {
 
           // Load adjacent units if no enemies present in transport territory
           if (Matches.territoryHasEnemyUnits(player, data).negate().test(transportTerritory)) {
-            final List<Unit> unitsToRemove = new ArrayList<>();
+            final var unitsToRemove = new ArrayList<Unit>();
             for (final Unit amphibUnit : remainingUnitsToLoad) {
-              if (map.getDistance(transportTerritory, ProData.unitTerritoryMap.get(amphibUnit))
-                  == 1) {
-                moveUnits.add(Collections.singletonList(amphibUnit));
-                transportsToLoad.add(Collections.singletonList(transport));
-                final Route route =
-                    new Route(ProData.unitTerritoryMap.get(amphibUnit), transportTerritory);
-                moveRoutes.add(route);
+              final Territory unitTerritory = ProData.unitTerritoryMap.get(amphibUnit);
+              if (map.getDistance(transportTerritory, unitTerritory) == 1) {
+                final Route route = new Route(unitTerritory, transportTerritory);
+                moves.addTransportLoad(amphibUnit, route, transport);
                 unitsToRemove.add(amphibUnit);
                 loadedUnits.add(amphibUnit);
               }
@@ -283,13 +284,10 @@ public final class ProMoveUtils {
               }
             }
             if (territoryToMoveTo != null) {
-              final List<Unit> unitsToMove = new ArrayList<>();
+              final var unitsToMove = new ArrayList<Unit>();
               unitsToMove.add(transport);
               unitsToMove.addAll(loadedUnits);
-              moveUnits.add(unitsToMove);
-              transportsToLoad.add(null);
-              final Route route = new Route(transportTerritory, territoryToMoveTo);
-              moveRoutes.add(route);
+              moves.addMove(unitsToMove, new Route(transportTerritory, territoryToMoveTo));
               transportTerritory = territoryToMoveTo;
             }
           }
@@ -311,13 +309,13 @@ public final class ProMoveUtils {
 
         // Unload transport
         if (!loadedUnits.isEmpty() && !t.isWater()) {
-          moveUnits.add(loadedUnits);
-          transportsToLoad.add(null);
           final Route route = new Route(transportTerritory, t);
-          moveRoutes.add(route);
+          moves.addMove(loadedUnits, route);
         }
       }
     }
+
+    moves.batchAndEmit(moveUnits, moveRoutes, unitsToTransports);
   }
 
   /**
@@ -350,7 +348,7 @@ public final class ProMoveUtils {
         }
 
         // Add unit to move list
-        final List<Unit> unitList = new ArrayList<>();
+        final var unitList = new ArrayList<Unit>();
         unitList.add(u);
         moveUnits.add(unitList);
 
@@ -402,7 +400,7 @@ public final class ProMoveUtils {
         }
 
         // Add unit to move list
-        final List<Unit> unitList = new ArrayList<>();
+        final var unitList = new ArrayList<Unit>();
         unitList.add(u);
         moveUnits.add(unitList);
 
@@ -436,7 +434,7 @@ public final class ProMoveUtils {
   public static void doMove(
       final List<Collection<Unit>> moveUnits,
       final List<Route> moveRoutes,
-      final List<Collection<Unit>> transportsToLoad,
+      final List<Map<Unit, Unit>> transportsToLoad,
       final IMoveDelegate moveDel) {
 
     final GameData data = ProData.getData();

game-core/src/main/java/games/strategy/triplea/ai/weak/WeakAi.java
@@ -122,28 +122,28 @@ public class WeakAi extends AbstractAi {
       final IMoveDelegate moveDel, final PlayerId player, final GameData data) {
     final List<Collection<Unit>> moveUnits = new ArrayList<>();
     final List<Route> moveRoutes = new ArrayList<>();
-    final List<Collection<Unit>> transportsToLoad = new ArrayList<>();
+    final var unitsToTransport = new ArrayList<Map<Unit, Unit>>();
     // load the transports first
     // they may be able to move farther
-    populateTransportLoad(data, moveUnits, moveRoutes, transportsToLoad, player);
-    doMove(moveUnits, moveRoutes, transportsToLoad, moveDel);
+    populateTransportLoad(data, moveUnits, moveRoutes, unitsToTransport, player);
+    doMove(moveUnits, moveRoutes, unitsToTransport, moveDel);
     moveRoutes.clear();
     moveUnits.clear();
-    transportsToLoad.clear();
+    unitsToTransport.clear();
     // do the rest of the moves
     populateNonCombat(data, moveUnits, moveRoutes, player);
     populateNonCombatSea(true, data, moveUnits, moveRoutes, player);
     doMove(moveUnits, moveRoutes, null, moveDel);
     moveUnits.clear();
     moveRoutes.clear();
-    transportsToLoad.clear();
+    unitsToTransport.clear();
     // load the transports again if we can
     // they may be able to move farther
-    populateTransportLoad(data, moveUnits, moveRoutes, transportsToLoad, player);
-    doMove(moveUnits, moveRoutes, transportsToLoad, moveDel);
+    populateTransportLoad(data, moveUnits, moveRoutes, unitsToTransport, player);
+    doMove(moveUnits, moveRoutes, unitsToTransport, moveDel);
     moveRoutes.clear();
     moveUnits.clear();
-    transportsToLoad.clear();
+    unitsToTransport.clear();
     // unload the transports that can be unloaded
     populateTransportUnloadNonCom(data, moveUnits, moveRoutes, player);
     doMove(moveUnits, moveRoutes, null, moveDel);
@@ -153,11 +153,11 @@ public class WeakAi extends AbstractAi {
       final IMoveDelegate moveDel, final PlayerId player, final GameData data) {
     final List<Collection<Unit>> moveUnits = new ArrayList<>();
     final List<Route> moveRoutes = new ArrayList<>();
-    final List<Collection<Unit>> transportsToLoad = new ArrayList<>();
+    final var unitsToTransports = new ArrayList<Map<Unit, Unit>>();
     // load the transports first
     // they may be able to take part in a battle
-    populateTransportLoad(data, moveUnits, moveRoutes, transportsToLoad, player);
-    doMove(moveUnits, moveRoutes, transportsToLoad, moveDel);
+    populateTransportLoad(data, moveUnits, moveRoutes, unitsToTransports, player);
+    doMove(moveUnits, moveRoutes, unitsToTransports, moveDel);
     moveRoutes.clear();
     moveUnits.clear();
     // we want to move loaded transports before we try to fight our battles
@@ -170,7 +170,7 @@ public class WeakAi extends AbstractAi {
     doMove(moveUnits, moveRoutes, null, moveDel);
     moveUnits.clear();
     moveRoutes.clear();
-    transportsToLoad.clear();
+    unitsToTransports.clear();
     // fight
     populateCombatMove(data, moveUnits, moveRoutes, player);
     populateCombatMoveSea(data, moveUnits, moveRoutes, player);
@@ -181,7 +181,7 @@ public class WeakAi extends AbstractAi {
       final GameData data,
       final List<Collection<Unit>> moveUnits,
       final List<Route> moveRoutes,
-      final List<Collection<Unit>> transportsToLoad,
+      final List<Map<Unit, Unit>> unitsToTransports,
       final PlayerId player) {
     if (!isAmphibAttack(player, data)) {
       return;
@@ -199,6 +199,7 @@ public class WeakAi extends AbstractAi {
         continue;
       }
       final List<Unit> units = new ArrayList<>();
+      final Map<Unit, Unit> transportMap = new HashMap<>();
       for (final Unit transport :
           neighbor.getUnitCollection().getMatches(Matches.unitIsOwnedBy(player))) {
         int free = TransportTracker.getAvailableCapacity(transport);
@@ -216,6 +217,7 @@ public class WeakAi extends AbstractAi {
             iter.remove();
             free -= ua.getTransportCost();
             units.add(current);
+            transportMap.put(current, transport);
           }
         }
       }
@@ -223,7 +225,7 @@ public class WeakAi extends AbstractAi {
         final Route route = new Route(capitol, neighbor);
         moveUnits.add(units);
         moveRoutes.add(route);
-        transportsToLoad.add(neighbor.getUnitCollection().getMatches(Matches.unitIsTransport()));
+        unitsToTransports.add(transportMap);
       }
     }
   }
@@ -262,7 +264,7 @@ public class WeakAi extends AbstractAi {
   private static void doMove(
       final List<Collection<Unit>> moveUnits,
       final List<Route> moveRoutes,
-      final List<Collection<Unit>> transportsToLoad,
+      final List<Map<Unit, Unit>> unitsToTransports,
       final IMoveDelegate moveDel) {
     for (int i = 0; i < moveRoutes.size(); i++) {
       if (moveRoutes.get(i) == null
@@ -272,10 +274,10 @@ public class WeakAi extends AbstractAi {
         continue;
       }
 
-      if (transportsToLoad == null) {
+      if (unitsToTransports == null) {
         moveDel.move(moveUnits.get(i), moveRoutes.get(i));
       } else {
-        moveDel.move(moveUnits.get(i), moveRoutes.get(i), transportsToLoad.get(i));
+        moveDel.move(moveUnits.get(i), moveRoutes.get(i), unitsToTransports.get(i));
       }
       pause();
     }

commit a94ffd58679737e42b5b5e6f6ca756089d962999

Fix incorrect units loaded when loading a transport.
This regressed in #5416, because unitsToTransports was only
computed once after that commit, instead of updated each time
the unit list was filtered.

This fixes #5706.

game-core/src/main/java/games/strategy/triplea/ui/MovePanel.java
@@ -7,6 +7,7 @@ import games.strategy.engine.data.PlayerId;
 import games.strategy.engine.data.Route;
 import games.strategy.engine.data.Territory;
 import games.strategy.engine.data.Unit;
+import games.strategy.engine.data.UnitCollection;
 import games.strategy.engine.data.UnitType;
 import games.strategy.engine.framework.LocalPlayers;
 import games.strategy.triplea.Properties;
@@ -1122,19 +1123,9 @@ public class MovePanel extends AbstractMovePanel implements KeyBindingSupplier {
       return;
     }
     getMap().hideMouseCursor();
-    // TODO kev check for already loaded airTransports
-    Map<Unit, Unit> unitsToTransports = Map.of();
-    if (MoveValidator.isLoad(units, dependentUnits, route, getData(), getCurrentPlayer())) {
-      final Collection<Unit> transportsToLoad =
-          route
-              .getEnd()
-              .getUnitCollection()
-              .getMatches(
-                  Matches.unitIsTransport().and(Matches.alliedUnit(getCurrentPlayer(), getData())));
-      unitsToTransports = TransportUtils.mapTransports(route, units, transportsToLoad);
-    }
+    final Collection<Unit> transportsToLoad = getPossibleTransportsToLoad(units, route);
     List<Unit> best = new ArrayList<>(units);
-    // if the player selects a land unit and other units when the
+    // if the player selects a land unit and other units then
     // only consider the non land units
     if (route.getStart().isWater() && route.getEnd().isWater() && !route.isLoad()) {
       best = CollectionUtils.getMatches(best, Matches.unitIsLand().negate());
@@ -1144,7 +1135,7 @@ public class MovePanel extends AbstractMovePanel implements KeyBindingSupplier {
     }
     sortUnitsToMoveReverse(best, route);
     MoveValidationResultWithDependents lastResult =
-        validateMoveWithDependents(best, unitsToTransports, route);
+        validateMoveWithDependents(best, transportsToLoad, route);
     final MoveValidationResult allUnitsResult = lastResult.getResult();
 
     if (!allUnitsResult.isMoveValid()) {
@@ -1152,11 +1143,11 @@ public class MovePanel extends AbstractMovePanel implements KeyBindingSupplier {
       if (!nonCombat
           && route.isUnload()
           && Matches.isTerritoryEnemy(getCurrentPlayer(), getData()).test(route.getEnd())) {
-        lastResult = validateMoveWithDependents(best, unitsToTransports, route);
+        lastResult = validateMoveWithDependents(best, transportsToLoad, route);
       }
       while (!best.isEmpty() && !lastResult.getResult().isMoveValid()) {
         best = best.subList(1, best.size());
-        lastResult = validateMoveWithDependents(best, unitsToTransports, route);
+        lastResult = validateMoveWithDependents(best, transportsToLoad, route);
       }
     }
     if (allUnitsResult.isMoveValid()) {
@@ -1191,6 +1182,17 @@ public class MovePanel extends AbstractMovePanel implements KeyBindingSupplier {
     unitsThatCanMoveOnRoute = new ArrayList<>(lastResult.getUnitsWithDependents());
   }
 
+  private Collection<Unit> getPossibleTransportsToLoad(
+      final Collection<Unit> units, final Route route) {
+    // TODO kev check for already loaded airTransports
+    if (MoveValidator.isLoad(units, dependentUnits, route, getData(), getCurrentPlayer())) {
+      final UnitCollection unitsAtEnd = route.getEnd().getUnitCollection();
+      return unitsAtEnd.getMatches(
+          Matches.unitIsTransport().and(Matches.alliedUnit(getCurrentPlayer(), getData())));
+    }
+    return List.of();
+  }
+
   @AllArgsConstructor
   @Getter
   private static class MoveValidationResultWithDependents {
@@ -1199,11 +1201,15 @@ public class MovePanel extends AbstractMovePanel implements KeyBindingSupplier {
   }
 
   private MoveValidationResultWithDependents validateMoveWithDependents(
-      final List<Unit> units, final Map<Unit, Unit> unitsToTransports, final Route route) {
+      final List<Unit> units, final Collection<Unit> transportsToLoad, final Route route) {
     final List<Unit> unitsWithDependents = addMustMoveWith(units);
     final MoveValidationResult result;
     getData().acquireReadLock();
     try {
+      final Map<Unit, Unit> unitsToTransports =
+          transportsToLoad.isEmpty()
+              ? Map.of()
+              : TransportUtils.mapTransports(route, units, transportsToLoad);
       final MoveDescription move =
           new MoveDescription(unitsWithDependents, route, unitsToTransports, dependentUnits);
       result =
@@ -1469,8 +1475,7 @@ public class MovePanel extends AbstractMovePanel implements KeyBindingSupplier {
     if (mustQueryUser) {
       final List<Unit> defaultSelections = new ArrayList<>(units.size());
       if (route.isLoad()) {
-        final Collection<Unit> transportsToLoad =
-            new ArrayList<>(getTransportsToLoad(route, units, false));
+        final Collection<Unit> transportsToLoad = getTransportsToLoad(route, units, false);
         defaultSelections.addAll(
             TransportUtils.mapTransports(route, units, transportsToLoad).keySet());
       } else {