5bb08ae96be4ea7c39f355659aa91c5dd510aeda
RoiEX <8350879+RoiEXLab@users.noreply.github.com>
2019-12-14 04:39:02 +0100

    Enhance type safety (#5699)
    Enhance type safety in websocket message type enums for handling websocket message payloads

--- http-clients/src/main/java/org/triplea/http/client/lobby/chat/messages/server/ChatServerMessageType.java
+++ http-clients/src/main/java/org/triplea/http/client/lobby/chat/messages/server/ChatServerMessageType.java
@@ -1,28 +1,37 @@
 package org.triplea.http.client.lobby.chat.messages.server;
 
-import java.util.function.Consumer;
-import java.util.function.Function;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.triplea.domain.data.PlayerName;
 import org.triplea.http.client.lobby.chat.ChatMessageListeners;
 import org.triplea.http.client.lobby.chat.ChatParticipant;
+import org.triplea.http.client.web.socket.messages.MessageTypeListenerBinding;
 import org.triplea.http.client.web.socket.messages.WebsocketMessageType;
 
 /** Chat message types that a server can send over websocket to client. */
-@AllArgsConstructor
-@SuppressWarnings("ImmutableEnumChecker")
 @Getter(onMethod_ = @Override)
+@AllArgsConstructor
 public enum ChatServerMessageType implements WebsocketMessageType<ChatMessageListeners> {
-  CHAT_EVENT(String.class, ChatMessageListeners::getChatEventListener),
-  CHAT_MESSAGE(ChatMessage.class, ChatMessageListeners::getChatMessageListener),
-  PLAYER_JOINED(ChatParticipant.class, ChatMessageListeners::getPlayerJoinedListener),
-  PLAYER_LEFT(PlayerName.class, ChatMessageListeners::getPlayerLeftListener),
-  PLAYER_LISTING(ChatterList.class, ChatMessageListeners::getConnectedListener),
-  PLAYER_SLAPPED(PlayerSlapped.class, ChatMessageListeners::getPlayerSlappedListener),
-  SERVER_ERROR(String.class, ChatMessageListeners::getServerErrorListener),
-  STATUS_CHANGED(StatusUpdate.class, ChatMessageListeners::getPlayerStatusListener);
+  CHAT_EVENT(
+      MessageTypeListenerBinding.of(String.class, ChatMessageListeners::getChatEventListener)),
+  CHAT_MESSAGE(
+      MessageTypeListenerBinding.of(
+          ChatMessage.class, ChatMessageListeners::getChatMessageListener)),
+  PLAYER_JOINED(
+      MessageTypeListenerBinding.of(
+          ChatParticipant.class, ChatMessageListeners::getPlayerJoinedListener)),
+  PLAYER_LEFT(
+      MessageTypeListenerBinding.of(PlayerName.class, ChatMessageListeners::getPlayerLeftListener)),
+  PLAYER_LISTING(
+      MessageTypeListenerBinding.of(ChatterList.class, ChatMessageListeners::getConnectedListener)),
+  PLAYER_SLAPPED(
+      MessageTypeListenerBinding.of(
+          PlayerSlapped.class, ChatMessageListeners::getPlayerSlappedListener)),
+  SERVER_ERROR(
+      MessageTypeListenerBinding.of(String.class, ChatMessageListeners::getServerErrorListener)),
+  STATUS_CHANGED(
+      MessageTypeListenerBinding.of(
+          StatusUpdate.class, ChatMessageListeners::getPlayerStatusListener));
 
-  private final Class<?> classType;
-  private final Function<ChatMessageListeners, Consumer<?>> listenerMethod;
+  private final MessageTypeListenerBinding<ChatMessageListeners, ?> messageTypeListenerBinding;
 }
--- http-clients/src/main/java/org/triplea/http/client/remote/actions/messages/server/ServerRemoteActionMessageType.java
+++ http-clients/src/main/java/org/triplea/http/client/remote/actions/messages/server/ServerRemoteActionMessageType.java
@@ -1,25 +1,23 @@
 package org.triplea.http.client.remote.actions.messages.server;
 
 import java.net.InetAddress;
-import java.util.function.Consumer;
-import java.util.function.Function;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.triplea.http.client.remote.actions.RemoteActionListeners;
+import org.triplea.http.client.web.socket.messages.MessageTypeListenerBinding;
 import org.triplea.http.client.web.socket.messages.WebsocketMessageType;
 
 /** Types of messages that can be sent from server to client indicating a 'remote action' */
-@AllArgsConstructor
 @Getter(onMethod_ = @Override)
-@SuppressWarnings("ImmutableEnumChecker")
+@AllArgsConstructor
 public enum ServerRemoteActionMessageType implements WebsocketMessageType<RemoteActionListeners> {
   /** Requests that the server receiving this message to disconnect and shutdown. */
-  SHUTDOWN(String.class, RemoteActionListeners::getShutdownListener),
+  SHUTDOWN(MessageTypeListenerBinding.of(String.class, RemoteActionListeners::getShutdownListener)),
 
   /** Indicates a player has been banned and they should be disconnected if present. */
-  PLAYER_BANNED(InetAddress.class, RemoteActionListeners::getBannedPlayerListener);
+  PLAYER_BANNED(
+      MessageTypeListenerBinding.of(
+          InetAddress.class, RemoteActionListeners::getBannedPlayerListener));
 
-  private final Class<?> classType;
-
-  private final Function<RemoteActionListeners, Consumer<?>> listenerMethod;
+  private final MessageTypeListenerBinding<RemoteActionListeners, ?> messageTypeListenerBinding;
 }
