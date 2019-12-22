85bd953efa6a1aee7bf5a169ff6eaf4f07f183a6
DanVanAtta <dhvatta@gmail.com>
2019-12-22 14:59:57 -0800

    Replace enum static constructor method with static import
    - Rename static constructor method 'of' to 'newBinding', then use static import
    - Use @AllArgsConstructor+@Getter to replace @Value annotation and use a boilerplate
      static constructor. Using '@Value' with the updated static constructor name causes
      a lombok conflict and the project fails to compile.
    - Update enum values to end with a comma
    - Replace '@Immutable' and '@Suppresswarning' and comment with a suppresswarning
      on enums.

--- http-clients/src/main/java/org/triplea/http/client/remote/actions/messages/server/ServerRemoteActionMessageType.java
+++ http-clients/src/main/java/org/triplea/http/client/remote/actions/messages/server/ServerRemoteActionMessageType.java
@@ -1,5 +1,7 @@
 package org.triplea.http.client.remote.actions.messages.server;
 
+import static org.triplea.http.client.web.socket.messages.MessageTypeListenerBinding.newBinding;
+
 import java.net.InetAddress;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -10,14 +12,14 @@ import org.triplea.http.client.web.socket.messages.WebsocketMessageType;
 /** Types of messages that can be sent from server to client indicating a 'remote action' */
 @Getter(onMethod_ = @Override)
 @AllArgsConstructor
+@SuppressWarnings("ImmutableEnumChecker")
 public enum ServerRemoteActionMessageType implements WebsocketMessageType<RemoteActionListeners> {
   /** Requests that the server receiving this message to disconnect and shutdown. */
-  SHUTDOWN(MessageTypeListenerBinding.of(String.class, RemoteActionListeners::getShutdownListener)),
+  SHUTDOWN(newBinding(String.class, RemoteActionListeners::getShutdownListener)),
 
   /** Indicates a player has been banned and they should be disconnected if present. */
-  PLAYER_BANNED(
-      MessageTypeListenerBinding.of(
-          InetAddress.class, RemoteActionListeners::getBannedPlayerListener));
+  PLAYER_BANNED(newBinding(InetAddress.class, RemoteActionListeners::getBannedPlayerListener)),
+  ;
 
   private final MessageTypeListenerBinding<RemoteActionListeners, ?> messageTypeListenerBinding;
 }
