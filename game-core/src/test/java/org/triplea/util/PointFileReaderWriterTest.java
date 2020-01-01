df0e2ec5895bbea9c6277408c6d541ba37ab84a7
DanVanAtta <dhvatta@gmail.com>
2020-01-01 11:39:05 -0800

    Restore "ImmutableMap#of" usage with comment on iteration order guarantee

--- game-core/src/test/java/org/triplea/util/PointFileReaderWriterTest.java
+++ game-core/src/test/java/org/triplea/util/PointFileReaderWriterTest.java
@@ -4,7 +4,6 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.aMapWithSize;
 import static org.hamcrest.Matchers.hasKey;
 import static org.hamcrest.Matchers.is;
-import static org.hamcrest.core.StringContains.containsString;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -490,6 +489,7 @@ final class PointFileReaderWriterTest {
     @Test
     void shouldWriteOnePointPerName() throws Exception {
       final Map<String, Point> pointsByName =
+          // ImmutableMap#of guarantees iteration ordering
           ImmutableMap.of(
               "United Kingdom", new Point(1011, 1021),
               "Germany", new Point(2011, 2021),
@@ -498,9 +498,13 @@ final class PointFileReaderWriterTest {
       final String content =
           writeToString(os -> PointFileReaderWriter.writeOneToOne(os, pointsByName));
 
-      assertThat(content, containsString("United Kingdom  (1011,1021)"));
-      assertThat(content, containsString("Germany  (2011,2021)"));
-      assertThat(content, containsString("Eastern United States  (3011,3021)"));
+      assertThat(
+          content,
+          is(
+              ""
+                  + "United Kingdom  (1011,1021) \r\n"
+                  + "Germany  (2011,2021) \r\n"
+                  + "Eastern United States  (3011,3021) "));
     }
   }
 
@@ -518,7 +522,8 @@ final class PointFileReaderWriterTest {
     @Test
     void shouldWriteMultiplePointsPerName() throws Exception {
       final Map<String, List<Point>> pointListsByName =
-          Map.of(
+          // ImmutableMap#of guarantees iteration ordering
+          ImmutableMap.of(
               "Belarus",
                   List.of(new Point(1011, 1021), new Point(1012, 1022), new Point(1013, 1023)),
               "54 Sea Zone", List.of(new Point(2011, 2021), new Point(2012, 2022)),
@@ -527,9 +532,13 @@ final class PointFileReaderWriterTest {
       final String content =
           writeToString(os -> PointFileReaderWriter.writeOneToMany(os, pointListsByName));
 
-      assertThat(content, containsString("Belarus  (1011,1021)  (1012,1022)  (1013,1023)"));
-      assertThat(content, containsString("54 Sea Zone  (2011,2021)  (2012,2022)"));
-      assertThat(content, containsString("Philippines  (3011,3021) "));
+      assertThat(
+          content,
+          is(
+              ""
+                  + "Belarus  (1011,1021)  (1012,1022)  (1013,1023) \r\n"
+                  + "54 Sea Zone  (2011,2021)  (2012,2022) \r\n"
+                  + "Philippines  (3011,3021) "));
     }
   }
 
@@ -547,7 +556,8 @@ final class PointFileReaderWriterTest {
     @Test
     void shouldWriteMultiplePlacementsPerName() throws Exception {
       final Map<String, Tuple<List<Point>, Boolean>> polygonListsByName =
-          Map.of(
+          // ImmutableMap#of guarantees iteration ordering
+          ImmutableMap.of(
               "Belarus",
                   Tuple.of(
                       List.of(new Point(1011, 1021), new Point(1012, 1022), new Point(1013, 1023)),
@@ -567,15 +577,11 @@ final class PointFileReaderWriterTest {
 
       assertThat(
           content,
-          containsString("Belarus  (1011,1021)  (1012,1022)  (1013,1023)  | overflowToLeft=true"));
-      assertThat(
-          content,
-          containsString(
-              "54 Sea Zone  (2011,2021)  (2012,2022)  (2013,2023)  | overflowToLeft=false"));
-      assertThat(
-          content,
-          containsString(
-              "Philippines  (3011,3021)  (3012,3022)  (3013,3023)  | overflowToLeft=true"));
+          is(
+              ""
+                  + "Belarus  (1011,1021)  (1012,1022)  (1013,1023)  | overflowToLeft=true\r\n"
+                  + "54 Sea Zone  (2011,2021)  (2012,2022)  (2013,2023)  | overflowToLeft=false\r\n"
+                  + "Philippines  (3011,3021)  (3012,3022)  (3013,3023)  | overflowToLeft=true"));
     }
   }
 
@@ -593,7 +599,8 @@ final class PointFileReaderWriterTest {
     @Test
     void shouldWriteMultiplePolygonsPerName() throws Exception {
       final Map<String, List<Polygon>> polygonListsByName =
-          Map.of(
+          // ImmutableMap#of guarantees iteration ordering
+          ImmutableMap.of(
               "Belarus",
               List.of(polygon(new Point(1011, 1021), new Point(1012, 1022), new Point(1013, 1023))),
               "54 Sea Zone",
@@ -609,17 +616,14 @@ final class PointFileReaderWriterTest {
       final String content =
           writeToString(os -> PointFileReaderWriter.writeOneToManyPolygons(os, polygonListsByName));
 
-      assertThat(content, containsString("Belarus  <  (1011,1021)  (1012,1022)  (1013,1023)  > "));
-
       assertThat(
           content,
-          containsString(
-              "54 Sea Zone  <  (2011,2021)  (2012,2022)  (2013,2023)  > "
-                  + " <  (2111,2121)  (2112,2122)  (2113,2123)  > "));
-      assertThat(
-          content,
-          containsString(
-              "Philippines  <  (3011,3021)  (3012,3022)  (3013,3023)  > "
+          is(
+              ""
+                  + "Belarus  <  (1011,1021)  (1012,1022)  (1013,1023)  > \r\n"
+                  + "54 Sea Zone  <  (2011,2021)  (2012,2022)  (2013,2023)  > "
+                  + " <  (2111,2121)  (2112,2122)  (2113,2123)  > \r\n"
+                  + "Philippines  <  (3011,3021)  (3012,3022)  (3013,3023)  > "
                   + " <  (3111,3121)  (3112,3122)  >  <  (3211,3221)  > "));
     }
 
