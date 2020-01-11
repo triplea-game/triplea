8c13031060442ac6105312eb0ea734e21f8ae5e5
Ron <ron.murhammer@gmail.com>
2020-01-11 11:27:17 -0600

    AI fix NPE by fixing calc initialization (#5838)

--- game-core/src/main/java/games/strategy/triplea/ai/fast/FastAi.java
+++ game-core/src/main/java/games/strategy/triplea/ai/fast/FastAi.java
@@ -3,22 +3,17 @@ package games.strategy.triplea.ai.fast;
 import games.strategy.engine.framework.startup.ui.PlayerType;
 import games.strategy.triplea.ai.pro.ProAi;
 import games.strategy.triplea.ai.pro.util.ProOddsCalculator;
-import games.strategy.triplea.odds.calculator.IBattleCalculator;
 
 /** Fast AI. */
 public class FastAi extends ProAi {
 
-  // Odds estimator
-  private final IBattleCalculator estimator;
-
   public FastAi(final String name) {
     super(name);
-    estimator = new FastOddsEstimator(getProData());
   }
 
   @Override
   protected void initializeCalc() {
-    calc = new ProOddsCalculator(estimator);
+    calc = new ProOddsCalculator(new FastOddsEstimator(getProData()));
   }
 
   @Override
