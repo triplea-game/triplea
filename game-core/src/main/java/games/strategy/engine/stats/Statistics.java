0c51090b5bc95020db98d9314723e71bfc89d187
tripleabuilderbot <tripleabuilderbot@gmail.com>
2020-01-22 01:14:27 +0000

    Auto-Formatting

--- game-core/src/main/java/games/strategy/engine/stats/Statistics.java
+++ game-core/src/main/java/games/strategy/engine/stats/Statistics.java
@@ -5,7 +5,7 @@ import lombok.Data;
 /**
  * Data class class that holds statistics information like "PUs over time per player".
  *
- * Instances are typically created by {@link StatisticsAggregator}.
+ * <p>Instances are typically created by {@link StatisticsAggregator}.
  */
 @Data
 public class Statistics {}
