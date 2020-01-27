8f666e147b6cbcd4edacc4a146aa6516119d5faf
tripleabuilderbot <tripleabuilderbot@gmail.com>
2020-01-27 16:19:55 +0000

    Auto-Formatting

--- game-core/src/main/java/games/strategy/triplea/ui/HeadedUiContext.java
+++ game-core/src/main/java/games/strategy/triplea/ui/HeadedUiContext.java
@@ -56,7 +56,7 @@ public class HeadedUiContext extends AbstractUiContext {
 
   @Override
   protected void internalSetMapDir(final String dir, final GameData data) {
-    if(resourceLoader != null) {
+    if (resourceLoader != null) {
       resourceLoader.close();
     }
     resourceLoader = ResourceLoader.getMapResourceLoader(dir);
