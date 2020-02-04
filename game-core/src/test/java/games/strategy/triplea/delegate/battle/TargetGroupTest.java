a0ed40f5e41b0a4cd71bd94f3cf76d5916c2ae7c
Ron <ron.murhammer@gmail.com>
2020-02-03 20:50:02 -0600

    Refactor unit tests

--- game-core/src/test/java/games/strategy/triplea/delegate/battle/TargetGroupTest.java
+++ game-core/src/test/java/games/strategy/triplea/delegate/battle/TargetGroupTest.java
@@ -1,6 +1,8 @@
 package games.strategy.triplea.delegate.battle;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+import static org.hamcrest.collection.IsEmptyCollection.empty;
 
 import games.strategy.engine.data.GameData;
 import games.strategy.engine.data.GamePlayer;
@@ -9,6 +11,7 @@ import games.strategy.triplea.delegate.AbstractDelegateTestCase;
 import games.strategy.triplea.delegate.GameDataTestUtil;
 import games.strategy.triplea.xml.TestMapGameData;
 import java.util.List;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class TargetGroupTest extends AbstractDelegateTestCase {
@@ -16,38 +19,43 @@ class TargetGroupTest extends AbstractDelegateTestCase {
   final GameData twwGameData = TestMapGameData.TWW.getGameData();
 
   @Test
+  @DisplayName("Verify no target groups if no units and no enemy units")
   void testNewTargetGroupsWithEmptyUnitsAndEnemyUnits() {
     final List<TargetGroup> result = TargetGroup.newTargetGroups(List.of(), List.of());
-    assertEquals(0, result.size());
+    assertThat(result, empty());
   }
 
   @Test
+  @DisplayName("Verify no target groups if no units but some enemy units")
   void testNewTargetGroupsWithEmptyUnits() {
     final GamePlayer germany = GameDataTestUtil.germany(twwGameData);
     final List<Unit> enemyUnits = GameDataTestUtil.germanInfantry(twwGameData).create(1, germany);
     final List<TargetGroup> result = TargetGroup.newTargetGroups(List.of(), enemyUnits);
-    assertEquals(0, result.size());
+    assertThat(result, empty());
   }
 
   @Test
+  @DisplayName("Verify no target groups if some units but no enemy units")
   void testNewTargetGroupsWithEmptyEnemyUnits() {
     final GamePlayer germany = GameDataTestUtil.germany(twwGameData);
     final List<Unit> units = GameDataTestUtil.germanInfantry(twwGameData).create(1, germany);
     final List<TargetGroup> result = TargetGroup.newTargetGroups(units, List.of());
-    assertEquals(0, result.size());
+    assertThat(result, empty());
   }
 
   @Test
+  @DisplayName("Verify 1 target group if an infantry attacks an infantry")
   void testNewTargetGroupsForOneUnitAndOneEnemyUnit() {
     final GamePlayer germany = GameDataTestUtil.germany(twwGameData);
     final List<Unit> units = GameDataTestUtil.germanInfantry(twwGameData).create(1, germany);
     final GamePlayer britain = GameDataTestUtil.britain(twwGameData);
     final List<Unit> enemyUnits = GameDataTestUtil.britishInfantry(twwGameData).create(1, britain);
     final List<TargetGroup> result = TargetGroup.newTargetGroups(units, enemyUnits);
-    assertEquals(1, result.size());
+    assertThat(result, hasSize(1));
   }
 
   @Test
+  @DisplayName("Verify 1 target group if an infantry attacks infantry and artilleries")
   void testNewTargetGroupsForOneUnitAndMultipleEnemyUnits() {
     final GamePlayer germany = GameDataTestUtil.germany(twwGameData);
     final List<Unit> units = GameDataTestUtil.germanInfantry(twwGameData).create(1, germany);
@@ -55,10 +63,11 @@ class TargetGroupTest extends AbstractDelegateTestCase {
     final List<Unit> enemyUnits = GameDataTestUtil.britishInfantry(twwGameData).create(1, britain);
     enemyUnits.addAll(GameDataTestUtil.britishArtillery(twwGameData).create(2, britain));
     final List<TargetGroup> result = TargetGroup.newTargetGroups(units, enemyUnits);
-    assertEquals(1, result.size());
+    assertThat(result, hasSize(1));
   }
 
   @Test
+  @DisplayName("Verify 1 target group if an infantry and artilleries attack an infantry")
   void testNewTargetGroupsForMultipleUnitsAndOneEnemyUnit() {
     final GamePlayer germany = GameDataTestUtil.germany(twwGameData);
     final List<Unit> units = GameDataTestUtil.germanInfantry(twwGameData).create(1, germany);
@@ -66,10 +75,12 @@ class TargetGroupTest extends AbstractDelegateTestCase {
     final GamePlayer britain = GameDataTestUtil.britain(twwGameData);
     final List<Unit> enemyUnits = GameDataTestUtil.britishInfantry(twwGameData).create(1, britain);
     final List<TargetGroup> result = TargetGroup.newTargetGroups(units, enemyUnits);
-    assertEquals(1, result.size());
+    assertThat(result, hasSize(1));
   }
 
   @Test
+  @DisplayName(
+      "Verify 1 target group if an infantry and artilleries attack infantry and artilleries")
   void testNewTargetGroupsForMultipleUnitsAndMultipleEnemyUnits() {
     final GamePlayer germany = GameDataTestUtil.germany(twwGameData);
     final List<Unit> units = GameDataTestUtil.germanInfantry(twwGameData).create(1, germany);
@@ -78,10 +89,12 @@ class TargetGroupTest extends AbstractDelegateTestCase {
     final List<Unit> enemyUnits = GameDataTestUtil.britishInfantry(twwGameData).create(1, britain);
     enemyUnits.addAll(GameDataTestUtil.britishArtillery(twwGameData).create(2, britain));
     final List<TargetGroup> result = TargetGroup.newTargetGroups(units, enemyUnits);
-    assertEquals(1, result.size());
+    assertThat(result, hasSize(1));
   }
 
   @Test
+  @DisplayName(
+      "Verify 2 target groups if sub and fighters attack sub and fighters as subs can only target subs")
   void testNewTargetGroupsForMultipleGroups() {
     final GamePlayer germany = GameDataTestUtil.germany(twwGameData);
     final List<Unit> units = GameDataTestUtil.germanSubmarine(twwGameData).create(1, germany);
@@ -90,6 +103,6 @@ class TargetGroupTest extends AbstractDelegateTestCase {
     final List<Unit> enemyUnits = GameDataTestUtil.britishSubmarine(twwGameData).create(1, britain);
     enemyUnits.addAll(GameDataTestUtil.britishFighter(twwGameData).create(2, britain));
     final List<TargetGroup> result = TargetGroup.newTargetGroups(units, enemyUnits);
-    assertEquals(2, result.size());
+    assertThat(result, hasSize(2));
   }
 }
