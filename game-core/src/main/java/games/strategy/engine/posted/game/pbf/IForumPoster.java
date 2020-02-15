a2b2055d847d35acab20b98b793ad467fcf40259
DanVanAtta <dhvatta@gmail.com>
2020-02-15 12:52:30 -0800

    Add TODO to merge IForumPoster constants to another class

--- game-core/src/main/java/games/strategy/engine/posted/game/pbf/IForumPoster.java
+++ game-core/src/main/java/games/strategy/engine/posted/game/pbf/IForumPoster.java
@@ -4,6 +4,7 @@ package games.strategy.engine.posted.game.pbf;
  * An interface for classes that can post a turn summary, the summary may also include a save game
  * if the implementing class supports this.
  */
+// TODO: merge these constants into another class, perhaps GameProperties or NodeBbForumPoster
 public interface IForumPoster {
   String NAME = "FORUM_POSTER_NAME";
   String TOPIC_ID = "FORUM_POSTER_TOPIC_ID";
