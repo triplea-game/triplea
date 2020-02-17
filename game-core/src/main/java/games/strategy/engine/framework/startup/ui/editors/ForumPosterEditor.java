9fdb7ac8e966fb8089368bd53d9fb4d680be4d5e
DanVanAtta <dhvatta@gmail.com>
2020-02-17 13:01:15 -0800

    Apply view-model pattern to PBEMEditor

--- game-core/src/main/java/games/strategy/engine/framework/startup/ui/editors/ForumPosterEditor.java
+++ game-core/src/main/java/games/strategy/engine/framework/startup/ui/editors/ForumPosterEditor.java
@@ -1,54 +1,35 @@
 package games.strategy.engine.framework.startup.ui.editors;
 
-import com.google.common.annotations.VisibleForTesting;
-import com.google.common.base.Preconditions;
-import games.strategy.engine.ClientContext;
-import games.strategy.engine.data.properties.GameProperties;
-import games.strategy.engine.framework.GameRunner;
-import games.strategy.engine.posted.game.pbf.IForumPoster;
-import games.strategy.engine.posted.game.pbf.NodeBbForumPoster;
-import java.awt.Graphics;
 import java.awt.GridBagConstraints;
 import java.awt.Insets;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
-import java.util.logging.Level;
-import javax.imageio.ImageIO;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
 import javax.swing.JTextField;
-import javax.swing.SwingUtilities;
-import lombok.extern.java.Log;
-import org.triplea.java.TimeManager;
+import org.triplea.java.ViewModelListener;
 import org.triplea.swing.DocumentListenerBuilder;
-import org.triplea.swing.ProgressWindow;
 
 /** A class for selecting which Forum poster to use. */
-@Log
-public class ForumPosterEditor extends EditorPanel {
+class ForumPosterEditor extends EditorPanel
+    implements ViewModelListener<ForumPosterEditorViewModel> {
   private static final long serialVersionUID = -6069315084412575053L;
   private final JButton viewPosts = new JButton("View Forum");
   private final JButton testForum = new JButton("Test Post");
   private final JTextField topicIdField = new JTextField();
   private final JLabel topicIdLabel = new JLabel("Topic Id:");
   private final JLabel forumLabel = new JLabel("Forums:");
-  private final JCheckBox includeSaveGame = new JCheckBox("Attach save game to summary");
+  private final JCheckBox attachSaveGameToSummary = new JCheckBox("Attach save game to summary");
   private final JCheckBox alsoPostAfterCombatMove = new JCheckBox("Also Post After Combat Move");
   private final JComboBox<String> forums = new JComboBox<>();
-  private final Runnable readyCallback;
 
-  public ForumPosterEditor(final Runnable readyCallback) {
-    this.readyCallback = readyCallback;
+  ForumPosterEditor(final ForumPosterEditorViewModel viewModel) {
+    viewModel.setView(this);
+
     final int bottomSpace = 1;
     final int labelSpace = 2;
     int row = 0;
-    NodeBbForumPoster.availablePosters().forEach(forums::addItem);
+
     add(
         forumLabel,
         new GridBagConstraints(
@@ -63,6 +44,10 @@ public class ForumPosterEditor extends EditorPanel {
             new Insets(0, 0, bottomSpace, labelSpace),
             0,
             0));
+
+    viewModel.getForumSelectionOptions().forEach(forums::addItem);
+    forums.addActionListener(e -> viewModel.setForumSelection((String) forums.getSelectedItem()));
+    forums.setSelectedItem(viewModel.getForumSelection());
     add(
         forums,
         new GridBagConstraints(
@@ -77,6 +62,7 @@ public class ForumPosterEditor extends EditorPanel {
             new Insets(0, 0, bottomSpace, 0),
             0,
             0));
+
     row++;
     add(
         topicIdLabel,
@@ -92,6 +78,9 @@ public class ForumPosterEditor extends EditorPanel {
             new Insets(0, 0, bottomSpace, labelSpace),
             0,
             0));
+
+    DocumentListenerBuilder.attachDocumentListener(
+        topicIdField, () -> viewModel.setTopicId(topicIdField.getText().trim()));
     add(
         topicIdField,
         new GridBagConstraints(
@@ -106,6 +95,8 @@ public class ForumPosterEditor extends EditorPanel {
             new Insets(0, 0, bottomSpace, 0),
             0,
             0));
+
+    viewPosts.addActionListener(e -> viewModel.viewForumButtonClicked());
     add(
         viewPosts,
         new GridBagConstraints(
@@ -136,8 +127,11 @@ public class ForumPosterEditor extends EditorPanel {
             0,
             0));
     row++;
+
+    attachSaveGameToSummary.addChangeListener(
+        e -> viewModel.setAttachSaveGameToSummary(attachSaveGameToSummary.isSelected()));
     add(
-        includeSaveGame,
+        attachSaveGameToSummary,
         new GridBagConstraints(
             0,
             row,
@@ -150,6 +144,8 @@ public class ForumPosterEditor extends EditorPanel {
             new Insets(0, 0, 0, 0),
             0,
             0));
+
+    testForum.addActionListener(e -> viewModel.testPostButtonClicked());
     add(
         testForum,
         new GridBagConstraints(
@@ -165,6 +161,8 @@ public class ForumPosterEditor extends EditorPanel {
             0,
             0));
     row++;
+    alsoPostAfterCombatMove.addActionListener(
+        e -> viewModel.setAlsoPostAfterCombatMove(alsoPostAfterCombatMove.isSelected()));
     add(
         alsoPostAfterCombatMove,
         new GridBagConstraints(
@@ -179,118 +177,19 @@ public class ForumPosterEditor extends EditorPanel {
             new Insets(0, 0, 0, 0),
             0,
             0));
-    setupListeners();
   }
 
-  private void checkFieldsAndNotify() {
-    areFieldsValid();
-    readyCallback.run();
-  }
+  @Override
+  public void viewModelChanged(final ForumPosterEditorViewModel forumPosterEditorViewModel) {
+    forums.setSelectedItem(forumPosterEditorViewModel.getForumSelection());
+    topicIdField.setText(forumPosterEditorViewModel.getTopicId());
+    alsoPostAfterCombatMove.setSelected(forumPosterEditorViewModel.isAlsoPostAfterCombatMove());
+    attachSaveGameToSummary.setSelected(forumPosterEditorViewModel.isAttachSaveGameToSummary());
 
-  /** Configures the listeners for the gui components. */
-  private void setupListeners() {
-    viewPosts.addActionListener(e -> newForumPoster().viewPosted());
-    testForum.addActionListener(e -> testForum());
-    forums.addItemListener(e -> checkFieldsAndNotify());
-    DocumentListenerBuilder.attachDocumentListener(topicIdField, this::checkFieldsAndNotify);
-  }
-
-  /** Tests the Forum poster. */
-  private void testForum() {
-    final NodeBbForumPoster poster = newForumPoster();
-    final ProgressWindow progressWindow =
-        new ProgressWindow(JOptionPane.getFrameForComponent(this), poster.getTestMessage());
-    progressWindow.setVisible(true);
-
-    new Thread(
-            () -> {
-              File f = null;
-              try {
-                f = File.createTempFile("123", ".jpg");
-                f.deleteOnExit();
-                final BufferedImage image = new BufferedImage(130, 40, BufferedImage.TYPE_INT_RGB);
-                final Graphics g = image.getGraphics();
-                g.drawString("Testing file upload", 10, 20);
-                ImageIO.write(image, "jpg", f);
-              } catch (final IOException e) {
-                // ignore
-              }
-              final CompletableFuture<String> future =
-                  poster.postTurnSummary(
-                      "Test summary from TripleA, engine version: "
-                          + ClientContext.engineVersion()
-                          + ", time: "
-                          + TimeManager.getLocalizedTime(),
-                      "Testing Forum poster",
-                      f != null ? f.toPath() : null);
-              progressWindow.setVisible(false);
-              try {
-                // now that we have a result, marshall it back unto the swing thread
-                future
-                    .thenAccept(
-                        message ->
-                            SwingUtilities.invokeLater(
-                                () ->
-                                    GameRunner.showMessageDialog(
-                                        message,
-                                        GameRunner.Title.of("Test Turn Summary Post"),
-                                        JOptionPane.INFORMATION_MESSAGE)))
-                    .exceptionally(
-                        throwable -> {
-                          SwingUtilities.invokeLater(
-                              () ->
-                                  GameRunner.showMessageDialog(
-                                      throwable.getMessage(),
-                                      GameRunner.Title.of("Test Turn Summary Post"),
-                                      JOptionPane.INFORMATION_MESSAGE));
-                          return null;
-                        })
-                    .get();
-              } catch (final InterruptedException e) {
-                Thread.currentThread().interrupt();
-              } catch (final ExecutionException e) {
-                log.log(Level.SEVERE, "Error while retrieving post", e);
-              }
-            })
-        .start();
-  }
-
-  /** Checks if all fields are filled out correctly and indicates an error otherwise. */
-  public boolean areFieldsValid() {
-    final boolean setupValid =
-        NodeBbForumPoster.isClientSettingSetupValidForServer((String) forums.getSelectedItem());
-    final boolean idValid = setLabelValid(isInt(topicIdField.getText()), topicIdLabel);
-    final boolean forumValid = setLabelValid(forums.getSelectedItem() != null, forumLabel);
-    final boolean allValid = setupValid && idValid && forumValid;
-    viewPosts.setEnabled(allValid);
-    testForum.setEnabled(allValid);
-    return allValid;
-  }
-
-  @VisibleForTesting
-  static boolean isInt(final String string) {
-    Preconditions.checkNotNull(string);
-    return string.matches("^-?\\d+$");
-  }
-
-  private NodeBbForumPoster newForumPoster() {
-    final String forumName = (String) forums.getSelectedItem();
-    Preconditions.checkNotNull(forumName);
-    return NodeBbForumPoster.newInstanceByName(forumName, Integer.parseInt(topicIdField.getText()));
-  }
-
-  public void applyToGameProperties(final GameProperties properties) {
-    properties.set(IForumPoster.NAME, forums.getSelectedItem());
-    properties.set(IForumPoster.TOPIC_ID, Integer.parseInt(topicIdField.getText()));
-    properties.set(IForumPoster.POST_AFTER_COMBAT, alsoPostAfterCombatMove.isSelected());
-    properties.set(IForumPoster.INCLUDE_SAVEGAME, includeSaveGame.isSelected());
-  }
-
-  public void populateFromGameProperties(final GameProperties properties) {
-    forums.setSelectedItem(properties.get(IForumPoster.NAME));
-    final Object topicId = properties.get(IForumPoster.TOPIC_ID);
-    topicIdField.setText(topicId == null ? "" : String.valueOf(topicId));
-    alsoPostAfterCombatMove.setSelected(properties.get(IForumPoster.POST_AFTER_COMBAT, false));
-    includeSaveGame.setSelected(properties.get(IForumPoster.INCLUDE_SAVEGAME, true));
+    final boolean isValid = forumPosterEditorViewModel.areFieldsValid();
+    setLabelValid(isValid, topicIdLabel);
+    setLabelValid(isValid, forumLabel);
+    viewPosts.setEnabled(isValid);
+    testForum.setEnabled(isValid);
   }
 }
---
+++ game-core/src/main/java/games/strategy/engine/framework/startup/ui/editors/ForumPosterEditorViewModel.java
@@ -0,0 +1,169 @@
+package games.strategy.engine.framework.startup.ui.editors;
+
+import games.strategy.engine.data.properties.GameProperties;
+import games.strategy.engine.posted.game.pbf.IForumPoster;
+import games.strategy.engine.posted.game.pbf.NodeBbForumPoster;
+import java.util.Optional;
+import lombok.Getter;
+import lombok.Setter;
+import org.triplea.java.Postconditions;
+import org.triplea.java.ViewModelListener;
+
+class ForumPosterEditorViewModel {
+  private final Runnable readyCallback;
+
+  @Setter
+  private ViewModelListener<ForumPosterEditorViewModel> view;
+
+  @Setter
+  private String forumSelection = "";
+  @Setter
+  @Getter
+  private String topicId = "";
+  @Setter
+  @Getter
+  private boolean attachSaveGameToSummary;
+  @Setter
+  @Getter
+  private boolean alsoPostAfterCombatMove;
+
+  ForumPosterEditorViewModel(final Runnable readyCallback) {
+    this.readyCallback = readyCallback;
+  }
+
+  ForumPosterEditorViewModel(final Runnable readyCallback, final GameProperties properties) {
+    this.readyCallback = readyCallback;
+    this.forumSelection = (String) properties.get(IForumPoster.NAME);
+    this.topicId = properties.get(IForumPoster.TOPIC_ID, "");
+    this.alsoPostAfterCombatMove = properties.get(IForumPoster.POST_AFTER_COMBAT, false);
+    this.attachSaveGameToSummary = properties.get(IForumPoster.INCLUDE_SAVEGAME, false);
+  }
+
+  String getForumSelection() {
+    return Optional.ofNullable(forumSelection) //
+        .orElse(getForumSelectionOptions().iterator().next());
+  }
+
+  Iterable<String> getForumSelectionOptions() {
+    final Iterable<String> selections = NodeBbForumPoster.availablePosters();
+    Postconditions.assertState(selections.iterator().hasNext());
+    return selections;
+  }
+
+  boolean areFieldsValid() {
+
+    return false;
+  }
+
+  public void populateFromGameProperties(final GameProperties properties) {
+    this.forumSelection = (String) properties.get(IForumPoster.NAME);
+    this.topicId = properties.get(IForumPoster.TOPIC_ID, "");
+    this.alsoPostAfterCombatMove = properties.get(IForumPoster.POST_AFTER_COMBAT, false);
+    this.attachSaveGameToSummary = properties.get(IForumPoster.INCLUDE_SAVEGAME, true);
+    view.viewModelChanged(this);
+  }
+
+  void viewForumButtonClicked() {
+    NodeBbForumPoster.newInstanceByName(forumSelection, Integer.parseInt(topicId)).viewPosted();
+  }
+
+  void testPostButtonClicked() {
+    //    final NodeBbForumPoster poster = newForumPoster();
+    //    final ProgressWindow progressWindow =
+    //        new ProgressWindow(JOptionPane.getFrameForComponent(this), poster.getTestMessage());
+    //    progressWindow.setVisible(true);
+    //
+    //    new Thread(
+    //            () -> {
+    //              File f = null;
+    //              try {
+    //                f = File.createTempFile("123", ".jpg");
+    //                f.deleteOnExit();
+    //                final BufferedImage image = new BufferedImage(130, 40,
+    // BufferedImage.TYPE_INT_RGB);
+    //                final Graphics g = image.getGraphics();
+    //                g.drawString("Testing file upload", 10, 20);
+    //                ImageIO.write(image, "jpg", f);
+    //              } catch (final IOException e) {
+    //                // ignore
+    //              }
+    //              final CompletableFuture<String> future =
+    //                  poster.postTurnSummary(
+    //                      "Test summary from TripleA, engine version: "
+    //                          + ClientContext.engineVersion()
+    //                          + ", time: "
+    //                          + TimeManager.getLocalizedTime(),
+    //                      "Testing Forum poster",
+    //                      f != null ? f.toPath() : null);
+    //              progressWindow.setVisible(false);
+    //              try {
+    //                // now that we have a result, marshall it back unto the swing thread
+    //                future
+    //                    .thenAccept(
+    //                        message ->
+    //                            SwingUtilities.invokeLater(
+    //                                () ->
+    //                                    GameRunner.showMessageDialog(
+    //                                        message,
+    //                                        GameRunner.Title.of("Test Turn Summary Post"),
+    //                                        JOptionPane.INFORMATION_MESSAGE)))
+    //                    .exceptionally(
+    //                        throwable -> {
+    //                          SwingUtilities.invokeLater(
+    //                              () ->
+    //                                  GameRunner.showMessageDialog(
+    //                                      throwable.getMessage(),
+    //                                      GameRunner.Title.of("Test Turn Summary Post"),
+    //                                      JOptionPane.INFORMATION_MESSAGE));
+    //                          return null;
+    //                        })
+    //                    .get();
+    //              } catch (final InterruptedException e) {
+    //                Thread.currentThread().interrupt();
+    //              } catch (final ExecutionException e) {
+    //                log.log(Level.SEVERE, "Error while retrieving post", e);
+    //              }
+    //            })
+    //        .start();
+
+  }
+
+  //  private void checkFieldsAndNotify() {
+  //    areFieldsValid();
+  //    readyCallback.run();
+  //  }
+
+  //  /** Configures the listeners for the gui components. */
+  //  private void setupListeners() {
+  ////    viewPosts.addActionListener(e -> newForumPoster().viewPosted());
+  ////    testForum.addActionListener(e -> testForum());
+  ////    forums.addItemListener(e -> checkFieldsAndNotify());
+  ////    DocumentListenerBuilder.attachDocumentListener(topicIdField, this::checkFieldsAndNotify);
+  //  }
+
+  //  /** Checks if all fields are filled out correctly and indicates an error otherwise. */
+  //  public boolean areFieldsValid() {
+  //    final boolean setupValid =
+  //        NodeBbForumPoster.isClientSettingSetupValidForServer((String) forums.getSelectedItem());
+  //    final boolean idValid = setLabelValid(isInt(topicIdField.getText()), topicIdLabel);
+  //    final boolean forumValid = setLabelValid(forums.getSelectedItem() != null, forumLabel);
+  //    final boolean allValid = setupValid && idValid && forumValid;
+  //    viewPosts.setEnabled(allValid);
+  //    testForum.setEnabled(allValid);
+  //    return allValid;
+  //  }
+
+  //  @VisibleForTesting
+  //  static boolean isInt(final String string) {
+  //    Preconditions.checkNotNull(string);
+  //    return string.matches("^-?\\d+$");
+  //  }
+
+  //  private NodeBbForumPoster newForumPoster() {
+  //    final String forumName = (String) forums.getSelectedItem();
+  //    Preconditions.checkNotNull(forumName);
+  //    return NodeBbForumPoster.newInstanceByName(forumName,
+  // Integer.parseInt(topicIdField.getText()));
+  //  }
+
+}
