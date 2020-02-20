5be20202db5c81b421d23fee9c5240f503705175
RoiEX <8350879+RoiEXLab@users.noreply.github.com>
2020-02-20 15:57:36 +0100

    Simplify Test Strings & Rename Test Methods

--- game-core/src/test/java/games/strategy/engine/random/MartiDiceRollerTest.java
+++ game-core/src/test/java/games/strategy/engine/random/MartiDiceRollerTest.java
@@ -15,31 +15,10 @@ import org.junit.jupiter.api.Test;
 
 public class MartiDiceRollerTest {
   private static final String successMessage =
-      "<!DOCTYPE html>\n"
-          + "<html>\n"
-          + "\t<head>\n"
-          + "\t\t<title>M.A.R.T.I. Server -- \"more accurate rolls than irony\"</title>\n"
-          + "\t</head>\n"
-          + "\t<body>\n"
-          + "\t\t<p>Dice results were sent via email.</p>"
-          + "<br><a href='https://dice.marti.triplea-game.org/MARTI_verify.php?iv=oTVwKksuDf4f0LW4j"
-          + "6U6yAklQWsxpdac%2FmX1NMP7uXY%3D&enc=vhDa64l%2FORX6X8di6LVdl5JQGC81uQyKE4Jt0gHoVsOV5Uxz"
-          + "msbVReOsBQonZmQi7JZAwGhL4HoFXzkJMjilNX1gRMs5%2FhXyjyoUryYGPzgOWIVG7IxkuOMRiot7kPqJQwNc"
-          + "f08ev8TNj9WxZTSWPNy2j1Y17piXRn%2FffXIGPDDcrmw2rSeB3lCq6ZjlKSmMCIqEXk%2FSsJjQO7jKq7CkWT"
-          + "ZPY2QxTfFuOmFEj1Ji7pp1GVuVcc5g90q5'>Click here to verify the roll.</a>"
-          + "<br>your dice are: 2,5,1<p><p>\t</body>\n"
-          + "</html>";
+      "Some text before<br>your dice are: 2,5,1<p><p>\t</body>\nsome text after";
 
   private static final String failureMessage =
-      "<!DOCTYPE html>\n"
-          + "<html>\n"
-          + "\t<head>\n"
-          + "\t\t<title>M.A.R.T.I. Server -- \"more accurate rolls than irony\"</title>\n"
-          + "\t</head>\n"
-          + "\t<body>\n"
-          + "\t\tfatal error: Emails [user@mail.invalid] are not registered."
-          + " Please register them at https://dice.marti.triplea-game.org/register.php .!\n"
-          + "\n";
+      "some text before\t\tfatal error: Error description.!\nsome text after";
 
   private final MartiDiceRoller martiDiceRoller =
       MartiDiceRoller.builder()
@@ -51,7 +30,7 @@ public class MartiDiceRollerTest {
 
   @Test
   @SuppressWarnings("unchecked")
-  void successfulMessageExtraction() throws Exception {
+  void successfulMessageGetsExtractedCorrectly() throws Exception {
     final Integer[] dice =
         Arrays.stream(martiDiceRoller.getDice(successMessage, 0)).boxed().toArray(Integer[]::new);
 
@@ -59,15 +38,11 @@ public class MartiDiceRollerTest {
   }
 
   @Test
-  void unsuccessfulMessageExtraction() {
+  void unsuccessfulMessageGetsExtractedCorrectly() {
     final Exception exception =
         assertThrows(
             InvocationTargetException.class, () -> martiDiceRoller.getDice(failureMessage, 0));
-    assertThat(
-        exception.getMessage(),
-        is(
-            " Emails [user@mail.invalid] are not registered."
-                + " Please register them at https://dice.marti.triplea-game.org/register.php ."));
+    assertThat(exception.getMessage(), is(" Error description."));
     assertThat(exception.getCause(), is(nullValue()));
   }
 
