66eca58bd50f1a507d004ead8c1f43a0b9947070
DanVanAtta <dhvatta@gmail.com>
2020-03-01 17:40:57 -0800

    Fix password too short error message when creating an account
    Previous password validator assumed we were looking at a hashed value,
    now we are looking at a plaintext value. The server validation of
    length is still needed as there is no guarantee someone will use
    our game-client when creating accounts.
    
    This fix updates password validation on account creation to simply
    check that the password is a minimum length.

--- http-server/src/main/java/org/triplea/server/user/account/create/PasswordValidation.java
+++ http-server/src/main/java/org/triplea/server/user/account/create/PasswordValidation.java
@@ -8,12 +8,12 @@ import java.util.function.Function;
 /** We expect password to be sent to server as a hash. Simply check that it is not too short. */
 public class PasswordValidation implements Function<String, Optional<String>> {
   /** Expected MIN_LENGTH is length of a SHA512 hashed string. */
-  @VisibleForTesting static final int MIN_LENGTH = 128;
+  @VisibleForTesting static final int MIN_LENGTH = 5;
 
   @Override
   public Optional<String> apply(final String password) {
     return Strings.nullToEmpty(password).trim().length() < MIN_LENGTH
-        ? Optional.of("Password not encoded properly, too short")
+        ? Optional.of("Password too short")
         : Optional.empty();
   }
 }
