71f74e1bbb6097f4b8445ad20b1b9293b90b797e
RoiEX <8350879+RoiEXLab@users.noreply.github.com>
2020-03-02 16:18:06 +0100

    Fix EndPointTest

--- game-core/src/test/java/games/strategy/engine/message/unifiedmessenger/EndPointTest.java
+++ game-core/src/test/java/games/strategy/engine/message/unifiedmessenger/EndPointTest.java
@@ -2,23 +2,26 @@ package games.strategy.engine.message.unifiedmessenger;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import games.strategy.engine.message.RemoteActionCode;
 import games.strategy.engine.message.RemoteMethodCall;
 import games.strategy.engine.message.RemoteMethodCallResults;
-import java.util.Comparator;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class EndPointTest {
 
+  interface TestInterface {
+    @RemoteActionCode(0)
+    @SuppressWarnings("unused")
+    int dummy();
+  }
+
   @Test
   void testEndPoint() throws Exception {
-    final EndPoint endPoint = new EndPoint("", Comparator.class, false);
-    endPoint.addImplementor((Comparator<Object>) (o1, o2) -> 2);
+    final EndPoint endPoint = new EndPoint("", TestInterface.class, false);
+    endPoint.addImplementor((TestInterface) () -> 2);
     final RemoteMethodCall call =
-        new RemoteMethodCall(
-            "",
-            Comparator.class.getMethod("compare", Object.class, Object.class),
-            new Object[] {"", ""});
+        new RemoteMethodCall("", TestInterface.class.getMethod("dummy"), new Object[] {});
     final List<RemoteMethodCallResults> results =
         endPoint.invokeLocal(call, endPoint.takeANumber(), null);
     assertEquals(1, results.size());
