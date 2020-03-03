78a0aa735bdcff94c369ce3c98a2371be7467a2b
RoiEX <8350879+RoiEXLab@users.noreply.github.com>
2020-03-03 12:50:14 +0100

    Enhance javadoc

--- game-core/src/main/java/games/strategy/engine/message/RemoteActionCode.java
+++ game-core/src/main/java/games/strategy/engine/message/RemoteActionCode.java
@@ -6,11 +6,10 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Annotation that sets the "opcode" for any particular method that may be used over the network
- * using the {@link RemoteMethodCall} mechanism. The integer doesn't carry any significant meaning,
- * but in order to stay compatible over the network it may not be changed at all. Any methods
- * signature of methods annotated with this annotation may not be altered if it should continue to
- * be compatible.
+ * Annotation that defines a method number, methods are called by method number using the {@link
+ * RemoteMethodCall} mechanism}". In order to stay compatible over the network, annotated methods'
+ * signature should not be altered (including: changes in signature, or changes in the method
+ * number).
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
