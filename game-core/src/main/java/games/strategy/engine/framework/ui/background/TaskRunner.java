e42972525dda2fa0571ee9a32799adf913ab91f4
DanVanAtta <dhvatta@gmail.com>
2020-03-04 00:15:42 -0800

    Improve javadoc on new utility class: TaskRunner

--- game-core/src/main/java/games/strategy/engine/framework/ui/background/TaskRunner.java
+++ game-core/src/main/java/games/strategy/engine/framework/ui/background/TaskRunner.java
@@ -11,11 +11,38 @@ import lombok.Builder;
 import org.triplea.java.function.ThrowingRunnable;
 import org.triplea.swing.SwingComponents;
 
+/**
+ * Runs a background task with a wait dialog in the foreground. Similar to {@see
+ * BackgroundTaskRunner} except:
+ *
+ * <ul>
+ *   <li>Wait dialog can be closed.
+ *   <li>Does not throw exceptions, instead invokes an exception handler
+ * </ul>
+ *
+ * @param <E> Exception type that can be thrown by the background task.
+ */
 @Builder
 public class TaskRunner<E extends Exception> {
+  /**
+   * Error callback handler. The background task encounters an exception, the background task is
+   * cancelled and this is invoked with the exception that occurred.
+   */
   @Nonnull private final Consumer<E> exceptionHandler;
+  /**
+   * Title of the wait spinner window that is displayed to user while a background action is being
+   * executed.
+   */
   @Nonnull private final String waitDialogTitle;
 
+  /**
+   * Runs a task with a closeable wait spinner dialog in the foreground. THe wait spinner closes
+   * when the task completes. If the spinner dialog is closed, then the background task is
+   * cancelled. If there are any exceptions encountered, the spinner dialog is closed and the {@see
+   * exceptionHandler} {@code Consumer} property is called.
+   *
+   * @param backgroundAction The action to be executed.
+   */
   public void run(final ThrowingRunnable<E> backgroundAction) {
     Preconditions.checkNotNull(backgroundAction);
     final AtomicReference<Throwable> exceptionRef = new AtomicReference<>();
