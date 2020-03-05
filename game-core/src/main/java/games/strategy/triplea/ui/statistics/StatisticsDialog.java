99fc8364f8a840180c87db93d980f163dde41ef4
Simon Lenz <post@simon-lenz.de>
2020-03-05 23:37:45 +0100

    1660 - delete dummy graphs

--- game-core/src/main/java/games/strategy/triplea/ui/statistics/StatisticsDialog.java
+++ game-core/src/main/java/games/strategy/triplea/ui/statistics/StatisticsDialog.java
@@ -10,8 +10,6 @@ import java.util.List;
 import javax.swing.JPanel;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.knowm.xchart.PieChart;
-import org.knowm.xchart.PieChartBuilder;
 import org.knowm.xchart.XChartPanel;
 import org.knowm.xchart.XYChart;
 import org.knowm.xchart.XYChartBuilder;
@@ -46,8 +44,6 @@ public class StatisticsDialog extends JPanel {
                 "VC", "Victory Cities", statistics.getVictoryCitiesOfPlayerInRound()));
 
     final JTabbedPaneBuilder tabbedPane = JTabbedPaneBuilder.builder();
-    tabbedPane.addTab("Lines", createDummyXyGraph(statistics));
-    tabbedPane.addTab("Pie", createDummyPieChart(statistics));
     for (final OverTimeChart chartData : overTimeCharts) {
       tabbedPane.addTab(chartData.getTitle(), createChart(chartData));
     }
@@ -61,32 +57,6 @@ public class StatisticsDialog extends JPanel {
         .getData()
         .rowMap()
         .forEach((key, value) -> chart.addSeries(key, new ArrayList<>(value.values())));
-    return new XChartPanel<>(chart);
-  }
-
-  private JPanel createDummyXyGraph(final Statistics statistics) {
-    final XYChart chart = xyChartDefaults.title("Sample Chart: " + statistics.toString()).build();
-    chart.addSeries("some value1", new double[] {2.0, 0.0, 40.0});
-    chart.addSeries("some value2", new double[] {3.0, 1.0, 41.0});
-    chart.addSeries("some value3", new double[] {4.0, 2.0, 42.0});
-    chart.addSeries("some value4", new double[] {5.0, 3.0, 43.0});
-    chart.addSeries("some value5", new double[] {6.0, 4.0, 44.0});
-    chart.addSeries("some value6", new double[] {7.0, 5.0, 45.0});
-    chart.addSeries("some value7", new double[] {8.0, 6.0, 46.0});
-    return new XChartPanel<>(chart);
-  }
-
-  private JPanel createDummyPieChart(final Statistics statistics) {
-    final PieChart chart =
-        new PieChartBuilder()
-            .theme(Styler.ChartTheme.XChart)
-            .title("Sample Chart: " + statistics.toString())
-            .build();
-    chart.addSeries("Value 1", 27);
-    chart.addSeries("Value 2", 63);
-    chart.addSeries("Value 3", 1);
-    chart.addSeries("Value 4", 9);
-    chart.getStyler().setLegendPosition(Styler.LegendPosition.InsideSE);
     return new XChartPanel<>(chart);
   }
 }
