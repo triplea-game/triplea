0b711308ac5201435c0dab73e84acc4e8867e21e
Ron <ron.murhammer@gmail.com>
2020-03-31 20:17:34 -0500

    Add method javadoc

--- game-core/src/main/java/games/strategy/triplea/delegate/battle/MustFightBattle.java
+++ game-core/src/main/java/games/strategy/triplea/delegate/battle/MustFightBattle.java
@@ -260,6 +260,11 @@ public class MustFightBattle extends DependentBattle implements BattleStepString
     }
   }
 
+  /**
+   * Used by battle results to get the remaining attackers after the battle is completed. It
+   * includes remaining attackers, retreated attackers, and if attacker won/draw then any owned
+   * units left in the territory.
+   */
   @Override
   public List<Unit> getRemainingAttackingUnits() {
     final List<Unit> remaining = new ArrayList<>(attackingUnitsRetreated);
@@ -276,6 +281,11 @@ public class MustFightBattle extends DependentBattle implements BattleStepString
     return remaining;
   }
 
+  /**
+   * Used by battle results to get the remaining defenders after the battle is completed. It
+   * includes remaining defenders, retreated defenders, and if defender won/draw then any owned
+   * units and enemy units of the attacker left in the territory.
+   */
   @Override
   public List<Unit> getRemainingDefendingUnits() {
     final Set<Unit> remaining = new HashSet<>(defendingUnitsRetreated);
