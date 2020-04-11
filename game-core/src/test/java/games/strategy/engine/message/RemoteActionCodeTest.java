a0eacdde0c7cc3dd8d18649076094592c7995ec8
RoiEX <8350879+RoiEXLab@users.noreply.github.com>
2020-04-11 16:10:58 +0200

    Move logic to aggregator

--- game-core/src/test/java/games/strategy/engine/message/RemoteActionCodeTest.java
+++ game-core/src/test/java/games/strategy/engine/message/RemoteActionCodeTest.java
@@ -4,11 +4,13 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
+import java.lang.reflect.Method;
 import java.util.stream.IntStream;
 import org.junit.jupiter.api.extension.ParameterContext;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.aggregator.AggregateWith;
 import org.junit.jupiter.params.aggregator.ArgumentsAccessor;
+import org.junit.jupiter.params.aggregator.ArgumentsAggregationException;
 import org.junit.jupiter.params.aggregator.ArgumentsAggregator;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
@@ -17,12 +19,7 @@ public class RemoteActionCodeTest {
   @ParameterizedTest
   @CsvFileSource(resources = "/required-op-codes.txt")
   void verifyCorrectOpCode(
-      final Class<?> clazz,
-      final int opCode,
-      final String methodName,
-      @AggregateWith(ClassAggregator.class) final Class<?>[] parameterTypes)
-      throws Exception {
-    var method = clazz.getMethod(methodName, parameterTypes);
+      final int opCode, @AggregateWith(MethodAggregator.class) final Method method) {
     var remoteActionCode = method.getAnnotation(RemoteActionCode.class);
 
     assumeTrue(remoteActionCode != null);
@@ -30,12 +27,23 @@ public class RemoteActionCodeTest {
     assertThat(remoteActionCode.value(), is(opCode));
   }
 
-  static class ClassAggregator implements ArgumentsAggregator {
+  static class MethodAggregator implements ArgumentsAggregator {
     @Override
-    public Class<?>[] aggregateArguments(ArgumentsAccessor arguments, ParameterContext context) {
-      return IntStream.range(context.getIndex(), arguments.size())
-          .mapToObj(i -> arguments.get(i, Class.class))
-          .toArray(Class<?>[]::new);
+    public Method aggregateArguments(
+        final ArgumentsAccessor arguments, final ParameterContext context)
+        throws ArgumentsAggregationException {
+      final int offset = context.getIndex();
+      final Class<?> clazz = arguments.get(offset, Class.class);
+      final String methodName = arguments.getString(offset + 1);
+      final Class<?>[] args =
+          IntStream.range(offset + 2, arguments.size())
+              .mapToObj(i -> arguments.get(i, Class.class))
+              .toArray(Class<?>[]::new);
+      try {
+        return clazz.getMethod(methodName, args);
+      } catch (final NoSuchMethodException e) {
+        throw new ArgumentsAggregationException("Invalid method specified", e);
+      }
     }
   }
 }
