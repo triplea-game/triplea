4a6793874653be1f25af10586a5dfe91c981b331
DanVanAtta <dhvatta@gmail.com>
2020-04-13 18:12:30 -0700

    Move testing of KeyCode to KeyCombination

--- swing-lib/src/test/java/org/triplea/swing/key/binding/KeyCombinationTest.java
+++ swing-lib/src/test/java/org/triplea/swing/key/binding/KeyCombinationTest.java
@@ -6,10 +6,14 @@ import java.util.Arrays;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-class KeyCodeTest {
+class KeyCombinationTest {
+
   @Test
   @DisplayName("Verify all key codes can be translated to a awt KeyStroke object")
   void toKeyStroke() {
-    assertDoesNotThrow(() -> Arrays.stream(KeyCode.values()).forEach(KeyCode::toKeyStroke));
+    assertDoesNotThrow(
+        () ->
+            Arrays.stream(KeyCode.values())
+                .forEach(keyCode -> KeyCombination.of(keyCode, ButtonDownMask.NONE).toKeyStroke()));
   }
 }
