1556ea99cf490e282773b2169e84d818f6b087c8
DanVanAtta <dhvatta@gmail.com>
2020-04-13 18:19:15 -0700

    Update test to verify all permutations of key code and button down mask.

--- swing-lib/src/test/java/org/triplea/swing/key/binding/KeyCombinationTest.java
+++ swing-lib/src/test/java/org/triplea/swing/key/binding/KeyCombinationTest.java
@@ -2,18 +2,33 @@ package org.triplea.swing.key.binding;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class KeyCombinationTest {
 
-  @Test
-  @DisplayName("Verify all key codes can be translated to a awt KeyStroke object")
-  void toKeyStroke() {
-    assertDoesNotThrow(
-        () ->
-            Arrays.stream(KeyCode.values())
-                .forEach(keyCode -> KeyCombination.of(keyCode, ButtonDownMask.NONE).toKeyStroke()));
+  @ParameterizedTest
+  @MethodSource
+  @DisplayName(
+      "Verify all key code with button down mask combinations can be "
+          + "translated to an awt KeyStroke object")
+  void toKeyStroke(final ButtonDownMask buttonDownMask, final KeyCode keyCode) {
+    assertDoesNotThrow(() -> KeyCombination.of(keyCode, buttonDownMask).toKeyStroke());
+  }
+
+  /** Returns all tuple combination of all keycodes with button down mask. */
+  @SuppressWarnings("unused")
+  private static List<Arguments> toKeyStroke() {
+    final List<Arguments> arguments = new ArrayList<>();
+    for (final ButtonDownMask buttonDownMask : ButtonDownMask.values()) {
+      for (final KeyCode keyCode : KeyCode.values()) {
+        arguments.add(Arguments.of(buttonDownMask, keyCode));
+      }
+    }
+    return arguments;
   }
 }
