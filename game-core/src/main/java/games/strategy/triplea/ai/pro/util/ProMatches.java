7d7174db12592d50de5ef4c907f66edd4f47d139
DanVanAtta <dhvatta@gmail.com>
2020-04-19 12:43:12 -0700

    Fix correct negation

--- game-core/src/main/java/games/strategy/triplea/ai/pro/util/ProMatches.java
+++ game-core/src/main/java/games/strategy/triplea/ai/pro/util/ProMatches.java
@@ -560,14 +560,14 @@ public final class ProMatches {
       final GamePlayer player, final UnitType unitType) {
     return Matches.unitIsOwnedBy(player)
         .and(Matches.unitIsOfType(unitType))
-        .and(Predicate.not(Unit::isTransporting));
+        .and(Unit::isTransporting);
   }
 
   public static Predicate<Unit> unitIsOwnedAndMatchesTypeAndNotTransporting(
       final GamePlayer player, final UnitType unitType) {
     return Matches.unitIsOwnedBy(player)
         .and(Matches.unitIsOfType(unitType))
-        .and(Unit::isTransporting);
+        .and(Predicate.not(Unit::isTransporting));
   }
 
   public static Predicate<Unit> unitIsOwnedCarrier(final GamePlayer player) {
