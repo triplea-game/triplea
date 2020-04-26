5b801bf1639181521d6b11d9b3c589a92b276873
DanVanAtta <dhvatta@gmail.com>
2020-04-25 18:56:45 -0700

    Use a static import for Mockito.when

--- http-server/src/test/java/org/triplea/modules/chat/ChatMessagingServiceTest.java
+++ http-server/src/test/java/org/triplea/modules/chat/ChatMessagingServiceTest.java
@@ -3,13 +3,13 @@ package org.triplea.modules.chat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import org.jdbi.v3.core.Jdbi;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.triplea.db.dao.chat.history.LobbyChatHistoryDao;
 import org.triplea.web.socket.WebSocketMessagingBus;
@@ -24,7 +24,7 @@ class ChatMessagingServiceTest {
   @Test
   @DisplayName("Verify that we add listeners to messaging bus when configure is called")
   void configureAddsListeners() {
-    Mockito.when(jdbi.onDemand(LobbyChatHistoryDao.class)).thenReturn(lobbyChatHistoryDao);
+    when(jdbi.onDemand(LobbyChatHistoryDao.class)).thenReturn(lobbyChatHistoryDao);
 
     ChatMessagingService.build(chatters, jdbi).configure(webSocketMessagingBus);
 
