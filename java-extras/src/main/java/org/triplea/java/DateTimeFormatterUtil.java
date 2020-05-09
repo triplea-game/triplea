1c615f3ac193abf26a237459c07295d1cffd05ff
DanVanAtta <dhvatta@gmail.com>
2020-05-09 11:52:12 -0700

    Date-time formatter allow format without timezone and improve test control

--- java-extras/src/main/java/org/triplea/java/DateTimeFormatterUtil.java
+++ java-extras/src/main/java/org/triplea/java/DateTimeFormatterUtil.java
@@ -3,36 +3,60 @@ package org.triplea.java;
 import com.google.common.annotations.VisibleForTesting;
 import java.time.Instant;
 import java.time.ZoneId;
+import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
+import lombok.AllArgsConstructor;
 import lombok.experimental.UtilityClass;
 
 /** Utility class for date-time formatting operations. */
 @UtilityClass
 public class DateTimeFormatterUtil {
 
-  /**
-   * Converts an instant into a Date formatted string that contains the date and timezone offset.
-   * EG: "2000-12-1 23:59 (GMT-5)"
-   */
-  public static String formatInstant(final Instant instant) {
-    return formatEpochMilli(instant, Locale.getDefault(), ZoneId.systemDefault());
-  }
+  private static Locale defaultLocale = Locale.getDefault();
+  private static ZoneId defaultZone = ZoneId.systemDefault();
 
-  /**
-   * Converts an epoch milli timestamp into a Date formatted string that contains the date and
-   * timezone offset. EG: "2000-12-1 23:59 (GMT-5)"
-   */
-  public static String formatEpochMilli(final long epochMilli) {
-    return formatEpochMilli(
-        Instant.ofEpochMilli(epochMilli), Locale.getDefault(), ZoneId.systemDefault());
+  @AllArgsConstructor
+  public enum FormatOption {
+    /** EG: "2000-12-1 23:59 (GMT-5) */
+    WITH_TIMEZONE("y-M-d H:m (O)"),
+
+    /** EG: "2000-12-1 23:59 */
+    WITHOUT_TIMEZONE("y-M-d H:m");
+
+    private final String pattern;
   }
 
   @VisibleForTesting
-  static String formatEpochMilli(final Instant instant, final Locale locale, final ZoneId zoneId) {
-    return DateTimeFormatter.ofPattern("y-M-d H:m (O)")
-        .withLocale(locale)
-        .withZone(zoneId)
+  public void setDefaultToUtc() {
+    defaultLocale = Locale.US;
+    defaultZone = ZoneId.ofOffset("UTC", ZoneOffset.UTC);
+  }
+
+  /**
+   * Converts an epoch milli timestamp into a Date formatted string that contains the date, time,
+   * and timezone offset. EG: "2000-12-1 23:59 (GMT-5)"
+   */
+  public static String formatEpochMilli(final long epochMilli) {
+    return formatEpochMilli(epochMilli, FormatOption.WITH_TIMEZONE);
+  }
+
+  public static String formatEpochMilli(final long epochMilli, final FormatOption formatOption) {
+    return formatInstant(Instant.ofEpochMilli(epochMilli), formatOption);
+  }
+
+  /**
+   * Converts an instant into a Date formatted string that contains the date, time and timezone
+   * offset. EG: "2000-12-1 23:59 (GMT-5)"
+   */
+  public static String formatInstant(final Instant instant) {
+    return formatInstant(instant, FormatOption.WITH_TIMEZONE);
+  }
+
+  private static String formatInstant(final Instant instant, final FormatOption formatOption) {
+    return DateTimeFormatter.ofPattern(formatOption.pattern)
+        .withLocale(defaultLocale)
+        .withZone(defaultZone)
         .format(instant);
   }
 }
--- java-extras/src/test/java/org/triplea/java/DateTimeFormatterUtilTest.java
+++ java-extras/src/test/java/org/triplea/java/DateTimeFormatterUtilTest.java
@@ -5,23 +5,34 @@ import static org.hamcrest.Matchers.is;
 
 import java.time.Instant;
 import java.time.LocalDateTime;
-import java.time.ZoneId;
 import java.time.ZoneOffset;
-import java.util.Locale;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.triplea.java.DateTimeFormatterUtil.FormatOption;
 
 class DateTimeFormatterUtilTest {
-  private static final ZoneId ZONE_ID = ZoneId.ofOffset("UTC", ZoneOffset.of("+08:00"));
   private static final long DEC_FIRST_EPOCH_MILLIS =
       LocalDateTime.of(2000, 12, 1, 23, 59) //
           .toInstant(ZoneOffset.ofHours(8))
           .toEpochMilli();
 
+  @BeforeAll
+  static void setDefaultToUtc() {
+    DateTimeFormatterUtil.setDefaultToUtc();
+  }
+
   @Test
-  void verifyFormatting() {
+  void verifyFormattingWithTimeZone() {
+    final String result =
+        DateTimeFormatterUtil.formatInstant(Instant.ofEpochMilli(DEC_FIRST_EPOCH_MILLIS));
+    assertThat(result, is("2000-12-1 15:59 (GMT)"));
+  }
+
+  @Test
+  void verifyFormattingNoTimeZone() {
     final String result =
         DateTimeFormatterUtil.formatEpochMilli(
-            Instant.ofEpochMilli(DEC_FIRST_EPOCH_MILLIS), Locale.ENGLISH, ZONE_ID);
-    assertThat(result, is("2000-12-1 23:59 (GMT+8)"));
+            DEC_FIRST_EPOCH_MILLIS, FormatOption.WITHOUT_TIMEZONE);
+    assertThat(result, is("2000-12-1 15:59"));
   }
 }
