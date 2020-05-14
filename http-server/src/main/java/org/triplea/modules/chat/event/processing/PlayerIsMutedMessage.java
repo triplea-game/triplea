b8ef3f6636d9b0a1635e318665174fbb712906ae
DanVanAtta <dhvatta@gmail.com>
2020-05-13 18:40:03 -0700

    Convert formatting function to be an inner class

--- http-server/src/main/java/org/triplea/modules/chat/event/processing/PlayerIsMutedMessage.java
+++ http-server/src/main/java/org/triplea/modules/chat/event/processing/PlayerIsMutedMessage.java
@@ -4,23 +4,33 @@ import com.google.common.annotations.VisibleForTesting;
 import java.time.Clock;
 import java.time.Duration;
 import java.time.Instant;
-import java.util.function.BiFunction;
+import java.util.function.Function;
+import lombok.Builder;
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
 class PlayerIsMutedMessage {
-
-  @VisibleForTesting
-  static final BiFunction<Clock, Instant, String> muteDurationRemainingToString =
-      (clock, muteExpiry) -> {
-        final long minutes = Duration.between(clock.instant(), muteExpiry).toMinutes();
-        return minutes > 0
-            ? minutes + " minutes"
-            : Duration.between(clock.instant(), muteExpiry).toSeconds() + " seconds";
-      };
+  private static Function<Instant, String> muteDurationFormatter =
+      MuteDurationRemainingCalculator.builder().build();
 
   String build(final Instant muteExpiry) {
-    return "You have been muted, expiring in: "
-        + muteDurationRemainingToString.apply(Clock.systemUTC(), muteExpiry);
+    return "You have been muted, expiring in: " + muteDurationFormatter.apply(muteExpiry);
+  }
+
+  /**
+   * Calculates a string of how many minutes are remaining in a mute until expired, or if less than
+   * a minute then how many seconds are left.
+   */
+  @VisibleForTesting
+  @Builder
+  static class MuteDurationRemainingCalculator implements Function<Instant, String> {
+    @Builder.Default private Clock clock = Clock.systemUTC();
+
+    public String apply(final Instant muteExpiry) {
+      final long minutes = Duration.between(clock.instant(), muteExpiry).toMinutes();
+      return minutes > 0
+          ? minutes + " minutes"
+          : Duration.between(clock.instant(), muteExpiry).toSeconds() + " seconds";
+    }
   }
 }
--- http-server/src/test/java/org/triplea/modules/chat/event/processing/PlayerIsMutedMessageTest.java
+++ http-server/src/test/java/org/triplea/modules/chat/event/processing/PlayerIsMutedMessageTest.java
@@ -8,6 +8,7 @@ import java.time.Clock;
 import java.time.Instant;
 import java.time.ZoneOffset;
 import java.time.temporal.ChronoUnit;
+import java.util.function.Function;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -17,14 +18,17 @@ import org.mockito.junit.jupiter.MockitoExtension;
 class PlayerIsMutedMessageTest {
   private static final Instant CURRENT_TIME = utcInstantOf(2020, 11, 1, 2, 20);
 
+  private Function<Instant, String> formattingFunction =
+      PlayerIsMutedMessage.MuteDurationRemainingCalculator.builder()
+          .clock(Clock.fixed(CURRENT_TIME, ZoneOffset.UTC))
+          .build();
+
   @Test
   @DisplayName("Verify an example mute message calculation with 10 minutes remaining")
   void verifyTimeDurationComputation() {
     final Instant banExpiry = CURRENT_TIME.plus(10, ChronoUnit.MINUTES);
 
-    final String result =
-        PlayerIsMutedMessage.muteDurationRemainingToString.apply(
-            Clock.fixed(CURRENT_TIME, ZoneOffset.UTC), banExpiry);
+    final String result = formattingFunction.apply(banExpiry);
 
     assertThat(result, is("10 minutes"));
   }
@@ -34,9 +38,7 @@ class PlayerIsMutedMessageTest {
   void verifyTimeDurationComputationWithSecondsRemaining() {
     final Instant banExpiry = CURRENT_TIME.plus(20, ChronoUnit.SECONDS);
 
-    final String result =
-        PlayerIsMutedMessage.muteDurationRemainingToString.apply(
-            Clock.fixed(CURRENT_TIME, ZoneOffset.UTC), banExpiry);
+    final String result = formattingFunction.apply(banExpiry);
 
     assertThat(result, is("20 seconds"));
   }
