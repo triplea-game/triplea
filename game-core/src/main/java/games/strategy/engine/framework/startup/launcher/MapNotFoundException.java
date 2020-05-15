94d32a2c132074a5468a435f58cda6fea7e496ed
DanVanAtta <dhvatta@gmail.com>
2020-05-15 00:00:28 -0700

    Improve map not found error messaging
    Tell the user the name of the map that could not be found
    and the paths on file system that were searched.

--- game-core/src/main/java/games/strategy/engine/framework/startup/launcher/MapNotFoundException.java
+++ game-core/src/main/java/games/strategy/engine/framework/startup/launcher/MapNotFoundException.java
@@ -1,5 +1,9 @@
 package games.strategy.engine.framework.startup.launcher;
 
+import java.io.File;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * An unchecked exception indicating the map associated with a save game was not found and must be
  * installed before game play can begin.
@@ -8,4 +12,12 @@ public class MapNotFoundException extends IllegalStateException {
   private static final long serialVersionUID = -1027460394367073991L;
 
   public MapNotFoundException() {}
+
+  public MapNotFoundException(final String mapName, final List<File> candidatePaths) {
+    super(
+        "Could not find map: "
+            + mapName
+            + "\nNot found, searched these locations:\n"
+            + candidatePaths.stream().map(File::getAbsolutePath).collect(Collectors.joining("\n")));
+  }
 }
