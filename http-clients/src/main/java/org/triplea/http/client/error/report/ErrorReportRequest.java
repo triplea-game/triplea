21de32ad53d4ce8b68f917c6422799bbcf4f81b4
RoiEX <8350879+RoiEXLab@users.noreply.github.com>
2020-05-30 15:05:06 +0200

    Don't trim issue body

--- http-clients/src/main/java/org/triplea/http/client/error/report/ErrorReportRequest.java
+++ http-clients/src/main/java/org/triplea/http/client/error/report/ErrorReportRequest.java
@@ -1,9 +1,11 @@
 package org.triplea.http.client.error.report;
 
 import com.google.common.base.Ascii;
+import javax.annotation.Nullable;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 import org.triplea.http.client.github.issues.GithubIssueClient;
@@ -15,16 +17,10 @@ import org.triplea.http.client.github.issues.GithubIssueClient;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ErrorReportRequest {
-  private String title;
-  private String body;
+  @Nullable private String title;
+  @Getter @Nullable private String body;
 
   public String getTitle() {
     return title == null ? null : Ascii.truncate(title, GithubIssueClient.TITLE_MAX_LENGTH, "...");
-  }
-
-  public String getBody() {
-    return body == null
-        ? null
-        : Ascii.truncate(body, GithubIssueClient.REPORT_BODY_MAX_LENGTH, "...");
   }
 }
