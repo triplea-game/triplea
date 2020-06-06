c921661351387e66ba053d6e4e4908d4148f9538
Dan Van Atta <dhvatta@gmail.com>
2020-06-06 15:41:51 -0700

    Fix NPE in auto-placement-finder when finding islands (#6580)
    * Fix NPE in auto-placement-finder when finding islands
    
    The contract of MapData#getContainedTerritoryPolygons (in javadoc)
    states the parameter should be a sea territory. The caller passes
    all territories to this method which then results in a NPE in
    the method. To fix, relax the contract and return an empty collection
    if the parameter is not a sea territory.
    
    Reported in: https://github.com/triplea-game/triplea/issues/6469

--- game-core/src/main/java/games/strategy/triplea/ui/mapdata/MapData.java
+++ game-core/src/main/java/games/strategy/triplea/ui/mapdata/MapData.java
@@ -515,11 +515,13 @@ public class MapData {
   /**
    * Returns the polygons for any territories contained within a given territory.
    *
-   * @param territoryName Name of the territory, expected to be a sea territory, where we will be
-   *     checking for any contained 'island' territories.
+   * @param territoryName Name of the territory, where we will be checking for any contained
+   *     'island' territories.
+   * @return Empty if the parameter is not a sea territory or contains no 'islands', otherwise
+   *     returns any 'islands' contained within the given territory.
    */
   public Set<Polygon> getContainedTerritoryPolygons(final String territoryName) {
-    return contains.get(territoryName).stream()
+    return contains.getOrDefault(territoryName, Set.of()).stream()
         .map(this::getPolygons)
         .flatMap(Collection::stream)
         .collect(Collectors.toSet());
