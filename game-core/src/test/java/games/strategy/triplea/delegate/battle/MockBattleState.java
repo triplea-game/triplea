d785386323545db6c2a105cfe572e452922640b5
Trevan Richins <trevan_github@therichins.net>
2020-06-07 08:17:25 -0600

    Make the MockBattleState a builder instead of having it wrap a builder

--- game-core/src/test/java/games/strategy/triplea/delegate/battle/MockBattleState.java
+++ game-core/src/test/java/games/strategy/triplea/delegate/battle/MockBattleState.java
@@ -5,29 +5,30 @@ import java.util.Collection;
 import java.util.List;
 import lombok.Builder;
 import lombok.NonNull;
-import lombok.experimental.UtilityClass;
 
-@UtilityClass
-public class MockBattleState {
+/**
+ * Simple implementation of BattleState for tests to use
+ *
+ * {@link #givenBattleState()} will return a builder with everything defaulted and the test
+ * can override the specific items needed.
+ */
+@Builder
+public class MockBattleState implements BattleState {
 
-  @Builder
-  public static class MockBattleStateBuilder implements BattleState {
-
-    final @NonNull Collection<Unit> attackingUnits;
-    final @NonNull Collection<Unit> defendingUnits;
-
-    @Override
-    public Collection<Unit> getAttackingUnits() {
-      return attackingUnits;
-    }
-
-    @Override
-    public Collection<Unit> getDefendingUnits() {
-      return defendingUnits;
-    }
+  public static MockBattleState.MockBattleStateBuilder givenBattleState() {
+    return MockBattleState.builder().attackingUnits(List.of()).defendingUnits(List.of());
   }
 
-  public static MockBattleStateBuilder.MockBattleStateBuilderBuilder givenBattleState() {
-    return MockBattleStateBuilder.builder().attackingUnits(List.of()).defendingUnits(List.of());
+  final @NonNull Collection<Unit> attackingUnits;
+  final @NonNull Collection<Unit> defendingUnits;
+
+  @Override
+  public Collection<Unit> getAttackingUnits() {
+    return attackingUnits;
   }
+
+  @Override
+  public Collection<Unit> getDefendingUnits() {
+    return defendingUnits;
+    }
 }
