54bc296542ae0f22de8d78e4a589bab1f53ae99a
tripleabuilderbot <tripleabuilderbot@gmail.com>
2020-06-10 06:44:39 +0000

    Auto-Formatting

--- http-clients/src/main/java/org/triplea/http/client/github/issues/GithubIssueClient.java
+++ http-clients/src/main/java/org/triplea/http/client/github/issues/GithubIssueClient.java
@@ -18,9 +18,7 @@ public class GithubIssueClient {
    * Arbitrary length to prevent titles from being too large and cluttering up the issue display.
    */
   public static final int TITLE_MAX_LENGTH = 125;
-  /**
-   * Max length for github issue body text.
-   */
+  /** Max length for github issue body text. */
   public static final int REPORT_BODY_MAX_LENGTH = 65536;
 
   /** If this client is set to 'test' mode, we will return a stubbed response. */
