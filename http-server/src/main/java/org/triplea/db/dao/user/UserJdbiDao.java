f73a1ec9bd40fa9c00ce51e837ad34d0696c0926
tripleabuilderbot <tripleabuilderbot@gmail.com>
2020-06-10 03:56:29 +0000

    Auto-Formatting

--- http-server/src/main/java/org/triplea/db/dao/user/UserJdbiDao.java
+++ http-server/src/main/java/org/triplea/db/dao/user/UserJdbiDao.java
@@ -15,8 +15,7 @@ public interface UserJdbiDao {
   @SqlQuery("select bcrypt_password from lobby_user where username = :username")
   Optional<String> getPassword(@Bind("username") String username);
 
-  @SqlUpdate(
-      "update lobby_user set bcrypt_password = :newPassword where id = :userId")
+  @SqlUpdate("update lobby_user set bcrypt_password = :newPassword where id = :userId")
   int updatePassword(@Bind("userId") int userId, @Bind("newPassword") String newPassword);
 
   @SqlQuery("select email from lobby_user where id = :userId")
