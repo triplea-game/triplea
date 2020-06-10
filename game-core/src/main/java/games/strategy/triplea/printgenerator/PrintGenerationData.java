aa9014a76915513f986c944d02122846a05c4162
DanVanAtta <dhvatta@gmail.com>
2020-06-09 21:56:41 -0700

    Use lombok for data class getters & setters

--- game-core/src/main/java/games/strategy/triplea/printgenerator/PrintGenerationData.java
+++ game-core/src/main/java/games/strategy/triplea/printgenerator/PrintGenerationData.java
@@ -2,26 +2,12 @@ package games.strategy.triplea.printgenerator;
 
 import games.strategy.engine.data.GameData;
 import java.io.File;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 class PrintGenerationData {
   private File outDir;
-  private GameData gameData;
-
-  PrintGenerationData() {}
-
-  File getOutDir() {
-    return outDir;
-  }
-
-  void setOutDir(final File outDir) {
-    this.outDir = outDir;
-  }
-
-  protected GameData getData() {
-    return gameData;
-  }
-
-  protected void setData(final GameData data) {
-    gameData = data;
-  }
+  private GameData data;
 }
