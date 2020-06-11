bb1a303bb499ec9fc400105ec2c7edc7d2a4d6aa
Dan Van Atta <dhvatta@gmail.com>
2020-06-10 17:09:49 -0700

    Remove legacy MD5Crypt Password (#6664)
    * Remove legacy MD5Crypt Password
    
    All accounts being migrated to 2.0 have a bcrypt password, we
    no longer need the legacy 'password' column on lobby_user
    which stored passes as MD5Crypt.
    
    This update:
    - removes the md5crypt'ed password column
    - adds a non-null constraint to the bcrypt_password column
    - moves SHA512 password hashing from server to client.
      The SHA512 password hash is simply so that those that
      control the server cannot know the plain-text password
      of a user. The SHA512 hash is *not* intended to protect
      the password on the client-side.
    - Simplifies some of the password hash verification logic
      by collapsing intermediary function objects.

--- http-server/src/main/java/org/triplea/modules/user/account/PasswordBCrypter.java
+++ http-server/src/main/java/org/triplea/modules/user/account/PasswordBCrypter.java
@@ -2,26 +2,10 @@ package org.triplea.modules.user.account;
 
 import at.favre.lib.crypto.bcrypt.BCrypt;
 import at.favre.lib.crypto.bcrypt.LongPasswordStrategies;
-import com.google.common.annotations.VisibleForTesting;
-import com.google.common.base.Preconditions;
-import com.google.common.io.BaseEncoding;
-import java.nio.charset.StandardCharsets;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.util.function.Function;
+import lombok.experimental.UtilityClass;
 
-public class PasswordBCrypter implements Function<String, String> {
-  // TODO: Md5-Deprecation This value needs to be kept in sync with Sha512Hasher
-  private static final String PSEUDO_SALT = "TripleA";
-  private static final String SHA_512 = "SHA-512";
-
-  @Override
-  public String apply(final String password) {
-    // TODO: Md5-Deprecation remove hashing here, move to client-side
-    final String hashed = hashPasswordWithSalt(password);
-    return hashPassword(hashed);
-  }
-
+@UtilityClass
+public class PasswordBCrypter {
   /**
    * This is a helper method designed to simplify the bcrypt API and hide some of the constants
    * involved. This method generates a hash with 10 rounds. This number is arbitrary and might
@@ -46,32 +30,5 @@ public class PasswordBCrypter implements Function<String, String> {
     return BCrypt.verifyer(null, LongPasswordStrategies.none())
         .verify(password.toCharArray(), hash.toCharArray())
         .verified;
-  }
-
-  /**
-   * Legacy security precaution. When the lobby still relied on plain TCP sockets, a pseudo-security
-   * for passwords was implemented to prevent them from leaking out into the internet. This
-   * algorithm was vulnerable to a MITM-attack, so in order to keep the user safe in case they
-   * re-used their TripleA password anywhere else, we took the password, prepended a fixed String
-   * provided by {@link #PSEUDO_SALT}, and SHA-512 hashed the whole thing before sending it over the
-   * network. This way an attacker would have to explicitly pre-calculate hashes for TripleA
-   * exclusively in order to brute-force the original password, which hopefully made the thing
-   * annoying enough for most people to try.
-   */
-  public static String hashPasswordWithSalt(final String password) {
-    Preconditions.checkNotNull(password);
-    return sha512(PSEUDO_SALT + password);
-  }
-
-  /** Creates a SHA-512 hash of the given String. */
-  @VisibleForTesting
-  static String sha512(final String input) {
-    try {
-      return BaseEncoding.base16()
-          .encode(MessageDigest.getInstance(SHA_512).digest(input.getBytes(StandardCharsets.UTF_8)))
-          .toLowerCase();
-    } catch (final NoSuchAlgorithmException e) {
-      throw new IllegalStateException(SHA_512 + " is not supported!", e);
-    }
   }
 }
