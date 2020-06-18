87971d6fecc5e81656b6b3fbff4c34b25fe74985
tripleabuilderbot <tripleabuilderbot@gmail.com>
2020-06-18 01:14:19 +0000

    Auto-Formatting

--- game-core/src/main/java/games/strategy/engine/lobby/client/login/LobbyLogin.java
+++ game-core/src/main/java/games/strategy/engine/lobby/client/login/LobbyLogin.java
@@ -6,7 +6,6 @@ import games.strategy.engine.framework.ui.MainFrame;
 import games.strategy.engine.framework.ui.background.BackgroundTaskRunner;
 import games.strategy.engine.lobby.client.LobbyClient;
 import games.strategy.engine.lobby.client.ui.LobbyFrame;
-import games.strategy.triplea.UrlConstants;
 import java.awt.Window;
 import java.io.IOException;
 import java.util.Optional;
@@ -24,8 +23,6 @@ import org.triplea.http.client.web.socket.client.connections.PlayerToLobbyConnec
 import org.triplea.live.servers.ServerProperties;
 import org.triplea.swing.DialogBuilder;
 import org.triplea.swing.SwingComponents;
-import org.triplea.swing.SwingComponents.DialogWithLinksParams;
-import org.triplea.swing.SwingComponents.DialogWithLinksTypes;
 
 /**
  * The client side of the lobby authentication protocol.
