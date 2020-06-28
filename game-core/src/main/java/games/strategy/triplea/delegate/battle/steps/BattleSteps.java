e4c8fa909e563d9ebcb7d8e6af699df59067eaef
Trevan Richins <trevan_github@therichins.net>
2020-06-28 15:53:02 -0600

    One more place that needs special defense first strike check

--- game-core/src/main/java/games/strategy/triplea/delegate/battle/steps/BattleSteps.java
+++ game-core/src/main/java/games/strategy/triplea/delegate/battle/steps/BattleSteps.java
@@ -202,7 +202,7 @@ public class BattleSteps implements BattleStepStrings, BattleState {
     // also, ww2v3/global rules, defending subs without sneak attack fire with all defenders
     final Collection<Unit> defendingUnitsAliveAndDamaged = new ArrayList<>(defendingUnits);
     defendingUnitsAliveAndDamaged.addAll(defendingWaitingToDie);
-    if (defendingUnitsAliveAndDamaged.stream().anyMatch(Matches.unitIsFirstStrike())
+    if (defendingUnitsAliveAndDamaged.stream().anyMatch(Matches.unitIsFirstStrikeOnDefense(gameData))
         && !defenderSubsFireFirst
         && (defendingSubsFireWithAllDefenders || defendingSubsFireWithAllDefendersAlways)) {
       steps.add(defender.getName() + FIRST_STRIKE_UNITS_FIRE);
