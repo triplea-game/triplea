8202a5fa42119d492ae2cb73d3a218fd9d37df5b
tripleabuilderbot <tripleabuilderbot@gmail.com>
2020-06-29 01:09:43 +0000

    Auto-Formatting

--- game-core/src/main/java/games/strategy/engine/chat/Chat.java
+++ game-core/src/main/java/games/strategy/engine/chat/Chat.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.HashSet;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.function.BiConsumer;
