6b2c0d686ccf924b34bf11cecd2859d7e3fec594
asvitkine <alexei.svitkine@gmail.com>
2020-07-09 21:16:22 -0400

    Improve PerfTimer utility. (#6850)
    * Improve PerfTimer utility.
    
    - Showing running average.
    - Passing a frequency param to print stats every N calls
    - If frequency is 0, no work is done. This allows adding
      the perf timer permanently to some code locations and
      having a pref setting to set the frequency / enable.

--- game-core/src/main/java/org/triplea/performance/PerfTimer.java
+++ game-core/src/main/java/org/triplea/performance/PerfTimer.java
@@ -23,52 +23,82 @@ import lombok.extern.java.Log;
 @Log
 public class PerfTimer implements Closeable {
 
-  private static final PerfTimer DISABLED_TIMER = new PerfTimer("disabled");
   private static final Map<String, AtomicLong> runningTotal = new HashMap<>();
-  final String title;
-  private final long startMillis;
+  private static final Map<String, AtomicLong> runningCount = new HashMap<>();
+  private final String title;
+  private final int reportingFrequency;
+  private final long startNanos;
 
-  private PerfTimer(final String title) {
+  private PerfTimer(final String title, final int reportingFrequency) {
     this.title = title;
-    this.startMillis = System.nanoTime();
+    this.reportingFrequency = reportingFrequency;
+    this.startNanos = System.nanoTime();
   }
 
   private long stopTimer() {
-    return System.nanoTime() - startMillis;
+    return System.nanoTime() - startNanos;
   }
 
   @Override
   public void close() {
-    processResult(stopTimer(), this);
+    if (this.reportingFrequency > 0) {
+      processResult(stopTimer(), this);
+    }
   }
 
   @SuppressWarnings("unused")
   public static PerfTimer startTimer(final String title) {
-    return new PerfTimer(title);
+    return startTimer(title, 1);
+  }
+
+  /**
+   * Creates a perf timer with a reporting frequency. The reporting frequency specifies N specifies
+   * that performance information should be printed every N executions of the timer. If 0, no
+   * information is printed.
+   *
+   * @param title The name of the timer
+   * @param reportingFrequency The reporting frequency.
+   * @return the perf timer object
+   */
+  @SuppressWarnings("unused")
+  public static PerfTimer startTimer(final String title, final int reportingFrequency) {
+    return new PerfTimer(title, reportingFrequency);
   }
 
   private static synchronized void processResult(final long stopNanos, final PerfTimer perfTimer) {
-    final long stopMicros = stopNanos / 1000;
-
-    final long milliFraction = (stopMicros % 1000) / 100;
-    final long millis = (stopMicros / 1000);
-
     final AtomicLong totalNanos =
         runningTotal.computeIfAbsent(perfTimer.title, key -> new AtomicLong(0));
-    totalNanos.set(totalNanos.get() + stopNanos);
+    final long totalNano = totalNanos.get() + stopNanos;
+    totalNanos.set(totalNano);
 
-    final long totalNano = totalNanos.get();
+    final AtomicLong totalCount =
+        runningCount.computeIfAbsent(perfTimer.title, key -> new AtomicLong(0));
+    final long newCount = totalCount.get() + 1;
+    totalCount.set(newCount);
+
+    if ((newCount % perfTimer.reportingFrequency) == 0) {
+      return;
+    }
+
     final long totalMillis = (totalNano / (1000 * 1000));
+    final long avgNanos = totalNano / newCount;
 
     log.info(
         String.format(
-            "%s: %s.%s ms; %s total ms;   %s ns; %s total ns",
-            perfTimer.title, //
-            millis,
-            milliFraction,
+            "%s: %s ms; %s total ms;   %s ns; %s total ns;   running avg %s ms",
+            perfTimer.title,
+            toMilllisString(stopNanos),
             totalMillis,
             stopNanos,
-            totalNano));
+            totalNano,
+            toMilllisString(avgNanos)));
+  }
+
+  private static String toMilllisString(final long nanos) {
+    final long micros = nanos / 1000;
+    final long millis = (micros / 1000);
+    final long milliFraction = (micros % 1000) / 100;
+    return String.format("%s.%s", millis, milliFraction);
   }
 
   public static <T> T time(final String title, final Supplier<T> functionToTime) {
