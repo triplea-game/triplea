ee0682185e8cfbd72a613a232ed2aa4bae033bea
Dan Van Atta <dhvatta@gmail.com>
2020-07-11 13:30:38 -0700

    Save game compatibility testing (#7101)
    * Extract test file listing utility to its own class
    
    * Improve file listing test utility robustness, print folder name if not found and improve variable naming
    
    * Add a test that will load save-games from test/resources/save-games and tries to load them.
    
    This test serves as a compatibility check.
    
    * Add task to download save-game files from assets & add list of save-game files to be downloaded
    
    * Improve compatibility test, assert various gameData properties are not null after load
    
    If we change the name of an item in GameData, it will not cause a loading exception but we will get an unexpected null value. This update makes the compatibility check more robust (but still not fool-proof)

--- smoke-testing/src/test/java/games/strategy/engine/data/ParseGameXmlsTest.java
+++ smoke-testing/src/test/java/games/strategy/engine/data/ParseGameXmlsTest.java
@@ -3,11 +3,7 @@ package games.strategy.engine.data;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
-import java.nio.file.Paths;
-import java.util.Arrays;
 import java.util.Collection;
-import java.util.Comparator;
-import java.util.stream.Collectors;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.triplea.test.common.Integration;
@@ -25,17 +21,6 @@ class ParseGameXmlsTest {
   }
 
   static Collection<File> parseGameFiles() {
-    return Arrays.stream(findMapXmlsFolder().listFiles())
-        .sorted(Comparator.comparing(File::getName))
-        .collect(Collectors.toList());
-  }
-
-  private static File findMapXmlsFolder() {
-    final File mapXmls = Paths.get("src", "test", "resources", "map-xmls").toFile();
-    if (mapXmls.exists()) {
-      return mapXmls;
-    }
-
-    return Paths.get("smoke-testing", "src", "test", "resources", "map-xmls").toFile();
+    return TestDataFileLister.listFilesInTestResourcesDirectory("map-xmls");
   }
 }
