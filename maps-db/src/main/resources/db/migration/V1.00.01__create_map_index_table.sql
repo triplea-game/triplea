142b57167befc5951d67520c8cf8081360243018
Dan Van Atta <dhvatta@gmail.com>
2020-08-18 19:17:15 -0700

    Add an initial schema for maps-db (#7265)
    * Add an initial schema for maps-db
    
    - Contains enough of a schema that we can model the triplea_maps.yaml file
    
    * Drop non-null constraint on map-thumbnail and add uniqueness constraints
    
    - map-version 'map-id' and 'version' should be unique, we expect only one version
      of a given map version.
    - each map-version URL should be unique as well so that only one map-version
      points to a specific location.

---
+++ maps-db/src/main/resources/db/migration/V1.00.01__create_map_index_table.sql
@@ -0,0 +1,37 @@
+comment on database maps_db is 'Database used to store, track and serve map information';
+
+create table map (
+  id serial primary key,
+  name varchar(256) not null unique check(length(name) > 3),
+  date_created timestamptz not null default now(),
+  date_updated timestamptz not null default now()
+);
+
+create table category (
+  id serial primary key,
+  name varchar(32) unique not null check (length(name) > 2),
+  date_created timestamptz not null default now()
+);
+
+insert into category (name) values
+  ('BEST'),
+  ('GOOD'),
+  ('EXPERIMENTAL');
+
+
+create table map_version (
+  id serial primary key,
+  map_id integer not null references map(id),
+  version integer not null check (version > 0),
+  url varchar(256) not null unique check (url like 'http%'),
+  thumbnail varchar(256) check (thumbnail is null or thumbnail like 'http%'),
+  category_id integer not null references category(id),
+  description varchar(4096),
+  date_created timestamptz not null default now(),
+  date_updated timestamptz not null default now()
+);
+
+create unique index map_version_uniq_version
+on map_version (map_id, version);
+
+
