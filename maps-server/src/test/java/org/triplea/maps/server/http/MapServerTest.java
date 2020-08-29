dbcd4eb39de172188f7f1c0e18c560e2879a1bfb
tripleabuilderbot <tripleabuilderbot@gmail.com>
2020-08-29 17:11:07 +0000

    Auto-Formatting

--- maps-server/src/test/java/org/triplea/maps/server/http/MapServerTest.java
+++ maps-server/src/test/java/org/triplea/maps/server/http/MapServerTest.java
@@ -1,7 +1,6 @@
 package org.triplea.maps.server.http;
 
 import com.github.database.rider.junit5.DBUnitExtension;
-import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.triplea.test.common.Integration;
 
