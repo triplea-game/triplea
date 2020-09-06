1d2b96aa19c84f68bbc974d91b7a40c2efa983dd
DanVanAtta <dhvatta@gmail.com>
2020-09-05 19:21:30 -0700

    Use XML-Scanner in game-core to do shallow-parsing
    The shallow-parsing is so we can find the game name of a XML file
    without doing the full parse of XML and without assembling
    a GameData object.
    
    This update changes shallow parsing to use a new XML Scanner
    (in xml-reader) that is based on StAX.
    
    The performance change for Bot startup speed is negligable, but
    for listing game files (clicking 'select map' button), there
    is a pretty nice speedup and XML shallow parsing time is
    reduced from on average 80ms (per XML) to 0.5ms
    
    Forward-looking, future work will incorporate the XML body-text
    scanner to read game-notes in a quick manner to speed up the listing
    of game notes in the 'select game' screen.

--- game-core/src/main/java/games/strategy/engine/data/gameparser/ShallowGameParser.java
+++ game-core/src/main/java/games/strategy/engine/data/gameparser/ShallowGameParser.java
@@ -2,19 +2,34 @@ package games.strategy.engine.data.gameparser;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 
-import games.strategy.engine.data.EngineVersionException;
-import games.strategy.engine.data.GameData;
 import games.strategy.engine.data.GameParseException;
 import java.io.InputStream;
+import javax.xml.stream.XMLStreamException;
 import lombok.experimental.UtilityClass;
+import org.triplea.generic.xml.scanner.AttributeScannerParameters;
+import org.triplea.generic.xml.scanner.XmlScanner;
 
 @UtilityClass
 public class ShallowGameParser {
 
   public static String readGameName(final String mapName, final InputStream stream)
-      throws GameParseException, EngineVersionException {
+      throws GameParseException {
     checkNotNull(mapName);
     checkNotNull(stream);
-    return new GameParser(new GameData(), mapName).parseShallow(stream).getGameName();
+
+    try {
+      return new XmlScanner(stream)
+          .scanForAttributeValue(
+              AttributeScannerParameters.builder().attributeName("name").tag("info").build())
+          .orElseThrow(
+              () ->
+                  new GameParseException(
+                      "Error reading Game XML file in map: "
+                          + mapName
+                          + ". Game file is missing the tag: '<info name=\"Game Name\"/>'"));
+    } catch (final XMLStreamException e) {
+      throw new GameParseException(
+          "Unexpected error reading Game XML file in map: " + mapName + ", " + e.getMessage(), e);
+    }
   }
 }
