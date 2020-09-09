1c61add5d1cf2079ba6f438d85f9c75235f8cb82
Dan Van Atta <dhvatta@gmail.com>
2020-09-08 18:03:47 -0700

    Update: do shallow parsing for select map info (#7570)
    When selecting a map, instead of loading a full GameData instead
    do a 'shallow parse' to and return a subset of the XML elements,
    enough such that we can fill in the map blurb.

--- game-core/src/main/java/games/strategy/engine/data/gameparser/ShallowGameParser.java
+++ game-core/src/main/java/games/strategy/engine/data/gameparser/ShallowGameParser.java
@@ -9,8 +9,11 @@ import java.util.logging.Level;
 import javax.xml.stream.XMLStreamException;
 import lombok.experimental.UtilityClass;
 import lombok.extern.java.Log;
+import org.triplea.generic.xml.reader.XmlMapper;
+import org.triplea.generic.xml.reader.exceptions.XmlParsingException;
 import org.triplea.generic.xml.scanner.AttributeScannerParameters;
 import org.triplea.generic.xml.scanner.XmlScanner;
+import org.triplea.map.data.elements.ShallowParsedGame;
 
 @UtilityClass
 @Log
@@ -43,6 +46,15 @@ public class ShallowGameParser {
           Level.WARNING,
           "Unexpected error reading Game XML file in map: " + mapName + ", " + e.getMessage(),
           e);
+      return Optional.empty();
+    }
+  }
+
+  public Optional<ShallowParsedGame> parseShallow(final InputStream inputStream) {
+    try {
+      return Optional.of(new XmlMapper(inputStream).mapXmlToObject(ShallowParsedGame.class));
+    } catch (final XmlParsingException e) {
+      log.log(Level.WARNING, "Unexpected error reading Game XML, " + e.getMessage(), e);
       return Optional.empty();
     }
   }
