9f6f5500a8b8d36383469cee0e773e374955d47f
Trevan Richins <trevan_github@therichins.net>
2020-09-15 07:09:32 -0600

    Convert general casualty removal

---
+++ game-core/src/main/java/games/strategy/triplea/delegate/battle/steps/change/ClearGeneralCasualties.java
@@ -0,0 +1,35 @@
+package games.strategy.triplea.delegate.battle.steps.change;
+
+import static games.strategy.triplea.delegate.battle.BattleStepStrings.REMOVE_CASUALTIES;
+
+import games.strategy.engine.delegate.IDelegateBridge;
+import games.strategy.triplea.delegate.ExecutionStack;
+import games.strategy.triplea.delegate.battle.BattleActions;
+import games.strategy.triplea.delegate.battle.BattleState;
+import games.strategy.triplea.delegate.battle.steps.BattleStep;
+import java.util.List;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class ClearGeneralCasualties implements BattleStep {
+  private static final long serialVersionUID = 995118832242624142L;
+
+  protected final BattleState battleState;
+
+  protected final BattleActions battleActions;
+
+  @Override
+  public List<String> getNames() {
+    return List.of(REMOVE_CASUALTIES);
+  }
+
+  @Override
+  public Order getOrder() {
+    return Order.GENERAL_REMOVE_CASUALTIES;
+  }
+
+  @Override
+  public void execute(final ExecutionStack stack, final IDelegateBridge bridge) {
+    battleActions.clearWaitingToDieAndDamagedChangesInto(bridge);
+  }
+}
