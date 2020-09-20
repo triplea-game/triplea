819814a33b12458526a35790822097a8ad70fff2
Dan Van Atta <dhvatta@gmail.com>
2020-09-20 13:14:52 -0700

    Simplify map creator window (#7721)
    * Simplify: group variable usage
    
    * Remove unnecessary paint and validate of container panel
    
    * Inline helper method 'setWidgetActivate'
    
    * Remove unnecessary invokeAndWait, map creator is only opened from the EDT
    
    * Convert MapCreator to be a stateless class, inline creation of JFrame

--- game-core/src/main/java/tools/map/making/MapCreator.java
+++ game-core/src/main/java/tools/map/making/MapCreator.java
@@ -2,6 +2,7 @@ package tools.map.making;
 
 import java.awt.BorderLayout;
 import java.awt.Component;
+import java.awt.Container;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
@@ -9,7 +10,7 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.WindowConstants;
-import org.triplea.java.Interruptibles;
+import lombok.experimental.UtilityClass;
 import org.triplea.swing.SwingAction;
 import tools.map.making.ui.properties.MapPropertiesPanel;
 import tools.map.making.ui.skin.MapSkinPanel;
@@ -17,78 +18,72 @@ import tools.map.making.ui.utilities.OptionalUtilitiesPanel;
 import tools.map.making.ui.xml.XmlUtilitiesPanel;
 
 /** A frame that will run the different map making utilities we have. */
-public class MapCreator extends JFrame {
-  private static final long serialVersionUID = 3593102638082774498L;
+@UtilityClass
+public class MapCreator {
 
-  private final JPanel mainPanel;
-  private final JPanel panel1 = MapPropertiesPanel.build();
-  private final JPanel panel2 = MapSkinPanel.build();
-  private final JPanel panel3 = XmlUtilitiesPanel.build();
-  private final JPanel panel4 = OptionalUtilitiesPanel.build();
+  public static void openMapCreatorWindow() {
+    final JFrame frame = new JFrame("TripleA Map Creator Tools");
 
-  private MapCreator() {
-    super("TripleA Map Creator Tools");
-    setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-    // components
-    mainPanel = new JPanel();
+    frame.setSize(800, 600);
+    frame.setLocationRelativeTo(null);
+    frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+
+    final JPanel panel1 = MapPropertiesPanel.build();
+    final JPanel panel2 = MapSkinPanel.build();
+    final JPanel panel3 = XmlUtilitiesPanel.build();
+    final JPanel panel4 = OptionalUtilitiesPanel.build();
+
+    final JPanel mainPanel = new JPanel();
     final JPanel sidePanel = new JPanel();
-    final JButton part1 = new JButton("Step 1: Map Properties");
-    final JButton part2 = new JButton("Step 2: Map Utilities");
-    final JButton part3 = new JButton("Step 3: Game XML");
-    final JButton part4 = new JButton("Other: Optional Things");
     sidePanel.setLayout(new BoxLayout(sidePanel, BoxLayout.PAGE_AXIS));
     sidePanel.add(Box.createVerticalGlue());
+
+    final JButton part1 = new JButton("Step 1: Map Properties");
+    part1.addActionListener(
+        SwingAction.of("Part 1", e -> swapContainerContents(mainPanel, panel1)));
     sidePanel.add(part1);
     part1.setAlignmentX(Component.CENTER_ALIGNMENT);
     sidePanel.add(Box.createVerticalGlue());
+
+    final JButton part2 = new JButton("Step 2: Map Utilities");
+    part2.addActionListener(
+        SwingAction.of("Part 2", e -> swapContainerContents(mainPanel, panel2)));
     sidePanel.add(part2);
     part2.setAlignmentX(Component.CENTER_ALIGNMENT);
     sidePanel.add(Box.createVerticalGlue());
+
+    final JButton part3 = new JButton("Step 3: Game XML");
+    part3.addActionListener(
+        SwingAction.of("Part 3", e -> swapContainerContents(mainPanel, panel3)));
     sidePanel.add(part3);
     part3.setAlignmentX(Component.CENTER_ALIGNMENT);
     sidePanel.add(Box.createVerticalGlue());
+
+    final JButton part4 = new JButton("Other: Optional Things");
+    part4.addActionListener(
+        SwingAction.of("Part 4", e -> swapContainerContents(mainPanel, panel4)));
     sidePanel.add(part4);
     part4.setAlignmentX(Component.CENTER_ALIGNMENT);
     sidePanel.add(Box.createVerticalGlue());
 
-    part1.addActionListener(SwingAction.of("Part 1", e -> setupMainPanel(panel1)));
-    part2.addActionListener(SwingAction.of("Part 2", e -> setupMainPanel(panel2)));
-    part3.addActionListener(SwingAction.of("Part 3", e -> setupMainPanel(panel3)));
-    part4.addActionListener(SwingAction.of("Part 4", e -> setupMainPanel(panel4)));
     // set up the menu actions
-    this.getContentPane().setLayout(new BorderLayout());
-    this.getContentPane().add(new JScrollPane(sidePanel), BorderLayout.WEST);
-    this.getContentPane().add(new JScrollPane(mainPanel), BorderLayout.CENTER);
+    frame.getContentPane().setLayout(new BorderLayout());
+    frame.getContentPane().add(new JScrollPane(sidePanel), BorderLayout.WEST);
+    frame.getContentPane().add(new JScrollPane(mainPanel), BorderLayout.CENTER);
+
     // now set up the main screen
-    setupMainPanel(panel1);
+    mainPanel.add(panel1);
+    frame.setVisible(true);
   }
 
-  private void setupMainPanel(final JPanel panel) {
-    mainPanel.removeAll();
-    mainPanel.add(panel);
-    setWidgetActivation();
-  }
+  private void swapContainerContents(final Container container, final JPanel panel) {
+    container.removeAll();
+    container.add(panel);
 
-  private void setWidgetActivation() {
     SwingAction.invokeNowOrLater(
         () -> {
-          mainPanel.validate();
-          mainPanel.repaint();
-          this.validate();
-          this.repaint();
+          container.revalidate();
+          container.repaint();
         });
-  }
-
-  /** Opens a map creator window. */
-  public static void openMapCreatorWindow() {
-    Interruptibles.await(
-        () ->
-            SwingAction.invokeAndWait(
-                () -> {
-                  final MapCreator creator = new MapCreator();
-                  creator.setSize(800, 600);
-                  creator.setLocationRelativeTo(null);
-                  creator.setVisible(true);
-                }));
   }
 }
