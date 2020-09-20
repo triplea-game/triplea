ce251bbe39d05dd6900845359ef4a79422c0d9dd
DanVanAtta <dhvatta@gmail.com>
2020-09-20 11:12:31 -0700

    Simplify: group variable usage

--- game-core/src/main/java/tools/map/making/MapCreator.java
+++ game-core/src/main/java/tools/map/making/MapCreator.java
@@ -13,6 +13,7 @@ import org.triplea.java.Interruptibles;
 import org.triplea.swing.SwingAction;
 import tools.map.making.ui.properties.MapPropertiesPanel;
 import tools.map.making.ui.skin.MapSkinPanel;
+import tools.map.making.ui.upload.UploadMapPanel;
 import tools.map.making.ui.utilities.OptionalUtilitiesPanel;
 import tools.map.making.ui.xml.XmlUtilitiesPanel;
 
@@ -32,29 +33,33 @@ public class MapCreator extends JFrame {
     // components
     mainPanel = new JPanel();
     final JPanel sidePanel = new JPanel();
-    final JButton part1 = new JButton("Step 1: Map Properties");
-    final JButton part2 = new JButton("Step 2: Map Utilities");
-    final JButton part3 = new JButton("Step 3: Game XML");
-    final JButton part4 = new JButton("Other: Optional Things");
     sidePanel.setLayout(new BoxLayout(sidePanel, BoxLayout.PAGE_AXIS));
     sidePanel.add(Box.createVerticalGlue());
+
+    final JButton part1 = new JButton("Step 1: Map Properties");
+    part1.addActionListener(SwingAction.of("Part 1", e -> setupMainPanel(panel1)));
     sidePanel.add(part1);
     part1.setAlignmentX(Component.CENTER_ALIGNMENT);
     sidePanel.add(Box.createVerticalGlue());
+
+    final JButton part2 = new JButton("Step 2: Map Utilities");
+    part2.addActionListener(SwingAction.of("Part 2", e -> setupMainPanel(panel2)));
     sidePanel.add(part2);
     part2.setAlignmentX(Component.CENTER_ALIGNMENT);
     sidePanel.add(Box.createVerticalGlue());
+
+    final JButton part3 = new JButton("Step 3: Game XML");
+    part3.addActionListener(SwingAction.of("Part 3", e -> setupMainPanel(panel3)));
     sidePanel.add(part3);
     part3.setAlignmentX(Component.CENTER_ALIGNMENT);
     sidePanel.add(Box.createVerticalGlue());
+
+    final JButton part4 = new JButton("Other: Optional Things");
+    part4.addActionListener(SwingAction.of("Part 4", e -> setupMainPanel(panel4)));
     sidePanel.add(part4);
     part4.setAlignmentX(Component.CENTER_ALIGNMENT);
     sidePanel.add(Box.createVerticalGlue());
 
-    part1.addActionListener(SwingAction.of("Part 1", e -> setupMainPanel(panel1)));
-    part2.addActionListener(SwingAction.of("Part 2", e -> setupMainPanel(panel2)));
-    part3.addActionListener(SwingAction.of("Part 3", e -> setupMainPanel(panel3)));
-    part4.addActionListener(SwingAction.of("Part 4", e -> setupMainPanel(panel4)));
     // set up the menu actions
     this.getContentPane().setLayout(new BorderLayout());
     this.getContentPane().add(new JScrollPane(sidePanel), BorderLayout.WEST);
