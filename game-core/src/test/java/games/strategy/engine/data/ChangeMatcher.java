b779405821202df2b47af8e273e113b27a6880e1
Trevan Richins <trevan_github@therichins.net>
2020-09-28 20:22:56 -0600

    Add @param javadoc

--- game-core/src/test/java/games/strategy/engine/data/ChangeMatcher.java
+++ game-core/src/test/java/games/strategy/engine/data/ChangeMatcher.java
@@ -2,5 +2,9 @@ package games.strategy.engine.data;
 
 import org.hamcrest.TypeSafeMatcher;
 
-/** Parent class for all matchers for {@link Change} objects */
+/**
+ * Parent class for all matchers for {@link Change} objects
+ *
+ * @param <T> The change object to match
+ */
 public abstract class ChangeMatcher<T> extends TypeSafeMatcher<T> {}
