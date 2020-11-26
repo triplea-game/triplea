cebc132129b38b8aa65d4547e1822526fe7ade02
Trevan Richins <trevan_github@therichins.net>
2020-11-26 11:13:53 -0700

    Rename variables and add comments

--- game-core/src/main/java/games/strategy/engine/data/changefactory/units/BombingUnitDamageChange.java
+++ game-core/src/main/java/games/strategy/engine/data/changefactory/units/BombingUnitDamageChange.java
@@ -17,19 +17,25 @@ import org.triplea.java.collections.IntegerMap;
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class BombingUnitDamageChange extends Change {
   private static final long serialVersionUID = 7093658184880237574L;
-  private final IntegerMap<String> hits;
-  private final IntegerMap<String> undoHits;
+  private final IntegerMap<String> newDamage;
+  private final IntegerMap<String> oldDamage;
   private final Collection<String> territoriesToNotify;
 
+  /**
+   * @param damage The amount of damage that the unit should have. It is NOT the difference of the
+   *     existing damage and the new damage.
+   * @param territoriesToNotify The territories that contain all the units
+   */
   public BombingUnitDamageChange(
-      final IntegerMap<Unit> hits, final Collection<Territory> territoriesToNotify) {
-    this.hits = new IntegerMap<>();
-    this.undoHits = new IntegerMap<>();
-    hits.entrySet()
+      final IntegerMap<Unit> damage, final Collection<Territory> territoriesToNotify) {
+    this.newDamage = new IntegerMap<>();
+    this.oldDamage = new IntegerMap<>();
+    damage
+        .entrySet()
         .forEach(
             entry -> {
-              this.hits.put(entry.getKey().getId().toString(), entry.getValue());
-              this.undoHits.put(entry.getKey().getId().toString(), entry.getKey().getUnitDamage());
+              this.newDamage.put(entry.getKey().getId().toString(), entry.getValue());
+              this.oldDamage.put(entry.getKey().getId().toString(), entry.getKey().getUnitDamage());
             });
     this.territoriesToNotify =
         territoriesToNotify.stream().map(Territory::getName).collect(Collectors.toList());
@@ -37,10 +43,11 @@ public class BombingUnitDamageChange extends Change {
 
   @Override
   protected void perform(final GameData data) {
-    hits.keySet()
+    newDamage
+        .keySet()
         .forEach(
             unitId -> {
-              data.getUnits().get(UUID.fromString(unitId)).setUnitDamage(hits.getInt(unitId));
+              data.getUnits().get(UUID.fromString(unitId)).setUnitDamage(newDamage.getInt(unitId));
             });
     this.territoriesToNotify.forEach(
         territory -> {
@@ -50,16 +57,6 @@ public class BombingUnitDamageChange extends Change {
 
   @Override
   public Change invert() {
-    return new BombingUnitDamageChange(undoHits, hits, territoriesToNotify);
-  }
-
-  @Override
-  public String toString() {
-    return "Bombing unit damage change. Hits:"
-        + hits
-        + " undoHits:"
-        + undoHits
-        + " territories:"
-        + territoriesToNotify;
+    return new BombingUnitDamageChange(oldDamage, newDamage, territoriesToNotify);
   }
 }
