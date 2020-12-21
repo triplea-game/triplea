5609ecacfc08e38ddf952ad1a25c62d42f3c3cd6
DanVanAtta <dhvatta@gmail.com>
2020-12-20 18:45:14 -0800

    Fix integ tests non-determinism, need to set DB data
    It is odd these tests were not failing all the time as we do clear data
    between tests. This update fixes tests that failed consistently when
    run on their own as they were missing a DB-data setup step.

--- lobby-server/src/test/java/org/triplea/modules/moderation/chat/history/GameChatHistoryControllerIntegrationTest.java
+++ lobby-server/src/test/java/org/triplea/modules/moderation/chat/history/GameChatHistoryControllerIntegrationTest.java
@@ -1,11 +1,14 @@
 package org.triplea.modules.moderation.chat.history;
 
+import com.github.database.rider.core.api.dataset.DataSet;
 import java.net.URI;
 import org.junit.jupiter.api.Test;
 import org.triplea.http.client.lobby.moderator.ModeratorChatClient;
 import org.triplea.modules.http.AllowedUserRole;
+import org.triplea.modules.http.LobbyServerTest;
 import org.triplea.modules.http.ProtectedEndpointTest;
 
+@DataSet(value = LobbyServerTest.LOBBY_USER_DATASET, useSequenceFiltering = false)
 class GameChatHistoryControllerIntegrationTest extends ProtectedEndpointTest<ModeratorChatClient> {
 
   GameChatHistoryControllerIntegrationTest(final URI localhost) {
