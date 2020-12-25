2fe60c0626a171c556f4d0f9c63ea7c245bd962a
tripleabuilderbot <tripleabuilderbot@gmail.com>
2020-12-25 20:48:30 +0000

    Auto-Formatting

--- game-core/src/main/java/games/strategy/triplea/ui/UiContext.java
+++ game-core/src/main/java/games/strategy/triplea/ui/UiContext.java
@@ -27,7 +27,6 @@ import java.awt.Window;
 import java.io.File;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
