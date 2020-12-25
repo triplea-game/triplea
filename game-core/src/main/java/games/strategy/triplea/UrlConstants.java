50adbbe66d5654d10625e3780f0609a5496e2505
DanVanAtta <dhvatta@gmail.com>
2020-12-25 12:32:09 -0800

    Fix Url links
    - old downloads page has been removed, merged with 'release notes'
    - rule book link is unused in the game code

--- game-core/src/main/java/games/strategy/triplea/UrlConstants.java
+++ game-core/src/main/java/games/strategy/triplea/UrlConstants.java
@@ -8,7 +8,6 @@ import lombok.experimental.UtilityClass;
 public final class UrlConstants {
   public static final String AXIS_AND_ALLIES_FORUM = "https://www.axisandallies.org/forums";
   public static final String DOWNLOAD_WEBSITE = "https://triplea-game.org/download/";
-  public static final String OLD_DOWNLOADS_WEBSITE = "https://triplea-game.org/old_downloads/";
   public static final String GITHUB_ISSUES = "https://github.com/triplea-game/triplea/issues/new";
   public static final String LICENSE_NOTICE =
       "https://github.com/triplea-game/triplea/blob/master/README.md#license";
@@ -21,7 +20,6 @@ public final class UrlConstants {
   public static final String PAYPAL_DONATE =
       "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=GKZL7598EDZLN";
   public static final String RELEASE_NOTES = "https://triplea-game.org/release_notes/";
-  public static final String RULE_BOOK = "https://triplea-game.org/files/TripleA_RuleBook.pdf";
   public static final String TRIPLEA_FORUM = "https://forums.triplea-game.org/";
   public static final String TRIPLEA_WEBSITE = "https://triplea-game.org/";
   public static final String USER_GUIDE = "https://triplea-game.org/user-guide/";
