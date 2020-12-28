59e08552afae241059a2201b7ad4b439ac3ed776
DanVanAtta <dhvatta@gmail.com>
2020-12-27 21:22:40 -0800

    Remove unused constructor

--- game-core/src/main/java/games/strategy/engine/framework/ui/GameChooserModel.java
+++ game-core/src/main/java/games/strategy/engine/framework/ui/GameChooserModel.java
@@ -1,6 +1,5 @@
 package games.strategy.engine.framework.ui;
 
-import games.strategy.engine.framework.map.file.system.loader.AvailableGamesFileSystemReader;
 import games.strategy.engine.framework.map.file.system.loader.AvailableGamesList;
 import java.util.Optional;
 import java.util.stream.IntStream;
@@ -12,19 +11,10 @@ import lombok.extern.slf4j.Slf4j;
 public final class GameChooserModel extends DefaultListModel<DefaultGameChooserEntry> {
   private static final long serialVersionUID = -2044689419834812524L;
 
-  /**
-   * Initializes a new {@code GameChooserModel} using all available maps installed in the user's
-   * maps folder. This method will block until all maps are parsed and should not be called from the
-   * EDT.
-   */
-  public GameChooserModel() {
-    this(AvailableGamesFileSystemReader.parseMapFiles());
-  }
-
   public GameChooserModel(final AvailableGamesList availableGamesList) {
     availableGamesList.getSortedGameEntries().forEach(this::addElement);
   }
-  
+
   /** Searches for a GameChooserEntry whose gameName matches the input parameter. */
   public Optional<DefaultGameChooserEntry> findByName(final String name) {
     return IntStream.range(0, size())
