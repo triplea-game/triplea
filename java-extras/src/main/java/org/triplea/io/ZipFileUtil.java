56521e51247ba0cf6755d6f77028de351d6ff979
RoiEX <8350879+RoiEXLab@users.noreply.github.com>
2021-01-04 01:16:09 +0100

    Use neat FileSystem API

--- java-extras/src/main/java/org/triplea/io/ZipFileUtil.java
+++ java-extras/src/main/java/org/triplea/io/ZipFileUtil.java
@@ -3,12 +3,11 @@ package org.triplea.io;
 import java.io.File;
 import java.io.IOException;
 import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.net.URLClassLoader;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 import java.util.stream.Collectors;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
@@ -24,21 +23,13 @@ public class ZipFileUtil {
    */
   public List<URI> findXmlFilesInZip(final File zip) {
     try (ZipFile zipFile = new ZipFile(zip);
-        URLClassLoader loader = new URLClassLoader(new URL[] {zip.toURI().toURL()})) {
+        FileSystem fileSystem = FileSystems.newFileSystem(zip.toPath(), null)) {
 
       return zipFile.stream()
           .map(ZipEntry::getName)
           .filter(name -> name.toLowerCase().endsWith(".xml"))
-          .map(loader::getResource)
-          .filter(Objects::nonNull)
-          .map(
-              url -> {
-                try {
-                  return url.toURI();
-                } catch (final URISyntaxException e) {
-                  throw new RuntimeException(e);
-                }
-              })
+          .map(fileSystem::getPath)
+          .map(Path::toUri)
           .collect(Collectors.toList());
     } catch (final IOException e) {
       log.error("Error reading zip file in: " + zip.getAbsolutePath(), e);
