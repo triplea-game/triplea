93d3034327d6ef8a8cd94dfe067cfbac80b49c50
DanVanAtta <dhvatta@gmail.com>
2021-01-17 19:55:21 -0800

    Update test case

--- maps-server/src/test/java/org/triplea/maps/listing/MapListingDaoTest.java
+++ maps-server/src/test/java/org/triplea/maps/listing/MapListingDaoTest.java
@@ -2,11 +2,9 @@ package org.triplea.maps.listing;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
-import static org.hamcrest.collection.IsEmptyCollection.empty;
 import static org.hamcrest.core.Is.is;
 
 import com.github.database.rider.core.api.dataset.DataSet;
-import java.util.Set;
 import org.junit.jupiter.api.Test;
 import org.triplea.maps.server.http.MapServerTest;
 
@@ -25,12 +23,11 @@ class MapListingDaoTest extends MapServerTest {
     final var results = mapListingDao.fetchMapListings();
 
     assertThat(results, hasSize(1));
-    final var mapDownloadListing = results.get(0).toMapDownloadListing(Set.of());
+    final var mapDownloadListing = results.get(0).toMapDownloadListing();
     assertThat(mapDownloadListing.getMapCategory(), is("category_name"));
     assertThat(mapDownloadListing.getDescription(), is("description of the map"));
     assertThat(mapDownloadListing.getPreviewImage(), is("http://map-thumbnail-url"));
     assertThat(mapDownloadListing.getMapName(), is("map-name"));
-    assertThat(mapDownloadListing.getMapsSkins(), is(empty()));
     assertThat(mapDownloadListing.getUrl(), is("http://map-download-url"));
     assertThat(mapDownloadListing.getVersion(), is("100"));
   }
