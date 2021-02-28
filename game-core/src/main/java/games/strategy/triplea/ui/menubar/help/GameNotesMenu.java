6bfc16df412daa13ba7fb929e70ef0abe41730f7
DanVanAtta <dhvatta@gmail.com>
2021-02-27 17:41:41 -0800

    Remove game notes action tab
    Several discussions have happened in forums, generall agreed that the
    game notes in the action tabs is not particularly useful. To keep things
    simpler, this update remove the game notes tab.

--- game-core/src/main/java/games/strategy/triplea/ui/menubar/help/GameNotesMenu.java
+++ game-core/src/main/java/games/strategy/triplea/ui/menubar/help/GameNotesMenu.java
@@ -1,11 +1,16 @@
 package games.strategy.triplea.ui.menubar.help;
 
-import games.strategy.triplea.ui.NotesPanel;
+import games.strategy.triplea.ui.UiContext;
+import java.awt.Color;
 import javax.swing.Action;
+import javax.swing.JComponent;
 import javax.swing.JDialog;
+import javax.swing.JEditorPane;
 import javax.swing.SwingUtilities;
 import lombok.experimental.UtilityClass;
 import org.triplea.swing.SwingAction;
+import org.triplea.swing.SwingComponents;
+import org.triplea.util.LocalizeHtml;
 
 @UtilityClass
 class GameNotesMenu {
@@ -18,7 +23,7 @@ class GameNotesMenu {
             SwingUtilities.invokeLater(
                 () -> {
                   final JDialog dialog =
-                      InformationDialog.createDialog(new NotesPanel(gameNotes), gameNotesTitle);
+                      InformationDialog.createDialog(notesPanel(gameNotes), gameNotesTitle);
                   if (dialog.getWidth() < 400) {
                     dialog.setSize(400, dialog.getHeight());
                   }
@@ -33,5 +38,17 @@ class GameNotesMenu {
                   }
                   dialog.setVisible(true);
                 }));
+  }
+
+  private static JComponent notesPanel(final String gameNotes) {
+    final String localizedHtml =
+        LocalizeHtml.localizeImgLinksInHtml(
+            gameNotes.trim(), UiContext.getResourceLoader().getMapLocation());
+
+    final JEditorPane gameNotesPane = new JEditorPane("text/html", localizedHtml);
+    gameNotesPane.setEditable(false);
+    gameNotesPane.setForeground(Color.BLACK);
+    gameNotesPane.setCaretPosition(0);
+    return SwingComponents.newJScrollPane(gameNotesPane);
   }
 }
