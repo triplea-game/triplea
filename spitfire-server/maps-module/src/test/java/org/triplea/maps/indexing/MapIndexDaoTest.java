fcdbffc9df7217a681bf80d4f983b6ab3224b2b5
Dan Van Atta <dhvatta@gmail.com>
2021-06-19 21:33:51 -0700

    Fix noisy DB rider log warning (#9380)
    Log message observed when running tests:
    
    WARNING: Could not find script engine by name 'http'
    
    This is caused seemingly by there being yaml string values
    beginning with "http:"
    
    As a hack around this we can change the test data to be "http-"
    and satisfy the column constraints of begins with 'http' and
    aso avoid an 'http:' prefix that triggers a noisy logging
    message.

--- spitfire-server/maps-module/src/test/java/org/triplea/maps/indexing/MapIndexDaoTest.java
+++ spitfire-server/maps-module/src/test/java/org/triplea/maps/indexing/MapIndexDaoTest.java
@@ -25,7 +25,7 @@ class MapIndexDaoTest {
     mapIndexDao.upsert(
         MapIndexingResult.builder()
             .mapName("map-name-3")
-            .mapRepoUri("http://map-repo-url-3")
+            .mapRepoUri("http-map-repo-url-3")
             .lastCommitDate(LocalDateTime.of(2000, 1, 12, 23, 59).toInstant(ZoneOffset.UTC))
             .build());
   }
@@ -36,7 +36,7 @@ class MapIndexDaoTest {
     mapIndexDao.upsert(
         MapIndexingResult.builder()
             .mapName("map-name-updated")
-            .mapRepoUri("http://map-repo-url-2")
+            .mapRepoUri("http-map-repo-url-2")
             .lastCommitDate(LocalDateTime.of(2000, 1, 12, 23, 59).toInstant(ZoneOffset.UTC))
             .build());
   }
@@ -47,7 +47,7 @@ class MapIndexDaoTest {
     mapIndexDao.upsert(
         MapIndexingResult.builder()
             .mapName("map-name-2")
-            .mapRepoUri("http://map-repo-url-2")
+            .mapRepoUri("http-map-repo-url-2")
             .lastCommitDate(LocalDateTime.of(2016, 1, 1, 23, 59, 20).toInstant(ZoneOffset.UTC))
             .build());
   }
@@ -55,6 +55,6 @@ class MapIndexDaoTest {
   @Test
   @ExpectedDataSet("expected/map_index_post_remove.yml")
   void removeMaps() {
-    mapIndexDao.removeMapsNotIn(List.of("http://map-repo-url"));
+    mapIndexDao.removeMapsNotIn(List.of("http-map-repo-url"));
   }
 }
