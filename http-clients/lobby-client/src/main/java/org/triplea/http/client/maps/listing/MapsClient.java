3ef0f30a87b7d9b8d9d43bbe88ba7b19bd0beae3
DanVanAtta <dhvatta@gmail.com>
2021-09-04 13:03:12 -0700

    Fix checkstyle - static variable in wrong order

--- http-clients/lobby-client/src/main/java/org/triplea/http/client/maps/listing/MapsClient.java
+++ http-clients/lobby-client/src/main/java/org/triplea/http/client/maps/listing/MapsClient.java
@@ -9,8 +9,8 @@ import org.triplea.http.client.HttpClient;
  * Http client to communicate with the maps server and get a listing of maps available for download.
  */
 public class MapsClient {
-  private final MapsFeignClient mapsFeignClient;
   public static final String MAPS_LISTING_PATH = "/maps/listing";
+  private final MapsFeignClient mapsFeignClient;
 
   public MapsClient(final URI mapsServerUri) {
     mapsFeignClient = new HttpClient<>(MapsFeignClient.class, mapsServerUri).get();
