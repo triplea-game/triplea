b380d2308ad70f282b58dbc820a7d0df137f30cb
Dan Van Atta <dhvatta@gmail.com>
2021-10-04 18:41:37 -0700

    Use current prod2-lobby as default (#9682)
    prod.triplea-game.org is not fully set up.
    Update lobby prod host URL to the current DNS alias
    in the meantime.

--- game-app/game-core/src/main/java/games/strategy/triplea/UrlConstants.java
+++ game-app/game-core/src/main/java/games/strategy/triplea/UrlConstants.java
@@ -22,5 +22,5 @@ public final class UrlConstants {
   public static final String USER_GUIDE = "https://triplea-game.org/user-guide/";
   public static final String MARTI_REGISTRATION = "https://dice.marti.triplea-game.org/";
   public static final String PRERELEASE_LOBBY = "https://prerelease.triplea-game.org";
-  public static final String PROD_LOBBY = "https://prod.triplea-game.org";
+  public static final String PROD_LOBBY = "https://prod2-lobby.triplea-game.org";
 }
