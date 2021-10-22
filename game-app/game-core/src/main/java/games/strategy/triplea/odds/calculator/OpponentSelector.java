d48c1b26a7b2282e54002559b79448d3bf9969cc
Emmanuel Perlow <86185690+emmanuel-perlow@users.noreply.github.com>
2021-10-22 14:36:23 +0000

    OpponentSelector: Remove unused imports

--- game-app/game-core/src/main/java/games/strategy/triplea/odds/calculator/OpponentSelector.java
+++ game-app/game-core/src/main/java/games/strategy/triplea/odds/calculator/OpponentSelector.java
@@ -9,7 +9,6 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
