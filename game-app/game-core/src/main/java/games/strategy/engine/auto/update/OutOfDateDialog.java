993409d5e94919c0906baf7f8245a6fb7ef52452
DanVanAtta <dhvatta@gmail.com>
2021-10-24 19:24:13 -0700

    Update out-of-date dialog, convert 'download link' to be a button.
    Instead of a dialog with links for download, release notes, and an 'ok' button;
    the dialog now has the release notes link and a button to 'download now'
    and a 'remind me later' button.

--- game-app/game-core/src/main/java/games/strategy/engine/auto/update/OutOfDateDialog.java
+++ game-app/game-core/src/main/java/games/strategy/engine/auto/update/OutOfDateDialog.java
@@ -2,37 +2,36 @@ package games.strategy.engine.auto.update;
 
 import games.strategy.triplea.UrlConstants;
 import javax.swing.JOptionPane;
-import javax.swing.SwingUtilities;
 import lombok.experimental.UtilityClass;
-import org.triplea.swing.EventThreadJOptionPane;
+import org.triplea.awt.OpenFileUtility;
 import org.triplea.swing.JEditorPaneWithClickableLinks;
 import org.triplea.util.Version;
 
 @UtilityClass
 class OutOfDateDialog {
   static void showOutOfDateComponent(final Version latestVersionOut) {
-    SwingUtilities.invokeLater(
-        () ->
-            EventThreadJOptionPane.showMessageDialog(
-                null,
-                new JEditorPaneWithClickableLinks(getOutOfDateMessage(latestVersionOut)),
-                "Update TripleA!",
-                JOptionPane.INFORMATION_MESSAGE));
+    final int result =
+        JOptionPane.showOptionDialog(
+            null,
+            new JEditorPaneWithClickableLinks(getOutOfDateMessage(latestVersionOut)),
+            "TripleA is out of date!",
+            JOptionPane.YES_NO_OPTION,
+            JOptionPane.PLAIN_MESSAGE,
+            null,
+            new Object[] {"Download Now", "Remind Me Later"},
+            null);
+
+    if (result == JOptionPane.YES_OPTION) {
+      OpenFileUtility.openUrl(UrlConstants.DOWNLOAD_WEBSITE);
+    }
   }
 
   private static String getOutOfDateMessage(final Version latestVersionOut) {
     return String.format(
         "<html>"
             + "<h2>TripleA %s is available!</h2>"
-            + "<br><br>Click to download: <a class=\"external\" href=\"%s\">%s</a>"
-            + "</a>"
-            + "<br><br>"
-            + "Release notes:<br /><a class=\"external\" href=\"%s\">%s</a><br />"
+            + "<center><a class=\"external\" href=\"%s\">Release Notes</a></center>"
             + "</html>",
-        latestVersionOut,
-        UrlConstants.DOWNLOAD_WEBSITE,
-        UrlConstants.DOWNLOAD_WEBSITE,
-        UrlConstants.RELEASE_NOTES,
-        UrlConstants.RELEASE_NOTES);
+        latestVersionOut, UrlConstants.RELEASE_NOTES);
   }
 }
