8121707c445fb2064ac9e8278bd077894cd29174
DanVanAtta <dhvatta@gmail.com>
2021-11-03 23:54:31 -0700

    Clean up, remove unused import

--- game-app/game-core/src/main/java/games/strategy/triplea/odds/calculator/BattleCalculatorPanel.java
+++ game-app/game-core/src/main/java/games/strategy/triplea/odds/calculator/BattleCalculatorPanel.java
@@ -50,7 +50,6 @@ import javax.swing.JScrollPane;
 import javax.swing.ListSelectionModel;
 import javax.swing.ScrollPaneConstants;
 import javax.swing.SwingUtilities;
-import lombok.Builder;
 import lombok.extern.slf4j.Slf4j;
 import org.triplea.java.ThreadRunner;
 import org.triplea.java.collections.CollectionUtils;
