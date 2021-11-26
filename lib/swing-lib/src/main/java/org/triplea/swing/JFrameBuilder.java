triplea-game/triplea

commit 7e4884cb7241e23ba2e7fa1b40caf94c06862aa1

Add 'maps' tab to moderator toolbox where map tags can be managed (#9554)

lib/swing-lib/src/main/java/org/triplea/swing/JFrameBuilder.java
@@ -26,6 +26,13 @@ import org.triplea.swing.key.binding.SwingKeyBinding;
  * </ul>
  */
 public class JFrameBuilder {
+  enum CloseBehavior {
+    DISPOSE,
+    EXIT
+  }
+
+  private CloseBehavior closeBehavior;
+
   private final Collection<Function<JFrame, Component>> children = new ArrayList<>();
   private boolean escapeClosesWindow;
   private boolean alwaysOnTop;
@@ -71,7 +78,6 @@ public class JFrameBuilder {
 
     frame.setMinimumSize(new Dimension(minWidth, minHeight));
     Optional.ofNullable(iconImage).ifPresent(frame::setIconImage);
-    frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
     if ((width > 0) || (height > 0)) {
       frame.setPreferredSize(new Dimension(width, height));
@@ -86,6 +92,14 @@ public class JFrameBuilder {
     }
     Optional.ofNullable(parent).ifPresent(frame::setLocationRelativeTo);
     Optional.ofNullable(layoutManager).ifPresent(frame.getContentPane()::setLayout);
+
+    if (closeBehavior != null) {
+      if (closeBehavior == CloseBehavior.DISPOSE) {
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+      } else {
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      }
+    }
 
     children.stream().map(component -> component.apply(frame)).forEach(frame::add);
 
@@ -176,6 +190,16 @@ public class JFrameBuilder {
 
   public JFrameBuilder visible(final boolean visible) {
     this.visible = visible;
+    return this;
+  }
+
+  public JFrameBuilder exitOnClose() {
+    this.closeBehavior = CloseBehavior.EXIT;
+    return this;
+  }
+
+  public JFrameBuilder disposeOnClose() {
+    this.closeBehavior = CloseBehavior.DISPOSE;
     return this;
   }
 }

.build/unit-testing/run
@@ -3,13 +3,24 @@
 set -u
 
 function main() {
-   set -x
   .build/setup-database
-  ./gradlew --quiet test jacocoTestReport --exclude-task :smoke-testing:test
+  ./gradlew test jacocoTestReport \
+     --exclude-task :smoke-testing:test \
+     --exclude-task :spitfire-server:lobby-module:test \
+     --exclude-task :spitfire-server:maps-module:test \
+     --exclude-task :spitfire-server:dropwizard-server:test
   result="$?"
-  set +x
-  # Run coverage reports only if tests passed
 
+  if [ "$result" = 0 ]; then
+  ./gradlew \
+     :spitfire-server:lobby-module:test \
+     :spitfire-server:maps-module:test \
+     :spitfire-server:dropwizard-server:test
+
+     result="$?"
+  fi
+
+  # Run coverage reports only if tests passed
   if [ "$result" = 0 ]; then
     echo "Uploading test coverage reports"
     # upload coverage report to codecov - https://github.com/codecov/example-gradle

game-app/game-core/src/main/java/games/strategy/engine/lobby/moderator/toolbox/tabs/TabFactory.java
@@ -5,6 +5,8 @@ import games.strategy.engine.lobby.moderator.toolbox.tabs.bad.words.BadWordsTab;
 import games.strategy.engine.lobby.moderator.toolbox.tabs.banned.names.BannedUsernamesTab;
 import games.strategy.engine.lobby.moderator.toolbox.tabs.banned.users.BannedUsersTab;
 import games.strategy.engine.lobby.moderator.toolbox.tabs.event.log.EventLogTab;
+import games.strategy.engine.lobby.moderator.toolbox.tabs.maps.MapsTab;
+import games.strategy.engine.lobby.moderator.toolbox.tabs.maps.MapsTabModel;
 import games.strategy.engine.lobby.moderator.toolbox.tabs.moderators.ModeratorsTab;
 import java.awt.Component;
 import javax.annotation.Nonnull;
@@ -28,6 +30,7 @@ public final class TabFactory {
         .addTab("Banned Users", buildBannedUsersTab())
         .addTab("Event Log", buildEventLogTab())
         .addTab("Moderators", buildModeratorsTab())
+        .addTab("Maps", buildMapsTab())
         .build();
   }
 
@@ -57,6 +60,21 @@ public final class TabFactory {
     return new ModeratorsTab(
             frame, httpModeratorToolboxClient.getToolboxModeratorManagementClient())
         .get();
+  }
+
+  private Component buildMapsTab() {
+    return MapsTab.builder()
+        .parentWindowHeight(frame.getWidth())
+        .mapsTabModel(buildMapsTabModel())
+        .build()
+        .get();
+  }
+
+  private MapsTabModel buildMapsTabModel() {
+    return MapsTabModel.builder()
+        .mapsClient(httpModeratorToolboxClient.getMapsClient())
+        .mapTagAdminClient(httpModeratorToolboxClient.getMapTagAdminClient())
+        .build();
   }
 
   private Component buildEventLogTab() {

game-app/game-core/src/main/java/games/strategy/engine/lobby/moderator/toolbox/tabs/maps/MapsTab.java
@@ -1,0 +1,129 @@
+package games.strategy.engine.lobby.moderator.toolbox.tabs.maps;
+
+import java.awt.Component;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import javax.swing.Box;
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JLabel;
+import javax.swing.JTextArea;
+import javax.swing.SwingConstants;
+import javax.swing.SwingUtilities;
+import lombok.Builder;
+import org.triplea.http.client.maps.listing.MapDownloadItem;
+import org.triplea.http.client.maps.tag.admin.MapTagMetaData;
+import org.triplea.java.ThreadRunner;
+import org.triplea.swing.JComboBoxBuilder;
+import org.triplea.swing.JSplitPaneBuilder;
+import org.triplea.swing.JTextAreaBuilder;
+import org.triplea.swing.SwingComponents;
+import org.triplea.swing.jpanel.JPanelBuilder;
+
+/** The maps tab contains a listing of all maps known to the maps server. */
+public class MapsTab implements MapsTabUi, Supplier<Component> {
+  private final MapsTabModel mapsTabModel;
+  private final Integer parentWindowHeight;
+  private final JTextArea messagesTextArea = JTextAreaBuilder.builder().rows(3).readOnly().build();
+
+  private final JButton helpButton =
+      SwingComponents.helpButton(
+          "Map Tagging Help",
+          "You can change map tags by selecting different values in the drop downs.<br>"
+              + "The server is updated immediately after selecting a new value.<br>"
+              + "Map tags are displayed in the download maps window.");
+
+  @Builder
+  public MapsTab(final MapsTabModel mapsTabModel, final Integer parentWindowHeight) {
+    this.mapsTabModel = mapsTabModel;
+    mapsTabModel.setMapsTabUi(this);
+    this.parentWindowHeight = parentWindowHeight;
+  }
+
+  @Override
+  public Component get() {
+    messagesTextArea.setVisible(false);
+    return new JPanelBuilder()
+        .borderLayout()
+        .addCenter(
+            JSplitPaneBuilder.builder()
+                .dividerLocation(parentWindowHeight - 120)
+                .giveExtraSpaceToTopAndLeftPanes()
+                .addTop(SwingComponents.newJScrollPane(mapTagTable()))
+                .addBottom(SwingComponents.newJScrollPane(messagesTextArea))
+                .build())
+        .addNorth(
+            new JPanelBuilder() //
+                .boxLayoutHorizontal()
+                .add(helpButton)
+                .add(Box.createGlue())
+                .build())
+        .build();
+  }
+
+  /**
+   * Returns a table displaying map name on the left and tag values across the row with text boxes
+   * and dropdowns to allow those values to be updated. Updates are sent to the backend after being
+   * selected from the drop down.
+   */
+  private JComponent mapTagTable() {
+    final List<MapTagMetaData> tagMetaData = mapsTabModel.fetchAllowedMapTagValues();
+
+    final List<MapDownloadItem> maps =
+        mapsTabModel.fetchMapsList().stream()
+            .sorted(Comparator.comparing(MapDownloadItem::getMapName))
+            .collect(Collectors.toList());
+
+    // two columns for every tag (name + value)
+    // plus one column for a map name label
+    // plus one column for save button
+    final int columnCount = (tagMetaData.size() * 2) + 1 + 1;
+    return new JPanelBuilder()
+        .add(
+            new JPanelBuilder()
+                .gridLayout(maps.size(), columnCount)
+                .addAll(
+                    maps.stream()
+                        .map(downloadItem -> buildMapRow(downloadItem, tagMetaData))
+                        .flatMap(Collection::stream)
+                        .collect(Collectors.toList()))
+                .build())
+        .build();
+  }
+
+  /** Builds a single map row with drop downs to change tag values for that map. */
+  private List<Component> buildMapRow(
+      final MapDownloadItem mapDownloadItem, final List<MapTagMetaData> mapTagMetaData) {
+
+    final List<Component> components = new ArrayList<>();
+    components.add(new JLabel(mapDownloadItem.getMapName()));
+
+    for (final MapTagMetaData mapTagMetaDataItem : mapTagMetaData) {
+      components.add(new JLabel(mapTagMetaDataItem.getTagName() + ":", SwingConstants.RIGHT));
+
+      components.add(
+          JComboBoxBuilder.builder()
+              .selectedItem(mapDownloadItem.getTagValue(mapTagMetaDataItem.getTagName()))
+              .items(mapTagMetaDataItem.getAllowedValues())
+              .enableAutoComplete()
+              .itemSelectedAction(
+                  selectedItem ->
+                      ThreadRunner.runInNewThread(
+                          () ->
+                              mapsTabModel.updateMapTag(
+                                  mapDownloadItem.getMapName(), mapTagMetaDataItem, selectedItem)))
+              .build());
+    }
+    return components;
+  }
+
+  @Override
+  public void showMessage(final String message) {
+    messagesTextArea.setVisible(true);
+    SwingUtilities.invokeLater(() -> messagesTextArea.append(message + "\n"));
+  }
+}

game-app/game-core/src/main/java/games/strategy/engine/lobby/moderator/toolbox/tabs/maps/MapsTabModel.java
@@ -1,0 +1,60 @@
+package games.strategy.engine.lobby.moderator.toolbox.tabs.maps;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import javax.annotation.Nonnull;
+import lombok.Builder;
+import lombok.Setter;
+import org.triplea.http.client.GenericServerResponse;
+import org.triplea.http.client.maps.listing.MapDownloadItem;
+import org.triplea.http.client.maps.listing.MapsClient;
+import org.triplea.http.client.maps.tag.admin.MapTagAdminClient;
+import org.triplea.http.client.maps.tag.admin.MapTagMetaData;
+import org.triplea.http.client.maps.tag.admin.UpdateMapTagRequest;
+
+public class MapsTabModel {
+  @Nonnull private final MapsClient mapsClient;
+  @Nonnull private final MapTagAdminClient mapTagAdminClient;
+
+  @Setter private MapsTabUi mapsTabUi;
+
+  @Builder
+  public MapsTabModel(
+      @Nonnull final MapsClient mapsClient, @Nonnull final MapTagAdminClient mapTagAdminClient) {
+    this.mapsClient = mapsClient;
+    this.mapTagAdminClient = mapTagAdminClient;
+  }
+
+  /** Queries server for the list of known maps. */
+  List<MapDownloadItem> fetchMapsList() {
+    return mapsClient.fetchMapDownloads();
+  }
+
+  /** Queries server for the set of available tag names mapped to their allowed values. */
+  List<MapTagMetaData> fetchAllowedMapTagValues() {
+    return mapTagAdminClient.fetchTagsMetaData().stream()
+        .sorted(Comparator.comparing(MapTagMetaData::getDisplayOrder))
+        .collect(Collectors.toList());
+  }
+
+  /** Sends a request to server to update a map tag to a new value. */
+  void updateMapTag(
+      final String mapName, final MapTagMetaData mapTagMetaData, final String tagValue) {
+    try {
+      final GenericServerResponse serverResponse =
+          mapTagAdminClient.updateMapTag(
+              UpdateMapTagRequest.builder()
+                  .mapName(mapName)
+                  .tagName(mapTagMetaData.getTagName())
+                  .newTagValue(tagValue)
+                  .build());
+      mapsTabUi.showMessage(
+          String.format(
+              "%s: %s",
+              serverResponse.isSuccess() ? "Success" : "Error", serverResponse.getMessage()));
+    } catch (final Exception e) {
+      mapsTabUi.showMessage(String.format("Error: %s", e));
+    }
+  }
+}

game-app/game-core/src/main/java/games/strategy/engine/lobby/moderator/toolbox/tabs/maps/MapsTabUi.java
@@ -1,0 +1,5 @@
+package games.strategy.engine.lobby.moderator.toolbox.tabs.maps;
+
+public interface MapsTabUi {
+  void showMessage(String message);
+}

http-clients/feign-common/src/main/java/org/triplea/http/client/GenericServerResponse.java
@@ -1,0 +1,26 @@
+package org.triplea.http.client;
+
+import javax.annotation.Nullable;
+import lombok.Builder;
+import lombok.Value;
+
+/**
+ * A common response object where the server response is simply a 'yes/no' confirmation with a
+ * confirmation message or an error message to the user.
+ */
+@Value
+@Builder
+public class GenericServerResponse {
+  public static final GenericServerResponse SUCCCESS =
+      GenericServerResponse.builder().success(true).build();
+
+  /** True if the requested operation was successful. */
+  boolean success;
+
+  /**
+   * May be null, any message the server wishes to return, eg: if success then this value could be
+   * null or a confirmation message, otherwise if not success, then the value should be an error
+   * message back to the user.
+   */
+  @Nullable String message;
+}

http-clients/lobby-client/src/main/java/org/triplea/http/client/lobby/moderator/toolbox/HttpModeratorToolboxClient.java
@@ -9,6 +9,8 @@ import org.triplea.http.client.lobby.moderator.toolbox.log.ToolboxAccessLogClien
 import org.triplea.http.client.lobby.moderator.toolbox.log.ToolboxEventLogClient;
 import org.triplea.http.client.lobby.moderator.toolbox.management.ToolboxModeratorManagementClient;
 import org.triplea.http.client.lobby.moderator.toolbox.words.ToolboxBadWordsClient;
+import org.triplea.http.client.maps.listing.MapsClient;
+import org.triplea.http.client.maps.tag.admin.MapTagAdminClient;
 
 @Getter
 public class HttpModeratorToolboxClient {
@@ -19,6 +21,8 @@ public class HttpModeratorToolboxClient {
   private final ToolboxModeratorManagementClient toolboxModeratorManagementClient;
   private final ToolboxBadWordsClient toolboxBadWordsClient;
   private final ToolboxEventLogClient toolboxEventLogClient;
+  private final MapsClient mapsClient;
+  private final MapTagAdminClient mapTagAdminClient;
 
   private HttpModeratorToolboxClient(final URI lobbyUri, final ApiKey apiKey) {
     toolboxAccessLogClient = ToolboxAccessLogClient.newClient(lobbyUri, apiKey);
@@ -27,6 +31,8 @@ public class HttpModeratorToolboxClient {
     toolboxModeratorManagementClient = ToolboxModeratorManagementClient.newClient(lobbyUri, apiKey);
     toolboxBadWordsClient = ToolboxBadWordsClient.newClient(lobbyUri, apiKey);
     toolboxEventLogClient = ToolboxEventLogClient.newClient(lobbyUri, apiKey);
+    mapsClient = new MapsClient(lobbyUri);
+    mapTagAdminClient = MapTagAdminClient.newClient(lobbyUri, apiKey);
   }
 
   public static HttpModeratorToolboxClient newClient(final URI lobbyUri, final ApiKey apiKey) {

commit b977c056f2f5847b449b7601e438d0fc3c76c292

Fix: by default dispose JFrame
Fixes a problem introduced in: 7e4884cb7241e23ba2e7fa1b40caf94c06862aa1
This update changes JFrameBuilder to by default dispose on close.
Before we would only dispose on close if explicitly set, this easily
results in JFrames not being disposed which causes the application
to remain open when these 'frames' are closed

lib/swing-lib/src/main/java/org/triplea/swing/JFrameBuilder.java
@@ -31,7 +31,7 @@ public class JFrameBuilder {
     EXIT
   }
 
-  private CloseBehavior closeBehavior;
+  private CloseBehavior closeBehavior = CloseBehavior.DISPOSE;
 
   private final Collection<Function<JFrame, Component>> children = new ArrayList<>();
   private boolean escapeClosesWindow;
@@ -93,12 +93,10 @@ public class JFrameBuilder {
     Optional.ofNullable(parent).ifPresent(frame::setLocationRelativeTo);
     Optional.ofNullable(layoutManager).ifPresent(frame.getContentPane()::setLayout);
 
-    if (closeBehavior != null) {
-      if (closeBehavior == CloseBehavior.DISPOSE) {
-        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-      } else {
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      }
+    if (closeBehavior == CloseBehavior.EXIT) {
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    } else {
+      frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
     }
 
     children.stream().map(component -> component.apply(frame)).forEach(frame::add);