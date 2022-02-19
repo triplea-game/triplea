29b0385d513988c4e8f9aeff6b53690085499f6c
bacrossland <bacrossland@gmail.com>
2022-02-18 22:50:25 -0600

    Fixing more linting issues

--- http-clients/feign-common/src/test/java/org/triplea/http/client/GenericServerResponseTest.java
+++ http-clients/feign-common/src/test/java/org/triplea/http/client/GenericServerResponseTest.java
@@ -1,30 +1,33 @@
 package org.triplea.http.client;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
 
 class GenericServerResponseTest {
 
-    @Test
-    void builder() {
-        GenericServerResponse genericServerResponse = GenericServerResponse.builder().build();
-        assertFalse(genericServerResponse.isSuccess());
-        assertNull(genericServerResponse.getMessage());
-    }
+  @Test
+  void builder() {
+    GenericServerResponse genericServerResponse = GenericServerResponse.builder().build();
+    assertFalse(genericServerResponse.isSuccess());
+    assertNull(genericServerResponse.getMessage());
+  }
 
-    @Test
-    void isSuccess() {
-        GenericServerResponse genericServerResponseTrue = GenericServerResponse.builder().success(true).build();
-        GenericServerResponse genericServerResponseFalse = GenericServerResponse.builder().success(false).build();
-        assertTrue(genericServerResponseTrue.isSuccess());
-        assertFalse(genericServerResponseFalse.isSuccess());
-        assertEquals(GenericServerResponse.SUCCESS, genericServerResponseTrue);
-    }
+  @Test
+  void isSuccess() {
+    GenericServerResponse genericServerResponseTrue =
+        GenericServerResponse.builder().success(true).build();
+    GenericServerResponse genericServerResponseFalse =
+        GenericServerResponse.builder().success(false).build();
+    assertTrue(genericServerResponseTrue.isSuccess());
+    assertFalse(genericServerResponseFalse.isSuccess());
+    assertEquals(GenericServerResponse.SUCCESS, genericServerResponseTrue);
+  }
 
-    @Test
-    void getMessage() {
-        GenericServerResponse genericServerResponse = GenericServerResponse.builder().message("test").build();
-        assertEquals(genericServerResponse.getMessage(), "test");
-    }
+  @Test
+  void getMessage() {
+    GenericServerResponse genericServerResponse =
+        GenericServerResponse.builder().message("test").build();
+    assertEquals(genericServerResponse.getMessage(), "test");
+  }
 }
