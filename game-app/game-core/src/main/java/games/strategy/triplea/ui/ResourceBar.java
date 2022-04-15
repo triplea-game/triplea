d3044cb5ddc5366f4cc681a24575c98fb70ce7e6
asvitkine <alexei.svitkine@gmail.com>
2022-04-15 10:24:17 -0400

    Fix long delay when using "undo all" with lots of moves.

--- game-app/game-core/src/main/java/games/strategy/triplea/ui/ResourceBar.java
+++ game-app/game-core/src/main/java/games/strategy/triplea/ui/ResourceBar.java
@@ -28,6 +28,7 @@ public class ResourceBar extends JPanel implements GameDataChangeListener {
   private final GameData gameData;
   private final UiContext uiContext;
   private final List<ResourceStat> resourceStats = new ArrayList<>();
+  private volatile boolean needsUpdate;
 
   public ResourceBar(final GameData data, final UiContext uiContext) {
     this.gameData = data;
@@ -53,51 +54,61 @@ public class ResourceBar extends JPanel implements GameDataChangeListener {
 
   @Override
   public void gameDataChanged(final Change change) {
-    gameData.acquireReadLock();
-    try {
-      final GamePlayer player = gameData.getSequence().getStep().getPlayerId();
-      if (player != null) {
-        final IntegerMap<Resource> resourceIncomes =
-            AbstractEndTurnDelegate.findEstimatedIncome(player, gameData);
-        SwingUtilities.invokeLater(
-            () -> {
-              this.removeAll();
-              int count = 0;
-              for (final ResourceStat resourceStat : resourceStats) {
-                final Resource resource = resourceStat.resource;
-                if (!resource.isDisplayedFor(player)) {
-                  continue;
-                }
-                final double quantity =
-                    resourceStat.getValue(player, gameData, uiContext.getMapData());
-                final StringBuilder text =
-                    new StringBuilder(IStat.DECIMAL_FORMAT.format(quantity) + " (");
-                if (resourceIncomes.getInt(resource) >= 0) {
-                  text.append("+");
-                }
-                text.append(resourceIncomes.getInt(resource)).append(")");
-                final JLabel label =
-                    uiContext.getResourceImageFactory().getLabel(resource, text.toString());
-                label.setBorder(BorderFactory.createEmptyBorder(0, 10, 0, 10));
-                add(
-                    label,
-                    new GridBagConstraints(
-                        count++,
-                        0,
-                        1,
-                        1,
-                        0,
-                        1,
-                        GridBagConstraints.WEST,
-                        GridBagConstraints.BOTH,
-                        new Insets(0, 0, 0, 0),
-                        0,
-                        0));
-              }
-            });
-      }
-    } finally {
-      gameData.releaseReadLock();
-    }
+    needsUpdate = true;
+    SwingUtilities.invokeLater(
+        () -> {
+          // When there are multiple gameDataChanged() notifications in a row, for example
+          // from an "undo all" action, no need to do this repeatedly. This will just run
+          // once if this code runs after all the notifications have been received.
+          if (needsUpdate) {
+            return;
+          }
+          final GamePlayer player;
+          final IntegerMap<Resource> resourceIncomes;
+          try {
+            gameData.acquireReadLock();
+            player = gameData.getSequence().getStep().getPlayerId();
+            if (player == null) {
+              return;
+            }
+            resourceIncomes = AbstractEndTurnDelegate.findEstimatedIncome(player, gameData);
+          } finally {
+            gameData.releaseReadLock();
+            needsUpdate = false;
+          }
+
+          this.removeAll();
+          int count = 0;
+          for (final ResourceStat resourceStat : resourceStats) {
+            final Resource resource = resourceStat.resource;
+            if (!resource.isDisplayedFor(player)) {
+              continue;
+            }
+            final double quantity = resourceStat.getValue(player, gameData, uiContext.getMapData());
+            final StringBuilder text =
+                new StringBuilder(IStat.DECIMAL_FORMAT.format(quantity) + " (");
+            if (resourceIncomes.getInt(resource) >= 0) {
+              text.append("+");
+            }
+            text.append(resourceIncomes.getInt(resource)).append(")");
+            final JLabel label =
+                uiContext.getResourceImageFactory().getLabel(resource, text.toString());
+            label.setBorder(BorderFactory.createEmptyBorder(0, 10, 0, 10));
+            add(
+                label,
+                new GridBagConstraints(
+                    count++,
+                    0,
+                    1,
+                    1,
+                    0,
+                    1,
+                    GridBagConstraints.WEST,
+                    GridBagConstraints.BOTH,
+                    new Insets(0, 0, 0, 0),
+                    0,
+                    0));
+          }
+        });
   }
 }
