triplea-game/triplea

commit ab181de80af6ab774a066b41d315753537bbc3a1

Fix text overlap in air transport load dialog. (#10285)
* Fix text overlap in air transport load dialog.

Also switch to a builder for grid bag constraints to improve readability.

game-app/game-core/src/main/java/games/strategy/triplea/ui/UnitChooser.java
@@ -37,6 +37,9 @@ import javax.swing.JTextArea;
 import lombok.experimental.UtilityClass;
 import org.triplea.java.Postconditions;
 import org.triplea.java.collections.IntegerMap;
+import org.triplea.swing.jpanel.GridBagConstraintsAnchor;
+import org.triplea.swing.jpanel.GridBagConstraintsBuilder;
+import org.triplea.swing.jpanel.GridBagConstraintsFill;
 
 /**
  * Shows units to choose including the controls to make the choice. Units are grouped by owner, type
@@ -475,9 +478,9 @@ public final class UnitChooser extends JPanel {
 
     void createComponents(final JPanel panel, final int rowIndex) {
       int gridx = 0;
-      for (int i = 0;
-          i < (hasMultipleHits ? Math.max(1, category.getHitPoints() - category.getDamaged()) : 1);
-          i++) {
+      int iterations =
+          (hasMultipleHits ? Math.max(1, category.getHitPoints() - category.getDamaged()) : 1);
+      for (int i = 0; i < iterations; i++) {
         final boolean damaged = i > 0;
         final ScrollableTextField scroll = new ScrollableTextField(0, category.getUnits().size());
         hitTexts.add(scroll);
@@ -485,83 +488,29 @@ public final class UnitChooser extends JPanel {
         scroll.addChangeListener(textFieldListener);
         final JLabel label = new JLabel("x" + category.getUnits().size());
         hitLabel.add(label);
+        final var builder =
+            new GridBagConstraintsBuilder(gridx, rowIndex)
+                .gridWidth(1)
+                .gridHeight(1)
+                .anchor(GridBagConstraintsAnchor.WEST)
+                .fill(GridBagConstraintsFill.HORIZONTAL);
         panel.add(
             new UnitChooserEntryIcon(damaged),
-            new GridBagConstraints(
-                gridx++,
-                rowIndex,
-                1,
-                1,
-                0,
-                0,
-                GridBagConstraints.WEST,
-                GridBagConstraints.HORIZONTAL,
-                new Insets(0, (i == 0 ? 0 : 8), 0, 0),
-                0,
-                0));
+            builder.insets(new Insets(0, (i == 0 ? 0 : 8), 0, 0)).gridX(gridx++).build());
         if (i == 0) {
           if (category.getMovement().compareTo(new BigDecimal(-1)) != 0) {
             panel.add(
                 new JLabel("mvt " + category.getMovement()),
-                new GridBagConstraints(
-                    gridx,
-                    rowIndex,
-                    1,
-                    1,
-                    0,
-                    0,
-                    GridBagConstraints.WEST,
-                    GridBagConstraints.HORIZONTAL,
-                    new Insets(0, 4, 0, 4),
-                    0,
-                    0));
+                builder.insets(new Insets(0, 4, 0, 4)).gridX(gridx++).build());
           }
-          gridx++;
           if (category.getTransportCost() != -1) {
             panel.add(
                 new JLabel("cst " + category.getTransportCost()),
-                new GridBagConstraints(
-                    gridx,
-                    rowIndex,
-                    1,
-                    1,
-                    0,
-                    0,
-                    GridBagConstraints.WEST,
-                    GridBagConstraints.HORIZONTAL,
-                    new Insets(0, 4, 0, 4),
-                    0,
-                    0));
+                builder.insets(new Insets(0, 4, 0, 4)).gridX(gridx++).build());
           }
         }
-        panel.add(
-            label,
-            new GridBagConstraints(
-                gridx++,
-                rowIndex,
-                1,
-                1,
-                0,
-                0,
-                GridBagConstraints.WEST,
-                GridBagConstraints.HORIZONTAL,
-                emptyInsets,
-                0,
-                0));
-        panel.add(
-            scroll,
-            new GridBagConstraints(
-                gridx++,
-                rowIndex,
-                1,
-                1,
-                0,
-                0,
-                GridBagConstraints.WEST,
-                GridBagConstraints.HORIZONTAL,
-                new Insets(0, 4, 0, 0),
-                0,
-                0));
+        panel.add(label, builder.insets(emptyInsets).gridX(gridx++).build());
+        panel.add(scroll, builder.insets(new Insets(0, 4, 0, 0)).gridX(gridx++).build());
         scroll.addChangeListener(field -> updateLeftToSelect());
       }
       updateLeftToSelect();

lib/swing-lib/src/main/java/org/triplea/swing/jpanel/GridBagConstraintsBuilder.java
@@ -26,8 +26,8 @@ import java.awt.Insets;
  * </pre></code>
  */
 public class GridBagConstraintsBuilder {
-  private final int gridX;
-  private final int gridY;
+  private int gridX;
+  private int gridY;
 
   private int gridWidth = 1;
   private int gridHeight = 1;
@@ -132,6 +132,16 @@ public class GridBagConstraintsBuilder {
   /** Default value is 0 */
   public GridBagConstraintsBuilder padY(final int padY) {
     this.padY = padY;
+    return this;
+  }
+
+  public GridBagConstraintsBuilder gridY(final int gridY) {
+    this.gridY = gridY;
+    return this;
+  }
+
+  public GridBagConstraintsBuilder gridX(final int gridX) {
+    this.gridX = gridX;
     return this;
   }
 }

commit 494244d9d3b47e117f070dd38631d6807f06276d

Fix issue with misaligned columns in unit selector.
Fixes a problem I introduced in https://github.com/triplea-game/triplea/pull/10285. Also add some comments.

game-app/game-core/src/main/java/games/strategy/triplea/ui/UnitChooser.java
@@ -498,16 +498,19 @@ public final class UnitChooser extends JPanel {
             new UnitChooserEntryIcon(damaged),
             builder.insets(new Insets(0, (i == 0 ? 0 : 8), 0, 0)).gridX(gridx++).build());
         if (i == 0) {
+          // -1 indicates a transport whose movement ended due to unloading already.
           if (category.getMovement().compareTo(new BigDecimal(-1)) != 0) {
             panel.add(
                 new JLabel("mvt " + category.getMovement()),
-                builder.insets(new Insets(0, 4, 0, 4)).gridX(gridx++).build());
+                builder.insets(new Insets(0, 4, 0, 4)).gridX(gridx).build());
           }
+          gridx++; // Increment outside the if to avoid misalignment.
           if (category.getTransportCost() != -1) {
             panel.add(
                 new JLabel("cst " + category.getTransportCost()),
-                builder.insets(new Insets(0, 4, 0, 4)).gridX(gridx++).build());
+                builder.insets(new Insets(0, 4, 0, 4)).gridX(gridx).build());
           }
+          gridx++; // Increment outside the if to avoid misalignment.
         }
         panel.add(label, builder.insets(emptyInsets).gridX(gridx++).build());
         panel.add(scroll, builder.insets(new Insets(0, 4, 0, 0)).gridX(gridx++).build());