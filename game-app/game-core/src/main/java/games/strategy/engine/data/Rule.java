f19f38c9ba19c7c8ac81702965b9c8f882fa3491
Attacktive <welcome.hell.dude@gmail.com>
2022-04-28 10:21:54 +0900

    Improve documentation of interface Rule.
    https://github.com/triplea-game/triplea/pull/10378#discussion_r859836766

--- game-app/game-core/src/main/java/games/strategy/engine/data/Rule.java
+++ game-app/game-core/src/main/java/games/strategy/engine/data/Rule.java
@@ -1,12 +1,18 @@
 package games.strategy.engine.data;
 
+import org.triplea.java.ChangeOnNextMajorRelease;
 import org.triplea.java.collections.CollectionUtils;
 import org.triplea.java.collections.IntegerMap;
 
 /**
  * Superclass for {@link games.strategy.engine.data.RepairRule} and {@link
  * games.strategy.engine.data.ProductionRule}.
+ *
+ * <p>It could become an {@code abstract class} instead in to contain more common code chunks from
+ * its children. That way, probably {@link games.strategy.engine.data.RuleComparator} can be
+ * entirely removed by making the child classes implement {@link java.lang.Comparable}.
  */
+@ChangeOnNextMajorRelease
 public interface Rule {
   String getName();
 
