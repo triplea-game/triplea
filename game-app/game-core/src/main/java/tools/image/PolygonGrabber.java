2debaf51e3ea37e5b0233751d8cea8b38106b442
asvitkine <alexei.svitkine@gmail.com>
2022-05-02 14:00:43 -0400

    Remove unused import.

--- game-app/game-core/src/main/java/tools/image/PolygonGrabber.java
+++ game-app/game-core/src/main/java/tools/image/PolygonGrabber.java
@@ -10,7 +10,6 @@ import java.awt.Image;
 import java.awt.Point;
 import java.awt.Polygon;
 import java.awt.Rectangle;
-import java.awt.event.ActionEvent;
 import java.awt.event.InputEvent;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseAdapter;
