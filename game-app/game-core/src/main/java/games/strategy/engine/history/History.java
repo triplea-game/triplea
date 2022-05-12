triplea-game/triplea

commit f6d040073279b0d0df2897396c05b11f23202069

Fix history related errors caused by some changes being done twice. (#10429)
* Fix errors related to history code caused by some changes being applied twice.

* Revert ai clean up.

game-app/game-core/src/main/java/games/strategy/engine/history/History.java
@@ -88,7 +88,7 @@ public class History extends DefaultTreeModel {
     } else if (node instanceof EventChild) {
       lastChangeIndex = ((Event) node.getParent()).getChangeEndIndex();
     } else if (node instanceof IndexedHistoryNode) {
-      lastChangeIndex = ((IndexedHistoryNode) node).getChangeStartIndex();
+      lastChangeIndex = ((IndexedHistoryNode) node).getChangeEndIndex();
     } else {
       lastChangeIndex = 0;
     }
@@ -114,7 +114,7 @@ public class History extends DefaultTreeModel {
   /** Changes the game state to reflect the historical state at {@code node}. */
   public synchronized void gotoNode(final HistoryNode node) {
     assertCorrectThread();
-    getGameData().acquireWriteLock();
+    gameData.acquireWriteLock();
     try {
       if (currentNode == null) {
         currentNode = getLastNode();
@@ -125,7 +125,7 @@ public class History extends DefaultTreeModel {
         gameData.performChange(dataChange);
       }
     } finally {
-      getGameData().releaseWriteLock();
+      gameData.releaseWriteLock();
     }
   }
 
@@ -136,7 +136,7 @@ public class History extends DefaultTreeModel {
   public synchronized void removeAllHistoryAfterNode(final HistoryNode removeAfterNode) {
     gotoNode(removeAfterNode);
     assertCorrectThread();
-    getGameData().acquireWriteLock();
+    gameData.acquireWriteLock();
     try {
       final int lastChange = getLastChange(removeAfterNode) + 1;
       while (changes.size() > lastChange) {
@@ -163,7 +163,7 @@ public class History extends DefaultTreeModel {
         this.removeNodeFromParent(nodesToRemove.remove(0));
       }
     } finally {
-      getGameData().releaseWriteLock();
+      gameData.releaseWriteLock();
     }
   }
 

game-app/game-core/src/main/java/games/strategy/engine/data/ResourceCollection.java
@@ -50,7 +50,7 @@ public class ResourceCollection extends GameDataComponent {
     final int current = getQuantity(resource);
     if ((current - quantity) < 0) {
       throw new IllegalArgumentException(
-          "Cant remove more than player has of resource: "
+          "Can't remove more than player has of resource: "
               + resource.getName()
               + ". current:"
               + current
@@ -58,10 +58,6 @@ public class ResourceCollection extends GameDataComponent {
               + quantity);
     }
     change(resource, -quantity);
-  }
-
-  public void removeAllOfResource(final Resource resource) {
-    resources.removeKey(resource);
   }
 
   private void change(final Resource resource, final int quantity) {

commit 37702f226438a4c2a430b191999f041aea183fb1

Fix selecting step nodes in history view not having an effect.
Fix selecting step nodes in history view not having an effect.

This was broken with https://github.com/triplea-game/triplea/pull/10429, which made the logically correct change, but ran into a bug where we weren't actually setting the change index on step nodes. Introduce a fix as well as handling of history from saved games that didn't set it.

game-app/game-core/src/main/java/games/strategy/engine/history/History.java
@@ -80,7 +80,7 @@ public class History extends DefaultTreeModel {
   }
 
   private int getLastChange(final HistoryNode node) {
-    final int lastChangeIndex;
+    int lastChangeIndex;
     if (node == getRoot()) {
       lastChangeIndex = 0;
     } else if (node instanceof Event) {
@@ -89,6 +89,11 @@ public class History extends DefaultTreeModel {
       lastChangeIndex = ((Event) node.getParent()).getChangeEndIndex();
     } else if (node instanceof IndexedHistoryNode) {
       lastChangeIndex = ((IndexedHistoryNode) node).getChangeEndIndex();
+      // If this node is still current, or comes from an old save game where we didn't set it, get
+      // the last change index from its last child node.
+      if (lastChangeIndex == -1 && node.getChildCount() > 0) {
+        lastChangeIndex = getLastChange((HistoryNode)node.getLastChild());
+      }
     } else {
       lastChangeIndex = 0;
     }

game-app/game-core/src/main/java/games/strategy/engine/history/HistoryWriter.java
@@ -88,6 +88,7 @@ public class HistoryWriter implements Serializable {
           parent.remove(current);
           history.nodesWereRemoved(parent, new int[] {index}, new Object[] {current});
         }
+        ((Step) current).setChangeEndIndex(history.getChanges().size());
         current = parent;
         return;
       }