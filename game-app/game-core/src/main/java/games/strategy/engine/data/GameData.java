13308d3f69d4df54db1c82a531fbeeb24698255b
asvitkine <alexei.svitkine@gmail.com>
2022-05-27 14:25:43 -0400

    When loading saves from past version, fix up null player references. (#10518)
    * When loading saves from past version, fix up null player references.
    
    * Do it in a different place.
    
    * Check for null data and use a method so it can be marked for removal.

--- game-app/game-core/src/main/java/games/strategy/engine/data/GameData.java
+++ game-app/game-core/src/main/java/games/strategy/engine/data/GameData.java
@@ -363,6 +363,24 @@ public class GameData implements Serializable, GameState {
     territoryListeners = new CopyOnWriteArrayList<>();
     dataChangeListeners = new CopyOnWriteArrayList<>();
     delegates = new HashMap<>();
+    fixUpNullPlayers();
+  }
+
+  @RemoveOnNextMajorRelease
+  private void fixUpNullPlayers() {
+    // Old save games may have territories and units owned by a different null player object that
+    // has a null data. Update them to the new null player, so that code can rely on getData()
+    // not being null.
+    for (Territory t : getMap().getTerritories()) {
+      if (t.getOwner().getData() == null) {
+        t.setOwner(playerList.getNullPlayer());
+      }
+    }
+    for (Unit u : getUnits()) {
+      if (u.getOwner().getData() == null) {
+        u.setOwner(playerList.getNullPlayer());
+      }
+    }
   }
 
   public interface Unlocker extends Closeable {
