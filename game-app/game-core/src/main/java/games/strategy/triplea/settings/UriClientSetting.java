3eff74e19f7b0f102c659ec8d21ab0157e9278ce
RoiEX <8350879+RoiEXLab@users.noreply.github.com>
2022-06-02 21:15:04 +0200

    Apply some IntelliJ suggestions to code (#10563)

--- game-app/game-core/src/main/java/games/strategy/triplea/settings/UriClientSetting.java
+++ game-app/game-core/src/main/java/games/strategy/triplea/settings/UriClientSetting.java
@@ -4,11 +4,11 @@ import java.net.URI;
 import java.net.URISyntaxException;
 
 final class UriClientSetting extends ClientSetting<URI> {
-  protected UriClientSetting(final String name) {
+  UriClientSetting(final String name) {
     super(URI.class, name);
   }
 
-  protected UriClientSetting(final String name, final URI defaultValue) {
+  UriClientSetting(final String name, final URI defaultValue) {
     super(URI.class, name, defaultValue);
   }
 
