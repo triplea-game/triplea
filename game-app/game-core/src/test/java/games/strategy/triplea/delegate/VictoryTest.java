8dc68cc78449722d28c80d1944723e3c9bc52a04
asvitkine <alexei.svitkine@gmail.com>
2022-06-13 15:47:49 -0400

    And one more test.

--- game-app/game-core/src/test/java/games/strategy/triplea/delegate/VictoryTest.java
+++ game-app/game-core/src/test/java/games/strategy/triplea/delegate/VictoryTest.java
@@ -284,6 +284,20 @@ class VictoryTest {
       assertThat(moveDelegate.move(tankAndInfantry, route), wasPerformedSuccessfully());
     }
 
+    @Test
+    void testAttackToOwnContestedTerritory() {
+      // Make transJordan owned by italians, but with enemy units.
+      transJordan.setOwner(italians);
+      assertThat(Matches.territoryHasEnemyUnits(italians).test(transJordan), is(true));
+
+      assertThat(moveDelegate.move(tankAndInfantry, route), isNotAllowedWithContestedAttackError());
+      assertThat(moveDelegate.move(tankUnit, route), isNotAllowedWithContestedAttackError());
+      assertThat(moveDelegate.move(infantryUnit, route), isNotAllowedWithContestedAttackError());
+      // If CAN_ATTACK_FROM_CONTESTED_TERRITORIES is true, the attack is valid.
+      gameData.getProperties().set(Constants.CAN_ATTACK_FROM_CONTESTED_TERRITORIES, true);
+      assertThat(moveDelegate.move(tankAndInfantry, route), wasPerformedSuccessfully());
+    }
+
     private void removeEnemyUnits(GamePlayer player, Territory t) {
       Predicate<Unit> isEnemy = Matches.unitIsEnemyOf(player);
       Collection<Unit> enemyUnits = CollectionUtils.getMatches(t.getUnits(), isEnemy);
