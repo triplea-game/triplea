a854842a93f3eb8bd4b51a6fa3ab52973568df7a
asvitkine <alexei.svitkine@gmail.com>
2022-06-16 19:03:55 -0400

    Fix codeclimate duplicate warning.

--- game-app/game-core/src/main/java/games/strategy/triplea/ui/EditPanel.java
+++ game-app/game-core/src/main/java/games/strategy/triplea/ui/EditPanel.java
@@ -703,25 +703,7 @@ class EditPanel extends ActionPanel {
               cancelEditAction.actionPerformed(null);
               return;
             }
-            sortUnitsToRemove(units);
-            units.sort(
-                new UnitBattleComparator(
-                        new TuvCostsCalculator().getCostsForTuv(player),
-                        getData(),
-                        CombatValueBuilder.mainCombatValue()
-                            .enemyUnits(List.of())
-                            .friendlyUnits(List.of())
-                            .side(BattleState.Side.OFFENSE)
-                            .gameSequence(getData().getSequence())
-                            .supportAttachments(getData().getUnitTypeList().getSupportRules())
-                            .lhtrHeavyBombers(
-                                Properties.getLhtrHeavyBombers(getData().getProperties()))
-                            .gameDiceSides(getData().getDiceSides())
-                            .territoryEffects(List.of())
-                            .build(),
-                        true,
-                        false)
-                    .reversed());
+            sortUnits(player, units);
             // unit mapped to <max, min, current>
             final Map<Unit, Triple<Integer, Integer, Integer>> currentDamageMap = new HashMap<>();
             for (final Unit u : units) {
@@ -791,25 +773,7 @@ class EditPanel extends ActionPanel {
               cancelEditAction.actionPerformed(null);
               return;
             }
-            sortUnitsToRemove(units);
-            units.sort(
-                new UnitBattleComparator(
-                        new TuvCostsCalculator().getCostsForTuv(player),
-                        getData(),
-                        CombatValueBuilder.mainCombatValue()
-                            .enemyUnits(List.of())
-                            .friendlyUnits(List.of())
-                            .side(BattleState.Side.OFFENSE)
-                            .gameSequence(getData().getSequence())
-                            .supportAttachments(getData().getUnitTypeList().getSupportRules())
-                            .lhtrHeavyBombers(
-                                Properties.getLhtrHeavyBombers(getData().getProperties()))
-                            .gameDiceSides(getData().getDiceSides())
-                            .territoryEffects(List.of())
-                            .build(),
-                        true,
-                        false)
-                    .reversed());
+            sortUnits(player, units);
             // unit mapped to <max, min, current>
             final Map<Unit, Triple<Integer, Integer, Integer>> currentDamageMap = new HashMap<>();
             for (final Unit u : units) {
@@ -968,6 +932,26 @@ class EditPanel extends ActionPanel {
     add(new JButton(changePoliticalRelationships));
     add(Box.createVerticalStrut(15));
     setWidgetActivation();
+  }
+
+  private void sortUnits(GamePlayer player, List<Unit> units) {
+    units.sort(
+        new UnitBattleComparator(
+                new TuvCostsCalculator().getCostsForTuv(player),
+                getData(),
+                CombatValueBuilder.mainCombatValue()
+                    .enemyUnits(List.of())
+                    .friendlyUnits(List.of())
+                    .side(BattleState.Side.OFFENSE)
+                    .gameSequence(getData().getSequence())
+                    .supportAttachments(getData().getUnitTypeList().getSupportRules())
+                    .lhtrHeavyBombers(Properties.getLhtrHeavyBombers(getData().getProperties()))
+                    .gameDiceSides(getData().getDiceSides())
+                    .territoryEffects(List.of())
+                    .build(),
+                true,
+                false)
+            .reversed());
   }
 
   @SuppressWarnings("PMD.UnusedFormalParameter")
