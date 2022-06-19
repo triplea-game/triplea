01b2b99a75ece5d333898d15f749a8b87e81b45e
RoiEX <8350879+RoiEXLab@users.noreply.github.com>
2022-06-19 03:43:12 +0200

    Fix test case

--- game-app/game-headed/src/test/java/games/strategy/engine/framework/startup/mc/HeadedPlayerTypesTest.java
+++ game-app/game-headed/src/test/java/games/strategy/engine/framework/startup/mc/HeadedPlayerTypesTest.java
@@ -3,12 +3,28 @@ package games.strategy.engine.framework.startup.mc;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 import games.strategy.engine.framework.startup.ui.PlayerTypes;
+import games.strategy.triplea.settings.ClientSetting;
 import java.util.List;
 import org.hamcrest.Matchers;
 import org.hamcrest.core.IsCollectionContaining;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.sonatype.goodies.prefs.memory.MemoryPreferences;
 
 public class HeadedPlayerTypesTest {
+
+  @BeforeEach
+  public final void initializeClientSettingPreferences() {
+    ClientSetting.setPreferences(new MemoryPreferences());
+  }
+
+  @AfterEach
+  @SuppressWarnings("static-method")
+  public final void uninitializeClientSettingPreferences() {
+    ClientSetting.resetPreferences();
+  }
+
   @Test
   void playerTypes() {
     final PlayerTypes playerTypes = new PlayerTypes(HeadedPlayerTypes.getPlayerTypes());
