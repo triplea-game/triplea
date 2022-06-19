4a602b01e959181170ec7d9cca892cf1433c8fcc
RoiEX <8350879+RoiEXLab@users.noreply.github.com>
2022-06-19 02:03:07 +0200

    Remove redundant serviceloader mechanism

--- game-app/game-headed/src/main/java/games/strategy/engine/framework/startup/mc/HeadedPlayerTypes.java
+++ game-app/game-headed/src/main/java/games/strategy/engine/framework/startup/mc/HeadedPlayerTypes.java
@@ -1,14 +1,11 @@
 package games.strategy.engine.framework.startup.mc;
 
 import games.strategy.engine.framework.startup.ui.PlayerTypes;
-import games.strategy.triplea.ai.AiProvider;
 import games.strategy.triplea.settings.ClientSetting;
 import java.util.Collection;
 import java.util.List;
-import java.util.ServiceLoader;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-import java.util.stream.StreamSupport;
 import org.triplea.ai.does.nothing.DoesNothingAiProvider;
 import org.triplea.ai.flowfield.FlowFieldAiProvider;
 
@@ -25,10 +22,7 @@ public class HeadedPlayerTypes {
             PlayerTypes.getBuiltInPlayerTypes(),
             List.of(
                 new PlayerTypes.AiType(new DoesNothingAiProvider()),
-                new PlayerTypes.AiType(new FlowFieldAiProvider())),
-            StreamSupport.stream(ServiceLoader.load(AiProvider.class).spliterator(), false)
-                .map(PlayerTypes.AiType::new)
-                .collect(Collectors.toSet()))
+                new PlayerTypes.AiType(new FlowFieldAiProvider())))
         .flatMap(Collection::stream)
         .filter(HeadedPlayerTypes::filterBetaPlayerType)
         .collect(Collectors.toList());
