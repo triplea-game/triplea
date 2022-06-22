5c4183aa3821e203ffb60b392cbaef851053d35c
RoiEX <8350879+RoiEXLab@users.noreply.github.com>
2022-06-22 15:46:30 +0200

    Use locks instead of polling to start headless game

--- game-app/game-core/src/main/java/org/triplea/game/server/HeadlessServerSetup.java
+++ game-app/game-core/src/main/java/org/triplea/game/server/HeadlessServerSetup.java
@@ -5,22 +5,32 @@ import games.strategy.engine.framework.startup.mc.IRemoteModelListener;
 import games.strategy.engine.framework.startup.mc.ServerModel;
 import games.strategy.engine.framework.startup.ui.panels.main.game.selector.GameSelectorModel;
 import java.util.Optional;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
 import org.triplea.game.chat.ChatModel;
 import org.triplea.game.startup.SetupModel;
+import org.triplea.java.Interruptibles;
 
 /** Server setup model. */
 class HeadlessServerSetup implements IRemoteModelListener, SetupModel {
   private final ServerModel model;
   private final GameSelectorModel gameSelectorModel;
+  private final ReentrantLock lock;
+  private final Condition playersUpdated;
+
+  private boolean isRunning = true;
 
   HeadlessServerSetup(final ServerModel model, final GameSelectorModel gameSelectorModel) {
     this.model = model;
     this.gameSelectorModel = gameSelectorModel;
     this.model.setRemoteModelListener(this);
+    lock = new ReentrantLock();
+    playersUpdated = lock.newCondition();
   }
 
   @Override
   public void cancel() {
+    signalPotentialStart(false);
     model.setRemoteModelListener(IRemoteModelListener.NULL_LISTENER);
     model.cancel();
   }
@@ -31,10 +41,14 @@ class HeadlessServerSetup implements IRemoteModelListener, SetupModel {
   }
 
   @Override
-  public void playerListChanged() {}
+  public void playerListChanged() {
+    signalPotentialStart(true);
+  }
 
   @Override
-  public void playersTakenChanged() {}
+  public void playersTakenChanged() {
+    signalPotentialStart(true);
+  }
 
   @Override
   public ChatModel getChatModel() {
@@ -53,5 +67,29 @@ class HeadlessServerSetup implements IRemoteModelListener, SetupModel {
   @Override
   public void postStartGame() {
     SetupModel.clearPbfPbemInformation(gameSelectorModel.getGameData().getProperties());
+  }
+
+  private void signalPotentialStart(boolean isRunning) {
+    lock.lock();
+    try {
+      this.isRunning = isRunning;
+      playersUpdated.signal();
+    } finally {
+      lock.unlock();
+    }
+  }
+
+  public boolean waitUntilStart() {
+    lock.lock();
+    try {
+      while (!canGameStart() && isRunning) {
+        if (!Interruptibles.await(playersUpdated::await)) {
+          return false;
+        }
+      }
+      return isRunning;
+    } finally {
+      lock.unlock();
+    }
   }
 }
---
+++ game-app/game-headless/src/test/java/org/triplea/game/server/HeadlessServerSetupTest.java
@@ -0,0 +1,107 @@
+package org.triplea.game.server;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import games.strategy.engine.data.GameData;
+import games.strategy.engine.framework.startup.mc.ServerModel;
+import games.strategy.engine.framework.startup.ui.panels.main.game.selector.GameSelectorModel;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
+import org.junit.jupiter.api.Test;
+
+public class HeadlessServerSetupTest {
+  private final ServerModel model = mock(ServerModel.class);
+  private final GameSelectorModel gameSelectorModel = mock(GameSelectorModel.class);
+  private final HeadlessServerSetup headlessServerSetup =
+      new HeadlessServerSetup(model, gameSelectorModel);
+
+  @Test
+  void verifyCancelDoesMakeWaitNonBlocking() throws Exception {
+    headlessServerSetup.cancel();
+    final var future = CompletableFuture.supplyAsync(headlessServerSetup::waitUntilStart);
+    assertThat(future.get(1, TimeUnit.SECONDS), is(false));
+  }
+
+  @Test
+  void verifyCancelDoesWakeUpThread() throws Exception {
+    final var countDownLatch = new CountDownLatch(1);
+    final var future =
+        CompletableFuture.supplyAsync(
+            () -> {
+              countDownLatch.countDown();
+              return headlessServerSetup.waitUntilStart();
+            });
+    countDownLatch.await();
+    headlessServerSetup.cancel();
+    assertThat(future.get(1, TimeUnit.SECONDS), is(false));
+  }
+
+  private void fulfillCondition() {
+    when(gameSelectorModel.getGameData()).thenReturn(mock(GameData.class));
+    // we can't use Map.of() here because containsValue(null) would throw an NPE
+    final Map<String, String> map = new HashMap<>();
+    map.put("Test", "Test");
+    when(model.getPlayersToNodeListing()).thenReturn(map);
+    when(model.getPlayersEnabledListing()).thenReturn(Map.of("Test", Boolean.TRUE));
+  }
+
+  @Test
+  void verifyFulfilledConditionDoesMakeWaitNonBlocking() throws Exception {
+    fulfillCondition();
+    final var future = CompletableFuture.supplyAsync(headlessServerSetup::waitUntilStart);
+    assertThat(future.get(1, TimeUnit.SECONDS), is(true));
+  }
+
+  @Test
+  void verifyWaitWaitsUntilConditionIsFulfilled_playerListChanged() throws Exception {
+    final var countDownLatch = new CountDownLatch(1);
+    final var future =
+        CompletableFuture.supplyAsync(
+            () -> {
+              countDownLatch.countDown();
+              return headlessServerSetup.waitUntilStart();
+            });
+    countDownLatch.await();
+    fulfillCondition();
+    headlessServerSetup.playerListChanged();
+    assertThat(future.get(1, TimeUnit.SECONDS), is(true));
+  }
+
+  @Test
+  void verifyWaitWaitsUntilConditionIsFulfilled_playersTakenChanged() throws Exception {
+    final var countDownLatch = new CountDownLatch(1);
+    final var future =
+        CompletableFuture.supplyAsync(
+            () -> {
+              countDownLatch.countDown();
+              return headlessServerSetup.waitUntilStart();
+            });
+    countDownLatch.await();
+    fulfillCondition();
+    headlessServerSetup.playersTakenChanged();
+    assertThat(future.get(1, TimeUnit.SECONDS), is(true));
+  }
+
+  @Test
+  void verifyInterruptsReturnFalse() throws Exception {
+    final var thread = new AtomicReference<Thread>();
+    final var countDownLatch = new CountDownLatch(1);
+    final var future =
+        CompletableFuture.supplyAsync(
+            () -> {
+              thread.set(Thread.currentThread());
+              countDownLatch.countDown();
+              return headlessServerSetup.waitUntilStart();
+            });
+    countDownLatch.await();
+    thread.get().interrupt();
+    assertThat(future.get(1, TimeUnit.SECONDS), is(false));
+  }
+}
