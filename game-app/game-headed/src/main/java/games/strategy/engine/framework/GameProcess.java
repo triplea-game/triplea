2a1f9211c11c570801bb02e62c0e2aa09b8a7aa1
RoiEX <8350879+RoiEXLab@users.noreply.github.com>
2022-06-23 19:52:27 +0200

    Avoid spawning process when joining server

--- game-app/game-headed/src/main/java/games/strategy/engine/framework/GameProcess.java
+++ game-app/game-headed/src/main/java/games/strategy/engine/framework/GameProcess.java
@@ -3,9 +3,7 @@ package games.strategy.engine.framework;
 import static games.strategy.engine.framework.CliProperties.LOBBY_GAME_COMMENTS;
 import static games.strategy.engine.framework.CliProperties.LOBBY_URI;
 import static games.strategy.engine.framework.CliProperties.SERVER_PASSWORD;
-import static games.strategy.engine.framework.CliProperties.TRIPLEA_CLIENT;
 import static games.strategy.engine.framework.CliProperties.TRIPLEA_GAME;
-import static games.strategy.engine.framework.CliProperties.TRIPLEA_HOST;
 import static games.strategy.engine.framework.CliProperties.TRIPLEA_NAME;
 import static games.strategy.engine.framework.CliProperties.TRIPLEA_PORT;
 import static games.strategy.engine.framework.CliProperties.TRIPLEA_SERVER;
@@ -13,9 +11,7 @@ import static games.strategy.engine.framework.CliProperties.TRIPLEA_SERVER;
 import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
-import org.triplea.domain.data.UserName;
 import org.triplea.game.client.HeadedApplicationContext;
-import org.triplea.lobby.common.GameDescription;
 
 public class GameProcess {
 
@@ -44,25 +40,6 @@ public class GameProcess {
     if (fileName.length() > 0) {
       commands.add("-D" + TRIPLEA_GAME + "=" + fileName);
     }
-    commands.add(getMainClassName());
-    ProcessRunnerUtil.exec(commands);
-  }
-
-  /** Spawns a new process to join a network game. */
-  public static void joinGame(final GameDescription description, final UserName userName) {
-    final GameDescription.GameStatus status = description.getStatus();
-    if (GameDescription.GameStatus.LAUNCHING == status) {
-      return;
-    }
-
-    final List<String> commands = new ArrayList<>();
-    ProcessRunnerUtil.populateBasicJavaArgs(commands);
-    final String prefix = "-D";
-    commands.add(prefix + TRIPLEA_CLIENT + "=true");
-    commands.add(prefix + TRIPLEA_PORT + "=" + description.getHostedBy().getPort());
-    commands.add(
-        prefix + TRIPLEA_HOST + "=" + description.getHostedBy().getAddress().getHostAddress());
-    commands.add(prefix + TRIPLEA_NAME + "=" + userName.getValue());
     commands.add(getMainClassName());
     ProcessRunnerUtil.exec(commands);
   }
