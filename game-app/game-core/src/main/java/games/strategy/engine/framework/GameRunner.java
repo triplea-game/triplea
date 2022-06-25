90e688270362531be91ab61d1fdea96de75f4e97
RoiEX <8350879+RoiEXLab@users.noreply.github.com>
2022-06-25 18:02:24 +0200

    Remove redundant latch logic (#10756)

--- game-app/game-core/src/main/java/games/strategy/engine/framework/GameRunner.java
+++ game-app/game-core/src/main/java/games/strategy/engine/framework/GameRunner.java
@@ -6,7 +6,6 @@ public final class GameRunner {
   public static final String BOT_GAME_HOST_COMMENT = "automated_host";
   public static final String BOT_GAME_HOST_NAME_PREFIX = "Bot";
   public static final int PORT = 3300;
-  public static final int MINIMUM_CLIENT_GAMEDATA_LOAD_GRACE_TIME = 20;
 
   public static boolean headless() {
     return Boolean.parseBoolean(System.getProperty(TRIPLEA_HEADLESS, "false"));
