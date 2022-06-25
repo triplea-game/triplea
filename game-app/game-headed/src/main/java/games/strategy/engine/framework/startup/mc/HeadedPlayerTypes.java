triplea-game/triplea

commit a9e96d4ffc80493b4ffd5eeee1ea2521ddd92dad

Refactor TripleAPlayer and move to game-headed (#10709)
* Simplify type tree of Player implementations

* Rename class AbstractBuiltInAi to AbstractAi

* Reduce usage of PlayerType in interface

* Wire labels instead of whole type

* Remove redundant serviceloader mechanism

* Reduce explicit dependencies on TripleAPlayer class

* Move TripleAPlayer to game-headed

* Remove AiProvider interface and implementations

* Add fallback method for compatibility

* Remove BATTLE_CALC_DUMMY player

* Fix test case

* Change method name

game-app/game-headed/src/main/java/games/strategy/engine/framework/startup/mc/HeadedPlayerTypes.java
@@ -1,34 +1,65 @@
 package games.strategy.engine.framework.startup.mc;
 
 import games.strategy.engine.framework.startup.ui.PlayerTypes;
-import games.strategy.triplea.ai.AiProvider;
+import games.strategy.engine.player.Player;
+import games.strategy.triplea.TripleAPlayer;
 import games.strategy.triplea.settings.ClientSetting;
 import java.util.Collection;
 import java.util.List;
-import java.util.ServiceLoader;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
-import java.util.stream.StreamSupport;
-import org.triplea.ai.does.nothing.DoesNothingAiProvider;
-import org.triplea.ai.flowfield.FlowFieldAiProvider;
+import org.triplea.ai.does.nothing.DoesNothingAi;
+import org.triplea.ai.flowfield.FlowFieldAi;
 
 public class HeadedPlayerTypes {
+
+  private static final String FLOW_FIELD_LABEL = "FlowField (AI)";
+
+  public static final PlayerTypes.Type HUMAN_PLAYER =
+      new PlayerTypes.Type("Human") {
+        @Override
+        public Player newPlayerWithName(final String name) {
+          return new TripleAPlayer(name, getLabel(), false);
+        }
+      };
+  /** A hidden player type to represent network connected players. */
+  public static final PlayerTypes.Type CLIENT_PLAYER =
+      new PlayerTypes.Type("Client", false) {
+        @Override
+        public Player newPlayerWithName(final String name) {
+          return new TripleAPlayer(name, getLabel(), true);
+        }
+      };
+
   private static boolean filterBetaPlayerType(final PlayerTypes.Type playerType) {
-    if (playerType.getLabel().equals("FlowField (AI)")) {
+    if (playerType.getLabel().equals(FLOW_FIELD_LABEL)) {
       return ClientSetting.showBetaFeatures.getValue().orElse(false);
     }
     return true;
   }
 
+  private static PlayerTypes.Type getDoesNothingType() {
+    return new PlayerTypes.Type(PlayerTypes.DOES_NOTHING_PLAYER_LABEL) {
+      @Override
+      public Player newPlayerWithName(String name) {
+        return new DoesNothingAi(name, getLabel());
+      }
+    };
+  }
+
+  private static PlayerTypes.Type getFlowFieldType() {
+    return new PlayerTypes.Type(FLOW_FIELD_LABEL) {
+      @Override
+      public Player newPlayerWithName(String name) {
+        return new FlowFieldAi(name, getLabel());
+      }
+    };
+  }
+
   public static Collection<PlayerTypes.Type> getPlayerTypes() {
     return Stream.of(
             PlayerTypes.getBuiltInPlayerTypes(),
-            List.of(
-                new PlayerTypes.AiType(new DoesNothingAiProvider()),
-                new PlayerTypes.AiType(new FlowFieldAiProvider())),
-            StreamSupport.stream(ServiceLoader.load(AiProvider.class).spliterator(), false)
-                .map(PlayerTypes.AiType::new)
-                .collect(Collectors.toSet()))
+            List.of(HUMAN_PLAYER, CLIENT_PLAYER, getDoesNothingType(), getFlowFieldType()))
         .flatMap(Collection::stream)
         .filter(HeadedPlayerTypes::filterBetaPlayerType)
         .collect(Collectors.toList());

game-app/ai/src/main/java/org/triplea/ai/does/nothing/DoesNothingAi.java
@@ -7,7 +7,6 @@ import games.strategy.engine.data.GameState;
 import games.strategy.engine.data.ResourceCollection;
 import games.strategy.engine.data.changefactory.ChangeFactory;
 import games.strategy.engine.delegate.IDelegateBridge;
-import games.strategy.engine.framework.startup.ui.PlayerTypes;
 import games.strategy.triplea.ai.AbstractAi;
 import games.strategy.triplea.ai.weak.WeakAi;
 import games.strategy.triplea.delegate.remote.IAbstractForumPosterDelegate;
@@ -22,8 +21,8 @@ import games.strategy.triplea.delegate.remote.ITechDelegate;
  */
 public class DoesNothingAi extends AbstractAi {
 
-  public DoesNothingAi(final String name, final PlayerTypes.AiType playerType) {
-    super(name, playerType);
+  public DoesNothingAi(final String name, final String playerLabel) {
+    super(name, playerLabel);
   }
 
   @Override

game-app/ai/src/main/java/org/triplea/ai/does/nothing/DoesNothingAiProvider.java
@@ -1,17 +1,0 @@
-package org.triplea.ai.does.nothing;
-
-import games.strategy.engine.framework.startup.ui.PlayerTypes;
-import games.strategy.triplea.ai.AbstractAi;
-import games.strategy.triplea.ai.AiProvider;
-
-public class DoesNothingAiProvider implements AiProvider {
-  @Override
-  public AbstractAi create(final String name, final PlayerTypes.AiType playerType) {
-    return new DoesNothingAi(name, playerType);
-  }
-
-  @Override
-  public String getLabel() {
-    return PlayerTypes.DOES_NOTHING_PLAYER_LABEL;
-  }
-}

game-app/ai/src/main/java/org/triplea/ai/flowfield/FlowFieldAi.java
@@ -4,7 +4,6 @@ import games.strategy.engine.data.GameData;
 import games.strategy.engine.data.GamePlayer;
 import games.strategy.engine.data.GameState;
 import games.strategy.engine.data.UnitType;
-import games.strategy.engine.framework.startup.ui.PlayerTypes;
 import games.strategy.engine.player.IPlayerBridge;
 import games.strategy.triplea.Properties;
 import games.strategy.triplea.ai.AbstractAi;
@@ -34,8 +33,8 @@ public class FlowFieldAi extends AbstractAi {
   @Getter private Collection<InfluenceMap> diffusions = new ArrayList<>();
   private int round = -1;
 
-  public FlowFieldAi(final String name, final PlayerTypes.AiType playerType) {
-    super(name, playerType);
+  public FlowFieldAi(final String name, final String playerLabel) {
+    super(name, playerLabel);
   }
 
   @Override

game-app/ai/src/main/java/org/triplea/ai/flowfield/FlowFieldAiProvider.java
@@ -1,17 +1,0 @@
-package org.triplea.ai.flowfield;
-
-import games.strategy.engine.framework.startup.ui.PlayerTypes;
-import games.strategy.triplea.ai.AbstractAi;
-import games.strategy.triplea.ai.AiProvider;
-
-public class FlowFieldAiProvider implements AiProvider {
-  @Override
-  public AbstractAi create(final String name, final PlayerTypes.AiType playerType) {
-    return new FlowFieldAi(name, playerType);
-  }
-
-  @Override
-  public String getLabel() {
-    return "FlowField (AI)";
-  }
-}

game-app/game-core/src/main/java/games/strategy/engine/framework/LocalPlayers.java
@@ -2,7 +2,6 @@ package games.strategy.engine.framework;
 
 import games.strategy.engine.data.GamePlayer;
 import games.strategy.engine.player.Player;
-import games.strategy.triplea.player.AbstractHumanPlayer;
 import java.util.Collections;
 import java.util.Set;
 
@@ -30,7 +29,6 @@ public class LocalPlayers {
 
   private static boolean isGamePlayerWithPlayerId(
       final Player player, final GamePlayer gamePlayer) {
-    return player.getGamePlayer().equals(gamePlayer)
-        && AbstractHumanPlayer.class.isAssignableFrom(player.getClass());
+    return player.getGamePlayer().equals(gamePlayer) && !player.isAi();
   }
 }

game-app/game-core/src/main/java/games/strategy/engine/framework/ServerGame.java
@@ -38,7 +38,6 @@ import games.strategy.engine.random.RandomStats;
 import games.strategy.net.INode;
 import games.strategy.net.Messengers;
 import games.strategy.net.websocket.ClientNetworkBridge;
-import games.strategy.triplea.TripleAPlayer;
 import games.strategy.triplea.delegate.DiceRoll;
 import games.strategy.triplea.settings.ClientSetting;
 import java.io.IOException;
@@ -618,7 +617,7 @@ public class ServerGame extends AbstractGame {
     bridge.getHistoryWriter().startEvent("Game Loaded");
     for (final Player player : localPlayers) {
       allPlayersString.remove(player.getName());
-      final boolean isHuman = player instanceof TripleAPlayer;
+      final boolean isAi = player.isAi();
       bridge
           .getHistoryWriter()
           .addChildToEvent(
@@ -629,10 +628,9 @@ public class ServerGame extends AbstractGame {
                       ? " are"
                       : " is")
                   + " now being played by: "
-                  + player.getPlayerType().getLabel());
+                  + player.getPlayerLabel());
       final GamePlayer p = data.getPlayerList().getPlayerId(player.getName());
-      final String newWhoAmI =
-          ((isHuman ? "Human" : "AI") + ":" + player.getPlayerType().getLabel());
+      final String newWhoAmI = (isAi ? "AI" : "Human") + ":" + player.getPlayerLabel();
       if (!p.getWhoAmI().equals(newWhoAmI)) {
         change.add(ChangeFactory.changePlayerWhoAmIChange(p, newWhoAmI));
       }

game-app/game-core/src/main/java/games/strategy/engine/framework/startup/launcher/LaunchAction.java
@@ -63,4 +63,6 @@ public interface LaunchAction {
    * @return true if the game should stop execution, false otherwise.
    */
   boolean promptGameStop(String status, String title, Path mapLocation);
+
+  PlayerTypes.Type getDefaultLocalPlayerType();
 }

commit ef60184fad7216a9474a53635fc3a8a17326e176

Fix regression introduced in #10709

game-app/game-headed/src/main/java/games/strategy/engine/framework/startup/mc/HeadedPlayerTypes.java
@@ -58,8 +58,9 @@ public class HeadedPlayerTypes {
 
   public static Collection<PlayerTypes.Type> getPlayerTypes() {
     return Stream.of(
+            List.of(HUMAN_PLAYER),
             PlayerTypes.getBuiltInPlayerTypes(),
-            List.of(HUMAN_PLAYER, CLIENT_PLAYER, getDoesNothingType(), getFlowFieldType()))
+            List.of(CLIENT_PLAYER, getDoesNothingType(), getFlowFieldType()))
         .flatMap(Collection::stream)
         .filter(HeadedPlayerTypes::filterBetaPlayerType)
         .collect(Collectors.toList());