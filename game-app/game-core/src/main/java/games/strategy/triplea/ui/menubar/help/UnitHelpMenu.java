5c6180af773f147f214c6a86d5eaca0cc0b4daf2
asvitkine <alexei.svitkine@gmail.com>
2023-09-20 18:19:04 -0400

    Fix NPE with UnitHelp.

--- game-app/game-core/src/main/java/games/strategy/triplea/ui/menubar/help/UnitHelpMenu.java
+++ game-app/game-core/src/main/java/games/strategy/triplea/ui/menubar/help/UnitHelpMenu.java
@@ -2,7 +2,9 @@ package games.strategy.triplea.ui.menubar.help;
 
 import games.strategy.engine.data.GameData;
 import games.strategy.engine.framework.ui.background.BackgroundTaskRunner;
+import games.strategy.engine.framework.ui.background.WaitDialog;
 import games.strategy.triplea.ui.UiContext;
+import java.awt.Dimension;
 import javax.swing.Action;
 import javax.swing.BorderFactory;
 import javax.swing.JDialog;
@@ -20,21 +22,26 @@ class UnitHelpMenu {
   Action buildMenu(final GameData gameData, final UiContext uiContext) {
     return SwingAction.of(
         unitHelpTitle,
-        e -> {
-          final Result<JDialog> result =
-              Interruptibles.awaitResult(
-                  () ->
-                      BackgroundTaskRunner.runInBackgroundAndReturn(
-                          "Calculating Data",
-                          () -> {
-                            String text = UnitStatsTable.getUnitStatsTable(gameData, uiContext);
-                            JEditorPane editorPane = new JEditorPane("text/html", text);
-                            editorPane.setEditable(false);
-                            JScrollPane scroll = new JScrollPane(editorPane);
-                            scroll.setBorder(BorderFactory.createEmptyBorder());
-                            return InformationDialog.createDialog(scroll, unitHelpTitle);
-                          }));
-          result.result.orElseThrow().setVisible(true);
+        event -> {
+          try {
+            BackgroundTaskRunner.runInBackgroundAndReturn(
+                UnitHelpMenu::showDialog,
+                "Calculating Data",
+                () -> UnitStatsTable.getUnitStatsTable(gameData, uiContext));
+          } catch (InterruptedException e) {
+            // Nothing to do.
+          }
         });
+  }
+
+  private static void showDialog(String text) {
+    final JEditorPane editorPane = new JEditorPane();
+    editorPane.setContentType("text/html");
+    editorPane.setEditable(false);
+    final JScrollPane scroll = new JScrollPane(editorPane);
+    scroll.setBorder(BorderFactory.createEmptyBorder());
+    editorPane.setText(text);
+    editorPane.setCaretPosition(0);
+    InformationDialog.createDialog(scroll, unitHelpTitle).setVisible(true);
   }
 }
