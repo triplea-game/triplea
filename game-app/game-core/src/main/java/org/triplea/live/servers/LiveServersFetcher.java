78f1382823031336aec04c3e3b3f6fbdeb074cc4
DanVanAtta <dhvatta@gmail.com>
2024-04-09 18:33:38 -0700

    Revert - Use static file for latest version & lobby message
    - Updates how we fetch "latest version", we go back to reading it from a static file
      hosted in the code repository.
    
    - Format of the code repository latest file 'servers.yml' is updated to have a top
      level 'message'. This removes the notion of a per-version message. The intent
      is for us to get off this file soon, the 'message' would one day come from
      somewhere else.
    
    - The fetch of the 'lobby message' is moved back to the file as well. We need to
      read this information on any successful login to lobby.
    
    - Remove dependency on docker compose tasks to run game-headed project. This needs
      to be redone.

--- game-app/game-core/src/main/java/org/triplea/live/servers/LiveServersFetcher.java
+++ game-app/game-core/src/main/java/org/triplea/live/servers/LiveServersFetcher.java
@@ -1,33 +1,58 @@
 package org.triplea.live.servers;
 
-import feign.FeignException;
-import games.strategy.triplea.settings.ClientSetting;
+import games.strategy.engine.framework.system.HttpProxy;
+import games.strategy.triplea.UrlConstants;
+import java.io.IOException;
+import java.util.Map;
 import java.util.Optional;
 import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
-import org.triplea.http.client.latest.version.LatestVersionClient;
 import org.triplea.http.client.latest.version.LatestVersionResponse;
+import org.triplea.io.CloseableDownloader;
+import org.triplea.io.ContentDownloader;
+import org.triplea.yaml.YamlReader;
 
 @Slf4j
 @UtilityClass
 public class LiveServersFetcher {
 
   /**
-   * Queries the lobby-server for the latest game engine version.
-   *
-   * @return Empty optional if server fails to return a value, otherwise latest game engine version
-   *     as known to the lobby-server.
+   * Fetches from remote source the latest TripleA version that has been released. This can be used
+   * to determine if the current version is out of date.
    */
   public static Optional<LatestVersionResponse> latestVersion() {
-    try {
+    try (CloseableDownloader downloader =
+        new ContentDownloader(UrlConstants.LIVE_SERVERS_URI, HttpProxy::addProxy)) {
+      var inputStream = downloader.getStream();
+      final Map<String, Object> yamlProps = YamlReader.readMap(inputStream);
+
       return Optional.of(
-          LatestVersionClient.newClient(ClientSetting.lobbyUri.getValueOrThrow())
-              .fetchLatestVersion());
-    } catch (final FeignException e) {
+          LatestVersionResponse.builder()
+              .latestEngineVersion((String) yamlProps.get("latest"))
+              .downloadUrl(UrlConstants.DOWNLOAD_WEBSITE)
+              .releaseNotesUrl(UrlConstants.RELEASE_NOTES)
+              .build());
+    } catch (final IOException e) {
       log.info(
           "Unable to complete engine out-of-date check. "
               + "(No internet connection or server not available?)",
           e);
+      return Optional.empty();
+    }
+  }
+
+  /**
+   * Reads from remote source the "lobby welcome message", this is the lobby-chat message displayed
+   * first to anyone joining the lobby.
+   */
+  public static Optional<String> getLobbyMessage() {
+    try (CloseableDownloader downloader =
+        new ContentDownloader(UrlConstants.LIVE_SERVERS_URI, HttpProxy::addProxy)) {
+      var inputStream = downloader.getStream();
+      final Map<String, Object> yamlProps = YamlReader.readMap(inputStream);
+      return Optional.of((String) yamlProps.get("message"));
+    } catch (IOException e) {
+      log.info("Unable to fetch lobby welcome message", e);
       return Optional.empty();
     }
   }
