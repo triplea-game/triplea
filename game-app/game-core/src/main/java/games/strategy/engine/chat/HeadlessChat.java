dcda979aa91444a7a430b60851b3a3d0d0c2c68d
Dan Van Atta <dhvatta@gmail.com>
2024-07-21 15:01:59 -0700

    Bots: Replace chat upload to lobby with simple info logging (#12741)
    Commentary:
      - Drawback: bot chat history will no longer be readily available
        to moderators. That was useful when deciding "he-said-she-said"
        style disputes.
      - Benefits: fixes potential infinite loop that would crash
        the bot & simplifies.
    
    Fix details:
      - Any warn/error message is sent as a chat message. Any
        chat message in turn is sent to the lobby. If there is
        an error sending a message to the lobby, it can yield
        an error message, which in turn triggers a chat message,
        which in turn yields another upload to the lobby thereby
        starting an infinite cycle.
        If we do not send messages up to the lobby, then this is
        no longer a problem.
    
    Future plans:
      - Overall, ideally we can invest efforts in "network relay"
        rather than bot hosts, and remove all of the headless-game
        code entirely.

--- game-app/game-core/src/main/java/games/strategy/engine/chat/HeadlessChat.java
+++ game-app/game-core/src/main/java/games/strategy/engine/chat/HeadlessChat.java
@@ -1,10 +1,12 @@
 package games.strategy.engine.chat;
 
 import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 import org.triplea.domain.data.UserName;
 import org.triplea.game.chat.ChatModel;
 
 /** Headless version of ChatPanel. */
+@Slf4j
 public class HeadlessChat implements ChatMessageListener, ChatModel {
   @Getter(onMethod_ = @Override)
   private final Chat chat;
@@ -17,7 +19,9 @@ public class HeadlessChat implements ChatMessageListener, ChatModel {
   public void eventReceived(final String eventText) {}
 
   @Override
-  public void messageReceived(final UserName fromPlayer, final String chatMessage) {}
+  public void messageReceived(final UserName fromPlayer, final String chatMessage) {
+    log.info(String.format("Chat Message {Player: %s, Message: %s}", fromPlayer, chatMessage));
+  }
 
   @Override
   public void slapped(final UserName from) {}
