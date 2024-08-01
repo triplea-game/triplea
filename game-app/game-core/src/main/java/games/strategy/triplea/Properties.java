3a58f997c9391eca430b96625c8a1911d408597e
WCSumpton <wc_sumpton@hotmail.com>
2024-08-01 16:27:10 -0400

    Update Properties.java

--- game-app/game-core/src/main/java/games/strategy/triplea/Properties.java
+++ game-app/game-core/src/main/java/games/strategy/triplea/Properties.java
@@ -483,7 +483,7 @@ public final class Properties implements Constants {
   }
 
   public static boolean getUseNonAirUnitsInNormalBattle(final GameProperties properties) {
-    return properties.get(NON_AIR_SBR_UNITS_IN_NORMAL_BATTLE,false);
+    return properties.get(NON_AIR_SBR_UNITS_IN_NORMAL_BATTLE, false);
   }
   public static boolean getRaidsMayBePreceededByAirBattles(final GameProperties properties) {
     return properties.get(RAIDS_MAY_BE_PRECEEDED_BY_AIR_BATTLES, false);
