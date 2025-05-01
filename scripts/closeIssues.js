44fc8e4f6e40a555455367022ca0087144a7eb48
frigoref <frigoref@web.de>
2025-05-01 10:41:25 +0200

    Create closeIssues.js in folder scripts

---
+++ scripts/closeIssues.js
@@ -0,0 +1,46 @@
+import { Octokit } from "@octokit/rest";
+
+const octokit = new Octokit({
+    auth: process.env.GITHUB_TOKEN,
+});
+
+async function closeIssues() {
+    try {
+        console.log('Fetching issues...');
+        const issues = await octokit.paginate(octokit.rest.issues.listForRepo, {
+            repo: 'triplea',
+            state: 'open',
+        });
+
+        console.log(`Fetched ${issues.length} issues.`);
+        const issuesToClose = issues.filter(issue => issue.title.startsWith('2.5.') );
+        console.log(`Found ${issuesToClose.length} issues to close.`);
+
+        for (const issue of issuesToClose) {
+            console.log(`Closing issue #${issue.number}...`);
+          /*
+            // Add label (if missing)
+            let labelIndex = issue.labels.indexOf('2.5');
+            if (labelIndex < 0 ) {
+                issue.labels.push('2.5');
+            }            
+            await octokit.rest.issues.update({
+                repo: 'triplea',
+                issue_number: issue.number,
+                state: 'closed',
+                labels: issue.labels,
+            });
+            await octokit.rest.issues.update({
+                repo: 'triplea',
+                issue_number: issue.number,
+                state: 'closed',
+            });
+        */
+            console.log(`Closed issue #${issue.number}.`);
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+closeIssues();
