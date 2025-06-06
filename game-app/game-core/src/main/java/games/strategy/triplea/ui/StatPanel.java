7e226ab6dac297c1e3075e8284e5be7746369006
Brazza <jjia@ufl.edu>
2025-06-06 14:16:01 +0800

    Import Fix

--- game-app/game-core/src/main/java/games/strategy/triplea/ui/StatPanel.java
+++ game-app/game-core/src/main/java/games/strategy/triplea/ui/StatPanel.java
@@ -24,7 +24,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
-import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import javax.swing.ImageIcon;
 import javax.swing.JPanel;
