4b30bf9fb3a1002bebb3e124ba62c62a44196d74
DanVanAtta <dhvatta@gmail.com>
2025-06-19 17:24:48 -0700

    Extract github action 'script' in verify-urls...yml to its own script
    Having the script portion in its own file:
    (1) makes it easier to run, which in turn makes it easier to modify and debug
    (2) makes modification easier, no worries about colliding with YAML formatting
    and paragraph indentation or special characters

---
+++ .github/workflows/verify-urls-in-triplea-maps.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -x
+echo "Extracting URLs from triplea_maps.yaml..."
+grep -Eo 'https://[^") >]+' triplea_maps.yaml | sort -u > triplea_maps_urls.txt || true
+
+if [ ! -s triplea_maps_urls.txt ]; then
+  echo "❌ No URLs found in triplea_maps.yaml."
+  exit 1
+fi
+
+echo "Checking URLs..."
+failed_urls=()
+
+while IFS= read -r url; do
+  status=$(curl -o /dev/null -s -w "%{http_code}" -I -L "$url")
+  if [[ ! "$status" = 200 ]]; then
+    failed_urls+=("$url")
+  fi
+done < triplea_maps_urls.txt
+
+if [ "${#failed_urls[@]}" -ne 0 ]; then
+  echo "❌ The following URLs failed verification:"
+  for url in "${failed_urls[@]}"; do
+    echo "$url"
+  done
+  exit 1
+else
+  echo "✅ All URLs are valid."
+fi
