18e25b2fcedb0aee372794de6191aa9e776e15dd
DanVanAtta <dhvatta@gmail.com>
2025-06-19 17:49:55 -0700

    verify-urls script: simplify, avoid using an intermediate 'txt' file for capturing results, loop over the results directly instead

--- .github/workflows/verify-urls-in-triplea-maps.sh
+++ .github/workflows/verify-urls-in-triplea-maps.sh
@@ -1,19 +1,9 @@
 #!/bin/bash
-set -x
-echo "Extracting URLs from triplea_maps.yaml..."
-grep -Eo 'https://[^") >]+' triplea_maps.yaml | sort -u > triplea_maps_urls.txt || true
 
-if [ ! -s triplea_maps_urls.txt ]; then
-  echo "❌ No URLs found in triplea_maps.yaml."
-  exit 1
-fi
-
-echo "Checking URLs..."
 failed_urls=()
-
-while IFS= read -r url; do
+for url in $(grep -Eo 'https://[^") >]+' triplea_maps.yaml | sort -u); do
   curl --fail --silent "$url" > /dev/null || failed_urls+=("$url")
-done < triplea_maps_urls.txt
+done
 
 if [ "${#failed_urls[@]}" -ne 0 ]; then
   echo "❌ The following URLs failed verification:"
