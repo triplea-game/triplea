ac362818cd69dad016fc8a2679f5449ca854f0a3
frigoref <frigoref@web.de>
2025-09-21 18:24:01 +0200

    Correct check in TripleAMapValidator

--- game-app/game-core/src/main/java/games/strategy/engine/data/gameparser/TripleAMapValidator.java
+++ game-app/game-core/src/main/java/games/strategy/engine/data/gameparser/TripleAMapValidator.java
@@ -1,6 +1,8 @@
 package games.strategy.engine.data.gameparser;
 
+import games.strategy.engine.data.GameData;
 import java.nio.file.Path;
+import java.util.Optional;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -8,13 +10,18 @@ public class TripleAMapValidator {
   public static void main(String[] args) throws Exception {
     if (args.length == 0) {
       log.warn("Usage: TripleAMapValidator <path-to-game1.xml> (<path-to-game2.xml>) ...");
-      System.exit(1);
+      return;
     }
     for (final String arg : args) {
       try {
         final Path xmlFilePath = Path.of(arg);
-        GameParser.parse(xmlFilePath, false);
-        log.info("✅ Map is valid {}", arg);
+        Optional<GameData> optionalGameData = GameParser.parse(xmlFilePath, false);
+        if (optionalGameData.isPresent()) {
+          log.info("✅ Map is valid {}", arg);
+        } else {
+          log.error("❌ Validation failed for {} ", arg);
+          System.exit(2);
+        }
       } catch (Exception e) {
         log.error("❌ Validation failed for {} ", arg, e);
         System.exit(2);
