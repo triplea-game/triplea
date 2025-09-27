1f31bf2dad0396cc445b07ec374d3186006429b5
frigoref <frigoref@web.de>
2025-09-27 12:25:10 +0200

    Add serialization to StateGameData to fix NotSerializableException

--- game-app/game-core/src/main/java/games/strategy/engine/data/statedata/StateGameData.java
+++ game-app/game-core/src/main/java/games/strategy/engine/data/statedata/StateGameData.java
@@ -2,15 +2,16 @@ package games.strategy.engine.data.statedata;
 
 import games.strategy.engine.data.GameData;
 import games.strategy.engine.history.History;
+import java.io.Serializable;
 import lombok.Getter;
 import lombok.Setter;
 
 /** Part of the {@link GameData} with changeable data for the game. */
-public class StateGameData {
+public class StateGameData implements Serializable {
 
   @Getter @Setter private History gameHistory;
 
   public StateGameData(GameData gameData) {
-    gameHistory = new History(gameData);
+    setGameHistory(new History(gameData));
   }
 }
