4a6bfc5bfa32304858f1e4bc2dcb4d378ae1e630
frigoref <frigoref@web.de>
2025-10-12 22:01:44 +0200

    - All non success cases use System.exit(1) - Exception more specific as InvalidPathException - Add comment to not forget removing logback.xml in the future

--- game-app/game-core/src/main/java/games/strategy/engine/data/gameparser/TripleAMapValidator.java
+++ game-app/game-core/src/main/java/games/strategy/engine/data/gameparser/TripleAMapValidator.java
@@ -1,16 +1,23 @@
 package games.strategy.engine.data.gameparser;
 
 import games.strategy.engine.data.GameData;
+import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
 import java.util.Optional;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class TripleAMapValidator {
-  public static void main(String[] args) throws Exception {
+  /**
+   * Uses a logback.xml to output logs to STDOUT. TODO: Remove the logback.xml when main method is
+   * removed.
+   *
+   * @param args paths to game XML files
+   */
+  public static void main(String[] args) {
     if (args.length == 0) {
       log.warn("Usage: TripleAMapValidator <path-to-game1.xml> (<path-to-game2.xml>) ...");
-      return;
+      System.exit(1);
     }
     for (final String arg : args) {
       try {
@@ -20,11 +27,11 @@ public class TripleAMapValidator {
           log.info("✅ Map is valid {}", arg);
         } else {
           log.error("❌ Validation failed for {} ", arg);
-          System.exit(2);
+          System.exit(1);
         }
-      } catch (Exception e) {
+      } catch (InvalidPathException e) {
         log.error("❌ Validation failed for {} ", arg, e);
-        System.exit(2);
+        System.exit(1);
       }
     }
   }
