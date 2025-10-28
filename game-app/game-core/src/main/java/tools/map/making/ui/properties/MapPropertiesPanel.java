de2ea49c20b62476428df46ad3e025e3dfd04ca2
frigoref <frigoref@web.de>
2025-10-28 19:56:41 +0100

    MapPropertiesPanel: Restructure code (new method addBlockToPanel) (#13809)

--- game-app/game-core/src/main/java/tools/map/making/ui/properties/MapPropertiesPanel.java
+++ game-app/game-core/src/main/java/tools/map/making/ui/properties/MapPropertiesPanel.java
@@ -2,6 +2,7 @@ package tools.map.making.ui.properties;
 
 import games.strategy.triplea.UrlConstants;
 import games.strategy.triplea.image.UnitImageFactory;
+import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
@@ -9,73 +10,75 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
-import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import lombok.experimental.UtilityClass;
 import org.triplea.awt.OpenFileUtility;
+import org.triplea.swing.JButtonBuilder;
+import org.triplea.swing.JTextAreaBuilder;
 import org.triplea.swing.SwingAction;
 import tools.image.FileSave;
 import tools.util.ToolArguments;
 
 @UtilityClass
 public class MapPropertiesPanel {
-  private static int unitWidth = UnitImageFactory.DEFAULT_UNIT_ICON_SIZE;
-  private static int unitHeight = UnitImageFactory.DEFAULT_UNIT_ICON_SIZE;
 
   public JPanel build() {
     final JPanel panel = new JPanel();
     panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
-    panel.add(Box.createVerticalStrut(30));
-    final JTextArea text = new JTextArea(12, 10);
-    text.setWrapStyleWord(true);
-    text.setLineWrap(true);
-    text.setText(
-        "Welcome to Veqryn's map creator program for TripleA."
-            + "\n\nBefore you begin, go create a folder in your directory: "
-            + "Users\\yourname\\triplea\\maps"
-            + "\nName the folder with a short name of your map, do not use any special characters "
-            + "in the name."
-            + "\nNext, create 5 folders inside your map folder, with these names: "
-            + "flags, units, baseTiles, reliefTiles, games"
-            + "\nThen, create a text file and rename it \"map.properties\" or use one created by "
-            + "this utility."
-            + "\n\nTo start the Map Utilities, have a png image of your map with just the "
-            + "territory borders and nothing else. The borders must be in black (hex: 000000) and "
-            + "there should not be any anti-aliasing (smoothing) of the lines or edges that stick "
-            + "out."
-            + "\nCreate a small image of the map (approx 250 pixels wide) and name "
-            + "it \"smallMap.jpeg\"."
-            + "\nPut these in the map's root folder. You can now start the map maker by clicking "
-            + "and filling in the details below, before moving on to 'Step 2' and running the "
-            + "map utilities.");
-    final JScrollPane scrollText = new JScrollPane(text);
-    panel.add(scrollText);
-    panel.add(Box.createVerticalStrut(30));
-    panel.add(new JLabel("Click button open up the readme file on how to make maps:"));
-    final JButton helpButton = new JButton("Start Tutorial  /  Show Help Document");
-    helpButton.addActionListener(e -> OpenFileUtility.openUrl(UrlConstants.MAP_MAKER_HELP));
-    panel.add(helpButton);
-    panel.add(Box.createVerticalStrut(30));
-    panel.add(new JLabel("Click button to select where your map folder is:"));
-    final JButton mapFolderButton = new JButton("Select Map Folder");
-    mapFolderButton.addActionListener(
-        SwingAction.of(
-            "Select Map Folder",
-            e -> {
-              final Path mapFolder =
-                  new FileSave("Where is your map's folder?", null, null).getFile();
-              if (mapFolder != null && Files.exists(mapFolder)) {
-                ToolArguments.MAP_FOLDER.setSystemProperty(mapFolder.toString());
-              }
-            }));
-    panel.add(mapFolderButton);
-    panel.add(Box.createVerticalStrut(30));
-    panel.add(new JLabel("Set the unit scaling (unit image zoom): "));
-    panel.add(new JLabel("Choose one of: 1.25, 1, 0.875, 0.8333, 0.75, 0.6666, 0.5625, 0.5"));
+    addBlockToPanel(panel);
+    addBlockToPanel(
+        panel,
+        new JScrollPane(
+            new JTextAreaBuilder()
+                .rows(12)
+                .columns(10)
+                .text(
+                    "Welcome to Veqryn's map creator program for TripleA."
+                        + "\n\nBefore you begin, go create a folder in your directory: "
+                        + "Users\\yourname\\triplea\\maps"
+                        + "\nName the folder with a short name of your map, do not use any special characters "
+                        + "in the name."
+                        + "\nNext, create 5 folders inside your map folder, with these names: "
+                        + "flags, units, baseTiles, reliefTiles, games"
+                        + "\nThen, create a text file and rename it \"map.properties\" or use one created by "
+                        + "this utility."
+                        + "\n\nTo start the Map Utilities, have a png image of your map with just the "
+                        + "territory borders and nothing else. The borders must be in black (hex: 000000) and "
+                        + "there should not be any anti-aliasing (smoothing) of the lines or edges that stick "
+                        + "out."
+                        + "\nCreate a small image of the map (approx 250 pixels wide) and name "
+                        + "it \"smallMap.jpeg\"."
+                        + "\nPut these in the map's root folder. You can now start the map maker by clicking "
+                        + "and filling in the details below, before moving on to 'Step 2' and running the "
+                        + "map utilities.")
+                .build()));
+    addBlockToPanel(
+        panel,
+        new JLabel("Click button open up the readme file on how to make maps:"),
+        new JButtonBuilder()
+            .title("Start Tutorial  /  Show Help Document")
+            .actionListener(() -> OpenFileUtility.openUrl(UrlConstants.MAP_MAKER_HELP))
+            .build());
+    addBlockToPanel(
+        panel,
+        new JLabel("Click button to select where your map folder is:"),
+        new JButtonBuilder()
+            .title("Select Map Folder")
+            .actionListener(
+                () ->
+                    SwingAction.of(
+                        "Select Map Folder",
+                        e -> {
+                          final Path mapFolder =
+                              new FileSave("Where is your map's folder?", null, null).getFile();
+                          if (mapFolder != null && Files.exists(mapFolder)) {
+                            ToolArguments.MAP_FOLDER.setSystemProperty(mapFolder.toString());
+                          }
+                        }))
+            .build());
 
     final JTextField unitZoomText = new JTextField("0.75");
     unitZoomText.setMaximumSize(new Dimension(100, 20));
@@ -90,10 +93,12 @@ public class MapPropertiesPanel {
             unitZoomText.setText(ToolArguments.UNIT_ZOOM.getSystemPropertyValuePlain());
           }
         });
-    panel.add(unitZoomText);
-    panel.add(Box.createVerticalStrut(30));
-    panel.add(new JLabel("Set the width of the unit images: "));
-    final JTextField unitWidthText = new JTextField("" + unitWidth);
+    addBlockToPanel(
+        panel,
+        new JLabel("Set the unit scaling (unit image zoom): "),
+        new JLabel("Choose one of: 1.25, 1, 0.875, 0.8333, 0.75, 0.6666, 0.5625, 0.5"),
+        unitZoomText);
+    final JTextField unitWidthText = new JTextField(UnitImageFactory.DEFAULT_UNIT_ICON_SIZE);
     unitWidthText.setMaximumSize(new Dimension(100, 20));
     unitWidthText.addFocusListener(
         new FocusListener() {
@@ -106,10 +111,8 @@ public class MapPropertiesPanel {
             unitWidthText.setText(ToolArguments.UNIT_WIDTH.getSystemPropertyValuePlain());
           }
         });
-    panel.add(unitWidthText);
-    panel.add(Box.createVerticalStrut(30));
-    panel.add(new JLabel("Set the height of the unit images: "));
-    final JTextField unitHeightText = new JTextField("" + unitHeight);
+    addBlockToPanel(panel, new JLabel("Set the width of the unit images: "), unitWidthText);
+    final JTextField unitHeightText = new JTextField(UnitImageFactory.DEFAULT_UNIT_ICON_SIZE);
     unitHeightText.setMaximumSize(new Dimension(100, 20));
     unitHeightText.addFocusListener(
         new FocusListener() {
@@ -122,8 +125,14 @@ public class MapPropertiesPanel {
             unitHeightText.setText(ToolArguments.UNIT_HEIGHT.getSystemPropertyValuePlain());
           }
         });
-    panel.add(unitHeightText);
-    panel.add(Box.createVerticalStrut(30));
+    addBlockToPanel(panel, new JLabel("Set the height of the unit images: "), unitHeightText);
     return panel;
+  }
+
+  private static void addBlockToPanel(JPanel panel, Component... components) {
+    for (Component component : components) {
+      panel.add(component);
+    }
+    panel.add(Box.createVerticalStrut(30));
   }
 }
