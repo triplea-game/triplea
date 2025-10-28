ab2f82fe9fc26363f0956836d895fbfc4217d217
frigoref <frigoref@web.de>
2025-10-28 22:27:57 +0100

    MapPropertiesPanel: Refit to panel (#13810)

--- game-app/game-core/src/main/java/tools/map/making/ui/properties/MapPropertiesPanel.java
+++ game-app/game-core/src/main/java/tools/map/making/ui/properties/MapPropertiesPanel.java
@@ -10,6 +10,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
+import javax.swing.JComponent;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
@@ -19,11 +20,14 @@ import org.triplea.awt.OpenFileUtility;
 import org.triplea.swing.JButtonBuilder;
 import org.triplea.swing.JTextAreaBuilder;
 import org.triplea.swing.SwingAction;
+import org.triplea.swing.jpanel.JPanelBuilder;
 import tools.image.FileSave;
 import tools.util.ToolArguments;
 
 @UtilityClass
 public class MapPropertiesPanel {
+
+  private static final int PANEL_LINE_SPACING = 23;
 
   public JPanel build() {
     final JPanel panel = new JPanel();
@@ -33,38 +37,47 @@ public class MapPropertiesPanel {
         panel,
         new JScrollPane(
             new JTextAreaBuilder()
-                .rows(12)
-                .columns(10)
+                .rows(15)
+                .columns(45)
                 .text(
-                    "Welcome to Veqryn's map creator program for TripleA."
-                        + "\n\nBefore you begin, go create a folder in your directory: "
-                        + "Users\\yourname\\triplea\\maps"
-                        + "\nName the folder with a short name of your map, do not use any special characters "
-                        + "in the name."
-                        + "\nNext, create 5 folders inside your map folder, with these names: "
-                        + "flags, units, baseTiles, reliefTiles, games"
-                        + "\nThen, create a text file and rename it \"map.properties\" or use one created by "
-                        + "this utility."
-                        + "\n\nTo start the Map Utilities, have a png image of your map with just the "
-                        + "territory borders and nothing else. The borders must be in black (hex: 000000) and "
-                        + "there should not be any anti-aliasing (smoothing) of the lines or edges that stick "
-                        + "out."
-                        + "\nCreate a small image of the map (approx 250 pixels wide) and name "
-                        + "it \"smallMap.jpeg\"."
-                        + "\nPut these in the map's root folder. You can now start the map maker by clicking "
-                        + "and filling in the details below, before moving on to 'Step 2' and running the "
-                        + "map utilities.")
+                    """
+                        Welcome to Veqryn's map creator program for TripleA.\
+
+
+                        Before you begin, go create a folder in your directory: \
+                        Users\\yourname\\triplea\\maps\
+
+                        Name the folder with a short name of your map, do not use any special characters \
+                        in the name.\
+
+                        Next, create 5 folders inside your map folder, with these names: \
+                        flags, units, baseTiles, reliefTiles, games\
+
+                        Then, create a text file and rename it "map.properties" or use one created by \
+                        this utility.\
+
+
+                        To start the Map Utilities, have a png image of your map with just the \
+                        territory borders and nothing else. The borders must be in black (hex: 000000) and \
+                        there should not be any anti-aliasing (smoothing) of the lines or edges that stick \
+                        out.\
+
+                        Create a small image of the map (approx 250 pixels wide) and name \
+                        it "smallMap.jpeg".\
+
+                        Put these in the map's root folder. You can now start the map maker by clicking \
+                        and filling in the details below, before moving on to 'Step 2' and running the \
+                        map utilities.""")
                 .build()));
     addBlockToPanel(
         panel,
-        new JLabel("Click button open up the readme file on how to make maps:"),
         new JButtonBuilder()
             .title("Start Tutorial  /  Show Help Document")
+            .toolTip("Open up the readme file on how to make maps")
             .actionListener(() -> OpenFileUtility.openUrl(UrlConstants.MAP_MAKER_HELP))
             .build());
     addBlockToPanel(
         panel,
-        new JLabel("Click button to select where your map folder is:"),
         new JButtonBuilder()
             .title("Select Map Folder")
             .actionListener(
@@ -79,60 +92,67 @@ public class MapPropertiesPanel {
                           }
                         }))
             .build());
-
-    final JTextField unitZoomText = new JTextField("0.75");
-    unitZoomText.setMaximumSize(new Dimension(100, 20));
-    unitZoomText.addFocusListener(
-        new FocusListener() {
-          @Override
-          public void focusGained(final FocusEvent e) {}
-
-          @Override
-          public void focusLost(final FocusEvent e) {
-            ToolArguments.UNIT_ZOOM.setSystemProperty(unitZoomText.getText());
-            unitZoomText.setText(ToolArguments.UNIT_ZOOM.getSystemPropertyValuePlain());
-          }
-        });
-    addBlockToPanel(
+    addInputFieldToPanel(
         panel,
-        new JLabel("Set the unit scaling (unit image zoom): "),
-        new JLabel("Choose one of: 1.25, 1, 0.875, 0.8333, 0.75, 0.6666, 0.5625, 0.5"),
-        unitZoomText);
-    final JTextField unitWidthText = new JTextField(UnitImageFactory.DEFAULT_UNIT_ICON_SIZE);
-    unitWidthText.setMaximumSize(new Dimension(100, 20));
-    unitWidthText.addFocusListener(
-        new FocusListener() {
-          @Override
-          public void focusGained(final FocusEvent e) {}
-
-          @Override
-          public void focusLost(final FocusEvent e) {
-            ToolArguments.UNIT_WIDTH.setSystemProperty(unitWidthText.getText());
-            unitWidthText.setText(ToolArguments.UNIT_WIDTH.getSystemPropertyValuePlain());
-          }
-        });
-    addBlockToPanel(panel, new JLabel("Set the width of the unit images: "), unitWidthText);
-    final JTextField unitHeightText = new JTextField(UnitImageFactory.DEFAULT_UNIT_ICON_SIZE);
-    unitHeightText.setMaximumSize(new Dimension(100, 20));
-    unitHeightText.addFocusListener(
-        new FocusListener() {
-          @Override
-          public void focusGained(final FocusEvent e) {}
-
-          @Override
-          public void focusLost(final FocusEvent e) {
-            ToolArguments.UNIT_HEIGHT.setSystemProperty(unitHeightText.getText());
-            unitHeightText.setText(ToolArguments.UNIT_HEIGHT.getSystemPropertyValuePlain());
-          }
-        });
-    addBlockToPanel(panel, new JLabel("Set the height of the unit images: "), unitHeightText);
+        "Unit scaling: ",
+        "Choose a scaling factor for the unit image.\nExamples: 1.25, 1, 0.875, 0.8333, 0.75, 0.6666, 0.5625, 0.5",
+        "0.75",
+        ToolArguments.UNIT_ZOOM);
+    addInputFieldToPanel(
+        panel,
+        "Unit width: ",
+        "Set width of the unit image",
+        Integer.toString(UnitImageFactory.DEFAULT_UNIT_ICON_SIZE),
+        ToolArguments.UNIT_WIDTH);
+    addInputFieldToPanel(
+        panel,
+        "Unit height: ",
+        "Set height of the unit image",
+        Integer.toString(UnitImageFactory.DEFAULT_UNIT_ICON_SIZE),
+        ToolArguments.UNIT_HEIGHT);
     return panel;
   }
 
-  private static void addBlockToPanel(JPanel panel, Component... components) {
-    for (Component component : components) {
+  private static void addInputFieldToPanel(
+      JPanel panel, String label, String tooltip, String text, ToolArguments toolsArgument) {
+    final JTextField toolsArgumentText = new JTextField(text);
+    Dimension fieldDimension = new Dimension(45, 20);
+    toolsArgumentText.setPreferredSize(fieldDimension);
+    toolsArgumentText.setMinimumSize(fieldDimension);
+    toolsArgumentText.setMaximumSize(fieldDimension);
+    toolsArgumentText.addFocusListener(
+        new FocusListener() {
+          @Override
+          public void focusGained(final FocusEvent e) {
+            /* interface method, but nothing to be done when gaining focus */
+          }
+
+          @Override
+          public void focusLost(final FocusEvent e) {
+            toolsArgument.setSystemProperty(toolsArgumentText.getText());
+            toolsArgumentText.setText(toolsArgument.getSystemPropertyValuePlain());
+          }
+        });
+    toolsArgumentText.setToolTipText(tooltip);
+    JLabel textFieldLabel = new JLabel(label);
+    Dimension labelDimension = new Dimension(80, 20);
+    textFieldLabel.setPreferredSize(labelDimension);
+    textFieldLabel.setMaximumSize(labelDimension);
+    textFieldLabel.setMinimumSize(labelDimension);
+    addBlockToPanel(
+        panel,
+        new JPanelBuilder()
+            .boxLayoutHorizontal()
+            .add(textFieldLabel)
+            .add(toolsArgumentText)
+            .build());
+  }
+
+  private static void addBlockToPanel(JPanel panel, JComponent... components) {
+    for (JComponent component : components) {
+      component.setAlignmentX(Component.LEFT_ALIGNMENT);
       panel.add(component);
     }
-    panel.add(Box.createVerticalStrut(30));
+    panel.add(Box.createVerticalStrut(PANEL_LINE_SPACING));
   }
 }
