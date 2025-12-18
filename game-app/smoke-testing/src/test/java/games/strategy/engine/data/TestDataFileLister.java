1764201bf205a33ebaa34f43d91a3776d10bc31a
Dan Van Atta <dhvatta@gmail.com>
2025-12-17 17:19:51 -0800

    Revert "Re-update to Gradle 9.2.1 (#13954)" (#13960)
    This reverts commit b6b75dc577e509d3b6e2503b0e5fbdf498e5160a.

--- game-app/smoke-testing/src/test/java/games/strategy/engine/data/TestDataFileLister.java
+++ game-app/smoke-testing/src/test/java/games/strategy/engine/data/TestDataFileLister.java
@@ -1,50 +1,43 @@
 package games.strategy.engine.data;
 
-import java.io.File;
 import java.io.IOException;
-import java.net.URISyntaxException;
-import java.net.URL;
+import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Comparator;
-import java.util.List;
 import java.util.stream.Collectors;
 import lombok.experimental.UtilityClass;
 import org.jetbrains.annotations.NonNls;
+import org.triplea.java.Postconditions;
 
 @UtilityClass
 class TestDataFileLister {
 
   /**
-   * Returns a list of files located in folder located at the root of the classpath.
+   * Returns a list of files located in folder like: 'test/resources/{folder-name}'.
    *
-   * @param folderName Name of the folder on the classpath
-   * @return List of all files located in specified folder
+   * @param folderName Name of the folder in 'test/resources'
+   * @return List of all files located in specified folder.
    */
-  Collection<Path> listFilesInTestClasspathDir(final @NonNls String folderName) throws IOException {
-    URL url = TestDataFileLister.class.getClassLoader().getResource(folderName);
-    if (url == null) {
-      throw new IllegalStateException("Folder " + folderName + " not found on classpath");
-    }
-
-    File folder;
-    try {
-      folder = new File(url.toURI());
-    } catch (URISyntaxException e) {
-      throw new IOException(e);
-    }
-    File[] files = folder.listFiles();
-
-    List<Path> results = new ArrayList<>();
-    if (files != null) {
-      for (File f : files) {
-        results.add(f.toPath());
-      }
-    }
-
-    return results.stream()
+  Collection<Path> listFilesInTestResourcesDirectory(final @NonNls String folderName)
+      throws IOException {
+    return Files.list(findFilesInFolder(folderName))
         .sorted(Comparator.comparing(Path::getFileName, Comparator.comparing(Path::toString)))
         .collect(Collectors.toList());
+  }
+
+  private static Path findFilesInFolder(final String folderName) {
+    Path resourcesFolder = Path.of("src", "test", "resources", folderName);
+    if (!Files.exists(resourcesFolder)) {
+      resourcesFolder = Path.of("smoke-testing", "src", "test", "resources", folderName);
+    }
+    if (!Files.exists(resourcesFolder)) {
+      resourcesFolder =
+          Path.of("game-app", "smoke-testing", "src", "test", "resources", folderName);
+    }
+
+    Postconditions.assertState(
+        Files.exists(resourcesFolder), "Could not find folder: " + folderName);
+    return resourcesFolder;
   }
 }
