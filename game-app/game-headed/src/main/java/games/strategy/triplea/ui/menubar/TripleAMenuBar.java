ba231ec52245f1deda5ecd0a4abd8421ca712bb7
frigoref <frigoref@web.de>
2026-01-04 17:37:34 +0100

    FileMenu as UtilityClass

--- game-app/game-headed/src/main/java/games/strategy/triplea/ui/menubar/TripleAMenuBar.java
+++ game-app/game-headed/src/main/java/games/strategy/triplea/ui/menubar/TripleAMenuBar.java
@@ -20,7 +20,7 @@ public final class TripleAMenuBar extends JMenuBar {
   public TripleAMenuBar(final TripleAFrame frame) {
     this.frame = frame;
 
-    add(new FileMenu(frame));
+    FileMenu.addTo(frame);
     add(new ViewMenu(frame));
     add(new GameMenu(frame));
     add(new ExportMenu(frame));
