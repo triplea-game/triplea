c4bfe3f8e6b8329c7ca1f01f7de99e55f2632e77
Aashiv Muthu Suthan S <aashivstudies05@gmail.com>
2026-01-14 02:25:38 +0530

    Apply code formatting with spotlessJavaApply

--- game-app/game-core/src/main/java/games/strategy/triplea/ui/unit/scroller/UnitScrollerModel.java
+++ game-app/game-core/src/main/java/games/strategy/triplea/ui/unit/scroller/UnitScrollerModel.java
@@ -17,52 +17,53 @@ import org.triplea.java.PredicateBuilder;
 /** Contains logic operations needed by the unit scroller. */
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 class UnitScrollerModel {
-    static List<Unit> getMoveableUnits(
-            final Territory t,
-            final UnitScroller.MovePhase movePhase,
-            final GamePlayer player,
-            final Collection<Unit> skippedUnits) {
-        Preconditions.checkNotNull(t);
+  static List<Unit> getMoveableUnits(
+      final Territory t,
+      final UnitScroller.MovePhase movePhase,
+      final GamePlayer player,
+      final Collection<Unit> skippedUnits) {
+    Preconditions.checkNotNull(t);
 
-        final Predicate<Unit> moveableUnitOwnedByMe = PredicateBuilder.of(Matches.unitIsOwnedBy(player))
-                .and(
-                        u ->
-                        // Unit has movement left
-                        Matches.unitHasMovementLeft().test(u)
-                                // Unit is already being transported
-                                // TODO: Check if transporting unit has movement left for sea transports
-                                || Matches.unitIsBeingTransported().test(u)
-                                // Unit has not moved and can be loaded onto an available transport in this
-                                // territory
-                                || (Matches.unitHasNotMoved().test(u)
-                                        && Matches.unitCanBeTransported().test(u)
-                                        && t.anyUnitsMatch(
-                                                Matches.unitCanTransport().and(Matches.unitIsOwnedBy(player)))))
-                // if not non combat, cannot move aa units
-                .andIf(
-                        movePhase == UnitScroller.MovePhase.COMBAT, Matches.unitCanMoveDuringCombatMove())
-                .build();
-        final List<Unit> units = t.getMatches(moveableUnitOwnedByMe);
-        units.removeAll(skippedUnits);
-        return units;
-    }
+    final Predicate<Unit> moveableUnitOwnedByMe =
+        PredicateBuilder.of(Matches.unitIsOwnedBy(player))
+            .and(
+                u ->
+                    // Unit has movement left
+                    Matches.unitHasMovementLeft().test(u)
+                        // Unit is already being transported
+                        // TODO: Check if transporting unit has movement left for sea transports
+                        || Matches.unitIsBeingTransported().test(u)
+                        // Unit has not moved and can be loaded onto an available transport in this
+                        // territory
+                        || (Matches.unitHasNotMoved().test(u)
+                            && Matches.unitCanBeTransported().test(u)
+                            && t.anyUnitsMatch(
+                                Matches.unitCanTransport().and(Matches.unitIsOwnedBy(player)))))
+            // if not non combat, cannot move aa units
+            .andIf(
+                movePhase == UnitScroller.MovePhase.COMBAT, Matches.unitCanMoveDuringCombatMove())
+            .build();
+    final List<Unit> units = t.getMatches(moveableUnitOwnedByMe);
+    units.removeAll(skippedUnits);
+    return units;
+  }
 
-    static int computeUnitsToMoveCount(
-            final Collection<Territory> territories,
-            final UnitScroller.MovePhase movePhase,
-            final GamePlayer player,
-            final Collection<Unit> skippedUnits) {
-        return territories.stream()
-                .map(t -> getMoveableUnits(t, movePhase, player, skippedUnits))
-                .mapToInt(List::size)
-                .sum();
-    }
+  static int computeUnitsToMoveCount(
+      final Collection<Territory> territories,
+      final UnitScroller.MovePhase movePhase,
+      final GamePlayer player,
+      final Collection<Unit> skippedUnits) {
+    return territories.stream()
+        .map(t -> getMoveableUnits(t, movePhase, player, skippedUnits))
+        .mapToInt(List::size)
+        .sum();
+  }
 
-    static List<UnitCategory> getUniqueUnitCategories(
-            final GamePlayer player, final List<Unit> units) {
-        return units.stream()
-                .map(unit -> new UnitCategory(unit.getType(), player))
-                .distinct()
-                .collect(Collectors.toList());
-    }
+  static List<UnitCategory> getUniqueUnitCategories(
+      final GamePlayer player, final List<Unit> units) {
+    return units.stream()
+        .map(unit -> new UnitCategory(unit.getType(), player))
+        .distinct()
+        .collect(Collectors.toList());
+  }
 }
