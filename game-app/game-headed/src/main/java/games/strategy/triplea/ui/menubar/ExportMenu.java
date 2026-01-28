6b95df3fc0feaf4145e87608714510e96c39dbf8
frigoref <frigoref@web.de>
2026-01-28 15:13:19 +0100

    printgenerator: Extract StatsInfo from ExportMenu

--- game-app/game-headed/src/main/java/games/strategy/triplea/ui/menubar/ExportMenu.java
+++ game-app/game-headed/src/main/java/games/strategy/triplea/ui/menubar/ExportMenu.java
@@ -1,30 +1,18 @@
 package games.strategy.triplea.ui.menubar;
 
-import com.google.common.collect.Iterables;
 import games.strategy.engine.data.GameData;
-import games.strategy.engine.data.GamePlayer;
-import games.strategy.engine.data.ProductionRule;
-import games.strategy.engine.data.Resource;
-import games.strategy.engine.data.UnitType;
 import games.strategy.engine.data.export.GameDataExporter;
 import games.strategy.engine.framework.GameDataManager;
 import games.strategy.engine.framework.GameDataUtils;
-import games.strategy.engine.framework.system.SystemProperties;
+import games.strategy.engine.history.History;
 import games.strategy.engine.history.HistoryNode;
-import games.strategy.engine.history.Round;
-import games.strategy.engine.history.Step;
-import games.strategy.engine.stats.IStat;
-import games.strategy.triplea.attachments.UnitAttachment;
-import games.strategy.triplea.delegate.EndRoundDelegate;
 import games.strategy.triplea.printgenerator.SetupFrame;
-import games.strategy.triplea.ui.ExtendedStats;
+import games.strategy.triplea.printgenerator.StatsInfo;
 import games.strategy.triplea.ui.TripleAFrame;
-import games.strategy.triplea.ui.UiContext;
 import games.strategy.triplea.ui.export.ScreenshotExporter;
 import games.strategy.triplea.ui.history.HistoryPanel;
 import games.strategy.triplea.ui.menubar.help.UnitStatsTable;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.io.Writer;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -32,26 +20,19 @@ import java.nio.file.Path;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
-import java.util.Collection;
-import java.util.Enumeration;
-import java.util.List;
 import java.util.Optional;
-import java.util.Set;
+import java.util.concurrent.CompletableFuture;
 import javax.annotation.Nonnull;
 import javax.swing.JComponent;
-import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JOptionPane;
 import javax.swing.WindowConstants;
-import javax.swing.tree.DefaultMutableTreeNode;
-import javax.swing.tree.TreeNode;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
-import org.triplea.config.product.ProductVersionReader;
 import org.triplea.map.data.elements.Game;
 import org.triplea.map.xml.writer.GameXmlWriter;
 import org.triplea.swing.FileChooser;
@@ -65,23 +46,6 @@ import org.triplea.util.FileNameUtils;
 final class ExportMenu {
 
   private static final String DATE_TIME_FORMAT_FILE_NAME = "yyyy_MM_dd";
-
-  @AllArgsConstructor(access = AccessLevel.PRIVATE)
-  @Getter
-  public enum Mnemonic {
-    EXPORT_PICTURE(KeyCode.E),
-    EXPORT_STATS_FULL(KeyCode.F),
-    EXPORT_STATS_SHORT(KeyCode.S),
-    EXPORT_XML(KeyCode.X),
-    EXPORT_CHARTS_UNIT(KeyCode.U),
-    EXPORT_CHARTS_SETUP(KeyCode.C);
-
-    private final KeyCode mnemonicCode;
-
-    public int getValue() {
-      return mnemonicCode.getInputEventCode();
-    }
-  }
 
   public static JMenu get(final TripleAFrame frame) {
     return new JMenuBuilder("Export", TripleAMenuBar.Mnemonic.EXPORT.getMnemonicCode())
@@ -160,222 +124,38 @@ final class ExportMenu {
     ScreenshotExporter.exportScreenshot(frame, gameData, curNode);
   }
 
-  private static void createAndSaveStats(final TripleAFrame frame, final boolean showPhaseStats) {
-    final GameData gameData = frame.getGame().getData();
-    final UiContext uiContext = frame.getUiContext();
-    final ExtendedStats statPanel = new ExtendedStats(gameData, uiContext);
-    final JFileChooser chooser = new JFileChooser();
-    chooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
-    final Path rootDir = Path.of(SystemProperties.getUserDir());
-    final int currentRound = gameData.getCurrentRound();
+  private void createAndSaveStats(final TripleAFrame frame, final boolean showPhaseStats) {
+    GameData gameData = frame.getGame().getData();
     final String defaultFileName =
         FileNameUtils.removeIllegalCharacters(
                 String.format(
                     "stats_%s_%s_round_%s_%s",
                     getCurrentDateTimeForFileName(),
                     gameData.getGameName(),
-                    currentRound,
+                    gameData.getCurrentRound(),
                     showPhaseStats ? "full" : "short"))
             + ".csv";
-    chooser.setSelectedFile(rootDir.resolve(defaultFileName).toFile());
-    if (chooser.showSaveDialog(frame) != JOptionPane.OK_OPTION) {
-      return;
-    }
-    final var cloneOptions = GameDataManager.Options.builder().withHistory(true).build();
-    final GameData clone = GameDataUtils.cloneGameData(gameData, cloneOptions).orElse(null);
-    if (clone == null) {
-      return;
-    }
-    try (PrintWriter writer = new PrintWriter(chooser.getSelectedFile(), StandardCharsets.UTF_8);
-        GameData.Unlocker ignored = gameData.acquireReadLock()) {
-      writer.append(defaultFileName).println(',');
-      writer.append("TripleA Engine Version: ,");
-      writer.append(ProductVersionReader.getCurrentVersion().toString()).println(',');
-      writer.append("Game Name: ,");
-      writer.append(gameData.getGameName()).println(',');
-      writer.println();
-      writer.append("Current Round: ,");
-      writer.print(currentRound);
-      writer.println(',');
-      writer.append("Number of Players: ,");
-      writer.print(gameData.getPlayerList().size());
-      writer.println(',');
-      writer.append("Number of Alliances: ,");
-      writer.print(gameData.getAllianceTracker().getAlliances().size());
-      writer.println(',');
-      writer.println();
-      writer.println("Turn Order: ,");
-      final List<GamePlayer> orderedPlayers = gameData.getPlayerList().getSortedPlayers();
-      for (final GamePlayer currentGamePlayer : orderedPlayers) {
-        writer.append(currentGamePlayer.getName()).append(',');
-        final Collection<String> allianceNames =
-            gameData.getAllianceTracker().getAlliancesPlayerIsIn(currentGamePlayer);
-        for (final String allianceName : allianceNames) {
-          writer.append(allianceName).append(',');
-        }
-        writer.println();
-      }
-      writer.println();
-      writer.append("Winners: ,");
-      final EndRoundDelegate delegateEndRound =
-          (EndRoundDelegate) gameData.getDelegateOptional("endRound").orElse(null);
-      if (delegateEndRound != null && delegateEndRound.getWinners() != null) {
-        for (final GamePlayer p : delegateEndRound.getWinners()) {
-          writer.append(p.getName()).append(',');
-        }
-      } else {
-        writer.append("none yet; game not over,");
-      }
-      writer.println();
-      writer.println();
-      writer.println("Resource Chart: ,");
-      for (final Resource resource : gameData.getResourceList().getResources()) {
-        writer.append(resource.getName()).println(',');
-      }
-      // if short, we won't both showing production and unit info
-      if (showPhaseStats) {
-        writer.println();
-        writer.println("Production Rules: ,");
-        writer.append("Name,Result,Quantity,Cost,Resource,\n");
-        final Collection<ProductionRule> purchaseOptions =
-            gameData.getProductionRuleList().getProductionRules();
-        for (final ProductionRule pr : purchaseOptions) {
-          final String costString = pr.toStringCosts().replaceAll(";? ", ",");
-          writer.append(pr.getName()).append(',');
-          writer.append(pr.getAnyResultKey().getName()).append(',');
-          writer.print(pr.getResults().getInt(pr.getAnyResultKey()));
-          writer.append(',').append(costString).println(',');
-        }
-        writer.println();
-        writer.println("Unit Types: ,");
-        writer.append("Name,Listed Abilities\n");
-        for (final UnitType unitType : gameData.getUnitTypeList()) {
-          final UnitAttachment ua = unitType.getUnitAttachment();
-          if (ua == null) {
-            continue;
-          }
-          final String toModify =
-              ua.allUnitStatsForExporter()
-                  .replaceAll("UnitType called | with:|games\\.strategy\\.engine\\.data\\.", "")
-                  .replaceAll("[\n,]", ";")
-                  .replaceAll(" {2}| ?, ?", ",");
-          writer.println(toModify);
-        }
-      }
-      writer.println();
-      writer.println(
-          showPhaseStats
-              ? "Full Stats (includes each phase that had activity),"
-              : "Short Stats (only shows first phase with activity per player per round),");
-      writer.println("Turn Stats: ,");
-      writer.append("Round,Player Turn,Phase Name,");
-      final Set<String> alliances = gameData.getAllianceTracker().getAlliances();
-      // its important here to use the player objects from the cloned game data
-      // the players for the stat panel are only relevant with respect to the game data they belong
-      // to
-      final List<GamePlayer> players = clone.getPlayerList().getSortedPlayers();
-
-      // extended stats covers stuff that doesn't show up in the game stats menu bar, like custom
-      // resources or tech  tokens or # techs, etc.
-      final Iterable<IStat> stats =
-          Iterables.concat(
-              List.of(statPanel.getStats()), List.of(statPanel.getStatsExtended(gameData)));
-      for (final IStat stat : stats) {
-        for (final GamePlayer player : players) {
-          writer.append(stat.getName()).append(' ');
-          writer.append(player.getName()).append(',');
-        }
-        for (final String alliance : alliances) {
-          writer.append(stat.getName()).append(' ');
-          writer.append(alliance).append(',');
-        }
-      }
-      writer.println();
-      clone.getHistory().enableSeeking(null);
-      clone.getHistory().gotoNode(clone.getHistory().getLastNode());
-      final Enumeration<TreeNode> nodes =
-          ((DefaultMutableTreeNode) clone.getHistory().getRoot()).preorderEnumeration();
-      Optional<GamePlayer> optionalCurrentPlayer = Optional.empty();
-      int round = 0;
-      while (nodes.hasMoreElements()) {
-        // we want to export on change of turn
-        final HistoryNode element = (HistoryNode) nodes.nextElement();
-        if (element instanceof Round) {
-          round++;
-        }
-        if (!(element instanceof Step step)) {
-          continue;
-        }
-        final Optional<GamePlayer> optionalStepPlayer = step.getPlayerId();
-        if (optionalStepPlayer.isEmpty() || optionalStepPlayer.get().isNull()) {
-          continue;
-        }
-        // this is to stop from having multiple entries for each players turn.
-        if (!showPhaseStats
-            && optionalStepPlayer.get().equals(optionalCurrentPlayer.orElse(null))) {
-          continue;
-        }
-        optionalCurrentPlayer = step.getPlayerId();
-        clone.getHistory().gotoNode(element);
-        final String playerName =
-            optionalCurrentPlayer.map(gamePlayer -> gamePlayer.getName() + ": ").orElse("");
-        String stepName = getStepName(step);
-        writer.print(round);
-        writer.append(',').append(playerName).append(',').append(stepName).append(',');
-        for (final IStat stat : stats) {
-          for (final GamePlayer player : players) {
-            writer
-                .append(
-                    IStat.DECIMAL_FORMAT.format(
-                        stat.getValue(player, clone, uiContext.getMapData())))
-                .append(',');
-          }
-          for (final String alliance : alliances) {
-            writer
-                .append(
-                    IStat.DECIMAL_FORMAT.format(
-                        stat.getValue(alliance, clone, uiContext.getMapData())))
-                .append(',');
-          }
-        }
-        writer.println();
-      }
-    } catch (final IOException e) {
-      log.error("Failed to write stats: " + chooser.getSelectedFile().getAbsolutePath(), e);
-    }
+    final CompletableFuture<GameData> cloneCompletableFuture =
+        getGameDataCloneWithHistory(gameData);
+    Optional<Path> chosenFilePath =
+        FileChooser.chooseExportFileWithDefaultName(frame, defaultFileName);
+    chosenFilePath.ifPresent(
+        path ->
+            StatsInfo.export(
+                path, frame.getUiContext(), showPhaseStats, cloneCompletableFuture.join()));
   }
 
-  @Nonnull
-  private static String getStepName(Step step) {
-    String stepName = step.getStepName();
-    // copied directly from TripleAPlayer, will probably have to be updated in the future if
-    // more delegates are made
-    if (stepName.endsWith("Bid")) {
-      stepName = "Bid";
-    } else if (stepName.endsWith("Tech")) {
-      stepName = "Tech";
-    } else if (stepName.endsWith("TechActivation")) {
-      stepName = "TechActivation";
-    } else if (stepName.endsWith("Purchase")) {
-      stepName = "Purchase";
-    } else if (stepName.endsWith("NonCombatMove")) {
-      stepName = "NonCombatMove";
-    } else if (stepName.endsWith("Move")) {
-      stepName = "Move";
-    } else if (stepName.endsWith("Battle")) {
-      stepName = "Battle";
-    } else if (stepName.endsWith("BidPlace")) {
-      stepName = "BidPlace";
-    } else if (stepName.endsWith("Place")) {
-      stepName = "Place";
-    } else if (stepName.endsWith("Politics")) {
-      stepName = "Politics";
-    } else if (stepName.endsWith("EndTurn")) {
-      stepName = "EndTurn";
-    } else {
-      stepName = "";
-    }
-    return stepName;
+  /**
+   * Provides a game data copy with its history by triggering an asynchronous process for the
+   * copying task.
+   *
+   * @return {@link GameData} clone as {@link CompletableFuture} incl. {@link History} which has
+   *     already {@link History#enableSeeking(HistoryPanel)} called. Fails with {@link
+   *     IllegalStateException} in case the cloning fails.
+   */
+  private CompletableFuture<GameData> getGameDataCloneWithHistory(final GameData gameData) {
+    return CompletableFuture.supplyAsync(
+        () -> GameDataUtils.cloneGameDataWithHistory(gameData, true));
   }
 
   private static void exportUnitCharts(final TripleAFrame frame) {
@@ -414,5 +194,22 @@ final class ExportMenu {
     chartsFrame.setLocationRelativeTo(chartsFrame);
     chartsFrame.setVisible(true);
     frame.getUiContext().addShutdownWindow(chartsFrame);
+  }
+
+  @AllArgsConstructor(access = AccessLevel.PRIVATE)
+  @Getter
+  public enum Mnemonic {
+    EXPORT_PICTURE(KeyCode.E),
+    EXPORT_STATS_FULL(KeyCode.F),
+    EXPORT_STATS_SHORT(KeyCode.S),
+    EXPORT_XML(KeyCode.X),
+    EXPORT_CHARTS_UNIT(KeyCode.U),
+    EXPORT_CHARTS_SETUP(KeyCode.C);
+
+    private final KeyCode mnemonicCode;
+
+    public int getValue() {
+      return mnemonicCode.getInputEventCode();
+    }
   }
 }
