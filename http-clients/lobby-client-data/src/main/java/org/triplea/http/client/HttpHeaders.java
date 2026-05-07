eb1ab64050cbe4764dbba0b7e39052ad5153fa43
DanVanAtta <dhvatta@gmail.com>
2026-05-07 10:09:21 -0700

    Extract HttpHeaders to lobby-client-data package so they can be shared
    Allows servers access to the shared HTTP header constants, notably
    by adding those constants to the shared library between server & client.

---
+++ http-clients/lobby-client-data/src/main/java/org/triplea/http/client/HttpHeaders.java
@@ -0,0 +1,9 @@
+package org.triplea.http.client;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class HttpHeaders {
+  public static final String VERSION_HEADER = "Triplea-Version";
+  public static final String SYSTEM_ID_HEADER = "System-Id";
+}
