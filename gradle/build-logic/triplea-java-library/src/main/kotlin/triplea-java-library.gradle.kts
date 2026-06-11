3474b14005d666297d8d88a2802d3245e0bf95d5
Dan Van Atta <dhvatta@gmail.com>
2026-06-10 18:18:58 -0700

    Build config: re-order expand wildcard to be below java format (#14564)
    Seems to resolve error:
    > LINE_UNDEFINED expandwildcardimports(java.lang.IllegalStateException) There was a problem interacting with Java-Parser; maybe you set an incompatible version? (...)

--- gradle/build-logic/triplea-java-library/src/main/kotlin/triplea-java-library.gradle.kts
+++ gradle/build-logic/triplea-java-library/src/main/kotlin/triplea-java-library.gradle.kts
@@ -26,8 +26,8 @@ spotless {
     }
 
     java {
-        expandWildcardImports()
         googleJavaFormat()
+        expandWildcardImports()
         removeUnusedImports()
         cleanthat()
             .sourceCompatibility("21")
