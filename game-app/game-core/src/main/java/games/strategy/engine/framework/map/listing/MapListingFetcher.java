bd13c5368d0dce425a54330c1c3909715a5e851f
DanVanAtta <dhvatta@gmail.com>
2026-06-10 20:51:22 -0700

    Update to always use maps server, stop using triplea_maps.yaml

--- game-app/game-core/src/main/java/games/strategy/engine/framework/map/listing/MapListingFetcher.java
+++ game-app/game-core/src/main/java/games/strategy/engine/framework/map/listing/MapListingFetcher.java
@@ -1,11 +1,7 @@
 package games.strategy.engine.framework.map.listing;
 
 import feign.FeignException;
-import games.strategy.engine.framework.map.download.DownloadConfiguration;
-import games.strategy.engine.framework.map.download.DownloadFileDescription;
-import games.strategy.engine.framework.map.download.DownloadFileParser;
 import games.strategy.triplea.settings.ClientSetting;
-import java.util.ArrayList;
 import java.util.List;
 import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
@@ -17,15 +13,8 @@ import org.triplea.http.client.maps.listing.MapsClient;
 @UtilityClass
 @Slf4j
 public class MapListingFetcher {
-  private static String GITHUB_MAP_DOWNLOAD_LIST =
-      "https://raw.githubusercontent.com/triplea-game/triplea/master/triplea_maps.yaml";
-
   /** Fetches the full listing of maps that are available for download. */
   public static List<MapDownloadItem> getMapDownloadList() {
-    if (!ClientSetting.useMapsServerBetaFeature.getValue().orElse(false)) {
-      return getMapDownloadListFromGithub();
-    }
-
     final var serverUri = ClientSetting.lobbyUri.getValueOrThrow();
     try {
       return MapsClient.newClient(
@@ -41,28 +30,5 @@ You can download the needed maps manually into your TripleA maps subfolder from 
           e);
       return List.of();
     }
-  }
-
-  private static List<MapDownloadItem> getMapDownloadListFromGithub() {
-    var result = new ArrayList<MapDownloadItem>();
-    for (var entry : download(GITHUB_MAP_DOWNLOAD_LIST)) {
-      result.add(
-          MapDownloadItem.builder()
-              .mapName(entry.getMapName())
-              .downloadUrl(entry.getUrl())
-              .description(entry.getDescription())
-              .previewImageUrl(entry.getImg())
-              .downloadSizeInBytes(-1L)
-              .lastCommitDateEpochMilli(0L)
-              .mapTags(List.of())
-              .build());
-    }
-    return result;
-  }
-
-  private static List<DownloadFileDescription> download(final String url) {
-    return DownloadConfiguration.contentReader()
-        .download(url, DownloadFileParser::parse)
-        .orElseGet(List::of);
   }
 }
