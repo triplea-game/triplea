dfd1d4c4d96a6e0b2bab1c0d431b49965351aa86
Dan Van Atta <dhvatta@gmail.com>
2026-09-04 18:20:11 -0700

    Convert misc lib and game-app tests to AssertJ (#14942)

--- lib/feign-common/src/test/java/org/triplea/http/client/JsonDecoderTest.java
+++ lib/feign-common/src/test/java/org/triplea/http/client/JsonDecoderTest.java
@@ -1,8 +1,6 @@
 package org.triplea.http.client;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.core.Is.is;
-import static org.hamcrest.core.IsNull.notNullValue;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import java.time.Instant;
 import java.time.LocalDateTime;
@@ -29,20 +27,20 @@ class JsonDecoderTest {
   void decoder() {
     final InstantExample event = JsonDecoder.decoder().fromJson(JSON_STRING, InstantExample.class);
 
-    assertThat(event.instant, notNullValue());
+    assertThat(event.instant).isNotNull();
 
     final LocalDateTime dateTime = LocalDateTime.ofInstant(event.instant, ZoneOffset.UTC);
-    assertThat(dateTime.getMonth(), is(Month.JUNE));
-    assertThat(dateTime.getDayOfMonth(), is(6));
-    assertThat(dateTime.getYear(), is(2019));
-    assertThat(dateTime.getHour(), is(4));
-    assertThat(dateTime.getMinute(), is(20));
+    assertThat(dateTime.getMonth()).isEqualTo(Month.JUNE);
+    assertThat(dateTime.getDayOfMonth()).isEqualTo(6);
+    assertThat(dateTime.getYear()).isEqualTo(2019);
+    assertThat(dateTime.getHour()).isEqualTo(4);
+    assertThat(dateTime.getMinute()).isEqualTo(20);
   }
 
   @Test
   void verifyEpochSecondAndNanoDecoding() {
     final InstantExample event = JsonDecoder.decoder().fromJson(JSON_STRING, InstantExample.class);
 
-    assertThat(event.instant, is(Instant.ofEpochSecond(1_559_794_806, 329_342_000)));
+    assertThat(event.instant).isEqualTo(Instant.ofEpochSecond(1_559_794_806, 329_342_000));
   }
 }
